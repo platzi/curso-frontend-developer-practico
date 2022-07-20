df1928bd1464ff7784547ecd1d2e0d7d1afe106c
Charles Duarte <charlesduarte97@gmail.com>
2022-07-20 11:39:08 -0400

    Finalizacion curso practico de JavaScript

--- main.js
+++ main.js
@@ -1,47 +1,59 @@
-// Desktop-Menu mostrar/ocultar
+// Desktop-Menu
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+// Menu-hamburguesa
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+// Shopping-Cart
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const myOrderDesktop = document.querySelector('#shoppingCartContainer');
+// Cards-container
+const cardsContainer = document.querySelector('.cards-container')
 
-// Añadimos un evento al hacer click en el menu Email
+// Product-Detail
+const productDetailContainer = document.querySelector('#productDetail');
+// Product-Detail-Close-Icon
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
+
+
+// Añadimos eventos al hacer click sobre el 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click',toggleMobileMenu);
+shoppingCart.addEventListener('click',toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside);
 
-// Añadimos un evento al hacer click en el menu Email
+
+// Añadimos una funcion a cada evento Click
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
     myOrderDesktop.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 }
 
-
-// Menu-hamburguesa mostrar/ocultar
-const burgerMenu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu')
-
-// Añadimos un evento al hacer click en el menu hamburguesa
-burgerMenu.addEventListener('click',toggleMobileMenu);
-
-// Creamos una función para el evento 'click' en menu hamburguesa
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
     // Cierra el carrito de compras al abrir el menu hamburguesa
     myOrderDesktop.classList.add('inactive');
-}
+    productDetailContainer.classList.add('inactive');
+}   
 
-
-// Shopping-Cart mostrar/ocultar
-const shoppingCart = document.querySelector('.navbar-shopping-cart')
-const myOrderDesktop = document.querySelector('#shoppingCartContainer')
-
-// Añadimos un evento al hacer click en el Shopping Cart
-shoppingCart.addEventListener('click',toggleCarritoAside);
-
-// Creamos una función para el evento 'click' en Shopping Cart
 function toggleCarritoAside(){
     myOrderDesktop.classList.toggle('inactive');
-     // Cierra el menu hamburguesa/email al abrir el carrito de compras
+     // Cierra el menu hamburguesa/email/productDetail al abrir el carrito de compras
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 }
 
+function openProductDetailAside() {
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive')
+}
+
+// Product-List
 const productList = [];
 productList.push({
     name: 'Bike',
@@ -60,25 +72,6 @@ productList.push({
     price: 1000,
     image: "https://falabella.scene7.com/is/image/Falabella/15028951_1?wid=1500&hei=1500&qlt=70"
 });
-productList.push({
-    name: 'Bike',
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-});
-
-productList.push({
-    name: 'Monitor',
-    price: 200,
-    image: "https://falabella.scene7.com/is/image/Falabella/15997897_1?wid=1500&hei=1500&qlt=70"
-});
-
-productList.push({
-    name: 'Macbook',
-    price: 1000,
-    image: "https://falabella.scene7.com/is/image/Falabella/15028951_1?wid=1500&hei=1500&qlt=70"
-});
-
-const cardsContainer = document.querySelector('.cards-container')
 
 function renderProducts(arr){
     for (product of productList){
@@ -88,6 +81,8 @@ function renderProducts(arr){
         const productImg = document.createElement('img');
         // product = {name, price, image} --> product.image
         productImg.setAttribute('src', product.image);
+
+        productImg.addEventListener('click',openProductDetailAside);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -118,5 +113,7 @@ function renderProducts(arr){
     }
 }
 
-
+function renderProductDetail(arr){
+    
+}
 renderProducts(productList);
