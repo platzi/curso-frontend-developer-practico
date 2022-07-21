34f57efd593b9dea4a336c3e2e041cdf58f4a238
Eduardo Luna <eolunas@gmail.com>
2022-07-21 18:48:13 -0400

    Mobile menu integrated and responsive improved

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+burgerMenu.addEventListener('click', toggleBurgerMenu)
 
 function toggleDesktopMenu() {
    desktopMenu.classList.toggle('inactive');
+}
+function toggleBurgerMenu() {
+   mobileMenu.classList.toggle('inactive');
 }
