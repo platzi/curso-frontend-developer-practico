1935ac0634d6a19776cc686cbd3108abd8e854a3
Carlos Dominguez <litosandres@hotmail.com>
2022-07-21 17:36:29 -0600

    agregamos el componente del carrito de compras al proyecto

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 const menuEmail = document.querySelector(".navbar-email")
 const menuHamIcon = document.querySelector('.menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+
 
 
 
 function toggleDesktopMenu() {
+    const isAsideClosed = !aside.classList.contains('inactive')
+
+    isAsideClosed && aside.classList.toggle('inactive')
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = !aside.classList.contains('inactive')
+
+    isAsideClosed && aside.classList.toggle('inactive')
+    
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = !mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = !desktopMenu.classList.contains('inactive')
+
+    isDesktopMenuClosed && desktopMenu.classList.toggle('inactive')
+    isMobileMenuClosed && mobileMenu.classList.toggle('inactive') 
+
+    aside.classList.toggle('inactive')
 }
