cf4607fabb603b3d39cb79e06f9f733b8bdbf8c2
Manuel Ducos <manuelducosp@gmail.com>
2022-07-20 22:51:55 -0300

    Incorporacion del carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-const navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const btnMobileMenu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const navEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const btnMobileMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const shoppingCartIcon = document.querySelector(".navbar-shopping-cart");
+const asideProductDetail = document.querySelector(".product-detail");
 
-navEmail.addEventListener('click', toggleDesktopMenu);
-btnMobileMenu.addEventListener('click', toggleMobileMenu)
+navEmail.addEventListener("click", toggleDesktopMenu);
+btnMobileMenu.addEventListener("click", toggleMobileMenu);
+shoppingCartIcon.addEventListener("click", toggleAsideProductDetail);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+  desktopMenu.classList.toggle("inactive");
+  asideProductDetail.classList.add("inactive");
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+  mobileMenu.classList.toggle("inactive");
+  asideProductDetail.classList.add("inactive");
+}
+
+function toggleAsideProductDetail() {
+  asideProductDetail.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
 }
