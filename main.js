f4ab90e635cdef95c6cfbaad5f3baf1f06a6434e
joshub <jos.ct101@gmail.com>
2022-07-21 23:51:11 -0500

    Uniendo Aside - My order al proyecto
    Signed-off-by: joshub <jos.ct101@gmail.com>

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const menuDesktop = document.querySelector('.inactive')
+const menuDesktop = document.querySelector('.desktop-menu');
+const menuMobile = document.querySelector('.mobile-menu');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
 const navEmail = document.querySelector('.navbar-email');
+const hamMenu = document.querySelector('.menu');
+const productDetailOrder = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', () => {
     menuDesktop.classList.toggle('inactive');
+    menuMobile.classList.add('inactive');
+    productDetailOrder.classList.add('inactive')
+});
+hamMenu.addEventListener('click', () => {
+    menuMobile.classList.toggle('inactive');
+    menuDesktop.classList.add('inactive');
+    productDetailOrder.classList.add('inactive')
+
+});
+cartIcon.addEventListener('click', () => {
+    productDetailOrder.classList.toggle('inactive');
+    menuMobile.classList.add('inactive');
+    menuDesktop.classList.add('inactive');
 });
