059baf3fb281e7a79359eca4f0e84e7def1aeef6
joserangel25 <jdrangel00@gmail.com>
2022-07-23 18:18:03 -0500

    se agrega funcionalidad en JS para abrir o cerrar menus, incluso el shopping car

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
+//Selectores del DOM
 const menuEmail = document.querySelector('.navbar-email');
-const menuContain = document.querySelector('.desktop-menu');
+const menuContainAccount = document.querySelector('.desktop-menu');
+const menuHamburguesa = document.querySelector('.menuIconHamburguesa');
+const menuContainMobile = document.querySelector('.mobile-menu');
+
+const menuIconCar = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', ()=> {
-  toggleMenu()
+  toggleMenu();
+  const isActiveAside = aside.classList.contains('inactive');
+  !isActiveAside && toggleMenuShoppingCar();
+});
+menuHamburguesa.addEventListener('click', ()=> {
+  toggleMenuHamburguesa();
+  const isActiveAside = aside.classList.contains('inactive');
+  !isActiveAside && toggleMenuShoppingCar();
+});
+menuIconCar.addEventListener('click', ()=> {
+  toggleMenuShoppingCar();
+  //Para cerrar el menu MObile izquierdo si está abierto
+  const isActiveMenuContainMobile = menuContainMobile.classList.contains('inactive');
+  !isActiveMenuContainMobile && toggleMenuHamburguesa();
+  //Para cerrar el menu Account si está abierto
+  const isActiveMenuAccount = menuContainAccount.classList.contains('inactive');
+  !isActiveMenuAccount && toggleMenu();
 });
 
-const toggleMenu = () => menuContain.classList.toggle('inactive');
+const toggleMenu = () => menuContainAccount.classList.toggle('inactive');
+const toggleMenuHamburguesa = () => menuContainMobile.classList.toggle('inactive');
+const toggleMenuShoppingCar = () => aside.classList.toggle('inactive');
