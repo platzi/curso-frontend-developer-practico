c9be8aff50b05906dd8c4259cd1bee9f5d474ccc
Geras4323 <german432yo@gmail.com>
2022-07-24 19:54:23 -0300

    Shopping Cart added

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-const menuEmail = document.querySelector(".navbar-email");
+const emailMenu = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-const burguerMenu = document.querySelector(".menu");
+
+const burguerMenuIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
+const cartMenuIcon = document.querySelector(".navbar-shopping-cart");
+const cart = document.querySelector(".product-detail");
+
+
+
+
+emailMenu.addEventListener("click", toggleDesktopMenu);
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+  cart.classList.add("inactive");
 }
 
-burguerMenu.addEventListener("click", toggleMobileMenu);
+burguerMenuIcon.addEventListener("click", toggleMobileMenu);
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
+  cart.classList.add("inactive");
+}
+
+cartMenuIcon.addEventListener("click", toggleCart);
+function toggleCart() {
+  cart.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
 }
