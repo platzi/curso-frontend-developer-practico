7a088b97ec078f1409e83ffedac4c79445c21cdd
Oggonei <oggonei@hotmail.com>
2022-07-24 22:28:45 -0500

    mobileMenu aded to navBar

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const BurguerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 
 navEmail.addEventListener('click',() => {
   desktopMenu.classList.toggle('inactive')
+})
+
+BurguerMenu.addEventListener('click',() => {
+  mobileMenu.classList.toggle('inactive')
 })
