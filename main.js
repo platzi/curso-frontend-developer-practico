2cdc6e327eb83e03fbf6b54a2abdd57cf49689ef
ulisescandela <ulisescandela99@gmail.com>
2022-07-25 12:13:43 -0500

    add mobile menu and media queries

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const navbarMail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarMail.addEventListener('click', toggleDesktopMenu);
+burgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     //Solución del instructor:
     //Este quita o pone la clase 'Inactive' dependiendo si ya la tiene
     desktopMenu.classList.toggle('inactive');
-    //MI SOLUCIÓN:
-    // let isNone = desktopMenu.style.display == "none";
-    // isNone ? desktopMenu.style.display = "block" : desktopMenu.style.display = "none";
+}
+
+function toggleMobileMenu() {
+    //Solución del instructor:
+    //Este quita o pone la clase 'Inactive' dependiendo si ya la tiene
+    mobileMenu.classList.toggle('inactive');
 }
