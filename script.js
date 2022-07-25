55801341d74265442a7ee7f6155b6a90f760bc20
Niko Raigoza <nikoraigoza@gmail.com>
2022-07-25 17:01:26 -0500

    Fusion del navbar con el carrito y los menu mobile y desktop

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuHamIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isCarritoClosed = aside.classList.contains('inactive')
+
+    if (!isCarritoClosed) {
+        aside.classList.add('inactive')
+    }
+    
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+    const isCarritoClosed = aside.classList.contains('inactive')
+
+    if (!isCarritoClosed) {
+        aside.classList.add('inactive')
+    }
+
+    menuMobile.classList.toggle('inactive')
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = menuMobile.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed) {
+        menuMobile.classList.add('inactive')
+    } else if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
 }
