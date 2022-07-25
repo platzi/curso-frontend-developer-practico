c84425bbb29afc4433053f8dc59c96162bcf5c6d
Eduar García <eduarfrancis11@gmail.com>
2022-07-24 22:31:13 -0600

    primeros cambios de menu destokp

---
+++ app.js
@@ -0,0 +1,8 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopmenu = document.querySelector('.desktop-menu');
+
+menuEmail = addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+    desktopmenu.classList.toggle('inactive');
+}
