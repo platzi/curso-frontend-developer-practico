b9c3fb9a20962e5b232185a27a93bbc7ab149184
Ariadna Bonive <ariadnabonive@gmail.com>
2022-07-25 18:43:35 -0300

    fusion del nav con el menu desplegable para mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuSmall = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenuMobile = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuSmall.addEventListener('click', toggleDesktopMenu);
+iconMenuMobile.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+
+};
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 
 };
