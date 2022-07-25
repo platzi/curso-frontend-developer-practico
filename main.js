9be660391cf197b3030893db9a23876624254f40
ulisescandela <ulisescandela99@gmail.com>
2022-07-25 12:52:13 -0500

    integrate cart and make it work with desktop menu and mobile menu

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 const navbarMail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const burgerIcon = document.querySelector('.menu');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const asideDetails = document.querySelector('.product-detail');
 
 navbarMail.addEventListener('click', toggleDesktopMenu);
 burgerIcon.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', toggleAsideDetails);
 
 function toggleDesktopMenu() {
-    //Solución del instructor:
-    //Este quita o pone la clase 'Inactive' dependiendo si ya la tiene
+    asideDetails.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    //Solución del instructor:
-    //Este quita o pone la clase 'Inactive' dependiendo si ya la tiene
+    desktopMenu.classList.add('inactive');
+    asideDetails.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAsideDetails() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    asideDetails.classList.toggle('inactive');
 }
