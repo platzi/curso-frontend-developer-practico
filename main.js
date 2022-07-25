13e37bd40c828368df85b61110d17416a816e635
Jose Rios <jrbernabe@gmail.com>
2022-07-25 16:49:05 -0400

    Add interactions between mobile menu, desktop menu and cart menu

--- main.js
+++ main.js
@@ -1,15 +1,47 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const cartMenuIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 mobileMenuIcon.addEventListener('click', toggleMobileMenu);
+cartMenuIcon.addEventListener('click', toggleCartMenu);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!isAsideClosed) {
+        aside.classList.toggle('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!isAsideClosed) {
+        aside.classList.toggle('inactive');
+    }
+    
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartMenu() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
+    
 }
