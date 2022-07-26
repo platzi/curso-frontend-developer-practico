eabe276c6a8ccc13cadae68315666c98f1dc7dd1
Balbin <xxbalbin@gmail.com>
2022-07-26 15:15:48 -0500

    mobile menu fixed

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const menuEmail = document.querySelector('.navbar-right'); // las clases de CSS se 
 const desktopMenu = document.querySelector('.desktop-menu'); // importan con un punto al inicio
+const menuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu); // El segundo parametro es una funcion a ejecutarse cuando se hace click (primero parametro)
 
@@ -8,5 +10,10 @@ function toggleDesktopMenu()
     desktopMenu.classList.toggle('inactive');
 }
 
+menuIcon.addEventListener('click', toggleMobileMenu);
 
+function toggleMobileMenu()
+{
+    mobileMenu.classList.toggle('inactive');
+}
 
