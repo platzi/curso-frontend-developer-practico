43420bf3bf60c0dd63bd0711843d891bee6e8f8b
francisco moreno <guerrerometal123@gmail.com>
2022-07-26 14:01:21 -0500

    despliegue del menu mobil con js y adicion de @media

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const logoMenu = document.querySelector('.menu')
+const mobilMenu = document.querySelector('.mobile-menu')
 
 navEmail.addEventListener('click', toggleDesktopMenu);
-
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive')
+}
+logoMenu.addEventListener('click', toggleMobilMenu)
+function toggleMobilMenu(){
+    mobilMenu.classList.toggle('inactive')
 }
