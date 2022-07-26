971a25ec4e00e7ebd0eb311a9d6a8fd45693534f
Balbin <xxbalbin@gmail.com>
2022-07-26 18:08:14 -0500

    shopping cart menu logic

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
-const menuEmail = document.querySelector('.navbar-right'); // las clases de CSS se 
+const menuEmail = document.querySelector('.navbar-email'); // las clases de CSS se 
 const desktopMenu = document.querySelector('.desktop-menu'); // importan con un punto al inicio
 const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const asideMenu = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu); // El segundo parametro es una funcion a ejecutarse cuando se hace click (primero parametro)
+menuIcon.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu()
 {
+    const isAsideMenuClosed = asideMenu.classList.contains('inactive');
+
+    if(!isAsideMenuClosed)
+    {
+        asideMenu.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
-menuIcon.addEventListener('click', toggleMobileMenu);
-
 function toggleMobileMenu()
 {
+    const isAsideMenuClosed = asideMenu.classList.contains('inactive');
+
+    if(!isAsideMenuClosed)
+    {
+        asideMenu.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 }
 
+function toggleCarritoAside()
+{
+    desktopMenu.classList.add('inactive'); // De esta forma siempre que entre
+                                         // a esta funcion, se cierra el desktop Menu
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    
+    if(!isMobileMenuClosed)
+    {
+        mobileMenu.classList.add('inactive');
+    }
+    asideMenu.classList.toggle('inactive');
+}
