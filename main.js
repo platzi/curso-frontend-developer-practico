7a2522f7c9e2cc8603363147cb299e112d3b6bcf
Piña <ale.casp88@gmail.com>
2022-07-26 12:11:09 -0500

    MobileDesktop Asaid y soluciones menus encimados

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector('.desktop-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobilepMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.toggle('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobilepMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.toggle('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed =  mobileMenu.classList.contains('inactive');
+    const isDesktopMenu = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.toggle('inactive');
+    }
+
+    if (!isDesktopMenu) {
+        desktopMenu.classList.toggle('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
