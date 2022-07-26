2c1b78d4213c41e6625525944c2a2e255d3a68a4
jose perez <josheperzng@gmail.com>
2022-07-26 15:45:29 -0400

    add shopping cart window

--- script.js
+++ script.js
@@ -3,18 +3,35 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileIconMenu = document.querySelector('.menu');
 const mainMenuMobile = document.querySelector('.mobile-menu');
+const cartIconShopping = document.querySelector('.navbar-shopping-cart');
+const cartShoppingList = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', showDesktopMenu);
 mobileIconMenu.addEventListener('click', showMobileMenu);
+cartIconShopping.addEventListener('click', showListCartShopping);
+
 
 function showDesktopMenu(){
   desktopMenu.classList.toggle('inactive');
+  cartShoppingList.classList.add('inactive');
 };
 
 function showMobileMenu(){
   mainMenuMobile.classList.toggle('inactive');
+cartShoppingList.classList.add('inactive')
 };
+
+function showListCartShopping(){
+ cartShoppingList.classList.toggle('inactive');
+ mainMenuMobile.classList.add('inactive');
+
+ const closedDestopMenu = desktopMenu.classList.contains('inactive');
+ ;
+ if (!closedDestopMenu){
+  desktopMenu.classList.toggle('inactive');
+ }
  
+}
 
 
 
