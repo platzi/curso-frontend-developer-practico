298773d477cc9ddf75ec52e07c18de071a549c71
Irving Vaquero <ivaqueroflores@gmail.com>
2022-07-26 16:11:47 -0600

    Agrege funcionalidad de visualizacion de productos

--- main.js
+++ main.js
@@ -12,7 +12,13 @@ let countCarrito        = document.querySelector('.navbar-shopping-cart div')
 let totalCarrito        = document.querySelector('.total')
 let modal               = document.querySelector('.modal')
 let cerrarModal         = document.querySelector('.boton-modal')
+let productImageInfor   = document.querySelector('.product-detail-left > img:nth-child(2)')
+let labelPriceInfo      = document.querySelector('.product-info-left p:nth-child(1)')
+let labelNameInfo       = document.querySelector('.product-info-left p:nth-child(2)')
+let labelInforInfo      = document.querySelector('.product-info-left p:nth-child(3)')
+let buttonInfo          = document.querySelector('.add-to-cart-button')
 let productosEnCarrito  = []
+let productoACarrito    = []
 
 countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
 totalCarrito.innerText = '$0.00'
@@ -21,9 +27,13 @@ emailMenu.addEventListener('click', toggleMenu);
 iconMenuMobile.addEventListener('click', toggleMenuMobile);
 iconShopping.addEventListener('click', toggleAsideShopping)
 productDetailClose.addEventListener('click',closeProductDetail)
+buttonInfo.addEventListener('click', butonClick)
 cerrarModal.addEventListener('click', closeModal)
 
 
+function butonClick(){
+    agregarCarrito(productoACarrito)
+}
 function toggleMenu(){
     productDetailLeft.classList.add('inactive')
     asideShopping.classList.add('inactive');
@@ -52,6 +62,7 @@ function openProductDetail(){
 }
 
 function closeProductDetail(){
+    productoACarrito = []
     asideShopping.classList.add('inactive');
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
@@ -70,49 +81,46 @@ function closeModal(){
 
 let productList = []
 productList.push({
-    name:"bake",
-    price:120,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name:"otro",
+    name:"house",
     price:180,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    image:"https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Casa en fraccionamiento privado en excelente zona residencial con caseta de vigilancia y guardia, a 15 minutos del centro de la ciudad y muy cerca de centros comerciales y escuelas.'
 })
 productList.push({
-    name:"bake",
-    price:150,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name:"bake",
+    name:"house model 2",
     price:120,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    image: "https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Renta: $ 30,000\nMetros de construcción: 297m2\nMetros de Terreno: 250m2'
 })
 productList.push({
-    name:"bake",
-    price:120,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    name:"house model 3",
+    price:200,
+    image:"https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Oportunidad de casa de dos plantas y con excelente ubicación, cerca de avenidas principales de la ciudad a tres minutos de la Jose María Iglesias, así como de centros comerciales, escuelas y centros de esparcimiento.'
 })
 productList.push({
-    name:"bake",
-    price:120,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    name:"house model 4",
+    price:895,
+    image:"https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Casa en Renta recién Remodelada en Paseo de las Misiones'
 })
 productList.push({
-    name:"bake",
-    price:120,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    name:"house model 5",
+    price:1500,
+    image:"https://images.pexels.com/photos/1438832/pexels-photo-1438832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Rento casa amueblada en Fraccionamiento Quintas del Sol, muy cerca de la Av. Ortíz Mena, terreno amplio, cochera con capacidad de hasta 4 carros, sala, comedor, cocina, lavandería, medio baño, recamara completa, todo en planta baja'
 })
 productList.push({
-    name:"bake",
-    price:120,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    name:"house model 6",
+    price:100,
+    image:"https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Propiedad en venta o renta ubicada sobre Av. Mirador'
 })
 productList.push({
-    name:"bake",
-    price:120,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    name:"house model 7",
+    price:3620,
+    image:"https://images.pexels.com/photos/53610/large-home-residential-house-architecture-53610.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Propiedad en venta o renta ubicada sobre Av. Mirador'
 })
 
 function renderListProduct(productList){
@@ -144,7 +152,10 @@ function renderListProduct(productList){
 
         divcardscontainer.appendChild(divproductCard)
 
-        imgproductcard.addEventListener('click', openProductDetail);
+        imgproductcard.addEventListener('click', function(){
+            mostrarInfoProduct(product.image, product.price, product.name, product.desc)
+            openProductDetail()
+        });
 
 
         imgproductinfo.addEventListener('click', function(){
@@ -162,6 +173,7 @@ function renderListProduct(productList){
 /* Funcion para agregar productos al carrito */
 function agregarCarrito(producto){
     if(verificarProducto(producto[0].name)){
+        productoACarrito= []
         return modal.style.display = 'grid'
     }
 
@@ -202,6 +214,19 @@ function agregarCarrito(producto){
         productosEnCarrito.splice(productosEnCarrito.indexOf(producto[0].name),1)
     }) 
 
+}
+
+function mostrarInfoProduct(imagen, precio, nombre, descripcion){
+    productImageInfor.setAttribute('src', imagen)
+    labelPriceInfo.innerText = '$' + precio
+    labelNameInfo.innerText = nombre
+    labelInforInfo.innerText = descripcion
+    productoACarrito = []
+    productoACarrito.push({
+        name: nombre,
+        price: precio,
+        image: imagen
+    })
 }
 
 renderListProduct(productList);
