fb26fc490bbc74d14fa0ac7eb257fa99da744ef9
jose perez <josheperzng@gmail.com>
2022-07-26 13:58:01 -0400

    add the main menu mobile an icon menu mobile

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileIconMenu = document.querySelector('.menu');
+const mainMenuMobile = document.querySelector('.mobile-menu');
 
-navEmail.addEventListener('mouseover', showDesktopMenu);
+navEmail.addEventListener('click', showDesktopMenu);
+mobileIconMenu.addEventListener('click', showMobileMenu);
 
 function showDesktopMenu(){
   desktopMenu.classList.toggle('inactive');
 };
+
+function showMobileMenu(){
+  mainMenuMobile.classList.toggle('inactive');
+};
+ 
+
+
+
+
+
+
+
+
