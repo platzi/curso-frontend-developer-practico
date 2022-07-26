c71036e97d11b114db687362170e3057bfd41661
Ariadna Bonive <ariadnabonive@gmail.com>
2022-07-26 11:17:23 -0300

    se maquetó el cards-container con JS

--- main.js
+++ main.js
@@ -1,17 +1,104 @@
-const menuSmall = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const iconMenuMobile = document.querySelector('.menu');
+
+const menuIconMobile = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-menuSmall.addEventListener('click', toggleDesktopMenu);
-iconMenuMobile.addEventListener('click', toggleMobileMenu);
+const menuIconCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer= document.querySelector('.cards-container');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuIconMobile.addEventListener('click', toggleMobileMenu);
+menuIconCart.addEventListener('click',toggleCarritoAside)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
-
+    aside.classList.add('inactive');
 };
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
-
+    aside.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
 };
+
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+};
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price:120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Mouse',
+    price:97,
+    image: 'https://images.unsplash.com/photo-1527814050087-3793815479db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1vdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+});
+
+productList.push({
+    name: 'Notebook',
+    price:600,
+    image: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
+});
+
+/* <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+      <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+      </div>
+        <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div> A continuación maquetamos este bloque de códico en JS */
+
+function renderProduct(arrayList) {
+    for (product of arrayList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        //product = {name, price, image} --> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProduct(productList);
+
