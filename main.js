abc8f87929e296e822385795ac3adacb9bc62258
je-martini <jesusmartini2017@gmail.com>
2022-07-27 09:00:50 -0400

    se agrego la funcion para desplegar el menu hambur en mobile

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
 
 navBarEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
+
+hamburgerMenu.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
