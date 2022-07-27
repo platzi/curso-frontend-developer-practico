0d4df6e83d5aa892eebc4fb6057b10372e254c22
mrivero40 <matiasrivero40@hotmail.com>
2022-07-26 22:14:39 -0300

    Correción de algunos estilos en css

--- main.js
+++ main.js
@@ -1,104 +1,74 @@
+/* MENU EN DESKTOP */
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-menuEmail.addEventListener('click', togleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
 
-function togleDesktopMenu() {
+function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     orderShopping.classList.add('inactive');
     shoppingDetail.classList.add('inactive');
 };
 
-
+/* MENU EN MOBILE */
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-menuHamIcon.addEventListener('click', togleMobileMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
-function togleMobileMenu() {
+function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
     desktopMenu.classList.add('inactive');
     orderShopping.classList.add('inactive');
     shoppingDetail.classList.add('inactive');
 };
 
-
+/* DETALLE DE CARRITO DE COMPRAS */
 const menuShopping = document.querySelector('.navbar-shopping-cart');
 const orderShopping = document.querySelector('.product-detail');
-menuShopping.addEventListener('click', togleShoppingMenu);
+menuShopping.addEventListener('click', toggleShoppingMenu);
 
-function togleShoppingMenu() {
+function toggleShoppingMenu() {
     orderShopping.classList.toggle('inactive');
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
     shoppingDetail.classList.add('inactive');
 };
 
-/* DETALLE DE COMPRAS */
-const shoppingDetail = document.querySelector('.shopping-detail');
-const shoppingDetailImg = document.querySelector('.cards-container');
-const shoppingDetailClose = document.querySelector('.shopping-detail-close');
-shoppingDetailClose.addEventListener('click', closeShopping);
-shoppingDetailImg.addEventListener('click', openShopping);
-function openShopping() {
-    shoppingDetail.classList.remove('inactive');
-    orderShopping.classList.add('inactive');
-}
-function closeShopping() {
-    shoppingDetail.classList.add('inactive');
-}
-
-
-
-
-
+/* ARRAY Y LISTA DE PRODUCTOS */
 const productList = [];
 productList.push({
-    name: 'Bici BMX',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name: 'Auricular MSI Gammer GH30',
+    price: 26.63,
+    image: 'https://mendohardware.com.ar/img/auricularMsiGamer.png',
 },
 {
-    name: 'Casco',
-    price: 15,
-    image: 'http://d3ugyf2ht6aenh.cloudfront.net/stores/628/448/products/ktm-factory-youth-01-8d0c8fabf156f46fe915258806367423-640-0.jpg',
+    name: 'Teclado y Mouse Gigabyte',
+    price: 6.61,
+    image: 'https://mendohardware.com.ar/img/kitGigabyte.png',
 },
 {
-    name: 'Inflador',
-    price: 10,
-    image: 'https://borabikes.com.ar/51998-large_default/Inflador-Bicicleta-Medidor-Presion-Beto-Doble-Accion-003ag.jpg',
+    name: 'Usb Wifi 300mbps',
+    price: 2.63,
+    image: 'https://mendohardware.com.ar/img/usbWifi300.png',
 },
 {
-    name: 'Bici BMX',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name: 'Mouse Inalámbrico Trust',
+    price: 3.66,
+    image: 'https://mendohardware.com.ar/img/mouseTrust.png',
 },
 {
-    name: 'Casco',
-    price: 15,
-    image: 'http://d3ugyf2ht6aenh.cloudfront.net/stores/628/448/products/ktm-factory-youth-01-8d0c8fabf156f46fe915258806367423-640-0.jpg',
+    name: 'Disco Solido 240gb',
+    price: 19.66,
+    image: 'https://mendohardware.com.ar/img/ssd240Gigabyte.png',
 },
 {
-    name: 'Inflador',
-    price: 10,
-    image: 'https://borabikes.com.ar/51998-large_default/Inflador-Bicicleta-Medidor-Presion-Beto-Doble-Accion-003ag.jpg',
-},
-{
-    name: 'Bici BMX',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-},
-{
-    name: 'Casco',
-    price: 15,
-    image: 'http://d3ugyf2ht6aenh.cloudfront.net/stores/628/448/products/ktm-factory-youth-01-8d0c8fabf156f46fe915258806367423-640-0.jpg',
-},
-{
-    name: 'Inflador',
-    price: 10,
-    image: 'https://borabikes.com.ar/51998-large_default/Inflador-Bicicleta-Medidor-Presion-Beto-Doble-Accion-003ag.jpg',
+    name: 'Auricular Xiaomi Basic 2',
+    price: 13.31,
+    image: 'https://mendohardware.com.ar/img/auriXiaomiBasic2.png',
 },
 );
 
+/* FUNCION DE RENDERIZADO DE PRODUCTOS DESDE JS */
 const cardsContainer = document.querySelector('.cards-container');
 function renderProducts (array) {
     for (product of array) {
@@ -107,13 +77,14 @@ function renderProducts (array) {
 
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openShopping);
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
 
         const productInfoDiv = document.createElement('div');
         const productPrice = document.createElement('p');
-        productPrice.innerText = `$ ${product.price}`;
+        productPrice.innerText = `u$s ${product.price}`;
         const productName = document.createElement('p');
         productName.innerText = `${product.name}`;
 
@@ -133,6 +104,19 @@ function renderProducts (array) {
 };
 renderProducts(productList);
 
+/* DETALLE DE UN PRODUCTO */
+const shoppingDetail = document.querySelector('.shopping-detail');
+const shoppingDetailClose = document.querySelector('.shopping-detail-close');
+shoppingDetailClose.addEventListener('click', closeShopping);
+function openShopping() {
+    shoppingDetail.classList.remove('inactive');
+    orderShopping.classList.add('inactive');
+};
+function closeShopping() {
+    shoppingDetail.classList.add('inactive');
+};
+
+/* HTML RENDERIZADO EN LA FUNCIÓN renderProducts */
 /* <section class="main-container">
     <div class="cards-container">
       <div class="product-card">
