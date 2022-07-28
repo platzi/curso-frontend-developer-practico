8a2455e0410859f904fe7c2044398366560468be
die-h <diegohermida94@gmail.com>
2022-07-28 00:22:13 -0300

    integration of class 13

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
+/* Variables para desplegar menu en desktop */
 const navMail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+
+/* Variables para desplegar menu en mobile */
 const burgerMenu = document.querySelector(".burgerMenuButton");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+/* Variables para desplegar el shopping-cart */
+const shoppingCartButton = document.querySelector(".navbar-shopping-cart");
+const shoppingCartAside = document.querySelector(".product-detail");
+
+console.log(shoppingCartAside);
+console.log(shoppingCartButton);
+console.log(burgerMenu);
+
+
 navMail.addEventListener("click",() => {
     desktopMenu.classList.toggle("inactive");
+    shoppingCartAside.classList.add("inactive");
 });
 
 burgerMenu.addEventListener("click",() => {
     mobileMenu.classList.toggle("inactive");
+    shoppingCartAside.classList.add("inactive");
+});
+
+shoppingCartButton.addEventListener("click",() => {
+    shoppingCartAside.classList.toggle("inactive");
+    mobileMenu.classList.add("inactive");
 });
