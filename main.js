95b83e3b4b5ff4a09e76b74698199c4075046140
Andres Sepulveda <andresfelipesepulveda@live.com>
2022-07-28 09:52:05 -0500

    mobile menu added

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 
 const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-navBarEmail.addEventListener('click', () => {
-  desktopMenu.classList.toggle('inactive')
-})
+const menuMobile = document.querySelector('.mobile-menu');
+const mobileBurger = document.querySelector('.menu');
+
+
+mobileBurger.addEventListener('click', () => menuMobile.classList.toggle('slide-right'));
+navBarEmail.addEventListener('click', () => desktopMenu.classList.toggle('inactive'))
