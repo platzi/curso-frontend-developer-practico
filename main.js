b50fa7a7cb1d6989088bca6266370d2d149b0a24
die-h <diegohermida94@gmail.com>
2022-07-27 22:36:33 -0300

    integration of class 8 to index.html

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const navMail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".burgerMenuButton");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-navMail.addEventListener("click",toggleDesktopMenu);
-
-function toggleDesktopMenu() {
+navMail.addEventListener("click",() => {
     desktopMenu.classList.toggle("inactive");
-}
+});
+
+burgerMenu.addEventListener("click",() => {
+    mobileMenu.classList.toggle("inactive");
+});
