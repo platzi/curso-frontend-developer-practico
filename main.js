4b5aa90e1dd7c4d3229ee704ddb1555651be3782
Juan Ramirez <juanchoramirez2001@gmail.com>
2022-07-29 13:32:59 -0500

    Haciendo peticion a api e imrimiendo los productos en el html

--- main.js
+++ main.js
@@ -5,6 +5,57 @@ const mobileMenu = document.querySelector('.mobile-menu')
 const menuIconCar = document.querySelector('.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
 
+
+const createProductsComponents = (data) => {
+  const cardsContainer = document.querySelector('.cards-container')
+  data.forEach(product => {
+  const productCard = document.createElement('div');
+  productCard.classList.add('product-card');
+
+  const productImg = document.createElement('img');
+  productImg.setAttribute('src', product.images[0]);
+
+  const productInfo = document.createElement('div');
+  productInfo.classList.add('product-info');
+
+  const productInfoDiv = document.createElement('div');
+
+  const productPrice = document.createElement('p');
+  productPrice.innerText = '$' + product.price;
+  const productName = document.createElement('p');
+  productName.innerText = product.title;
+
+  productInfoDiv.appendChild(productPrice);
+  productInfoDiv.appendChild(productName);
+
+  const productInfoFigure = document.createElement('figure');
+  const productImgCart = document.createElement('img');
+  productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+  productInfoFigure.appendChild(productImgCart);
+
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard);
+ })
+
+}
+
+function getProducts(){
+  const API = 'https://api.escuelajs.co/api/v1/products'
+  
+  fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      createProductsComponents(data)
+    })
+} getProducts()
+
+
 navEmail.addEventListener('click', () => {
   desktopMenu.classList.toggle('inactive')
   isCarClose()
