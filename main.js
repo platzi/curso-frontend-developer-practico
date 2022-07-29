0c7441c59b2bad72d561c15c27d7769037d8d79a
Erick Rojas <erickrojasf97@gmail.com>
2022-07-28 19:01:25 -0500

    Fusion del menu mobile

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-menuEmail.addEventListener('click', toggleDesktopMenu)
+const burguerMenu= document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+burguerMenu.addEventListener('click', toggleMobileMenu);
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
