9be0ec7823827857757c9a9e9f7c6151ab7b6e26
Marianela Hernandez <marrowlove2817@gmail.com>
2022-07-30 00:51:18 +0100

    fusion del navbar!

---
+++ main.js
@@ -0,0 +1,8 @@
+const navEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+
+navEmail.addEventListener("click", toggleDesktopMenu);
+
+function toggleDesktopMenu(params) {
+  desktopMenu.classList.toggle("inactive");
+}
