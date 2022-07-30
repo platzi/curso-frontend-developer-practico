7585a889c5967c4e6a96e4fa2eec0a5cb037cb9a
Mauricio-Rubio <mau.r.h54@gmail.com>
2022-07-30 14:43:22 -0500

    Menu mobile

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const $ = (element) => document.querySelector(element);
 const email = $('.navbar-email');
 const desktopMenu= $('.desktop-menu');
+const menu = $('.menu');
+const mobileMenu = $('.mobile-menu');
+console.log({menu});
+
 email.addEventListener('click', toggleDesktop)
+menu.addEventListener('click', toggleMenu )
 function toggleDesktop() {
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMenu() {
+    mobileMenu.classList.toggle('slide-right-menu');
 }
