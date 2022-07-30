1b4cd0d94ef8da4e44af9e3b346f17276bb79c6d
Daniela Stornelli <dsstornelli@gmail.com>
2022-07-30 13:08:13 -0300

    Detalles de un producto: fusionamos dos componentes que se parecen demasiado entre sí

--- main.js
+++ main.js
@@ -3,48 +3,48 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container')
 
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguesa.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside);
-aside.addEventListener('click', toggleAside);
+menuCarritoIcon.addEventListener('click', toggleCarritoShoppingCartContainer);
+shoppingCartContainer.addEventListener('click', toggleShoppingCartContainer);
 
 
-function toggleDesktopMenu () { //Si esta abierto el aside y presiono el DesktopMenu...
-    aside.classList.add('inactive'); //el aside se va a cerrar. 
+function toggleDesktopMenu () { //Si esta abierto el shoppingCartContainer y presiono el DesktopMenu...
+    shoppingCartContainer.classList.add('inactive'); //el shoppingCartContainer se va a cerrar. 
     desktopMenu.classList.toggle('inactive');
 }
 
 
-function toggleAside () {   //al clickear Aside
+function toggleShoppingCartContainer () {   //al clickear Aside
     desktopMenu.classList.add('inactive'); //Si el desktop estaba abierto, se cerrará
-    aside.classList.toggle('inactive'); //Y el aside se abrirá.
+    shoppingCartContainer.classList.toggle('inactive'); //Y el aside se abrirá.
 }
 
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isShoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if (!isAsideClosed) {
-        aside.classList.add ('inactive');
+    if (!isShoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add ('inactive');
     }
     
     mobileMenu.classList.toggle('inactive');
 }
 
 
-function toggleCarritoAside() {
+function toggleCarritoShoppingCartContainer() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add ('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 
