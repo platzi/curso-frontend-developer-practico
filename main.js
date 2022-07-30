6b74884b38ee90fc8360e9ddfeeb7d8b65227209
Marianela Hernandez <marrowlove2817@gmail.com>
2022-07-30 16:23:12 +0100

    fusion en mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
+burgerMenuIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(params) {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(params) {
+  mobileMenu.classList.toggle("inactive");
 }
