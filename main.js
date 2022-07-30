26edf8e2afb5437e661f75ed48a0b16ec885df67
je-martini <jesusmartini2017@gmail.com>
2022-07-30 09:59:24 -0400

    se le agrego la funcionabilidad de aparecer o desaparecer a los botones de acuerdo a si chocan entre si o no

--- main.js
+++ main.js
@@ -1,17 +1,54 @@
+// menu flotante del correo
 const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 const hamburgerMenu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const shoppingCar = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+
 
 navBarEmail.addEventListener("click", toggleDesktopMenu);
 
+hamburgerMenu.addEventListener("click", toggleMobileMenu);
+
+shoppingCar.addEventListener("click", toggleProductDetail);
+
+
+
 function toggleDesktopMenu() {
+    const isProductDetailClosed = productDetail.classList.contains('inactive') ;
+
+    if (!isProductDetailClosed){
+        productDetail.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
-hamburgerMenu.addEventListener("click", toggleMobileMenu);
-
 function toggleMobileMenu() {
+    const isProductDetailClosed = productDetail.classList.contains('inactive') ;
+
+    if (!isProductDetailClosed){
+        productDetail.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetail() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive') ;
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    };
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    productDetail.classList.toggle('inactive');
 }
 
