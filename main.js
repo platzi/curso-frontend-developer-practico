ff04aed378101f0032fe82ca151e4b03c3b3553a
alebeta <alebeta0000@gmail.com>
2022-07-30 15:49:51 -0500

    Fusion del menu en Mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIco = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIco.addEventListener('click', toggleMobilepMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobilepMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
