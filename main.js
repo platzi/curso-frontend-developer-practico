db6ab75e0488890ffb442541845720fca6ac3e2c
alebeta <alebeta0000@gmail.com>
2022-07-30 16:59:37 -0500

    Integracion del Carrito de Compras en el menu Desktop y Mobile

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIco = document.querySelector('.navbar-shopping-cart');
 const menuHamIco = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIco.addEventListener('click', toggleMobilepMenu);
+menuHamIco.addEventListener('click', toggleMobileMenu);
+menuCarritoIco.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+  
+    if (!isAsideClosed) {
+      aside.classList.add('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
-}
-
-function toggleMobilepMenu() {
+  }
+  
+  function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+  
+    if (!isAsideClosed) {
+      aside.classList.add('inactive'); 
+    }
+    
     mobileMenu.classList.toggle('inactive');
-}
+  }
+  
+  function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+      mobileMenu.classList.add('inactive'); 
+    }
+    
+    aside.classList.toggle('inactive');
+  }
