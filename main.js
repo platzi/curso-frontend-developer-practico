5dc9484f1883c5c3ecfeb26ece896e3f15f17e28
Jhonney Eduardo <Jhonneycp@gmail.com>
2022-07-30 14:10:59 -0300

    adicion de los toggles

--- main.js
+++ main.js
@@ -5,86 +5,111 @@ const menu = document.querySelector('.menu')
 const navbarShoppingCard = document.querySelector('.navbar-shopping-cart')
 const menuMobile = document.querySelector('.mobile-menu')
 const productDetail = document.querySelector('.product-detail')
+const productDetailTwo = document.querySelector('.product-detail-two')
+const productDetailTwoClose = document.querySelector('.product-detail-two-close')
 
 
 const toggleDesktopMenu = () =>{
     const isProductDetailClosed = productDetail.classList.contains('inactive')
+    const isProductDetailTwoClosed = productDetailTwo.classList.contains('inactive')
 
-    if(!isProductDetailClosed){
+    if(!isProductDetailClosed || !isProductDetailTwoClosed){
         productDetail.classList.add('inactive')
+        productDetailTwo.classList.add('inactive')
     }
-
+    
     desktopMenu.classList.toggle('inactive')
 }
 
 const toggleMenuMobile = () =>{
     const isProductDetailClosed = productDetail.classList.contains('inactive')
-
-    if(!isProductDetailClosed){
+    const isProductDetailTwoClosed = productDetailTwo.classList.contains('inactive')
+    
+    if(!isProductDetailClosed || !isProductDetailTwoClosed){
         productDetail.classList.add('inactive')
+        productDetailTwo.classList.add('inactive')
     }
-
+    
     menuMobile.classList.toggle('inactive')
 }
 
 const toggleProductDetail = () =>{
     const isMenuMobileClosed = menuMobile.classList.contains('inactive')
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
-
-    if(!isMenuMobileClosed || !isDesktopMenuClosed){
+    const isProductDetailTwoClosed = productDetailTwo.classList.contains('inactive')
+    
+    if(!isMenuMobileClosed || !isDesktopMenuClosed || !isProductDetailTwoClosed ){
         menuMobile.classList.add('inactive')
         desktopMenu.classList.add('inactive')
+        productDetailTwo.classList.add('inactive')
+    }
+    
+    productDetail.classList.toggle('inactive')
+}
+
+const closeProductDetail = () =>{
+    productDetailTwo.classList.contains('inactive')? null : productDetailTwo.classList.add('inactive')
+}
+
+
+const openProductDetailAside = () =>{
+    const isMenuMobileClosed = menuMobile.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    const isProductDetailClosed = productDetail.classList.contains('inactive')
+
+    
+
+    if(!isMenuMobileClosed || !isDesktopMenuClosed || !isProductDetailClosed){
+        menuMobile.classList.add('inactive')
+        desktopMenu.classList.add('inactive')
+        productDetail.classList.add('inactive')
     }
 
-    productDetail.classList.toggle('inactive')
+    productDetailTwo.classList.remove('inactive')
 }
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
 menu.addEventListener('click', toggleMenuMobile)
 navbarShoppingCard.addEventListener('click', toggleProductDetail)
+productDetailTwoClose.addEventListener('click', closeProductDetail)
 
-const productList = []
-
-productList.push({
+const productList = [{
     name: 'Bike',
     price: '120',
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
-productList.push({
+},{
     name: 'Bike',
     price: '120',
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
-productList.push({
+},{
     name: 'Bike',
     price: '120',
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
-productList.push({
+},{
     name: 'Bike',
     price: '120',
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
-productList.push({
+},{
     name: 'Bike',
     price: '120',
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+}
+]
 
 const items = productList.map(it=>
     `
     <div class="product-card">
-    <img src=${it.image} alt="">
+    <img id="img" src=${it.image} alt="" onclick="openProductDetailAside()">
     <div class="product-info">
-      <div>
-        <p>${it.price}</p>
-        <p>${it.name}</p>
-      </div>
-      <figure>
-        <img src="./icons/bt_add_to_cart.svg" alt="">
-      </figure>
+    <div>
+    <p>${it.price}</p>
+    <p>${it.name}</p>
     </div>
-  </div>
-    `).join('')
-
+    <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+        </div>
+        </div>
+            `).join('')
+        
 cardsContainer.innerHTML= items
