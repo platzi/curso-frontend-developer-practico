50a4cedb4f74d11e896bd6ae7fbfd62eefdf7b02
maxiferado <maxiferado@gmail.com>
2022-07-30 21:50:27 -0500

    Ultimo commit de la noche despues de lograr Tails

--- main.js
+++ main.js
@@ -3,41 +3,70 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHam = document.querySelector('.mobile-menu');
 const menuHamIcon = document.querySelector('.menu');
 const carIcon = document.querySelector('.navbar-shopping-cart');
-const menuCar = document.querySelector('.product-detail')
+const menuCar = document.querySelector('.product-detail');
 let validador = menuEmail.classList.contains("inactive");
 
 
-menuEmail.addEventListener('click',toggleDesktopMenu);
 
+//menuEmail.addEventListener('click',toggleDesktopMenu);
 
-function toggleDesktopMenu(){ 
-    var toggle = desktopMenu.classList.contains("inactive");
-   if (toggle){
-    desktopMenu.classList.remove("inactive");
-   }else if(!toggle){
-    desktopMenu.classList.add("inactive");
-   }  
-}
-
-// Solucion profesor
-
-// menuEmail.addEventListener('click',toggleProfesor);
-
-// function toggleProfesor() {
-
-//     desktopMenu.classList.toggle('inactive');
-
+// function toggleDesktopMenu(){ 
+//     var toggle = desktopMenu.classList.contains("inactive");
+//    if (toggle){
+//     desktopMenu.classList.remove("inactive");
+//    }else if(!toggle){
+//     desktopMenu.classList.add("inactive");
+//    }  
 // }
+
+//Solucion profesor
+
+menuEmail.addEventListener('click',toggleProfesor);
+
+function toggleProfesor() {
+    const menuCarcheck = menuCar.classList.contains('inactive');
+
+    if(!menuCarcheck){        
+        menuCar.classList.add('inactive');
+    }
+
+
+    desktopMenu.classList.toggle('inactive');
+
+}
 
 
 menuHamIcon.addEventListener('click',inactiveHamMenu);
 
-function inactiveHamMenu() {    
+function inactiveHamMenu() {   
+    const menuCarcheck = menuCar.classList.contains('inactive');
+     
+
+    if(!menuCarcheck){
+
+        menuCar.classList.add('inactive');
+
+    }
+
+   
+    
     menuHam.classList.toggle('inactive');
 }
 
 carIcon.addEventListener('click', carMenuActive);
 
 function carMenuActive(){
-    menuCar.classList.toggle('inactive')
+    const menuHamcheck = menuHam.classList.contains('inactive'); 
+    const isdesktopMenuOpend = desktopMenu.classList.contains('inactive');
+
+    if(!menuHamcheck){
+        menuHam.classList.add('inactive');
+    }
+    if(!isdesktopMenuOpend){
+        console.log('Que verga');
+        desktopMenu.classList.add('inactive');
+    }  
+
+    menuCar.classList.toggle('inactive');
+    
 }
