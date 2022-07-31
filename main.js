7738ebe46cd658ee77a46d23634e7d6e1ac64c9e
Gabriel Villanueva <gvillanuevavega@gmail.com>
2022-07-30 22:26:47 -0500

    Agregando la funcionalidad del menu para los dispositivs Mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const navbarEmail= document.querySelector('.navbar-email')
 const desktopMenu= document.querySelector('.desktop-menu')
 
+const mobileMenu= document.querySelector('.mobile-menu')
+const imgMenu= document.querySelector('.menu')
+
 navbarEmail.addEventListener('click', toggleDesktopMenu)
+imgMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
 
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
