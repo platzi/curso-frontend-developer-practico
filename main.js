051c81699ce91ca3c65ab56ffa2c06d7d0acffa7
Gabriel Villanueva <gvillanuevavega@gmail.com>
2022-07-30 21:40:24 -0500

    2 Juntando la clase 7 con la clase 11 y dandole interactividad

---
+++ main.js
@@ -0,0 +1,9 @@
+const navbarEmail= document.querySelector('.navbar-email')
+const desktopMenu= document.querySelector('.desktop-menu')
+
+navbarEmail.addEventListener('click', toggleDesktopMenu)
+
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive');
+
+}
