0c6847636527bd1b7e1ed425c418c368b0e9f925
elduverx <duverwild@gmail.com>
2022-07-31 20:06:26 +0200

    added all files and functiones necessaries

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const aside = document.querySelector('aside')
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 const menuCart = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailButton = document.querySelector('.product-detail-close')
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -13,6 +15,7 @@ const cardsContainer = document.querySelector('.cards-container');
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
 menuCart.addEventListener('click', toggleCartMenu)
+productDetailButton.addEventListener('click', closeProductDetailAside)
 
 
 function toggleDesktopMenu(){
@@ -25,11 +28,12 @@ function toggleDesktopMenu(){
 }
 
 function toggleMobileMenu(){
-   const isAsideClosed = aside.classList.contains('inactive')
+   const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
+   
    if(!isAsideClosed){
-      aside.classList.add('inactive')
+      shoppingCartContainer.classList.add('inactive')
    }
-
+   closeProductDetailAside()
    mobileMenu.classList.toggle('inactive')
 }
 
@@ -41,9 +45,26 @@ function toggleMobileMenu(){
    if(!isMobileMenuClosed){
       mobileMenu.classList.add('inactive')
    }
-    aside.classList.toggle('inactive')
+
+   const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
+   
+   if(!isProductDetailClosed){
+      productDetailContainer.classList.add('inactive')
+   }
+   shoppingCartContainer.classList.toggle('inactive')
 
  }
+
+ function openProductDetailAside(){
+   shoppingCartContainer.classList.add('inactive')
+   productDetailContainer.classList.remove('inactive')
+
+
+ }
+ function closeProductDetailAside(){
+   productDetailContainer.classList.add('inactive')
+ }
+
 
  const productList = []
  productList.push({ name:'Bike',
@@ -100,6 +121,7 @@ function renderProducts(arr){
    
    const productImg = document.createElement('img')
    productImg.setAttribute('src', product.image)
+   productImg.addEventListener('click', openProductDetailAside)
    
    const productInfo = document.createElement('div');
    productInfo.classList.add('product-info')
