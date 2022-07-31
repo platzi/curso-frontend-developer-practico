1bd6b41effd902a1c9b2ccf46edcd17db1b1e936
Andy <andreamarg0224@gmail.com>
2022-07-31 00:09:33 -0500

    Integración de menú en Mobile

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuBurger.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
-  console.log('CLICK')
   desktopMenu.classList.toggle('inactive');
 }
 
-console.log('Js funcionando');
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('inactive');
+}
 
