f89a0349b078e86bb71d443ac0eaed3c05b9af71
JonatanLeistmeister <ljona054@gmail.com>
2022-08-01 13:15:08 -0300

    Fucion del home con el mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', mostrarMenu);
+burgerMenu.addEventListener('click', mostrarMenuMobile);
 
 function mostrarMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function mostrarMenuMobile() {
+    mobileMenu.classList.toggle('inactive');
 }
