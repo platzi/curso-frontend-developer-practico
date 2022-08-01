991b6a558142c255d7b634ddce0af723ba44bdc2
av952 <av952@github.com>
2022-08-01 13:00:18 -0500

    class 19 menu en mobile

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const navbarEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 //const inactive = document.querySelector('.inactive')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMEnu = document.querySelector('.mobile-menu')
 
 navbarEmail.addEventListener('click',function(){
     desktopMenu.classList.toggle('inactive')
+})
+menuHamIcon.addEventListener('click',function(){
+    mobileMEnu.classList.toggle('inactive')
 })
