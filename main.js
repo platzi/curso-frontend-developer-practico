512859c900a538ee6dfb1fbc15fb898ac0d5434d
Gabriel Villanueva <gvillanuevavega@gmail.com>
2022-08-01 12:23:40 -0500

    Lo que se logro fue meter los elementos de compra y hacer una maquetación mediante JavaScript, Así cada vez que tengamos más objetos den la lista, aparezca automaticamente, al final encapsulamos el for en una función para que sea más manejable

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const mobileMenu= document.querySelector('.mobile-menu')
 const imgMenuMobile= document.querySelector('.menu')
 const carritoCompras= document.querySelector('.navbar-shopping-cart')
 const productDetail= document.querySelector('.product-detail')
+const cardsContainer= document.querySelector('.cards-container')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
 imgMenuMobile.addEventListener('click', toggleMobileMenu)
@@ -26,3 +27,77 @@ function toggleAsideShoppingCart(){
 
 }
 
+const productList= [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>
+*/
+
+//Mostramos los productos mediante Arrays
+function renderProducts(arr){
+    for(product of arr){
+        const productCard= document.createElement('div')
+        productCard.classList.add('product-card');
+    
+        const productImg= document.createElement('img');
+        productImg.setAttribute('src',product.img)
+    
+        const productInfo= document.createElement('div')
+        productInfo.classList.add('product-info');
+    
+      
+        const productInfoDiv= document.createElement('div')
+    
+        const productPrice= document.createElement('p')
+        productPrice.innerText= '$ '+ product.price
+    
+        const productName= document.createElement('p')
+        productName.innerText= product.name
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+        const productInfoFigure=document.createElement('figure')
+        const productImgCart= document.createElement('img')
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg')
+    
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+    
+        productInfoFigure.appendChild(productImgCart)
+    
+    
+        cardsContainer.appendChild(productCard)
+    } 
+}
+
+renderProducts(productList)
