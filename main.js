5de9d138bc2d196701266c3ffbc29f438ae65815
Andres Badillo <r.andres.badillo@gmail.com>
2022-07-31 20:29:48 -0500

    Se fusionó el carrito de compras

--- main.js
+++ main.js
@@ -2,14 +2,41 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartMenu);
 
 function toggleDesktopMenu() {
+    const isProductDetailClosed = productDetail.classList.contains('inactive');
+
+    if (!isProductDetailClosed) {
+        productDetail.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isProductDetailClosed = productDetail.classList.contains('inactive');
+
+    if (!isProductDetailClosed) {
+        productDetail.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartMenu() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed || !isDesktopMenuClosed) {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+
+    productDetail.classList.toggle('inactive');
 }
