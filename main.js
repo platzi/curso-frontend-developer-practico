7f1a1f28dda8b56aed3368153a221767f102ef2d
Gabriel Villanueva <gvillanuevavega@gmail.com>
2022-08-01 10:51:42 -0500

    Aqui se agrego la funcinalidad de que aparezca el menu , la información del carrito de compras en desktop y en mobile

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const navbarEmail= document.querySelector('.navbar-email')
 const desktopMenu= document.querySelector('.desktop-menu')
-
 const mobileMenu= document.querySelector('.mobile-menu')
-const imgMenu= document.querySelector('.menu')
+const imgMenuMobile= document.querySelector('.menu')
+const carritoCompras= document.querySelector('.navbar-shopping-cart')
+const productDetail= document.querySelector('.product-detail')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
-imgMenu.addEventListener('click', toggleMobileMenu)
+imgMenuMobile.addEventListener('click', toggleMobileMenu)
+carritoCompras.addEventListener('click', toggleAsideShoppingCart)
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive')
+    desktopMenu.classList.toggle('inactive')
+
+}   
+function toggleMobileMenu(){
+    productDetail.classList.add('inactive')
+    mobileMenu.classList.toggle('inactive');
+
+}
+function toggleAsideShoppingCart(){
+        mobileMenu.classList.add('inactive')
+        desktopMenu.classList.add('inactive')
+        productDetail.classList.toggle('inactive')
+
 }
 
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
-}
