a1b372f3589b0e13a21b1768693b36210bcce429
alexvillhen <alexvillhen@gmail.com>
2022-08-01 14:05:21 -0600

    finished mobile menu

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+const mobileBtn = document.querySelector("#mobile-menu-btn")
+const mobileMenu = document.querySelector(".mobile-menu");
+
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
 }
 
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+}
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+mobileBtn.addEventListener('click', toggleMobileMenu);
