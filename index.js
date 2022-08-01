6028dd1f7d970cd7010ae21471ad53b2a57d8b54
Jesus Geovanni Labastida Vazquez <geo.labastidaa@gmail.com>
2022-08-01 09:25:22 -0600

    add more items and refined menus

--- index.js
+++ index.js
@@ -6,54 +6,110 @@ const mHambur = document.querySelector('.mobile-menu');
 
 const menuCarritoIcon  = document.querySelector('.navbar-shopping-cart');
 const shoppingCartDetail = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 
 const cardsContainer = document.querySelector('.cards-container');
 
 show.addEventListener('click', toggleDesktopMenu);
 menuHambur.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon .addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', toggleProductDetailAside);
 
 function toggleMobileMenu(){
     mHambur.classList.toggle('inactive');
     shoppingCartDetail.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
+
 }
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
     shoppingCartDetail.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
+
 }
 
 function toggleCarritoAside(){
     shoppingCartDetail.classList.toggle('inactive');
     mHambur.classList.add('inactive');
     desktopMenu.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
+}
+
+function toggleProductDetailAside(){
+    productDetailContainer.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mHambur.classList.add('inactive');
+    shoppingCartDetail.classList.add('inactive');
 }
 
 const productList = [];
 
 // Creamos un array y agregamos los productos
 productList.push({
-    name : 'Bike',
-    price : 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name : 'Fruit Basket',
+    price : 520,
+    image: 'https://images.pexels.com/photos/5217965/pexels-photo-5217965.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
 });
 
 productList.push({
-    name : 'Car',
-    price : 1200,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name : 'Kiwi ayee',
+    price : 20,
+    image: 'https://images.pexels.com/photos/6156993/pexels-photo-6156993.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
 });
 
 productList.push({
-    name : 'Motorbike',
-    price : 500,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name : 'Blueberries',
+    price : 100,
+    image: 'https://images.pexels.com/photos/2539177/pexels-photo-2539177.jpeg?auto=compress&cs=tinysrgb&w=940&h=650',
+});
+
+productList.push({
+    name : 'Pineapple',
+    price : 40,
+    image: 'https://images.pexels.com/photos/6157056/pexels-photo-6157056.jpeg?auto=compress&cs=tinysrgb&w=940&h=650&dpr=2',
+});
+
+productList.push({
+    name : 'Oranges',
+    price : 80,
+    image: 'https://images.pexels.com/photos/7195248/pexels-photo-7195248.jpeg?auto=compress&cs=tinysrgb&w=940&h=650&dpr=2',
+});
+
+productList.push({
+    name : 'Grapefuit',
+    price : 50,
+    image: 'https://images.pexels.com/photos/7065187/pexels-photo-7065187.jpeg?auto=compress&cs=tinysrgb&w=940&h=650&dpr=2',
+});
+
+productList.push({
+    name : 'Avocado',
+    price : 90,
+    image: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=940&h=650&dpr=2',
+});
+
+productList.push({
+    name : 'Banana',
+    price : 60,
+    image: 'https://images.pexels.com/photos/5217996/pexels-photo-5217996.jpeg?auto=compress&cs=tinysrgb&w=940&h=650&dpr=2',
+});
+
+productList.push({
+    name : 'Greens',
+    price : 30,
+    image: 'https://images.pexels.com/photos/7195272/pexels-photo-7195272.jpeg?auto=compress&cs=tinysrgb&w=940&h=650&dpr=2',
+});
+
+productList.push({
+    name : 'Mango',
+    price : 30,
+    image: 'https://images.pexels.com/photos/5217960/pexels-photo-5217960.jpeg?auto=compress&cs=tinysrgb&w=940&h=650&dpr=2',
 });
 
 // Ya con el array listo, insertamos los productos al HTML recorriendo el array
 
-/*
-<div class="product-card">
+/* <div class="product-card">
     <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
     <div class="product-info">
         <div>
@@ -76,6 +132,7 @@ function renderProducts(arr){
         // product = {name,price,image} -> product.image
         const productImg = document.createElement('img'); 
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', toggleProductDetailAside);
         
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
