9feaa658d548da69d632a45d041ab3086bb81cd3
elgarcito <edgarjaguirre@hotmail.com>
2022-08-02 16:34:11 -0300

    We add the fusion to use it in the movile version

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const menuEmail =document.querySelector('.navbar-email')
 const desktopMenu =document.querySelector('.desktop-menu')
+const menuHamIcon =document.querySelector('.menu');
+const mobileMenu =document.querySelector('.mobile-menu')
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu)
+
+menuHamIcon.addEventListener('click',toggleMobileMenu)
+
+function toggleMobileMenu(){
+
+    //desktopMenu.append(inactive);
+    mobileMenu.classList.toggle('inactive')
+    
+    }
+
+
 
 function toggleDesktopMenu(){
 
