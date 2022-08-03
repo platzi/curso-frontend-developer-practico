7a987c6f00772e62bc40c10da5b29b6f05600ade
Cecilio-Cauich <celiocatu@gmail.com>
2022-08-03 16:28:34 -0500

    fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.getElementById('prueba');
 
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click', toggDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
