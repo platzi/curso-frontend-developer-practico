fec367a56d523fee816f67efd48970bfab0f0be3
siendomiguel <siendomigueldev@gmail.com>
2022-08-03 10:42:16 -0500

    fusion navbar with mobile menu

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const btnBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 const toggleDesktopMenu = () => {
     desktopMenu.classList.toggle('inactive');
 }
 
 navBarEmail.addEventListener('click', toggleDesktopMenu);
+
+
+const toggleMobileMenu = () => {
+    mobileMenu.classList.toggle('inactive');
+}
+
+btnBurger.addEventListener('click', toggleMobileMenu);
+
+
+
 
 
