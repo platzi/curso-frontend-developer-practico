8af963d8b4bdecd122afb86958788b34bcaa9f6d
silfredo11 <ibarrasilfredo@gmail.com>
2022-08-03 16:47:11 -0500

    cree todo lo despegable del menu

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
@@ -12,5 +17,29 @@ function toggleDesktopMenu(){
 }
 
 function toggleMobileMenu(){
+
+    const isCarritoAsideClosed = aside.classList.contains("inactive");
+    
+
+    if (!isCarritoAsideClosed){
+        aside.classList.add("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive");
+
 }
+
+function toggleCarritoAside(){
+
+    const isMobileMenuclosed = mobileMenu.classList.contains("inactive");
+
+    if (!isMobileMenuclosed){
+        mobileMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
+
+}
+
+
+
