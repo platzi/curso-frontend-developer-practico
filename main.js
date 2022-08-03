9d3e1b4133b72c84d1172208e3eb81c7d152d823
Kevin Cárdenas <kevinf.cardenas.11@gmail.com>
2022-08-02 21:26:47 -0500

    Mobile menu merge

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 emailMenu.addEventListener('click', toggleDesktopMenu);
+iconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
