f5063f3579c80c1f9e02cf3788c4b300264fb972
Gabriel Villanueva <gvillanuevavega@gmail.com>
2022-08-03 13:27:41 -0500

    en este commit se han agregado las funcionalidades de abrir y cerrar la pantalla  de detalles de un producto, también se corrigieron algunos errores de estilos

--- main.js
+++ main.js
@@ -5,27 +5,50 @@ const imgMenuMobile= document.querySelector('.menu')
 const carritoCompras= document.querySelector('.navbar-shopping-cart')
 const productDetail= document.querySelector('.product-detail')
 const cardsContainer= document.querySelector('.cards-container')
+const clickProductImage=document.querySelector('.click-image')
+const clickCloseProductImage=document.querySelector('.product-detail-close')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
 imgMenuMobile.addEventListener('click', toggleMobileMenu)
 carritoCompras.addEventListener('click', toggleAsideShoppingCart)
+clickCloseProductImage.addEventListener('click', closeProductDetail)
 
 function toggleDesktopMenu(){
     productDetail.classList.add('inactive')
+    clickProductImage.classList.add('inactive')
     desktopMenu.classList.toggle('inactive')
+   
 
 }   
 function toggleMobileMenu(){
     productDetail.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
+    clickProductImage.classList.add('inactive')
 
 }
 function toggleAsideShoppingCart(){
-        mobileMenu.classList.add('inactive')
-        desktopMenu.classList.add('inactive')
-        productDetail.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    clickProductImage.classList.add('inactive')
+    productDetail.classList.toggle('inactive')
 
 }
+
+function openProductDetail(){
+    clickProductImage.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    productDetail.classList.add('inactive')
+}
+
+function closeProductDetail(){
+    clickProductImage.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    productDetail.classList.add('inactive')
+
+}
+//Mostramos los productos mediante Arrays
 
 const productList= [];
 productList.push({
@@ -43,24 +66,28 @@ productList.push({
     price: 120,
     img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
 
 
-/*
-<div class="product-card">
-<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-<div class="product-info">
-  <div>
-    <p>$120,00</p>
-    <p>Bike</p>
-  </div>
-  <figure>
-    <img src="./icons/bt_add_to_cart.svg" alt="">
-  </figure>
-</div>
-</div>
-*/
-
-//Mostramos los productos mediante Arrays
 function renderProducts(arr){
     for(product of arr){
         const productCard= document.createElement('div')
@@ -68,6 +95,7 @@ function renderProducts(arr){
     
         const productImg= document.createElement('img');
         productImg.setAttribute('src',product.img)
+        productImg.addEventListener('click',openProductDetail)
     
         const productInfo= document.createElement('div')
         productInfo.classList.add('product-info');
@@ -94,10 +122,13 @@ function renderProducts(arr){
         productInfo.appendChild(productInfoFigure)
     
         productInfoFigure.appendChild(productImgCart)
-    
-    
         cardsContainer.appendChild(productCard)
     } 
+
+
+
 }
 
 renderProducts(productList)
+//Mostrar información del producto cuando le hacemos click
+
