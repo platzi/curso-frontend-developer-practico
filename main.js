fb005d9c711c69c9e454d5e2ed758ab934fbbe50
Kevin-MartinezP <kevin.pineda.martinez@gmail.com>
2022-08-03 15:58:53 -0600

    Agregando contenido dinamico para mostrar los productos

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ productList.push({
     name: "Bike",
     price: 120,
     image:
-        "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "https://ecoinventos.com/wp-content/uploads/2021/01/CityQ.jpg",
     description: "Es una bicicleta 4",
 });
 /*Asignando Eventos */
@@ -61,22 +61,36 @@ addEventListener("resize", () => {
 /*Funciones */
 /*Función que muestra el menú para cerrar sección o mostrar nuestra información */
 function toggleDesktopMenu() {
+    /*Variables que verifican clases de las etiquetas de HTML */
+    const isDetailsProductOpen = detallesDeProducto.classList.contains("inactive");
+
+    /*Condicionales para controlar la funcionalidad entre todos los componentes al abrirse y cerrarse */
     /*Se crea una variable local en la función para preguntar si el menú del carrito de compras contiene la clase "Inactive" */
     const isCarritoClosed = menuCarritoDetalles.classList.contains("inactive");
     /*Una condicional que evalúa si el menú del carrito esto abierto se le agrega la clase "Inactive" */
     if (!isCarritoClosed) {
         menuCarritoDetalles.classList.add("inactive");
     }
+
+    if (!isDetailsProductOpen) {
+        detallesDeProducto.classList.add("inactive")
+    }
+
     /*Al presionar en el menu DesktopMenu se cambia por cada click su clase "Inactive" */
     desktopMenu.classList.toggle("inactive");
 }
 /*Función que muestra el menú del navbar al hacer click en el menú hamburguesa */
 function toggleMobileMenu() {
+    /*Variables que verifican clases de las etiquetas de HTML */
     const isCarritoClosed = menuCarritoDetalles.classList.contains("inactive");
     const isMobileMenuOpen = mobileMenu.classList.contains("inactive");
+    const isDetailsProductOpen = detallesDeProducto.classList.contains("inactive");
 
+    /*Condicionales para controlar la funcionalidad entre todos los componentes al abrirse y cerrarse */
     if (!isCarritoClosed) {
         menuCarritoDetalles.classList.add("inactive");
+    }else if (!isDetailsProductOpen) {
+        detallesDeProducto.classList.add("inactive");
     }
 
     if (isMobileMenuOpen) {
@@ -92,18 +106,24 @@ function toggleMobileMenu() {
 }
 /*Función que muestra el menú del carrito de compras */
 function toggleMenuCarrito() {
+    /*Variables que verifican clases de las etiquetas de HTML */
     const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
     const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
     const menuCarritoOpen = menuCarritoDetalles.classList.contains("inactive");
-
+    const isDetailsProductOpen = detallesDeProducto.classList.contains("inactive");
+    /*Condicionales para controlar la funcionalidad entre todos los componentes al abrirse y cerrarse */
     if (!isDesktopMenuClosed) {
         desktopMenu.classList.add("inactive");
+    }else if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }else if (!isDetailsProductOpen) {
+        detallesDeProducto.classList.add("inactive");
     }
-
+    /*
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add("inactive");
     }
-
+    */
     if (menuCarritoOpen && innerWidth < 640) {
         iconChange.style.display = "none";
         hamburguerMenu.style.display = "block";
@@ -165,13 +185,26 @@ showProductsOnScreen(productList);
 
 /*Función que renderiza y obtiene los datos para mostrarlos como detalles del producto */
 const openProductDetailContainer = (product) => {
-    
+        /*Variables que verifican clases de las etiquetas de HTML */
+        const isMenuCarOpen = menuCarritoDetalles.classList.contains("inactive");
+        const isDesktopMenuOpen = desktopMenu.classList.contains("inactive");
+        const isMenuMobileOpen = mobileMenu.classList.contains("inactive");
+        /*Condicionales para controlar la funcionalidad entre todos los componentes al abrirse y cerrarse */
+        if (!isDesktopMenuOpen) {
+            desktopMenu.classList.toggle("inactive");
+        } else if (!isMenuCarOpen) {
+            menuCarritoDetalles.classList.toggle("inactive");
+        } else if (!isMenuMobileOpen) {
+            mobileMenu.classList.toggle("inactive");
+        }
+
+        detallesDeProducto.innerHTML= " ";
         const productDetailClosed = document.createElement("div");
         productDetailClosed.classList.add("product-detail-close");
+        productDetailClosed.addEventListener("click", closeDetailsProduct);
         const productDetailCloseIcon = document.createElement("img");
         productDetailCloseIcon.classList.add("product-detail-close__icon");
         productDetailCloseIcon.setAttribute("src", "./icons/icon_close.png");
-        productDetailCloseIcon.addEventListener("click", closeDetailsProduct);
         productDetailClosed.append(productDetailCloseIcon);
     
         const productDetailImg = document.createElement("img");
@@ -203,12 +236,14 @@ const openProductDetailContainer = (product) => {
             addCartButton
         );
 
-
     detallesDeProducto.append(productDetailClosed, productDetailImg, productInfo);
     detallesDeProducto.classList.remove("inactive");
 };
 
 /*Función para cerrar el contenedor de los detalles del producto */
 function closeDetailsProduct() {
+    /*Limpia el contenedor de los detalles de los productos cada ves que se ejecuta el cierre de este */
+    
+    /*Agrega la clase "Inactive al contenedor detallesDelProducto" */
     detallesDeProducto.classList.add("inactive");
 }
