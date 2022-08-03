1810e2709157bb4f2c62562e3725de75f68e8b5f
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-08-03 10:11:18 -0500

    Se agregó un toggle al aside de product-detail cada vez que se le da clic sobre el icono del carrito de compras. Además se agregó dentro de las funciones toggle de los menú de que si hay un menú abierto cuando vamos a abrir otro, que se cierre el que está abierto.

--- index.js
+++ index.js
@@ -2,14 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuShopCarIcon = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
+menuShopCarIcon.addEventListener('click', toggleProductDetail);
 
 function toggleDesktopMenu() {
+  const isProductDetailClosed = productDetail.classList.contains('inactive');
+
+  if (!isProductDetailClosed) {
+    productDetail.classList.add('inactive');
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  const isProductDetailClosed = productDetail.classList.contains('inactive');
+
+  if (!isProductDetailClosed) {
+    productDetail.classList.add('inactive');
+  }
+
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetail() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+
+  if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add('inactive');
+  }
+
+  productDetail.classList.toggle('inactive');
 }
