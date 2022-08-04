f64e37e7e488df5867d62e8ffaa6e94c824c51e5
Jasonparra21 <jeissonparra1212@gmail.com>
2022-08-04 00:47:15 -0500

    MOBILE MENU TERMINADO

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const menuemail = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
-menuemail.addEventListener('click', toggledesktopmenu);
+const mobilicon = document.querySelector('.menu');
+const mobilemenu = document.querySelector('.mobile-menu');
 
+menuemail.addEventListener('click', toggledesktopmenu);
+mobilicon.addEventListener('click', togglemobilemenu);
 function toggledesktopmenu() {
     desktopmenu.classList.toggle('inactive');
+}
+
+function togglemobilemenu(){
+    mobilemenu.classList.toggle('inactive');
 }
