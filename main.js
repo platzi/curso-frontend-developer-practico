1f5a85e5b2370781b75bb79c0b5b887014e44310
AndressWalt <julianwalteros415263@gmail.com>
2022-08-04 18:29:35 -0500

    Añadimos los detalles del producto clickeadoy creamos funcion para cerrar todo menos lo que se solicite

--- main.js
+++ main.js
@@ -3,45 +3,33 @@ const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu")
 const menuCartIcon = document.querySelector(".navbar-shopping-cart")
 const mobileMenu = document.querySelector(".mobile-menu")
-const aside = document.querySelector(".product-detail")
+const aside = document.querySelector(".cart-product")
 const cardsContainer = document.querySelector(".cards-container")
+const productDetails = document.querySelector(".product-specifities")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
 menuHamIcon.addEventListener("click", toggleMobileMenu)
 menuCartIcon.addEventListener("click", toggleAsideCart)
 
-function toggleDesktopMenu() {
-    const asideIsClosed = aside.classList.contains("inactive")
-    
-    if(!asideIsClosed) {
-        aside.classList.add("inactive")
-    }
+function closeAll(){
+    aside.classList.add("inactive")
+    mobileMenu.classList.add("inactive")
+    productDetails.classList.add("inactive")
+    desktopMenu.classList.add("inactive")
+}
 
+function toggleDesktopMenu() {
+    closeAll()
     desktopMenu.classList.toggle("inactive")
 }
 
 function toggleMobileMenu() {
-    const asideIsClosed = aside.classList.contains("inactive")
-    
-    if(!asideIsClosed) {
-        aside.classList.add("inactive")
-    }
-    
+    closeAll()
     mobileMenu.classList.toggle("inactive")
 }
 
 function toggleAsideCart() {
-    const mobileMenuIsClosed = mobileMenu.classList.contains("inactive")
-    const desktopMenuIsClosed = desktopMenu.classList.contains("inactive")
-    
-    if(!mobileMenuIsClosed) {
-        mobileMenu.classList.add("inactive")
-    }
-
-    if(!desktopMenuIsClosed) {
-        desktopMenu.classList.add("inactive")
-    }
-
+    closeAll()
     aside.classList.toggle("inactive")
 }
 
@@ -62,29 +50,33 @@ productList.push({
     price: 420,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 })
-
+productList.push({
+    name: "Laptop",
+    price: 400,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+})
 
 /* <div class="product-card">
-        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-        <div class="product-info">
-          <div>
-            <p>$120,00</p>
-            <p>Bike</p>
-          </div>
-          <figure>
-            <img src="./icons/bt_add_to_cart.svg" alt="">
-          </figure>
-        </div>
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+<div>
+<p>$120,00</p>
+<p>Bike</p>
+</div>
+<figure>
+<img src="./icons/bt_add_to_cart.svg" alt="">
+</figure>
+</div>
 </div> */
 
 function renderProducts(arr){
     for(product of arr) {
         const productCard = document.createElement("div")
         productCard.classList.add("product-card")
-    
+        
         const productImg = document.createElement("img")
         productImg.setAttribute("src", product.image)
-    
+        
         const productInfo = document.createElement("div")
         productInfo.classList.add("product-info")
         
@@ -100,19 +92,30 @@ function renderProducts(arr){
         const productInfoFigure = document.createElement("figure")
         const productImgCart = document.createElement("img")
         productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
-    
+        
         productInfoFigure.appendChild(productImgCart)
-    
+        
         productInfoDiv.appendChild(productPrice)
         productInfoDiv.appendChild(productName)
-    
+        
         productInfo.appendChild(productInfoDiv)
         productInfo.appendChild(productInfoFigure)
-    
+        
         productCard.appendChild(productImg)
         productCard.appendChild(productInfo)
-    
+        
         cardsContainer.appendChild(productCard)
     }
 }
 renderProducts(productList)
+
+const cardProduct = document.querySelectorAll(".product-card")
+
+for (let i = 0; i < cardProduct.length; i++) {
+    cardProduct[i].addEventListener("click", toggleAsideDetails)
+}
+
+function toggleAsideDetails() {
+    closeAll()
+    productDetails.classList.toggle("inactive")
+}
