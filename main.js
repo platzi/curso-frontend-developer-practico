7e77630666b79470c8ed4460f6fc65bf1e32aed4
gvieiradev <ing.gabrielvieira@gmail.com>
2022-08-04 16:14:55 -0400

    uniendo clase13 a index

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarrito = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarrito.addEventListener("click", toggleMenuCarrito);
 
 function toggleDesktopMenu(){
+    const isAsideClosed =  aside.classList.toggle("inactive");
+
+    if (!isAsideClosed){
+        aside.classList.toggle("inactive");
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed =  aside.classList.toggle("inactive");
+
+    if (!isAsideClosed){
+        aside.classList.toggle("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive");
 }
+
+function toggleMenuCarrito(){
+    const isMobileClosed = mobileMenu.classList.toggle("inactive");
+    const isDesktopMenuClosed = desktopMenu.classList.toggle("inactive");
+    
+    if(!isMobileClosed){
+        mobileMenu.classList.toggle("inactive");
+    }
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.toggle("inactive");
+    }
+
+    aside.classList.toggle("inactive");
+};
