32f499598fde291c41677e3777e302b83d6ea21a
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-08-03 21:41:47 -0500

    Se abre un aside con información detallada cada vez que se le da clic sobre la imagen de un componente y se cierra cuando se le da en la x.

--- index.js
+++ index.js
@@ -3,36 +3,42 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuShopCarIcon = document.querySelector('.navbar-shopping-cart');
-const productDetail = document.querySelector('.product-detail');
+const shoppingCartDetail = document.querySelector('.shopping-cart-detail');
 const cardsContainer = document.querySelector('.cards-container');
+const productDetail = document.querySelector('.product-detail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
-menuShopCarIcon.addEventListener('click', toggleProductDetail);
+menuShopCarIcon.addEventListener('click', toggleShoppingCartDetail);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
-  const isProductDetailClosed = productDetail.classList.contains('inactive');
+  const isShoppingCartDetailClosed = shoppingCartDetail.classList.contains('inactive');
 
-  if (!isProductDetailClosed) {
-    productDetail.classList.add('inactive');
+  if (!isShoppingCartDetailClosed) {
+    shoppingCartDetail.classList.add('inactive');
   }
 
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  const isProductDetailClosed = productDetail.classList.contains('inactive');
+  const isShoppingCartDetailClosed = shoppingCartDetail.classList.contains('inactive');
 
-  if (!isProductDetailClosed) {
-    productDetail.classList.add('inactive');
+  if (!isShoppingCartDetailClosed) {
+    shoppingCartDetail.classList.add('inactive');
   }
+
+  closeProductDetailAside();
 
   mobileMenu.classList.toggle('inactive');
 }
 
-function toggleProductDetail() {
+function toggleShoppingCartDetail() {
   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
   const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+  const isProductDetailClosed = productDetail.classList.contains('inactive');
 
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');
@@ -42,7 +48,21 @@ function toggleProductDetail() {
     desktopMenu.classList.add('inactive');
   }
 
-  productDetail.classList.toggle('inactive');
+  if (!isProductDetailClosed) {
+    productDetail.classList.add('inactive');
+  }
+
+  shoppingCartDetail.classList.toggle('inactive');
+}
+
+function openProductDetailAside() {
+  shoppingCartDetail.classList.add('inactive');
+
+  productDetail.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+  productDetail.classList.add('inactive');
 }
 
 const productList = [];
@@ -59,42 +79,64 @@ productList.push({
 productList.push({
   name: 'Computer',
   price: 620,
-  image: 'https://images.pexels.com/photos/7238759/pexels-photo-7238759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  image: 'https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+  name: 'Old Tv',
+  price: 80,
+  image: 'https://images.pexels.com/photos/333984/pexels-photo-333984.jpeg?cs=srgb&dl=pexels-rene-asmussen-333984.jpg&fm=jpg'
+});
+productList.push({
+  name: 'Pair of small scissors',
+  price: 15,
+  image: 'https://images.pexels.com/photos/4226910/pexels-photo-4226910.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+  name: 'Camera',
+  price: 750,
+  image: 'https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+  name: 'Classical guitar',
+  price: 385,
+  image: 'https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 });
 
 function renderProducts(arreglo) {
   for (product of arreglo) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
-  
+
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
-  
+    productImg.style.cursor = 'pointer';
+    productImg.addEventListener('click', openProductDetailAside);
+
     const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
-  
+    productInfo.classList.add('product-detail-info');
+
     const productInfoDiv = document.createElement('div');
-  
+
     const productPrice = document.createElement('p');
     productPrice.innerText = '$' + product.price;
     const productName = document.createElement('p');
     productName.innerText = product.name;
-  
+
     productInfoDiv.appendChild(productPrice);
     productInfoDiv.appendChild(productName);
-  
+
     const productInfoFigure = document.createElement('figure');
     const productImgCart = document.createElement('img');
     productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-  
+
     productInfoFigure.appendChild(productImgCart);
-  
+
     productInfo.appendChild(productInfoDiv);
     productInfo.appendChild(productInfoFigure);
-  
+
     productCard.appendChild(productImg);
     productCard.appendChild(productInfo);
-  
+
     cardsContainer.appendChild(productCard);
   }
 }
