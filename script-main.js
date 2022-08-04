1c2d7dc17873cbf26e3d0ad0eb02212682278853
LuisM11 <lumarinb@unal.edu.co>
2022-08-04 00:41:21 -0500

    visualizacion de menus desplegables sincronicamente

--- script-main.js
+++ script-main.js
@@ -1,34 +1,37 @@
-const navbarRmenu = document.getElementsByClassName("desktop-menu inactive")
+const navbarRmenu = document.getElementsByClassName("desktop-menu")
 const navbarR = document.querySelector('.navbar-email')
 
 const burguerContainer = document.querySelector('.burguerMenu')
 const burguerIcon = document.querySelector('.menu');
 const displayedBurguer = document.querySelector('.mobile-menu')
 
+const cartMenu = document.querySelector(".product-detail")
+const cartIcon = document.querySelector(".navbar-shopping-cart")
+
 navbarR.addEventListener('click',toggleDesktopMenu)
-function toggleDesktopMenu(event){
-    if(navbarRmenu[0].style.display == ""){
-        navbarRmenu[0].style.display = "block"
+function toggleDesktopMenu(){
+    if(!cartMenu.classList.contains('inactive')){
+        cartMenu.classList.toggle('inactive')
     }
-    else{
-        navbarRmenu[0].style.display = "";
+    navbarRmenu[0].classList.toggle('inactive')
+    
+}
+cartIcon.addEventListener('click', (event)=> {
+    if(!navbarRmenu[0].classList.contains('inactive')){
+        navbarRmenu[0].classList.toggle('inactive')
     }
-}
-/* navEmail.addEventListener('click', () => {
-    menuDesktop.classList.toggle('inactive');    //CON TOGGLE Y ARROW FUNCTION
-}); */
-
-burguerIcon.addEventListener('click',(event)=>{displayedBurguer.classList.toggle('inactive')})
-
-
-/* function f(x){
-    if (x.matches){
-        document.body.style.backgroundColor = "yellow";
-    } else {
-        document.body.style.backgroundColor = "pink";
-  }
-}
-const media = window.matchMedia("(max-width: 640px)")
-f(media)
-media.addEventListener("changes",function (){f(media)}) */
+    
+    if(!displayedBurguer.classList.contains('inactive')){
+        displayedBurguer.classList.toggle('inactive')
+    }
+    
+    cartMenu.classList.toggle('inactive')
+    
+})
+burguerIcon.addEventListener('click',(event)=>{
+    if(!cartMenu.classList.contains('inactive')){
+        cartMenu.classList.toggle('inactive')
+    }
+    displayedBurguer.classList.toggle('inactive')
+})
 
