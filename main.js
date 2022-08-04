904ed0b8fbc75762605a196a5962fd347626bd55
Cecilio-Cauich <celiocatu@gmail.com>
2022-08-03 23:04:43 -0500

    carrito de compras

--- main.js
+++ main.js
@@ -4,14 +4,28 @@ const desktopMenu = document.getElementById('prueba');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const asidecarrito = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleAsideCarrito)
 
 
 function toggDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    asidecarrito.classList.add('inactive');
+
 }
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+    asidecarrito.classList.add('inactive');
+}
+
+function toggleAsideCarrito(){
+    asidecarrito.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+
 }
