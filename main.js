1bb569ca6812cfb168d1a29fb1d7dd671d71d88f
GermanHv <ghove7@gmail.com>
2022-08-03 23:10:08 -0500

    3. Carrito de compras

--- main.js
+++ main.js
@@ -5,13 +5,25 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector ('.mobile-menu')
 
+// Menu carrito
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', togglemenuHamIcon)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
 }
 
 function togglemenuHamIcon(){
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside () {
+ mobileMenu.classList.add('inactive')
+ aside.classList.toggle('inactive')
 }
