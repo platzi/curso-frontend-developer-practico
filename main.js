0a9f0e578cc4b4ff82008073abeb00afb7427b74
Jasonparra21 <jeissonparra1212@gmail.com>
2022-08-04 22:59:56 -0500

    Se agrega lista de productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const mobilicon = document.querySelector('.menu');
 const mobilemenu = document.querySelector('.mobile-menu');
 const menucarticon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardscontainer = document.querySelector('.cards-container');
 
 
 menuemail.addEventListener('click', toggledesktopmenu);
@@ -39,3 +40,76 @@ function toggleshopingcart(){
     aside.classList.toggle('inactive');
 
 }
+
+    const productlist = [];
+    
+    productlist.push({
+        name:   'Bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    })
+    productlist.push({
+        name:   'Balon',
+        price: 120,
+        image: 'https://www.pngmart.com/files/1/2014-World-Cup-Soccer-Ball-PNG.png',
+    })
+    productlist.push({
+        name:   'Carro',
+        price: 999999,
+        image: 'https://www.pngplay.com/wp-content/uploads/13/Kia-Niro-EV-PNG-Photos.png',
+    })
+    productlist.push({
+        name:   'Bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    })
+    productlist.push({
+        name:   'Balon',
+        price: 120,
+        image: 'https://www.pngmart.com/files/1/2014-World-Cup-Soccer-Ball-PNG.png',
+    })
+    productlist.push({
+        name:   'Carro',
+        price: 999999,
+        image: 'https://www.pngplay.com/wp-content/uploads/13/Kia-Niro-EV-PNG-Photos.png',
+    })
+
+    function renderproducts(arr) {   
+    for (product of productlist) {
+        const productcard = document.createElement('div');
+        productcard.classList.add('product-card');
+        
+        const productimg = document.createElement('img');
+        productimg.setAttribute('src', product.image);
+
+        const productinfo = document.createElement('div');
+        productinfo.classList.add('product-info')
+
+        const productinfodiv = document.createElement('div');
+
+        const productprice = document.createElement('p');
+        productprice.innerText = '$' + product.price;
+
+        const productname = document.createElement('p');
+        productname.innerText = product.name;
+        
+        productinfodiv.appendChild(productprice);
+        productinfodiv.appendChild(productname);
+        
+        productinfofigure = document.createElement('figure');
+        const productimgcart = document.createElement('img')
+        productimgcart.setAttribute('src', './icons/bt_add_to_cart.svg');
+        
+        productinfofigure.appendChild(productimgcart);
+
+        productinfo.appendChild(productinfodiv);
+        productinfo.appendChild(productinfofigure);
+        
+        productcard.appendChild(productimg);
+        productcard.appendChild(productinfo);
+
+        cardscontainer.appendChild(productcard);
+        
+    }
+    }      
+    renderproducts(productlist);
