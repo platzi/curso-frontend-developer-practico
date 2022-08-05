00e345b7680424d8841c0fd9e0236d33d4af1f19
elgarcito <edgarjaguirre@hotmail.com>
2022-08-05 15:38:16 -0300

    we add the shop cart menu and functions that allow to show or not show the items when we click on them

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-const menuEmail =document.querySelector('.navbar-email')
-const desktopMenu =document.querySelector('.desktop-menu')
+const menuEmail =document.querySelector('.navbar-email');
+const desktopMenu =document.querySelector('.desktop-menu');
 const menuHamIcon =document.querySelector('.menu');
-const mobileMenu =document.querySelector('.mobile-menu')
-
+const mobileMenu =document.querySelector('.mobile-menu');
+const menuCarritoIcon= document.querySelector('.navbar-shopping-cart');
+const aside= document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu)
 
 menuHamIcon.addEventListener('click',toggleMobileMenu)
 
+menuCarritoIcon.addEventListener('click',toggleCarritoAside)
+
+
 function toggleMobileMenu(){
+    const isAsideCLosed=aside.classList.contains('inactive');
 
     //desktopMenu.append(inactive);
-    mobileMenu.classList.toggle('inactive')
-    
+    //obileMenu.classList.toggle('inactive');
+
+
+
+    if (!isAsideCLosed) {
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+
     }
 
 
@@ -20,6 +32,19 @@ function toggleMobileMenu(){
 function toggleDesktopMenu(){
 
 //desktopMenu.append(inactive);
-desktopMenu.classList.toggle('inactive')
+desktopMenu.classList.toggle('inactive');
 
 }
+
+
+
+
+function toggleCarritoAside(){
+//Cuando abrimos el carrito cerramos el menu movile siempre con la primera expresion
+    const isMobileMenuCLosed=mobileMenu.classList.contains('inactive');
+//si el menu movile esta abierto se mantiene se mantiene cerrado
+    if (!isMobileMenuCLosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+    }
