abfdde003072c5379919aa84fea0875ec8bc7b03
Yasdani Brenes <yasdanibrenes@gmail.com>
2022-08-04 20:27:18 -0600

    Interaccion del menu, cart y user menu en desktop y mobile

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
+const cartMenu = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+cartMenu.addEventListener('click', toggleCartMenu);
+
 
 function toggleDesktopMenu() {
+  const isAsideMenuClose = aside.classList.contains('inactive');
+
+  if (!isAsideMenuClose) {
+    aside.classList.add('inactive');
+  }
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+  const isAsideMenuClose = aside.classList.contains('inactive');
+
+  if (!isAsideMenuClose) {
+    aside.classList.add('inactive');
+  }
+
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartMenu() {
+  const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClose) {
+    mobileMenu.classList.add('inactive');
+  }
+
+  aside.classList.toggle('inactive');
 }
