c93a4a597611e17dbcf42dab223dc98f65742626
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-08-06 16:55:34 -0500

    fix interaction menus

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const burgerMenuIcon = document.querySelector('.burger-menu-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const cardsContainer = document.querySelector('.cards-container');
 
 // EventsListeners
 menuEmail.addEventListener('click', toggleDesktopMenu)
 burgerMenuIcon.addEventListener('click', toggleMobileMenu)
 menuCartIcon.addEventListener('click', toggleCartAside)
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu(){
     const isAsideOpen = !shoppingCartContainer.classList.contains('inactive')
@@ -27,17 +30,35 @@ function toggleMobileMenu(){
     if(isAsideOpen){
         shoppingCartContainer.classList.add('inactive')
     }
+
+    closeProductDetailAside()
+    
     mobileMenu.classList.toggle('inactive')
 };
 
 function toggleCartAside(){
     const isMobileMenuOpen = !mobileMenu.classList.contains('inactive')
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
 
     if(isMobileMenuOpen){
         mobileMenu.classList.add('inactive')
     }
+
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive')
+    }
+
     shoppingCartContainer.classList.toggle('inactive')
 };
+
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive')
+    productDetailContainer.classList.remove('inactive')
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive')
+}
 
 // Product list
 const productList = [];
@@ -63,6 +84,7 @@ for (product of productList){
 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside)
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info')
