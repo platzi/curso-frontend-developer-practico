c72d5b2c83483e90294f0385eb7a087038ef15e3
jeseoz33 <jeseoz33@gmail.com>
2022-08-05 22:37:36 -0500

    agregamos el desktopMenu y el cartMenu al index html

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const cartMenu = document.querySelector('.navbar-shopping-cart');
+
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
+cartMenu.addEventListener('click', toggleAsideCart);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAsideCart(){
+    aside.classList.toggle('inactive');
 }
