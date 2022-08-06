46e23dd37d8865399385bdcc0169174d8ccb72e9
jodaraca <josedavidrc59@gmail.com>
2022-08-06 15:32:30 -0400

    cambios menu

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navright = document.querySelector('.navbar-right');
 const desktopmenu = document.querySelector('.desktop-menu');
+const burgermenu = document.querySelector('.menu');
+const mobilemenu = document.querySelector('.mobile-menu');
 
 navright.addEventListener('click', toggleDesktopmenu);
+burgermenu.addEventListener('click', toggleMobileMenu);
 
 function  toggleDesktopmenu (){
     desktopmenu.classList.toggle('inactive');
+}   
+
+
+function  toggleMobileMenu (){
+    mobilemenu.classList.toggle('inactive');
 }   
