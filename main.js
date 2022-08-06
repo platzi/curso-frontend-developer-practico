386f895b9b655347c01d5183e38cb33db7819e1d
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-08-06 11:39:37 -0500

    merge mobile menu with navbar and add burger mewnu logic

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenuIcon = document.querySelector('.burger-menu-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+burgerMenuIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
-}
+};
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
+};
+
