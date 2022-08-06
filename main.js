3ef7834b42efe560dee041e627348b5014bae5bf
jodaraca <josedavidrc59@gmail.com>
2022-08-06 15:55:41 -0400

    se incluye detalle

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-const navright = document.querySelector('.navbar-right');
+const navright = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
 const burgermenu = document.querySelector('.menu');
 const mobilemenu = document.querySelector('.mobile-menu');
+const shoppingCard = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 navright.addEventListener('click', toggleDesktopmenu);
 burgermenu.addEventListener('click', toggleMobileMenu);
+shoppingCard.addEventListener('click', toggleProductDetail);
 
 function  toggleDesktopmenu (){
+    aside.classList.add('inactive');
+    mobilemenu.classList.add('inactive');
+
     desktopmenu.classList.toggle('inactive');
 }   
 
 
 function  toggleMobileMenu (){
+    aside.classList.add('inactive');
+    desktopmenu.classList.add('inactive');
+
     mobilemenu.classList.toggle('inactive');
 }   
+
+function  toggleProductDetail (){
+    mobilemenu.classList.add('inactive');
+    desktopmenu.classList.add('inactive');
+
+    aside.classList.toggle('inactive');
+}  
