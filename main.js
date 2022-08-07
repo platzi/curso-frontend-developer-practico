d4c1ba8ef64b590d656aedf7e77706f69f97bb03
imestudio <programadoralphax@gmail.com>
2022-08-08 01:55:09 +0200

    comments added

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+/* QuerySelector selecciona cualquier selector ya sea clase/id */
 
 menuEmail.addEventListener("click", toggleDesktopMenu); 
 
 function toggleDesktopMenu(){
     console.log("click");
     desktopMenu.classList.toggle("inactive");
+    /* El método "toogle" lo que hace es alternar entre on y off
+    
+    En este caso lo que hace es cambiar el display: none que previamente hemos especificado en el CSS*/
 }
