ac8d6e7ba9c8b6e8159b32e6ad5f4e3061d9fd03
kburrion <burrion.kevin@gmail.com>
2022-08-06 21:39:19 -0600

    Dynamic product list added

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuShoppingCart.addEventListener('click', toggleShoppingCartAside);
@@ -16,8 +18,7 @@ function toggleDesktopMenu(){
     if(!isAsideClosed){
         aside.classList.add('inactive')
     }
-    desktopMenu.classList.toggle('inactive')
-    
+    desktopMenu.classList.toggle('inactive')    
 }
 
 function toggleMobileMenu(){
@@ -37,3 +38,63 @@ function toggleShoppingCartAside(){
     }
     aside.classList.toggle('inactive')       
 }
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Monitor',
+    price: 220,
+    image: 'https://cdn.mos.cms.futurecdn.net/BwSR4PFK3i5ZyhFEmZUBMS.jpg',
+})
+
+productList.push({
+    name: 'Computer',
+    price: 620,
+    image: 'https://www.digitaltrends.com/wp-content/uploads/2022/04/dell-xps-desktop-8950-review-04.jpg?fit=720%2C720&p=1',
+})
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+    
+        const productImg =document.createElement('img');
+        productImg.setAttribute('src', product.image)
+        // Product = {name, price, image} => product.image
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info')
+    
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText='$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProducts(productList);
