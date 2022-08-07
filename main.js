15a1b4e49644783ac719947dbc8380729b5493ff
angeldavid <angelcontreras0777@gmail.com>
2022-08-06 20:22:27 -0400

    Agregamos las imagenes a la tienda

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-
 const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
-const cardsContaienr = document.querySelector('.cards-container');
+const productDetailsCloseIcon = document.querySelector('.product-detail-close');
+const cardsContainer = document.querySelector('.cards-container');
+const productDetailContainer = document.querySelector('#productDetail');
+
+// const productDetailImg = document.querySelector('#productDetail');
 
 menuEmail.addEventListener('click', toggleDestopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailsCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDestopMenu(){
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -27,6 +32,7 @@ function toggleMobileMenu(){
     if(!isAsideClosed){
         shoppingCartContainer.classList.add('inactive');
     }
+    closeProductDetailAside();
 
     mobileMenu.classList.toggle('inactive');
 }
@@ -41,14 +47,33 @@ function toggleCarritoAside(){
         desktopMenu.classList.add('inactive');
     }
 
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
+    }
+
     shoppingCartContainer.classList.toggle('inactive');
 }
+
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+    
+    shoppingCartContainer.classList.remove('menu-animation')
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
+    productDetailImg.removeAttribute('src');
+}
+
+
 /*Lista de productos */
 const productList = [];
 productList.push({
-    name: 'Bike',
+    name: 'Bicicleta',
     price: 120,
-    image: "https://www.lapolar.cl/dw/image/v2/BCPP_PRD/on/demandware.static/-/Sites-master-catalog/default/dwa9dc92bd/images/large/23287226.jpg?sw=1200&sh=1200&sm=fit",
+    image: "https://www.oxfordstore.cl/media/catalog/product/cache/e2b750e3a1f8d67cf5cdd4eb0e9445ad/b/a/ba2931_emerald_29_negro-celeste_2022_02_1_1.jpg",
 });
 productList.push({
     name: 'Pantalla',
@@ -58,8 +83,37 @@ productList.push({
 productList.push({
     name: 'Laptop',
     price: 620,
-    image: "https://consumer.huawei.com/content/dam/huawei-cbg-site/latam/cl/support/laptop-user-guide/img/laptop.png",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO9Z2mUufFeCPkVhxWji63ap31nzJCVJDixoIygRqQltEBzHXIhiFg0prKI8rh1g9S47M&usqp=CAU",
 });
+productList.push({
+    name: 'Audifonos',
+    price: 90,
+    image: "https://cdn.shopify.com/s/files/1/0086/2758/5129/products/A3032_img_feature_01_v2_d9fdb9e6-8c89-4a66-beed-3142152ba008_560x560.png?v=1598470396",
+});
+
+productList.push({
+    name: 'Audifonos-Inalámbricos',
+    price: 55,
+    image: "https://www.sony.cl/image/6ff70f66ce935f02861635b3e936a70c?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF",
+});
+productList.push({
+    name: 'Cable Usb',
+    price: 10,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzqTIVLKN-sfrEOKTiDI0ZhyFiRjKOLdhxLQ&usqp=CAU",
+});
+productList.push({
+    name: 'Micrófono de mano',
+    price: 25,
+    image: "https://www.sielcom.cl/wp-content/uploads/2021/11/microfono-dinamico-profesional-novik-neo-fnk580-011-42ee60ca48aed64fc115150775848154-640-0.jpeg",
+});
+productList.push({
+    name: 'Ratones Gaming Pc',
+    price: 15,
+    image: "https://i.blogs.es/7c0651/roccat-kone-aimo/450_1000.webp",
+});
+
+
+
 
 /*
 <div class="product-card">
@@ -83,7 +137,8 @@ function renderProducts(arr){
 
         //product = {name, price, image} -> product.image
         const productImg = document.createElement('img');
-        productImg.setAttribute('src', product.image);
+        productImg.setAttribute('src',product.image);
+        productImg.addEventListener('click',openProductDetailAside);
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -95,6 +150,7 @@ function renderProducts(arr){
         const productName = document.createElement('p');
         productName.innerText =  product.name;
 
+        //Regresando y metiendo los productos
         productInfoDiv.appendChild(productPrice);
         productInfoDiv.appendChild(productName);
 
@@ -102,6 +158,7 @@ function renderProducts(arr){
         const productImgCart = document.createElement('img');
         productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
 
+        //Regresando y metiendo los productos
         productInfoFigure.appendChild(productImgCart);
 
         productInfo.appendChild(productInfoDiv);
@@ -110,7 +167,7 @@ function renderProducts(arr){
         productCard.appendChild(productImg);
         productCard.appendChild(productInfo);
 
-        cardsContaienr.appendChild(productCard);
+        cardsContainer.appendChild(productCard);
     }
 }
 renderProducts(productList);
