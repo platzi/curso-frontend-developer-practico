68080530d31874214deb56a47ff28b0a63044cfc
Jorge Pinell <jpinell1075@gmail.com>
2022-08-06 21:37:35 -0600

    Integrando menú mobile y media query

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDestopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDestopMenu() {
     // console.log('click');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
