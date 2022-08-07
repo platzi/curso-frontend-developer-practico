ce2a83e6e1666fba7a845570f8f77147c4afb7be
TikenDev <webdesignerfran@gmail.comgit version 2.30.1 (Apple Git-130)tiken@MacBook-Pro-de-Tiken ~ % git config --global user.name TikenDevtiken@MacBook-Pro-de-Tiken ~ % git config --global user.email webdesignerfran@gmail.com>
2022-08-07 21:31:02 +0200

    Productos dinámicos con JS

---
+++ main.js
@@ -0,0 +1,91 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamburgerMenu = document.querySelector('#hamburgerMenu');
+const cart = document.querySelector('.navbar-shopping-cart');
+const asideProductDetail = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburgerMenu.addEventListener('click', toggleMobileMenu);
+cart.addEventListener('click', toggleCartMenu);
+
+
+function toggleDesktopMenu() {
+    asideProductDetail.classList.add('hide-cart');
+    desktopMenu.classList.toggle('inactive');
+
+}
+
+function toggleMobileMenu() {
+    asideProductDetail.classList.add('hide-cart');
+    mobileMenu.classList.toggle('hide-menu');
+}
+
+function toggleCartMenu() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('hide-menu')
+    asideProductDetail.classList.toggle('hide-cart');
+}
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Monitor',
+    price: 400,
+    image: './img/monitor.jpeg'
+});
+
+productList.push({
+    name: 'Laptop',
+    price: 1200,
+    image: './img/laptop.jpeg'
+});
+
+
+
+function renderProducts(arr) {
+
+    for (product of arr) {
+    
+       const productCard = document.createElement('div');
+       productCard.classList.add('product-card');
+    
+       const productImg = document.createElement('img');
+       productImg.setAttribute('src', product.image);
+    
+       const productInfo = document.createElement('div');
+       productInfo.classList.add('product-info');
+    
+       const productInfoDiv = document.createElement('div');
+    
+       const productPrice = document.createElement('p');
+       productPrice.textContent = `${product.price}€`;
+       const productName = document.createElement('p');
+       productName.textContent = product.name;
+    
+       productInfoDiv.append(productPrice, productName);
+    
+       const productInfofigure = document.createElement('figure');
+       const imgAddCart = document.createElement('img');
+       imgAddCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+       productInfofigure.appendChild(imgAddCart);
+    
+       productInfo.append(productInfoDiv, productInfofigure);
+    
+       productCard.append(productImg, productInfo);
+    
+       cardsContainer.appendChild(productCard);
+    
+    }
+
+}
+
+renderProducts(productList);
