4a2d2fe0537942ef806337ad3d57c5042b799f4c
Chema Torres <chemaaa94@gmail.com>
2022-08-08 15:27:50 -0500

    add commit class toggle

--- src/main.js
+++ src/main.js
@@ -1,16 +1,36 @@
+// MENU EMAIL
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+menuEmail.addEventListener("click", toggleDesktopMenu);
+
+
+// MOBILE MENU BAR
 const menuBurguerIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
-
-menuEmail.addEventListener("click", toggleDesktopMenu);
 menuBurguerIcon.addEventListener("click", toggleMobileMenu);
 
 
+// ASIDE CART MENU
+const menuCart = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail")
+menuCart.addEventListener("click",  toggleAsideCart);
+
+
+// MENU BAR
 function toggleDesktopMenu(){
+    aside.classList.add("inactive");
     desktopMenu.classList.toggle("inactive");
 }
 
+// MOBILE MENU
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle("inactive")
+    aside.classList.add("inactive");
+    mobileMenu.classList.toggle("inactive");
+}
+
+// ASIDE CART
+function toggleAsideCart(){
+    mobileMenu.classList.add("inactive")
+    desktopMenu.classList.add("inactive")
+    aside.classList.toggle("inactive")
 }
