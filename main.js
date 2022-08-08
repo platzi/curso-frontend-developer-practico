61c6c82a2178ca553827df26ab466cb60f7a1a6c
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-08-07 20:36:47 -0500

    clase 3 segundo menu

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+// con esto haces que el menu pequeño aparesca y se oculte
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobieMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobieMenu);
 
 function toggleDesktopMenu() {
     console.log('click');
     desktopMenu.classList.toggle('inactive');
 }
-console.log('aqui');
+
+function toggleMobieMenu() {
+    mobieMenu.classList.toggle('inactive');
+}
