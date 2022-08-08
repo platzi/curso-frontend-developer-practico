aea28637bce3f8981862f2c1acf5245285607208
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-08-07 23:46:28 -0300

    Carrito de compras

--- main.js
+++ main.js
@@ -4,12 +4,43 @@ const desktopMenu = document.querySelector(".desktop-menu")
 const iconMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const asideProductDetail = document.querySelector(".product-detail");
+
 navEmail.addEventListener('click', toggleDesktopMenu);
 iconMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+    const isAsideClosed = asideProductDetail.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        asideProductDetail.classList.add('inactive');
+    }
+
+    desktopMenu.classList.toggle('inactive');
 }
+
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    const isAsideClosed = asideProductDetail.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        asideProductDetail.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    //Si posee la clase inactive , esta oculto
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    //Si mobileMenu esta open, hay que cerrarlo
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    asideProductDetail.classList.toggle('inactive');
 }
