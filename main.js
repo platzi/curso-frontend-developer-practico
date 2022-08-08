b54527196d6794e4e4bb200172240de907df1e5a
cavera <cavera.de@gmail.com>
2022-08-08 18:09:50 -0500

    cargando datos al panel de product detail basado en el elemento seleccionado

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const productDetailCloseIcon = document.querySelector(".product-detail-close");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const productDetailContainer = document.querySelector("#productDetail");
+
+const productDetailName = document.querySelector("#productDetailName");
+const productDetailPrice = document.querySelector("#productDetailPrice");
+const productDetailImage = document.querySelector("#productDetailImage");
+const productDetailDescription = document.querySelector("#productDetailDescription");
+
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -42,15 +48,27 @@ function toggleCarritoAside(e) {
 }
 
 function openProductDetailAside(e) {
+	const productId = e.target.id;
+	const productData = productList[productId];
 	desktopMenu.classList.add("inactive");
 	mobileMenu.classList.add("inactive");
 	shoppingCartContainer.classList.add("inactive");
+
+	productDetailName.innerText = productData.name;
+	productDetailPrice.innerText = `$ ${productData.price}`;
+	productDetailDescription.innerText = productData.description;
+	productDetailImage.setAttribute("src", productData.image);
 
 	productDetailContainer.classList.remove("inactive");
 }
 
 function closeProductDetailAside(e) {
 	productDetailContainer.classList.add("inactive");
+
+	productDetailName.innerText = "";
+	productDetailPrice.innerText = "";
+	productDetailDescription.innerText = "";
+	productDetailImage.setAttribute("src", "");
 }
 
 function closeAsidePanels() {
@@ -66,28 +84,33 @@ const productList = [
 		name: "Bike",
 		price: 120,
 		image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		description: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
 	},
 	{
 		name: "TV",
 		price: 1344,
-		image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		image: "https://images.pexels.com/photos/6976094/pexels-photo-6976094.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750",
+		description: "Get the best of Old School aesthetic with the best technology.",
 	},
 	{
 		name: "PC",
 		price: 3200,
-		image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		image: "https://images.pexels.com/photos/2585916/pexels-photo-2585916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750",
+		description: "For home, and office!",
 	},
 ];
 
 renderProducts(productList);
 
 function renderProducts(arr) {
-	for (const product of arr) {
+	// for (const product of arr) {
+	arr.forEach((product, index) => {
 		const productCard = document.createElement("div");
 		productCard.classList.add("product-card");
 
 		const productImg = document.createElement("img");
 		productImg.setAttribute("src", product.image);
+		productImg.setAttribute("id", index);
 		productImg.addEventListener("click", openProductDetailAside);
 
 		const productInfo = document.createElement("div");
@@ -117,5 +140,5 @@ function renderProducts(arr) {
 		productCard.appendChild(productInfo);
 
 		cardsContainer.appendChild(productCard);
-	}
+	});
 }
