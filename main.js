5eb1086328cb224ff86670322aab5910661b27d4
PC trabajo <miguelandresbuitragocruz@gmail.com>
2022-08-08 18:03:44 -0500

    Clase 8 de agosto

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuEmail = document.querySelector(".navbar-email");
+continue
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+ // CONTINUAR CLASE DESDE MINUTO 18:00
+}
