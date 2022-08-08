76f26d8e69f72056c702afac4ba9e0fa05151f3c
Alejandro Manzo <nanommfp@gmail.com>
2022-08-08 16:06:44 +0200

    agregar y progrmar el menu de movil

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 let menuEmail = document.querySelector(".navbar-email");
-let desktopMenu = document.querySelector(".desktop-menu")
+let desktopMenu = document.querySelector(".desktop-menu");
+let menuHamburguesa = document.querySelector(".menu");
+let mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamburguesa.addEventListener("click", toggleBurguerMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactivo");
+    console.log("hola")
+}
+
+function toggleBurguerMenu() {
+    mobileMenu.classList.toggle("inactivo");
     console.log("hola")
 }
