dca4f74fe1b9db4a8d9846d79e1239f17163a11c
joserphc <joserphc@gmail.com>
2022-08-08 10:18:03 -0500

    se agregó product details y algunos estilos del mobile menu

--- src/script/script.js
+++ src/script/script.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carButtonIcon = document.querySelector('.navbar-shopping-cart');
-const asideProductDetail = document.querySelector('.product-detail');
+const shoppingCart = document.querySelector('#shopping-cart-container');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -13,10 +13,10 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 
 burgerMenuIcon.addEventListener('click', toggleMobileMenu);
     function toggleMobileMenu(){
-        const isProductDetail = asideProductDetail.classList.contains('inactive');
+        const isProductDetail = shoppingCart.classList.contains('inactive');
 
         if(!isProductDetail){
-            asideProductDetail.classList.add('inactive');
+            shoppingCart.classList.add('inactive');
         }
         mobileMenu.classList.toggle('inactive');
     }
@@ -31,7 +31,7 @@ carButtonIcon.addEventListener('click', toggleProductDetail);
         }else if(!isDesktopMenuOpen){
             desktopMenu.classList.add('inactive');
         }
-        asideProductDetail.classList.toggle('inactive');
+        shoppingCart.classList.toggle('inactive');
     }
 
     /* Lista de productos */
@@ -42,14 +42,29 @@ carButtonIcon.addEventListener('click', toggleProductDetail);
         name: 'Bike'
     })
     productDetail.push({
-        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        image: 'https://www.sellcell.com/assets/images/home/sell-phone.png',
         price: 250.00,
         name: 'Phone'
     })    
     productDetail.push({
-        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        image: 'https://static2.4gsm.com/eng_pl_WK-Design-Wireless-Bluetooth-Headphones-black-M8-black-79905_1.jpg',
         price: 20.00,
         name: 'Headphone'
+    })
+    productDetail.push({
+        image: 'https://panamericana.vteximg.com.br/arquivos/ids/373973-414-317/camara-instantanea-square-sq1-azul-74101203400.jpg?v=637437445697130000',
+        price: 80.00,
+        name: 'Camera'
+    })
+    productDetail.push({
+        image: 'https://http2.mlstatic.com/D_NQ_NP_997147-MLA49718399013_042022-V.jpg',
+        price: 350.00,
+        name: 'Laptop'
+    })
+    productDetail.push({
+        image: 'https://www.venta-de.com.co/sh-img/u_10167312_monitor%2Bpc.jpg',
+        price: 440.00,
+        name: 'Computer'
     })
 
     function createProductCard(arr){
