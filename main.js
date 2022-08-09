2dbc1286926a7500a2395a949af4167996bef143
Victor sanchez <vese9876@gmail.com>
2022-08-09 15:25:31 -0500

    Product detail configurado sin problemas con los demas eventos

--- main.js
+++ main.js
@@ -2,28 +2,46 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu  = document.querySelector('.desktop-menu');
 const menuCarritoIcon  = document.querySelector('.navbar-shopping-cart');
 const menuHanIcon  = document.querySelector('.menu');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const mobileMenu  = document.querySelector('.mobile-menu');
 const shoppingCartContainer  = document.querySelector('#shoppingCartContainer');
+const productDetailConteiner  = document.querySelector('#productDetail');
 const cardsConteiner = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHanIcon.addEventListener('click',toggleMobileMenu);
-menuCarritoIcon.addEventListener('click',toggleCarritoAside)
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+//funcion del icono close del detalle de producto
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
     shoppingCartContainer.classList.add('inactive');
+    productDetailConteiner.classList.add('inactive');
 }
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
     shoppingCartContainer.classList.add('inactive');
+    productDetailConteiner.classList.add('inactive');
 }
 
 function toggleCarritoAside(){
     shoppingCartContainer.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
+    productDetailConteiner.classList.add('inactive');
+}
+
+//funcion para abrir producto detail
+function openProductDetailAside(){
+    productDetailConteiner.classList.remove('inactive');
+    shoppingCartContainer.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+}
+
+function closeProductDetailAside(){
+    productDetailConteiner.classList.add('inactive');
 }
 
 const productList = [];
@@ -33,15 +51,41 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push({
-    name: 'Pantalla',
-    price: 220,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name: 'Laptop',
+    price: 2220,
+    image: 'https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
 productList.push({
-    name: 'Compu',
-    price: 620,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name: 'Computadora',
+    price: 6020,
+    image: 'https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
+productList.push({
+    name: 'Iphone',
+    price: 4000,
+    image: 'https://images.pexels.com/photos/607812/pexels-photo-607812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Adidas',
+    price: 350,
+    image: 'https://images.pexels.com/photos/1464648/pexels-photo-1464648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Nike',
+    price: 280,
+    image: 'https://images.pexels.com/photos/1456705/pexels-photo-1456705.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Adidas 2',
+    price: 180,
+    image: 'https://images.pexels.com/photos/3281608/pexels-photo-3281608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Nike2',
+    price: 480,
+    image: 'https://images.pexels.com/photos/2385477/pexels-photo-2385477.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
 
 function renderProducts(arr){
     for (product of arr) {
@@ -50,6 +94,8 @@ function renderProducts(arr){
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src',product.image);
+        //agregando evento click para que muestre detalles
+        productImg.addEventListener('click',openProductDetailAside);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
