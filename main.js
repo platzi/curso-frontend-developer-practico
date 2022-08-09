296f8dd055d1eb6454c483c7916a2f992ea314ef
Alejandro Manzo <nanommfp@gmail.com>
2022-08-09 14:55:43 +0200

    aqui logue que solo pudiese estar un menu abierto a la vez, no importa si es en la vercion mobil o desktop

--- main.js
+++ main.js
@@ -2,16 +2,53 @@ let menuEmail = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
 let menuHamburguesa = document.querySelector(".menu");
 let mobileMenu = document.querySelector(".mobile-menu")
+let menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+let productDetail = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
-menuHamburguesa.addEventListener("click", toggleBurguerMenu)
+menuHamburguesa.addEventListener("click", toggleBurguerMenu);
+menuCarritoIcon.addEventListener("click", toggleProductDetail)
+
+
+function toggleBurguerMenu() {
+    let isproductDetailClosed = productDetail.classList.contains("inactivo");
+
+    if (!isproductDetailClosed) {
+        productDetail.classList.add("inactivo")
+    } 
+
+    mobileMenu.classList.toggle("inactivo");
+    console.log("hola")
+}
+
+
+
 
 function toggleDesktopMenu() {
+    let isproductDetailClosed = productDetail.classList.contains("inactivo");
+
+    if (!isproductDetailClosed) {
+        productDetail.classList.add("inactivo")
+    } 
+
     desktopMenu.classList.toggle("inactivo");
     console.log("hola")
 }
 
-function toggleBurguerMenu() {
-    mobileMenu.classList.toggle("inactivo");
+
+function toggleProductDetail() {
+    let isMobileMenuClosed = mobileMenu.classList.contains("inactivo");
+
+    let isDesktopMenuClosed = desktopMenu.classList.contains("inactivo");
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add("inactivo")
+    } 
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactivo")
+    } 
+
+    productDetail.classList.toggle("inactivo");
     console.log("hola")
 }
