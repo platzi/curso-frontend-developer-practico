5bbbbe4937723ffd1815e43f316220b9822f9758
Julio Hernandez <juliohernandezcm@gmail.com>
2022-08-09 12:02:41 -0400

    add fusion burguer icon and mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const BurguerMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navBarEmail.addEventListener('click', toggleDesktopMenu);
+BurguerMenuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+   mobileMenu.classList.toggle('inactive');
 }
