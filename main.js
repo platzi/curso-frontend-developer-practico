49db88abe520edf94ce6753d91d998990b732940
fabnupi23 <31360808+fabnupi23@users.noreply.github.com>
2022-08-08 23:37:20 -0500

    Lista de productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart'); //Cream
 const desktopMenu = document.querySelector('.desktop-menu'); // Cremamos una variable llamada desktopMenu y le decimos que seleccione a cualquier elemento que tenga la clase desktop-menu  
 const mobileMenu = document.querySelector('.mobile-menu');  //También necesitamos un selector para ese menú hamburguesa 
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container'); //Creamos el selector cardsContainer
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu); //utilizamos su propiedad addEventListener para ejecutar algo cuando le demo click.
@@ -46,3 +47,73 @@ function toggleCarritoAside(){  //cada vez que querramos abrir el carrito si le 
     aside.classList.toggle('inactive');
 }
 
+
+const producList = [];
+producList.push({
+    name: 'JavaScript',
+    price: 120,
+    image: 'https://www.anerbarrena.com/wp-content/uploads/2017/08/programacion-javascript-js-e1503258707880.jpg',
+});
+
+producList.push({
+    name: 'React',
+    price: 220,
+    image: 'https://www.syntonize.com/wp-content/uploads/2021/02/React-Syntonize.png',
+});
+
+producList.push({
+    name: 'HTML',
+    price: 320,
+    image: 'https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw',
+});
+
+producList.push({
+    name: 'GitHub',
+    price: 420,
+    image: 'https://rockcontent.com/es/wp-content/uploads/sites/3/2020/07/qu%C3%A9-es-git-hub-y-c%C3%B3mo-funciona-1024x576.jpg',
+});
+
+
+//Esto lo que nos va a permitir es crear cada uno de estos elementos de nuestro html pero utilizando JavaScript 
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');  //Creamos un componente div
+        productCard.classList.add('product-card');      //Le asignamos una clase
+    
+        const productImg = document.createElement('img');      //Creamos una imagen y no le agregamos una clase sino un src
+        //el src no debe ser el enlace de la imagen, sino que debe ser la propiedad image de cada uno de los objetos que representan un producto dentro del array de productos.
+        productImg.setAttribute('src', product.image); //se cambia el atributo de src y se le agrega lo que venga en product.image
+    
+        const productInfo = document.createElement('div');  //Creamos un componente div
+        productInfo.classList.add('product-info');      //Le asignamos una clase
+    
+        const productInfoDiv = document.createElement('div'); //Creamos un componente div
+    
+        const productPrice = document.createElement('p'); //Creamos parrafos
+        productPrice.innerText = '$' + product.price; //A estos parrafos le insertamos el texto con una propiedad 
+        const productName = document.createElement('p');  //Creamos parrafos
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+    
+        const productInfoFigure = document.createElement('figure'); 
+        const productImgCart = document.createElement('img'); //Acá vamos a tener nuestra imagen de producto.
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg'); //También le hacemos una insersión del atributo src.
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProducts(producList);
