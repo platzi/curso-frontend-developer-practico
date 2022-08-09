265c88d353744077d65ca8d0a4c9e9a19f864809
Victor sanchez <vese9876@gmail.com>
2022-08-08 22:32:17 -0500

    Conectando el product detail

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu  = document.querySelector('.desktop-menu');
+const menuCarritoIcon  = document.querySelector('.navbar-shopping-cart');
 const menuHanIcon  = document.querySelector('.menu');
 const mobileMenu  = document.querySelector('.mobile-menu');
+const aside  = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHanIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+function toggleCarritoAside(){
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
 }
