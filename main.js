b3dc442d72c4ceba2263e62dee6372d03d732759
Cristian Coral <erag8n@gmail.com>
2022-08-08 22:10:18 -0500

    Se adicionó el menu aside, falta arreglar cierres y aperturas de los otros menu.

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobilepMenu)
+aside.addEventListener('click', toggleAside)
 
 function toggleDesktopMenu (){
   desktopMenu.classList.toggle('inactive');
@@ -12,4 +15,8 @@ function toggleDesktopMenu (){
 
 function toggleMobilepMenu (){
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAside (){
+  aside.classList.toggle('inactive');
 }
