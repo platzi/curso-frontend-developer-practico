4e49e2520a4b0e6c7fe3bd00ef19705d94ffacca
PC trabajo <miguelandresbuitragocruz@gmail.com>
2022-08-09 12:32:44 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu= document.querySelector(".desktop-menu");
-const menuLeftIcon= document.querySelector(".menu");
-const mobileMenu= document.querySelector(".mobile-menu");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
+const menuLeftIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuLeftIcon.addEventListener("click", toggleMobileMenu);
+menuCarIcon.addEventListener("click", toggleCarAside);
+
 
 function toggleDesktopMenu(){
+    const isCarAsideClose = aside.classList.contains('inactive');
+    if(!isCarAsideClose){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu(){
+    const isCarAsideClose = aside.classList.contains('inactive');
+    if(!isCarAsideClose){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarAside(){
+    const ismobileMenuClose = mobileMenu.classList.contains('inactive');
+    if(!ismobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
