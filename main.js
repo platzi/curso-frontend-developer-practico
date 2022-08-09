74f5fee7dc064a6b686a563cf604e66a0175c08f
Davfool888 <davfool888@gmail.com>
2022-08-09 11:15:21 -0500

    añadiendo el carrito de compras y otras modificaciones

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 const menuEmail = document.querySelector('.navbar-email')
-const DesktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    DesktopMenu.classList.toggle('inactive')
+    
+    desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu (){
+    const isAsideMenuClosed = aside.classList.contains('inactive')
+
+    if (!isAsideMenuClosed){
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside (){
+const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+    // aside.classList.toggle('inactive')
+
+
+if (!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive')
+}
+
+aside.classList.toggle('inactive')
+    
 }
