adfd034f6d37e5264ced110ea301c038c1d6119c
Victor sanchez <vese9876@gmail.com>
2022-08-09 13:50:49 -0500

    Conectando el product detail en el index

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu  = document.querySelector('.desktop-menu');
 const menuCarritoIcon  = document.querySelector('.navbar-shopping-cart');
 const menuHanIcon  = document.querySelector('.menu');
 const mobileMenu  = document.querySelector('.mobile-menu');
-const aside  = document.querySelector('.product-detail');
+const shoppingCartContainer  = document.querySelector('#shoppingCartContainer');
 const cardsConteiner = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
@@ -12,16 +12,16 @@ menuCarritoIcon.addEventListener('click',toggleCarritoAside)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 
 function toggleCarritoAside(){
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
 }
