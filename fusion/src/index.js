e57818c6a4bb0bfb659d6502f54faa309b9c1cc1
carloandf <carlos.andres.ff2912@gmail.com>
2022-08-10 18:49:40 -0500

    Fusionando el navbar con el mobile menu

--- fusion/src/index.js
+++ fusion/src/index.js
@@ -1,9 +1,13 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuHamburger = document.querySelector('.hamburger');
 
 const toggleDesktopMenu = () => desktopMenu.classList.toggle('toggle');
+const toggleMobileMenu = () => mobileMenu.classList.toggle('mobile-menu-toggle');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburger.addEventListener('click', toggleMobileMenu);
 
 window.addEventListener("click",  ({ target }) => {
   if(desktopMenu.classList.contains('toggle') && target != desktopMenu && target != navbarEmail) {
