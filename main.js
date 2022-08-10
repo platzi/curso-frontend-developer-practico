d24ef08caa0ac1ddedc8f4e6175ae4c5df193d74
Carlos Guzman <guzmanfc93@gmail.com>
2022-08-09 20:21:42 -0600

    clase 20 curso practico JS, agregamos el aside

--- main.js
+++ main.js
@@ -2,15 +2,58 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const burgerMenu = document.querySelector('.menu');
+const carritoMenuIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 burgerMenu.addEventListener('click',toggleMobileMenu);
+carritoMenuIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+   
+        let emailNavbar='desktopMenu';
+        closeWindows(emailNavbar);
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+        let mobileMenu='mobileMenu';
+        closeWindows(mobileMenu);
+}
+
+function toggleCarritoAside(){
+        let carritoIcon='carritoDeCompras';
+        closeWindows(carritoIcon);
+
+}
+
+function closeWindows(etiqueta){
+    console.log(etiqueta);
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isAsideOpen = !aside.classList.contains('inactive');
+    const isdesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+
+    switch (etiqueta) {
+        case 'desktopMenu':
+            if (isAsideOpen) {
+                aside.classList.add('inactive');
+            }
+            desktopMenu.classList.toggle('inactive');
+            break;
+        case 'carritoDeCompras':
+            if (isdesktopMenuOpen) {
+                desktopMenu.classList.add('inactive');
+            }else if (isMobileMenuOpen) {
+                mobileMenu.classList.add('inactive');
+            }
+            aside.classList.toggle('inactive');
+            break;
+        case 'mobileMenu':
+            if (isAsideOpen) {
+                aside.classList.add('inactive');
+            }
+            mobileMenu.classList.toggle('inactive');
+            break
+    }
+
 }
