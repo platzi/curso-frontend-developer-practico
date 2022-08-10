e6b9281be6bceea4a618869e192ef47045f1eee5
Efrx <efrainrood@gmail.com>
2022-08-09 22:17:43 -0500

    Fusión del menú en mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
-    // display: block;
+    // display: block; switch to none
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
+window.onresize = function () {
+    document.documentElement.scrollWidth <= 640 && desktopMenu.classList.add('inactive');
+    document.documentElement.scrollWidth >= 641 && mobileMenu.classList.add('inactive');
+  };
+  onresize();
