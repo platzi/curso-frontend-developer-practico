65658a4982cd3095faf6cfbdf17d2e5a21a92046
dudley delgado <arturobonilla0412@gmail.com>
2022-08-10 10:28:23 +0200

    Add functions about active/inactive toggles in navbar

--- main.js
+++ main.js
@@ -1,13 +1,64 @@
+//Variables
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
+//Evento
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+//menuCarritoIcon.addEventListener('click', togglePrueba);
 
+//Función
+/*classList lo que hace es seleccionar las clases del elemento que se le esta indicando*/
+/*El metodo toggle realiza add o remove de la clase que le indiquemos al realizar el evento arriba indicado en este caso click*/
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('js funcionando');
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if(!isAsideClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+/* function togglePrueba(){
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if(!isAsideClose) {
+        desktopMenu.classList.add('inactive');
+    }
+
+} */
+
 
 
 
