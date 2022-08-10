c7d0b0d5dcb6e28c0daa0a06ea2b09b463213e16
rockwatch <rocksistor@gmail.com>
2022-08-10 18:13:45 -0400

    styling menus and aside-product-detail with appear-dissappear element

--- logic.js
+++ logic.js
@@ -1,8 +1,31 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingIcon = document.querySelector('.navbar-shopping-cart');
+const asideProductDetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingIcon.addEventListener('click', toggleProductDetail);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+  if (!asideProductDetail.classList.contains('inactive')) {
+    asideProductDetail.classList.add('inactive');
+  }
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
+  if (!asideProductDetail.classList.contains('inactive')) {
+    asideProductDetail.classList.add('inactive');
+  }
+}
+
+function toggleProductDetail() {
+  asideProductDetail.classList.toggle('inactive');
+  if (!mobileMenu.classList.contains('inactive')) {
+    mobileMenu.classList.add('inactive');
+  }
 }
