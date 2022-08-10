5d129eee35d0cfe2c279fd6d8cdc0607b3cd41ef
dudley delgado <arturobonilla0412@gmail.com>
2022-08-10 21:36:12 +0200

    add new aside product-detail and class inactive, the next laboral day finished this component

--- main.js
+++ main.js
@@ -4,57 +4,57 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 //Evento
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+menuCarritoIcon.addEventListener('click', toggleCarritoshoppingCartContainer);
 //menuCarritoIcon.addEventListener('click', togglePrueba);
 
 //Función
 /*classList lo que hace es seleccionar las clases del elemento que se le esta indicando*/
 /*El metodo toggle realiza add o remove de la clase que le indiquemos al realizar el evento arriba indicado en este caso click*/
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if(!isAsideClosed) {
-        aside.classList.add('inactive');
+    if(!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if(!isAsideClosed) {
-        aside.classList.add('inactive');
+    if(!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCarritoAside(){
+function toggleCarritoshoppingCartContainer(){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
 
     if(!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }
 
-    if(!isAsideClosed) {
+    if(!isshoppingCartContainerClosed) {
         desktopMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 /* function togglePrueba(){
-    const isAsideClose = aside.classList.contains('inactive');
+    const isshoppingCartContainerClose = shoppingCartContainer.classList.contains('inactive');
 
-    if(!isAsideClose) {
+    if(!isshoppingCartContainerClose) {
         desktopMenu.classList.add('inactive');
     }
 
