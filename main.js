756c9fd1399d04fe4ce174c761762f81916c167e
Carlos Guzman <guzmanfc93@gmail.com>
2022-08-09 21:37:11 -0600

    clase 21 de curso practico JS, agrgamos la lista de productos mediante JS

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const burgerMenu = document.querySelector('.menu');
 const carritoMenuIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
-
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 burgerMenu.addEventListener('click',toggleMobileMenu);
@@ -57,3 +57,66 @@ function closeWindows(etiqueta){
     }
 
 }
+
+function renderProducts(arr) {
+  /* el for of nos siver para recorrer un objeto por sus atributos, esto nos sirve para que no tengamos que usar el indice de un array con mayor dificultad cuando se habla de objetos.
+  
+  para maquetar desde el JS con la manipulacion del DOM debemos primero crear los elementos del html de arriba hacia abajo con document.createElement('Elemento a crear: div,p,section, img, etc...') de arriba hacia abajo, luego debemos de ir anexando a los elementos padres cada uno de los elecmentos hijos con appendChild(variable otorgada las elementos), se puede ocupar el append(variable1,variable2,etc...) para agregar mas elementos de una vez en el elemento padre*/  
+for (const product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    
+    const img = document.createElement('img');
+    img.setAttribute('src',product.image);
+    
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+    
+    const productInfoDiv = document.createElement('div');
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerText='$'+product.price;
+    
+    const productName = document.createElement('p');
+    productName.innerText=product.name;
+    
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+    
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+    productInfoFigure.appendChild(productImgCart);
+    
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+    
+    productCard.append(img,productInfo);
+    
+    cardsContainer.appendChild(productCard);
+        
+    }
+}
+
+const productList =[];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: 12000,
+    image: 'https://m.media-amazon.com/images/I/81tmsk6LYiL._AC_SL1500_.jpg'
+});
+
+productList.push({
+    name: 'Computador',
+    price: 1200,
+    image: 'https://m.media-amazon.com/images/I/71n0k0TSRwS._AC_SL1500_.jpg'
+});
+
+renderProducts(productList);
+
