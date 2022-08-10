13fa5ddd0497a501fdce89d0ea105ceb83fbea29
juanca1096 <jcvj16@gmail.com>
2022-08-10 11:00:44 -0500

    Integracion del aside que contiene los productos en el carrito e implementamos pasar el inactive a los otros menus cuando estan abiertos y queremos abrir otro menu

--- main.js
+++ main.js
@@ -2,14 +2,41 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
-menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed){
+        aside.classList.toggle('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.toggle('inactive');
+    }
+
    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isNavMenu = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.toggle('inactive');
+    }
+    if (!isNavMenu){
+        desktopMenu.classList.toggle('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
