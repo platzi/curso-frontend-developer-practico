5d55c498d374dcdcea3a97130a44a8727cea242b
g-pine <pinogallegos.gabriel@gmail.com>
2022-08-11 18:31:35 -0400

    Se agrega interacción del carrito de compras

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
@@ -12,4 +15,8 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive');
 }
