7a836dcfbace625b8d9ae1c1a4017ae9456eddb4
Luis Ramos <luisramossaravia7@gmail.com>
2022-08-11 04:14:41 -0500

    pull requests

--- main.js
+++ main.js
@@ -8,10 +8,14 @@ const mobileMenu = document.querySelector('.mobile-menu'); //contenedor MenuMobi
 
 //ASIDE
 const menuCarritoIcon =document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCardContainer = document.querySelector('#shoppingCardContainer');
 
 //CONTENEDOR DE LISTA DE PRODUCTOS
 const cardsContainer = document.querySelector('.cards-container');
+
+//CONTENEDOR DE DETALLES PRODUCTOS
+const productDetailContainer = document.querySelector('#productDetail')
+const productoDetailCloseIcon = document.querySelector('.product-detail-close')
 
 //cada vez que se le haga click a email, pasara a la funcion
 menuEmail.addEventListener('click', toggleDesktopEmail);
@@ -19,14 +23,16 @@ menuEmail.addEventListener('click', toggleDesktopEmail);
 iconMenu.addEventListener('click', toggleMobileMenu);
 
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+//para cerrar el contenedor de detalle producto
+productoDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 // la funcion tiene por opcion el TOGGLE que es para aparecer y desaparecer
 // classList nos permite tener algunos metodos de desktopMenu
 function toggleDesktopEmail(){
     //para que el aside se cierre cada vez que hago click a email
-    const isAside = aside.classList.contains('inactive');
+    const isAside = shoppingCardContainer.classList.contains('inactive');
     if(!isAside){
-        aside.classList.add('inactive');
+        shoppingCardContainer.classList.add('inactive');
     }
     //para que abra y se cierre 
     desktopMenu.classList.toggle('inactive');
@@ -35,10 +41,13 @@ function toggleDesktopEmail(){
 //FUNCION DE MOBILE MENU
 function toggleMobileMenu(){
     //para que el aside se cierre cada vez que hago click en MenuMobile
-    const isAside = aside.classList.contains('inactive');
+    const isAside = shoppingCardContainer.classList.contains('inactive');
     if(!isAside){
-        aside.classList.add('inactive');
+        shoppingCardContainer.classList.add('inactive');
     }
+    //cada vez que hacemos click en menu, lo demas se cerrara
+    //llamamos a la funcion de cerrar 
+    closeProductDetailAside();
 
     //para que abra y se cierre
     mobileMenu.classList.toggle('inactive');
@@ -59,9 +68,25 @@ function toggleCarritoAside(){
         desktopMenu.classList.add('inactive')
     }
 
+    const isproductDetailClose = productDetailContainer.classList.contains('inactive')
+    if(!isproductDetailClose){ 
+        //si esta abierto el menu, le pasamos para que se cierre
+        productDetailContainer.classList.add('inactive');
+    }
     //para que se cierre o se habra el contenedor
-    aside.classList.toggle('inactive');
+    shoppingCardContainer.classList.toggle('inactive');
 }
+//FUNCION DE DETALLES PRODUCTO
+    function openProductDetailAside(){ //el addeventlistener esta dentro de la funcion render, en imagen
+        //para que ESTE ACTIVO, le quitamos el inactive con remove
+        productDetailContainer.classList.remove('inactive');
+        //cuando quiera entrar nuevamente a carrito de compras, este lo desaparecera
+        shoppingCardContainer.classList.add('inactive')
+    }
+    function closeProductDetailAside(){
+        //Cerrar el contenedor
+        productDetailContainer.classList.add('inactive');
+    }
 
 //LISTA DE PRODUCTOS CREADA DESDE JAVASCRIP
 //1.Creamos el array
@@ -82,19 +107,16 @@ productList.push({
     price: 250,
     Image : "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
-
 productList.push({
     name:'Bike',
     price: 12700,
     Image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
-
 productList.push({
     name:'Bicycle helmet',
     price: 1200,
     Image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
 });
-
 productList.push ({
     name:'Bicycle helmet',
     price: 1600,
@@ -145,8 +167,9 @@ productList.push ({
             </div>
         </div> */
 
+function renderProducts(arr){
 //Creamos los elementos de nuestros array
-for (product of productList){
+for (product of arr){
     //creamos el elemento DIV y su clase product-card
     const productCard = document.createElement('div')
     productCard.classList.add('product-card')
@@ -155,6 +178,8 @@ for (product of productList){
         //product = {name, price, image} -> product.image
         const productImg = document.createElement('img')
         productImg.setAttribute('src', product.Image)
+        //sirve para poder realizar la funcion de solo abrir cuando se haga click
+        productImg.addEventListener('click', openProductDetailAside)
 
         //CREAMOS EL DIV DE LA CLASS:product-info
         const productInfo = document.createElement('div')
@@ -194,3 +219,5 @@ for (product of productList){
 //CONTENEDOR GENERAL, QUE VIENE DESDE HTML
 cardsContainer.appendChild(productCard)
 }
+}
+renderProducts(productList);
