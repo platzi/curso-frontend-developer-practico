0628e76a63d71b6acf75a1bc7e137bd35f08a6f9
Tommosho <tc_lex@hotmail.com>
2022-08-11 15:33:48 -0500

    MenuHabugesa, y pequeños ajustes al media query

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const menuMail = document.getElementById('menuMail');
 const menuDesktop = document.querySelector('.desktop-menu');
-
-
+const menuHamburgesa = document.querySelector('.menu')
+const menuMobile = document.querySelector('.mobile-menu')
 
 menuMail.addEventListener('click',toggleMenu)
+menuHamburgesa.addEventListener('click',toggleMenuMobile)
+
 
 
 function toggleMenu() {
     menuDesktop.classList.toggle("inactive")
+}
+function toggleMenuMobile() {
+    menuMobile.classList.toggle("inactive")
 }
