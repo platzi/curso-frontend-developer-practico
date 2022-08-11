f89bd99de441818ae222b80a490fe2f751520dcd
carlosandf <carlos.andres.ff2912@gmail.com>
2022-08-10 22:08:21 -0500

    Fusionando el navbar con shopping-cart

--- fusion/src/index.js
+++ fusion/src/index.js
@@ -2,12 +2,17 @@ const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuHamburger = document.querySelector('.hamburger');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCart = document.querySelector('.shopping-cart-container');
+
 
 const toggleDesktopMenu = () => desktopMenu.classList.toggle('toggle');
 const toggleMobileMenu = () => mobileMenu.classList.toggle('mobile-menu-toggle');
+const toggleShoppingCart = () => shoppingCart.classList.toggle('toggle-shopping-cart');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburger.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', toggleShoppingCart);
 
 window.addEventListener("click",  ({ target }) => {
   if(desktopMenu.classList.contains('toggle') && target != desktopMenu && target != navbarEmail) {
