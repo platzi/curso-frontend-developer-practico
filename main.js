125d43b56bf7d9912eba04416d15cd5feab5bdfd
siendomiguel <siendomigueldev@gmail.com>
2022-08-11 10:55:03 -0500

    adding more functionality to the menu

--- main.js
+++ main.js
@@ -4,8 +4,20 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const btnBurger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+//---------------------------
 
 const toggleDesktopMenu = () => {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
+
     desktopMenu.classList.toggle('inactive');
 }
 
@@ -13,12 +25,35 @@ navBarEmail.addEventListener('click', toggleDesktopMenu);
 
 
 const toggleMobileMenu = () => {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    
     mobileMenu.classList.toggle('inactive');
+    
 }
 
 btnBurger.addEventListener('click', toggleMobileMenu);
 
 
+const showDetailCart = () => {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isShowDetailCart = aside.classList.contains('inactive')
 
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
 
+    if (isShowDetailCart){
+        desktopMenu.classList.add('inactive');
+    }
+
+    
+    aside.classList.toggle('inactive');
+}
+
+menuCartIcon.addEventListener('click', showDetailCart);
 
