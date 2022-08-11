5c4c46c4968bfda7174a63bd881075a4efa6a4ca
rockwatch <rocksistor@gmail.com>
2022-08-11 17:27:26 -0400

    created product list from js using for of loop and an array. added a margin-top on this container

--- logic.js
+++ logic.js
@@ -29,3 +29,80 @@ function toggleProductDetail() {
     mobileMenu.classList.add('inactive');
   }
 }
+
+/* <div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div> */
+
+const cardsContainer = document.querySelector('.cards-container');
+
+
+const productList = [];
+productList.push({
+  name: 'bike',
+  price: 120,
+  image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+  name: 'screen',
+  price: 220,
+  image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+  name: 'pc',
+  price: 720,
+  image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+//otra utilidad de las funciones es la organización, podemos tener el ciclo for of por dentro de una funcion para poder reutilizar el mismo código cada vez que mandemos a llamar a la funcion. Esto nos permite activar el ciclo cada vez que hagamos click a un determinado boton u otra cosa.
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCart = document.createElement('div');
+    productCart.classList.add('product-card');
+  
+    //product = {name, price, image} -> product.image
+    const productImage = document.createElement('img');
+    productImage.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');  
+  
+    const productDescription = document.createElement('div');
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+    
+    productDescription.appendChild(productName) 
+    productDescription.appendChild(productPrice);
+  
+    productInfo.appendChild(productDescription) 
+    productInfo.appendChild(productInfoFigure);
+  
+    productCart.appendChild(productImage);
+    productCart.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCart);
+  }
+}
+
+renderProducts(productList);
+
