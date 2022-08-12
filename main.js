bbeae7b59ae462a8d5827b0ded267046da146700
Rodrigo Andre <rodrigoandregutierrez@gmail.com>
2022-08-12 05:26:31 -0400

    se agrega funciones de ux botones y menus

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const productDetailCloseIcon = document.querySelector(".product-detail-close");
 const burgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
+const productDetailContainer = document.querySelector("#productDetail");
 const cards = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenu.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside);
 
 function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
@@ -26,6 +29,8 @@ function toggleMobileMenu() {
         shoppingCartContainer.classList.add("inactive");
     }
     mobileMenu.classList.toggle("inactive");
+
+    closeProductDetailAside();
 }
 
 function toggleCarritoAside() {
@@ -35,11 +40,27 @@ function toggleCarritoAside() {
         mobileMenu.classList.add("inactive");
     }
 
+    const isProductDetailClosed =
+        productDetailContainer.classList.contains("inactive");
+
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add("inactive");
+    }
+
     shoppingCartContainer.classList.toggle("inactive");
 }
 
-const productList = [];
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add("inactive");
 
+    productDetailContainer.classList.remove("inactive");
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add("inactive");
+}
+
+const productList = [];
 productList.push({
     name: "Bike",
     price: 120,
@@ -63,6 +84,7 @@ function renderProducts(arr) {
 
         const productImg = document.createElement("img");
         productImg.setAttribute("src", product.image);
+        productImg.addEventListener("click", openProductDetailAside);
 
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
