5953de9cf3d92d7ea55e543d5c0f6189ef263dd6
Daniel Campos <dancamposcastillo@gmail.com>
2022-08-11 20:55:14 -0500

    Fusionando clase 8 mobile-menu a index

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu =  document.querySelector('.mobile-menu');
+
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
 
     console.log('click');
     desktopMenu.classList.toggle('inactive')// Esto lo que hace es que la va a intercambiar osea la quitara o pondra.
+
+}
+
+function toggleMobileMenu()
+{
+
+    mobileMenu.classList.toggle('inactive');
 
 }
