b7cb366ecc317c0620edfd7dfedde6e652ea9783
Angie Cabanillas <acabanillasb2@upao.edu.pe>
2022-08-11 23:38:44 -0500

    chore: :zap: Carrito de compras

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
-const navEmail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const mobileHamIcon = document.querySelector('.menu');
+
+const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-navEmail.addEventListener('click',toggleDesktopMenu);
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail');
+
+
+
+menuEmail.addEventListener('click',toggleDesktopMenu);
 function toggleDesktopMenu(){
+    const isAsideOpen =!aside.classList.contains('inactive');
+    if (isAsideOpen){
+        //se cierra aside
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
-mobileHamIcon.addEventListener ('click',toggleMobileMenu);
-function toggleMobileMenu (){
+menuHamIcon.addEventListener ('click',toggleMobileMenu);
+function toggleMobileMenu(){
+    const isAsideOpen =!aside.classList.contains('inactive');
+    if (isAsideOpen){
+        //se cierra aside
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+menuCarritoIcon.addEventListener ('click',toggleAside);
+function toggleAside(){
+    const isMobileMenuOpen =!mobileMenu.classList.contains('inactive');
+    if (isMobileMenuOpen){
+        //se cierra mobilemenu
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
