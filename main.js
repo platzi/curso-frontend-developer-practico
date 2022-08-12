35a253b18b32755450f05d6de9d616590a290c50
Joe Pulgarin <joe-pulga1@hotmail.com>
2022-08-11 23:31:29 -0500

    menu mobile e interactivdad con js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const BurguerMenu = document.querySelector('.menu');
+const MobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+BurguerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
-
     desktopMenu.classList.toggle('inactive')
-    
+}
 
+function toggleMobileMenu() {
+    MobileMenu.classList.toggle('inactive')
 }
