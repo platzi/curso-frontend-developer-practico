92416e6cff1db4de80b5bd48f10e185066b26d18
Daniel Campos <dancamposcastillo@gmail.com>
2022-08-12 14:31:11 -0500

    Agregando clase 6, creando lista de productos con maquetacion de HTML desde JS

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu =  document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
-
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -49,3 +49,89 @@ function toggleCarritoAside()
  
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+
+})
+
+productList.push({
+
+    name: 'Smart TV',
+    price: 1200,
+    image: 'https://images.samsung.com/is/image/samsung/mx-full-hd-t5300-un43t5300afxzx-frontblack-234815390?$650_519_PNG$',
+
+})
+
+productList.push({
+
+    name: 'Gamer PC',
+    price: 500,
+    image: 'https://quieromipc.com.mx/wp-content/uploads/2022/03/Pc-GAMER-Media-Ryzen-7-5700g-16gb-Fusion-X-1.png',
+
+})
+
+
+/*<div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>
+*/
+
+function renderProducts(array){
+
+    for (product of array){
+        // Por cada producto estamos maqueteando HTML
+            const productCard = document.createElement('div');
+            productCard.classList.add('product-card');
+        
+            const productImg = document.createElement('img');
+            productImg.setAttribute('src', product.image);
+        
+            const productInfo = document.createElement('div');
+            productInfo.classList.add('product-info');
+        
+            const productInfoDiv = document.createElement('div');
+        
+            const productPrice = document.createElement('p');
+            productPrice.innerText = '$'+ product.price;
+            const productName = document.createElement('p');
+            productName.innerText = product.name;
+        
+            productInfoDiv.appendChild(productPrice);
+            productInfoDiv.appendChild(productName);
+        
+            const productFigure = document.createElement('figure');
+            const imgFigure = document.createElement('img');
+            imgFigure.setAttribute('src', './icons/bt_add_to_cart.svg');
+        
+            // Ahora hay que irlos anidando
+        
+            productFigure.appendChild(imgFigure);
+        
+        
+            productInfo.appendChild(productInfoDiv);
+            productInfo.appendChild(productFigure)
+        
+            productCard.appendChild(productImg);
+            productCard.appendChild(productInfo);
+        
+            cardsContainer.appendChild(productCard);
+        
+        }
+
+}
+
+renderProducts(productList);
