95be659bb704bafbefb98388698c55122fbff9b7
Tommosho <tc_lex@hotmail.com>
2022-08-12 01:05:58 -0500

    productos en el main agregados, .gitignore agg

--- main.js
+++ main.js
@@ -4,41 +4,121 @@ const menuHamburgesa = document.querySelector('.menu')
 const menuMobile = document.querySelector('.mobile-menu')
 const shoppingCar = document.querySelector('.navbar-shopping-cart')
 const product_detail = document.querySelector('.product-detail')
+const productsContainer = document.querySelector('.cards-container')
 
-menuMail.addEventListener('click',toggleMenu)
-menuHamburgesa.addEventListener('click',toggleMenuMobile)
-shoppingCar.addEventListener('click',toggleMenushoppingCar)
+menuMail.addEventListener('click', toggleMenu)
+menuHamburgesa.addEventListener('click', toggleMenuMobile)
+shoppingCar.addEventListener('click', toggleMenushoppingCar)
+class productGenerator{
+    constructor({
+        src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        name ='Productaso',
+        price=69,
+    }){
+        this.name=name;
+        this.price=price;
+        this.src=src;
+    }
+    render(place){
+        const cardsContainer = document.createElement('div');
+        cardsContainer.setAttribute('class', 'cards-container');
+        const productCard = document.createElement("div");
+        productCard.setAttribute('class', 'product-card');
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src',this.src);
+        productImg.setAttribute('alt',`${this.name}!!, que buen producto`);
+        const productInfo = document.createElement('div');
+        productInfo.setAttribute('class', 'product-info');
+        const productInfoDetail = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText=`$${this.price}.00`
+        const productName = document.createElement('p');
+        productName.innerText=`${this.name}`
+        const productIconContainer= document.createElement('figure');
+        const productIcon = document.createElement('img');
+        productIcon.setAttribute('src', "./icons/bt_add_to_cart.svg");
+        // empaquetando todos los elementos
+        productIconContainer.appendChild(productIcon);
+        productInfoDetail.append(productPrice, productName);
+        //...
+        productInfo.append(productInfoDetail,productIconContainer);
+        //..
+        productCard.append(productImg,productInfo);
+        place.appendChild(productCard);
+    }
+}
+const firstProduct=new productGenerator({});
+const lastProduct=new productGenerator({name:'Ultimo',price:1000});
+
+const products = []
+products.push(firstProduct)
+products.push(lastProduct)
+
+renderElements(products) 
 
 
-function toggleMenu() {
-    if(!product_detail.classList.contains("inactive")){
+
+//Funciones
+function renderElements(arr) {
+    for (let product of products) {
+        const cardsContainer = document.createElement('div');
+        cardsContainer.setAttribute('class', 'cards-container');
+        const productCard = document.createElement("div");
+        productCard.setAttribute('class', 'product-card');
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src',"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" );
+        productImg.setAttribute('alt',`${product.name}!!, que buen producto`);
+        const productInfo = document.createElement('div');
+        productInfo.setAttribute('class', 'product-info');
+        const productInfoDetail = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText=`$${product.price}.00`
+        const productName = document.createElement('p');
+        productName.innerText=`${product.name}`
+        const productIconContainer= document.createElement('figure');
+        const productIcon = document.createElement('img');
+        productIcon.setAttribute('src', "./icons/bt_add_to_cart.svg");
+        // empaquetando todos los elementos
+        productIconContainer.appendChild(productIcon);
+        productInfoDetail.append(productPrice, productName);
+        //...
+        productInfo.append(productInfoDetail,productIconContainer);
+        //..
+        productCard.append(productImg,productInfo);
+        productsContainer.appendChild(productCard);
         
+
+    }
+}
+function toggleMenu() {
+    if (!product_detail.classList.contains("inactive")) {
+
         product_detail.classList.add("inactive")
     }
     menuDesktop.classList.toggle("inactive")
 }
-
 function toggleMenuMobile() {
-    if(!product_detail.classList.contains("inactive")){
-        
+    if (!product_detail.classList.contains("inactive")) {
+
         product_detail.classList.add("inactive")
     }
-    if(!menuDesktop.classList.contains("inactive")){
-        
+    if (!menuDesktop.classList.contains("inactive")) {
+
         menuDesktop.classList.add("inactive")
     }
     menuMobile.classList.toggle("inactive")
 }
-function toggleMenushoppingCar(){
+function toggleMenushoppingCar() {
     console.log('hola')
-    
-    if(!menuDesktop.classList.contains("inactive")){
-        
+
+    if (!menuDesktop.classList.contains("inactive")) {
+
         menuDesktop.classList.add("inactive")
     }
-    if(!menuMobile.classList.contains("inactive")){
-        
+    if (!menuMobile.classList.contains("inactive")) {
+
         menuMobile.classList.add("inactive")
     }
     product_detail.classList.toggle("inactive")
 }
+
