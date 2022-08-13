e6114ced8cdb5c2bb66bfbff241edffcf353e3c6
chrisgtian <chrisgtian@gmail.com>
2022-08-13 00:11:13 -0500

    Integración del menú mobile (clase8.html) al archivo general index.html

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 navbarEmail.addEventListener('click', modifyDesktopMenu);
+iconMenu.addEventListener('click', modifyMobileMenu);
 
 function modifyDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function modifyMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
