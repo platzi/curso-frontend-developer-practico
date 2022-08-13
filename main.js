f871584752d1cd5ab643f44408185435cf0005c5
jesusLayton <jesuslayton92@gmail.com>
2022-08-13 13:36:41 -0500

    actulizacion de proyecto

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const hamburgIcon = document.querySelector(".menu");
 const mobileMenu= document.querySelector(".mobile-menu");
 const menuCarritoIcon= document.querySelector(".navbar-shopping-cart");
 const aside= document.querySelector(".product-detail");
+const cartsContaines= document.querySelector(".cards-container");
 
 menu.addEventListener("click", toggleDestopMenu);
 hamburgIcon.addEventListener("click", toggleMobileMenu);
@@ -40,3 +41,102 @@ function toggleCarritoMenu(){
     }
     aside.classList.toggle('inactive');
 }
+
+const productList=[];
+
+productList.push({
+    name:'Bike',
+    price:120,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+});
+
+productList.push({
+    name:'Pantalla',
+    price:470,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+});
+
+productList.push({
+    name:'Computador',
+    price:650,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+});
+productList.push({
+    name:'Escritorio',
+    price:650,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+});
+productList.push({
+    name:'Bike',
+    price:120,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+});
+
+productList.push({
+    name:'Pantalla',
+    price:470,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+});
+
+productList.push({
+    name:'Computador',
+    price:650,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+});
+productList.push({
+    name:'Escritorio',
+    price:650,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+});
+
+
+function renderProducts (arr){
+    for (product of arr){
+        const producCard = document.createElement('div');
+        producCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        //product ={nome, price, image} 
+    
+        const producInfo = document.createElement('div');
+        producInfo.classList.add('product-info');
+    
+        const producInfoDiv = document.createElement('div');
+    
+        const producPrice = document.createElement('p');
+        producPrice.innerText = '$' + product.price;
+    
+        const producName = document.createElement('p');
+        producName.innerText = product.name;
+    
+        producInfoDiv.appendChild(producPrice);
+        producInfoDiv.appendChild(producName);
+    
+        const producInfoFigure = document.createElement('figure');
+        const producImgCart = document.createElement('img');
+        producImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        producInfoFigure.appendChild(producImgCart);
+    
+        producInfo.appendChild(producInfoDiv);
+        producInfo.appendChild(producInfoFigure);
+    
+        producCard.appendChild(productImg);
+        producCard.appendChild(producInfo);
+    
+        cartsContaines.appendChild(producCard);
+    
+    }
+
+}
+
+renderProducts(productList);
