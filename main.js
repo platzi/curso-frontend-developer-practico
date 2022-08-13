77ca1b2c15da6c3dc812caa3ddb8b8dc71e2b946
yuliamrg <yuliam.rivera@outlook.com>
2022-08-13 14:39:58 -0500

    se fusiona el menu mobile de la clase 8

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const navMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-navEmail.addEventListener('click', toggleMenu)
+navEmail.addEventListener('click', toggleMenu);
+navMenu.addEventListener('click', toggleMenuMobile);
 
 function toggleMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMenuMobile() {
+    mobileMenu.classList.toggle('inactive');
 }
