486070c6797e3cf49ae6e415880fb7c34cf92f69
Bansant <lautaa26@gmail.com>
2022-08-13 22:43:54 -0300

    aside y arregle algun error al abrir menu en mobile

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
+/* menu */
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+/* carrito */
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+/* opciones menu celular */
 const menuHamIcon = document.querySelector('.hamburgesita');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-
+/* escuchadores */
 menuEmail.addEventListener('click',toggleDesktopMenu);
+
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+
+menuCarritoIcon.addEventListener('click',toggleCarritoAside)
+/* funciones */
 
 
 function toggleDesktopMenu(){
 desktopMenu.classList.toggle('inactive')
+aside.classList.add('inactive') 
 };
+
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive')
-    };
+    aside.classList.add('inactive') 
+       
+};
+
+function toggleCarritoAside(){
+    aside.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    
+};    
