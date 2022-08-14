b780fcefb5f368feb5e53a716ce829797ec0d0f3
Rodrigo Rojas <arrp1239@gmail.com>
2022-08-14 00:08:46 -0400

    Fusion mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const imgMenuMobile = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+imgMenuMobile.addEventListener('click', toggleMobileMenu);
