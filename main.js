bb748c4896a6571e934d91ab4b847fa02c31f08e
Bansant <lautaa26@gmail.com>
2022-08-14 23:18:45 -0300

    arreglando buggs

--- main.js
+++ main.js
@@ -8,34 +8,47 @@ const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const menuHamIcon = document.querySelector('.hamburgesita');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cardsContainer = document.querySelector('.cards-container');
-
+/* Producto */
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 /* escuchadores */
 menuEmail.addEventListener('click',toggleDesktopMenu);
 
 menuHamIcon.addEventListener('click',toggleMobileMenu);
 
-menuCarritoIcon.addEventListener('click',toggleCarritoshoppingCartContainer)
+menuCarritoIcon.addEventListener('click',toggleCarritoshoppingCartContainer);
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside)
 /* funciones */
 
 
 function toggleDesktopMenu(){
-desktopMenu.classList.toggle('inactive')
-shoppingCartContainer.classList.add('inactive') 
+desktopMenu.classList.toggle('inactive');
+shoppingCartContainer.classList.add('inactive') ;
+productDetailContainer.classList.add('inactive');
 };
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
-    shoppingCartContainer.classList.add('inactive') 
+    mobileMenu.classList.toggle('inactive');
+    shoppingCartContainer.classList.add('inactive') ;
+    productDetailContainer.classList.add('inactive');
        
 };
 
 function toggleCarritoshoppingCartContainer(){
-    shoppingCartContainer.classList.toggle('inactive')
-    mobileMenu.classList.add('inactive')
-    desktopMenu.classList.add('inactive')
+    shoppingCartContainer.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
     
-};   
-
+};  
+function openProductDetailAside(){
+    productDetailContainer.classList.remove('inactive');
+    shoppingCartContainer.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+} ;
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
+};
 /* product list / maquetacion de html en cartas */
 const productList = [];
 productList.push({
@@ -58,6 +71,8 @@ productList.push({
     price:250,
     image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
+
+/* functions Render */
 function renderProducts(arr) {
     for (product of productList) {
         const productCard = document.createElement('div');
@@ -65,7 +80,10 @@ function renderProducts(arr) {
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src',product.image);
-    
+
+        productImg.addEventListener('click',openProductDetailAside);
+
+
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
     
