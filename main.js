c489b842ef7337af1a673074198d8e0c5b434c2b
Rodigo Gutierrez <rgutierrez2090@gmail.com>
2022-08-15 12:17:00 +0200

    agregando carrito de compras

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
   console.log('click');
@@ -13,5 +17,12 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
   console.log('click');
+  aside.classList.add('inactive');
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+  console.log('click');
+  mobileMenu.classList.add('inactive');
+  aside.classList.toggle('inactive');
 }
