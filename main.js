8cb0415f408b8c464659385e0cf7847c0b689f00
Eliezer Hernandez <eliezersam93@gmail.com>
2022-08-15 16:48:55 -0400

    Agregando los componentes del archivo clase13.html y clase10.html. Ambas estructuras HTML fueron implementadas en el documento index.html, ademas se agregaron los estilos de cada documento al archivo styles.scss y se desarrollo la programacion en el archivo main.js.

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const $menuEmail = document.querySelector(".navbar-email")
 const $desktopMenu = document.querySelector(".desktop-menu")
+const $iconHamburgerMenu = document.querySelector(".menu")
+const $mobileMenu = document.querySelector(".mobile-menu")
+const $aside = document.querySelector(".product-detail");
+const $menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
 
 $menuEmail.addEventListener("click",toggleMenuDesktop)
+$iconHamburgerMenu.addEventListener("click",toggleMenuMobile)
+$menuCarritoIcon.addEventListener("click",toggleCarritoAside)
 
 function toggleMenuDesktop(){
+    $aside.classList.add("inactive")
     $desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMenuMobile(){
+    $aside.classList.add("inactive")
+    $mobileMenu.classList.toggle("inactive")
+}
+
+function toggleCarritoAside(){
+    $desktopMenu.classList.add("inactive")
+    $mobileMenu.classList.add("inactive")
+    $aside.classList.toggle("inactive")
 }
