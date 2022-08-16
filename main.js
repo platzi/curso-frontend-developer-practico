721731c1804710dd159ff64ebd9464e9389a0b58
Juan Pablo Rosas <juanpabloro18@gmail.com>
2022-08-15 20:23:30 -0500

    Constucción de index sobre el header y pagina de venta con JS

---
+++ main.js
@@ -0,0 +1,130 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    console.log('click');
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    // aside.classList.toggle('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 320,
+    Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Tenis Nike',
+    price: 50,
+    Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'tenis adidas',
+    price: 60,
+    Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'pants adidas',
+    price: 45,
+    Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'balon de basketball',
+    price: 40,
+    Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'jearsy Lakers',
+    price: 50,
+    Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+/*
+<div class="product-card">
+  <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">        <div class="product-info">
+     <div>
+       <p>$120,00</p>
+            <p>Bike</p>
+    </div>
+    <figure>
+      <img src="./icons/bt_add_to_cart.svg" alt="">
+   </figure>
+   </div>
+ </div>
+*/
+
+function renderProducts (arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+    // prodcut =  {name, price, image} --> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.Image);
+        
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const prodcutInfoDiv = document.createElement('div');
+    
+        const prodcutPrice = document.createElement('p');
+        prodcutPrice.innerText = '$' + product.price;
+        const prodcutName = document.createElement('p');
+        prodcutName.innerText = product.name;
+    
+        prodcutInfoDiv.appendChild(prodcutPrice);
+        prodcutInfoDiv.appendChild(prodcutName);
+    
+        const prodcutInfoFigure = document.createElement('figure');
+        const prodcutImgCard = document.createElement('img');
+        prodcutImgCard.setAttribute('src', '/icons/bt_add_to_cart.svg');
+    
+        prodcutInfoFigure.appendChild(prodcutImgCard);
+    
+    
+        productInfo.appendChild(prodcutInfoDiv);
+        productInfo.appendChild(prodcutInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
