5c1cf3453db8f1adf896c1946e2af506349ce25c
hermsale <hermsale@gmail.com>
2022-08-16 17:22:21 -0300

    se agrego animaciones de menu desplegable, en el mobile-menu, y se agrego una query min-width para corregir la vista de los menu en distintos tamaños

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
+// desktop 
+
 // boton para mostrar u ocultar el desktop menu
 const navbarEmail = document.querySelector('.navbar-email');
 // desktop menu
 const desktopMenu = document.querySelector('.desktop-menu');
 
+///////// mobile ///////
+
+// icono hamburguesa - boton
+const burgerIcon = document.querySelector('.menu')
+// mobile menu - menu desplegable
+const mobileMenu = document.querySelector('.mobile-menu');
+
+// evento desktop menu 
 navbarEmail.addEventListener('click',toggleDesktopMenu);
+
+// evento mobile menu 
+burgerIcon.addEventListener('click',toggleMobileMenu);
 
 function toggleDesktopMenu(){
     // intercambiamos entre agregar o quitar la clase 
     desktopMenu.classList.toggle('inactive')
+}
+
+// activa transicion del menu mobile 
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('menuMobile-activo')
 }
