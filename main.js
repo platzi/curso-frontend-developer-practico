de0907b87bb282b20c2ea94b9ec1d5b4f1309a7a
chrisgtian <chrisgtian@gmail.com>
2022-08-16 14:01:51 -0500

    Agregado el listado de productos al carrito de compra (integrado clase13.html a index.html)

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const iconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const navShoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+
 navbarEmail.addEventListener('click', modifyDesktopMenu);
 iconMenu.addEventListener('click', modifyMobileMenu);
+navShoppingCart.addEventListener('click', modifyProductDetail);
 
 function modifyDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
 }
 
 function modifyMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
+}
+
+function modifyProductDetail() {
+    productDetail.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
 }
