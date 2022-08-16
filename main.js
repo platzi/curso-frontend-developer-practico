533bbc36c6d6ac28d704421f45e5c5283785778d
Camilo Puentes <jcpsandoval94@gmail.com>
2022-08-16 16:12:23 -0500

    Se agrega constantes de la clase mobile-menu y menu, y se realiza la funcion toggle para el menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const btnMenuMobile = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+btnMenuMobile.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
