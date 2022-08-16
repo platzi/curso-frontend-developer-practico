ed14507ea453483d5404b64990ba65ca59f996ae
ferneynava <ferneynava@gmail.com>
2022-08-15 22:52:18 -0500

    :white_check_mark: Feat: Fusión del menú en mobile
    Feat: Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mediaQuery = window.matchMedia("(max-width: 640px)");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
-
+menuHamIcon.addEventListener("click",  toggleMobileMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive"); // Si la tiene, la quita. Si no la tiene, la pone.
-    // if(!desktopMenu.classList.toggle("inactive")){
-    //     desktopMenu.classList.remove("inactive");
-    //  }else{
-    //     desktopMenu.classList.add("inactive");
-    //  }
+    desktopMenu.classList.toggle("inactive");
+// //     // if(!desktopMenu.classList.toggle("inactive")){
+// //     //     desktopMenu.classList.remove("inactive");
+// //     //  }else{
+// //     //     desktopMenu.classList.add("inactive");
+// //     //  }
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
+}
+
+//Utilizando solo una función para mostrar los menus
+//  function toggleMenus (){
+//      if(mediaQuery.matches){
+//          mobileMenu.classList.toggle("inactive");
+
+//      } else {
+//          desktopMenu.classList.toggle("inactive");
+//      }
+    
+//  }
+
