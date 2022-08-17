1e9644b75753228a634e7edac884e533b6ce2d63
Nicolas Parada <nicolas.pc.1700@gmail.com>
2022-08-17 11:45:33 -0500

    Se agrega un aside que refleja una descripcion del producto, junto a esto se arreglaron los estilos de ambos aside, el de carito y el de este nuevo

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 /*Menus despligables*/
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shopingCartContainer = document.querySelector("#shopingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 
@@ -16,20 +16,20 @@ menuHamIcon.addEventListener('click', toggleMobilepMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritopAside);
 
 function toggleDesktopMenu () {
-    const isAsideClosed =  aside.classList.contains('inactive');
+    const isAsideClosed =  shopingCartContainer.classList.contains('inactive');
 
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shopingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobilepMenu ()  {
-    const isAsideClosed =  aside.classList.contains('inactive');
+    const isAsideClosed =  shopingCartContainer.classList.contains('inactive');
 
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shopingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -42,7 +42,7 @@ function toggleCarritopAside ()  {
         mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shopingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
