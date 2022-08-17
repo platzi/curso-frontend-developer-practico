1e424d9d39dca80eb9cede4266d6edc89b9bd999
Israel Torres <dietokotipelto@gmail.com>
2022-08-17 12:51:12 -0500

    menu functionalities

--- main.js
+++ main.js
@@ -2,17 +2,21 @@ const menuEmail = document.querySelector('.navbar-email');
 const deskTopMenu = document.querySelector('.desktop-menu');
 const shopingCartIcon = document.querySelector('.navbar-shopping-cart');
 const cardsContainer = document.querySelector('.cards-container');
+const closeButtonProductDetail = document.getElementById('closeProductDetail');
 
-const menuMobile = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const menuMobile = document.querySelector('.menu');//hamburguer icon
+const mobileMenu = document.querySelector('.mobile-menu');//mobile menu
 const shopingCartMenu = document.querySelector('.product-detail');
+const productDetail = document.querySelector('.product-detail-secondary');
 
 
 menuEmail.addEventListener('click', toggleMenu);
 menuMobile.addEventListener('click', toggleMenu);
 shopingCartIcon.addEventListener('click', toggleMenu);
+closeButtonProductDetail.addEventListener('click',showProductDetail);
 
 function toggleMenu(event) {
+    productDetail.classList.add('inactive');
     if(event.target.className == 'navbar-email'){
         deskTopMenu.classList.toggle('inactive');
         shopingCartMenu.classList.add('inactive');
@@ -25,6 +29,15 @@ function toggleMenu(event) {
         deskTopMenu.classList.add('inactive');
     }
 }
+function showProductDetail(event){
+    shopingCartMenu.classList.add('inactive');
+    deskTopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    if(productDetail.classList.contains('inactive'))
+        productDetail.classList.remove('inactive');
+    else
+        productDetail.classList.add('inactive');
+}
 
 function setProducts(productList){
     for(product of productList){
@@ -33,6 +46,8 @@ function setProducts(productList){
     
         const img = document.createElement('img');
         img.setAttribute('src', product.image);
+        img.setAttribute('class', 'product.image');
+        img.addEventListener('click',showProductDetail);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -68,6 +83,14 @@ productList.push({
     image : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 },{
     name : 'Computadora',
+    price : '12000.00',
+    image : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+},{
+    name : 'Pelota',
+    price : '12000.00',
+    image : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+},{
+    name : 'botella',
     price : '12000.00',
     image : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
