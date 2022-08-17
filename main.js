6c64c6db9ef9be96d6d114aeefa8bba6ca8a8d3f
cristian-nicp <cristian.nicp@gmail.com>
2022-08-17 00:11:02 -0300

    interaccion con menuBurger agregado

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 // Variables
 const desktopMenu = document.querySelector('.desktop-menu');
 const navbarEmail = document.querySelector('.navbar-email');
+const menuBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-
+// Desktop Menu
 navbarEmail.addEventListener('click',() => {
     desktopMenu.classList.toggle('showMenu');
-    console.log("clicked, working")
+    console.log("clicked, working navbarEmail")
     /* remove classlist if clicked out */
+})
+
+// Mobile Menu
+menuBurger.addEventListener('click',() => {
+    //burger in x
+    menuBurger.setAttribute('src', './icons/x.svg');
+    if (mobileMenu.classList.contains('showMobileMenu')) {
+        menuBurger.setAttribute('src', './icons/icon_menu.svg');
+    }
+
+    // menuBurger.classList.toggle('showMenu');
+    mobileMenu.classList.toggle('showMobileMenu');
+    console.log("clicked, working menuBurger")
+    /* remove classlist if clicked out */
+
+
 })
