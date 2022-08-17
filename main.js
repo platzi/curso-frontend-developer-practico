bd772008b87e9da491ded19004976a980bdfd58d
Marianela Hernandez <marrowlove2817@gmail.com>
2022-08-17 19:45:10 +0100

    reconstruyendo el aside

--- main.js
+++ main.js
@@ -3,41 +3,43 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const burgerMenuIcon = document.querySelector(".menu");
 const carritoMenuIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenuIcon.addEventListener("click", toggleMobileMenu);
-carritoMenuIcon.addEventListener("click", toggleCarritoAside);
+carritoMenuIcon.addEventListener("click", toggleCarritoshoppingCartContainer);
 
 function toggleDesktopMenu(params) {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isShoppingCartContainerClosed =
+    shoppingCartContainer.classList.contains("inactive");
 
-  if (!isAsideClosed) {
-    aside.classList.add("inactive");
+  if (!isShoppingCartContainerClosed) {
+    shoppingCartContainer.classList.add("inactive");
   }
 
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(params) {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isShoppingCartContainerClosed =
+    shoppingCartContainer.classList.contains("inactive");
 
-  if (!isAsideClosed) {
-    aside.classList.add("inactive");
+  if (!isShoppingCartContainerClosed) {
+    shoppingCartContainer.classList.add("inactive");
   }
 
   mobileMenu.classList.toggle("inactive");
 }
 
-function toggleCarritoAside(params) {
+function toggleCarritoshoppingCartContainer(params) {
   const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
 
   if (!isDesktopMenuClosed) {
     desktopMenu.classList.add("inactive");
   }
 
-  aside.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
