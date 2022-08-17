f62b15e3118d9d56545bdb06b84a38a43ade4265
cristian-nicp <cristian.nicp@gmail.com>
2022-08-17 15:00:29 -0300

    interaccion con carrito de compras

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const navbarEmail = document.querySelector('.navbar-email');
 const menuBurger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 // Desktop Menu
 navbarEmail.addEventListener('click',() => {
+    // para que no se solape el menu con el contenido
+    const isAsideOpen = aside.classList.contains('showAside');
+    if (isAsideOpen) {
+        aside.classList.remove('showAside');
+    }
+
     desktopMenu.classList.toggle('showMenu');
     console.log("clicked, working navbarEmail")
     /* remove classlist if clicked out */
@@ -13,6 +21,13 @@ navbarEmail.addEventListener('click',() => {
 
 // Mobile Menu
 menuBurger.addEventListener('click',() => {
+    const isAsideOpen = aside.classList.contains('showAside');
+    // para que no se solape el menu con el product detail
+    if (isAsideOpen) {
+        aside.classList.remove('showAside');
+    }
+    
+
     //burger in x
     menuBurger.setAttribute('src', './icons/x.svg');
     if (mobileMenu.classList.contains('showMobileMenu')) {
@@ -22,7 +37,19 @@ menuBurger.addEventListener('click',() => {
     // menuBurger.classList.toggle('showMenu');
     mobileMenu.classList.toggle('showMobileMenu');
     console.log("clicked, working menuBurger")
-    /* remove classlist if clicked out */
-
 
 })
+
+// Shopping Cart
+shoppingCart.addEventListener('click',() => {
+    // para que no se solapen las ventanas
+    const isMobileMenuOpen = mobileMenu.classList.contains('showMobileMenu');
+    const isAsideOpen = aside.classList.contains('showAside');
+
+    //si el mobileMenu esta open
+    if (isMobileMenuOpen) {
+        mobileMenu.classList.toggle('showMobileMenu');
+    }
+
+    aside.classList.toggle('showAside');
+});
