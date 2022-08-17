0bd20cc9251ecdfdb90a43aeb57a9d28a1b43da2
Sharon <sharoncorpas9730@gmail.com>
2022-08-17 16:25:03 -0500

    se fucionan lel nav, productos y se genera interactividad en los menú. Tambien se realiza maquetacion de los productos desde js

---
+++ main.js
@@ -0,0 +1,133 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+ 
+
+const menuShoppingCar = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+const cardsContainer = document.querySelector('.cards-container');
+
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+iconMenu.addEventListener('click',  toggleMobileMenu);
+menuShoppingCar.addEventListener('click',  toggleAside);
+
+
+function toggleDesktopMenu(){
+    
+    aside.classList.add('inactive')
+    desktopMenu.classList.toggle('inactive');
+
+}
+
+   function toggleMobileMenu(){   
+    
+    aside.classList.add('inactive')
+    mobileMenu.classList.toggle('inactive');
+
+/*const isAsideClosed = aside.classList.contains('inactive');
+
+   if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+     mobileMenu.classList.toggle('inactive');
+
+     La misma funcion con condicionales para vericar si esta abierto o cerrado.
+    */
+    
+
+}
+
+function toggleAside(){
+
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive')
+
+   /* const isMobileClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive')*/
+}
+
+
+
+           const productList = [];
+           productList.push({
+            name: 'Bike',
+            price: 120,
+            image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+           });
+           productList.push({
+            name: 'Pantalla',
+            price: 120,
+            image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+           });
+           productList.push({
+            name: 'Compu',
+            price: 120,
+            image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+           });
+           productList.push({
+            name: 'Compu',
+            price: 120,
+            image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+           });
+           productList.push({
+            name: 'Compu',
+            price: 120,
+            image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+           });
+           productList.push({
+            name: 'Compu',
+            price: 120,
+            image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+           });
+
+
+  
+        function renderProducts(arr){ 
+      for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+
+        //product = {name, price, image} --> product.image
+        const productImg = document.createElement('img'); 
+        productImg.setAttribute('src', product.image); //modificar su propiedad src, por el objeto 
+         
+        const productInfo= document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productDiv.appendChild(productPrice);
+        productDiv.appendChild(productName);
+
+        const productFigure = document.createElement('figure'); 
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg' );
+
+        productFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productDiv);
+        productInfo.appendChild(productFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+
+      }
+    };
+
+    renderProducts(productList);
