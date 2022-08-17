538249e77cc1e27b25c4dc2e019a229895c6bbfc
Camilo Puentes <jcpsandoval94@gmail.com>
2022-08-17 08:49:24 -0500

    Se agrega funcion de toggle para el aside, se corrigen conflictos de aperturas de lso distintos menus

--- main.js
+++ main.js
@@ -2,14 +2,45 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const btnMenuMobile = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
-btnMenuMobile.addEventListener('click', toggleMobileMenu)
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+btnMenuMobile.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    //aside.classList.toggle('inactive');
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    }
+    
+    aside.classList.toggle('inactive')
+
 }
