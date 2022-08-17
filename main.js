0c8bbb142a208d46c2ea526b2be74e53dfdeb1d3
Nicolas Parada <nicolas.pc.1700@gmail.com>
2022-08-16 20:43:03 -0500

    Se arregla la interacion entre los menus desplegables

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 
+/* Click sobre */
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+
+/*Menus despligables*/
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobilepMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritopAside);
 
 function toggleDesktopMenu () {
+    const isAsideClosed =  aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobilepMenu ()  {
+    const isAsideClosed =  aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritopAside ()  {
+    const isMobileMenuClosed =  mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
