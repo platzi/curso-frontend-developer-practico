d90be2ad65cce2a1dbc7ed3cd7b3219cf996d0d5
jdalmario0044 <josealmario084@gmail.com>
2022-08-17 23:06:55 -0500

    Clase 21  no se hacia commit desde la clase 18, se unificaron varios archivos HTML y se trabajo en el archivo js

--- main.js
+++ main.js
@@ -1,10 +1,105 @@
 // Selector de elementos HTML
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCarIcon = document.querySelector('.navbar-shopping-cart');
+const asideShoppingCart = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurger.addEventListener('click', toggleMobilepMenu);
+shoppingCarIcon.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu() {
+    const isAsideShoppingCartClosed = asideShoppingCart.classList.contains('inactive');
+
+    if (!isAsideShoppingCartClosed) {
+        asideShoppingCart.classList.add('inactive');
+     }
+
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobilepMenu() {
+    const isAsideShoppingCartClosed = asideShoppingCart.classList.contains('inactive');
+
+    if (!isAsideShoppingCartClosed) {
+       asideShoppingCart.classList.add('inactive');
+    }
+    
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+       mobileMenu.classList.add('inactive');
+    }
+    // if (!isDesktopMenuClosed) {
+    //    desktopMenu.classList.add('inactive');   
+    // }
+    desktopMenu.classList.add('inactive');
+    asideShoppingCart.classList.toggle('inactive');
+}
+
+const productList = [];
+
+productList.push({
+    name: 'bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'   
+});
+productList.push({
+    name: 'Phone Nokia',
+    price: 250,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Mouse Logitech',
+    price: 25,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const ProductImg = document.createElement('img');
+        ProductImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productinfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        
+    
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        
+        cardsContainer.appendChild(productCard);
+        productCard.appendChild(ProductImg);
+        productCard.appendChild(productInfo);
+        productInfo.appendChild(productinfoDiv);
+        productinfoDiv.appendChild(productPrice);
+        productinfoDiv.appendChild(productName);
+        productInfo.appendChild(productInfoFigure);
+        productInfoFigure.appendChild(productImgCart);
+    }
+}
+
+renderProducts(productList);
