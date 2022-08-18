79ae0ea6d5489f0196081f7eb2ed619b4d489d7e
eduzgs <eduardo.zgs@gmail.com>
2022-08-18 17:20:08 -0400

    Menu desktop mobile

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
-menuEmail.addEventListener('click', toggleMenu );
+const imgBurger = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu")
 
-function toggleMenu(event){
+menuEmail.addEventListener('click', toggleMenuDesktop);
+imgBurger.addEventListener('click', toggleMenuMobile);
+
+function toggleMenuDesktop(){
     desktopMenu.classList.toggle('inactive');
 }// con toggle es mas facil velo pero felicidades por completar el reto
 // Mi forma de cumplir el reto
@@ -13,3 +17,8 @@ function toggleMenu(event){
 //     desktopMenu.classList.remove('inactive');
 //     } else{
 //         desktopMenu.classList.add('inactive');
+
+function toggleMenuMobile(){
+    console.log('yea');
+    mobileMenu.classList.toggle('inactivem');
+}
