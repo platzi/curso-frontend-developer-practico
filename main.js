0a99bfdff7568beecd5c15d1ba6b539b439f651f
kafka22 <nicolascabrera108@gmail.com>
2022-08-17 23:49:37 -0300

    Creando interacciones entre todos los componentes

--- main.js
+++ main.js
@@ -3,14 +3,57 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carritoIcon = document.querySelector('.navbar-shopping-cart');
-const asideProduct = document.querySelector('.product-detail');
+const asideProduct = document.querySelector('.myorder');
 const cardsContainer = document.querySelector('.cards-container');
+const productDetail = document.querySelector('.product-detail');
+const productDetailClose = document.querySelector('.product-detail-close');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuHamIcon.addEventListener('click', toogleMobileMenu);
 carritoIcon.addEventListener('click', toogleAsideProduct);
+productDetailClose.addEventListener('click', closeProductDetailAside);
 
 function toogleAsideProduct(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetail = productDetail.classList.contains('inactive');
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    }
+    if(!isProductDetail){
+        productDetail.classList.add('inactive');
+    }
+    asideProduct.classList.toggle('inactive');
+ }
+
+function toogleMobileMenu(){
+    const isAsideClosed = asideProduct.classList.contains('inactive')
+    const isProductDetail = productDetail.classList.contains('inactive');
+    if (!isAsideClosed){
+        asideProduct.classList.add('inactive');
+    }
+    if(!isProductDetail){
+        productDetail.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toogleDesktopMenu(){
+    const isAsideClosed = asideProduct.classList.contains('inactive');
+    const isProductDetail = productDetail.classList.contains('inactive');
+    if (!isAsideClosed){
+        asideProduct.classList.add('inactive');
+    }
+    if(!isProductDetail){
+        productDetail.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
+}
+function openProductDetailAside(){
+    const isAsideClosed = asideProduct.classList.contains('inactive');
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
     if(!isMobileMenuClosed){
@@ -19,28 +62,39 @@ function toogleAsideProduct(){
     if(!isDesktopMenuClosed){
         desktopMenu.classList.add('inactive')
     }
-    asideProduct.classList.toggle('inactive');
- }
-
-function toogleMobileMenu(){
-    const isAsideClosed = asideProduct.classList.contains('inactive')
     if (!isAsideClosed){
         asideProduct.classList.add('inactive');
     }
-    mobileMenu.classList.toggle('inactive');
+
+    productDetail.classList.remove('inactive');
 }
 
-function toogleDesktopMenu(){
-    const isAsideClosed = asideProduct.classList.contains('inactive')
-    if (!isAsideClosed){
-        asideProduct.classList.add('inactive');
-    }
-    desktopMenu.classList.toggle('inactive');
+
+
+
+function closeProductDetailAside(){
+    productDetail.classList.add('inactive');
 }
+
 
 //a generar los productos desde javascript!
 //creamos el array con varios elementos que son objetos...
 const productList = [];
+productList.push({
+    nombre:"bike",
+    precio: 120,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    nombre:"guitarra",
+    precio: 1120,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    nombre:"bandoneon",
+    precio: 1120,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
 productList.push({
     nombre:"bike",
     precio: 120,
@@ -76,16 +130,18 @@ productList.push({
     //creando elementos: cambiamos sus fuentes, añadimos clases, le insertamos texto y valor.
     //crear padres e hijos. ¿Quien va dentro de quien?
     //si encapsulamos todo el ciclo for dentro de una funcion, podemos utilizarlo despues de otras maneras!
+
 function renderProducts(arr){
     for (product of arr){
         const productCard = document.createElement('div');
-        productCard.classList.add('product-card');
+        productCard.classList.add('main-product-card');
     
         const img = document.createElement('img');
         img.setAttribute('src', product.imagen);
+        img.addEventListener('click', openProductDetailAside);
     
         const productInfo = document.createElement('div');
-        productInfo.classList.add('product-info');
+        productInfo.classList.add('main-product-info');
     
          const productInfoDiv = document.createElement('div');
     
