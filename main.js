1081afe70ec6f54d9531537b93a1cddb8204a6e0
jose matheus <matheusjosenolberto23@gmail.com>
2022-08-18 22:07:54 +0200

    mostrar y ocultar menu de navegacion

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ const navLogin = document.querySelector('.navbar-email');
 
 const menu = document.querySelector('.desktop-menu');
 
+const categorias = document.querySelector('.menu');
+
+const movilV = document.querySelector('.mobile-menu');
+
 navLogin.addEventListener('click', mostrar);
+categorias.addEventListener('click', mostrarMenu);
 
 function mostrar() {
     menu.classList.toggle('inactive')
 
+};
+
+function mostrarMenu() {
+    movilV.classList.toggle('inactive')
 };
