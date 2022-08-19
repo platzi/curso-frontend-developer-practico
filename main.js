c07e94a0befa5bbe3adc78b4a4dd973862c0a743
hermsale <hermsale@gmail.com>
2022-08-19 17:24:03 -0300

    creamos un array, pusheamos los productos. y por medio de AppendChild se renderizo. Se encapsulo en una function todo el codigo de renderizado, para poder utilizarlo con otros arrays

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ const mobileMenu = document.querySelector('.mobile-menu');
 // boton shopping cart 
 const shoppingCart = document.querySelector('.navbar-shopping-cart');
 // muestra aside navbar shopping cart 
-const asideShoppingCart = document.querySelector('.product-detail')
+const asideShoppingCart = document.querySelector('.product-detail');
+
+const cardsContainer = document.querySelector('.cards-container');
 
 //////////////////////////// EVENTOS //////////////////////////
 // desktop menu 
@@ -79,3 +81,116 @@ function toggleAsideShoppingCart(){
 
     
 }
+
+    // ejemplo de elemento que recreamos desde JS
+    // 1)  <div class="product-card">
+    //     <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    //     <div class="product-info">
+    //       <div>
+    //         <p>$120,00</p>
+    //         <p>Bike</p>
+    //       </div>
+    //       <figure>
+    //         <img src="./icons/bt_add_to_cart.svg" alt="">
+    //       </figure>
+    //     </div>
+    //   </div> 
+
+ // encapsulamos eel codigo en una function, como buena practica - es practico para reutilizar en otros arrays tambien
+    // o eventualmente podemos cargarla o deshabilitar por medio de una interfaz de boton por ejemplo 
+    // CREACION DEL ELEMENTO CON EL DOM  y CARGA DE INTERFAZ
+    function renderProducts(arr){
+        for(product of arr){
+            // creamos el div
+            const productCard = document.createElement('div');
+            // le agregamos la clase al Elemento
+            productCard.classList.add('product-card');
+    
+            // creamos un elemento img
+            const productImg = document.createElement('img');
+            // modificamos el atributo src, por medio de setAttribute 
+            productImg.setAttribute('src',product.img);
+    
+             // creamos el div
+             const productInfo = document.createElement('div');
+             // le agregamos la clase al Elemento
+             productInfo.classList.add('product-info');
+    
+             const productInfoDiv = document.createElement('div');
+    
+            //  parrafo que contendra el precio 
+             const productPrice = document.createElement('p');
+    
+            // incertamos el precio
+            productPrice.innerText = '$'+product.price;
+    
+            // parrafo que contendra el nombre 
+            const productName = document.createElement('p');
+            // incertamos el nombre 
+            productName.innerText = product.name;
+    
+            // colocamos dentro del div los dos parrafos 
+            productInfoDiv.appendChild(productPrice);
+            productInfoDiv.appendChild(productName);
+    
+            const productInfoFigure = document.createElement('figure');
+    
+            const productImgCart = document.createElement('img');
+            productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+            // metemos dentro de la etiqueta figure, el productInfoFigure
+            productInfoFigure.appendChild(productImgCart);
+    
+            // agregamos a productInfo los dos bloques productInfo y el figure 
+            productInfo.appendChild(productInfoDiv);
+            productInfo.appendChild(productInfoFigure);
+    
+            // agregamos a productCard los bloques 
+            productCard.appendChild(productImg);
+            productCard.appendChild(productInfo);
+    
+            // agregamos al cards Product el producto
+    
+            cardsContainer.appendChild(productCard);
+        }
+    }
+
+
+///////////// AGREGAMOS MANUALMENTE PRODUCTOS AL ARRAY ////////////////
+const productList = [];
+
+// caracteristicas de los elementos del array: imagen, nombre y precio. 
+
+productList.push({
+    name: 'Byke',
+    price: 120,
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productList.push({
+    name: 'PC',
+    price: 1400,
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productList.push({
+    name: 'Smart TV',
+    price: 12000,
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+console.log(productList)
+   
+     // renderizamos los productos
+     renderProducts(productList);
+   
+// nos da el elemento del array 
+for(product of productList){
+    console.log(product.name);
+    console.log(product.img);
+}
+
+// nos da el indice 
+// for(product in productList){
+//     console.log(product);
+// }
