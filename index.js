c902c8b37c98736c1c7c87e4de34c4f90ee87e85
Alejandro Fonseca <fonsecacuzaalejandro@gmial.com>
2022-08-20 11:07:53 +0200

    version movil terminada y otros cambios

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let email = document.querySelector(".navbar-email");
 let menu = document.querySelector(".desktop-menu");
+let mobile_menu = document.querySelector(".mobile-menu");
+let menu_hamb = document.querySelector(".menu");
 
 email.addEventListener("click", () => {
-    menu.classList.toggle("ocultar");
+    menu.classList.toggle("oculta");
+})
+
+menu_hamb.addEventListener("click", () => {
+    mobile_menu.classList.toggle("oculta");
 })
