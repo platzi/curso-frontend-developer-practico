3bdf5304ad6fbb356cd244d9dfd68f0752c50887
eduzgs <eduardo.zgs@gmail.com>
2022-08-20 16:55:08 -0400

    Burguer menu, menu desktop and aside interactives

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
 const imgBurger = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu")
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const imgShoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
+
 
 menuEmail.addEventListener('click', toggleMenuDesktop);
 imgBurger.addEventListener('click', toggleMenuMobile);
+imgShoppingCart.addEventListener('click', toggleShoppingAside);
 
 function toggleMenuDesktop(){
+    const isAsideOpen = !productDetail.classList.contains('inactive');
+    console.log(isAsideOpen);
+    if(isAsideOpen){
+        productDetail.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }// con toggle es mas facil velo pero felicidades por completar el reto
 // Mi forma de cumplir el reto
@@ -19,6 +29,22 @@ function toggleMenuDesktop(){
 //         desktopMenu.classList.add('inactive');
 
 function toggleMenuMobile(){
-    console.log('yea');
-    mobileMenu.classList.toggle('inactivem');
+    const isAsideOpen = !productDetail.classList.contains('inactive');
+    console.log(isAsideOpen);
+    if(isAsideOpen){
+        productDetail.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingAside(){
+    const ismMobileOpen = !mobileMenu.classList.contains('inactive');
+    const isMenuOpen = !desktopMenu.classList.contains('inactive');
+    console.log(ismMobileOpen);
+    if(ismMobileOpen){
+        mobileMenu.classList.add('inactive');
+    } else if(isMenuOpen){
+        desktopMenu.classList.add('inactive');
+    }
+    productDetail.classList.toggle('inactive');
 }
