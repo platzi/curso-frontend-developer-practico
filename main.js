487e0a59a122fea5edd08a299d35810966762134
davidcol <mezad55@gmail.com>
2022-08-19 23:04:41 -0500

    Integramos funcionalidades al menu de la tienda

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 const menuEmail = document.querySelector('.navbar-email');
 const miniMenu = document.querySelector('.desktop-menu');
 
+const menuCarritoeIcon = document.querySelector('.navbar-shopping-cart');
+const carritoMenu = document.querySelector('.product-detail');
+
+const menuMobileIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleMiniMenu);
+menuMobileIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoeIcon.addEventListener('click', toggleCarritoMenu);
 
 function toggleMiniMenu() {
-    console.log('click');
+    
     miniMenu.classList.toggle('inactive');
+    carritoMenu.classList.add('inactive');
 }
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+    carritoMenu.classList.add('inactive');
+}
+
+function toggleCarritoMenu() {
+    mobileMenu.classList.add('inactive');
+    miniMenu.classList.add('inactive');
+    carritoMenu.classList.toggle('inactive');
+    
+}
