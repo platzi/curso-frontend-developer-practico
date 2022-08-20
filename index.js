5eea31a6061af6dee3c7dd9160c5631536717098
Alejandro Fonseca <fonsecacuzaalejandro@gmial.com>
2022-08-20 12:17:13 +0200

    le agrege la funcionalidad al boton de carrito de compras y otras funcionalidades

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ let menu = document.querySelector(".desktop-menu");
 let mobile_menu = document.querySelector(".mobile-menu");
 let menu_hamb = document.querySelector(".menu");
 
+let icono_carrito = document.querySelector(".navbar-shopping-cart");
+let aside = document.querySelector(".product-detail");
+
 email.addEventListener("click", () => {
     menu.classList.toggle("oculta");
 })
 
 menu_hamb.addEventListener("click", () => {
     mobile_menu.classList.toggle("oculta");
+})
+
+icono_carrito.addEventListener("click", () => {
+    aside.classList.toggle("oculta");
+    if (!mobile_menu.classList.contains('oculta')) {
+        mobile_menu.classList.add('oculta')
+    }
+
 })
