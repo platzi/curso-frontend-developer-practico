0de0c05a41257f90aebf7a839cb067bcf737ac58
Jeferson <alexandermejicanos57@gmail.com>
2022-08-19 23:35:11 -1200

    Merge mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobilMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburgerIcon.addEventListener('click', toggleMobilMenu);
