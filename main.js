6821f535794d20d82cfdca7f1ee06285e61326b6
ferneynava <ferneynava@gmail.com>
2022-08-20 10:40:53 -0500

    :white_check_mark: Feat: Carrito de compras
    Feat: Carrito de compras

--- main.js
+++ main.js
@@ -1,24 +1,54 @@
 const navEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const mediaQuery = window.matchMedia("(max-width: 640px)");
+const aside = document.querySelector(".product-detail");
+let bandera1;
+//const mediaQuery = window.matchMedia("(max-width: 640px)");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click",  toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClose = aside.classList.contains("inactive");
     desktopMenu.classList.toggle("inactive");
+
+    if(!isAsideClose){
+        aside.classList.add("inactive");
+    }
 // //     // if(!desktopMenu.classList.toggle("inactive")){
 // //     //     desktopMenu.classList.remove("inactive");
 // //     //  }else{
 // //     //     desktopMenu.classList.add("inactive");
 // //     //  }
+    
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle("inactive");
+    const isAsideClose = aside.classList.contains("inactive");
+    mobileMenu.classList.toggle("inactive"); //Si esta lo elimina, si no esta lo activa 
+    if(!isAsideClose){
+        aside.classList.add("inactive");
+    }
 }
+
+function toggleCarritoAside(){    
+    const isMobileMenuClose =  mobileMenu.classList.contains("inactive");
+    const isDesktopMenuClose = desktopMenu.classList.contains("inactive"); // El elemento contiene la clase inactive = true (cerrado)
+                                                                           // El elemento no contiene la clase inactive = falso (abierto)
+
+    aside.classList.toggle("inactive");
+    if(!isMobileMenuClose){ // si esta abierto 
+        mobileMenu.classList.add("inactive");
+    }
+
+    if(!isDesktopMenuClose){
+        desktopMenu.classList.add("inactive")
+    }   
+}
+
 
 //Utilizando solo una función para mostrar los menus
 //  function toggleMenus (){
@@ -30,4 +60,3 @@ function toggleMobileMenu() {
 //      }
     
 //  }
-
