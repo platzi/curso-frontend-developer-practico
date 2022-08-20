8f482ca2837e348a9421ab7a689d05c139864d85
Jeferson <alexandermejicanos57@gmail.com>
2022-08-19 19:24:02 -1200

    Merge of the desktop menu

---
+++ main.js
@@ -0,0 +1,8 @@
+const navEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
+
+navEmail.addEventListener('click', toggleDesktopMenu);
