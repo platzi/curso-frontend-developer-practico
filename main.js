7a0ffe97c37ccac2aafa8e65eb6800de19fe765c
pamelaovalles <pamela.ovalles.18@gmail.com>
2022-08-20 12:23:23 -0400

    fusion del menu mobile
    anadiendo el menu mobile, se utilizo media query para cuando pase a desktop no se quede activo el menu mbile

--- main.js
+++ main.js
@@ -2,11 +2,22 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+//MENU MOBILE
+const menuHamburger = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 //WORK
 function toggleDesktopMenu() {
   // si la clase inactive está presente la elimina, de lo contrario la añade
   desktopMenu.classList.toggle("inactive");
 }
 
+function toggleMobileMenu() {
+  // si la clase inactive está presente la elimina, de lo contrario la añade
+  mobileMenu.classList.toggle("inactive");
+}
+
 //EVENT
 navEmail.addEventListener("click", toggleDesktopMenu);
+
+menuHamburger.addEventListener("click", toggleMobileMenu);
