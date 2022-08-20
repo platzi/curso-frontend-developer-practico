8c6460f07cbb985339e49b0b7ec1d013d5adfd33
mariosepulveda <masepulvedar@uqvirtual.edu.co>
2022-08-19 23:48:34 -0500

    fusion de la lista de productos de la aplicacion y genera la lista con un array desde js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
@@ -44,7 +45,50 @@ function toggleMobileMenu() {
     }
 
     mobileMenu.classList.toggle('inactive');
-
-
 }
+
+const productList = [];
+const tamList = 10;
+for(let i = 1; i<=tamList;i++){
+    productList.push({
+        name: `Bike_${i}`,
+        price: 120,
+        image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    });
+}
+
+for(product of productList){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src',product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productInfoFigure = document.createElement('figure');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.append(productPrice,productName);
+
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.append(productInfoDiv,productInfoFigure);
+
+    productCard.append(productImg,productInfo);
+
+    cardsContainer.appendChild(productCard);
+}
+
+
 
