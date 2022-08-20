1e7f6d0fb4825fe1a6ac9f8c25498b7392a444f9
Esteban Mejia <estebandeveloper10@gmail.com>
2022-08-20 14:50:58 -0500

    Agregamos el meni de detalles del producto en la fusion y arreglamos responsive desing

--- main.js
+++ main.js
@@ -4,18 +4,46 @@ const navEmail = document.querySelector(`.navbar-email`);
 const desktopMenu = document.querySelector(`.desktop-menu`);
 const navHamIcon = document.querySelector(`.ham-menu-icon`);
 const mobileNav =  document.querySelector(`.mobile-menu`);
+const navCarShop = document.querySelector(`.navbar-shopping-cart`);
+const ProdutDetail = document.querySelector(`.product-detail`);
 
 
 navEmail.addEventListener(`click`, toggleDesktopMenu);
 navHamIcon.addEventListener(`click`, toggleMobileMenu);
-
+navCarShop.addEventListener(`click`, toggleProductDetail);
 
 
 function toggleDesktopMenu() {
-    console.log(`click`)
+    const isProductDetailClosed = ProdutDetail.classList.contains(`inactive`);
+    if(!isProductDetailClosed) {
+        ProdutDetail.classList.add(`inactive`);
+    }
+
     desktopMenu.classList.toggle(`inactive`);
 }
 
 function toggleMobileMenu() {
+    const isProductDetailClosed = ProdutDetail.classList.contains(`inactive`);
+
+
+    if(!isProductDetailClosed){
+        ProdutDetail.classList.add(`inactive`);
+    }
     mobileNav.classList.toggle(`inactive`);
+}
+
+function toggleProductDetail() {
+
+    const isMobileNavClosed = mobileNav.classList.contains(`inactive`);
+    const isDesktopMenuClosed = desktopMenu.classList.contains(`inactive`);
+
+    if(!isMobileNavClosed){
+        mobileNav.classList.add(`inactive`);
+    }
+    
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add(`inactive`);
+    }
+    ProdutDetail.classList.toggle(`inactive`);
+
 }
