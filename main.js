eae537860c12bd09209123d6091bb5a92e568d42
jose matheus <matheusjosenolberto23@gmail.com>
2022-08-22 00:39:59 +0200

    arreglando las vistas de los menu

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ const navLogin = document.querySelector('.navbar-email');
 
 const menu = document.querySelector('.desktop-menu');
 
+const carritoIcon = document.querySelector('.navbar-shopping-cart');
+
 const categorias = document.querySelector('.menu');
 
 const movilV = document.querySelector('.mobile-menu');
 
+const aside = document.querySelector('.product-detail');
+
 navLogin.addEventListener('click', mostrar);
 categorias.addEventListener('click', mostrarMenu);
+/*aside.addEventListener('click',  )*/
+carritoIcon.addEventListener('click', toggleCcarritoIcon)
 
 function mostrar() {
     menu.classList.toggle('inactive')
@@ -16,4 +22,10 @@ function mostrar() {
 
 function mostrarMenu() {
     movilV.classList.toggle('inactive')
+};
+
+function toggleCcarritoIcon() {
+    aside.classList.toggle('inactive')
+
+
 };
