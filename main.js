34058b9ab4f972f609e47b96a101d8bc459dcb4e
Miguel Perez <mperezs@antioquia.gov.co>
2022-08-22 11:08:07 -0500

    Menus Completos con mobile

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
- const menuEmail = document.querySelector(".navbar-email");
- const menudesktopMenu = document.querySelector(".desktop-menu");
- const menuHamIcon = document.querySelector(".menu");
- const mobileMenu = document.querySelector(".mobile-menu");
+const menuEmail = document.querySelector(".navbar-email");
+const menudesktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
- menuEmail.addEventListener('click', toogleDesktoMenu);
- menuHamIcon.addEventListener('click', toogleMobileMenu);
+menuEmail.addEventListener("click", function () {
+  toogleGeneral(menudesktopMenu);
+});
+menuHamIcon.addEventListener("click", function () {
+  toogleGeneral(mobileMenu);
+});
+menuCarritoIcon.addEventListener("click", function () {
+  toogleGeneral(aside);
+});
 
+function toogleGeneral(visualizar) {
+  const estadoToogle = !visualizar.classList.contains("inactive");
+  aside.classList.add("inactive");
+  mobileMenu.classList.add("inactive");
+  menudesktopMenu.classList.add("inactive");
 
- function toogleDesktoMenu() {
-    
-    menudesktopMenu.classList.toggle('inactive');
- }
-
- function toogleMobileMenu() {
-    
-    mobileMenu.classList.toggle('inactive');
- }
+  if (estadoToogle) {
+    visualizar.classList.add("inactive");
+  } else {
+    visualizar.classList.remove("inactive");
+  }
+}
