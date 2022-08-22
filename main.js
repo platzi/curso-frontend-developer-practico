f0717bb87989d2fec5f7423566cd309112cedefc
Miguel Perez <mperezs@antioquia.gov.co>
2022-08-22 09:36:14 -0500

    Menus Completos

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
  const menuEmail = document.querySelector(".navbar-email");
  const menudesktopMenu = document.querySelector(".desktop-menu");
+ const menuHamIcon = document.querySelector(".menu");
+ const mobileMenu = document.querySelector(".mobile-menu");
+
  menuEmail.addEventListener('click', toogleDesktoMenu);
+ menuHamIcon.addEventListener('click', toogleMobileMenu);
+
 
  function toogleDesktoMenu() {
-    console.log ("Hola");
+    
     menudesktopMenu.classList.toggle('inactive');
+ }
+
+ function toogleMobileMenu() {
+    
+    mobileMenu.classList.toggle('inactive');
  }
