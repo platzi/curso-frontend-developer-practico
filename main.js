0c3b706b6922cab0b088de8becabbf4d5f1c4df2
Kevin Garcia <kevingarcore@gmail.com>
2022-08-22 01:25:52 -0500

    shopping cart integre and funcional

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-const userEmail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
+const menuBurger = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
-const hamburgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
-userEmail.addEventListener('click', toggleDesktopMenu);
-hamburgerMenu.addEventListener('click', toggleMobileMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurger.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu () {
+  const isAsideClosed = aside.classList.contains('inactive');
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu () {
+  const isAsideClosed = aside.classList.contains('inactive');
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside () {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClosed || !isDesktopMenuClosed) {
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+  }
+
+  aside.classList.toggle('inactive');
 }
