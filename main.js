aab6c078232e460d5f010359405b92ee2a40756a
Miguel Perez <mperezs@antioquia.gov.co>
2022-08-22 14:23:03 -0500

    Arreglo en javascript, maquitacion html desde javascript

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const menudesktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
-const cardsContainer = document.querySelector(".cards-container");
+const shoppingCarContainer = document.querySelector("#shoppingCarContainer");
+const  cardsContainer =  document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", function () {
   toogleGeneral(menudesktopMenu);
@@ -13,12 +13,12 @@ menuHamIcon.addEventListener("click", function () {
   toogleGeneral(mobileMenu);
 });
 menuCarritoIcon.addEventListener("click", function () {
-  toogleGeneral(aside);
+  toogleGeneral(shoppingCarContainer);
 });
 
 function toogleGeneral(visualizar) {
   const estadoToogle = !visualizar.classList.contains("inactive");
-  aside.classList.add("inactive");
+  shoppingCarContainer.classList.add("inactive");
   mobileMenu.classList.add("inactive");
   menudesktopMenu.classList.add("inactive");
 
@@ -31,60 +31,63 @@ function toogleGeneral(visualizar) {
 
 const productList = [];
 productList.push({
-  name: "Bike",
-  price: 120000,
-  image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-});
+    name: "Bike",
+    price: 120000,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  });
+  
+  productList.push({
+    name: "Pantalla",
+    price: 820000,
+    image: "https://www.lg.com/co/images/monitores/md07545401/gallery/D-1.jpg",
+  });
+  
+  productList.push({
+      name: "Computador",
+      price: 1720000,
+      image:
+        "https://www.compulago.net/wp-content/uploads/erp/productos/2323%2006333927/1621307495760.jpg",
+    });
+ 
 
-productList.push({
-  name: "Pantalla",
-  price: 820000,
-  image: "https://www.lg.com/co/images/monitores/md07545401/gallery/D-1.jpg",
-});
+    function renderproducts (arr) {
+        for (product of arr) {
 
-productList.push({
-  name: "Computador",
-  price: 1720000,
-  image:
-    "https://www.compulago.net/wp-content/uploads/erp/productos/2323%2006333927/1621307495760.jpg",
-});
+            const productCard= document.createElement('div');
+            productCard.classList.add('product-card');
+    
+            const productImgp = document.createElement('img');
+            productImgp.setAttribute('src', product.image);
+    
+            const productInfo= document.createElement('div');
+            
+            productInfo.classList.add('product-info');
+    
+            const productInfoDiv= document.createElement('div');
+    
+            const productPrice= document.createElement('p');
+            productPrice.innerText = '$ '+ product.price;
+    
+            const productName= document.createElement('p');
+            productName.innerText = '$ '+ product.name;
+            
+            productInfoDiv.appendChild(productPrice);
+            productInfoDiv.appendChild(productName);
+    
+            const productInfofigure= document.createElement('figure');
+            const productImgCart= document.createElement('img');
+            productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+            productInfofigure.appendChild(productImgCart);
+    
+            productInfo.appendChild(productInfoDiv);
+            productInfo.appendChild(productInfofigure);
+            productCard.appendChild(productImgp);
+            productCard.appendChild(productInfo);
+            cardsContainer.appendChild(productCard);
+          
+    }
+    }
 
-function renderproducts(arr) {
-  for (product of arr) {
-    const productCard = document.createElement("div");
-    productCard.classList.add("product-card");
-
-    const productImgp = document.createElement("img");
-    productImgp.setAttribute("src", product.image);
-
-    const productInfo = document.createElement("div");
-
-    productInfo.classList.add("product-info");
-
-    const productInfoDiv = document.createElement("div");
-
-    const productPrice = document.createElement("p");
-    productPrice.innerText = "$ " + product.price;
-
-    const productName = document.createElement("p");
-    productName.innerText = "$ " + product.name;
-
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
-
-    const productInfofigure = document.createElement("figure");
-    const productImgCart = document.createElement("img");
-    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
-
-    productInfofigure.appendChild(productImgCart);
-
-    productInfo.appendChild(productInfoDiv);
-    productInfo.appendChild(productInfofigure);
-    productCard.appendChild(productImgp);
-    productCard.appendChild(productInfo);
-    cardsContainer.appendChild(productCard);
-  }
-}
-
-renderproducts(productList);
+   renderproducts (productList);
