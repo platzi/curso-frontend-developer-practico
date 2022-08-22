24dca496ecda2634fc79f5c3c6680f1e975288b6
Angel Fermin <angelfermin14@gmail.com>
2022-08-21 23:22:59 -0400

    commit del dia 21, Se fuciono menu mobile y se corrigio el css para los temas de responsi con los menus

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMEnu = document.querySelector('.desktop-menu');
+const mobileMenu =document.querySelector('.mobile-menu');
+const navMovile = document.querySelector('.menu');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+navMovile.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
-    desktopMEnu.classList.toggle('inactive')
+    desktopMEnu.classList.toggle('inactive');
 
+
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 
 }
