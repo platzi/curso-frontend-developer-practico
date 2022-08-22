62109d2ea7f334e8f7a69b7676941787e5f14f93
Kevin Garcia <kevingarcore@gmail.com>
2022-08-22 00:14:38 -0500

    integration of mobile menu and nav bar

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const userEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 userEmail.addEventListener('click', toggleDesktopMenu);
+hamburgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu () {
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu () {
+  mobileMenu.classList.toggle('inactive');
 }
