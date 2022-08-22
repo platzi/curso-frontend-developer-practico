7e788b7f47f45d64e2e69bbfac854502eea33606
Juan Villa <ing.villa.jimenez@gmail.com>
2022-08-21 20:06:41 -0500

    fusion menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const hamburgerButton = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navbarEmail.addEventListener("click", toggleDesktopMenu);
+hamburgerButton.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
