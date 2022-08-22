5cc0b5185d55be443d879d6161b8730b17bbcc28
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-22 10:55:49 -0500

    Insertion of product details, arrangement of the css, arrangement detail ccs in mobile menu

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuIcon = document.querySelector('.menu');
 const menuShoppingCartIcon = document.querySelector('.navbar-shopping-cart')
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const shoppingCartAside = document.querySelector('.product-detail');
+const shoppingCartAside = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -56,21 +56,6 @@ productList.push({
     price: 820,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
-
-/*
-<div class="product-card">
-<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-<div class="product-info">
-  <div>
-    <p>$120,00</p>
-    <p>Bike</p>
-  </div>
-  <figure>
-    <img src="./icons/bt_add_to_cart.svg" alt="">
-  </figure>
-</div>
-</div>
-*/
 
 for (product of productList) {
     const productCard = document.createElement('div');
