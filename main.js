a0f82ccd61de8a78698f569ac04716e14c3e7e82
darckronoz <crispipe14@gmail.com>
2022-08-23 16:02:38 -0500

    product list added to the page

--- main.js
+++ main.js
@@ -4,10 +4,83 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const linemenu = document.querySelector('.menu');
 const shopingMenu = document.querySelector('.navbar-shopping-cart');
 const orderDetail = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 email_navbar.addEventListener('click', toggleNavMenu);
 linemenu.addEventListener('click', toggleMobMenu);
 shopingMenu.addEventListener('click', toggleOrders);
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'TV',
+    price: 1120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Graphics Card',
+    price: 3500,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Book',
+    price: 50,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pen',
+    price: 99999,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+fillProducts(productList);
+
+function fillProducts(list) {
+    for(n of list) {
+        //create elements 
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', n.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + n.price + ',00';
+        const productName = document.createElement('p');
+        productName.innerText = n.name;
+    
+        const cartIconFigure = document.createElement('figure');
+    
+        const cartIcon = document.createElement('img');
+        cartIcon.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        //fill elements
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        productInfo.appendChild(productInfoDiv);
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        productInfo.appendChild(cartIconFigure);
+        cartIconFigure.appendChild(cartIcon);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+
 
 function toggleNavMenu() {
     let isOrderDetailOpen = !orderDetail.classList.contains('inactive');
@@ -46,4 +119,6 @@ function toggleOrders() {
         orderDetail.classList.toggle('inactive');
     }
 }
+
+
 
