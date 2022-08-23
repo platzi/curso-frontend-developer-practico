3d44a48bfc8a049c3a04e70705d8a7c1fe60654e
danielcardvilla <danielcardvillarroel@gmail.com>
2022-08-23 00:03:23 -0500

    tercera integracion

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     DesktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+
 }
