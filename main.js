bb231551807d242c31b88f2fe38e1b1969dcbbd2
Jeferson <alexandermejicanos57@gmail.com>
2022-08-23 02:07:15 -1200

    Merge product list

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamburgerIcon = document.querySelector('.menu');
 const menuShoppingCar = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const carContainer = document.querySelector('.cards-container');
 
 function toggleDesktopMenu() {
     const isAsideOpen = !aside.classList.contains('inactive');
@@ -40,6 +41,64 @@ function toggleShoppingCar() {
 
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Screen',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Computer',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+function renderProduct(array) {
+    for (product of array) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const divContainer = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        divContainer.append(productPrice, productName);
+    
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.append(divContainer, productInfoFigure);
+    
+        productCard.append(img, productInfo);
+    
+        carContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProduct(productList);
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburgerIcon.addEventListener('click', toggleMobilMenu);
