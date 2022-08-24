d3d6440eab178f13ed7303aec8d63454a921c554
IvoPiccinini <ivopiccinini8@gmail.com>
2022-08-24 11:57:25 -0300

    Agregue el menu desplegable de la izquierda (en mobile)

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     // .classList es para ver qué clases tiene el elemento
     // .toggle es para activar o desactivar la clase seleccionada
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
