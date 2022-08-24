2ac8d1bbd6becf523568da892d643de55936514f
Eduardo Díaz <eduaedodiaz@gmail.com>
2022-08-24 01:13:26 -0500

    Interacción entre todos los componentes

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 
 function toggleDesktopMenu() {
@@ -29,7 +32,7 @@ function toggleMobileMenu() {
     if(!isAsideClosed) {
         shoppingCartContainer.classList.add('inactive');
     }   
-
+    closeProductDetailAside();
     mobileMenu.classList.toggle('inactive');
 }
 
@@ -39,8 +42,23 @@ function toggleCarritoAside() {
     if(!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }   
+       
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+       
+    if(!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive');
+    }   
+    
     shoppingCartContainer.classList.toggle('inactive');
     
+}
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive');
+
+    productDetailContainer.classList.remove('inactive');
+}
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -82,6 +100,7 @@ function renderProducts(arr) {
      
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside);
      
         
         const productInfo = document.createElement('div');
