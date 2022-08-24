408b8c1ab7a374c4b2d2071ea1c9544d602d9be9
danielcardvilla <danielcardvillarroel@gmail.com>
2022-08-24 01:00:52 -0500

    Revisar erores de la clase 20 // no aparece el carrito de compras//

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-
-
-
-
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
     DesktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!isAsideClosed) {
+        // si el mobileMenu is open, hay que cerrarlo
+        mobileMenu.classList.add('inactive');
+        }
 
+        mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+/*     aside.classList.toggle('inactive');*/
+    if (!isMobileMenuClosed) {
+    // si el mobileMenu is open, hay que cerrarlo
+    mobileMenu.classList.add('inactive');
+    }
+}  
+
