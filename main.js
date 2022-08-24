169ca6d0330712fb3c859b7fdcad6f2aed7d1ea0
Luis Garcia <luiscarlopz@gmail.com>
2022-08-23 20:04:27 -0600

    Se añadió función para evitar generar o colapsar menús de elementos

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", togglemobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
+  const isAsideClosed =
+    aside.classList.contains(
+      "inactive"
+    ); /*Esto hace que la lógica funcione cuando un elemento se abra entonces el otro se cierra.*/
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
 }
 
 function togglemobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  /*Funciona y se explica mejor aquí - hace que la lógica funcione cuando un elemento se abra entonces el otro se cierra.*/
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
 }
