882794b2c21862c6fdd4439bb0b2f8538e9adfe6
Saul De la fuente B <saul.delafuenteb@gmail.com>
2022-08-23 23:31:48 -0500

    Se integra el aside del carrito de compras

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const navEmail = document.querySelector('.navbar-email');
-const dektopMenu = document.querySelector('.desktop-menu'); 
+const desktopMenu = document.querySelector('.desktop-menu'); 
 const menuHamIcon = document.querySelector('.menu'); 
 const mobileMenu = document.querySelector('.mobile-menu'); 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 navEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarrito);
 //***Funciones*********************************************************************** */
 function toggleDesktopMenu() {
-    dektopMenu.classList.toggle('inactive');
-    
+    desktopMenu.classList.toggle('inactive');
+    if (!desktopMenu.classList.contains('inactive')) {
+        aside.classList.add('inactive');
+    }
 }
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    if (!mobileMenu.classList.contains('inactive')) {
+        aside.classList.add('inactive');
+    }
+    
+}
+function toggleCarrito() {
+    
+    if (!mobileMenu.classList.contains('inactive')) {
+        aside.classList.add('inactive');
+        return;
+    }
+    if (!desktopMenu.classList.contains('inactive')) {
+         aside.classList.add('inactive');
+        return;
+    }
+    aside.classList.toggle('inactive');
+    
 }
