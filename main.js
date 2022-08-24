540cb6d7d0eea6d84c67262b27f494762a497105
mariosepulveda <masepulvedar@uqvirtual.edu.co>
2022-08-23 22:04:09 -0500

    FUSION DE LA VERDADERA DSCRIPCION DEL PRODUCTO

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -23,25 +23,25 @@ menuCarritoIcon.addEventListener('click',()=>{
         desktopMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 
 });
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');   
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');   
     
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');   
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');   
     
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
