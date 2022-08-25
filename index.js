fe35d74a02474d5f4de9095b4d8368b0000f9661
Saul De la fuente B <saul.delafuenteb@gmail.com>
2022-08-24 23:04:55 -0500

    Se construyo el contenido en el DOM desde Java Script usando la creacion de elementos y el metodo appendChild(nodo)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu'); 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
-
+const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -36,3 +36,61 @@ function toggleCarrito() {
     aside.classList.toggle('inactive');
     
 }
+function renderProducts(array) {
+
+    for (product of array) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    const img = document.createElement('img');
+    img.setAttribute('src', product.image);
+    
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+   
+
+
+    const fig = document.createElement('figure');
+    const imgFigure = document.createElement('img');
+    imgFigure.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+    //COnstruyendo el HTML
+    fig.appendChild(imgFigure);
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(fig);
+    productCard.appendChild(img);
+    productCard.appendChild(productInfo);
+    cardsContainer.appendChild(productCard);
+
+}
+}
+
+//Simulando la base de datos
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: 'Pantalla',
+    price: 1200,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: 'Laptop',
+    price: 12500,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+renderProducts(productList);
+
