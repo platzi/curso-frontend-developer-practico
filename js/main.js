729a42a78e60c130d55218c39c0db273c7c510a4
Efrx <efrainrood@gmail.com>
2022-08-24 21:29:06 -0500

    agregamos sincronizacion a el aside de la info del producto que cliqueamos

--- js/main.js
+++ js/main.js
@@ -78,62 +78,84 @@ window.onresize = function () {
       name: 'Bike',
       price: 120,
       image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
     },
 
     {
       name: 'Smath TV',
       price: 220,
-      image: "https://images.unsplash.com/photo-1528928441742-b4ccac1bb04c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",},
-
+      image: "https://images.unsplash.com/photo-1528928441742-b4ccac1bb04c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
     {
       name: 'Urban T-shirt',
       price: 30,
-      image: "https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",},
+      image: "https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
     {
       name: 'Runners shoes',
       price: 25,
-      image: "https://images.pexels.com/photos/12714890/pexels-photo-12714890.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",},
+      image: "https://images.pexels.com/photos/12714890/pexels-photo-12714890.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
     {
       name: 'Smarthphone',
       price: 80,
-      image: "https://i.pinimg.com/564x/f6/ee/ec/f6eeece1e2493e867a76ad705ea8939d.jpg",},
+      image: "https://i.pinimg.com/564x/f6/ee/ec/f6eeece1e2493e867a76ad705ea8939d.jpg",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
     {
       name: 'PC Gamer',
       price: 5,
-      image: "https://i.pinimg.com/564x/4d/66/ce/4d66ce27cee973c3c3b416a5d7e1eae1.jpg",},
+      image: "https://i.pinimg.com/564x/4d/66/ce/4d66ce27cee973c3c3b416a5d7e1eae1.jpg",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
     {
       name: 'Headset',
       price: 50,
-      image: "https://i.pinimg.com/236x/ca/d6/5c/cad65c0cf8c512ffcd5ec647d4359cd0.jpg",},
+      image: "https://i.pinimg.com/236x/ca/d6/5c/cad65c0cf8c512ffcd5ec647d4359cd0.jpg",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
     { 
       name: 'Pants',
       price: 8,
-      image: "https://i.pinimg.com/736x/e9/54/6e/e9546eebf085de7959b9a8e833a8e049.jpg",},
+      image: "https://i.pinimg.com/736x/e9/54/6e/e9546eebf085de7959b9a8e833a8e049.jpg",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
     {
       name: 'Fan',
       price: 20,
-      image: "https://i.pinimg.com/236x/09/dc/30/09dc30c97eac8602185e29b82243fe79.jpg",},
+      image: "https://i.pinimg.com/236x/09/dc/30/09dc30c97eac8602185e29b82243fe79.jpg",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
     {
       name: 'Fan',
       price: 20,
-      image: "https://i.pinimg.com/236x/09/dc/30/09dc30c97eac8602185e29b82243fe79.jpg",},
+      image: "https://i.pinimg.com/236x/09/dc/30/09dc30c97eac8602185e29b82243fe79.jpg",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
     {
       name: 'Fan',
       price: 20,
-      image: "https://i.pinimg.com/236x/09/dc/30/09dc30c97eac8602185e29b82243fe79.jpg",},
+      image: "https://i.pinimg.com/236x/09/dc/30/09dc30c97eac8602185e29b82243fe79.jpg",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
     {
       name: 'Fan',
       price: 20,
-      image: "https://i.pinimg.com/236x/09/dc/30/09dc30c97eac8602185e29b82243fe79.jpg",},
+      image: "https://i.pinimg.com/236x/09/dc/30/09dc30c97eac8602185e29b82243fe79.jpg",
+      info: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace.",
+    },
 
   ];
   
@@ -146,10 +168,10 @@ function renderProducts(arr) {
       /* HTML */
       `
       <div class="product-card">
-        <img class="card-img" src=" ${product.image} " alt="">
+        <img class="card-img" id="${count}" src=" ${product.image} " alt="">
         <div class="product-info">
           <div>
-            <p> ${product.price} </p>
+            <p>$ ${product.price} </p>
             <p> ${product.name} </p>
           </div>
           <figure>
@@ -183,26 +205,9 @@ function closeProductDetailAside() {
 // cardsContainer.childNodes...
 
 const myOrderList = [];
+const detailOfProduct = {};
 
-function addProductToCart () {
-  cardsContainer.addEventListener('click',(e) => {
-    if(e.target && e.target.classList.contains('addToCart')){
-      // console.log(String(e.target.getAttribute('id')));
-      let orderId = parseInt(e.target.getAttribute('id'));
-      myOrderList.push(
-        {
-          name: String(productList[orderId].name),
-          price: String(productList[orderId].price),
-          image: String(productList[orderId].image)
-        }
-      );
-      // console.log(myOrderList);
-      renderMyCar(myOrderList);
-    }
-  });
-}
-
-function renderMyCar(arr) {
+function renderMyCart(arr) {
   // Borrar lo que ya esta renderizado y renderizar otra vez.
   const myOrderContent = document.querySelector('.my-order-content');
   myOrderContent.innerHTML = "";
@@ -225,4 +230,58 @@ function renderMyCar(arr) {
   return;
 }
 
-addProductToCart();
+function addToCartList(id) {
+  myOrderList.push(
+    {
+      name: String(productList[id].name),
+      price: String(productList[id].price),
+      image: String(productList[id].image)
+    });
+    return;
+}
+
+function setProductDetail(id) {
+  detailOfProduct.name = String(productList[id].name);
+  detailOfProduct.price = String(productList[id].price);
+  detailOfProduct.image = String(productList[id].image);
+  detailOfProduct.info = String(productList[id].info)
+  return;
+}
+
+function renderProductDetail() {
+  const productDetailCont = document.querySelector('.product-detail-content');
+  productDetailCont.innerHTML = "";
+  const div = document.createRange().createContextualFragment(
+    /* HTML */
+    `
+    <img src=" ${detailOfProduct.image} " alt="">
+      <div class="product-info">
+        <p>${detailOfProduct.name}</p> 
+        <p>$${detailOfProduct.price}</p> 
+        <p>${detailOfProduct.info}</p>
+        <button class="primary-button add-to-cart-button">
+          <img src="./icons/bt_add_to_cart.svg" alt="add to cart">
+          Add to cart
+        </button>
+      </div>
+    `
+  );
+  productDetailCont.append(div);
+}
+
+cardsContainer.addEventListener('click',(e) => {
+  let orderId = parseInt(e.target.getAttribute('id'));
+  if(e.target && e.target.classList.contains('addToCart')){
+
+    // console.log(String(e.target.getAttribute('id')));
+    addToCartList(orderId);
+    // console.log(myOrderList);
+    renderMyCart(myOrderList);
+
+  } else if(e.target && e.target.classList.contains('card-img')){
+
+    setProductDetail(orderId);
+    renderProductDetail();
+    openProductDetailAside();
+  }
+});
