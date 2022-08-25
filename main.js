a1f837240570a855f792826aad1e878ebb5adaf8
Drend <byronthyto@gmail.com>
2022-08-24 20:04:26 -0500

    Se agrega menu de pantallas de mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuMail = document.querySelector(".navbar-email");
 const DesktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 menuMail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(){
     DesktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
 
 
