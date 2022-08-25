5bcf431e25a50a7f012432073a160c3d6e0ed133
Augusto ROjas <augurojascreativo@gmail.com>
2022-08-24 20:00:11 -0700

    fusion del menu mobile

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleMenuDesktop);
+menuHamIcon.addEventListener('click', toggleMenuMobile);
 
 function toggleMenuDesktop() {
     menuDesktop.classList.toggle('inactive');
+}
+
+function toggleMenuMobile() {
+    mobileMenu.classList.toggle('inactive');
 }
