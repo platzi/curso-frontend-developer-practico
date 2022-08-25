e743dbbe8d668ba719c47699fb4716e8feae0e16
danielcardvilla <danielcardvillarroel@gmail.com>
2022-08-25 16:06:21 -0500

    la clase 21 fue horrenda, hasta este punto no hay bugs

--- script.js
+++ script.js
@@ -4,28 +4,25 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart"); //clase
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
-menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() { // no eliminar esto funciona
     const isAsideClosed = aside.classList.contains('inactive');
-
     
     if (!isAsideClosed) {
         aside.classList.add('inactive');    
     }
-   
-    desktopMenu.classList.toggle('inactive');
 
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){ // no eliminar esto funciona 
     const isAsideClosed = aside.classList.contains('inactive');
 
-    
     if (!isAsideClosed) {
         aside.classList.add('inactive');    
     }
@@ -33,15 +30,82 @@ function toggleMobileMenu(){ // no eliminar esto funciona
     mobileMenu.classList.toggle('inactive');
 
 }
-
 function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     
 if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');    
 }
-
     aside.classList.toggle('inactive');
 }
 
+    const productList = [];
+    productList.push({
+        name: 'bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+
+    });
+   
+    productList.push({
+        name: 'teclado',
+        price: 94,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        
+    });
     
+    productList.push({
+        name: 'Mouse',
+        price: 99,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        
+    }); 
+
+    productList.push({
+        name: 'poco x3',
+        price: 300,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        
+    }); 
+
+    for (product of productList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+         //product= {name, price, image} --> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image); 
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+       
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');0
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+
+    }
+
+ 
+      
+   
