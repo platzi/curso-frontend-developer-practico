322a9b3cff72eaa31e3ff148bd3759a0e1683d79
Jorge Sevilla <jorge.garcia@compasso.com.br>
2022-08-25 21:25:34 -0300

    Carrito de compras, hacr aparecer y desaparecer menus

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-const navMail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const menuMobileIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const navMail = document.querySelector(".navbar-email");
+const menuMobileIcon = document.querySelector(".menu");
+const menuMobile = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
+const aside = document.querySelector(".product-detail");
 
-navMail.addEventListener('click', toggleDesktopMenu);
-menuMobileIcon.addEventListener('click', toggleMobileMenu);
+navMail.addEventListener("click", toggleDesktopMenu);
+menuMobileIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  desktopMenu.classList.toggle("inactive");
 }
 
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  menuMobile.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = menuMobile.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    menuMobile.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
 }
