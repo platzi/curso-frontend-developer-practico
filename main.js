fab837ece071aa4b62defdd5c4226204d1fcc6b8
Santiago Triana <sgoth27@gmail.com>
2022-08-25 19:05:38 -0500

    ajustes de menus y lista de productos dinamico en js

--- main.js
+++ main.js
@@ -4,5 +4,129 @@ const desktopMenu = document.querySelector(".desktop-menu");
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+
+
     desktopMenu.classList.toggle("inactive");
 };
+
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle("inactive");
+};
+
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive");
+    }
+    //si mobileMenu esta open, hay que cerr
+
+   aside.classList.toggle("inactive");
+
+    
+};
+
+const productList = [];
+productList.push({
+    name: 'bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'pantalla',
+    price:220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Computador',
+    price:620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>
+*/
+
+
+
+
+//ahora se debe insertar los productos en el html. Para ello se debe hacer por medio de un ciclo
+const cardsContainer = document.querySelector('.cards-container');
+
+//se encapsula el ciclo en una funcion para poder usarlo con diferentes tipos de arrays por eso se pasa como parametro un array
+//se deje de esta forma para que sea escalable la aplicación
+function renderProducts(array){
+    for (product of array){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+        //otra forma mas corta de agregar nodos es
+        //productInfoDiv.append(productPrice,productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCard);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProducts(productList);
