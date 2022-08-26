69a105579bfcde6bc9059194c0938c018f890bbe
Kevin Garcia <kevingarcore@gmail.com>
2022-08-26 02:47:55 -0500

    the aside detail shows product selected

--- main.js
+++ main.js
@@ -4,15 +4,37 @@ const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
-const productDetailsConteiner = document.querySelector('#productDetail');
-const productDetailClose = document.querySelector('.product-detail-close');
 const cardsContainer = document.querySelector('.cards-container');
+
+const productDetailsContainer = document.querySelector('#productDetail');
+const productClose = document.querySelector('.product-detail-close');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurger.addEventListener('click', toggleMobileMenu);
 menuCartIcon.addEventListener('click', toggleCartAside);
-productDetailClose.addEventListener('click', closeProductDetails);
+productClose.addEventListener('click', closeProductDetails)
+
+const productList = [];
+productList.push({
+  name: 'Bike',
+  price: 150,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  description: 'This is a bike'
+});
+productList.push({
+  name: 'Screen',
+  price: 250,
+  image: 'https://resources.sanborns.com.mx/imagenes-sanborns-ii/1200/8806092249073.jpg?scale=350',
+  description: 'This is a smartTV'
+});
+productList.push({
+  name: 'SmartPhone',
+  price: 350,
+  image: 'https://m.media-amazon.com/images/I/716OmvUFy1L._AC_SS450_.jpg',
+  description: 'This is a SmartPhone'
+});
+
 
 function toggleDesktopMenu () {
   const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -42,23 +64,26 @@ function toggleCartAside () {
   shoppingCartContainer.classList.toggle('inactive');
 }
 
-function openProductDetails () {
-  productDetailsConteiner.classList.remove('inactive');
+function openProductDetails (event) {
+  renderProductAside(event);
+  productDetailsContainer.classList.remove('inactive');
 }
 
 function closeProductDetails () {
-  productDetailsConteiner.classList.add('inactive');
+  productDetailsContainer.classList.add('inactive');
 }
 
 function renderProducts (array) {
-  for (product of array) {
+  array.forEach(product => {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
   
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
     productImg.addEventListener('click', openProductDetails);
-  
+    productImg.addEventListener('click', renderProductAside);
+    productImg.addEventListener('click', (event) => console.log(event));
+    
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
   
@@ -84,28 +109,23 @@ function renderProducts (array) {
     productCard.appendChild(productInfo);
   
     cardsContainer.appendChild(productCard);
-  }
+  });
 };
 
-const productList = [];
-productList.push({
-  name: 'Bike',
-  price: 150,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-  description: 'This is a bike'
-});
-productList.push({
-  name: 'Screen',
-  price: 250,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-  description: 'This is a smartTV'
-});
-productList.push({
-  name: 'SmartPhone',
-  price: 350,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-  description: 'This is a SmartPhone'
-});
+function renderProductAside (event) {
+  const imgFromList = event.path[0].src;
+  const infoAside = event.path[1].childNodes[1];
 
+  const price = infoAside.querySelector('div p:nth-child(1)');
+  const name = infoAside.querySelector('div p:nth-child(2)');
+
+  const imgDetailAside = document.querySelector('#img-detail-aside');
+  imgDetailAside.setAttribute('src', imgFromList);
+
+  const priceAside = document.querySelector('#price-aside');
+  priceAside.innerText = price.textContent;
+  const nameAside = document.querySelector('#name-aside');
+  nameAside.innerText = name.textContent;
+};
 
 renderProducts(productList);
