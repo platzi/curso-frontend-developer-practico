d00e0b381720761863ee1c1333ade1f282f4df32
Diego Hanssel Cabrales <diegohansselp24@gmail.com>
2022-08-26 12:03:29 -0600

    creamos tres archivos nuevos html,css y javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+ const nav_email = document.querySelector(`.navbar-email`);
+const desktopMenu = document.querySelector(`.desktop-menu`);
+
+ nav_email.addEventListener(`click`, toggleDesktopMenu);
+
+ function toggleDesktopMenu(){
+    console.log(`click_desktopMenu`)
+    desktopMenu.classList.toggle(`active`);
+ }
