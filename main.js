80238bf246a09b9af24b9a6c9dc0bf2e421d434f
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-08-26 14:24:52 -0400

    adding shopping cart & burger menu buttons

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector(".navbar-email");
+const burgerMenu = document.querySelector(".menu");
+const iconCardtMenu = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener("click", toggleDesktopMenu);
+burgerMenu.addEventListener("click", toggleMobileMenu);
+iconCardtMenu.addEventListener("click", toggleIconCartMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+    const isIconCardtMenuClosed = aside.classList.contains("inactive");
+
+  if (!isIconCardtMenuClosed) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+  desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+    const isIconCardtMenuClosed = aside.classList.contains("inactive");
+
+  if (!isIconCardtMenuClosed) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleIconCartMenu() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
 }
