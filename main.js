8baaf6ee95950c159e1591eab6008febcc2f749b
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-08-26 19:58:13 -0400

    fixing experience pop-ups menu

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const burgerMenu = document.querySelector(".menu");
 const iconCardtMenu = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -11,20 +11,20 @@ burgerMenu.addEventListener("click", toggleMobileMenu);
 iconCardtMenu.addEventListener("click", toggleIconCartMenu);
 
 function toggleDesktopMenu() {
-    const isIconCardtMenuClosed = aside.classList.contains("inactive");
+    const isIconCardtMenuClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isIconCardtMenuClosed) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
   mobileMenu.classList.toggle("inactive");
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    const isIconCardtMenuClosed = aside.classList.contains("inactive");
+    const isIconCardtMenuClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isIconCardtMenuClosed) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
   mobileMenu.classList.toggle("inactive");
 }
@@ -35,7 +35,7 @@ function toggleIconCartMenu() {
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add("inactive");
   }
-  aside.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
