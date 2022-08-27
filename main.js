93517112598c779b5e63fb21466595b267e30d4d
Alfredo20164920 <apaz20164920@gmail.com>
2022-08-27 13:15:20 -0500

    Create funcionality to close menus and close others

--- main.js
+++ main.js
@@ -6,11 +6,34 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuHamIcon = document.querySelector(".menu");
 
+// Menu carrito toggle
+const menuCarrito = document.querySelector(".navbar-shopping-cart");
+const carrito = document.querySelector(".product-detail");
+
+
 // Event listeners
-menuEmail.addEventListener("click", () => toogleMenu(desktopMenu));
-menuHamIcon.addEventListener("click", () => toogleMenu(mobileMenu));
+menuEmail.addEventListener("click", () => {
+	toogleMenu(desktopMenu, carrito);
+});
+menuHamIcon.addEventListener("click", () => {
+	toogleMenu(mobileMenu, carrito)
+});
+menuCarrito.addEventListener("click", () => {
+	toogleMenu(carrito, [mobileMenu, desktopMenu]);
+});
 
 // functions
-function toogleMenu(element) {
-	element.classList.toggle("inactive");
+function toogleMenu(menuOpen, menusClose) {
+	// Verfiy if the menusClose is an array
+	menusClose = Array.isArray(menusClose) ? menusClose : [menusClose];
+
+	closeMenu(menusClose);
+
+	menuOpen.classList.toggle("inactive");
+}
+
+function closeMenu(elements) {
+	elements.forEach((element) => {
+		element.classList.add("inactive");
+	});
 }
