42955499aef7843fbaa740ce78771677f4e64e65
Jean <Jean0405@outlook.es>
2022-08-26 23:53:14 -0500

    Creacion y configuración menú mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navbar_email = document.querySelector(".navbar-email");
 const desktop_menu = document.querySelector(".desktop-menu");
+const menuMobile = document.querySelector(".menu");
+const mobile_menu = document.querySelector(".mobile-menu");
 
 navbar_email.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
   desktop_menu.classList.toggle("inactive");
+}
+
+menuMobile.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+  mobile_menu.classList.toggle("inactive");
 }
