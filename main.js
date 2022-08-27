7c9221208d1ce350ecf32badcb51e73c4edaeca1
Jean <Jean0405@outlook.es>
2022-08-27 15:13:39 -0500

    creacion de SHOPPING CART DETAIL desplegable

--- main.js
+++ main.js
@@ -2,15 +2,25 @@ const navbar_email = document.querySelector(".navbar-email");
 const desktop_menu = document.querySelector(".desktop-menu");
 const menuMobile = document.querySelector(".menu");
 const mobile_menu = document.querySelector(".mobile-menu");
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const asideCart = document.querySelector(".product-detail");
 
 navbar_email.addEventListener("click", toggleDesktopMenu);
+shoppingCart.addEventListener("click", toggleShoppingAside);
+menuMobile.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktop_menu.classList.toggle("inactive");
+  asideCart.classList.add("inactive");
 }
 
-menuMobile.addEventListener("click", toggleMobileMenu);
-
 function toggleMobileMenu() {
+  asideCart.classList.add("inactive");
   mobile_menu.classList.toggle("inactive");
+}
+
+function toggleShoppingAside() {
+  asideCart.classList.toggle("inactive");
+  mobile_menu.classList.add("inactive");
+  desktop_menu.classList.add("inactive");
 }
