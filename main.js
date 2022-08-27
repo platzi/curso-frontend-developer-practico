b78840c4a938ee810c175a4ac9823128fe60bef0
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-08-26 20:41:07 -0400

    fixing experience pop-ups menu2

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const burgerMenu = document.querySelector(".menu");
 const iconCardtMenu = document.querySelector(".navbar-shopping-cart");
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
+const productDetailContainer = document.querySelector("#productDetail");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenu.addEventListener("click", toggleMobileMenu);
 iconCardtMenu.addEventListener("click", toggleIconCartMenu);
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside);
 
 function toggleDesktopMenu() {
     const isIconCardtMenuClosed = shoppingCartContainer.classList.contains("inactive");
@@ -21,13 +24,16 @@ function toggleDesktopMenu() {
 }
 
 function toggleMobileMenu() {
-    const isIconCardtMenuClosed = shoppingCartContainer.classList.contains("inactive");
+    const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
-  if (!isIconCardtMenuClosed) {
+  if (!isAsideClosed) {
     shoppingCartContainer.classList.add("inactive");
   }
+  closeProductDetailAside();
   mobileMenu.classList.toggle("inactive");
 }
+
+
 
 function toggleIconCartMenu() {
   const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
@@ -35,9 +41,26 @@ function toggleIconCartMenu() {
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add("inactive");
   }
+
+
+  const isProductDetailClosed = productDetailContainer.classList.contains("inactive");
+
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add("inactive");
+  }
   shoppingCartContainer.classList.toggle("inactive");
 }
 
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add("inactive");
+    
+    productDetailContainer.classList.remove('inactive')
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive')
+
+}
 const productList = [];
 productList.push({
     name: 'Bike',
@@ -65,6 +88,7 @@ function renderProducts(arr) {
      
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside)
      
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
