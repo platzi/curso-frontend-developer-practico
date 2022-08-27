09d31ed93abf0860bf6f6d6fa672997962822073
Diego Hanssel Cabrales <diegohansselp24@gmail.com>
2022-08-26 21:51:06 -0600

    restauramos un problema en la parte de aside y carrito de compras

--- script.js
+++ script.js
@@ -3,22 +3,38 @@ const desktopMenu = document.querySelector(`.desktop-menu`);
 const menuHamIcon = document.querySelector(`.menu`);
 const mobileMenu = document.querySelector(`.mobile-menu`);
 const menuCarIcon = document.querySelector(`.navbar-shopping-cart`);
-const asideDetails = document.querySelector(`.product-detail`);
+const aside = document.querySelector(`.product-detail`);
 
 nav_email.addEventListener(`click`, toggleDesktopMenu);
 menuHamIcon.addEventListener(`click`, toggleMobileMenu);
 menuCarIcon.addEventListener(`click`, toggleCarAside);
 
 function toggleDesktopMenu(){
-  console.log(`click_desktopMenu`);
+  const isAsideClosed = aside.classList.contains(`inactive`);
+
+  if (!isAsideClosed) {
+    aside.classList.add(`inactive`);
+  } 
+  
   desktopMenu.classList.toggle(`inactive`);
 }
+
 function toggleMobileMenu(){
-  mobileMenu.classList.toggle(`inactive`);
-}
-function toggleCarAside(){
-  const isMobileMenuClose = mobileMenu.classList.contains(`inactive`);
   const isAsideClose = aside.classList.contains(`inactive`);
 
-  if (mobileMenu.classList.contains(`inactive`));
+  if (!isAsideClose) {
+    aside.classList.add(`inactive`);
+  }
+
+  mobileMenu.classList.toggle(`inactive`);
+}
+
+function toggleCarAside(){
+  const isMobileMenuClose = mobileMenu.classList.contains(`inactive`);
+ 
+  if (!isMobileMenuClose) {
+    mobileMenu.classList.add(`inactive`);
+  }
+
+  aside.classList.toggle(`inactive`);
 }
