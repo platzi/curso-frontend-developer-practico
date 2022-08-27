e5660876b3a1afaf5f0d09a061df03aac05cf9e2
Diego Hanssel Cabrales <diegohansselp24@gmail.com>
2022-08-26 18:08:41 -0600

    le hemos dado mas caracteristicas con javascript a nuestros html y hemos agregado cambios a nuestros estilos

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
- const nav_email = document.querySelector(`.navbar-email`);
+const nav_email = document.querySelector(`.navbar-email`);
 const desktopMenu = document.querySelector(`.desktop-menu`);
+const menuHamIcon = document.querySelector(`.menu`);
+const mobileMenu = document.querySelector(`.mobile-menu`);
+const menuCarIcon = document.querySelector(`.navbar-shopping-cart`);
+const asideDetails = document.querySelector(`.product-detail`);
 
- nav_email.addEventListener(`click`, toggleDesktopMenu);
+nav_email.addEventListener(`click`, toggleDesktopMenu);
+menuHamIcon.addEventListener(`click`, toggleMobileMenu);
+menuCarIcon.addEventListener(`click`, toggleCarAside);
 
- function toggleDesktopMenu(){
-    console.log(`click_desktopMenu`)
-    desktopMenu.classList.toggle(`inactive`);
- }
+function toggleDesktopMenu(){
+  console.log(`click_desktopMenu`);
+  desktopMenu.classList.toggle(`inactive`);
+}
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle(`inactive`);
+}
+function toggleCarAside(){
+  const isMobileMenuClose = mobileMenu.classList.contains(`inactive`);
+  const isAsideClose = aside.classList.contains(`inactive`);
+
+  if (mobileMenu.classList.contains(`inactive`));
+}
