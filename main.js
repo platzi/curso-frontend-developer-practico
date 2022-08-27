fd1c711fcf989611ce6a2cf22f67789fe2750241
Mariano Tourné <marianotourne@gmail.com>
2022-08-27 19:00:22 -0300

    Add interaction between menus

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 const menuBtn = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuProductDetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    menuProductDetail.classList.add('inactive');
 }
 
 
@@ -14,4 +19,13 @@ menuBtn.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+    menuProductDetail.classList.add('inactive');
+}
+
+menuCarritoIcon.addEventListener('click', toggleProductDetail);
+
+function toggleProductDetail(){
+    menuProductDetail.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
 }
