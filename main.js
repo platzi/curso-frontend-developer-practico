1c6a27241433106f20dcf803f5404779d2e1c4cf
Alfredo20164920 <apaz20164920@gmail.com>
2022-08-27 18:39:37 -0500

    Render product detail with specific data

--- main.js
+++ main.js
@@ -1,20 +1,60 @@
-// Desktop menu toggle
+//Class
+class Product {
+	constructor(name, price, img, description) {
+		this.name = name;
+		this.price = price;
+		this.img = img;
+		this.description = description;
+	}
+
+	// Getters
+	getName() {
+		return this.name;
+	}
+
+	getPrice() {
+		return this.price;
+	}
+
+	getImg() {
+		return this.img;
+	}
+
+	getDescription() {
+		return this.description;
+	}
+}
+
+// Variables
+const productList = [];
+
+// Select elements
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-
-// Mobile menu toggle
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuHamIcon = document.querySelector(".menu");
-
-// Menu carrito toggle
 const menuCarrito = document.querySelector(".navbar-shopping-cart");
 const carrito = document.querySelector(".shoppingCardDetail");
+const productDetail = document.querySelector(".product-detail");
+const productDetailClose = document.querySelector(".product-detail-close");
 
 
 // Event listeners
 menuEmail.addEventListener("click", () => toogleMenu(desktopMenu, carrito));
 menuHamIcon.addEventListener("click", () => toogleMenu(mobileMenu, carrito));
 menuCarrito.addEventListener("click", () => toogleMenu(carrito, [mobileMenu, desktopMenu]));
+productDetailClose.addEventListener("click", closeProductDetail);
+
+// Add products
+const numberProducts = 3;
+for(let i = 0; i < numberProducts; i++){
+	addProduct(
+		`Producto ${i+1}`, 
+		Math.floor(Math.random() * 1000), 
+		`https://picsum.photos/500/${Math.floor(Math.random() * (700 - 300) + 300)}`, 
+		`Descripción del producto ${i+1} - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo laboriosam eum officia, aut a doloremque ab!`
+	);
+}
 
 // functions
 function toogleMenu(menuOpen, menusClose) {
@@ -32,43 +72,34 @@ function closeMenu(elements) {
 	});
 }
 
-// Product class
-class Product {
-	constructor(name, price, img) {
-		this.name = name;
-		this.price = price;
-		this.img = img;
-	}
+function closeProductDetail() {
+	const productDetail = document.querySelector(".product-detail");
+	productDetail.classList.add("inactive");
 
-	// Getters
-	getName() {
-		return this.name;
-	}
-
-	getPrice() {
-		return this.price;
-	}
-
-	getImg() {
-		return this.img;
-	}
 }
 
-
-const productList = []
-
-const addProduct = (name, price, img) => {
-	productList.push(new Product(name, price, img));
+function addProduct (name, price, img, description) {
+	productList.push(new Product(name, price, img, description));
 }
 
-// Add random products
-const numberProducts = 3;
-for(let i = 0; i < numberProducts; i++){
-	addProduct(`Producto ${i+1}`, Math.floor(Math.random() * 1000), `https://picsum.photos/200/${Math.floor(Math.random() * (500 - 300) + 300)}`)
+function openDetail(product) {
+	const productDetail = document.querySelector(".product-detail");
+	productDetail.classList.remove("inactive");
+
+	const productImg = document.querySelector("#product_img");
+	const info_name = document.querySelector("#info_name");
+	const info_price = document.querySelector("#info_price");
+	const info_description = document.querySelector("#info_description");
+	
+	productImg.setAttribute("src", product.getImg());
+	info_name.textContent = product.getName();
+	info_price.textContent = `$${product.getPrice()}`;
+	info_description.textContent = product.getDescription();
+	
 }
 
 function renderProducts(list){
-	for(product of list){
+	for(const [i, product] of list.entries()){
 		// Create elements
 		const cards_container = document.querySelector(".cards-container");
 		const productCard = document.createElement("div");
@@ -83,6 +114,10 @@ function renderProducts(list){
 		// Add classes
 		productCard.classList.add("product-card");
 		productInfo.classList.add("product-info");
+
+		// Add event listener
+		productImg.addEventListener("click", () => openDetail(product));
+		productName.addEventListener("click", () => openDetail(product));
 		
 		// Add attributes and content
 		productName.textContent = product.getName();
