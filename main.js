359eb7507fbc678bd1a696375e946241633fef57
Azul Petrocelli <azulpetrocelli81@gmail.com>
2022-08-27 22:01:52 -0300

    Fusión del menú en desktop

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuEmail = document.querySelector('.navbar-email');
+const menuDesktop = document.querySelector('.desktop-menu')
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
+
+function toggleDesktopMenu () {
+  menuDesktop.classList.toggle('inactive');
+}
