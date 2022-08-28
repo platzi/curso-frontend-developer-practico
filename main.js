1904bd252a5b1ae09e9666ba87bcf483dd4cb940
Azul Petrocelli <azulpetrocelli81@gmail.com>
2022-08-27 22:56:11 -0300

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu')
-
+const mobileMenuButton = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+mobileMenuButton.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu () {
   menuDesktop.classList.toggle('inactive');
+}
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive')
 }
