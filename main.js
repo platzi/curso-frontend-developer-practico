1401404d90992d82034de1fe242bf7312f44c9d1
Jean <Jean0405@outlook.es>
2022-08-28 12:33:14 -0500

    Iniciando products cards

--- main.js
+++ main.js
@@ -24,3 +24,45 @@ function toggleShoppingAside() {
   mobile_menu.classList.add("inactive");
   desktop_menu.classList.add("inactive");
 }
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  precio: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Pantalla",
+  precio: 220,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Computador",
+  price: 3320,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+for (product of productList) {
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
+
+  const img = document.createElement("img");
+  img.setAttribute("src", product.image);
+
+  const productInfo = document.createElement("div");
+  productInfo.classList.add("product-info");
+
+  const productInfoDiv = document.createElement("div");
+  const productPrice = document.createElement("p");
+  productPrice.innerText = "$" + product.price;
+
+  const productName = document.createElement("p");
+  productName.innerText = product.name;
+
+  const productInfoFigure = document.createElement("figure");
+  const productImgCart = document.createElement("img");
+  productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+}
