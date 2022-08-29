3b3c66123302c76bc4111889dbcd59d4f9df2aed
GabrielPuig1975 <gabrielfernando075puig@gmail.com>
2022-08-29 09:24:47 -0300

    Commit de lo visto hasta la clase 21

---
+++ main.js
@@ -0,0 +1,123 @@
+const navBarEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.shoppingCart');
+const productDetail = document.querySelector('.product-detail');
+
+navBarEmail.addEventListener('click', () => {
+  desktopMenu.classList.toggle('inactive');
+
+  
+});
+
+burgerMenu.addEventListener('click', () => {
+    
+    const productDetailClosed = productDetail.classList.contains('inactive');
+    
+    if(!productDetailClosed) {
+      productDetail.classList.add('inactive');
+    }
+    
+    mobileMenu.classList.toggle('inactive');
+
+});
+
+shoppingCart.addEventListener('click', () => {
+
+    const isMobileClosedMenu = mobileMenu.classList.contains('inactive');
+    
+    if(!isMobileClosedMenu) {
+    mobileMenu.classList.add('inactive');
+    }
+        productDetail.classList.toggle('inactive');
+})
+    
+const productList = [];
+
+productList.push({
+  name: 'bike',
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  alt: ""
+})
+
+productList.push({
+  name: 'pantalla',
+  price: 320,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  alt: ""
+})
+
+productList.push({
+  name: 'compu',
+  price: 1220,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  alt: ""
+})
+
+productList.push({
+  name: 'bike',
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  alt: ""
+})
+
+productList.push({
+  name: 'pantalla',
+  price: 320,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  alt: ""
+})
+
+productList.push({
+  name: 'compu',
+  price: 1220,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  alt: ""
+})
+
+const cardsContainer = document.querySelector('.cards-container');
+
+function renderProducts(arr) {
+  for(product of productList){
+
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+  
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productFigure = document.createElement('figure');
+    
+    const productFigureImg = document.createElement('img');
+  
+    productFigureImg.setAttribute('src',"./icons/bt_add_to_cart.svg");
+  
+    productFigure.appendChild(productFigureImg);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productList);
