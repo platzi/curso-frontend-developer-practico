e8472da5b95c069b5bcb3b7384a1cac668446cb6
LuanJuis <octojuan@gmail.com>
2022-08-29 12:22:02 -0400

    agregamos products details

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -11,20 +11,20 @@ menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isCarritoAsideClosed = aside.classList.contains("inactive")
+    const isCarritoAsideClosed = shoppingCartContainer.classList.contains("inactive")
 
     if (!isCarritoAsideClosed) {
-        aside.classList.toggle("inactive");
+        shoppingCartContainer.classList.toggle("inactive");
     }
 
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    const isCarritoAsideClosed = aside.classList.contains("inactive");
+    const isCarritoAsideClosed = shoppingCartContainer.classList.contains("inactive");
     
     if (!isCarritoAsideClosed) {
-        aside.classList.toggle("inactive");
+        shoppingCartContainer.classList.toggle("inactive");
     }
 
     mobileMenu.classList.toggle("inactive");
@@ -42,7 +42,7 @@ function toggleCarritoAside() {
         mobileMenu.classList.toggle("inactive");
     }
 
-    aside.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
