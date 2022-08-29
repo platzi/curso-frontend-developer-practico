2b107338e865ef28b816df61f6117900275fe3d2
Daniel Medina <daniel@medinaarmada.com>
2022-08-29 14:53:43 -0400

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleMenuDesktop);
+menuHamIcon.addEventListener('click', toggleMenuMobile);
 
 function toggleMenuDesktop(event){
   desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMenuMobile(event){
+  mobileMenu.classList.toggle('inactive');
+}
+
