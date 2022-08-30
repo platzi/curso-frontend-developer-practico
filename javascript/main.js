6ce629dd9826a4e01f4d9f25347474145eac00ec
CabosManuel <cabos.apolaya@gmail.com>
2022-08-29 19:13:38 -0500

    Move cart to index, styles and click events

--- javascript/main.js
+++ javascript/main.js
@@ -2,14 +2,25 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenuButton = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartButton = document.querySelector('.navbar-shopping-cart');
+const shoppingCart = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 mobileMenuButton.addEventListener('click', toggleMobileMenu);
+shoppingCartButton.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+  shoppingCart.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle('inactive');
+  shoppingCart.classList.add('inactive');
+}
+
+function toggleShoppingCart() {
+  shoppingCart.classList.toggle('inactive');
+  mobileMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
 }
