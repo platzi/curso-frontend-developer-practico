100c056147795a82e18033c5ece799c737d9b212
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-08-30 13:14:08 -0600

    we call the menu class and the mobile-menu class and create a function to activate and deactivate the menu for the mobile

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const NavEmail = document.querySelector(".navbar-email");
 const DesktopMenu = document.querySelector(".desktop-menu");
+const BurgerStyleMenuBar = document.querySelector(".menu");
+const MobileMenu = document.querySelector(".mobile-menu");
 
 
 NavEmail.addEventListener("click" , exchangeDesktopMenu);
+BurgerStyleMenuBar.addEventListener("click" , exchangeMobileMenu);
+
 
 function exchangeDesktopMenu (){
     DesktopMenu.classList.toggle("inactive");
-    console.log("Click");
+}
+
+function exchangeMobileMenu (){
+    MobileMenu.classList.toggle("inactive");
 }
