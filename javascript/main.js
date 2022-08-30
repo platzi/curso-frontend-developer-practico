38fcec31e67cc5125299867a43de3d8024aaf436
CabosManuel <cabos.apolaya@gmail.com>
2022-08-30 11:47:04 -0500

    Move product-grid to index, styles and render products

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,4 @@
+// Toggle Menu email, Mobile menu and Shopping cart
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenuButton = document.querySelector('.menu');
@@ -24,3 +25,82 @@ function toggleShoppingCart() {
   mobileMenu.classList.add('inactive');
   desktopMenu.classList.add('inactive');
 }
+
+// Render Cards Products
+function renderProducts(products) {
+  for (const product of products) {
+    const cardsContainer = document.querySelector('.cards-container');
+  
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    
+    const img = document.createElement('img')
+    img.setAttribute('src', product.img);
+    
+    const productPrice = document.createElement('p');
+    const productName = document.createElement('p');
+    productPrice.innerText = `S/. ${product.price}`
+    productName.innerText = product.name;
+    
+    const productInfo = document.createElement('div')
+    productInfo.classList.add('product-info');
+    
+    const priceName = document.createElement('div');
+    priceName.append(productPrice, productName);
+    
+    const btnAddToCart = document.createElement('figure');
+    const imgAddToCart = document.createElement('img');
+    imgAddToCart.setAttribute('src', '../icons/bt_add_to_cart.svg');
+    btnAddToCart.append(imgAddToCart);
+    
+    productInfo.append(priceName, btnAddToCart);
+    productCard.append(img, productInfo);
+    cardsContainer.append(productCard);
+  }
+}
+
+let productList = [];
+productList.push(
+  {
+    name: 'Bike S',
+    price: 400.0,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Bike M',
+    price: 500.0,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Bike L',
+    price: 600.0,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Bike XL',
+    price: 800.0,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Bike S',
+    price: 400.0,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Bike M',
+    price: 500.0,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Bike L',
+    price: 600.0,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Bike XL',
+    price: 800.0,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  }
+);
+
+renderProducts(productList);
