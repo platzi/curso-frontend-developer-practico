57bf6490222e1dfeab355994eea53c79351d5a29
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-08-31 10:33:18 -0600

    I corrected the name of the const aside and inside the functions we create const and ifs for the menus

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const BurgerStyleMenuBar = document.querySelector(".menu");
 const MobileMenu = document.querySelector(".mobile-menu");
 
 const CartMenu = document.querySelector(".navbar-shopping-cart")
-const Asaid = document.querySelector(".product-detail");
+const Aside = document.querySelector(".product-detail");
 
 
 NavEmail.addEventListener("click" , exchangeDesktopMenu);
@@ -14,13 +14,28 @@ CartMenu.addEventListener("click" , exchangeCartAside);
 
 
 function exchangeDesktopMenu (){
+    const IsAsideClosed = Aside.classList.contains ("inactive");
+
+    if (!IsAsideClosed){
+        Aside.classList.add("inactive");
+    }
     DesktopMenu.classList.toggle("inactive");
 }
 
 function exchangeMobileMenu (){
+    const IsAsideClosed = Aside.classList.contains ("inactive");
+
+    if (!IsAsideClosed){
+        Aside.classList.add("inactive");
+    }
     MobileMenu.classList.toggle("inactive");
 }
 
 function exchangeCartAside (){
-    Asaid.classList.toggle("inactive");
+    const IsMobileMenuClosed = MobileMenu.classList.contains ("inactive");
+
+    if (!IsMobileMenuClosed ){
+        MobileMenu.classList.add("inactive")
+    }
+    Aside.classList.toggle("inactive");
 }
