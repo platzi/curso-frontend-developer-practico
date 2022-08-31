6907fc6989dbf54f2ee9e0a7ce9e389739867222
J Aguilar <cntynla@A2>
2022-08-31 13:27:28 -0600

    Maquetacion de sitio web desde JS catalogo de productos

--- <>main.js
+++ <>main.js
@@ -4,6 +4,7 @@ const burguerMenuIco = document.querySelector(".menu");
 const menuMovil = document.querySelector(".mobile-menu");
 const menuCarritoIco = document.querySelector(".navbar-shopping-cart");
 const carritoDetalle = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 /* menu interactivo Email */
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -11,32 +12,113 @@ burguerMenuIco.addEventListener("click", toggleMovilMenu);
 menuCarritoIco.addEventListener("click", toggleCarrito);
 
 function toggleDesktopMenu() {
-    const isCarritoDetalle = carritoDetalle.classList.contains("inactivo");
-    if (!isCarritoDetalle) {
-        carritoDetalle.classList.add("inactivo");
-    }
+  const isCarritoDetalle = carritoDetalle.classList.contains("inactivo");
+  if (!isCarritoDetalle) {
+    carritoDetalle.classList.add("inactivo");
+  }
 
-    console.log("Click");
-    desktopMenu.classList.toggle("inactivo");
+  console.log("Click");
+  desktopMenu.classList.toggle("inactivo");
+}
+/* muestra las imagenes de los productos de manera dinamica y se utilizara hasta ser invocada */
+function renderProducts(arreglo) {
+  for (product of arreglo) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    //product = {nombre, precio,imagen} -> product.image
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerHTML = "$" + product.precio;
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImgCard = document.createElement("img");
+    productImgCard.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCard);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+  }
 }
 
 console.log("js funcionando");
 
 /* menu movil */
 function toggleMovilMenu() {
-    const isCarritoDetalle = carritoDetalle.classList.contains("inactivo");
-    if (!isCarritoDetalle) {
-        carritoDetalle.classList.add("inactivo");
-    }
-    menuMovil.classList.toggle("inactivo");
+  const isCarritoDetalle = carritoDetalle.classList.contains("inactivo");
+  if (!isCarritoDetalle) {
+    carritoDetalle.classList.add("inactivo");
+  }
+  menuMovil.classList.toggle("inactivo");
 }
 
 function toggleCarrito() {
-    const isMenuMovilclosed = menuMovil.classList.contains("inactivo");
-    const isMenuDesktopClosed = desktopMenu.classList.contains("inactivo");
-    if (!isMenuMovilclosed || !isMenuDesktopClosed) {
-        menuMovil.classList.add("inactivo");
-        desktopMenu.classList.add("inactivo");
-    }
-    carritoDetalle.classList.toggle("inactivo");
+  const isMenuMovilclosed = menuMovil.classList.contains("inactivo");
+  const isMenuDesktopClosed = desktopMenu.classList.contains("inactivo");
+  if (!isMenuMovilclosed || !isMenuDesktopClosed) {
+    menuMovil.classList.add("inactivo");
+    desktopMenu.classList.add("inactivo");
+  }
+  carritoDetalle.classList.toggle("inactivo");
 }
+
+const productList = [];
+productList.push({
+  name: "Bici",
+  precio: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Pantalla",
+  precio: 220,
+  image:
+    "https://cdni.russiatoday.com/actualidad/public_images/5b0/5b09719338f07164d4d9781d7edd7f0e_article.jpg",
+});
+productList.push({
+  name: "CPU",
+  precio: 500,
+  image:
+    "https://loopulo.com/wp-content/uploads/2020/10/modding-san-miguel-ordenador-inspirado-fabrica-futurista-cerveza-loopulo-00.jpg",
+});
+productList.push({
+  name: "Silla Gamer",
+  precio: 350,
+  image:
+    "https://dlcdnimgs.asus.com/websites/global/products/lfuhfxz4b9lmxdqm/img/main/bg-07-m.jpg",
+});
+
+/*             <div class="product-card">
+                <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    alt="">
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>Bike</p>
+                    </div>
+                    <figure>
+                        <img src="./icons/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div> */
+
+renderProducts(productList);
--- readme.md
+++ readme.md
@@ -13,3 +13,5 @@
 * se utiliza una funcion con CSS que impide se muestren elementos en pantalla a menos que se cumpla un ancho minimo o maximo.
 * integracion de un menu movil.
 * integracion de carrito de compras con detalle y condicionantes para que no se superpngan otros menus cuande tenemos vista movil.
+* Maquetacion de html desde JS para el autollenado de un catalogo de productos (ejemplo de la vida real). 
+* (Uso de Array, Function y Objeto literal JSON)Pregparando nustra pagina para recibir datos desde una API para el autollenado de nuestra pagina. 
