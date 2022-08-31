679dbd9f6f75fa496ea42f5925faa59a9468104d
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-08-31 08:45:17 -0600

    we order the const and add two queryselector, we create a click event for the cart and a function for the aside

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const NavEmail = document.querySelector(".navbar-email");
 const DesktopMenu = document.querySelector(".desktop-menu");
+
 const BurgerStyleMenuBar = document.querySelector(".menu");
 const MobileMenu = document.querySelector(".mobile-menu");
+
+const CartMenu = document.querySelector(".navbar-shopping-cart")
+const Asaid = document.querySelector(".product-detail");
 
 
 NavEmail.addEventListener("click" , exchangeDesktopMenu);
 BurgerStyleMenuBar.addEventListener("click" , exchangeMobileMenu);
+CartMenu.addEventListener("click" , exchangeCartAside);
 
 
 function exchangeDesktopMenu (){
@@ -14,4 +19,8 @@ function exchangeDesktopMenu (){
 
 function exchangeMobileMenu (){
     MobileMenu.classList.toggle("inactive");
+}
+
+function exchangeCartAside (){
+    Asaid.classList.toggle("inactive");
 }
