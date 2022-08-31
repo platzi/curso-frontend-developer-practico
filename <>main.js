b5f9158e5d104e3b983edf375cccd3d5a7729f05
J Aguilar <cntynla@A2>
2022-08-30 19:44:23 -0600

    integracion de funciones y menu de carrito de compras

--- <>main.js
+++ <>main.js
@@ -1,22 +1,42 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const burguerMenuIco = document.querySelector('.menu');
-const menuMovil = document.querySelector('.mobile-menu');
-
-
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenuIco = document.querySelector(".menu");
+const menuMovil = document.querySelector(".mobile-menu");
+const menuCarritoIco = document.querySelector(".navbar-shopping-cart");
+const carritoDetalle = document.querySelector(".product-detail");
 
 /* menu interactivo Email */
-menuEmail.addEventListener('click',toggleDesktopMenu);
-burguerMenuIco.addEventListener('click',toggleMovilMenu);
+menuEmail.addEventListener("click", toggleDesktopMenu);
+burguerMenuIco.addEventListener("click", toggleMovilMenu);
+menuCarritoIco.addEventListener("click", toggleCarrito);
 
 function toggleDesktopMenu() {
-    console.log('Click');
-    desktopMenu.classList.toggle('inactivo');
+    const isCarritoDetalle = carritoDetalle.classList.contains("inactivo");
+    if (!isCarritoDetalle) {
+        carritoDetalle.classList.add("inactivo");
+    }
+
+    console.log("Click");
+    desktopMenu.classList.toggle("inactivo");
 }
 
-console.log('js funcionando');
+console.log("js funcionando");
 
 /* menu movil */
-function toggleMovilMenu () {
-    menuMovil.classList.toggle('inactivo');
+function toggleMovilMenu() {
+    const isCarritoDetalle = carritoDetalle.classList.contains("inactivo");
+    if (!isCarritoDetalle) {
+        carritoDetalle.classList.add("inactivo");
+    }
+    menuMovil.classList.toggle("inactivo");
+}
+
+function toggleCarrito() {
+    const isMenuMovilclosed = menuMovil.classList.contains("inactivo");
+    const isMenuDesktopClosed = desktopMenu.classList.contains("inactivo");
+    if (!isMenuMovilclosed || !isMenuDesktopClosed) {
+        menuMovil.classList.add("inactivo");
+        desktopMenu.classList.add("inactivo");
+    }
+    carritoDetalle.classList.toggle("inactivo");
 }
--- readme.md
+++ readme.md
@@ -12,3 +12,4 @@
 * una barra de menu interactivo por medio de toggle.
 * se utiliza una funcion con CSS que impide se muestren elementos en pantalla a menos que se cumpla un ancho minimo o maximo.
 * integracion de un menu movil.
+* integracion de carrito de compras con detalle y condicionantes para que no se superpngan otros menus cuande tenemos vista movil.
