618e4f6651fa7d2f76276e0a84885f23f381fd69
jasnled <ja.ledezsa21@gmail.com>
2022-09-01 10:02:42 -0600

    integracion del carrito de compras

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const desktopMenu = document.querySelector('.desktop-menu');
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    if (!aside.classList.contains('inactive')){
+        aside.classList.add('inactive');
+    }
+
 }
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
@@ -11,8 +15,29 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const iconMobileMenu = document.querySelector('.menu')
 
 function toggleMobileMenu() {
-    console.log("click en menu");
+    
     mobileMenu.classList.toggle('inactive');
+    if (!aside.classList.contains('inactive')){
+        aside.classList.add('inactive');
+    }
 }
 
 iconMobileMenu.addEventListener('click', toggleMobileMenu);
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+function toggleProductDetailMenu() {
+    
+    aside.classList.toggle('inactive');
+    if (!mobileMenu.classList.contains('inactive')){
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!desktopMenu.classList.contains('inactive')){
+        desktopMenu.classList.add('inactive');
+    }
+    
+}
+
+menuCarritoIcon.addEventListener('click',toggleProductDetailMenu);
