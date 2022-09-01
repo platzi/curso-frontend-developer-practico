f89d10bb37d4962c426713efc4c56982ddb1f287
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2022-09-01 11:39:12 -0500

    agregue le menu hamburguesa

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navBarEmail = document.querySelector(".navbar-email")
 const desktopMenu= document.querySelector(".desktop-menu")
+const menuHamIcon=document.querySelector(".menu")
+const mobileMenu=document.querySelector(".mobile-menu")
 
 navBarEmail.addEventListener("click",togleMenu)
+menuHamIcon.addEventListener("click",togleMobileMenu)
 
 function togleMenu() {
     desktopMenu.classList.toggle("inactive")
+}
+
+function togleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
 }
