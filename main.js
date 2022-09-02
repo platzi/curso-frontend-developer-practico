bb6793d60ba67d9a537dcdd19d88bb899bee97eb
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2022-09-02 12:27:58 -0500

    Descripcion de producto
    Agregue la descirpcion del producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu= document.querySelector(".desktop-menu")
 const menuHamIcon=document.querySelector(".menu")
 const mobileMenu=document.querySelector(".mobile-menu")
 const iconShoping=document.querySelector(".navbar-shopping-cart")
-const aside = document.querySelector(".product-detail")
+const shoppingCartContainer = document.querySelector("#shopingCartContainer")
 const cardsContainer = document.querySelector('.cards-container')
 
 navBarEmail.addEventListener("click",togleMenu)
@@ -12,20 +12,20 @@ iconShoping.addEventListener("click",togleMenuDetail)
 
 function togleMenu() {
 
-    const isAsideClose=aside.classList.contains("inactive")
+    const isAsideClose=shoppingCartContainer.classList.contains("inactive")
 
     if (!isAsideClose) {
-        aside.classList.add("inactive")
+        shoppingCartContainer.classList.add("inactive")
     }
     
     desktopMenu.classList.toggle("inactive")
 }
 
 function togleMobileMenu() {
-    const isAsideClose=aside.classList.contains("inactive")
+    const isAsideClose=shoppingCartContainer.classList.contains("inactive")
 
     if (!isAsideClose) {
-        aside.classList.add("inactive")
+        shoppingCartContainer.classList.add("inactive")
     }
     mobileMenu.classList.toggle("inactive")
 }
@@ -42,7 +42,7 @@ function togleMenuDetail() {
         desktopMenu.classList.add("inactive")
     }
 
-     aside.classList.toggle("inactive")
+     shoppingCartContainer.classList.toggle("inactive")
 
 
 }
