685aa919aae2e1e420fb5c8451302e78e2f2d2ae
Diego José España Carrillo <diegojespana@gmail.com>
2022-09-01 23:46:44 -0600

    Interaccion entre Todos los Componentes

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarrito = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarrito.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetail)
 
 function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    const isProductDetailAsideClosed = productDetailContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
         shoppingCartContainer.classList.add('inactive');
     }
+    if (!isProductDetailAsideClosed){
+        productDetailContainer.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    const isProductDetailAsideClosed = productDetailContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
         shoppingCartContainer.classList.add('inactive');
+    }
+    if (!isProductDetailAsideClosed){
+        productDetailContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -32,6 +44,7 @@ function toggleMobileMenu() {
 function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetailAsideClosed = productDetailContainer.classList.contains('inactive');
 
     shoppingCartContainer.classList.toggle('inactive');
 
@@ -41,7 +54,32 @@ function toggleCarritoAside() {
     if (!isDesktopMenuClosed){
         desktopMenu.classList.add('inactive');
     }
+    if (!isProductDetailAsideClosed){
+        productDetailContainer.classList.add('inactive');
+    }
 
+}
+
+function openProductDetailAside (){
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        shoppingCartContainer.classList.add('inactive');
+    }
+    if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetail (){
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -60,6 +98,22 @@ productList.push({
     price: 650,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
+productList.push({
+    name: 'bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 300,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: 'Compu',
+    price: 650,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
 
 function renderProducts(arr){
     for(product of arr){
@@ -68,6 +122,7 @@ function renderProducts(arr){
 
         const productImg=document.createElement('img');
         productImg.setAttribute('src',product.image);
+        productImg.addEventListener('click', openProductDetailAside);
 
         const productInfo=document.createElement('div');
         productInfo.classList.add('product-info');
