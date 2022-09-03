3a79b2ab8597a6274b958b9a685aabf2c2ee7fd2
fabztech <fabztech@outlook.com>
2022-09-03 13:58:15 -0500

    Clase 19 finalizada - Fusión del menú mobile

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+// Toggle Menu Desktop
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
 	desktopMenu.classList.toggle("inactive");
+}
+
+// Toggle Menu Mobile
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuHamIcon = document.querySelector(".menu-icon");
+
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+	mobileMenu.classList.toggle("inactive");
 }
