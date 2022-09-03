d989dc44f331abb86b6ac23720ac4a03931fa06e
Noriega402 <d.noriegachajon@gmail.com>
2022-09-02 19:06:27 -0600

    Add interacction with aside of products and mobile-menu

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 let mail = document.getElementById('email');
 let deskMenu = document.getElementById('menu-desktop');
+
 let iconHamburguer = document.getElementById('hamburguer');
 let mobileMenu = document.getElementById('menu-hamburguer');
+
+let shoppingCard = document.getElementById('cart');
+let detailProduct = document.querySelector('.product-detail');
+
 
 mail.addEventListener('click', () =>{
     deskMenu.classList.toggle('inactive');
@@ -10,4 +15,8 @@ mail.addEventListener('click', () =>{
 iconHamburguer.addEventListener('click', () => {
     iconHamburguer.classList.toggle('is-active');
     mobileMenu.classList.toggle('inactive');
+});
+
+shoppingCard.addEventListener('click', () => {
+    detailProduct.classList.toggle('inactive');
 });
