304fe363cfe206ac8a7fff2935e2432a886b79b6
andresflorez0799 <andresflorez0799@gmail.com>
2022-09-02 23:41:44 -0500

    ajuste en js, se agrega funcion cargue detalle item

--- js/index.js
+++ js/index.js
@@ -13,25 +13,29 @@ const productDetailAddToCart = document.querySelector(".add-to-cart-button");
 const inactivarContenedor = (elemento) => elemento.classList.add('inactive');
 const activarContenedor = (elemento) => elemento.classList.remove('inactive');
 const toogleInactivar = (elemento) => elemento.classList.toggle('inactive');
+const containsInactive = (elemento) => elemento.classList.contains('inactive');
 
 //... Eventos
 productDetailContainerClose.addEventListener('click', () => inactivarContenedor(productDetailContainer));
 
 menuEmailIcon.addEventListener('click', () => {
-    if (asideCarrito.classList.contains('inactive')){
-        toogleInactivar(desktopMenu);
+    if (containsInactive(desktopMenu)) {
+        inactivarContenedor(asideCarrito);
+        inactivarContenedor(mobileMenu);
         inactivarContenedor(productDetailContainer);
     }
+    toogleInactivar(desktopMenu);
 });
 menuMobileIcon.addEventListener('click', () => {
-    if (mobileMenu.classList.contains('inactive')) {
+    if (containsInactive(mobileMenu)) {
         inactivarContenedor(asideCarrito);
+        inactivarContenedor(desktopMenu);
         inactivarContenedor(productDetailContainer);
     }
     toogleInactivar(mobileMenu);
 });
 menuCarritoIcon.addEventListener('click', () => {
-    if (asideCarrito.classList.contains('inactive')) {
+    if (containsInactive(asideCarrito)) {
         inactivarContenedor(mobileMenu);
         inactivarContenedor(desktopMenu);
         inactivarContenedor(productDetailContainer);
@@ -40,27 +44,50 @@ menuCarritoIcon.addEventListener('click', () => {
 });
 
 //Load data Dummy api for https://bobs-burgers-api-ui.herokuapp.com/
+//https://bobsburgers-api.herokuapp.com/characters/?limit=20&skip=234
+//https://rickandmortyapi.com/api/character
+//https://rickandmortyapi.com/documentation/#rest
 const GetLoadProducts = (fnPintar) => {
-    fetch("https://bobsburgers-api.herokuapp.com/characters/?limit=20&skip=234")
-    .then(res => res.json()).then(data => {
-        fnPintar(data);
-    });
+    fetch("https://rickandmortyapi.com/api/character")
+        .then(res => res.json()).then(data => {
+            fnPintar(data.results);
+        });
 }
 
+const openDetailContainer = (id, urlDetalle) => {
+    activarContenedor(productDetailContainer);
+    let imagen = document.querySelector('#valorDetailImagen');
+    let precio = document.querySelector('#valorDetailPrecio');
+    let nombre = document.querySelector('#valorDetailNombre');
+    let descripcion = document.querySelector('#valorDetailDescripcion');
+
+    fetch(urlDetalle)
+        .then(res => res.json())
+        .then(data => {
+            imagen.setAttribute('src', data.image);
+            precio.innerHTML = 'Id: ' + id + ', ' + data.species + ' - ' + data.gender;
+            nombre.innerHTML = data.name;
+            descripcion.innerHTML = 'Status: ' + data.status + ', Origin: ' + data.origin.name + ', location: ' + data.location.name;
+        });
+};
+
 const pintarProductos = (datos) => {
-    datos.forEach(element => {        
+    console.log(datos);
+
+    datos.forEach(element => {
         let divRaiz = document.createElement('div');
         divRaiz.classList = 'product-card';
-        
+
         let imagen = document.createElement('img');
         imagen.setAttribute('src', element.image);
-        imagen.addEventListener('click', () => {
-            productDetailContainer.classList.remove('inactive');
+        imagen.addEventListener('click', (event) => {
+            event.preventDefault();
+            openDetailContainer(element.id, element.url);
         });
 
         let divInfo = document.createElement('div');
-        divInfo.classList ='product-info';
-    
+        divInfo.classList = 'product-info';
+
         let divNombre = document.createElement('div');
         divNombre.innerHTML = `<p>${element.id}</p><p>${element.name}</p>`
 
@@ -69,11 +96,24 @@ const pintarProductos = (datos) => {
         img.setAttribute('src', './icons/bt_add_to_cart.svg');
         fig.appendChild(img);
         divInfo.appendChild(divNombre);
-        divInfo.appendChild(fig);    
+        divInfo.appendChild(fig);
         divRaiz.appendChild(imagen);
-        divRaiz.appendChild(divInfo);    
-        document.querySelector('.cards-container').appendChild(divRaiz);     
-    }); 
+        divRaiz.appendChild(divInfo);
+        document.querySelector('.cards-container').appendChild(divRaiz);
+    });
 };
 
 GetLoadProducts(pintarProductos);
+
+//... Deshabilitar scroll
+function inhabilitarScroll() {
+    window.onscroll=function(){
+        window.scrollTo(window.scrollX, window.scrollY);
+    };
+}
+
+//... Habilitar scroll
+function habilitarScroll() {
+    window.onscroll=function(){        
+    };
+}
