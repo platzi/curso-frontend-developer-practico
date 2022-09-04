c34a8d044bdb61d8b3d1e10cdc5d7783115b02b2
Daniel Pinto <apintod@uninorte.edu.co>
2022-09-04 11:43:43 -0500

    carrito de compras

--- main.js
+++ main.js
@@ -1,51 +1,87 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const carritoIcon = document.querySelector('.navbar-shopping-cart');
+const asideCarritoPodructDetail = document.querySelector(".product-detail")
+
 function toggleDesktopMenu() {
+
+    let ismobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    
+    
+    if (ismobileMenuOpen){
+            
+        mobileMenu.classList.add('inactive');
+
+    }
+
+    
     
     desktopMenu.classList.toggle('inactive');
-    console.log(desktopMenu.classList);
 
 }
 
 function toggleMobileMenu() {
+
+    let isasideOpen = !asideCarritoPodructDetail.classList.contains('inactive');
+
+    if (isasideOpen){
+            
+        asideCarritoPodructDetail.classList.add('inactive');
+
+    }
     
     mobileMenu.classList.toggle('inactive');
-    console.log(desktopMenu.classList);
+
+}
+
+function toggleCarritoAside() {
+
+    let ismobileMenuOpen = !mobileMenu.classList.contains('inactive');
+
+    let isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+    
+    
+    if (ismobileMenuOpen){
+            
+        mobileMenu.classList.add('inactive');
+
+    }
+
+    if (isDesktopMenuOpen){
+            
+        desktopMenu.classList.add('inactive');
+
+    }
+    
+    asideCarritoPodructDetail.classList.toggle('inactive');
 
 }
 
 /* function inactiveisactive() {
-    
-    let listclass = desktopMenu.classList;
 
-    const set = new Set(listclass);
-    const indx = ;
-
-    console.log(indx);
-    console.log(type(set));
-    console.log(Array.isArray(set));
-    console.log(set.indexOf('inactive'));
-
-    if(1 === 1){
-
-        desktopMenu.classList.add('inactive')
-
-    }else{
+    if(mobileMenu.classList.contains('inactive')){
 
         menuEmail.addEventListener('click', toggleDesktopMenu);
 
+    }else{
+
+        desktopMenu.classList.add('inactive')
+
     }
 
-    console.log(listclass);
 
 } */
 
 //window.addEventListener('click', inactiveisactive);
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+carritoIcon.addEventListener('click', toggleCarritoAside);
 
 //console.log(desktopMenu.classList);
