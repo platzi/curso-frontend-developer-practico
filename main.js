d98129647085295d687b58bdbf86e3222359ce0c
Alex Mata <alexmatag9314@hotmail.com>
2022-09-03 23:34:01 -0500

    Agregamos los productos a la pagia principal, se modificó el codigo html y js para que se mostraran los productos sin hardcodear la pagina, se preparó codigo js para que se mostraran los productos de una forma mas sencilla de leer y aplicar a futuro si se necesita crecer el sitio.

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container'); 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleMiCarritoAside);
 
+/* Ocultar menu al dar clic en el correo PC */
 function toggleDesktopMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
 
@@ -18,6 +20,7 @@ function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
+/* Ocultar menu al dar clic en el icono hamburguesa*/
 function toggleMobileMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
 
@@ -27,6 +30,7 @@ function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
 }
 
+/* Oculatmos carrito de compras aside */
 function toggleMiCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
@@ -34,5 +38,115 @@ function toggleMiCarritoAside() {
         mobileMenu.classList.add('inactive');
     }
     aside.classList.toggle('inactive');
-
 }
+
+/* Creamos nuestra lista de productos */
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Computadora',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+
+
+
+
+// Segunda opcion para obtener los elementos del arreglo
+// con el in podemos obtener el indice de cada objeto del array
+/*for (product in productList) {
+    console.log(product);
+}*/
+
+function renderProducts(array) {
+    /* Insertamos los productos en el html */
+    //agarramos el array con productList y a cada elemento lo llamamos product
+    for (product of productList) {
+
+        // creamos el contenedor padre general product-card
+        const productCard =  document.createElement('div');
+        // añadimos la clase product-card a la etiqueta div
+        productCard.classList.add('product-card');
+
+        // asignamos la url de la imagen del producto de productList
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        // creamos el div hijo/padre product-info que contendrá precio, nombre e icono/figure de carrito
+        const productInfo =  document.createElement('div');
+        // asignamos la clase product-info al hijo de product-card y padre de los elementos de nombre, precio y carrito
+        productInfo.classList.add('product-info');
+
+        // creamos el div padre que contendrá precio y nombre
+        const productInfoDiv = document.createElement('div');
+
+        // creamos el P que contiene el precio del producto
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+
+        // creamos el P que contendrá el nombre del producto obtenido desde productList
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        // asignamos los valores de precio y nombre dentro del div de InfoDiv creado lineas arriba
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        // creamos el elemento de figura que contendrá el icono de carrito
+        const productInfoFigure = document.createElement('figure');
+        // creamos el elemento de imagen que tendrá el elemento img del carrito 
+        const productImgCart = document.createElement('img');
+        // asignamos la url del carrito de compras
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        // dentro de las etiquetas de figure del carrito de compras le añadimos el icono del carrito ya con la url del icono carrito
+        productInfoFigure.appendChild(productImgCart);
+
+        // dentro de la etiqueta de product-info le añadimos las etiquetas de precio, nombre y el icono del carrito de compras
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        // asignamos las etiquetas de la imagen principal
+        // asignamos las etiquetas de product-info (tiene nombre, precio y carrito)
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        // esta es la etiqueta padre de los productos
+        // añadimos las dos lineas anteriores que contienen ya todos los elemnentos de imagen principal asi como nombre, precio y carrito
+        cardsContainer.appendChild(productCard);
+        
+    }
+}
+
+renderProducts(productList);
+
+
+/* OPCION PARA AÑADIR OBJETOS AL HTML */
+/*
+for (product of productList) {
+    const htmlCards = `<div class="product-card">
+            <img src="${product.image}" alt="">
+            <div class="product-info">
+                <div>
+                    <p>$${product.price}</p>
+                    <p>${product.name}</p>
+                </div>
+                <figure>
+                    <img src="./icons/bt_add_to_cart.svg" alt="">
+                </figure>
+            </div>
+        </div>`
+    const cardsContainer = document.querySelector('.cards-container'); 
+    cardsContainer.innerHTML += htmlCards;
+}
+*/
