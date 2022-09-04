838c0cc568f348656f4e9cf5c7168207389a3920
fabztech <fabztech@outlook.com>
2022-09-04 00:03:45 -0500

    Clase 21 done - Lista de productos HTML a partir de arrays

--- main.js
+++ main.js
@@ -42,3 +42,75 @@ function toggleCarrito() {
 	}
 	asideCartMenu.classList.toggle("inactive");
 }
+
+const productList = [];
+
+productList.push({
+	name: "Bike",
+	price: 120,
+	image:
+		"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+	name: "Pantalla",
+	price: 1200,
+	image:
+		"https://www.lg.com/cac/images/televisores/md07504124/gallery/N01_d1.jpg",
+});
+
+productList.push({
+	name: "Xbox",
+	price: 500,
+	image:
+		"https://exitocol.vtexassets.com/arquivos/ids/7234186/consola-xbox-series-x-1tb-microsoft.jpg?v=637532554326130000",
+});
+
+productList.push({
+	name: "Teclado",
+	price: 100,
+	image:
+		"https://http2.mlstatic.com/D_NQ_NP_745312-MLA46504064560_062021-O.webp",
+});
+
+function renderProducts(arr) {
+	for (product of arr) {
+		const productCard = document.createElement("div");
+		productCard.classList.add("product-card");
+
+		const productImg = document.createElement("img");
+		productImg.setAttribute("src", product.image);
+
+		const productInfo = document.createElement("div");
+		productInfo.classList.add("product-info");
+
+		const productInfoDiv = document.createElement("div");
+
+		const productPrecio = document.createElement("p");
+		productPrecio.innerText = "$ " + product.price;
+
+		const productName = document.createElement("p");
+		productName.innerText = product.name;
+
+		productInfoDiv.appendChild(productPrecio);
+		productInfoDiv.appendChild(productName);
+
+		const productFigure = document.createElement("figure");
+
+		const productCartIcon = document.createElement("img");
+		productCartIcon.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+		productFigure.appendChild(productCartIcon);
+
+		productInfo.appendChild(productInfoDiv);
+		productInfo.appendChild(productFigure);
+
+		productCard.appendChild(productImg);
+		productCard.appendChild(productInfo);
+
+		const cardsContainer = document.querySelector(".cards-container");
+		cardsContainer.appendChild(productCard);
+	}
+}
+
+renderProducts(productList);
