efe1f8928455ac0c35419ec3655f1c43c7f59d08
Camilo Barrios <camilobarriosdev@gmail.com>
2022-09-04 17:11:23 -0500

    Agregamos eventos para el menu-mobile y modificamos un poco los archivos 'fusionadores'.

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 //Seleccionamos la clase de la etiqueta a la que se le va a dar click.
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu =document.querySelector('.desktop-menu');
+const menuIcon =document.querySelector('.menu');
+const mobileMenu =document.querySelector('.mobile-menu');
+
 
 //Le añadimos el evento.
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
 
 //Por defecto está inactiva, con cada click aparece y con otro desaparece.
+
 function toggleDesktopMenu() {
-       desktopMenu.classList.toggle('inactive'); //
+       desktopMenu.classList.toggle('inactive'); //Logica 
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
