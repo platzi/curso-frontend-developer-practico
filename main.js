f2f455a0a2042e9e90c9cf65c577b8032ef0387b
Raul Ochoa <raulochoa3005@gmail.com>
2022-09-04 10:18:54 -0430

    fusión menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuIconMobile = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuIconMobile.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
