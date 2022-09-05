2cf15ce93c05f7757cb387b040f756660b60a0e6
Camilo Barrios <camilobarriosdev@gmail.com>
2022-09-04 19:33:24 -0500

    Se agregaron eventos al carrito de compras, y se solucionaron detalles con los menus.

--- main.js
+++ main.js
@@ -3,18 +3,49 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu =document.querySelector('.desktop-menu');
 const menuIcon =document.querySelector('.menu');
 const mobileMenu =document.querySelector('.mobile-menu');
+const menuCarrito =document.querySelector('.navbar-shopping-cart');
+const productDetail =document.querySelector('.product-detail');
 
 
 //Le añadimos el evento.
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleProductDetail);
 
 //Por defecto está inactiva, con cada click aparece y con otro desaparece.
 
 function toggleDesktopMenu() {
-       desktopMenu.classList.toggle('inactive'); //Logica 
+
+    const isProductDetailClosed = productDetail.classList.contains('inactive');
+  
+    if (!isProductDetailClosed){ 
+        productDetail.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+
+    const isProductDetailClosed = productDetail.classList.contains('inactive');
+  
+    if (!isProductDetailClosed){ //Si está abierto el carrito(en mobile), lo cerramos.
+        productDetail.classList.add('inactive');
+    }
+    //una vez cerrado, activamos nuestro menú mobile.
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetail(){
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed){ //Si está abierto el menú mobile, lo cerramos.
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isDesktopMenuClosed){ //Si está abierto el menú Desktop, lo cerramos.
+        desktopMenu.classList.add('inactive');
+    }
+    //una vez cerrado, activamos nuestro carrito.
+    productDetail.classList.toggle('inactive');
 }
