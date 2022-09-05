10cac13fe37561843b5356d3feb7385d164f1202
Victor Monje <monjev086@gmail.com>
2022-09-05 13:34:03 -0600

    menu mobile

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+//desktop
 const nav_email = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+//mobile
+const menuHam = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+//ordenes de compra
+const navShoping = document.querySelector('.navbar-shopping-cart')
+const producDetail = document.querySelector('.product-detail')
+
 
 nav_email.addEventListener('click', toggleDesktopMenu)
-
+menuHam.addEventListener('click', toogleMobileMenu)
+navShoping.addEventListener('click', toogleCar)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toogleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toogleCar() {
+    producDetail.classList.toggle('inactive')
 }
