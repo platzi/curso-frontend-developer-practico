80936c7011cd368ae030e40ed5f5b073a14dc481
SebastAgui <aguilar1221@gmail.com>
2022-09-05 00:13:06 -0500

    Se realiza el menu mobile

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 // cada que le damos click al email debe aparecer el menu.
 const menuMail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
-    // cada que le demos click se hara la funcion 
+    // vamos a crear el query del menu hamburguesa pra mobile
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+
+// cada que le demos click se hara la funcion 
+
+
 menuMail.addEventListener('click', toggleDEsktopMenu);
 
+// evenlistener del menuHamIcon
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+
 function toggleDEsktopMenu() {
-    // lo que vamos  a hacer es colocarle o quitarle la clase inactive si ya la tiene o no
+    // * lo que vamos  a hacer es colocarle o quitarle la clase inactive si ya la tiene o no
     desktopMenu.classList.toggle('inactive');
+}
+
+
+function toggleMobileMenu() {
+    // lo que vamos  a hacer es colocarle o quitarle la clase inactive si ya la tiene o no
+    mobileMenu.classList.toggle('inactive');
 }
