004637309c975dbc5a79ab941e8afe1cb5df93a9
Ruben Hernandez <rubhernandez42@gmail.com>
2022-09-05 19:39:17 -0400

    we added the product-detail part with all styles, we included the js so it will be displayed properly, modified the previous functions

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+
 const burguerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
-burguerIcon.addEventListener("click", toggleMobileMenu);
+burguerIcon.addEventListener("click", showMobileMenu);
+menuCarritoIcon.addEventListener("click", showMyOrder);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
 }
 
-function toggleMobileMenu() {
+function showMobileMenu() {
+  aside.classList.add("inactive");
   mobileMenu.classList.toggle("inactive");
+}
+
+function showMyOrder() {
+  mobileMenu.classList.add("inactive");
+  aside.classList.toggle("inactive");
 }
