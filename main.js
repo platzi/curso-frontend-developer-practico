20ad27fd16e56c211b5c5991461256b157cb6ef1
andrespuello <andresfpuello16@gmail.com>
2022-09-05 07:48:09 -0400

    Add index.html main.js styles.css

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const linav = document.querySelector(".navbar-email")
 const desmenu = document.querySelector(".desktop-menu")
+const burguer = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 linav.addEventListener('click', desplegable)
+burguer.addEventListener('click', hamburguesa)
+
 
 function desplegable() {
     desmenu.classList.toggle('inactive')
+}
+
+function hamburguesa() {
+    mobileMenu.classList.toggle('burguerI')
 }
