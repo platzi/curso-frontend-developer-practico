3593d10f0c942eeeb119e404643d937954f73ae4
Natalia Doria <ndoriamoreno@gmail.com>
2022-09-06 11:58:43 -0500

    agrega funcionalidad al menu hamburguesa para pantallas mobiles

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let desktopMenu = document.querySelector('.desktop-menu');
 let navbarEmail = document.querySelector('.navbar-email');
+let hamburgerIcon = document.querySelector('.menu');
+let mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleMenu);
 
+hamburgerIcon.addEventListener('click', toggleMobileMenu);
+
 function toggleMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
