2c825e47d04606b9982d53c7da110aceb403ad0b
andrespuello <andresfpuello16@gmail.com>
2022-09-06 14:02:37 -0400

    modificacion hecha

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const linav = document.querySelector(".navbar-email")
 const desmenu = document.querySelector(".desktop-menu")
 const burguer = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const carrito = document.querySelector(".navbar-shopping-cart")
+const listaProductos = document.querySelector(".product-detail")
 
 linav.addEventListener('click', desplegable)
 burguer.addEventListener('click', hamburguesa)
-
+carrito.addEventListener('click', car)
 
 function desplegable() {
     desmenu.classList.toggle('inactive')
@@ -13,4 +15,8 @@ function desplegable() {
 
 function hamburguesa() {
     mobileMenu.classList.toggle('burguerI')
+}
+
+function car() {
+    listaProductos.classList.toggle('inactive')
 }
