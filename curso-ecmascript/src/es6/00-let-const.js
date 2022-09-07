58c94172a4eff802ea29b58853e641e133ae127a
fdcorrea12 <fdcorrea12@hotmail.com>
2022-09-07 18:41:16 -0500

    ecmascript

---
+++ curso-ecmascript/src/es6/00-let-const.js
@@ -0,0 +1,25 @@
+var lastName ='David';
+lastName = 'oscar'
+console.log(lastName)
+
+let fruit = 'apple';
+fruit = 'kiwi'
+console.log(fruit)
+
+const animal = 'Dog';
+animal = 'Cat'
+console.log(animal)
+
+const fruits = () => {
+    if(true){
+        var fruit1 ='apple' //fun
+        let fruit2 ='kiwi'
+        const fruit3 ='banana'
+    }
+
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+
+fruits();
---
+++ curso-ecmascript/src/es6/01-arrow-function.js
@@ -0,0 +1,11 @@
+function square(num){
+    return num * num;
+}
+
+square(4);
+
+const square = (num) =>{
+    return num * num;
+}
+
+const square = num => num * num
