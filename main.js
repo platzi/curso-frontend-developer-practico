8ee67b65837a55c644dce4fc53fc3a39b8581962
Colman <chcolmanpy@gmail.com>
2022-09-07 16:33:09 -0400

    product detail funcional pt 2/2

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ const menuHamIcon = document.querySelector('.menu ');
 const mobileMenu = document.querySelector('.mobile-menu ');
 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+const productDetailCloseIcon=document.querySelector('.product-detail-close');
+
 //const menuCarritoIcon = document.querySelector('.mobile-menu ');
 const shoppingCartContainer= document.querySelector('#shoppingCartContainer');
+const productDetailContainer= document.querySelector('#productDetail');
 
 
 const cardsContainer= document.querySelector('.cards-container');
@@ -14,6 +18,7 @@ const cardsContainer= document.querySelector('.cards-container');
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu(){
     const isAsideClosed=shoppingCartContainer.classList.contains('inactive');
@@ -27,6 +32,7 @@ function toggleMobileMenu(){
     if(!isAsideClosed){
         shoppingCartContainer.classList.add('inactive')
     }
+    closeProductDetailAside();
     mobileMenu.classList.toggle('inactive');
 }
 
@@ -40,6 +46,25 @@ function toggleCarritoAside(){
         desktopMenu.classList.add('inactive');
     }
     shoppingCartContainer.classList.toggle('inactive')
+
+
+
+    const isProductDetailClosed=productDetailContainer.classList.contains('inactive');
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive')
+    }
+}
+
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive');
+   
+   
+    productDetailContainer.classList.remove('inactive');
+    
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
 
 }
 
@@ -68,6 +93,7 @@ function renderProducts(arr){
     
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click',openProductDetailAside);
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
