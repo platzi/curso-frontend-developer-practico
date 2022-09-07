df7fbb217c88197de7c9b016d4cc08dafeed1baf
Jeremias Schreiner <jeremias-ush@hotmail.com>
2022-09-07 15:42:32 -0300

    Agregacion del carrito de compras

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+
 const menuHambugerIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 
+const menuCartIcon = document.querySelector(".navbar-shopping-cart")
+const asideCart = document.querySelector(".product-detail")
 
 menuEmail.addEventListener(
     "click",
     //toggle desktop menu
     ()=>{
         desktopMenu.classList.toggle("inactive")
+        asideCart.classList.add("inactive")
     }
 )
 
@@ -18,5 +22,16 @@ menuHambugerIcon.addEventListener(
      //toggle mobile menu
     ()=>{
         mobileMenu.classList.toggle("inactive")
+        asideCart.classList.add("inactive")
+    }
+)
+
+menuCartIcon.addEventListener(
+    "click",
+    //toggle aside cart
+    ()=>{
+        asideCart.classList.toggle("inactive")
+        desktopMenu.classList.add("inactive")
+        mobileMenu.classList.add("inactive")
     }
 )
