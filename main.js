07935046afca5eef659fffdfa143949040ab8bea
Jeremias Schreiner <jeremias-ush@hotmail.com>
2022-09-07 14:17:02 -0300

    agregacion del menu mobile en index.html

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const menuHambugerIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 
 menuEmail.addEventListener(
@@ -7,5 +9,14 @@ menuEmail.addEventListener(
     //toggle desktop menu
     ()=>{
         desktopMenu.classList.toggle("inactive")
+    }
+)
+
+
+menuHambugerIcon.addEventListener(
+    "click",
+     //toggle mobile menu
+    ()=>{
+        mobileMenu.classList.toggle("inactive")
     }
 )
