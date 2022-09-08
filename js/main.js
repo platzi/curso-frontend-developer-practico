87a977901f8f634fa56a461d8f2fdb0d7b16f9cd
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-09-08 16:41:41 -0500

    Fusión del menú para movil

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const mobileMenuLine = document.querySelector('.mobile-menu ul:nth-child(1)');
 
 const toggleDesktopMenu = () => {
     desktopMenu.classList.toggle('inactive');
 };
 
+const toggleMobileMenu = () => {
+    mobileMenu.classList.toggle('active');
+    mobileMenuLine.classList.toggle('active');
+};
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
