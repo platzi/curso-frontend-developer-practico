d4c8bbdc8e33f28c06cba28ae80fc8fca1b7bbd7
tiagoreez <tiagorpo8@gmail.com>
2022-09-08 18:49:31 -0500

    Implemtacion de renderizado desde js y integracion de paginas

--- main.js
+++ main.js
@@ -1,23 +1,101 @@
+//selectores para el menu de usuario
 const menuEmail = document.querySelector('.navbar-email')
 const menusito = document.querySelector('.desktop-menu')
 
+//selectores para el menu en mobile
 const menuHamburger = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 
+//selectores para el carrito
+const  menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const  menuCarrito = document.querySelector(".product-detail")
 
+// event listeners 
 menuEmail.addEventListener("click", toggleShowMenuDesktop)
 menuHamburger.addEventListener("click",toggleShowMenuMobile)
+menuCarritoIcon.addEventListener("click",toggleShowCarritoMenu)
+
+// funciones toggle
 
 function toggleShowMenuDesktop(){
-
+    menuCarrito.classList.add("inactive")
     menusito.classList.toggle("inactive") 
-    
-
 
  }
 
  function toggleShowMenuMobile(){
-
-    mobileMenu.classList.toggle("inactive-mobile")
+    menuCarrito.classList.add("inactive")
+    mobileMenu.classList.toggle("inactive")
 
  }
+
+ function toggleShowCarritoMenu(){
+    menusito.classList.add("inactive")
+    mobileMenu.classList.add("inactive")
+    menuCarrito.classList.toggle("inactive")
+
+ }
+
+ function renderProducts(arr){
+
+    for(product of arr){
+        const cardContainer = document.querySelector(".cards-container")
+        const productCard = document.createElement('div')
+        productCard.classList.add("product-card")
+    
+        const img = document.createElement('img')
+        img.setAttribute('src',product.image)
+        productCard.appendChild(img)
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add("product-info")
+    
+        const info = document.createElement("div")
+        const pPrice = document.createElement("p")
+        pPrice.innerText = product.price
+        const pName = document.createElement("p")
+        pName.innerText = product.name
+    
+        info.append(pPrice)
+        info.append(pName)
+    
+        productInfo.append(info)
+        
+        const figure = document.createElement("figure")
+        const imgCarrito  = document.createElement("img")
+        imgCarrito.setAttribute("src","./icons/bt_add_to_cart.svg")
+    
+        figure.append(imgCarrito)
+        productInfo.append(figure)
+        
+        productCard.append(productInfo) 
+    
+        cardContainer.append(productCard)
+    
+    }
+
+ }
+
+const productList = []
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+})
+productList.push({
+    name: 'Apple iPhone 14 Pro',
+    price: 1199,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+})
+productList.push({
+    name: 'Football',
+    price: 100,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+})
+
+
+renderProducts(productList)
+
