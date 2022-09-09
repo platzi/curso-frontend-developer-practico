6d6d79cdde1158bd0750d0c8ce6e257be584320a
manumao <manuelmao94@gmail.com>
2022-09-09 17:44:49 -0300

    Agregando el modulo de product detail y reacomodandolo para nuestra pagina

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ const menucarritoIcon = document.querySelector('.navbar-shopping-cart');
 
 const mobileMenu = document.querySelector('.mobile-menu');
 const desktopMenu = document.querySelector('.desktop-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', () => {
-    if (!aside.classList.contains('inactive')) {
-        aside.classList.add('inactive');
+    if (!shoppingCartContainer.classList.contains('inactive')) {
+        shoppingCartContainer.classList.add('inactive');
     }
     desktopMenu.classList.toggle("inactive");
 })
 
 menuHamIcon.addEventListener('click', () => {
-    if (!aside.classList.contains('inactive')) {
-        aside.classList.add('inactive');
+    if (!shoppingCartContainer.classList.contains('inactive')) {
+        shoppingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive')
 })
@@ -30,7 +30,7 @@ menucarritoIcon.addEventListener('click', ()=> {
     if (!desktopMenu.classList.contains('inactive')) {
         desktopMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
 })
 
 const productsList = [];
