a191f438e6483e1c6a1376c6ec3d164f31553999
xiagoz <christ2194.gn@gmail.com>
2022-09-08 21:30:40 -0500

    Fusión del menu mobile

--- src/source/main.js
+++ src/source/main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurguer = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurguer.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(event) {
     event.preventDefault();
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
