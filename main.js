d0f7c8b3e6b76ea0770ac89c64c2fa3ac699ae2b
manumao <manuelmao94@gmail.com>
2022-09-09 13:10:17 -0300

    Integración del menu mobile y shopping cart

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 const navEmail = document.querySelector('.navbar-email');
+
+const menuHamIcon = document.querySelector('#hamIcon');
+const menucarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+const mobileMenu = document.querySelector('.mobile-menu');
 const desktopMenu = document.querySelector('.desktop-menu');
+const aside = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', () => {
+    if (!aside.classList.contains('inactive')) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle("inactive");
 })
+
+menuHamIcon.addEventListener('click', () => {
+    if (!aside.classList.contains('inactive')) {
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive')
+})
+
+menucarritoIcon.addEventListener('click', ()=> {
+    if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive');
+    }
+    if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive')
+})
+
