b0d6e50f7f6693eacf5a0826d7e76e3de89943fb
Paul Francis <paul.fcb55@gmail.com>
2022-09-09 14:55:15 -0500

    añadiendo un aside de productos

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
 
+// addEventListener
 menuEmail.addEventListener('click', toggleDesktopMenu );
 menuHamIcon.addEventListener('click', toggleMobileMenu );
+menuCarritoIcon.addEventListener('click', toggleCarritoAside );
 
+// Fucniones
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside() {
+    const isMobileClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if (!isMobileClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
+
+
 }
