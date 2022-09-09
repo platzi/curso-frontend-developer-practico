079f7180b7dd7c6dd69a923bcd55982164957791
Fabián García Vázquez <fabiangarciav@gmail.com>
2022-09-09 01:36:52 -0500

    Se integraron a las funciones toggle las condicionales para que solo apareciera un menú a la vez (desktop, carrito o mobil)

--- main.js
+++ main.js
@@ -6,17 +6,29 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail'); 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click',toggleMovileMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMovileMenu() {
+function toggleMobileMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside() {
-    aside.classList.toggle('inactive'); 
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    if (!isMobileMenuClose) {
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
