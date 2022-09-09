036d8d5f1f1e7cd42ec076eecd30e07b385f52f9
Fabián García Vázquez <fabiangarciav@gmail.com>
2022-09-09 01:04:07 -0500

    Se integró la funcionalidad para togglear el carrito de compras

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail'); 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMovileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMovileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive'); 
 }
