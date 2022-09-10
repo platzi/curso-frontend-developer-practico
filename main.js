7ee66d4a98a648068b5fe692f29745f61b85b20c
Daniel Sucno <daniksucno363@gmail.com>
2022-09-09 21:01:15 -0300

    Unificando los archivos y agregando nuevo feature

--- main.js
+++ main.js
@@ -1,100 +1,127 @@
-const navEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
-const burguerMenu = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-const menuCartIcon = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
-const cardsContainer = document.querySelector(".cards-container");
+const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const cardsContainer = document.querySelector('.cards-container');
+const productDetailContainer = document.querySelector('#productDetail');
+const btnCloseAsideDetail = document.querySelector('.product-detail-close');
+const darkendBackground = document.querySelector('.darkend');
 
-navEmail.addEventListener("click", toggleDesktopMenu);
-burguerMenu.addEventListener("click", toggleMobileMenu);
-menuCartIcon.addEventListener("click", toggleCartAside);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+btnCloseAsideDetail.addEventListener('click', closeAsideDetails);
 
+function openAsideDetails(){
+    const isShoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isShoppingCartContainerClosed){
+        shoppingCartContainer.classList.add('inactive');
+    }
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    
+    productDetailContainer.classList.remove('inactive');
+    darkendBackground.classList.remove('inactive');
+}
+function closeAsideDetails(){
+    productDetailContainer.classList.add('inactive');
+    darkendBackground.classList.add('inactive');
+}
 function toggleDesktopMenu() {
-    if (!aside.classList.contains("inactive")) {
-        aside.classList.add("inactive");
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    const isDetailAsideClosed = productDetailContainer.classList.contains('inactive');
+   
+    if (!isAsideClosed) {
+        shoppingCartContainer.classList.add('inactive');
     }
-    desktopMenu.classList.toggle("inactive");
-}
-function toggleMobileMenu() {
-    if (!aside.classList.contains("inactive")) {
-        aside.classList.add("inactive");
+    if (!isDetailAsideClosed) {
+        productDetailContainer.classList.add('inactive');
     }
-    mobileMenu.classList.toggle("inactive");
-}
-function toggleCartAside() {
-    if (!mobileMenu.classList.contains("inactive")) {
-        mobileMenu.classList.add("inactive");
-    }
-    if (!desktopMenu.classList.contains("inactive")) {
-        desktopMenu.classList.add("inactive");
-    }
-    aside.classList.toggle("inactive");
-}
-{
-    /* <div class="product-card">
-          <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-          <div class="product-info">
-            <div>
-              <p>$120,00</p>
-              <p>Bike</p>
-            </div>
-            <figure>
-              <img src="./icons/bt_add_to_cart.svg" alt="">
-            </figure>
-          </div>
-        </div> */
-}
-const productList = [];
 
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    const isDetailAsideClosed = productDetailContainer.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        shoppingCartContainer.classList.add('inactive');
+    }
+    if (!isDetailAsideClosed) {
+        productDetailContainer.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDetailAsideClosed = productDetailContainer.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isDetailAsideClosed) {
+        productDetailContainer.classList.add('inactive');
+    }
+
+    shoppingCartContainer.classList.toggle('inactive');
+}
+
+const productList = [];
 productList.push({
-    name: "bike",
+    name: 'Bike',
     price: 120,
-    image:
-        "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push({
-    name: "bike",
-    price: 300,
-    image:
-        "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push({
-    name: "bike",
-    price: 200,
-    image:
-        "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
 function renderProducts(arr) {
     for (product of arr) {
-        const productCard = document.createElement("div");
-        productCard.classList.add("product-card");
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
 
-        const productImg = document.createElement("img");
-        productImg.setAttribute("src", product.image);
+        // product= {name, price, image} -> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openAsideDetails);
 
-        const productInfo = document.createElement("div");
-        productInfo.classList.add("product-info");
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
 
-        const divPrice = document.createElement("div");
+        const productInfoDiv = document.createElement('div');
 
-        const price = document.createElement("p");
-        price.innerText = "$" + product.price;
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
 
-        const name = document.createElement("p");
-        name.innerText = product.name;
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
 
-        const figure = document.createElement("figure");
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
 
-        const imgIcon = document.createElement("img");
-        imgIcon.setAttribute("src", "./icons/bt_add_to_cart.svg");
+        productInfoFigure.appendChild(productImgCart);
 
-        figure.appendChild(imgIcon);
-        divPrice.appendChild(price);
-        divPrice.appendChild(name);
-        productInfo.appendChild(divPrice);
-        productInfo.appendChild(figure);
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
 
         productCard.appendChild(productImg);
         productCard.appendChild(productInfo);
