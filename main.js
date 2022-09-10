85b88c6cd21558621b1408c00e007afb2ac11d10
libian19 <libiansimpson19@gmail.com>
2022-09-09 23:31:10 -0400

    Integracion de componentes de vista principal

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 //ACTIVAR EL MENU DEL EMAIL
 const activarEmail = document.querySelector('.navbar-email');
 const desktopMenu= document.querySelector('.desktop-menu');
-
 //ACTIVAR EL MENU BURGERICON
 const burgerIcon= document.querySelector('.menu');
 const movilMenu= document.querySelector('.mobile-menu');
-
 //ACTIVAR MY ORDER
 const shoppingCart= document.querySelector('.navbar-shopping-cart');
 const myOrder= document.querySelector('.productList-detail');
-
-//LISTA DE PRODUCTOS
+//DETALLE DEL PRODUCTO
+const productImg= document.querySelector('.product-img');
+const productDetail= document.querySelector('.product-detail');
+const productDetailClosed = document.querySelector('.product-detail-closed');
+//CARDS CONTAINER
+const cardsContainer = document.querySelector('.cards-container');
 
 
 activarEmail.addEventListener('click', toggleMenuEmail);
 burgerIcon.addEventListener('click', toggleMenuMobil);
 shoppingCart.addEventListener('click', togglemyOrder);
+//productImg.addEventListener('click', productDetailOpen);
+productDetailClosed.addEventListener('click', closedProductDetail);
+
 
 function toggleMenuEmail(){
    const ismyOrderClosed= myOrder.classList.contains('inactive'); 
@@ -32,6 +37,7 @@ function toggleMenuMobil(){
    if (!ismyOrderClosed){
       myOrder.classList.add('inactive');
    } 
+   productDetail.classList.add('inactive');
    movilMenu.classList.toggle('inactive');
 }
 
@@ -42,6 +48,16 @@ function togglemyOrder(){
       movilMenu.classList.add('inactive');
    } 
    myOrder.classList.toggle('inactive');
+}
+
+function openProductDetail(){
+   myOrder.classList.add('inactive');
+   productDetail.classList.remove('inactive');
+   
+}
+
+function closedProductDetail(){
+   productDetail.classList.add('inactive');
 }
 
 const productList = [];
@@ -76,23 +92,41 @@ productList.push({
    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 })
 
-function products(arr){
+function renderProducts(arr){
    for (product of arr){
-      const htmlCards = `<div class="product-card">
-      <img src= ${product.image} class="product-img">
-      <div class="product-info">
-        <div>
-          <p>$${product.price}</p>
-          <p>${product.name}</p>
-        </div>
-        <figure>
-          <img src="./icons/bt_add_to_cart.svg" alt="">
-        </figure>
-      </div>
-      </div>`
+      
+      const productCard = document.createElement('div');
+      productCard.classList.add('product-card');
 
-      const cardsContainer = document.querySelector('.cards-container');
-      cardsContainer.innerHTML += htmlCards;
+      const productImg = document.createElement('img');
+      productImg.setAttribute('src', product.image);
+      productImg.addEventListener('click', openProductDetail);
+
+      const productInfo = document.createElement('div');
+      productInfo.classList.add('product-info');
+
+      const productInfoDiv = document.createElement('div');
+      const productPrice = document.createElement('p');
+      productPrice.innerText = '$' + product.price;
+      const productName = document.createElement('p');
+      productName.innerText = product.name;
+
+      productInfoDiv.appendChild(productPrice);
+      productInfoDiv.appendChild(productName);
+
+      const productInfoFigure = document.createElement('figure');
+      const productImgCart = document.createElement('img');
+      productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+            
+      productInfoFigure.appendChild(productImgCart);
+      
+      productInfo.appendChild(productInfoDiv);
+      productInfo.appendChild(productInfoFigure);
+
+      productCard.appendChild(productImg);
+      productCard.appendChild(productInfo);
+
+      cardsContainer.appendChild(productCard);
    }
 }
-products(productList);
+renderProducts(productList);
