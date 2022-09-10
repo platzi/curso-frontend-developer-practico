022eaa2defe269f96c41018b8096db8a0f716179
spracso <spracso@gmail.com>
2022-09-09 21:08:19 -0400

    Clase 19 , Agregando el menu para moviles.

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcom = document.querySelector('.menu-ham-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
+menuHamIcom.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     console.log('Click Email');
     desktopMenu.classList.toggle('inactive');
+}   
+
+function toggleMobileMenu() {
+    console.log('Click Mobile Menu');
+    mobileMenu.classList.toggle('inactive');
 }   
 
 console.log('main.js iniciado en HTML')
