f0e84701a055f341d281f0a16249c7dc12c616df
Ruben Hernandez <rubhernandez42@gmail.com>
2022-09-10 21:44:08 -0400

    We created the function renderProducts which displays each of the products, writing HTML in JS

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const mobileMenu = document.querySelector(".mobile-menu");
 
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burguerIcon.addEventListener("click", showMobileMenu);
@@ -29,3 +30,111 @@ function showMyOrder() {
   aside.classList.toggle("inactive");
   desktopMenu.classList.add("inactive");
 }
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Laptop",
+  price: 1200,
+  image:
+    "https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+  name: "Headphones",
+  price: 200,
+  image:
+    "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+  name: "Books",
+  price: 20,
+  image:
+    "https://images.pexels.com/photos/768125/pexels-photo-768125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+  name: "Iphone",
+  price: 750,
+  image:
+    "https://images.pexels.com/photos/1294886/pexels-photo-1294886.jpeg?auto=compress&cs=tinysrgb&w=600",
+});
+
+productList.push({
+  name: "Headphones",
+  price: 200,
+  image:
+    "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+  name: "Watch",
+  price: 350,
+  image:
+    "https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&w=600",
+});
+
+productList.push({
+  name: "Shoes",
+  price: 150,
+  image:
+    "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600",
+});
+
+productList.push({
+  name: "Wallets",
+  price: 180,
+  image:
+    "https://images.pexels.com/photos/4452509/pexels-photo-4452509.jpeg?auto=compress&cs=tinysrgb&w=600",
+});
+
+productList.push({
+  name: "Charger",
+  price: 70,
+  image:
+    "https://images.pexels.com/photos/5208832/pexels-photo-5208832.jpeg?auto=compress&cs=tinysrgb&w=600",
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("article");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    productInfoDiv.append(productPrice, productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImg, productInfo);
+
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productList);
