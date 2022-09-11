193a260bb0deb706ef1ba8dec5e35c19469df163
Gino Llano <ginollano00@gmail.com>
2022-09-11 18:36:16 -0500

    Se agrega el menu para mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('nav > .menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu()
 {
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu()
+{
+    mobileMenu.classList.toggle('inactive');
 }
