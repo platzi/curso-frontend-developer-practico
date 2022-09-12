ca18aa8427b488e5dff84922c09fc6de3524c8bf
uno0uno <anderson.electronico@gmail.com>
2022-09-12 17:15:01 -0500

    cuando la persona le da click en la imagen se abre los detaller del producto y cuando le da click en el boton de cerrar del producto se cierra

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const burguerMenu= document.querySelector('.menu');
 const mobilshopcard= document.querySelector('.navbar-shopping-cart');
+const pDetailclose = document.querySelector('.product-detail-close');
 
 const desktopMenu= document.querySelector('.desktop-menu');
 const mobilMenu= document.querySelector('.mobile-menu');
 const shoppingCartContainer= document.querySelector('#shoppingCartContainer');
 const cardsContainer= document.querySelector('.cards-container');
+const pDetail=document.querySelector('#productDetail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleBurguerMenu);
 mobilshopcard.addEventListener('click', togglemobilshopcard);
+pDetailclose.addEventListener('click', closeProductDetailsAside);
+
 
 function toggleDesktopMenu(){
     const ismobilshopcardactiveClosed = shoppingCartContainer.classList.contains('inactive');
@@ -35,25 +39,12 @@ function togglemobilshopcard(){
     }
     shoppingCartContainer.classList.toggle('inactive');
 }
-
-/*
-<div class="product-card">
-<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-<div class="product-info">
-  <div>
-    <p>$120,00</p>
-    <p>Bike</p>
-  </div>
-  <figure>
-    <img src="./icons/bt_add_to_cart.svg" alt="">
-  </figure>
-</div>
-</div>
-*/
-
-
-/*Aqui es cuando usamos una API REST para hacer consultas 
-de la base de datos*/
+function openProductDetailsAside(){
+    pDetail.classList.remove('inactive');
+}
+function closeProductDetailsAside(){
+    pDetail.classList.add('inactive');
+}
 
 const productList = [];
 
@@ -102,44 +93,68 @@ productList.push({
     price: 500,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
-
-for ( producto of productList){
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card')
-
-    const ProductImg = document.createElement('img');
-    ProductImg.setAttribute('src', producto.image);
-
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info')
-
-    const productInfoDiv = document.createElement('div');
-
-    const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + producto.price;
-    const productName = document.createElement('p');
-    productName.innerText = producto.name;
-
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
-
-    const productInfoFigure = document.createElement('figure');
-    const productImgCard = document.createElement('img');
-    productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
-
-    productInfoFigure.appendChild(productImgCard);
-
-    productInfo.appendChild(productInfoDiv);
-    productInfo.appendChild(productInfoFigure);
-
-    productCard.appendChild(ProductImg);
-    productCard.appendChild(productInfo);
-
-
-    cardsContainer.appendChild(productCard);
-
+function renderProducts(arr){
+    for ( producto of productList){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+    
+        const ProductImg = document.createElement('img');
+        ProductImg.setAttribute('src', producto.image);
+        ProductImg.addEventListener('click', openProductDetailsAside);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info')
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + producto.price;
+        const productName = document.createElement('p');
+        productName.innerText = producto.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCard);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(ProductImg);
+        productCard.appendChild(productInfo);
+    
+    
+        cardsContainer.appendChild(productCard);
+    
+    }
 }
+renderProducts(productList);
+
+
 
 // Aquí procedemos a hacer una renderizacíon de la base de datos
 
+
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>
+*/
+
+
+/*Aqui es cuando usamos una API REST para hacer consultas 
+de la base de datos*/
 
