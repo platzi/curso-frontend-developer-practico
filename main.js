7786dc8c215fc9aba87144b9c6555d07b76d5c5f
Camilo Del Valle Ledesma <camilodelvallel@gmail.com>
2022-09-12 09:17:18 -0500

    funcion toggle en navbar funcionando

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const desktopMenu = document.querySelector('.desktop-menu')
 
 menuEmail.addEventListener('click', toogleDesktopMenu)
 
-function toogleDesktopMenu {
-
+function toogleDesktopMenu() {
+    console.log("click")
+    desktopMenu.classList.toggle('inactive')
 }
+
