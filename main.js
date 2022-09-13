adcd53d4530687a2c7e2406b91293555eefe4ac8
moniehz <monie.developer@gmail.com>
2022-09-12 21:27:34 -0500

    product detail aside fusion

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const BurgerIconMenu  = document.querySelector('.menu');
+const BurgerIconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 BurgerIconMenu.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
 }
 
-function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
+}
+
+function toggleCartAside(){
+    aside.classList.toggle("inactive");
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
 }
