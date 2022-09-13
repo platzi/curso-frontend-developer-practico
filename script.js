a6a7c83f0c135e9eba52bd712ac85837075a89b3
Natalia Doria <ndoriamoreno@gmail.com>
2022-09-13 15:27:42 -0500

    Integracion de la lista de productos de forma dinamica utilizando js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let desktopMenu = document.querySelector('.desktop-menu');
-let navbarEmail = document.querySelector('.navbar-email');
-let hamburgerIcon = document.querySelector('.menu');
-let mobileMenu = document.querySelector('.mobile-menu');
-let carritoIcon = document.querySelector('.navbar-shopping-cart');
-let aside = document.querySelector('.product-detail');
-let closeAsideIcon = document.querySelector('.product-detail-close');
+const desktopMenu = document.querySelector('.desktop-menu');
+const navbarEmail = document.querySelector('.navbar-email');
+const hamburgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const carritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const closeAsideIcon = document.querySelector('.product-detail-close');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 navbarEmail.addEventListener('click', toggleMenu);
@@ -14,7 +15,7 @@ closeAsideIcon.addEventListener('click', toggleAside);
 
 
 function toggleMenu() {
-    let isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
 // Pregunta si el aside se está mostrando en pantalla es decir que no contiene la clase inactive
 // si está activo le hace toggle a la clase para que se agregue la clase inactive y ya no aparezca en pantalla
     if(!isAsideClosed) {
@@ -25,7 +26,7 @@ function toggleMenu() {
 }
 
 function toggleMobileMenu() {
-    let isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
 // Pregunta si el aside se está mostrando en pantalla es decir que no contiene la clase inactive
 // si está activo le hace toggle a la clase para que se agregue la clase inactive y ya no aparezca en pantalla
     if(!isAsideClosed) {
@@ -36,10 +37,81 @@ function toggleMobileMenu() {
 }
 
 function toggleAside() {
-    let isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
     if(!isMobileMenuClosed) {
         mobileMenu.classList.toggle('inactive');
     }
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'MacBook Air',
+    price: 980,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Chair',
+    price: 250,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+
+// <!-- <div class="product-card">
+//         <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+//         <div class="product-info">
+//           <div>
+//             <p>$120,00</p>
+//             <p>Bike</p>
+//           </div>
+//           <figure>
+//             <img src="./icons/bt_add_to_cart.svg" alt="">
+//           </figure>
+//         </div>
+//       </div> -->
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName= document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const figure = document.createElement('figure');
+        const figureImage = document.createElement('img');
+        figureImage.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+        figure.appendChild(figureImage);
+    
+        productInfo.appendChild(figure);
+        productInfo.appendChild(productInfoDiv);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
