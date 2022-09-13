2bad16edb63eb91d570a9c0275cba434a4731eb8
Alvaro Vergara <alvaro.vergara.f@gmail.com>
2022-09-13 03:28:38 -0300

    agregando menus desktop y mobile

--- index.js
+++ index.js
@@ -6,10 +6,37 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+menuCarritoIcon.addEventListener('click', toggleCartMenu);
+
 function toggleDesktopMenu(){
+    const isCartMenuClosed = aside.classList.contains('inactive');
+
+    if(!isCartMenuClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isCartMenuClosed = aside.classList.contains('inactive');
+
+    if(!isCartMenuClosed){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartMenu(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }else if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+        aside.classList.toggle('inactive');
+    
 }
