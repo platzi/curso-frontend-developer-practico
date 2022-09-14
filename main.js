cf373f2c27346895dea874108879c9a397318989
Kevin Castaño <castanno123@gmail.com>
2022-09-14 17:30:32 -0500

    Conectamos el menu mobile con JavaScript

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+//Desktop
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+//Mobile
+const menuBurguer = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+//Desktop
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+//Mobile
+menuBurguer.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
