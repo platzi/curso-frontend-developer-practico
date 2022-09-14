6f2016912d6b37d2d26c7d24d4402247fb01148d
Fabian Gutierrez <gutierrez.snehiderfabian@gmail.com>
2022-09-14 00:37:53 -0500

    organizado el carrito de compras, pero sin arreglaar el conflicto que hay cuando se despliegan

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const menuEmails = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmails.addEventListener('click', toggleDesktopMenu)
+menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
+function toggleCarritoAside() {
+    const isMobileMenuClosed = !mobileMenu.classList.contains('inactive');
+    const isAsideClosed = !aside.classList.contains('inactive');
+    aside.classList.toggle('inactive')
+    if (isAsideClosed) {
+        
+    }else{
+        
+    }
 }
