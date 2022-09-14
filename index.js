1347cf29c84d50d97cd03c981839b83561ca7f4c
Alvaro Vergara <alvaro.vergara.f@gmail.com>
2022-09-14 02:18:50 -0300

    creando productos desde js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 menuCarritoIcon.addEventListener('click', toggleCartMenu);
 
+const cardsContainer = document.querySelector('.cards-container');
+
 function toggleDesktopMenu(){
     const isCartMenuClosed = aside.classList.contains('inactive');
 
@@ -40,3 +42,62 @@ function toggleCartMenu(){
         aside.classList.toggle('inactive');
     
 }
+
+const productList = [];
+
+//hago un push para agregar productos al array a través de un objeto
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""'
+});
+
+productList.push({
+    name: 'Monitor',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""'
+});
+
+productList.push({
+    name: 'Tablet',
+    price: 100,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"'
+});
+
+//for in sirve para mostrar la posición de un elemento en un array, for of para mostrar los elementos de un array
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+        productInfoDiv.append(productPrice, productName)
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.append(productImgCart);
+
+        productInfo.append(productInfoDiv, productInfoFigure);
+
+        productCard.append(productImg, productInfo);
+
+        cardsContainer.append(productCard);
+}
+}
+
+renderProducts(productList);
