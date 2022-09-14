6435647da007b3a18236d08cb4e1f1e8c1da6b0e
Aldo Mejia <aldonear@gmail.com>
2022-09-14 01:49:23 -0500

    Integration of mobile_menu *page 8* to index

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 function toggleDesktopMenu(){
-
     desktopMenu.classList.toggle('inactive');    
 }
 
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
