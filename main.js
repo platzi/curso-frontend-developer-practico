f21d2cd361169efa4c6d1ec4d1d9bed7abe9a530
dublop <dnlopezavila@gmail.com>
2022-09-15 18:00:48 -0400

    Se agrego menu mobile a index.html y style.css,y se agrego un funcion toogle en main.js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toogleDesktopMenu);
+menuHamburger.addEventListener('click', toogleMobilepMenu);
 
 function toogleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobilepMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
