2c4b18d61a92857f779f9859354f8cc361ebf66d
JhordyMora <jhordy.mora@web.de>
2022-09-15 14:57:20 +0200

    Integration of Menus

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const shoppingCartMenu = document.querySelector(".navbar-shopping-cart");
-const productDetailMenu =document.querySelector(".product-detail")
+const productDetailMenu = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 email.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
@@ -61,3 +62,92 @@ function toggleShoppingMenu(){
 
     productDetailMenu.classList.toggle("inactive");
 }
+
+const productList = [];
+productList.push({
+    name: "bike",
+    price: 120,
+    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: "bike2",
+    price: 240,
+    img: "https://img.welt.de/img/wirtschaft/webwelt/mobile238799647/5941624067-ci23x11-w1136/Reduziert-aber-komplett-Ein-Urban-Bike-mit-allem-dran.jpg"
+});
+productList.push({
+    name: "bike3",
+    price: 360,
+    img: "https://bikepacking.com/wp-content/uploads/2022/02/state-4130-all-road-review_1-2000x1334.jpg"
+});
+
+/* estructura que vamos a hacer dentro del for each loop dentro de la funcion
+<div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+        <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+        </div>
+        <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div>
+*/
+function renderProducts(arrProducts){
+    for (product of arrProducts){
+        
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+        // este appendchild mete adentro de otra etiqueta el objeto que se ponga dentro de ella. Tambien se puede usar solo append. La diferencia es
+        // este appendchild mete adentro de otra etiqueta el objeto que se ponga dentro de ella. Tambien se puede usar solo append. La diferencia es
+        // que appendChild solo agrega uno el append puede ser varios. Siempre se debe hacer despues de haber creado el elemento con CreateElement
+        cardsContainer.appendChild(productCard);
+        
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.img);
+        
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+        // que appendChild solo agrega uno el append puede ser varios. Siempre se debe hacer despues de haber creado el elemento con CreateElement
+        /*
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        */
+        productCard.append(productImg, productInfo);
+        
+        const productInfoDiv = document.createElement("div");
+        
+        const productPrice = document.createElement("p");
+        productPrice.innerHTML = "$" + product.price;
+        
+        const productName = document.createElement("p");
+        productName.innerHTML = product.name;
+        // este appendchild mete adentro de otra etiqueta el objeto que se ponga dentro de ella. Tambien se puede usar solo append. La diferencia es
+        // que appendChild solo agrega uno el append puede ser varios. Siempre se debe hacer despues de haber creado el elemento con CreateElement
+        /*
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+        */
+    productInfoDiv.append(productPrice, productName);
+    
+    const productInfoFigure = document.createElement("figure");
+    
+    
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    
+    // este appendchild mete adentro de otra etiqueta el objeto que se ponga dentro de ella. Tambien se puede usar solo append. La diferencia es
+    // que appendChild solo agrega uno el append puede ser varios. Siempre se debe hacer despues de haber creado el elemento con CreateElement
+    productInfoFigure.appendChild(productImgCart);
+    // este appendchild mete adentro de otra etiqueta el objeto que se ponga dentro de ella. Tambien se puede usar solo append. La diferencia es
+    // que appendChild solo agrega uno el append puede ser varios. Siempre se debe hacer despues de haber creado el elemento con CreateElement
+    /*
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+    */
+    productInfo.append(productInfoDiv, productInfoFigure);
+    }
+}
+
+renderProducts(productList);
