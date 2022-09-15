762723769f73802dc67d29097a8a18a3b07592b6
Isaac Aguirre <isaac.edsel@gmail.com>
2022-09-15 17:54:06 -0500

    Agregué lógica en javascript para que los menús se cierren al hacer click en el body, y agregué un overlay negro debajo de los menús abiertos para mejorar el UX

--- main.js
+++ main.js
@@ -5,45 +5,144 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
+const mainContainer = document.querySelector('.main-container');
+const overlayBackground = document.querySelector('#overlay');
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+mainContainer.addEventListener('click', closeMenusClick);
 
 
+
+
+
+
+
+
+function closeMenusClick() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    const isAsideMenuClosed = shoppingCartContainer.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed || !isDesktopMenuClosed || !isAsideMenuClosed )  {
+
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');   
+        shoppingCartContainer.classList.add('inactive');
+        overlayBackground.classList.add('inactive');
+    }
+}
+
+
+
+
+
+
+
+
+// Si shoppingCartContainer abierto, cerrarlo si desktopMenu se abre
 function toggleDesktopMenu() {
     const isAsideMenuClosed = shoppingCartContainer.classList.contains('inactive');
 
     if ( ! isAsideMenuClosed) {
         shoppingCartContainer.classList.add('inactive');
+        overlayBackground.classList.add('inactive');
+
     }
 
     desktopMenu.classList.toggle('inactive');
+    overlayBackground.classList.toggle('inactive');
 }
 
+// Si shoppingCartContainer abierto, cerrarlo si mobileMenu se abre
 function toggleMobileMenu() {
     const isAsideMenuClosed = shoppingCartContainer.classList.contains('inactive');
 
     if ( ! isAsideMenuClosed) {
         shoppingCartContainer.classList.add('inactive');
+        overlayBackground.classList.add('inactive');
+
     }
 
     mobileMenu.classList.toggle('inactive');
+    overlayBackground.classList.toggle('inactive');
+
 }
 
+
+// Si mobileMenu abierto, cerrarlo si shoppingCartContainer se abre
 function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
 
     if ( ! isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
+        overlayBackground.classList.add('inactive');
+    }
+
+    if ( ! isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+        overlayBackground.classList.add('inactive');
+
     }
 
     shoppingCartContainer.classList.toggle('inactive');
+    overlayBackground.classList.toggle('inactive');
+
 }
 
 
+
 const productList = [];
+productList.push({
+    name: 'Bicicleta',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Bicicleta',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Bicicleta',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
 productList.push({
     name: 'Bicicleta',
     price: 120,
