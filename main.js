ad201892c22a6aafb9c1fa9c857ec9a6316813b0
Fabian Gutierrez <gutierrez.snehiderfabian@gmail.com>
2022-09-15 00:44:39 -0500

    se agrego la seccion de product detail pero esta con la clase inactive

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmails.addEventListener('click', toggleDesktopMenu)
@@ -11,17 +11,17 @@ menuHamIcon.addEventListener('click', toggleMobileMenu)
 menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive')
-    if (!isAsideClosed) {
-        aside.classList.add('inactive')
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive')
+    if (!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive')
     }
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive')
-    if (!isAsideClosed) {
-        aside.classList.add('inactive')
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive')
+    if (!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive')
     }
     mobileMenu.classList.toggle('inactive')
 }
@@ -32,24 +32,24 @@ function toggleCarritoAside() {
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive')
     }
-    aside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
 }
 
 
 const productList = [];
 productList.push({
     name: 'Bike',
-    precio: 120,
+    price: 120,
     image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
 productList.push({
     name: 'compu',
-    precio: 1120,
+    price: 1120,
     image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
 productList.push({
     name: 'pantalla',
-    precio: 500,
+    price: 500,
     image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
 
