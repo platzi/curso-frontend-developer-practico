317bbdee761ce1e8b5e9dfaf17f0bd65b0f5b019
jazito <jazito@gmail.com>
2022-09-15 17:17:22 -0500

    Se agrego el menu-nav-mobile al menu y se realizo su interaccion

--- main.js
+++ main.js
@@ -3,9 +3,19 @@
 const menu = document.querySelector(".desktop-menu");
 const navEmail = document.querySelector(".navbar-email");
 
+//menu-var-mobile se añade a navbar
+const mobileMenu = document.querySelector(".mobile-menu");
+const navMobile = document.querySelector(".menu");
+
 navEmail.addEventListener("click", toggleDesktopMenu);
+
+navMobile.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     menu.classList.toggle("inactive");
 
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
