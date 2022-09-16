d73a426aa3a2f094f0d376b39fd020e8f471cbd6
rodripantano <rodripantano1310@gmail.com>
2022-09-16 16:34:52 -0300

    parte 2

--- main.js
+++ main.js
@@ -5,38 +5,51 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCart = document.querySelector('.navbar-shopping-cart');
-const productDetail = document.querySelector('.product-detail');
+const cartDetail = document.querySelector('.cart-detail');
 const cardsContainer = document.querySelector('.cards-container'); // esta variable es para conectar el html que creamos desde JS hacia el mismo html.
+const productDetailContainer = document.querySelector('.product-detail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 
 //se activa un escuchador de eventos, con el cual decimos que al hacer click se inicialice una función(en este caso 'toggleDesktopMenu'). 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
-shoppingCart.addEventListener('click', toggleProductDetail);
+shoppingCart.addEventListener('click', togglecartDetail);
+productDetailCloseIcon.addEventListener('click', closeProductDetail);
 
 //declaramos la función en la cual usamos un classList.toggle que sirve para saca o pone la clase dependiendo el estado que falte. 
 //Y tambien con el classList.contains que devuelve lo contrario de lo que hay para que se cierre cuando abrimos otro y con 
 //classList.add agregamos la clase que le sacamos recien para que vuelva a aparecer.
 
 function toggleDesktopMenu() {
-    const isProductDetailClosed = productDetail.classList.contains('inactive');
-    if (!isProductDetailClosed) {
-        productDetail.classList.add('inactive');
+    const iscartDetailClosed = cartDetail.classList.contains('inactive');
+    if (!iscartDetailClosed) {
+        cartDetail.classList.add('inactive');
+    }
+
+    const isProductDetailContainerClosed = productDetailContainer.classList.contains('inactive');
+    if (!isProductDetailContainerClosed) {
+        productDetailContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isProductDetailClosed = productDetail.classList.contains('inactive');
-    if (!isProductDetailClosed) {
-        productDetail.classList.add('inactive');
+    const iscartDetailClosed = cartDetail.classList.contains('inactive');
+    if (!iscartDetailClosed) {
+        cartDetail.classList.add('inactive');
+    }
+
+    const isProductDetailContainerClosed = productDetailContainer.classList.contains('inactive');
+    if (!isProductDetailContainerClosed) {
+        productDetailContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleProductDetail() {
+function togglecartDetail() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
@@ -47,7 +60,26 @@ function toggleProductDetail() {
         desktopMenu.classList.add('inactive');
     }
 
-    productDetail.classList.toggle('inactive');
+    const isProductDetailContainerClosed = productDetailContainer.classList.contains('inactive');
+    if (!isProductDetailContainerClosed) {
+        productDetailContainer.classList.add('inactive');
+    }
+
+    cartDetail.classList.toggle('inactive');
+}
+
+function openProductDetail() {
+//aca no es necesario hacer toda la logica del if ya que es solo para cuando abrimos el productDitail entonces solo cuando lo abrimos hacemos que se cierre todo el resto.
+//Basicamente es la diferencia que se puede hacer al separar en open y close, que no se puede hacer al usar un toggle.
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    cartDetail.classList.add('inactive');
+
+
+    productDetailContainer.classList.remove('inactive');
+}
+function closeProductDetail(){
+    productDetailContainer.classList.add('inactive');
 }
 
 
@@ -58,7 +90,7 @@ function toggleProductDetail() {
     <img
           src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
           alt="">
-    <div class="product-info">
+    <div class="product-price-name">
         <div>
             <p>$120,00</p>
             <p>Bike</p>
@@ -74,7 +106,7 @@ const productList = [];
 
 productList.push({
     name: 'Bike',
-    price: 120,
+    price: 220,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
@@ -90,6 +122,67 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
+productList.push({
+    name: 'Bike',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Rueda',
+    price: 50,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Cuadro',
+    price: 100,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Bike',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Rueda',
+    price: 50,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Cuadro',
+    price: 100,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Bike',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Rueda',
+    price: 50,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Cuadro',
+    price: 100,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Bike',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Rueda',
+    price: 50,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Cuadro',
+    price: 100,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
 //Se utiliza un for of para crear todas las partes de la card utilizando el document.createElement.
 function renderProducts(arr) {
     for (product of arr) {
@@ -98,9 +191,10 @@ function renderProducts(arr) {
 
         const img = document.createElement('img');
         img.setAttribute('src', product.image);
+        img.addEventListener('click', openProductDetail);
 
         const productInfo = document.createElement('div');
-        productInfo.classList.add('product-info');
+        productInfo.classList.add('product-price-name');
 
         const productInfoDiv = document.createElement('div');
 
