f216c594867d0e33ad77ba3c7d6d5f6215ae2bfe
Pablo Rizzieri <ppablorizzieri@gmail.com>
2022-09-16 02:12:32 -0300

    toggle menus and events

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+const menuCartIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
+menuIcon.addEventListener('click', toggleMobileMenu)
+menuCartIcon.addEventListener('click', toggleCartAside)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+    aside.classList.add('inactive')
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+    aside.classList.add('inactive')
+}
+
+function toggleCartAside() {
+    aside.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+}
+
