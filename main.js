30d3f44e07153c06bcef972251c096ffacc2ddca
dublop <dnlopezavila@gmail.com>
2022-09-16 07:14:53 -0400

    Se agrego el aside de detalles de carrito, estilos y funcion de abrir carrito desde nav y correcciones

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const cartDetail = document.querySelector('.product-detail');
 
 navbarEmail.addEventListener('click', toogleDesktopMenu);
 menuHamburger.addEventListener('click', toogleMobilepMenu);
+cartIcon.addEventListener('click', toogleCartDetail);
 
 function toogleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    cartDetail.classList.add('inactive');
 }
 
 function toogleMobilepMenu(){
     mobileMenu.classList.toggle('inactive');
+    cartDetail.classList.add('inactive');
+}
+
+function toogleCartDetail(){
+    cartDetail.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
 }
