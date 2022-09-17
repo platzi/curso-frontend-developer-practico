e60352349a0d9cf9947e0f5d3b3120e1e17608b1
Kevin Castaño <castanno123@gmail.com>
2022-09-17 01:21:29 -0500

    Incluimos los productos desde un array con Javascript, creando un HTML mas limpio

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container');
+
 //Mobile
 const menuBurguer = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
@@ -21,7 +23,6 @@ function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
-
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleCarritoAside() {
@@ -38,8 +39,6 @@ function toggleCarritoAside() {
     aside.classList.toggle('inactive')
 }
 
-
-
 //Mobile
 menuBurguer.addEventListener('click', toggleMobileMenu);
 
@@ -51,3 +50,88 @@ function toggleMobileMenu() {
     }
     mobileMenu.classList.toggle('inactive')
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: '220',
+    image: 'https://picsum.photos/250/250',
+});
+productList.push({
+    name: 'Computadora',
+    price: '420',
+    image: 'https://picsum.photos/250/251',
+});
+productList.push({
+    name: 'Pantalla',
+    price: '220',
+    image: 'https://picsum.photos/250/249',
+});
+productList.push({
+    name: 'Computadora',
+    price: '420',
+    image: 'https://picsum.photos/250/252',
+});
+productList.push({
+    name: 'Pantalla',
+    price: '220',
+    image: 'https://picsum.photos/251/250',
+});
+productList.push({
+    name: 'Computadora',
+    price: '420',
+    image: 'https://picsum.photos/251/251',
+});
+productList.push({
+    name: 'Pantalla',
+    price: '220',
+    image: 'https://picsum.photos/249/250',
+});
+productList.push({
+    name: 'Computadora',
+    price: '420',
+    image: 'https://picsum.photos/249/249',
+});
+
+function renderProducts(array) {
+    for (product of array) {
+       const productCard = document.createElement('div');
+       productCard.classList.add('product-card');
+    
+       const productImg = document.createElement('img');
+       productImg.setAttribute('src', product.image);
+    
+       const productInfo = document.createElement('div');
+       productInfo.classList.add('product-info');
+       
+       const productInfoDiv = document.createElement('div');
+       
+       const productPrice = document.createElement('p');
+       productPrice.innerText = '$' + product.price;
+    
+       const productName = document.createElement('p');
+       productName.innerText = product.name;
+       
+       const productInfoFigure = document.createElement('figure');
+       
+       const productImgCart = document.createElement('img');
+       productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+       productInfoFigure.appendChild(productImgCart);
+       
+       productInfoDiv.append(productPrice, productName);
+    
+       productInfo.append(productInfoDiv, productInfoFigure);
+    
+       productCard.append(productImg ,productInfo);
+    
+       cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
