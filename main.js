079abb19f065c95175836c1396a0140839926127
Irving mtz <al2173074687@azc.uam.mx>
2022-09-17 01:56:43 -0500

    Fusion del menu en mobile (clase8)

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
+menuHamburger.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu (){
-    console.log("click");
-    //la propiedad toogle:Si es visible se elimina, de lo contrario la añade
    desktopMenu.classList.toggle('inactive');
- 
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
