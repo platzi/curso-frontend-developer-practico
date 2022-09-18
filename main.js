3f85246b5edc04aff8ec2286792b3a839fc5dde2
Archebiz <adriansalazar2497@gmail.com>
2022-09-18 13:47:11 -0500

    Fusion Carrito de Compras

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu=   document.querySelector('.desktop-menu');
 const menuHamIcon=   document.querySelector('.menu');
 const mobileMenu=   document.querySelector('.mobile-menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside=document.querySelector('.product-detail')
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
-function toggleDesktopMenu(){    
-   desktopMenu.classList.toggle('inactive');
+menuCartIcon.addEventListener('click',toggleCartAside);
+
+function toggleDesktopMenu(){       
+    aside.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu(){    
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCartAside(){
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
