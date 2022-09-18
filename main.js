d062f9699de5ef7848c941f8d8f5d84ee8123f93
chrispaz88 <patopazchris@gmail.com>
2022-09-17 19:39:35 -0500

    Adding JS to buttons

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const buttonCorreo = document.querySelector('#navbar_email');
+const btnMobileMenu = document.querySelector('#img_mobile_menu');
+const btnCompra = document.querySelector('#navbar_shopping_cart');
 
 buttonCorreo.addEventListener('click', toggleDesktopMenu);
+btnMobileMenu.addEventListener('click',toggleMobileMenu);
+btnCompra.addEventListener('click',toggleCarrito);
+
+const desktopMenu = document.querySelector('#desktop_menu');
+const mobileMenu = document.querySelector('#mobile_menu');
+const carritoMenu = document.querySelector('#aside_product_detail');
 
 function toggleDesktopMenu(){
-    const desktopMenu = document.querySelector('#desktop_menu');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive'); //Intercambiar clases
+}
+
+function toggleCarrito(){
+    carritoMenu.classList.toggle('inactive');
 }
