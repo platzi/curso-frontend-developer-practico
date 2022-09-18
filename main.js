3153a742bd53300050157a95e3be85544e73ac69
Jaimeandres97 <ja.moreno12@uniandes.edu.co>
2022-09-18 17:40:36 -0500

    Comentarios y correcciones entre la interaccion de menus

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 //abre y cierra el menu del carrito de compras cuando se hace click
 menuCartIcon.addEventListener('click', toggleCartMenu);
 
-
+//Despliega el menu al oprimir el email en el menu desktop
 function toggleDesktopMenu(){
 //Se puede solucionar el cerrar el menu contrario cuando se abre el otro de dos maneras
     //De esta forma verificamos que el menu contrario contiene la clase inactive
@@ -39,7 +39,7 @@ function toggleDesktopMenu(){
 
     desktopMenu.classList.toggle('inactive');
 }
-
+//Despliega el menu hamburguesa de la vista mobile
 function toggleMobileMenu() {
     const isCartIconMenuClosed = cartIconMenu.classList.contains('inactive');
     
@@ -50,27 +50,30 @@ function toggleMobileMenu() {
 
     mobileMenu.classList.toggle('inactive');
 }
-
+//Despliega el menu del carrito de compras
 function toggleCartMenu() {
     //De esta forma activo la clase inactive en los otros dos menus asi se cierran en el momento de hacer click y el menu deseado se abre
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
     productDetailContainer.classList.add('inactive');
-
     cartIconMenu.classList.toggle('inactive');
 }
-
+//Abre el banner de informacion de producto al dar click en la imagen
 function openProductDetailAside() {
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    cartIconMenu.classList.add('inactive');
     productDetailContainer.classList.remove('inactive');
 }
-
+//Cierra el banner de informacion de producto al dar click en la X
 function closeProductDetailAside() {
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
     cartIconMenu.classList.add('inactive');
-    productDetailContainer.classList.add('inactive');
+    productDetailContainer.classList.toggle('inactive');
 }
 
+//Lista de productos
 const productList = [];
 productList.push({
     name: 'Bike', 
@@ -96,27 +99,25 @@ productList.push({
 //Maquetacion de productos en la lista del contenedor principal de la pagina
 function renderProducts(product) {
     for (product of productList) {
+
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
-    
         const img = document.createElement('img');
         img.setAttribute('src', product.image);
+        //Abre el banner de detalle de productos
         img.addEventListener('click', openProductDetailAside)
-    
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
-        
         const productInfoDiv = document.createElement('div');
-    
         const productInfoPrice = document.createElement('p');
         productInfoPrice.innerText = '$' + product.price;
         const productInfoName = document.createElement('p');
         productInfoName.innerText = product.name;
-    
         const productInfoFigure = document.createElement('figure');
         const productInfoFigureImg = document.createElement('img');
         productInfoFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
         
+        //Union de los elementos
         productInfoFigure.append(productInfoFigureImg);
         productInfoDiv.append(productInfoPrice, productInfoName);
         productInfo.append(productInfoDiv, productInfoFigure);
@@ -125,22 +126,20 @@ function renderProducts(product) {
     }
 }
 
+//Maquetacion del banner que se despliega con la informacion del producto al cual se da click
 function renderSpecification(product) {
     for (product of productList) {
         const iconClose = document.createElement('div');
         iconClose.classList.add('product-specific-close');
-
         const iconCloseImg = document.createElement('img');
         iconCloseImg.setAttribute('src', './icons/icon_close.png');
+        //Cierra el banner cuando se le da click a la imagen de X
         iconCloseImg.addEventListener('click', closeProductDetailAside);
-
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
         productImg.setAttribute('alt', 'Product image');
-
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-specific-info');
-
         const productInfoPrice = document.createElement('p');
         productInfoPrice.innerText = '$' + product.price;
         const productInfoName = document.createElement('p');
@@ -151,11 +150,11 @@ function renderSpecification(product) {
         addButton.classList.add('primary-button');
         addButton.classList.add('add-to-cart-button');
         addButton.innerText = 'Add to cart';
-
         const buttonImg = document.createElement('img');
         buttonImg.setAttribute('src', './icons/bt_add_to_cart.svg');
         buttonImg.setAttribute('alt', 'add to cart');
 
+        //Union de los elementos
         productInfo.append(productInfoPrice, productInfoName, productInfoDescription, addButton);
         iconClose.append(iconCloseImg);
         productDetailContainer.append(iconClose, productImg, productInfo);
