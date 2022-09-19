13e116367350a45cc900ba23d04c8623b1109100
Natalia Doria <ndoriamoreno@gmail.com>
2022-09-19 13:05:51 -0500

    Interactividad del product detail aside para que no se superponga con las otras funcionalidades

--- script.js
+++ script.js
@@ -4,43 +4,69 @@ const hamburgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('#shoppingCartContainer');
+const productDetailAside = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const cardsContainer = document.querySelector('.cards-container');
 
 
-navbarEmail.addEventListener('click', toggleMenu);
+navbarEmail.addEventListener('click', toggleDesktopMenu);
 hamburgerIcon.addEventListener('click', toggleMobileMenu);
 carritoIcon.addEventListener('click', toggleAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 
-function toggleMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+function toggleDesktopMenu() {
+    // const isAsideClosed = aside.classList.contains('inactive');
 // Pregunta si el aside se está mostrando en pantalla es decir que no contiene la clase inactive
 // si está activo le hace toggle a la clase para que se agregue la clase inactive y ya no aparezca en pantalla
-    if(!isAsideClosed) {
-        aside.classList.toggle('inactive');
-    }
-// luego de esa verificación si hace toggle del desktop menu para que no quede una cosa sobre otra y se pueda ver bien
+//     if(!isAsideClosed) {
+//         aside.classList.toggle('inactive');
+//     }
+// // luego de esa verificación si hace toggle del desktop menu para que no quede una cosa sobre otra y se pueda ver bien
+    aside.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    productDetailAside.classList.add('inactive');
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
-// Pregunta si el aside se está mostrando en pantalla es decir que no contiene la clase inactive
-// si está activo le hace toggle a la clase para que se agregue la clase inactive y ya no aparezca en pantalla
-    if(!isAsideClosed) {
-        aside.classList.toggle('inactive');
-    }
-// luego de esa verificación si hace toggle del mobile menu para que no quede una cosa sobre otra
+//     const isAsideClosed = aside.classList.contains('inactive');
+// // Pregunta si el aside se está mostrando en pantalla es decir que no contiene la clase inactive
+// // si está activo le hace toggle a la clase para que se agregue la clase inactive y ya no aparezca en pantalla
+//     if(!isAsideClosed) {
+//         aside.classList.toggle('inactive');
+//     }
+// // luego de esa verificación si hace toggle del mobile menu para que no quede una cosa sobre otra
+
+// OTRA FORMA MAS SENCILLA DE HACERLO
+// Agregarle la clase inactive a todo lo que pueda estar abierto
+
+    aside.classList.add('inactive');
+    productDetailAside.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleAside() {
-    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    productDetailAside.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
 
-    if(!isMobileMenuClosed) {
-        mobileMenu.classList.toggle('inactive');
-    }
     aside.classList.toggle('inactive');
+}
+
+function openProductDetailAside() {
+    aside.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+
+    productDetailAside.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailAside.classList.add('inactive');
 }
 
 const productList = [];
@@ -87,36 +113,39 @@ function renderProducts(arr) {
     for (product of arr) {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card')
-    
+
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
-    
+        // Agregamos el add event listener al product img para que al darle click a cualquiera
+        // de las imagenes podamos abrir el product detail aside
+        productImg.addEventListener('click', openProductDetailAside);
+
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
-    
+
         const productInfoDiv = document.createElement('div');
-    
+
         const productPrice = document.createElement('p');
         productPrice.innerText = '$' + product.price;
-    
+
         const productName= document.createElement('p');
         productName.innerText = product.name;
-    
+
         productInfoDiv.appendChild(productPrice);
         productInfoDiv.appendChild(productName);
-    
+
         const figure = document.createElement('figure');
         const figureImage = document.createElement('img');
         figureImage.setAttribute('src', './icons/bt_add_to_cart.svg')
-    
+
         figure.appendChild(figureImage);
-    
+
         productInfo.appendChild(figure);
         productInfo.appendChild(productInfoDiv);
-    
+
         productCard.appendChild(productImg);
         productCard.appendChild(productInfo);
-    
+
         cardsContainer.appendChild(productCard);
     }
 }
