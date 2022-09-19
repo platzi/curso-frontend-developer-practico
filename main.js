d94d3853b062d57f05f49d46ca4091dda4be4e79
Christian Dávila <72448530+CADV92@users.noreply.github.com>
2022-09-19 16:54:54 -0500

    Add Aside (productDetail)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,14 +11,14 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  aside.classList.add('inactive');
+  shoppingCartContainer.classList.add('inactive');
   desktopMenu.classList.toggle('inactive');
 };
 function toggleMobileMenu() {
-  aside.classList.add('inactive');
+  shoppingCartContainer.classList.add('inactive');
   mobileMenu.classList.toggle('inactive');
-  // if (!aside.classList.contains('inactive')) {
-  //   aside.classList.add('inactive')
+  // if (!shoppingCartContainer.classList.contains('inactive')) {
+  //   shoppingCartContainer.classList.add('inactive')
   // }
 };
 function toggleCarritoAside() {
@@ -28,7 +28,7 @@ function toggleCarritoAside() {
   // }
   mobileMenu.classList.add('inactive');
   desktopMenu.classList.add('inactive');
-  aside.classList.toggle('inactive');
+  shoppingCartContainer.classList.toggle('inactive');
 };
 
 const productList = [];
