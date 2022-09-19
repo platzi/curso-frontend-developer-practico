846fc090f88877bd168bd2c8ac2d3cbfefe88b4e
Jose {Andres} Prendiz <jprendiz@gmail.com>
2022-09-18 21:07:21 -0600

    Detalles de un producto

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const carritoCompras = document.querySelector('.navbar-shopping-cart')
 
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-const aside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
@@ -18,13 +18,13 @@ function toggleDesktopMenu() {
     }
     desktopMenu.classList.toggle('inactive')
 }
-
+    
 function toggleMobileMenu() {
 
-    const isCarritoClosed = aside.classList.contains('inactive')
+    const isCarritoClosed = shoppingCartContainer.classList.contains('inactive')
 
     if(!isCarritoClosed) {
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
     mobileMenu.classList.toggle('inactive')
 }
@@ -35,7 +35,7 @@ function toggleAsideCompras() {
     if(!isMobilMenuClosed) {
         mobileMenu.classList.add('inactive')
     }
-    aside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
 }
 
     let productos = []
