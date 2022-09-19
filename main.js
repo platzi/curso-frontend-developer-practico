326ceb49adaf2d55191d71668134a90f78bb6435
Jose {Andres} Prendiz <jprendiz@gmail.com>
2022-09-18 21:45:59 -0600

    Todos los componentes integrados

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email')
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const desktopMenu = document.querySelector('.desktop-menu')
 const carritoCompras = document.querySelector('.navbar-shopping-cart')
 
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
+const productDetailContainer = document.querySelector('#productDetail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
 carritoCompras.addEventListener('click', toggleAsideCompras)
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu() {
     const isMobilMenuClosed = mobileMenu.classList.contains('inactive')
@@ -18,7 +21,7 @@ function toggleDesktopMenu() {
     }
     desktopMenu.classList.toggle('inactive')
 }
-    
+
 function toggleMobileMenu() {
 
     const isCarritoClosed = shoppingCartContainer.classList.contains('inactive')
@@ -26,6 +29,9 @@ function toggleMobileMenu() {
     if(!isCarritoClosed) {
         shoppingCartContainer.classList.add('inactive')
     }
+    
+    closeProductDetailAside()
+
     mobileMenu.classList.toggle('inactive')
 }
 
@@ -35,9 +41,24 @@ function toggleAsideCompras() {
     if(!isMobilMenuClosed) {
         mobileMenu.classList.add('inactive')
     }
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
+
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive')
+    }
+
     shoppingCartContainer.classList.toggle('inactive')
 }
 
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive')
+    productDetailContainer.classList.remove('inactive')
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive')
+}
     let productos = []
 
     productos.push({
@@ -123,59 +144,52 @@ function toggleAsideCompras() {
 
     const cardsContainer = document.querySelector('.cards-container')
 
-    for (const producto of productos) {
+    function renderProduct(arr) {
 
-        // Creamos el contenedor principal
-        const productCard = document.createElement('div')
-        productCard.classList.add('product-card')
-
-        // Creamos la img
-        const imageCard = document.createElement('img')
-        imageCard.setAttribute('src', producto.img)
-
-        // Creamos el div con producto info
-        const productInfo = document.createElement('div')
-        productInfo.classList.add('product-info')
-
-        // Creamos el div que agrupa el precio y el nombre
-        const productDiv = document.createElement('div')
-
-        // Creamos la etiqueta p para el precio y para el nombre
-        const productPrice = document.createElement('p')
-        productPrice.innerText = '$' + producto.price
-
-        const productName = document.createElement('p')
-        productName.innerText = producto.name 
-
-        // Creamos figure
-        const figureIcon = document.createElement('figure')
-
-        // Creamos el icono
-        const iconFigure = document.createElement('img')
-        iconFigure.setAttribute('src', './icons/bt_add_to_cart.svg')
-
-        //Estructuramos el html
-        figureIcon.appendChild(iconFigure)  // Agregamos el icono al figure
-
-        productDiv.append(productPrice, productName)    // Agregamos precio y nombre a div
-
-        productInfo.append(productDiv, figureIcon)  // Agregamos etiquetas e icono a produc-info
-
-        productCard.append(imageCard, productInfo)    // Agregamos la imagen y product-info al contenedor principal
-
-        cardsContainer.appendChild(productCard)  // agregamos al contenedor de html
-
-        /* <div class="product-card">
-        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-        <div class="product-info">
-            <div>
-                <p>$120,00</p>
-                <p>Bike</p>
-            </div>
-            <figure>
-                <img src="./icons/bt_add_to_cart.svg" alt="">
-            </figure>
-        </div>
-    </div> */
+        for (const producto of productos) {
+    
+            // Creamos el contenedor principal
+            const productCard = document.createElement('div')
+            productCard.classList.add('product-card')
+    
+            // Creamos la img
+            const imageCard = document.createElement('img')
+            imageCard.setAttribute('src', producto.img)
+            imageCard.addEventListener('click', openProductDetailAside)
+    
+            // Creamos el div con producto info
+            const productInfo = document.createElement('div')
+            productInfo.classList.add('product-info')
+    
+            // Creamos el div que agrupa el precio y el nombre
+            const productDiv = document.createElement('div')
+    
+            // Creamos la etiqueta p para el precio y para el nombre
+            const productPrice = document.createElement('p')
+            productPrice.innerText = '$' + producto.price
+    
+            const productName = document.createElement('p')
+            productName.innerText = producto.name 
+    
+            // Creamos figure
+            const figureIcon = document.createElement('figure')
+    
+            // Creamos el icono
+            const iconFigure = document.createElement('img')
+            iconFigure.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+            //Estructuramos el html
+            figureIcon.appendChild(iconFigure)  // Agregamos el icono al figure
+    
+            productDiv.append(productPrice, productName)    // Agregamos precio y nombre a div
+    
+            productInfo.append(productDiv, figureIcon)  // Agregamos etiquetas e icono a produc-info
+    
+            productCard.append(imageCard, productInfo)    // Agregamos la imagen y product-info al contenedor principal
+    
+            cardsContainer.appendChild(productCard)  // agregamos al contenedor de html
+        }
 
     }
+
+    renderProduct(productos)
