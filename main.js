6711b430c858b58dd4ac42b2ba9d58997d963a44
Gotenm <mauf182@gmail.com>
2022-09-20 06:32:29 -0500

    integracion del carrito

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-const menuEmail = document.querySelector('.navbar-right');
+const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    console.log('Click');
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!isAsideClosed){
+        aside.classList.add('incative');
+    }
     DesktopMenu.classList.toggle('incative');
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!isAsideClosed){
+        aside.classList.add('incative');
+    }
+
     mobileMenu.classList.toggle('incative');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed){
+            mobileMenu.classList.add('incative');
+        }
+    aside.classList.toggle('incative')
 }
