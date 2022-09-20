77fb4f71f0b29d9347b4f6b8300b95e855a9a473
gimaiter <bbrandgc@gmail.com>
2022-09-19 23:12:19 -0300

    Intentandolo antes de verlo mobile-menu

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamburguesa = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-menuEmail.addEventListener("click", desplegarMenu)
+menuEmail.addEventListener("click", function(){desplegarMenu()});
+menuHamburguesa.addEventListener("click", function(){desplegarMobileMenu()});
 
 function desplegarMenu(){
     desktopMenu.classList.toggle("inactive");
 }
 
+function desplegarMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+}
+
+console.log(menuHamburguesa);
