7ccff1b0afb0706a83f7b09cbbe513206ebbc702
Alexisliao10 <Alexisliao10@hotmail.com>
2022-09-19 20:30:17 -0500

    menu mobile added

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ menuEmail.addEventListener("click", toggleDesktopMeny);
 function toggleDesktopMeny() {
   desktopMenu.classList.toggle("inactive");
 }
+
+// mobile selector
+
+const btnIconMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+btnIconMenu.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+}
