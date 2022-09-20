806cd360ae8aa1d7618b7e1dca8cc07780cd40a4
Alexisliao10 <Alexisliao10@hotmail.com>
2022-09-20 17:04:16 -0500

    product Card added to html using DOM manipulation

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const mobileMenu = document.querySelector(".mobile-menu");
 //Shopping cart
 const shopCartIcon = document.querySelector(".navbar-shopping-cart");
 const returnShopCart = document.querySelector(".title-container");
+//ProductCard
+const cardsContainer = document.querySelector(".cards-container");
 
 //event
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -40,10 +42,86 @@ function toggleMobileMenu() {
 //aside function
 
 function toggleCarritoAside() {
-  if (mobileMenu.classList.contains("inactive")) {
+  if (
+    mobileMenu.classList.contains("inactive") &&
+    desktopMenu.classList.contains("inactive")
+  ) {
     aside.classList.toggle("inactive");
   } else {
     mobileMenu.classList.add("inactive");
-    aside.classList.toggle("inactive");
+    desktopMenu.classList.add("inactive");
+    aside.classList.remove("inactive");
   }
+}
+
+//product list
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Bike2",
+  price: 150,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Bike3",
+  price: 200,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+// <div class="product-card">
+// 				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+// 					alt="">
+// 				<div class="product-info">
+// 					<div>
+// 						<p>$120,00</p>
+// 						<p>Bike</p>
+// 					</div>
+// 					<figure>
+// 						<img src="./icons/bt_add_to_cart.svg" alt="">
+// 					</figure>
+// 				</div>
+// 			</div>
+
+//creando productos del carrito con manipulación del DOM
+for (product of productList) {
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
+
+  const productImg = document.createElement("img");
+  productImg.setAttribute("src", product.image);
+
+  const productInfo = document.createElement("div");
+  productInfo.classList.add("product-info");
+
+  const productInfoDiv = document.createElement("div");
+
+  const productPrice = document.createElement("p");
+  productPrice.innerText = "$" + product.price;
+  const productName = document.createElement("p");
+  productName.innerText = product.name;
+
+  productInfoDiv.appendChild(productPrice);
+  productInfoDiv.appendChild(productName);
+
+  const productInfoFigure = document.createElement("figure");
+  const productInfoFigureImg = document.createElement("img");
+  productInfoFigureImg.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+  productInfoFigure.appendChild(productInfoFigureImg);
+
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard);
 }
