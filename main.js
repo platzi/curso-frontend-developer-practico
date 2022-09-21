e6144e305048340613dfeb19f73eb1896ba3423e
Mariazucena Calderon <mariazucenacr2707@gmail.com>
2022-09-21 14:49:27 -0500

    Se agrego una barra de navegación a la versión movil

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+//Variables
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+//Eventos
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 
+//Funciones
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
