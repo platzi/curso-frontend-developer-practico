ffb2ad0bbf57bf24f0e131380ed4e88ea5552643
FabianRecalde <fabi.recalde.c@gmail.com>
2022-09-21 09:19:10 -0400

    Se agrego el carrito de compras al index y se solucionaron problemas de superposicion de menus a traves de toggle

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
-const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu")
+const menuEmail = document.querySelector(".navbar-email"); //toma el elemento email del archivo html
+const desktopMenu = document.querySelector(".desktop-menu"); //toma el elemento de menu del archivo html
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
+menuEmail.addEventListener("click", toggleDesktopMenu); //agrega un eventListener al elemento email, cuando se haga click va a ocurrir algo
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive"); 
+function toggleDesktopMenu() { //es lo que ocurre cuandose hace click. la funcion toggle
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle("inactive");desktopMenu.classList.toggle("inactive"); 
+}
+
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+    const isAsideClosedAgain = aside.classList.contains("inactive");
+
+    if (!isAsideClosedAgain) {
+        aside.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle("inactive"); 
+}
+
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add("inactive");
+    }
+    
+    aside.classList.toggle("inactive");
 }
