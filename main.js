2647fc285ba2f9a65057807439e3b8853f1305b5
alevasquez <alevasquez@hotmail.com>
2022-09-22 00:17:46 +0200

    Test

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
+
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
