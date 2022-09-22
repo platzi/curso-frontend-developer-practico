b105e61cb1fad28b96a5eac1c038cd0b5a6056b0
Kevin Riquelme <kriquelme10@gmail.com>
2022-09-21 22:21:16 -0300

    Maquetacion desde JS y creacion de array con objetos

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuHamb = document.querySelector('.menu-hamb');
 const mobileMenu= document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 
 
@@ -58,6 +60,97 @@ function toggleMenuMobile () {
 
 
 
+const productList = []; //Array que nos devolveria JavaScript si hicieramos consultas
+                        // a nuestra base de datos.
 
 
+// Cargo el array con los elementos que tiene nuestro objeto
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Computer',
+    price: 420,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Car',
+    price: 5000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Computer',
+    price: 420,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Car',
+    price: 5000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+
+
+
+
+
+
+function renderProducts(arr){
+    //Insertamos el array en nuestro HTML
+
+    for (product of productList) {
+
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card'); 
+
+    
+
+
+    //product = {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+    
+    
+
+    const productInfoDiv = document.createElement('div');
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+   
+
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+    
+
+    cardsContainer.appendChild(productCard);
+}   
+}
+
+renderProducts(productList);
 
