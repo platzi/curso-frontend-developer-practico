44c00db42903ba039eb108dfedea7dfd45bbf6b6
DanielLopez <efrain.loapz@gmail.com>
2022-09-21 19:04:32 -0600

    Agregando componente mobile

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuemail = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu ');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 
 menuemail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
         desktopmenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
