a22f3ecb130023c40c5df17540da8ffe7f5917d5
code-from-scratch1 <alb.mar.cta@gmail.com>
2022-09-23 14:18:40 -0500

    class 20, adding aside products details, solving conflicts to open and close the menues with JS

--- main.js
+++ main.js
@@ -5,19 +5,31 @@ const desktopMenu   = document.querySelector('.desktop-menu');
     /* Menu Mobile */
 const menuLinesLeft = document.querySelector('.menu');
 const mobileMenu    = document.querySelector('.mobile-menu');
+    /* shopping kart */
+const shoppingKart  = document.querySelector('.navbar-shopping-cart')
+const asideProduct  = document.querySelector('.product-detail')
 
 /* Eventos */
     /* Menu Desktop */
 menuEmail.addEventListener('click', toggleDesktopMenu);
+shoppingKart.addEventListener('click', toggleAsideProduct);
     /* Mobile Menu */
 menuLinesLeft.addEventListener('click', toggleMobileMenu);
+
 
 /* Funtions */
     /* Menu Desktop */
 function toggleDesktopMenu(){
+    asideProduct.classList.add('inactive')
     desktopMenu.classList.toggle('inactive');
 }
-    /* Mobile Menu */
+function toggleAsideProduct(){
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    asideProduct.classList.toggle('inactive')
+}
+/* Mobile Menu */
 function toggleMobileMenu(){
+    asideProduct.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
 }
