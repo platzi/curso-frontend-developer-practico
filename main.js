da465eb15eb0dad131ef14876b765644fd22a488
jrealesdelaossa <jrealesdelaossa@gmail.com>
2022-09-23 16:18:40 -0500

    integration all menus

--- main.js
+++ main.js
@@ -1,26 +1,60 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const MENU_CARRITO_ICON = document.querySelector('.navbar-shopping-cart');
+const ASIDE = document.querySelector('.product-detail');
+
+/* Implementación del menú para desktop */
 menuEmail.addEventListener('click', toggleDesktopMenu)
 
 function toggleDesktopMenu() {
     //console.log(desktopMenu.classList.contains('inactive'));
+    let isAsideOpen = !ASIDE.classList.contains('inactive')
 
-    if (desktopMenu.classList.contains('inactive')) {
-        desktopMenu.classList.remove('inactive');
+    if (isAsideOpen) {
+        ASIDE.classList.toggle('inactive');
+        desktopMenu.classList.toggle('inactive');
     } else {
-        desktopMenu.classList.add('inactive');
+        desktopMenu.classList.toggle('inactive');
     }
+
 }
 
 /**
  * Implementación del menu para mobile.
  */
-const menuHamIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+    let isCarMenuOpen = !ASIDE.classList.contains('inactive');
+
+    if (isCarMenuOpen) {
+        ASIDE.classList.toggle('inactive');
+        mobileMenu.classList.toggle('inactive');
+    } else {
+        mobileMenu.classList.toggle('inactive');
+    }
+}
+
+/**
+ * Implementación de detalles de compra - clase 13
+ */
+MENU_CARRITO_ICON.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside() {
+    let isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    let isDesktopMenuOpen = !desktopMenu.classList.contains('inactive')
+
+    if (isMobileMenuOpen) {
+        mobileMenu.classList.toggle('inactive');
+        ASIDE.classList.toggle('inactive');
+    } else if (isDesktopMenuOpen) {
+        desktopMenu.classList.toggle('inactive')
+        ASIDE.classList.toggle('inactive');
+    } else {
+        ASIDE.classList.toggle('inactive');
+    }
 }
