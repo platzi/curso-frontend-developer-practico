f40c94bb42146262c2595cb48f5f8388f81cd748
code-from-scratch1 <alb.mar.cta@gmail.com>
2022-09-23 12:54:37 -0500

    Clase 19, integrar el menu para mobile, corregir medias querries e integrar JS

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+/* Variables */
+    /* Menu Desktop */
+const menuEmail     = document.querySelector('.navbar-email');
+const desktopMenu   = document.querySelector('.desktop-menu');
+    /* Menu Mobile */
+const menuLinesLeft = document.querySelector('.menu');
+const mobileMenu    = document.querySelector('.mobile-menu');
 
+/* Eventos */
+    /* Menu Desktop */
 menuEmail.addEventListener('click', toggleDesktopMenu);
+    /* Mobile Menu */
+menuLinesLeft.addEventListener('click', toggleMobileMenu);
 
+/* Funtions */
+    /* Menu Desktop */
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+    /* Mobile Menu */
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
