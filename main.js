054f2ac88d2d0d68c05dbd508ac1e0cfdf5cc312
DanielLopez <efrain.loapz@gmail.com>
2022-09-23 14:23:44 -0600

    Agregando detalles del producto

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const cardsContainer = document.querySelector('.cards-container');
 
 const carMobileIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 
 
@@ -15,21 +15,21 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 carMobileIcon.addEventListener('click', toggleCarMobileMenu);
 
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
 
     if (!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
         desktopmenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
 
     if (!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -44,7 +44,7 @@ function toggleCarMobileMenu(){
     }else if (!isDesktopMenuClosed){
         desktopmenu.classList.add('inactive');
     }
-        aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
    
 }
 
