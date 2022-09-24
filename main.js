1c749d13aac0daddc34e98142b5f1739e0fae243
joahanjr <joahanramirez7@gmail.com>
2022-09-24 01:27:01 -0500

    implementación de menu mobile y mediaquery

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navemail = document.querySelector(".navbar-email");
 const desktopmenu = document.querySelector(".desktop-menu");
+const menuburger = document.querySelector(".menu");
+const menumobile = document.querySelector(".mobile-menu");
+
 
 navemail.addEventListener("click", toggledesktopmenu);
+menuburger.addEventListener("click", togglemobilemenu);
+
 
 function toggledesktopmenu (){
     desktopmenu.classList.toggle("inactive");
+}
+function togglemobilemenu(){
+    menumobile.classList.toggle("inactive");
 }
