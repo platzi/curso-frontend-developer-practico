a78dc75c479d3a83445ae9b4916778f9b21ca63d
joahanjr <joahanramirez7@gmail.com>
2022-09-24 23:50:21 -0500

    implementando el despliegue de los menus

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
+/* barra despliegue desktop */
 const navemail = document.querySelector(".navbar-email");
 const desktopmenu = document.querySelector(".desktop-menu");
+/* barra despliegue mobile */
 const menuburger = document.querySelector(".menu");
 const menumobile = document.querySelector(".mobile-menu");
+/* barra despliegue mobile -aside */
+const menucar = document.querySelector(".navbar-shopping-cart");
+const menuproduct = document.querySelector(".product-detail");
 
 
+menucar.addEventListener("click", togglecarmenu);
 navemail.addEventListener("click", toggledesktopmenu);
 menuburger.addEventListener("click", togglemobilemenu);
 
 
 function toggledesktopmenu (){
+    menuproduct.classList.add("inactive")
     desktopmenu.classList.toggle("inactive");
 }
-function togglemobilemenu(){
+function togglemobilemenu(){  
+    menuproduct.classList.add("inactive");
     menumobile.classList.toggle("inactive");
+}
+function togglecarmenu () {
+    desktopmenu.classList.add("inactive")
+    menumobile.classList.add("inactive");
+    menuproduct.classList.toggle("inactive");
 }
