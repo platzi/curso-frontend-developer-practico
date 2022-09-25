b6118e53b1f2519fe955f3b35ad503039a7cba0a
Cristian Micchele <cristian.aiki1@gmail.com>
2022-09-25 10:33:06 -0300

    interactuamos con el menu mobile colocando js y toggle

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDekstopMenu);
+burgerMenu.addEventListener("click", togglemobileMenu);
 
 function toggleDekstopMenu(){
 desktopMenu.classList.toggle("inactive") // Togle hace aparecer y desaparecer.
+}
+
+function togglemobileMenu(){
+    mobileMenu.classList.toggle("inactive") 
 }
