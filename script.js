932cb184dd583d9d08362eba6441593205019d16
Cristian Micchele <cristian.aiki1@gmail.com>
2022-09-25 11:02:56 -0300

    colocamos el toggle en nuestro carrito de compras

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu")
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
 menuEmail.addEventListener("click", toggleDekstopMenu);
 burgerMenu.addEventListener("click", togglemobileMenu);
+menuCarritoIcon.addEventListener("click", togglecarritoAside)
 
 function toggleDekstopMenu(){
 desktopMenu.classList.toggle("inactive") // Togle hace aparecer y desaparecer.
@@ -12,4 +15,9 @@ desktopMenu.classList.toggle("inactive") // Togle hace aparecer y desaparecer.
 
 function togglemobileMenu(){
     mobileMenu.classList.toggle("inactive") 
+}
+
+function togglecarritoAside(){
+    aside.classList.toggle("inactive") 
+
 }
