8f679c1bc07166ec527336d3da7e43800c4d2b8a
joahanjr <joahanramirez7@gmail.com>
2022-09-26 12:21:11 -0500

    implementacion de la seleccion de productos

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const menuburger = document.querySelector(".menu");
 const menumobile = document.querySelector(".mobile-menu");
 /* barra despliegue mobile -aside */
 const menucar = document.querySelector(".navbar-shopping-cart");
-const menuproduct = document.querySelector(".product-detail");
+const shoppingcartcontainer = document.querySelector("#shoppingcartcontainer");
 
 const cardscontainer = document.querySelector(".cards-container");
 
@@ -14,20 +14,18 @@ const cardscontainer = document.querySelector(".cards-container");
 menucar.addEventListener("click", togglecarmenu);
 navemail.addEventListener("click", toggledesktopmenu);
 menuburger.addEventListener("click", togglemobilemenu);
-
-
 function toggledesktopmenu (){
-    menuproduct.classList.add("inactive")
+    shoppingcartcontainer.classList.add("inactive");
     desktopmenu.classList.toggle("inactive");
 }
 function togglemobilemenu(){  
-    menuproduct.classList.add("inactive");
+    shoppingcartcontainer.classList.add("inactive");
     menumobile.classList.toggle("inactive");
 }
 function togglecarmenu () {
-    desktopmenu.classList.add("inactive")
+    desktopmenu.classList.add("inactive");
     menumobile.classList.add("inactive");
-    menuproduct.classList.toggle("inactive");
+    shoppingcartcontainer.classList.toggle("inactive");
 }
 
 const productlist = [];
@@ -59,13 +57,13 @@ for (products of productlist){
     productinfo.classList.add("product-info");
 
     const productdiv = document.createElement("div");
-    
+
     const productprice = document.createElement("p");
     productprice.innerText = "$" + products.price;
 
     const productname = document.createElement("p");
     productname.innerText = products.name;
-    
+
     productdiv.append(productprice, productname);
 
     const productfigure = document.createElement("figure");
