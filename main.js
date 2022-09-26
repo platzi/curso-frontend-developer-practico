8916e30aedde508826d2ae5e2e3f2b89d42b494a
joahanjr <joahanramirez7@gmail.com>
2022-09-26 01:06:22 -0500

    desplegar los productos por JS maquetado de los productos de HTML a JS

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const menumobile = document.querySelector(".mobile-menu");
 const menucar = document.querySelector(".navbar-shopping-cart");
 const menuproduct = document.querySelector(".product-detail");
 
+const cardscontainer = document.querySelector(".cards-container");
+
 
 menucar.addEventListener("click", togglecarmenu);
 navemail.addEventListener("click", toggledesktopmenu);
@@ -26,4 +28,54 @@ function togglecarmenu () {
     desktopmenu.classList.add("inactive")
     menumobile.classList.add("inactive");
     menuproduct.classList.toggle("inactive");
+}
+
+const productlist = [];
+
+productlist.push({
+    name: "bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productlist.push({
+    name: "TV",
+    price: 220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productlist.push({
+    name: "Laptop",
+    price: 150,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+for (products of productlist){
+    const productcard = document.createElement("div");
+    productcard.classList.add("product-card");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", products.image);
+
+    const productinfo = document.createElement("div");
+    productinfo.classList.add("product-info");
+
+    const productdiv = document.createElement("div");
+    
+    const productprice = document.createElement("p");
+    productprice.innerText = "$" + products.price;
+
+    const productname = document.createElement("p");
+    productname.innerText = products.name;
+    
+    productdiv.append(productprice, productname);
+
+    const productfigure = document.createElement("figure");
+    const productimg = document.createElement("img");
+    img.setAttribute("src", "./img/image.png");
+
+    productfigure.append(productimg);
+    productinfo.append(productdiv, productdiv);
+
+    productcard.append(img, productinfo);
+
+    cardscontainer.append(productcard);
 }
