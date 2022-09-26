c10a5daa78e7ca13cc91c2676d5e08c01fbedaca
Luis Ortega <up190656@alumnos.upa.edu.mx>
2022-09-25 21:53:15 -0500

    Added shopping car, product list, and bugs solved

--- main.js
+++ main.js
@@ -1,8 +1,106 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCar = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCar.addEventListener('click', toggleProductDetail);
 
 function toggleDesktopMenu() {
+    const isProductMenuClosed = productDetail.classList.contains('inactive');
+
+    if (!isProductMenuClosed) {
+        productDetail.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    const isProductMenuClosed = productDetail.classList.contains('inactive');
+
+    if (!isProductMenuClosed) {
+        productDetail.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetail() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        productDetail.classList.add('inactive');
+    }
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    productDetail.classList.toggle('inactive'); 
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Keyboard',
+    price: 20,
+    image: 'https://images.pexels.com/photos/841228/pexels-photo-841228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'Phone',
+    price: 300,
+    image: 'https://images.pexels.com/photos/607812/pexels-photo-607812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'TV',
+    price: 400,
+    image: 'https://images.pexels.com/photos/6186813/pexels-photo-6186813.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+function renderProducts (arr) {
+    for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCard = document.createElement('img');
+    productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCard);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(img);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
