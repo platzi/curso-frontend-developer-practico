9c77223c8b376f75a93cc4bcc6ea5a0a046e9b12
coxmau77 <coxmau77@gmail.com>
2022-09-27 16:51:28 -0300

    Fusion del menu clase 19, se agregaron estilos y javaScript

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ navbarEmail.addEventListener("click", toggleDesktopMenu);
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
 }
+
+// Seleccionar mobel menu para hacer que aparezca y desaparezca
+// y el menu hamburguesa
+const mobileMenu = document.querySelector(".mobile-menu");
+const burguerMenuIcon = document.querySelector(".menu");
+
+burguerMenuIcon.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
+}
