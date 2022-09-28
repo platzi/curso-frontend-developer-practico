6b8d1538c06e1f129150bbd8e95d47c33aa4db67
Cristian Micchele <cristian.aiki1@gmail.com>
2022-09-28 15:12:00 -0300

    agregamos carritos de compras

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const burgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu")
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
 const aside = document.querySelector(".product-detail")
+const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDekstopMenu);
 burgerMenu.addEventListener("click", togglemobileMenu);
 menuCarritoIcon.addEventListener("click", togglecarritoAside)
 
 function toggleDekstopMenu(){
+
 desktopMenu.classList.toggle("inactive") // Togle hace aparecer y desaparecer.
 }
 
@@ -18,6 +20,78 @@ function togglemobileMenu(){
 }
 
 function togglecarritoAside(){
-    aside.classList.toggle("inactive") 
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+ // Si el mobile menu esta abierto y el destkopmenu tambien, le agregamos inactive
+    if (!isMobileMenuClosed || !isDesktopMenuClosed) {
+        mobileMenu.classList.toggle('inactive');
+        desktopMenu.classList.toggle('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    Precio: 120,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Pantalla",
+    Precio: 220,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+
+productList.push({
+    name: "Compu",
+    Precio: 520,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+
+for (product of productList){ // El producto de la lista de productos , osea cada elemento del array;
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.imagen);
+
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = '$' + product.Precio;
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+
+
+    const productInfoFigure = document.createElement("figure");
+
+
+    const productInfoImg = document.createElement("img");
+    productInfoImg.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productInfoImg); // Agregar un solo nodo
+
+    productInfo.append(productInfoDiv , productInfoFigure); // Agregar varios nodos
+    
+    productCard.append(productImg, productInfo);
+
+    cardsContainer. appendChild(productCard);
+
 
 }
