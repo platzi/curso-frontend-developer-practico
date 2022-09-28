9d716f4009bc5d4b3ecbfc99bffcd8eac30ed01e
r3is3rsf <robertoperez@gmail.com>
2022-09-28 19:45:33 +0200

    add js for open or close menu

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
 const desktopMenu = document.querySelector(".desktop-menu");
-const navEmail = document.querySelector(".navbar-email");
+const navEmail = document.querySelector(".navbar-email"); //Sera clickeable
+const menuHamburger = document.querySelector(".menu"); //Sera clickeable
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCart = document.querySelector(".navbar-shopping-cart"); //Sera clickeable
+const aside = document.querySelector(".product-detail");
 
+function toggleMenuCart()  {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive");
 
-const toggleDesktopMenu = () =>{
-            desktopMenu.classList.toggle("inactive");
+    }
+    else if(!isDesktopMenuClosed){
+        desktopMenu.classList.add("inactive");
+
+    }
+    aside.classList.toggle("inactive");
 
 
 }
 
+function toggleMobileMenu()  {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+    mobileMenu.classList.toggle("inactive");
+
+}
+
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+      aside.classList.add("inactive");
+    }
+    desktopMenu.classList.toggle("inactive");
+    
+
+}
+
+menuHamburger.addEventListener("click",toggleMobileMenu);
 navEmail.addEventListener("click",  toggleDesktopMenu);
+menuCart.addEventListener("click",toggleMenuCart);
