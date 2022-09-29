698eadcb210159be1f9e7b3bb7ac9aac6f4ed42f
David-Postobon-PC <davidalvarezj@gmail.com>
2022-09-28 21:07:15 -0500

    se integra la el menu de movile hamburgeza

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuhamicon = document.querySelector(".menuhamicon");
+const movileleftMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuhamicon.addEventListener("click", togglmovileMenu);
 
 function toggleDesktopMenu() {
 	desktopMenu.classList.toggle("inactive");
+}
+
+
+function togglmovileMenu() {
+	movileleftMenu.classList.toggle("inactive");
 }
