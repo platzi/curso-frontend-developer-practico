4714662f3cf3c92e1ee651520264c93e50ffecb6
jroargit <jroar26.git@gmail.com>
2022-09-29 15:18:22 -0500

    fusion mobil and cart
    Fusion of mobile menu and add functionality to hamburger menu.
    Fusion of shopping cart menu and add functionaliti to shopping cart

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const navEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const hamburgerMenu = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu");
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const shoppingCartMenu = document.querySelector(".product-detail");
 
-navEmail.addEventListener('click', tooggleDesktopMenu)
 
-function tooggleDesktopMenu(){
+navEmail.addEventListener('click', toggleDesktopMenu)
+hamburgerMenu.addEventListener('click', toggleMobileMenu)
+shoppingCart.addEventListener('click', toggleShoppingCartMenu)
+
+
+function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleShoppingCartMenu(){
+    shoppingCartMenu.classList.toggle('inactive')
 }
