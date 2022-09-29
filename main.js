4b477b602c1b860044523b123afc19a9069b78e3
Jaisson Criollo <jeison14-16@hotmail.com>
2022-09-29 12:21:46 -0500

    Fusion del mobile_menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector('.navbar-email');//Seleccionamos navbar email que esta dentro de navbar-righ
+const desktopMenu = document.querySelector('.desktop-menu');//Seleccionamos todo el menu que parece y desaparece en la parte derecha cclic en email
+const menuHamIcon = document.querySelector('.menu');//Seleccionamos el icono hamburguesa
+const mobileMenu = document.querySelector('.mobile-menu');//Seleccionamos menu de mobile
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);//Escuchamos evento clic cuando damos clic sobre email parte derecha
+menuHamIcon.addEventListener('click', toggleMobileMenu);//Escuchamos evento cuando damos clic al boton hamburguesa
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu(){//FUncion que activa y desactiva el menu desktop
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){//FUncion que activa y desactiva el menu mobile
+    mobileMenu.classList.toggle('inactive');
 }
