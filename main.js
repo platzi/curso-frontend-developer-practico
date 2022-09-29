a2f84124583f458d1325264fb11e143c62f5cf9e
Lisbeth Quintero <lquintero28@live.com>
2022-09-29 09:36:51 -0500

    Fusión del carrito de compras, mostrar y ocultar menús y carrito de compras, de acuerdo a la necesidad

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 const menuEmail = document.querySelector (".navbar-email")
 const desktopMenu = document.querySelector (".desktop-menu")
 const menuHamIcon = document.querySelector (".menu")
+const menuCarIcon = document.querySelector (".navbar-shopping-cart")
 const mobileMenu = document.querySelector (".mobile-menu")
+const aside = document.querySelector (".product-detail")
 
 menuEmail.addEventListener ("click", toggleDesktopMenu)
 menuHamIcon.addEventListener ("click", toggleMobileMenu)
+menuCarIcon.addEventListener ("click", toggleCarAside)
 
 function toggleDesktopMenu () {
+    const isCarClosed = aside.classList.contains ("inactive")
+
+    if  (!isCarClosed) {
+        aside.classList.add ("inactive")
+    } 
     desktopMenu.classList.toggle("inactive")
 }
 
 function toggleMobileMenu () {
+    const isCarClosed = aside.classList.contains ("inactive")
+    
+
+    if  (!isCarClosed) {
+        aside.classList.add ("inactive")
+    } 
+
+   
     mobileMenu.classList.toggle("inactive")
+}
+
+function toggleCarAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains ("inactive")
+    const isDesktopMenuClosed = desktopMenu.classList.contains ("inactive")
+
+    if  (!isMobileMenuClosed || !isDesktopMenuClosed) {
+        mobileMenu.classList.add ("inactive")
+        desktopMenu.classList.add ("inactive")
+    }
+    
+   
+
+    aside.classList.toggle("inactive")
 }
