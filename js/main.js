f1069b6552102237605565670a5217fab3140b3c
Adhemar <adhemarduran@gmail.com>
2022-09-29 17:24:31 -0400

    add shopping car

--- js/main.js
+++ js/main.js
@@ -2,14 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCarIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamIcon.addEventListener('click', toggleMobileMenu);
+shoppingCarIcon.addEventListener('click', toggleAside);
 
 function toggleDesktopMenu() {
+    const isAsideOpen = !aside.classList.contains('inactive');
+
+    if (isAsideOpen) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideOpen = !aside.classList.contains('inactive');
+
+    if (isAsideOpen) {
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAside() {
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+    
+    if (isMobileMenuOpen) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (isDesktopMenuOpen) {
+        desktopMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
 }
