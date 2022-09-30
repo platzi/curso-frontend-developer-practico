933553310431fd5df1ec3df03013ccb7087fbde9
Adhemar <adhemarduran@gmail.com>
2022-09-29 21:20:25 -0400

    add product detail

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const hamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCarIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const aside = document.querySelector('.shopping-cart-container');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
