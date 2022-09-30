b091b308b6039385be6aca1df118690e380845cc
coxmau77 <coxmau77@gmail.com>
2022-09-30 10:59:42 -0300

    Fusion de la clase 22 detalle de producto

--- main.js
+++ main.js
@@ -1,49 +1,9 @@
-// const navbarEmail = document.querySelector(".navbar-email");
-// const desktopMenu = document.querySelector(".desktop-menu");
-
-// navbarEmail.addEventListener("click", toggleDesktopMenu);
-
-// function toggleDesktopMenu() {
-//     desktopMenu.classList.toggle("inactive");
-// }
-
-// // Seleccionar mobile menu para hacer que aparezca y desaparezca
-// // y el menu hamburguesa clase 19
-// const mobileMenu = document.querySelector(".mobile-menu");
-// const burguerMenuIcon = document.querySelector(".menu");
-
-// burguerMenuIcon.addEventListener("click", toggleMobileMenu);
-
-// function toggleMobileMenu() {
-//     mobileMenu.classList.toggle("inactive");
-// }
-
-// //Seleccionamos el ocono del carrito para mostrar el detalle del pedido clase 20
-// const navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
-
-// //aside
-// const productDetail = document.querySelector(".product-detail");
-// navbarShoppingCart.addEventListener("click", toggleShoppinngCart);
-
-// function toggleShoppinngCart() {
-
-//     const isMobileMenuClosed = mobileMenu.classList.contains("incactive");
-//     const isProductDetailClosed = productDetail.classList.contains("incactive");
-
-//     // Esto hace que se cierre si se abre otra opcion
-//     if (isMobileMenuClosed) {
-//         mobileMenu.classList.add("inactive");
-//     }
-
-//     productDetail.classList.toggle("inactive");
-// }
-
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.getElementById('shopping-cart-container');
 
 //Seleccion del card container clase 21
 const cardsContainer = document.querySelector(".cards-container");
@@ -53,20 +13,20 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -79,7 +39,7 @@ function toggleCarritoAside() {
         mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 //Clase 21
