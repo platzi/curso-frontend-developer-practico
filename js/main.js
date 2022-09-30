d39e7879ca3ef5f720ae8b1c478b4795eccdbc4e
Adhemar <adhemarduran@gmail.com>
2022-09-29 20:45:00 -0400

    add product list

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const hamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCarIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamIcon.addEventListener('click', toggleMobileMenu);
@@ -43,3 +44,79 @@ function toggleAside() {
     
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Screen',
+    price: 300,
+    image: 'https://m.media-amazon.com/images/I/4105K8zLeWL._AC_SY580_.jpg'
+});
+productList.push({
+    name: 'Computer',
+    price: 200,
+    image: 'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'
+});
+productList.push({
+    name: 'Cellphone',
+    price: 100,
+    image: 'https://media.karousell.com/media/photos/products/2021/3/25/apple_iphone_11_1616634106_2ad9f115_progressive'
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Screen',
+    price: 300,
+    image: 'https://m.media-amazon.com/images/I/4105K8zLeWL._AC_SY580_.jpg'
+});
+productList.push({
+    name: 'Computer',
+    price: 200,
+    image: 'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'
+});
+productList.push({
+    name: 'Cellphone',
+    price: 100,
+    image: 'https://media.karousell.com/media/photos/products/2021/3/25/apple_iphone_11_1616634106_2ad9f115_progressive'
+});
+
+function renderProducts(productList) {
+    for (product of productList) {
+        const productCard = document.createElement('DIV');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('IMG');
+        productImg.setAttribute('src', product.image);
+        
+        const productInfo = document.createElement('DIV');
+        productInfo.classList.add('product-info');
+        
+        const productInfoDiv = document.createElement('DIV');
+    
+        const productPrice = document.createElement('P');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('P');
+        productName.innerText = product.name;
+    
+        const productInfoFigure = document.createElement('FIGURE');
+        const productImgCard = document.createElement('IMG');
+        productImgCard.setAttribute('src', '../icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCard);
+        productInfoDiv.append(productPrice, productName);
+        productInfo.append(productInfoDiv, productInfoFigure);
+        productCard.append(productImg, productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
