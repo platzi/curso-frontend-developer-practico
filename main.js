3c26b7ce1874f1cebb43fd6856dbfdc52f41e650
Julio Hernandez <juliohernandezcm@gmail.com>
2022-09-30 11:36:34 +0200

    fusion shopping cart

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const navbarShoppingCartIcon = document.querySelector('.navbar-shopping-cart');
 const burguerIconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const asideShoppingCart = document.querySelector('.product-detail');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 burguerIconMenu.addEventListener('click', toggleMobileMenu);
+navbarShoppingCartIcon.addEventListener('click', toggleAsideShoppingCart);
 
 function toggleDesktopMenu() {
 	desktopMenu.classList.toggle('inactive');
+	asideShoppingCart.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
+	asideShoppingCart.classList.add('inactive');
 	mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAsideShoppingCart() {
+	mobileMenu.classList.add('inactive');
+	desktopMenu.classList.add('inactive');
+	asideShoppingCart.classList.toggle('inactive');
 }
