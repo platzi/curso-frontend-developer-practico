1e66decc829d728dcf56a134a6845667418c7481
Julio Hernandez <juliohernandezcm@gmail.com>
2022-09-30 14:48:32 +0200

    renderProduct function finished

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const navbarShoppingCartIcon = document.querySelector('.navbar-shopping-cart');
 const burguerIconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const asideShoppingCart = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 burguerIconMenu.addEventListener('click', toggleMobileMenu);
@@ -24,3 +25,83 @@ function toggleAsideShoppingCart() {
 	desktopMenu.classList.add('inactive');
 	asideShoppingCart.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+	name: 'Bike',
+	price: 120,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+	name: 'Monitor',
+	price: 220,
+	image: 'https://images.pexels.com/photos/1714202/pexels-photo-1714202.jpeg?auto=compress&cs=tinysrgb&w=800',
+});
+productList.push({
+	name: 'Macbook Air',
+	price: 800,
+	image: 'https://images.pexels.com/photos/3693732/pexels-photo-3693732.jpeg?auto=compress&cs=tinysrgb&w=800',
+});
+productList.push({
+	name: 'Bike',
+	price: 120,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+	name: 'Monitor',
+	price: 220,
+	image: 'https://images.pexels.com/photos/1714202/pexels-photo-1714202.jpeg?auto=compress&cs=tinysrgb&w=800',
+});
+productList.push({
+	name: 'Macbook Air',
+	price: 800,
+	image: 'https://images.pexels.com/photos/3693732/pexels-photo-3693732.jpeg?auto=compress&cs=tinysrgb&w=800',
+});
+productList.push({
+	name: 'Bike',
+	price: 120,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+	name: 'Monitor',
+	price: 220,
+	image: 'https://images.pexels.com/photos/1714202/pexels-photo-1714202.jpeg?auto=compress&cs=tinysrgb&w=800',
+});
+productList.push({
+	name: 'Macbook Air',
+	price: 800,
+	image: 'https://images.pexels.com/photos/3693732/pexels-photo-3693732.jpeg?auto=compress&cs=tinysrgb&w=800',
+});
+
+function renderProducts(arr) {
+	for (product of arr) {
+		const productCard = document.createElement('div');
+		productCard.classList.add('product-card');
+
+		const productImage = document.createElement('img');
+		productImage.setAttribute('src', product.image);
+
+		const productInfo = document.createElement('div');
+		productInfo.classList.add('product-info');
+
+		const productInfoDiv = document.createElement('div');
+
+		const paraphrasePrice = document.createElement('p');
+		paraphrasePrice.innerText = product.price;
+
+		const paraphraseName = document.createElement('p');
+		paraphraseName.innerText = product.name;
+
+		const figureProduct = document.createElement('figure');
+
+		const btnAddToCartIcon = document.createElement('img');
+		btnAddToCartIcon.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+		figureProduct.append(btnAddToCartIcon);
+		productInfoDiv.append(paraphrasePrice, paraphraseName);
+		productInfo.append(productInfoDiv, figureProduct);
+		productCard.append(productImage, productInfo);
+		cardsContainer.append(productCard);
+	}
+}
+renderProducts(productList);
