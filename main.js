680e97dd6a9c6f079b90ee09635c786b3e417766
Eduardo Cruz <edu_cm_95@hotmail.com>
2022-10-01 16:13:58 -0300

    Lista de productos:HTML a partir de arrays

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const aside =  document.querySelector(".product-detail");
 
+const cardsConteiner = document.querySelector(".cards-container");  
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click",toggleMobileMenu);
 menuCarritoIcon.addEventListener("click",toggleCarritoAside);
@@ -45,3 +47,62 @@ const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
         aside.classList.toggle("inactive") 
 };
     
+const productList = [];
+
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    });
+
+productList.push({
+    name: "BenelliTnt",
+    price: 12000,
+    image: "https://4.bp.blogspot.com/-shY9XV-sMNQ/V81lpWeD68I/AAAAAAAAAUk/9zjVXHWgelQlVAXO4tgFRmHIdvqaUMVJQCLcB/s1600/Benelli%2BTNT%2B300.jpg"
+    });
+
+productList.push({
+     name: "Mac",
+     price: 550,
+     image: "https://tweakers.net/i/rrfMaW1jBGNlrOg6rgB4v0f6o6U=/full-fit-in/4920x3264/filters:max_bytes(3145728):no_upscale():strip_icc():fill(white):strip_exif()/f/image/cgJEBUD5pyFbMjDhpAqa4AAn.jpg?f=user_large"
+     });
+
+     function renderProducts(arr){
+     for (product of productList) {
+       const productCard = document.createElement("div");
+       productCard.classList.add("product-card");
+
+        // produc = {name, price ,image} -> produc.image
+       const productImag = document.createElement("img");
+       productImag.setAttribute("src", product.image);  //agregamos un src//
+
+       const productInfo = document.createElement("div");
+       productInfo.classList.add("product-info");
+
+       const productInfoDiv = document.createElement("div");
+
+       const productPrice = document.createElement("p");
+       productPrice.innerText = "$" + product.price;
+
+       const productName = document.createElement("p");
+       productName.innerText =  product.name;
+
+       productInfoDiv.appendChild(productPrice);
+       productInfoDiv.appendChild(productName);
+
+       const productInfoFigure = document.createElement("figure");
+       const productImgCart= document.createElement("img");
+       productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+       productInfoFigure.appendChild(productImgCart); 
+
+
+       productInfo.appendChild(productInfoDiv);
+       productInfo.appendChild(productInfoFigure);
+
+       productCard.appendChild(productImag);
+       productCard.appendChild(productInfo);
+
+       cardsConteiner.appendChild(productCard);
+    }
+};
+    renderProducts(productList);
