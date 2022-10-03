43e62f318d54d2360c301d63dc65a75c00e87a84
Jhan Vásquez <jhanvasquez95@gmail.com>
2022-10-03 16:04:44 -0500

    Ultimos cambios

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuMobileBtn = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
 
@@ -7,13 +11,48 @@ const desktopMenu = document.querySelector('.desktop-menu');
 eventListeners();
 function eventListeners() {
     menuEmail.addEventListener('click', toggleDesktopMenu);
+
+    menuMobileBtn.addEventListener('click', toggleMobileMenu);
+
+    menuCarritoIcon.addEventListener('click', toggleCarrito);
 }
 
 
 
 //Funciones
 function toggleDesktopMenu() {
+
+    const isAsideMenuClosed = aside.classList.contains('inactive');
+
+    if(!isAsideMenuClosed) {
+        aside.classList.add('inactive');
+        }
+
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('Funciona????');
+function toggleMobileMenu() {
+
+    const isAsideMenuClosed = aside.classList.contains('inactive');
+
+    if(!isAsideMenuClosed) {
+        aside.classList.add('inactive');
+        }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarrito() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const ismenuCarritoClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+    } else if(!ismenuCarritoClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
