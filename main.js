10ec75b81f864cbd31bf84ab752a1e8ad803797c
Jovanny <jovannyalca@gmail.com>
2022-10-02 23:36:25 -0500

    un cambio mas menu mobil menu carrito y menu desktop-email. falta unos detallitos

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const menuHamIcon = document.querySelector(".menu-ham-icon");
+const mobileMenu = document.querySelector(".mobile-menu");
+const asaid = document.querySelector(".product-detail")
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAsaide);
 
-function toggleDesktopMenu (){
-   desktopMenu.classList.toggle("inactive")
+
+function toggleDesktopMenu(){
+   const isMenuCarritoOpen = asaid.classList.contains('inactive-car');
+   if (isMenuCarritoOpen) {
+      asaid.classList.remove('inactive-car');
+      
+   }
+   desktopMenu.classList.toggle('inactive');
 }
-console.log("todo ok");
+
+/* funcion para activar el menu en mobil*/
+
+function toggleMobileMenu(){
+   const isMenuCarritoOpen = asaid.classList.contains('inactive-car');
+   if (isMenuCarritoOpen) {
+      asaid.classList.toggle('inactive-car');
+      
+   }
+    mobileMenu.classList.toggle('inactive-ham');
+    console.log('activa toggle en el menu en mobil');
+ }
+
+
+
+/*funcion para el menu del carrito*/ 
+ function toggleCarritoAsaide(){
+   const isMobileMenuOpen = mobileMenu.classList.contains('inactive-ham');
+   const isMenuDeskOpen = desktopMenu.classList.contains('inactive');
+   if (isMobileMenuOpen || isMenuDeskOpen) {
+      mobileMenu.classList.toggle('inactive-ham');
+      desktopMenu.classList.remove('inactive');
+      console.log('desactiva menu mobil');
+   }
+   asaid.classList.toggle('inactive-car');
+ }
