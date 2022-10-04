3d880f69159d5402c42b4ea2f7b15ba516880ec9
Mauricio Tovar <mauriciodtovar@gmail.com>
2022-10-04 10:10:44 -0300

    Fusión ProfileMenu, MobileMenu, AsideMenu con JS

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const burgerIconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cartIconAside = document.querySelector('.navbar-shopping-cart');
+const asideProductDetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerIconMenu.addEventListener('click', toggleMobileMenu);
+cartIconAside.addEventListener('click', toggleMyOrderAside);
+
 
 function toggleDesktopMenu () {
+    asideProductDetail.classList.add('inactive')
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu () {
+    asideProductDetail.classList.add('inactive')
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleMyOrderAside () {
+    mobileMenu.classList.add('inactive')
+    asideProductDetail.classList.toggle('inactive')
 }
