911a8ebc580d064accbccce05855988b11aad14f
Brandon Huerta <bran.7594@gmail.com>
2022-10-04 14:27:22 -0500

    se agrego el carrito de compras y se soluciono el problema de los menus y carritos abiertos al mismo tiempo

--- main.js
+++ main.js
@@ -4,13 +4,38 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarIcon.addEventListener('click', toggleAside);
 
 function toggleDesktopMenu(){
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if(!isAsideClose){
+        aside.classList.add('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if(!isAsideClose){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAside(){
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+        aside.classList.toggle('inactive');     
 }
