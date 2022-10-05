42e80c00c6d1d5806b186b82238715f7ad4a4f07
mauricio ocampo marulanda <maoca2085@gmail.com>
2022-10-04 20:28:13 -0500

    Cambios parciales en la funcionalidad del carrito de compras

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector('.navbar-email')
 const deskTopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const productShoppingCart = document.querySelector('.product-detail')
+const shoppingCart = document.querySelector('.navbar-shopping-cart')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu() {
  deskTopMenu.classList.toggle('inactive');
@@ -12,4 +15,9 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart() {
+    productShoppingCart.classList.toggle('inactive');
+    console.log("Funciona")
 }
