8ce7095e4a9c9df9a31720db482bf2884834c2a0
Juan <yo217@hotmail.es>
2022-10-05 07:18:03 -0500

    product cards

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
 const mobileMenu = document.querySelector(".mobile-menu")
 const aside = document.querySelector(".product-detail")
 
-
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside)
@@ -46,3 +45,21 @@ function toggleCarritoAside(){
 
     aside.classList.toggle("inactive")
 }
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: "Car",
+    price: 2400,
+    image: "https://www.carrosyclasicos.com/media/k2/items/cache/8f316ac48e41dc8633e89a01554f58d5_XL.jpg"
+});
+productList.push({
+    name: "Computer",
+    price: 1000,
+    image: "https://media.wired.com/photos/62b4a66bcf5463e00aca58a7/191:100/w_1280,c_limit/MSI-Katana-GF66-Gear.jpg"
+});
+
