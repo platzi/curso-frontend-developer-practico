dce3126b3ab613f6b2d29b9c9c77266630b5155a
Mauricio Ocampo 2 <maoca2085@hotmail.com>
2022-10-05 01:21:57 -0500

    Avance de funcionalidad menus

--- main.js
+++ main.js
@@ -10,14 +10,78 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 shoppingCart.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu() {
- deskTopMenu.classList.toggle('inactive');
-}
+    let isDesktopMenuClosed = deskTopMenu.classList.contains('inactive');
+    let isShoppingCartClosed = productShoppingCart.classList.contains('inactive');
 
-function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+    if(isDesktopMenuClosed && isShoppingCartClosed){
+        deskTopMenu.classList.remove('inactive');
+
+    }else if(!isShoppingCartClosed){
+        productShoppingCart.classList.add('inactive');
+        deskTopMenu.classList.remove('inactive')
+    }
+    else{
+       
+        deskTopMenu.classList.toggle('inactive');
+    }
+
 }
 
 function toggleShoppingCart() {
-    productShoppingCart.classList.toggle('inactive');
-    console.log("Funciona")
+    let isDesktopMenuClosed = deskTopMenu.classList.contains('inactive');
+    let isShoppingCartClosed = shoppingCart.classList.contains('inactive');
+    let isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if(!isDesktopMenuClosed){
+        if(isShoppingCartClosed){
+        DesktopMenu.classList.add('inactive')
+        productShoppingCart.classList.remove('inactive');
+        } else {
+            desktopMenu.classList.add('inactive');
+            productShoppingCart.classList.add('inactive')
+        }
+    }    
+
+    if(isDesktopMenuClosed){
+        if(isShoppingCartClosed){
+            productShoppingCart.classList.remove('inactive');
+        } else {
+          productShoppingCart.classList.add('inactive')
+        }
+    }
+    if(!isMobileMenuClosed) {
+        if(isShoppingCartClosed){
+            mobileMenu.classList.add('inactive')
+            productShoppingCart.classList.remove('inactive');
+            } else {
+                mobileMenu.classList.add('inactive');
+                productShoppingCart.classList.add('inactive')
+            }
+        }  
+        if(isMobileMenuClosed){
+            if(isShoppingCartClosed){
+              productShoppingCart.classList.remove('inactive');
+            } else {
+              productShoppingCart.classList.add('inactive')
+            }
+        }  
+}
+
+function toggleMobileMenu() {
+    let isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    let isShoppingCartClosed = shoppingCart.classList.contains('inactive');
+
+    if(!isShoppingCartClosed){
+        if(isMobileMenuClosed){
+        productShoppingCart.classList.add('inactive')
+        mobileMenu.classList.remove('inactive');
+        } else {
+            productShoppingCart.classList.add('inactive')
+            mobileMenu.classList.add('inactive');
+        }
+    }    
+    if(isShoppingCartClosed) {
+       
+        mobileMenu.classList.toggle('inactive');
+        } 
 }
