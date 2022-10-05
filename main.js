234f3c0b8592a1980011626727e1d66838890fa5
juanvalgreen <gabroval@hotmail.com>
2022-10-05 17:10:59 -0500

    integration of left menu for mobile also with media queries

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const userMenu=document.querySelector('.desktop-menu');
+const userMenuDesktop=document.querySelector('.desktop-menu');
 const userEmail=document.querySelector('.navbar-email');
+const burguerMenuIcon=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
 
-userEmail.addEventListener('click',toggleUserMenu);
+//userEmail.addEventListener('click',toggleUserMenu);
+//burguerMenuIcon.addEventListener('click',toggleMobileMenu);
 
-function toggleUserMenu(){
-    userMenu.classList.toggle('inactive');
-    console.log();
+userEmail.addEventListener("click",function(){toggleMenu(userMenuDesktop)});
+burguerMenuIcon.addEventListener("click", function(){toggleMenu(mobileMenu)});
+
+function toggleMenu(elemento){
+    
+    elemento.classList.toggle("inactive");
 }
