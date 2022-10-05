542174b39e93b6e97640cfd21b3572d6ddb868d5
estuardolofish <ellp2038@gmail.com>
2022-10-04 23:08:32 -0600

    Clase 21 - Lista de productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const desktopMenu = document.querySelector('.desktop-menu'); // menu chiquito al
 const mobileMenu = document.querySelector('.mobile-menu'); // menu que saca las 3 barritas de la izquierda
 const aside = document.querySelector('.product-detail'); // detalle del producto al darle click al carrito de compras
 
+const cardsContainer = document.querySelector('.cards-container'); // Seleccioando la clase para insertarle todo el codigo html echo en js
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -41,3 +43,63 @@ function toggleCarritoAside(){
 
 }
 
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Compu',
+    price: 200,
+    image: 'https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+
+function renderProducts(arr){
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image)
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = 'Q. ' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
