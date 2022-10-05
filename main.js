fddf3221edd02bc1816d53e530be72c9181d81ab
Jhan Vásquez <jhanvasquez95@gmail.com>
2022-10-05 17:39:09 -0500

    Issue con el incono de agregar al carrito

--- main.js
+++ main.js
@@ -88,6 +88,8 @@ for ( product of productList ) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
 
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
 
 
     const productInfo = document.createElement('div');
