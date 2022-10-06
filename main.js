b5e8f95545393a7c3b0639e3929c3d5fe73411c3
Daniel Cuevas <ivandaniel1224@gmail.com>
2022-10-06 17:19:42 -0500

    Finished menu

--- main.js
+++ main.js
@@ -2,14 +2,41 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenu.addEventListener("click", toggleMobileMenu);
+menuCartIcon.addEventListener("click", toggleCartAside);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCartAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed || !isDesktopMenuClosed) {
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
 }
