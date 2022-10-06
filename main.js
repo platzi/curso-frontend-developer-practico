1a92c738704def4ff58cd3359d828f0ffd165c5e
jhonat23 <jhonat23@gmail.com>
2022-10-06 11:07:32 -0500

    clase 19

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 
-
+//class 18
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+//class 19
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
