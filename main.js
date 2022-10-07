6912e5a154b02f5641a1c403f78bbc550a9c6667
Marcelo Colman <marcelopcolman@hotmail.com>
2022-10-06 21:40:16 -0300

    2 actualizacion de archivos

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamicon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+menuHamicon.addEventListener('click', toogleMobileMenu);
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
+function toogleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
