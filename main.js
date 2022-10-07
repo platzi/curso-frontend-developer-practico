1717aa8c8c1d1216b0a2acfa4076e89452b17f1a
Miguel Angel Osonio <miguelos1128@gmail.com>
2022-10-07 07:09:44 -0500

    Fusion del Menu en Mobile y modificacion del js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-const menuIcon = document.querySelector('.menu');
+const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuIcon.addEventListener('click', toggleMobileMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
