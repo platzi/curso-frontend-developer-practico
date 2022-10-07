d2333657da22a23a1c06f71c4d93dd639e24f842
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2022-10-06 22:04:09 -0500

    Se puso menu desplegable para mobile

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
-const menuDesktop = document.querySelector('.desktop-menu')
+const menuDesktop = document.querySelector('.desktop-menu');
+const menuMobile = document.querySelector('.mobile-menu')
+const menuMobileOption = document.querySelector('.menu')
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuMobileOption.addEventListener("click", toggleDesktopMenuMobile);
 
 function toggleDesktopMenu(){
-        menuDesktop.classList.toggle('inactive')
+        menuDesktop.classList.toggle('inactive');
 
+}
+
+function toggleDesktopMenuMobile(){
+        menuMobile.classList.toggle('inactive');
 }
