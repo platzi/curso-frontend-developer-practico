5d0789c7b2d2cb23351a5f60c804761be511a3f8
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2022-10-06 22:34:15 -0500

    Falta acomdar inconvenientes con la sinconizacion de los menus

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
-const menuMobile = document.querySelector('.mobile-menu')
-const menuMobileOption = document.querySelector('.menu')
+
+const menuMobileOption = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
+
+const shoopingCart = document.querySelector('.navbar-shopping-cart');
+const containerCartShooping = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuMobileOption.addEventListener("click", toggleDesktopMenuMobile);
+shoopingCart.addEventListener('click',toggleContainerCart);
 
 function toggleDesktopMenu(){
         menuDesktop.classList.toggle('inactive');
@@ -14,4 +19,8 @@ function toggleDesktopMenu(){
 
 function toggleDesktopMenuMobile(){
         menuMobile.classList.toggle('inactive');
+}
+
+function toggleContainerCart(){
+        containerCartShooping.classList.toggle('inactive');
 }
