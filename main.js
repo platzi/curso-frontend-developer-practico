e3640ef0f61ac2e3f5d82daeca5eeab80d05784b
Juan Pablo Gaviria <juan.gaviria@gorillalogic.com>
2022-10-07 20:55:48 -0500

    Adding cart details

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const menuEmail = document.getElementById('menu-email');
 const desktopContextMenu = document.getElementById('desktop-menu');
+const burguerButton = document.getElementById('burguer-menu');
+const mobileMenu = document.getElementById('mobile-menu');
+const cartDetail = document.getElementById('cart-detail');
+const cartButton = document.getElementById('cart-button');
 
 menuEmail.addEventListener('click', () => {
   desktopContextMenu.classList.toggle('inactive');
+
+  cartDetail.classList.add('inactive');
+});
+
+burguerButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('inactive');
+
+  cartDetail.classList.add('inactive');
+});
+
+cartButton.addEventListener('click', () => {
+  cartDetail.classList.toggle('inactive');
+
+  mobileMenu.classList.add('inactive');
+
+  desktopContextMenu.classList.add('inactive');
 });
