8a9ad7cb5c66f543d14406b83b161d61476f6ec8
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2022-10-09 15:47:20 -0500

    Se agregan los productos por medio de JavaScript

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ const menuMobile = document.querySelector('.mobile-menu');
 const shoopingCart = document.querySelector('.navbar-shopping-cart');
 const containerCartShooping = document.querySelector('.product-detail');
 
+const mainContainerCart = document.querySelector('.cards-container');
+const mainContainer = document.querySelector('.main-container');
+
+
+const productList = [];
+productList.push({
+        name:'Computer',
+        price:220,
+        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+        name: 'Cellphone',
+        price: 5620,
+        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+        name: 'Bike',
+        price: 5961,
+        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuMobileOption.addEventListener("click", toggleDesktopMenuMobile);
@@ -46,3 +68,45 @@ function toggleContainerCart(){
         }
         
 }
+
+for(product of productList){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const img = document.createElement('img');
+        img.setAttribute('src',product.image);
+
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const info = document.createElement('div');
+
+        const price = document.createElement('p');
+        price.innerText ='$'+ product.price ;
+
+        const name = document.createElement('p');
+        name.innerText = product.name;
+
+        const cart = document.createElement('figure');
+
+        const cartImage = document.createElement('img');
+        cartImage.setAttribute('src',"./icons/bt_add_to_cart.svg");
+      
+
+        cart.appendChild(cartImage);
+        info.appendChild(price);
+        info.appendChild(name);
+        productInfo.appendChild(info);
+        productInfo.appendChild(cart);
+        productCard.appendChild(img);
+        productCard.appendChild(productInfo) ;
+        
+        mainContainerCart.appendChild(productCard);
+        
+        
+}
+
+mainContainer.appendChild(mainContainerCart);
+
+
