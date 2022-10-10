a78cb3c988f190b22e447840e68df6a2549294d5
Adan Saavedra <adansaavedraa@icloud.com>
2022-10-09 21:14:54 -0500

    add carrito

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuBurger = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarrito = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuBurger.addEventListener('click',toggleMobileMenu);
+menuCarrito.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+    
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
 }
