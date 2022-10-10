9fddba11e079763d2af11a5f40731c8777b955ca
Jessi Paez <jessimar1852@gmail.com>
2022-10-10 18:36:51 -0300

    add: fusion del menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopmenu = document.querySelector('.desktop-menu')
+const desktopmenu = document.querySelector('.desktop-menu');
+const menuHambIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHambIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
   desktopmenu.classList.toggle('inactive')
-  console.log('click')
 }
-console.log('muy bien')
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('inactive')
+}
