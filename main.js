8d644d9d99172dd8f67d4684a6a7003013497af5
plgjke <plgjke@gmail.com>
2022-10-11 20:47:54 -0300

    Tercer commit fusión de pestañas desplegables de navbar

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 const menuEmail = document.querySelector(".navbar-email");
+const menuHamIcon = document.querySelector(".menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
-const menuHamIcom = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const asideMenu = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
-menuHamIcom.addEventListener("click", toggleMobileMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarIcon.addEventListener("click", toggleCarAside);
 
 function toggleDesktopMenu() {
+  const isAsideMenuClosed = asideMenu.classList.contains("inactive");
+
+  if (!isAsideMenuClosed) {
+    asideMenu.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  const isAsideMenuClosed = asideMenu.classList.contains("inactive");
+
+  if (!isAsideMenuClosed) {
+    asideMenu.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add("inactive");
+  }
+
+  asideMenu.classList.toggle("inactive");
 }
