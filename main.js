834ddaa403b0b02811e2bb6ab8c16499176ee495
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2022-10-11 16:22:45 -0500

    Detalles del producto, sin interaccion

--- main.js
+++ main.js
@@ -28,6 +28,16 @@ productList.push({
         price: 5961,
         image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
+productList.push({
+        name: 'Bike',
+        price: 5961,
+        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+        name: 'Bike',
+        price: 5961,
+        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
