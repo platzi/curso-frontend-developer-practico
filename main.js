bcd04a248397207be5fdebcb95f91a19ced49c6e
= <=>
2022-10-11 17:55:08 -0400

    agregamos varias funciones de despliegue de elementos y agregamos el carrito de mis compras de el archivo clase13.html

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu () {
-    desktopMenu.classList.toggle("inactive")
+    aside.classList.add("inactive");
+    desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle("inactive")
+    aside.classList.add("inactive")
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+    mobileMenu.classList.add ("inactive")
+    desktopMenu.classList.add("inactive");
+    aside.classList.toggle("inactive");
 }
