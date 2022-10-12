8ca10b0fa337a9e4d42d7829b1f84385559a78d2
Edwin  Castro <edwin@DESKTOP-6GSOE11.localdomain>
2022-10-11 20:34:39 -0500

    Detalles del producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const MobilMenu = document.querySelector('.mobile-menu');
 const MenuCartIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container')
 
 MenuMail.addEventListener('click', toggleDesktopMenu);
@@ -11,18 +11,18 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 MenuCartIcon.addEventListener('click', toggleProductDetail);
 
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-const isAsideClosed = aside.classList.contains('inactive');
+const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     MobilMenu.classList.toggle('inactive');
@@ -35,7 +35,7 @@ function toggleProductDetail(){
     MobilMenu.classList.add('inactive');
    } 
 
-    aside.classList.toggle('inactive');
+   shoppingCartContainer.classList.toggle('inactive');
 }
 // creacion de Arrays
 
