8b8ed642bb75a9ae89f9f32bff4b1d8810f7de2e
= <=>
2022-10-12 19:36:19 -0400

    agregamos product detail de la clase12.html

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 
 const cardsContainer = document.querySelector(".cards-container");
 
@@ -14,19 +14,19 @@ menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu () {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    aside.classList.add("inactive")
+    shoppingCartContainer.classList.add("inactive")
     mobileMenu.classList.toggle("inactive");
 }
 
 function toggleCarritoAside() {
     mobileMenu.classList.add ("inactive")
     desktopMenu.classList.add("inactive");
-    aside.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
