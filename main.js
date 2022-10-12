b415eec333fd8f3a472fcb68196998f3542eec3e
gacz1205 <gacz1205@gmail.com>
2022-10-12 12:06:45 -0500

    Fusión del menú en Desktop version

---
+++ main.js
@@ -0,0 +1,10 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toogleDesktopMenu);
+
+
+function toogleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive');
+    console.log('click');
+}
