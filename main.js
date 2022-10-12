099651e1e42db15c24a6562e92f4f0a83a08abc4
Richard Gonzalez <rikygonzalez09@gmail.com>
2022-10-12 20:45:01 -0300

    Listado de productos de html y js a partir de arrays

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+
 
 function toggleDesktopMenu() {
   const isAsideClosed = aside.classList.contains("inactive");
@@ -41,3 +43,97 @@ function toggleCarritoAside() {
 
   aside.classList.toggle("inactive");
 }
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Pantalla",
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Computadora",
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Bike",
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Pantalla",
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Computadora",
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard =  document.createElement("div");
+    productCard.classList.add("product-card");
+  
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+  
+    const productInfo =  document.createElement("div");
+    productInfo.classList.add("product-info");
+  
+    const productInfoDiv = document.createElement("div");
+  
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+}
+renderProducts(productList);
+
+// menuEmail.addEventListener("click", prueba);
+// menuHamIcon.addEventListener("click", prueba);
+// menuCarritoIcon.addEventListener("click", prueba);
+
+// function prueba() {
+//   const asideOculto = aside.classList.contains("inactive");
+//   const mobileOculto = mobileMenu.classList.contains("inactive");
+//   const desktopOculto = desktopMenu.classList.contains("inactive");
+
+//   if (asideOculto) {
+//     aside.classList.add("inactive");
+//     desktopMenu.classList.toggle("inactive");
+//     mobileMenu.classList.toggle("inactive");
+//   } else if (mobileOculto) {
+//     mobileMenu.classList.add("inactive");
+//     desktopMenu.classList.toggle("inactive");
+//     aside.classList.toggle("inactive");
+//   } else if (desktopOculto) {
+//     desktopMenu.classList.add("inactive");
+//     aside.classList.toggle("inactive");
+//     mobileMenu.classList.toggle("inactive");
+//   }   
+// }
