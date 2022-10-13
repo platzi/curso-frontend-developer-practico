f3d0e85c675a9c19826cedbd3efba50f78b60746
Alice Bagoud <99303912+alicebagoud@users.noreply.github.com>
2022-10-13 12:23:52 -0500

    main

--- main.js
+++ main.js
@@ -2,10 +2,20 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive'); /* cada vez que le den click a ese elemento de email en el menu,
-      se va a ejecutar esta funcion. */
+  desktopMenu.classList.toggle('inactive'); 
+  /* cada vez que le den click a ese elemento de email en el menu,
+  se va a ejecutar esta funcion. */
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
