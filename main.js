9678dac5e6bfd1888bd5119b8864813875f64cf8
ArtoriasDelAbismo <jerojournade1@gmail.com>
2022-10-12 21:08:08 -0300

    inactive add or remove as needed

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const shoppingCartLogo = document.querySelector('.navbar-shopping-cart');
 const shoppingCartList = document.querySelector('#shoppingCartList');
 const cardsContainer = document.querySelector('.cards-container');
 
+const productDetailContainer = document.querySelector('#productDetail');
+const closeButton = document.querySelector('.product-detail-close');
+
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
@@ -18,18 +21,37 @@ shoppingCartLogo.addEventListener('click', toggleShopingCartList);
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
     shoppingCartList.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
     shoppingCartList.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 }
 
 function toggleShopingCartList() {
     shoppingCartList.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 }
+
+function openProductDetailAside() {
+    productDetailContainer.classList.remove('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    shoppingCartList.classList.add('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    shoppingCartList.classList.add('inactive');
+}
+
+
 
 const productList = [];
 
@@ -42,25 +64,31 @@ productList.push({
 productList.push({
     name: 'Couch',
     price: 150,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://images.pexels.com/photos/1631918/pexels-photo-1631918.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 })
 
 productList.push({
     name: 'Lamp',
     price: 95,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://images.pexels.com/photos/1112598/pexels-photo-1112598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
 
 productList.push({
     name: 'Magic Mouse',
-    price: 150460,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    price: 50460,
+    image: 'https://i0.wp.com/www.sohrabosati.com/wp-content/uploads/2021/06/Apple-Magic-Mouse-Charging_1.jpg?resize=1024%2C576',
 });
 
 productList.push({ 
     name: 'Watch',
     price: 190,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({ 
+    name: 'Trip to the Moon',
+    price: 15500,
+    image: 'https://images.pexels.com/photos/23769/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
 
 function renderProducts(arr) {
@@ -72,7 +100,9 @@ function renderProducts(arr) {
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
-    
+        productImg.addEventListener('click', openProductDetailAside);
+        closeButton.addEventListener('click', closeProductDetailAside);
+
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
     
@@ -106,3 +136,5 @@ function renderProducts(arr) {
 }
 
 renderProducts(productList)
+
+    
