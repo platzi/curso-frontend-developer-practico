1530adce620e78af7c44319f5e0cb12093ea0c2f
Blanca <blancabcbt@gmail.com>
2022-10-13 14:22:07 +0200

    add navbar and toggle email

---
+++ main.js
@@ -0,0 +1,14 @@
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector('.desktop-menu');
+
+
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive');
+}
+
+console.log("JJJ")
+
+
