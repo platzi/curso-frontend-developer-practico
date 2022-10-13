dc88eaae2aa1366d689660d5d8f064ac1719eac1
Christian Larios <christiandlf15@gmail.com>
2022-10-13 07:58:30 -0500

    Trabajo realizado hasta la clase 21

--- main.js
+++ main.js
@@ -2,13 +2,28 @@ const email = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const cartIcon = document.querySelector('.navbar-shopping-cart')
+const cart = document.querySelector('.product-detail')
 
-email.addEventListener('click', toggle)
-mobileIcon.addEventListener('click', toggle2)
 
-function toggle(){
+email.addEventListener('click', toggleEmail)
+mobileIcon.addEventListener('click', toggleMenu)
+cartIcon.addEventListener('click',toggleCart)
+
+function toggleEmail(){
+  if(!cart.classList.contains('inactive')){
+    cart.classList.toggle('inactive')
+  }
   desktopMenu.classList.toggle('inactive');
 }
-function toggle2(){
+function toggleMenu(){
+  if(!cart.classList.contains('inactive')){
+    cart.classList.toggle('inactive')
+  }
   mobileMenu.classList.toggle('inactive');
+}
+function toggleCart(){
+  if(!mobileMenu.classList.contains('inactive')){mobileMenu.classList.toggle('inactive')}
+  if(!desktopMenu.classList.contains('inactive')){desktopMenu.classList.toggle('inactive')}
+  cart.classList.toggle('inactive');
 }
