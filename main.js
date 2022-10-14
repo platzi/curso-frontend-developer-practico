a89fde15e8dd15f7ce713f361e10e964ca52ed29
Andrea Bustos <andreastefbustos@gmail.com>
2022-10-14 13:28:22 -0400

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktop = document.querySelector('.desktop-menu')
+const burgerIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+burgerIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
-    console.log('click')
     desktop.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
