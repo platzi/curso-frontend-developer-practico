512c82114043947d1fdb484b3e7405ffef173843
Héctor Galindo <hec.gph@proton.me>
2022-10-14 08:59:19 -0500

    fusión del menú en mobile y arreglo en el CSS

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.burger-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
 
+// Desktop
 navEmail.addEventListener('click', toggleDesktopMenu)
+
+// Mobile
+burgerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu ()
 {
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu ()
+{
+    mobileMenu.classList.toggle('inactive')
 }
