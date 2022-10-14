62475ead6d429f344c35b434f1308101a23dafec
Agueda94 <solorzangaby169@gmail.com>
2022-10-14 17:15:57 -0400

    diseño mobile y desktop para los menu

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const email = document.querySelector(".navbar-email");
 const menu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 email.addEventListener("click", toggleMenu);
+burgerMenu.addEventListener("click", toggleMobile);
 
 function toggleMenu (){
 
     menu.classList.toggle("inactive");
 
+}
+
+function toggleMobile (){
+    
+    mobileMenu.classList.toggle("inactive");
 }
