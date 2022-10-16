47e40a7ccc063165d7310dfc23ac0b58b2c17fcd
XxSGExX <elbraun21@gmail.com>
2022-10-16 11:27:29 -0400

    Modificacion del Mobile Menu para el desplegue de las opciones y modificacion en el desktop para el responsive

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,18 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHambungerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHambungerIcon.addEventListener('click', toggleMobileMenu);
+
 
 // JS
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
