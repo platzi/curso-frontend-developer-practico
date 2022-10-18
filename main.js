6988aaf1bd5b9c0d494243f697036819bcae8408
sebassierra21 <sebastian-2109@hotmail.com>
2022-10-17 20:01:47 -0500

    Funcionalidades del menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navShopCar = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navShopCar.addEventListener('click', toggleDesktopMenu)
+burguerMenu.addEventListener('click', toggleMobilepMenu)
 
 function toggleDesktopMenu() {
    desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobilepMenu() {
+   mobileMenu.classList.toggle('inactive');
+}
+
