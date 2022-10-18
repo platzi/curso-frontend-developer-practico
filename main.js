79411618ee1103991279ef308002d194b5f3f337
will_08 <wbahoque71@gmail.com>
2022-10-18 16:08:49 -0500

    Aprendi a ocultar elementos en la pantalla pc y movil + fusione los dos menus en vista de pantalla pc y movil

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+// Diferentes seleccionadores de elementos html
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamImg = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+// Escuchador de eventos "clicks"
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamImg.addEventListener('click', toggleMobileMenu);
 
+// Funciones para manejar eventos en el html
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
