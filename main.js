a82f5e6f7df4019a8f410d64edec9696482424ac
kevin gutierrez <harold.dev1993@gmail.com>
2022-10-19 00:53:49 -0500

    avanzando

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 
 const menuIconMobile = document.querySelector('.menu')
-const menuMobile = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
@@ -13,13 +13,31 @@ menuEmail.addEventListener('click', toggleDesktopMenu)
 menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+     }
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
-    menuMobile.classList.toggle('inactive')
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+     }
+
+     mobileMenu.classList.toggle('inactive')
 }
 
 function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed) {
+       mobileMenu.classList.add('inactive')
+    }
+
     aside.classList.toggle('inactive')
 }
