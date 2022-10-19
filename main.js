b04ae68ecc5c0a4a9228610b6d715e33b06a9384
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-10-18 21:01:22 -0500

    avanzando ando otra vez xd

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 // menuDesktop
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+
+const menuIconMobile = document.querySelector('.menu')
+const menuMobile = document.querySelector('.mobile-menu')
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+
+menuIconMobile.addEventListener('click', toggleMobileMenu)
 menuEmail.addEventListener('click', toggleDesktopMenu)
-// menuDesktop
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
 }
 
-
-// menuMobile
-const menuIconMobile = document.querySelector('.menu')
-const menuMobile = document.querySelector('.mobile-menu')
-
-menuIconMobile.addEventListener('click', toggleMobileMenu)
-
-// menuMobile
 function toggleMobileMenu() {
     menuMobile.classList.toggle('inactive')
 }
 
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive')
+}
