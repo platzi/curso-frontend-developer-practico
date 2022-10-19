32900b0b767e81f801e9c9b7e6a4aa0ad2eb7c29
jsanchez71 <julio.sanchez71@hotmail.com>
2022-10-19 15:14:58 -0500

    Integración del archivo clase 13.html

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+  mobileMenu.classList.add('inactive');
+  aside.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle('inactive');
+  desktopMenu.classList.add('inactive');
+  aside.classList.add('inactive');
+}
+
+function toggleCarritoAside() {
+  aside.classList.toggle('inactive');
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
 }
