99a2981aee65a27d4cf9787c762be1bb607e488a
Camilo Blandon <camiloblandon69@gmail.com>
2022-10-18 19:59:35 -0500

    ajuste en el menu mobile

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const navEmail =document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
+const menuHamIcon=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click',toggleDestopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 function toggleDestopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
