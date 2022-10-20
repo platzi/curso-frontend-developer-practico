5cc311de83f4aa4d9bea289a6627a0eaaeec2234
jsanchez71 <julio.sanchez71@hotmail.com>
2022-10-20 15:59:06 -0500

    Integración del archivo clase 12.html

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');    // Requerido para poder agregar un nodo al contenedor principal
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -14,17 +14,17 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
   mobileMenu.classList.add('inactive');
-  aside.classList.add('inactive');
+  shoppingCartContainer.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle('inactive');
   desktopMenu.classList.add('inactive');
-  aside.classList.add('inactive');
+  shoppingCartContainer.classList.add('inactive');
 }
 
 function toggleCarritoAside() {
-  aside.classList.toggle('inactive');
+  shoppingCartContainer.classList.toggle('inactive');
   desktopMenu.classList.add('inactive');
   mobileMenu.classList.add('inactive');
 }
