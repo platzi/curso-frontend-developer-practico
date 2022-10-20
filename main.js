0bc00892d1153501b64e682f2b0522ff0d4ccbad
oscarvedo <balnicor@gmail.com>
2022-10-20 14:08:50 -0500

    Menus laterales implementados

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHamburguerIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguerIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  const desktopMenu = document.querySelector('.desktop-menu');
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+  aside.classList.toggle('inactive');
 }
