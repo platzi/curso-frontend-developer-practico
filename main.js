1fc3c394d95a116737b6830bbe0c0f574a3e228c
john lopez <johnguitarmusica@gmail.com>
2022-10-20 23:42:14 -0500

    agregamos carrito de compras en su primera parte

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const menuEmail = document.querySelector(".navbar-email");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
   console.log("hola diste click");
@@ -14,4 +17,9 @@ function toggleDesktopMenu() {
 function toggleMobileMenu() {
   console.log("hola diste click");
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  console.log("ya me dio ");
+  aside.classList.toggle("inactive");
 }
