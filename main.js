d5d090b4750acfd4080349069da0b7fa71aab5d4
Thiago Almaraz <turquito215@gmail.com>
2022-10-21 12:57:45 -0300

    Fusion de menu hamburguesa

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', togglemobileMenu);
 
 function toggleDesktopMenu() {
-    console.log('click');
     desktopMenu.classList.toggle('inactive')
 }
 
-console.log("JS funcionando")
+function togglemobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
