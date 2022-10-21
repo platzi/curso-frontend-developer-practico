1183a52b975be84d59973dd20be044cee69094fa
will_08 <wbahoque71@gmail.com>
2022-10-21 16:54:38 -0500

    Arreglar la informacion en detail y falta hacer el pull request

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 // Diferentes seleccionadores de elementos html
 const menuEmail = document.querySelector('.navbar-email');
 const menuCarroIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
+const imgProduct = document.getElementById('detail');
 const menuHamImg = document.querySelector('.menu');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#product-detail');
 const cartcontainer = document.querySelector('.cards-container');
 
 // Escuchador de eventos "clicks"
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamImg.addEventListener('click', toggleMobileMenu);
 menuCarroIcon.addEventListener('click', toggleAside);
+productDetailCloseIcon.addEventListener('click', closeProductoDetailAside);
+
 
 // Funciones para manejar eventos en el html
 function toggleDesktopMenu() {
     const veriaside = shoppingCartContainer.classList.contains('inactive');
-    if (!veriaside) {
+    const veriProductDetail = productDetailContainer.classList.contains('inactive');
+    if (!veriaside || !veriProductDetail) {
         shoppingCartContainer.classList.add('inactive');
+        productDetailContainer.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
@@ -25,18 +32,41 @@ function toggleMobileMenu() {
     if (!veriaside) {
         shoppingCartContainer.classList.add('inactive');
     }
+    closeProductoDetailAside();
     mobileMenu.classList.toggle('inactive');
 }
 function toggleAside() {
     // shoppingCartContainer.classList.toggle('inactive'); se comenta porque se necesita un condicional para verificar sino hay otro elemento abierto.
     const verimobileMenu = mobileMenu.classList.contains('inactive'); // se declara esta variable para capturar si el menumobile esta abierto
     const veridesktopMenu = desktopMenu.classList.contains('inactive');
+    const veriProductDetail = productDetailContainer.classList.contains('inactive');
     // Este condicional niega el estado en el que se encuentre el elemento para proceder a cerrar los elementos si estan abiertos. 
-    if (!verimobileMenu || !veridesktopMenu) {
+    if (!verimobileMenu || !veridesktopMenu || !veriProductDetail) {
         mobileMenu.classList.add('inactive');
         desktopMenu.classList.add('inactive');
+        productDetailContainer.classList.add('inactive');
     }
     shoppingCartContainer.classList.toggle('inactive'); // con este codigo mandamos a activar o desactivar la clase "inactive" en el html.
+}
+function openProductDetailAside() {
+    const veriaside = shoppingCartContainer.classList.contains('inactive');
+    const veridesktopMenu = desktopMenu.classList.contains('inactive');
+    if (!veriaside || !veridesktopMenu) {
+        shoppingCartContainer.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+    function mostrar(recibido) {
+        for (gone of recibido){
+            const Productimg = document.createElement('img');
+            Productimg.setAttribute('src', gone.image); 
+            imgProduct.append(Productimg);
+        }
+    }
+    mostrar(productList);
+    productDetailContainer.classList.remove('inactive');
+}
+function closeProductoDetailAside() {
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = []; // Array para devolver codigo JS
@@ -55,6 +85,8 @@ productList.push({
     price: 750,
     image: 'https://images.pexels.com/photos/2399840/pexels-photo-2399840.jpeg?cs=srgb&dl=pexels-%E6%B4%8B%E6%A6%A4-%E9%83%AD-2399840.jpg&fm=jpg&_gl=1*1tqvzm3*_ga*NDgzNjUzNzkzLjE2NjM2MjU2NzI.*_ga_8JE65Q40S6*MTY2NjIwOTk2NS42LjEuMTY2NjIxMDE1NS4wLjAuMA..'
 });
+
+
 
 /* 
 <div class="product-card">
@@ -79,7 +111,9 @@ function maquetacion(arr) {
         // product = {name, price, image} --> product.image
         const Productimg = document.createElement('img');
         Productimg.setAttribute('src', product.image);
+        Productimg.addEventListener('click',openProductDetailAside);
         
+    
         const productoInfo = document.createElement('div');
         productoInfo.classList.add('product-info');
     
