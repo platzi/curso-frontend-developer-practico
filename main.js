1a883b5ac2f96ff5cc8d5ba1173689baa8591891
Yair Chira <chirayair@gmail.com>
2022-10-22 00:55:00 -0400

    fucionando html y ccs, adicional agregando JavaScript pagina 2

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmanil = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 
 menuEmanil.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
