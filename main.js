0794eeaa94def45259cd49ea59f4c195ca48b10e
greenmancoder <juan_diego_toro@hotmail.com>
2022-10-23 18:54:06 -0500

    adición de productDetail con css

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuHamIcon = document.querySelector('.menu');
 const menuIconCarrito = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -15,10 +15,10 @@ function toggleDesktopMenu(){
 }
 
 function toggleMobileMenu(){
-    const isAsideCLosed = aside.classList.contains('inactive');
+    const isAsideCLosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideCLosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -26,13 +26,13 @@ function toggleMobileMenu(){
 
 function toggleCarritoAside(){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 //Para tener la "base de datos de los productos"
