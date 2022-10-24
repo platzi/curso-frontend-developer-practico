b7c040d6c4f24599796d91f0c843fa51e4a249b4
Yair Chira <chirayair@gmail.com>
2022-10-24 15:32:28 -0400

    fucionando html y css, adicional agregando con JS lista de productos a partir de arrays clase 6

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 
 menuEmanil.addEventListener('click', toggleDesktopMenu);
@@ -40,3 +42,60 @@ function toggleCarritoAside() {
     aside.classList.toggle('inactive');
 
 }
+
+const productList = [];
+productList.push({
+    name: 'Bicicleta extreme hard',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Telefono Iphone 14 ProMax',
+    price: 1249,
+    image: 'https://dealntechcdn.b-cdn.net/wp-content/uploads/2021/06/Apple-iPhone-14-Pro-Max-represents.jpg'
+});
+productList.push({
+    name: 'reloj Apple Watch serie 7',
+    price: 155,
+    image: 'https://th.bing.com/th/id/OIP.E8kuUX7plfjJOAQIUf96pgHaF7?pid=ImgDet&rs=1'
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        // product = {name,price,image} --> product.image
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div')
+    
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price;
+        const producName = document.createElement('p')
+        producName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(producName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList);
