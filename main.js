5ae1aa846d46c2b023fbc4e774361b11a1414dba
Daniel Ramirez <dermirez@gmail.com>
2022-10-24 17:36:29 -0500

    Se agrega al proyecto el archivo clase8 que es el menu de la version mobile, y se agregan media queries para que trabajen los menus de la version mobile y desktop coordinadamente

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const habMenuIcon = document.querySelector('.menu');
+const mobileHamMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     console.log('Click');
     desktopMenu.classList.toggle('inactive');
+}
+
+habMenuIcon.addEventListener('click', togglemobileHabMenu);
+
+function togglemobileHabMenu () {
+    mobileHamMenu.classList.toggle('inactive')
 }
