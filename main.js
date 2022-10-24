6a07f8d3cbc4cf958fd7607b297579b595d8a089
Acer <ltdrake1984@gmail.com>
2022-10-24 11:14:11 -0500

    Added togglemenu interactions

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDrop = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuCart = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
+
 menuEmail.addEventListener('click', toggleMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCart.addEventListener('click', toggleProducts);
 
 function toggleMenu(){
     menuDrop.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
+}
+
+function toggleProducts(){
+    productDetail.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
 }
