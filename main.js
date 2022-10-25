524cdc38ce53c08116ed46b5fb550b404e172c07
Cesar Augusto Arbelaez Bemudez <caaber2@gmail.com>
2022-10-25 10:21:02 -0500

    Todas las vistas con manipulación del DOM JS

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const menuCarritoHamIcon = document.querySelector(".navbar-shopping-cart");
+const productDetailCloseIcon = document.querySelector(".product-detail-close");
 const mobileMenu = document.querySelector(".mobile-menu");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
+const productDetailContainer = document.querySelector("#productDetail");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoHamIcon.addEventListener("click", toggleCarritoAside);
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside);
 
 function toggleDesktopMenu() {
     const isAsideMenuClosed = shoppingCartContainer.classList.contains("inactive");
@@ -26,6 +29,8 @@ function toggleMobileMenu() {
     if (!isAsideMenuClosed) {
         shoppingCartContainer.classList.add("inactive");
     }
+
+    closeProductDetailAside();
     
     mobileMenu.classList.toggle("inactive");
 }
@@ -39,9 +44,24 @@ function toggleCarritoAside() {
         desktopMenu.classList.add("inactive"); 
         shoppingCartContainer.classList.add("inactive");
     } 
+
+    const isProductDetailClosed = productDetailContainer.classList.contains("inactive");
+
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add("inactive");
+    }
     
     shoppingCartContainer.classList.toggle("inactive"); 
 
+}
+
+function openPrdouctDetailAside() {
+    shoppingCartContainer.classList.add("inactive");
+    productDetailContainer.classList.remove("inactive");
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add("inactive");
 }
 
 const productList = [];
@@ -85,6 +105,7 @@ function renderProducts(arr) {
     
         const productImg = document.createElement("img");
         productImg.setAttribute("src", product.image);
+        productImg.addEventListener("click", openPrdouctDetailAside);
     
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
