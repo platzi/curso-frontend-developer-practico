09172bc89358cd0fbf74779c1242d2f0917bbf58
Guillermo Moreno <guillermolpzm@gmail.com>
2022-10-25 18:19:50 -0500

    clase8.html merged into index.html

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.getElementById("desktop-menu")
+const bugerMenu = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+bugerMenu.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive")
 }
