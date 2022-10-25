56da31b2e826e9d1723d022f475da95d7dc2443a
Jeremi Rojas <Jeremirojas2626@gmail.com>
2022-10-25 18:19:08 -0400

    Fusion Menu Mobile

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu =document.querySelector('.desktop-menu');
+const menuhamIcon =document.querySelector('.menu');
+const mobileMenu =document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuhamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
-    console.log ('Click')
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
