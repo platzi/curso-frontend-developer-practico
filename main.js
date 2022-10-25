e343da8adef642eab7518cb88eed302420f13926
Victor <emilio-andrade86@hotmail.com>
2022-10-25 18:20:05 -0500

    Integracion de menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', menuDesktop);
+menuHamIcon.addEventListener('click', menuMobile);
 
 function menuDesktop(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function menuMobile(){
+    mobileMenu.classList.toggle('inactive');
 }
