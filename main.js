bc079482b01b040d044a7d96fdefc5fccac33742
Mgonzal3z <lomilito@hotmail.com>
2022-10-25 00:57:37 -0500

    Carrito ce compras

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-const menuBurger = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
 
    desktopMenu.classList.toggle('inactive');
+   aside.classList.add('inactive');
 }
+
+const menuBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuBurger.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu(){
 
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
+
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+menuCarrito.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside(){
+
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+}
+
+
