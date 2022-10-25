915a22d00539179702d6848888fbc5fbda234e1c
Mgonzal3z <lomilito@hotmail.com>
2022-10-24 23:05:53 -0500

    Displays y menus desplegables

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
 
    desktopMenu.classList.toggle('inactive');
+}
+
+menuBurger.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+
+    mobileMenu.classList.toggle('inactive');
 }
