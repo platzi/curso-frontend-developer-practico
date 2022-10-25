8d526d01be2d1d1e84fbf118de81e835299ca371
Thiago Almaraz <turquito215@gmail.com>
2022-10-25 11:58:02 -0300

    Crear las cards en javascrtpt

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -40,21 +42,21 @@ function toggleCarritoAside() {
 }
 
 const productList = [];
-productList.push({
-  name: 'Bike',
-  price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
-productList.push({
-  name: 'Pantalla',
-  price: 220,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
-productList.push({
-  name: 'Compu',
-  price: 620,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
+productList.push ({
+  name:'Bike',
+  price: 12700,
+  image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push ({
+  name:'Bicycle helmet',
+  price: 1200,
+  image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+  name:'Seat',
+  price: 300,
+  image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
 
 /*
 <div class="product-card">
@@ -71,33 +73,41 @@ productList.push({
 </div>
 */
  
-for (product of productList) {
-  const productCard = document.createElement('div');
-  productCard.classList.add(product-card);
-
-  const img = document.createElement('img');
-  img.setAttribute('src', product.image);
-
-  const productInfo = document.createElement('div');
-  productInfo.classList.add(product-info);
-
-  const productInfoDiv = document.createElement('div');
-
-  const productPrice = document.createElement('p');
-  productPrice.innerText = '$' + product.price;
-  const productName = document.createElement('p');
-  productName.innerText = product.name;
+function renderProducts(arr) {
+  for(product of arr){
+    const productCard=document.createElement('div');
+    productCard.classList.add('product-card');
   
-  productInfoDiv.appendChild(productPrice);
-  productInfoDiv.appendChild(productName);
+    const productImg=document.createElement('img');
+    productImg.setAttribute('src',product.image);
   
-  const productInfoFigure = document.createElement('figure');
-  const productImgCart = document.createElement('img');
-  img.setAttribute('src', './icons/bt_add_to_cart.svg');
-
-  productInfoFigure.appendChild(productImgCart);
-
-  productInfo.appendChild(productInfoDiv);
-  productInfo.appendChild(productInfoFigure);
-
+    const productInfo=document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv=document.createElement('div');
+  
+    const productPrice=document.createElement('p');
+    productPrice.innerText='$'+product.price;
+    const productName=document.createElement('p');
+    productName.innerText=product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName)
+  
+    const productInfoFigure=document.createElement('figure');
+    const productImgCart=document.createElement('img');
+    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
 }
+
+renderProducts(productList);
