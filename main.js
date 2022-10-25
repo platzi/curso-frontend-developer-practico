49b3e4534690f49d3b6625ebb802b1a61eb62031
Daniel Ramirez <dermirez@gmail.com>
2022-10-25 13:57:16 -0500

    Se finaliza la logica comenzada en el commit anterior, ahora sí se integran y trabajan coordinadamente los menus de email, aside (carrito) y hamburger (mobile) para las versiones desktop y mobile

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHabIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
-const habMenuIcon = document.querySelector('.menu');
-const mobileHamMenu = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHabIcon.addEventListener('click', togglemobileHabMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    console.log('Click');
+    const isAsideMenuClosed = aside.classList.contains('inactive');
+    // si el menu aside está abierto, entonces hay que cerrarlo
+    if (!isAsideMenuClosed) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
-habMenuIcon.addEventListener('click', togglemobileHabMenu);
-
 function togglemobileHabMenu () {
-    mobileHamMenu.classList.toggle('inactive')
+    const isAsideMenuClosed = aside.classList.contains('inactive');
+    // si el menu aside está abierto, entonces hay que cerrarlo
+    if (!isAsideMenuClosed) {
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isEmailMenuClosed = menuEmail.classList.contains('inactive');
+    // si el Mobile Menu está abierto, hay que cerrarlo
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isEmailMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+}
+
