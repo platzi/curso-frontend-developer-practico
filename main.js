584b6c49c6e07e09322d36c7f85fcae3eb0f9da9
Fabian Caro <fabiancaro101@gmail.com>
2022-10-26 12:37:58 -0500

    Fusion del menu en mobile y fusionar carrito de compras en mobile y desktop

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 
 function toggleDesktopMenu() {
+    const isAsideClosed =  aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed =  aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    // Guardo en una variable el valor de true o flase de su clase inactive
+    const isMobileMenuClosed =  mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    // si esta abierto el menu mobile, lo cierro
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }// si esta abierto el menu desktop, lo cierro
+    else if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+    // abro el aside
+    aside.classList.toggle('inactive');
 }
