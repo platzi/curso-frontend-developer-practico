0363d731db3b20c57ed5fad939c84a599b7deafe
Cristian Acevedo <cristian.acevedo@ibacrea.com>
2022-10-25 20:08:01 -0500

    added shopping cart and mobile features

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuEmail.addEventListener('mouseover', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
+}
+
+burgerMenu.addEventListener('click', toggleMobileMenu)
+
+//si el mobile menu está activo, el aside se inactivará viceversa
+function toggleMobileMenu() {
+    aside.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
+}
+
+menuCart.addEventListener('click', toggleCart);
+
+//si el aside está activo, el mobile menu se inactivará viceversa
+function toggleCart() {
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
