dc30764089aeba642048e0a6b1ab2676a3f72651
yilver7u7 <yilversgg@gmail.com>
2022-10-26 11:19:43 -0500

    Hemos añadido el menu de mobile, ademas le añadimos una animacion de transicion al menu de mobile

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const icon = document.querySelector(".icon");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 
 
 menuEmail.addEventListener("click", showDesktopMenu);
+icon.addEventListener("click", showMobileMenu);
 
 function showDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
 }
-console.log("Desktop")
+
+function showMobileMenu(){
+    mobileMenu.classList.toggle("activo");
+}
