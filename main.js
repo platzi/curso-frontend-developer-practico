ace6e58753c17073f5df1fda611ba1c800c75b9a
yilver7u7 <yilversgg@gmail.com>
2022-10-26 18:13:22 -0500

    Hemos añadido el Aside del icono de comprar y ademas si el menu mobile esta abierto, pero vas a abrir el aside se cierra el mobile

--- main.js
+++ main.js
@@ -2,14 +2,32 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const icon = document.querySelector(".icon");
 const mobileMenu = document.querySelector(".mobile-menu");
+const iconCar = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 
 
 menuEmail.addEventListener("click", showDesktopMenu);
 icon.addEventListener("click", showMobileMenu);
+iconCar.addEventListener("click", showAside)
+
+function showAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("activo")
+    const isAsideMenu= mobileMenu.classList.contains("inactive")
+
+    
+    if(isMobileMenuClosed){
+        mobileMenu.classList.remove("activo")
+        aside.classList.toggle("inactive");
+    }else{
+        //Abrir el Aside
+        aside.classList.toggle("inactive");
+    }
+}
 
 function showDesktopMenu() {
     desktopMenu.classList.toggle("activar");
+    
 }
 
 function showMobileMenu(){
