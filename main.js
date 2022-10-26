3ff06132ecbb5d3a6ee29a5dc9d3272a5b413551
Jeremi Rojas <Jeremirojas2626@gmail.com>
2022-10-26 11:51:32 -0400

    Carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu =document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuhamIcon =document.querySelector('.menu');
 const mobileMenu =document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuhamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside() {
+    
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
