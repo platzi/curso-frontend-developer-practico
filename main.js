4374b8a646e0a3ff26f2e305433a9d9347089fc8
Mgonzal3z <lomilito@hotmail.com>
2022-10-26 21:14:33 -0500

    Trabajando en el catalogo

--- main.js
+++ main.js
@@ -32,4 +32,50 @@ function toggleCarritoAside(){
     desktopMenu.classList.add('inactive');
 }
 
+const productsList = [];
+productsList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
 
+productsList.push({
+    name: 'Pantalla',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productsList.push({
+    name: 'Compu',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+for(product of productsList){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+
+    const productName = document.createElement()
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    img.setAttribute('src','./icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+}
