5c9a55f1c2c0b42db59af1b2d8559a5df5a167b1
Julián Castrillón <juliancastrillon681@gmail.com>
2022-10-26 23:02:24 -0500

    Integración del aside y añadir clase inactive según el contenedor que esté abierto en el momento

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,40 @@
 
 window.addEventListener('load',()=>{
 
+    // Declaración e inicialización de variables; 
     const menu = document.querySelector('.navbar-email'); 
     const desktop_menu = document.querySelector('.desktop-menu'); 
+    const IconBurguer = document.querySelector('.menu'); 
+    const mobile_menu = document.querySelector('.mobile-menu');  
+    const aside = document.querySelector('.product-detail'); 
+    const car = document.querySelector('.navbar-shopping-cart');
     
+    // Instrucciones;
     menu.addEventListener('click',()=>{
         // Cada vez que se le da click, agrega o quita la clase inactive; 
         desktop_menu.classList.toggle('inactive'); 
+
+        // Condición con operador ternario; 
+        !aside.classList.contains('inactive') ? aside.classList.add('inactive') : 0;
+        !mobile_menu.classList.contains('inactive') ? mobile_menu.classList.add('inactive') : 0; 
     })
     
 
-    // Icono hamburguesa
-    const IconHamburguer = document.querySelector('.menu'); 
-    const mobile_menu = document.querySelector('.mobile-menu'); 
-
-    IconHamburguer.addEventListener('click',()=>{
+    IconBurguer.addEventListener('click',()=>{
         mobile_menu.classList.toggle('inactive'); 
+
+        // Condición con operador ternario; 
+        !aside.classList.contains('inactive') ? aside.classList.add('inactive') : 0;
+        !desktop_menu.classList.contains('inactive') ? desktop_menu.classList.add('inactive') : 0; 
     }); 
+
+    car.addEventListener('click',()=>{
+        aside.classList.toggle('inactive');
+
+        // Condición con operador ternario; 
+        !desktop_menu.classList.contains('inactive') ? desktop_menu.classList.add('inactive') : 0; 
+        !mobile_menu.classList.contains('inactive') ? mobile_menu.classList.add('inactive') : 0; 
+    });
+
 }); 
 
