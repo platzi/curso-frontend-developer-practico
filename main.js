a6c184c192636810d2462d71cd965057eaf634e9
Fabian Caro <fabiancaro101@gmail.com>
2022-10-27 13:42:01 -0500

    Interaccion entre todos los componentes

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ const desktopMenu = document.querySelector('.desktop-menu');
 
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
+const productDetailContainer = document.querySelector('#productDetail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
+
 
 
 function toggleDesktopMenu() {
@@ -30,6 +34,8 @@ function toggleMobileMenu() {
         shoppingCartContainer.classList.add('inactive');
     }
 
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle('inactive');
 }
 
@@ -37,6 +43,7 @@ function toggleCarritoAside() {
     // Guardo en una variable el valor de true o flase de su clase inactive
     const isMobileMenuClosed =  mobileMenu.classList.contains('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
 
     // si esta abierto el menu mobile, lo cierro
     if(!isMobileMenuClosed){
@@ -45,9 +52,22 @@ function toggleCarritoAside() {
     else if (!isDesktopMenuClosed) {
         desktopMenu.classList.add('inactive');
     }
+    else if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
+    }
     // abro el aside
     shoppingCartContainer.classList.toggle('inactive');
 }
+
+function openProductDeatilAside() {
+    shoppingCartContainer.classList.add('inactive')
+    // remove para quitar la clase inactive
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
+  }
 
 const productList = [];
 productList.push({
@@ -59,13 +79,13 @@ productList.push({
 productList.push({
     name: 'Pantalla',
     price: 220,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
 
 productList.push({
     name: 'Compu',
     price: 620,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
 
 
@@ -77,6 +97,7 @@ function renderProducts(arr){
         // product= {name, price, image} -> product.image
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDeatilAside);
       
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
