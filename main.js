74f87cbe3d26c340aab9ed2bed31932295cf7002
ronaldvic <ronaldblancobalboa@gmail.com>
2022-10-27 19:18:44 -0400

    icono carrito ventana emergente aside agregado

--- main.js
+++ main.js
@@ -2,13 +2,33 @@ const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMenuHamIcon)
+menuCarritoIcon.addEventListener('click', toggleMenuCarrito)
+
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMenuHamIcon(){
+    const isAsideClosed = aside.classList.contains('inactive')
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleMenuCarrito(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')
 }
