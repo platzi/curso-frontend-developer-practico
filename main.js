1e74d768f7684eb7a6cc73c82127dfd68d480646
gaby gramajo <ggabrieldeveloper@gmail.com>
2022-10-28 15:23:11 -0300

    add shoping cart

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 const navEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const burgerMenu = document.querySelector(".menu");
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const asideProductDetail = document.querySelector(".product-detail");
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
-
 burgerMenu.addEventListener("click", toggleMobileMenu);
+shoppingCart.addEventListener("click", toggleShopingCart);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = asideProductDetail.classList.contains("inactive");
+
+  if(!isAsideClosed) {
+    asideProductDetail.classList.add('inactive');
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
-
 function toggleMobileMenu() {
+  const isAsideClosed = asideProductDetail.classList.contains("inactive");
+
+  if(!isAsideClosed) {
+    asideProductDetail.classList.add('inactive');
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+function toggleShopingCart() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if(!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+
+  asideProductDetail.classList.toggle("inactive");
 }
