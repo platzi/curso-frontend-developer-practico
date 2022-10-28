1ccbfaad8db1b1d920de390273dfc7f24fe22ccc
Ariel <alarmoreira@gmail.com>
2022-10-28 20:32:59 -0300

    Seorganizan las templates en una carpeta

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+//Constantes;
 const $menuEmail = document.querySelector('.navbar-email');
 const $desktopMenu = document.querySelector('.desktop-menu');
 
+const $menuHambIcon = document.querySelector('.menu'); 
+const $mobileMenu = document.querySelector('.mobile-menu');
+
+//Agregar el escuchador de eventos; 
 $menuEmail.addEventListener('click', toggleDesktopMenu);
+$menuHambIcon.addEventListener('click', toggleMobileMenu); 
+
+//Funciones que agregar la clase inactive mediante 'toggle'; 
 function toggleDesktopMenu(){
     $desktopMenu.classList.toggle('inactive');
 };
+
+function toggleMobileMenu(){
+    $mobileMenu.classList.toggle('inactive');    
+};
+
 
