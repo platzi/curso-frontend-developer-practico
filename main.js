09aede786a2085f71cfe5b9e1fd1d7d9a82ff203
Luis Tabares <tabaresluis@hotmail.com>
2022-10-28 19:17:29 -0400

    se agrego menu mobile y funcionalidad

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu= document.querySelector('.desktop-menu')
+const mobileMenu= document.querySelector('.mobile-menu')
+const menuHamb = document.querySelector('.menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuHamb.addEventListener('click', toggleHambMenu)
 
 function toggleDesktopMenu(){
-    console.log("asdasd")
     desktopMenu.classList.toggle('inactive')
+}
+function toggleHambMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
 
