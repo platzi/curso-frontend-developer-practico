69a90be3d5b50aac481bbf083e254d90cc118bb1
Libardo Arroyave <laastos@gmail.com>
2022-10-28 15:15:41 -0500

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // Show / Hide Desktop Menu
 const navEmail = document.querySelector('.navbar-email');
 navEmail.addEventListener('click', () => {
-  const desktopMenu = document.querySelector('.desktop-menu').classList.toggle('inactive');
+  document.querySelector('.desktop-menu').classList.toggle('inactive');
+});
+
+// Show / hide Mobile menu
+const menuHamIcon = document.querySelector('.menu');
+menuHamIcon.addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.toggle('inactive');
+
 });
