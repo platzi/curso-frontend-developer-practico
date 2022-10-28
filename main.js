ef6fd608093a957e2720472fbc07a5b5b295a2ac
gaby gramajo <ggabrieldeveloper@gmail.com>
2022-10-28 17:28:42 -0300

    add isDesktopMenuClose

--- main.js
+++ main.js
@@ -30,9 +30,14 @@ function toggleMobileMenu() {
 }
 function toggleShopingCart() {
   const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
 
   if(!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');
+  }
+
+  if(!isDesktopMenuClose) {
+    desktopMenu.classList.add('inactive');
   }
 
   asideProductDetail.classList.toggle("inactive");
@@ -95,17 +100,3 @@ function renderProducts(productList) {
     cartContainers.appendChild(productCard);
   }
 }
-/*
-<div class="product-card">
-        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-        <div class="product-info">
-          <div>
-            <p>$120,00</p>
-            <p>Bike</p>
-          </div>
-          <figure>
-            <img src="./icons/bt_add_to_cart.svg" alt="">
-          </figure>
-        </div>
-      </div>
-*/ 
