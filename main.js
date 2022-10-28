0c8b02165d4db3b9fa8a1cbdb8cc664ae7a919cf
Jason <jasongiovany@gmail.com>
2022-10-28 17:02:51 -0600

    Adding HTML product cards via JS

--- main.js
+++ main.js
@@ -4,12 +4,19 @@ const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if(!isAsideClose){
+        aside.classList.add("inactive");
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
@@ -21,6 +28,7 @@ function toggleMobileMenu(){
     }
     mobileMenu.classList.toggle("inactive");
 }
+
 function toggleCarritoAside(){
     const isMobileMenuClose = mobileMenu.classList.contains('inactive');
 
@@ -31,3 +39,76 @@ function toggleCarritoAside(){
     aside.classList.toggle("inactive");
 
 }
+
+const productList = [];
+
+productList.push({
+    name: 'Muscletech Gold Whey',
+    price: 580.00,
+    image: "https://images-na.ssl-images-amazon.com/images/I/51N2w3Cj+wL._SX600_.jpg"
+})
+productList.push({
+    name: 'GAT Testrol Gold',
+    price: 230.00,
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51BplV3U-CL._SX600_.jpg'
+})
+productList.push({
+    name: 'C4 Extreme',
+    price: 395.00,
+    image: 'http://cdn.shopify.com/s/files/1/1492/2278/products/c4-extreme-30sv-ibr.jpg?v=1652397847'
+})
+
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>
+*/
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image)
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText = "Q. " + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCard);
+        
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
