de2cbd1b0f712cc9e7781035f6cc13293d7991c8
Jason <jasongiovany@gmail.com>
2022-10-28 12:01:51 -0600

    Updating cart, new styles

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-const navEmail = document.querySelector(".navbar-email");
+const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-navEmail.addEventListener("click", toggleDesktopMenu);
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if(!isAsideClose){
+        aside.classList.add("inactive");
+    }
+    mobileMenu.classList.toggle("inactive");
+}
+function toggleCarritoAside(){
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClose){
+        mobileMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
+
 }
