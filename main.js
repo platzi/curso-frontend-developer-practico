dd5d24ac63852bcdd17a206e6091f8f48ef49f69
jcoime8 <jcoime8@gmail.com>
2022-10-28 22:27:20 -0500

    Menu funcional Mobil

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const email = document.querySelector(".navbar-email");
-const menuDesktop = document.querySelector(".desktop-menu")
+const menuDesktop = document.querySelector(".desktop-menu");
+const mobilMenu = document.querySelector(".mobile-menu");
+const menuHamlist = document.querySelector(".menu");
 email.addEventListener('click', toggleDesktopMenu);
+menuHamlist.addEventListener('click',toggleMobilMenu);
 
 function toggleDesktopMenu(){
     menuDesktop.classList.toggle('inactive');
+}
+
+function toggleMobilMenu(){
+    mobilMenu.classList.toggle('inactive');
 }
