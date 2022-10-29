b2cc18ce77bbe2cdc5c968d47298575639cbe27c
Jacobo giraldo <jacobitogiraldo3@gmail.com>
2022-10-28 19:10:27 -0500

    Agregamos las funcionalidades del menu mobile

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+const menu = document.querySelector(".menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
 
@@ -7,3 +9,8 @@ function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive")
 }
 
+menu.addEventListener("click", toggleMobileMenu)
+
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle("inactive")
+}
