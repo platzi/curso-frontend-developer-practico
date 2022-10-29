020443e93293f5f4c4826cc4acaccb054b061dfa
yilver7u7 <yilversgg@gmail.com>
2022-10-29 16:31:43 -0500

    Hasta ahora el maquetado parece estar bien solo falta ralizar los append correctamente

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const icon = document.querySelector(".icon");
 const mobileMenu = document.querySelector(".mobile-menu");
 const iconCar = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 
 
@@ -44,4 +45,77 @@ function showMobileMenu(){
         aside.classList.add("inactive");
         mobileMenu.classList.toggle("activo");
     }
+}
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "XBOX",
+    price: 500,
+    image: "/img/xbox.jpg",
+});
+productList.push({
+    name: "Compu",
+    price: 7000,
+    image: "/img/portatil.jpg",
+})
+
+/*
+<div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>
+*/
+
+
+for (const product of productList) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    //Ahora le creamos la etiqueta de imagen para que ella se encuenetre dentro de nuestro div
+    const img = document.createElement("img");
+    img.setAttribute("src",product.image);
+
+
+    //Creamos el siguiente div product-info
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-Info");
+    
+
+    //div vacio con el cotenido de nuestros productos
+    const productInfoDiv = document.createElement("div");
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$"+ product.price;
+    const productname = document.createElement("p");
+    productPrice.innerText = product.name;
+    
+    
+    //Cramos nuestro figure
+    const figure = document.createElement("figure");
+    const figureImg = document.createElement("img");
+    figureImg.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    
+    //Terminamos el maquetado insertando en donde correspondan cada etiqueta
+    /*figure.appendChild(figureImg);
+    productInfoDiv.append(productPrice, productname);
+    productInfoDiv.appendChild(figure)
+    productInfo.append(productInfoDiv, figure);
+    productCard.append(img,productInfo);
+    cardsContainer.appendChild(productCard);*/
+    productInfoDiv.appendChild(figureImg);
+    
+
 }
