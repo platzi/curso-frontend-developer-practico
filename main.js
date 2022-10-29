44ca8c66827097a7b3146ce204dee8aba77b1104
Jacobo giraldo <jacobitogiraldo3@gmail.com>
2022-10-28 21:35:41 -0500

    Agregamos nuevas funciones del carrito de compras de la web

--- main.js
+++ main.js
@@ -2,15 +2,41 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const menu = document.querySelector(".menu")
-
+const shoppingCart = document.querySelector("#carrito")
+const asideOrders = document.querySelector(".product-detail")
 menuEmail.addEventListener("click", toggleDesktopMenu)
 
 function toggleDesktopMenu(){
+    const isAsideClose = asideOrders.classList.contains("inactive")
+
+    if (!isAsideClose){
+      asideOrders.classList.add("inactive")
+      }
     desktopMenu.classList.toggle("inactive")
+    
 }
 
 menu.addEventListener("click", toggleMobileMenu)
 
 function toggleMobileMenu(){
-  mobileMenu.classList.toggle("inactive")
+    const isAsideClose = asideOrders.classList.contains("inactive")
+    
+    if (!isAsideClose){
+      asideOrders.classList.add("inactive")
+    }
+    mobileMenu.classList.toggle("inactive")
+}
+
+shoppingCart.addEventListener("click", toggleAsideOrders)
+
+function toggleAsideOrders(){
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive")
+    }
+
+    asideOrders.classList.toggle("inactive")
+    
+
 }
