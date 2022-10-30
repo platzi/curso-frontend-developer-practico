dd177137e41c3282d0693a36d7c6d4310726b42a
Luis Tabares <tabaresluis@hotmail.com>
2022-10-30 13:27:59 -0400

    se agrego order list funcionalidad y lista de productos

--- main.js
+++ main.js
@@ -2,14 +2,28 @@ const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu= document.querySelector('.desktop-menu')
 const mobileMenu= document.querySelector('.mobile-menu')
 const menuHamb = document.querySelector('.menu')
+const orderList = document.querySelector('.product-detail')
+const shoppingCartImg = document.querySelector('.shopping-cart-img')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamb.addEventListener('click', toggleHambMenu)
+shoppingCartImg.addEventListener('click', toggleOrderList)
+
+function toggleOrderList(){
+    orderList.classList.toggle('inactive')
+    desktopMenu.classList.add('inactive')
+    mobileMenu.classList.add('inactive')
+   
+}
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+    orderList.classList.add('inactive')
+    
 }
 function toggleHambMenu(){
     mobileMenu.classList.toggle('inactive')
+    orderList.classList.add('inactive')
+    
 }
 
