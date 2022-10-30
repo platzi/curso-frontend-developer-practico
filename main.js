fb107f9e7ce289efec389e2f79b8a235d39fdd1e
Sebastian Maria <sebastianalejandromaria@gmail.com>
2022-10-30 10:11:49 -0300

    Agregamos funciones en js para abrir y cerrar solapas de carrito y menu, y que estas no se superpongan

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Funciones para escuchar click en menu mobile y MyAccount
 const navbarEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const mobileMenuIcon = document.querySelector(".menu")
@@ -9,9 +10,36 @@ mobileMenuIcon.addEventListener('click', toggleMobileMenu);
 
 
 function toggleDesktopMenu(){
+    const isAsideShopCartClosed = asideShopCart.classList.contains('inactive')
+
+    if (!isAsideShopCartClosed){
+        asideShopCart.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isAsideShopCartClosed = asideShopCart.classList.contains('inactive')
+
+    if (!isAsideShopCartClosed){
+        asideShopCart.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+// Funciones para escuchar click en carrito de compras
+const shopCartImg = document.querySelector(".navbar-shopping-cart")
+const asideShopCart = document.querySelector(".product-detail")
+
+shopCartImg.addEventListener('click', toggleShopCart);
+
+function toggleShopCart(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    asideShopCart.classList.toggle('inactive');
 }
