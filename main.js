19b4481cadf994e9d2e36f7583bb213ed026f373
eduardo cano <eduardo_cano_perez@live.com.mx>
2022-10-31 13:45:28 -0600

    se modifican el produc-card y produc-container, para agregar productos desde el archivo main.js, se crea un ciclo for para agregar productos automaticos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -38,4 +39,56 @@ function toggleCarritoAside() {
     }
 
     aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'smartTV',
+    price: 320,
+    image: 'https://images.pexels.com/photos/6976094/pexels-photo-6976094.jpeg?cs=srgb&dl=pexels-koolshooters-6976094.jpg&fm=jpg'
+})
+
+productList.push({
+    name: 'PC',
+    price: 620,
+    image: 'https://images.pexels.com/photos/792199/pexels-photo-792199.jpeg?cs=srgb&dl=pexels-nao-triponez-792199.jpg&fm=jpg&_gl=1*1k4hk73*_ga*MTU4MzczNTQxLjE2NjcwMDY3MjY.*_ga_8JE65Q40S6*MTY2NzAwNjczMC4xLjEuMTY2NzAwNjkyNi4wLjAuMA..'
+})
+
+for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card')
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    
+    productInfoFigure.append(productImgCart);
+
+    productInfoDiv.append(productPrice, productName);
+
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImg, productInfo);
+
+    cardsContainer.append(productCard);
 }
