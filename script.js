ec505a4436f7c3ba0f90d300f7e5016520e5198a
protondk <edgarjosue2911@gmail.com>
2022-10-30 22:16:42 -0600

    Finished product list

--- script.js
+++ script.js
@@ -2,15 +2,144 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toggleCartAside)
 
+/**
+ * "If the aside is not closed, close it, then toggle the desktop menu."
+ */
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
+//Here below, inside the functions, we have the conditionals that allow for the mobileMenu and the aside to exchange states. For example if mobileMenu is open, and we try to open aside, mobileMenu will close and aside will open.
+
+
+/**
+ * If the aside is not closed, add the inactive class to it. Then toggle the inactive class on the
+ * mobile menu
+ */
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive'); 
 }
+
+/**
+ * If the mobile menu is open, close it, then toggle the cart aside
+ */
+function toggleCartAside (){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive'); 
+}
+
+// Here below we have the code to create one card and add products to them with arrays
+
+const productList = [];
+
+productList.push({
+    name: 'iPhone 14 Pro Max',
+    price: 1199.99,
+    image: 'https://cdn.shopify.com/s/files/1/1043/3082/products/iPhone_14_Pro_Max_Silver_PDP_Image_Position-2_Design_SEA_1800x1800.jpg?v=1662620837'
+},
+{
+    name: 'Samsung - 75" Smart TV',
+    price: 599.99,
+    image: 'https://cdn.elchangarro.com.sv/image/uploads/ec/105633-92645234_0138972398.jpg?quality=40&width=1000&height=1000'
+},
+{
+    name: 'Macbook Pro',
+    price: 1500,
+    image: 'https://www.nexus.com.bd/images/detailed/5/Apple_MacBook_Pro_Core_i9_9th_Gen_16GB_RAM_1TB_Space_Gray.jpg'
+},
+{
+    name: 'Play Station 5',
+    price: 499.99,
+    image: 'https://media.direct.playstation.com/is/image/sierialto/PS5-front-with-dualsense'
+},
+{
+    name: 'Nintendo Switch OLED',
+    price: 349.99,
+    image: 'https://media.gamestop.com/i/gamestop/11149258'
+},
+{
+    name: 'Apple iPad',
+    price: 329.99,
+    image: 'https://ie.selectonline.com/media/catalog/product/cache/bb9e1b18b231b11cd7447bbf02a454cb/i/P/iPad_10.2_inch_Wi-Fi_Silver_PDP_Image_Position-1__GBEN.jpg'
+},
+{
+    name: 'Samsung Galaxy S22 Ultra',
+    price: 999.99,
+    image: 'https://buketomnisportpweb.s3.us-east-2.amazonaws.com/seo/AAOQqPIq2bFSICu02c9XZKLRHBDqMrZxeGxvMuWP.png'
+});
+
+
+function renderProducts(array){
+/* Creating a card for each product in the productList array. */
+for (product of array){
+    //Creating each HTML element and adding classes, text and sources.
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    const productInfoFigure = document.createElement('figure');
+
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+    // Placing each created element inside its container
+    productInfoDiv.append(productPrice, productName);
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImg, productInfo);
+
+    cardsContainer.appendChild(productCard);
+}
+}
+
+renderProducts(productList);
+
+
