69a4a10509e1186c08c29865a13fc9ef9360da8d
Franklin Perez <mifannt@gmail.com>
2022-10-31 17:55:26 -0400

    Función para desplegar desktop-menu en navbar rigth

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ const desktopMenu = document.querySelector('.desktop-menu');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 
+//Así lo había escrito 
+/* function toogleDesktopMenu(){
+   if (desktopMenu.classList.contains('inactive')){
+    desktopMenu.classList.remove('inactive')
+    } else {
+    desktopMenu.classList.add('inactive');
+   }     
+}; */
+
+//Así lo escribió juandc
 function toogleDesktopMenu(){
-    console.log('click');
-if (desktopMenu.classList.contains){
-    desktopMenu.classList.remove('.inactive')
-} else {
-    desktopMenu.classList.add('.inactive');
-}
+    desktopMenu.classList.toggle('inactive');
 };
