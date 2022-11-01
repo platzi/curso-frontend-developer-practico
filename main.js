5571ddbe8bcc4df30077f50358f859044d3cdd55
esteban <esteban2002mesa@gmail.com>
2022-11-01 11:44:20 -0500

    aside agregado y los menús se cierran automáticamente

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
 
 const burgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenu.addEventListener("click", toggleMobileMenu);
+menuCarIcon.addEventListener("click", toggleCarAside);
 
 function toggleDesktopMenu() {
+    closeOthermenu(aside);
+
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+    closeOthermenu(aside);
+
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarAside() {
+    closeOthermenu(desktopMenu);
+    closeOthermenu(mobileMenu);
+
+    aside.classList.toggle("inactive");
+}
+
+function closeOthermenu(menu) {
+    if (!menu.classList.contains("inactive")) {
+        menu.classList.add("inactive");
+    }
 }
