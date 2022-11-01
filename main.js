6d61ffe3d607c844507c9fc49c4ba10232045d3a
estebanbus <estebanjbustamante@gmail.com>
2022-11-01 19:00:20 -0400

    Fusion carrito de compras

--- main.js
+++ main.js
@@ -1,8 +1,77 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuIcon = document.querySelector('.menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
 
     desktopMenu.classList.toggle('inactive');
 } 
+
+function toggleMobileMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive');
+} 
+
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.toggle('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    } if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+const productList = []
+productList.push({
+    name: 'Bike',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pantalla',
+    price: '220',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Laptop',
+    price: '350',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+{/* <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+        <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+        </div>
+        <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div> */}
+
+for (product of productList) {
+    document.createElement()
+}
