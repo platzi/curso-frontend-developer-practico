017f5f68d315df5d4803a5dc4acd202422279ed2
Dann1140 <danielcan.16@gmail.com>
2022-11-02 13:56:14 -0500

    added products in the main page and render function util

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const iconMenuToggler = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const cartIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 function toggleDesktopMenu() {
   const isAsideMenuOn = !aside.classList.contains("inactive");
@@ -26,7 +27,66 @@ function toggleCartAside() {
   }
   aside.classList.toggle("inactive");
 }
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
 
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = `$${product.price}`;
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement("figure");
+
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfoDiv.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Electric Bike",
+  price: 320,
+  image:
+    "https://images.pexels.com/photos/11074451/pexels-photo-11074451.jpeg?cs=srgb&dl=pexels-gonzalo-carlos-novillo-lapeyra-11074451.jpg&fm=jpg&_gl=1*q9m55*_ga*ODgxOTI3OTIwLjE2NjYyMjg5MzQ.*_ga_8JE65Q40S6*MTY2NzQxNDU3NS4zLjEuMTY2NzQxNDYwNi4wLjAuMA..",
+});
+productList.push({
+  name: "Carbon Fiber Bike",
+  price: 520,
+  image:
+    "https://images.pexels.com/photos/11074447/pexels-photo-11074447.jpeg?cs=srgb&dl=pexels-gonzalo-carlos-novillo-lapeyra-11074447.jpg&fm=jpg&_gl=1*nzptz9*_ga*ODgxOTI3OTIwLjE2NjYyMjg5MzQ.*_ga_8JE65Q40S6*MTY2NzQxNDU3NS4zLjEuMTY2NzQxNDY2MC4wLjAuMA..",
+});
+
+renderProducts(productList);
 menuEmail.addEventListener("click", toggleDesktopMenu);
 iconMenuToggler.addEventListener("click", toggleMobileMenu);
 cartIcon.addEventListener("click", toggleCartAside);
