5e0902fbd5a8dee1a998bcf8032f54bc5c3761ac
mifannt <mifannt@gmail.com>
2022-11-02 16:04:36 -0400

    Fusión de Aside Menu y lógica de menus

--- main.js
+++ main.js
@@ -2,10 +2,23 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 
 menu.addEventListener('click', toogleMobileMenu);
+
+menuCarritoIcon.addEventListener('click', toogleCarritoMenu);
+
+//Así lo escribió juandc
+function toogleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
+};
 
 //Así lo había escrito 
 /* function toogleDesktopMenu(){
@@ -16,10 +29,19 @@ menu.addEventListener('click', toogleMobileMenu);
    }     
 }; */
 
-//Así lo escribió juandc
-function toogleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
-};
 function toogleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+};
+
+function toogleCarritoMenu(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive'); 
+    const desktopMenuClosed = desktopMenu.classList.contains('inactive');
+    if(!desktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 };
