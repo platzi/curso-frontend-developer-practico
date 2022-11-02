8dbc86d85da636299160f594f3438cd19a21953f
Dann1140 <danielcan.16@gmail.com>
2022-11-01 19:47:22 -0500

    fusionando navbar y modulo de menú

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle("inactive");
+}
+menuEmail.addEventListener("click", toggleDesktopMenu);
