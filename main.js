ec98ae2aa7295e3d376aefde57fdfb9f26904ca1
Dann1140 <danielcan.16@gmail.com>
2022-11-01 20:22:40 -0500

    aÃdded mobile menu with media queries

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const iconMenuToggler = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
 }
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+}
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
+iconMenuToggler.addEventListener("click", toggleMobileMenu);
