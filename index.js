5d4675f38b6c7ad22e664d59c27c15ab9cf65c7f
Diana Castaño <dianaczapata@gmail.com>
2022-11-01 23:54:06 -0500

    modificacion de las funciones  toggleMobileMenu y toggleAside

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const menuEmail= document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const HamMenuIcon= document.querySelector(".menu")
+const carMenuIcon= document.querySelector(".navbar-shopping-cart")
 const mobileMenu= document.querySelector(".mobile-menu")
+const aside= document.querySelector(".product-detail")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
 HamMenuIcon.addEventListener("pointerenter", toggleMobileMenu)
+carMenuIcon.addEventListener("pointerenter", toggleAside)
 
 function toggleDesktopMenu(){
+    const isAsideClosed= aside.classList.contains("inactive")  
+
+     if(!isAsideClosed){
+        aside.classList.add("inactive")
+    }
     desktopMenu.classList.toggle("inactive")
 }
 
 function toggleMobileMenu(){
+     const isAsideClosed= aside.classList.contains("inactive")  
+
+     if(!isAsideClosed){
+        aside.classList.add("inactive")
+    }
+
     mobileMenu.classList.toggle("inactive")
+}
+
+function toggleAside(){
+    const isMobileMenuClosed= mobileMenu.classList.contains("inactive")  
+
+     if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive")
+    }
+
+    aside.classList.toggle("inactive")
 }
