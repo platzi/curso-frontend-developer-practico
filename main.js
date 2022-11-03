5c67283c132ddba9d14d5518d4fc88401d1af818
Daniel Rubiano <danielrubianomeneses@gmail.com>
2022-11-03 12:07:15 -0500

    funcionalidad de los botones del menu y carrito de compras com js

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenuMobile = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const iconCarrito = document.querySelector('.navbar-shopping-cart')
+const orderCarrito = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+iconMenuMobile.addEventListener('click', toggleMobileMenu);
+iconCarrito.addEventListener('click', toggleIconCarrito);
+
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+    const isordercarritoClosed = orderCarrito.classList.contains('inactive');
 
+    if (!isordercarritoClosed){
+        orderCarrito.classList.add('inactive')
+    };   
+    desktopMenu.classList.toggle('inactive')
+};
+
+function toggleMobileMenu(){
+    const isordercarritoClosed = orderCarrito.classList.contains('inactive');
+
+    if (!isordercarritoClosed){
+        orderCarrito.classList.add('inactive')
+    };
+    mobileMenu.classList.toggle('inactive')
+};
+
+function toggleIconCarrito(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuclosed = desktopMenu.classList.contains('inactive')
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    };
+    if (!isDesktopMenuclosed){
+        desktopMenu.classList.add('inactive')
+    }
+    orderCarrito.classList.toggle('inactive')
 }
