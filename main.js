310435822f935b6e7fad91896d114bc4c34ea266
sac-ctl2017 <ing_steven@hotmail.es>
2022-11-04 08:07:23 -0500

    Creando y Empauqetando etiquetas HTML desde JS uso de ciclos para crear codigo HTML

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 
 const menuCarIcon = document.querySelector('.navbar-shopping-cart');
 const asideCar = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 
@@ -35,7 +36,13 @@ function toggleMobileMenu(){
     asideCar.classList.add('inactive');
 }
 
+// El ejercicio lo realicé antes de terminr la clase por eso salio sin el metodo contain del atributo class
+// Pendiente de Modificar
 
+/*  
+    desktopMenu.classList.contain('inactive')
+
+*/ 
 function toggleDesktopMenu(){
     //Asi lo realizó el teacher.
     //desktopMenu.classList.toggle('inactive');
@@ -52,3 +59,88 @@ function toggleDesktopMenu(){
 
     
 }
+
+const productList = [];
+console.log(productList)
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+
+})
+
+console.log(productList)
+productList.push({
+    name: 'Scooter',
+    price: 300,
+    image: 'https://images.squarespace-cdn.com/content/v1/5defdaadc0d6df613ed4c82f/9aa3e754-987d-4fd4-a49a-815b4a7e3ca2/2022+SP+scooter+studio+photos+no+background.png?format=500w',
+
+})
+productList.push({
+    name: 'Motorcycle',
+    price: 1200,
+    image: 'https://i1.wp.com/jolta.com/wp-content/uploads/2021/10/MotoTec_36v-500w_ElectricBike4.jpeg?fit=1000%2C750&ssl=1',
+
+})
+
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+    <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+    </div>
+    <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+</div>
+</div>
+
+*/
+
+function renderProducts (arrays){
+    for (item of arrays) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const img = document.createElement('img')
+        img.setAttribute('src',item.image)
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info');
+    
+    
+        // Div pra el precio y el Nombre --> productPrice y productName
+        const divProductInfo = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerHTML = `$ ${item.price}`
+    
+        const productName = document.createElement('p');
+        productName.innerHTML = `${item.name}`
+    
+        // Empaquetado de las etiquet P en el Div de divProductInfo
+        divProductInfo.append(productPrice,productName)
+    
+        // Figure para  --> imgBtn
+        const productFigures = document.createElement('figure');
+       
+        const imgBtn = document.createElement('img')
+        imgBtn.setAttribute('src','./icons/bt_add_to_cart.svg')
+    
+        // Empaquetado de imgBtn en productFigures
+        productFigures.appendChild(imgBtn)
+    
+        // Empaquetado de productInfo --> divProductInfo y productFigures
+        productInfo.append(divProductInfo,productFigures)
+    
+        // Empaquetado del productCard --> productInfo e img 
+        productCard.append(img,productInfo)
+    
+        // Empaquetado del contenedor cardsContainer --> productCard
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
