c3a0f21757ebcf38cf720b3bdbff21be6ed42771
sac-ctl2017 <ing_steven@hotmail.es>
2022-11-03 22:08:05 -0500

    Modificando el menu del car shop utilizando lo aprendido en clase, arreglando errores pasados

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
+/* Seleciono elementos del menu desktop */
 const menuEmail = document.querySelector('.navbar-email'); 
 const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamburguer = document.querySelector('.menuHamburguer');
+
+
+/* Seleciono elementos del menu Mobile */
+
+const menuHamburguer = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+
+
+/* Seleciono elementos del Shopping Car */
+
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const asideCar = document.querySelector('.product-detail');
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguer.addEventListener('click',toggleMobileMenu);
+menuCarIcon.addEventListener('click',toggleAsideMenu);
+
+
+function toggleAsideMenu(){
+    asideCar.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive')  
+    desktopMenu.classList.add('inactive');  
+}
+
 
 // Esta si la realicé como lo explicó el teacher, 
 //Le puse el cursor en mobile pero me parece que no va.
-function toggleMobileMenu(){
- mobileMenu.classList.toggle('inactive');    
+function toggleMobileMenu(){    
+    mobileMenu.classList.toggle('inactive');        
+    asideCar.classList.add('inactive');
 }
 
 
@@ -20,6 +43,7 @@ function toggleDesktopMenu(){
     //console.log(toggleClass)
     if (toggleClass === 'desktop-menu inactive') {
         desktopMenu.classList.remove('inactive');
+        asideCar.classList.add('inactive');
     }
 
     if (toggleClass === 'desktop-menu') {
