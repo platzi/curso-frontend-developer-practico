f26e8b51df157369562741f826a0c3236f0e7b8b
sac-ctl2017 <ing_steven@hotmail.es>
2022-11-03 20:50:53 -0500

    Modificando el Menu para Mbile aplicando los conceptos de la ultima clase

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email'); 
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburguer = document.querySelector('.menuHamburguer');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguer.addEventListener('click',toggleMobileMenu);
 
+// Esta si la realicé como lo explicó el teacher, 
+//Le puse el cursor en mobile pero me parece que no va.
+function toggleMobileMenu(){
+ mobileMenu.classList.toggle('inactive');    
+}
 
 
 function toggleDesktopMenu(){
