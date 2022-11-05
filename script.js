ff2ba140dfbe9a580607831d0611ca1fc35fa4d0
Cristian Javier <javierculgua@gmail.com>
2022-11-05 10:32:56 -0600

    integramos em menu mobile y restingimos su vista desde desktop

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const menuemail = document.querySelector('.navbar-email');
+const desktopmenu = document.querySelector('.desktop-menu');
+const menuhamicon = document.querySelector('.menu');
+const mobilemenu = document.querySelector('.mobile-menu');
+
+
 menuemail.addEventListener('click', toggledesktopmenu);
-const desktopmenu = document.querySelector('.desktop-menu')
+menuhamicon.addEventListener('click', togglemobilemenu);
+
 
 function toggledesktopmenu(){
     desktopmenu.classList.toggle('inactive');
+}
+function togglemobilemenu(){
+    mobilemenu.classList.toggle('inactive');
 }
