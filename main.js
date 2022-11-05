0e2f8fb01fbeb3efff18eedb0e0eff95647d1496
fspind71 <fspind@gmail.com>
2022-11-05 18:57:59 -0300

    Lista de productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -1,15 +1,146 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const menuEmail=document.querySelector('.navbar-email');
+const menuHamIcon=document.querySelector('.menu');
+const menuCarritoIcon=document.querySelector('.navbar-shopping-cart');
+const desktopMenu=document.querySelector('.desktop-menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+const aside=document.querySelector('.product-detail');
+const CardsContainer=document.querySelector('.cards-container');
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
-function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive');
+function toggleDesktopMenu(){
+  const isAsideClosed=aside.classList.contains('inactive');
+if(!isAsideClosed){aside.classList.add('inactive');
+}
+desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu() {
-  mobileMenu.classList.toggle('inactive');
+function toggleMobileMenu(){
+  const isAsideClosed=aside.classList.contains('inactive');
+  if(!isAsideClosed){
+    aside.classList.add('inactive');
+}
+mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+  const isMobileMenuClosed=mobileMenu.classList.contains('inactive');
+  if(!isMobileMenuClosed){mobileMenu.classList.add('inactive');
+}
+
+aside.classList.toggle('inactive');
+}
+
+const productList =[];
+productList.push({
+  name: 'Nikon F60',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon FG',
+  price: 160,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon D800',
+  price: 520,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon XR-8',
+  price: 320,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon AF',
+  price: 220,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon D750',
+  price: 820,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon F60',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon FG',
+  price: 160,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon D800',
+  price: 520,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon XR-8',
+  price: 320,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon AF',
+  price: 220,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+productList.push({
+  name: 'Nikon D750',
+  price: 820,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',  
+});
+
+/* <!-- <div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div> 
+*/
+
+for (product of productList) {
+
+  const productCard = document.createElement('div');
+  productCard.classList.add('product-card');
+
+  const productImg =document.createElement('img');
+  productImg.setAttribute('src', product.image);
+
+  const productInfo = document.createElement('div');
+  productInfo.classList.add('product-info');
+
+  const productInfoDiv = document.createElement('div');  
+
+  const productPrice = document.createElement('p');
+  productPrice.innerText = '$' + product.price;
+  const productName = document.createElement('p');
+  productName.innerText = product.name;
+
+  productInfoDiv.appendChild(productPrice);
+  productInfoDiv.appendChild(productName);
+
+  const productInfoFigure = document.createElement('figure'); 
+  const productImgCart = document.createElement('img');
+  productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+  productInfoFigure.appendChild(productImgCart);
+
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  CardsContainer.appendChild(productCard);
+
 }
