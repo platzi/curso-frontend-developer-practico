b523b0b7230f1fe312365f78830f3e3e3e321512
brandongc9911 <brandongc9911@gmail.com>
2022-11-05 21:51:20 -0500

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+const burguerMenu = document.querySelector(".burguerMenu");
+const mobile_menu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", ()=>{
     desktopMenu.classList.toggle("inactive")
+})
+
+burguerMenu.addEventListener("click", ()=>{
+    mobile_menu.classList.toggle("inactive")
 })
