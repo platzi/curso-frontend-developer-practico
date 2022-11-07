da8039d53d40a13289eec156aa2c976594d9dcc7
Víctor A <victor.d.araya@gmail.com>
2022-11-07 04:09:50 -0300

    Se agrega funcionalidad al menu hamburguesa para version mobile, correspondiente al archivo 8

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const memuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHanIcon = document.querySelector("img");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-// Funcion toggle para menu email.
+// Funciones listeners
 memuEmail.addEventListener("click", toggleDesktopMenu);
+menuHanIcon.addEventListener("click", toggleMobileMenu);
+
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
