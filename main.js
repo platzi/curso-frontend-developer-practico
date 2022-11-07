f159dbfa08ee10a608a1a218721ffde81c3cf749
Jorgemanjarrez15 <manjarrezjorge1500@gmail.com>
2022-11-06 21:49:05 -0500

    agregamos el menu de mobile,añadimos un media query y con js asinamos un evento de click al menuhamburger

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burggerMenu =  document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burggerMenu.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('inactive')
+}
 
 function toggleDesktopMenu(){
   desktopMenu.classList.toggle('inactive')
