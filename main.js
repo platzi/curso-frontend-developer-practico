2bb97801f95885351084cc824cb47856f10e39e3
mifannt <mifannt@gmail.com>
2022-11-07 15:49:16 -0400

    Integración de lista de productos

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 
@@ -40,8 +42,103 @@ function toogleMobileMenu(){
 function toogleCarritoMenu(){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive'); 
     const desktopMenuClosed = desktopMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    };
     if(!desktopMenuClosed){
         desktopMenu.classList.add('inactive');
-    }
+    };
     aside.classList.toggle('inactive');
+     
 };
+
+//Para optimizar el html insertaremos los productos desde js
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Guitar',
+    price: 499,
+    image: 'https://m.media-amazon.com/images/I/81iFA1sBZoL.jpg'
+});
+productList.push({
+    name: 'MinilabIV',
+    price: 99,
+    image: 'https://files.soniccdn.com/images/articles/original/42422.jpg'
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Guitar',
+    price: 499,
+    image: 'https://m.media-amazon.com/images/I/81iFA1sBZoL.jpg'
+});
+productList.push({
+    name: 'MinilabIV',
+    price: 99,
+    image: 'https://files.soniccdn.com/images/articles/original/42422.jpg'
+});
+
+
+/*<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>*/
+
+// Escribiendo la etiqutea de productos para insertarla desde JS
+
+
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+        
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+};
+
+renderProducts(productList);
