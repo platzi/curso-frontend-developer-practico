04428b29ea031091d51665bfdf4994b768dfde1d
Jorgemanjarrez15 <manjarrezjorge1500@gmail.com>
2022-11-07 09:45:03 -0500

    vinculamos un aside sobre los productos y creamos un array con un objeto dentro  y del onjeto creamos los productos de la app por medio de manipuelcion del DOM

--- main.js
+++ main.js
@@ -2,14 +2,106 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burggerMenu =  document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burggerMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritosAside);
 
 function toggleMobileMenu(){
+  const isAsideClosed = aside.classList.contains('inactive')
+
+  if(!isAsideClosed){
+    aside.classList.add('inactive')
+  }
   mobileMenu.classList.toggle('inactive')
 }
 
 function toggleDesktopMenu(){
+  const isAsideClosed = aside.classList.contains('inactive')
+
+  if(!isAsideClosed){
+    aside.classList.add('inactive')
+  }
+
   desktopMenu.classList.toggle('inactive')
 }
+
+function toggleCarritosAside(){
+    //pregunatamos si la const mobilemenu tien la clase inactive
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    //preguntamos si la clase ismobilemenu no esta cerrado le agregue la clase inactive
+    if(!isMobileMenuClosed){
+      mobileMenu.classList.add('inactive');
+    }
+  aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+  name: 'bike',
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+productList.push({
+  name: 'PC',
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+productList.push({
+  name: 'TV',
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+//recorrer el array productList
+
+const cartContainer = document.querySelector('.cards-container');
+
+function renderProducts(arr) {
+
+  for (const product of arr) {
+    //crear los elementos
+    const productCart = document.createElement('div');
+    productCart.classList.add('product-card');
+  
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+  
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+  
+    //meter las etiquetas dentro de sus contenedores
+  
+    productInfoFigure.appendChild(productImgCart);
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+    productCart.appendChild(productImg);
+    productCart.appendChild(productInfo);
+  
+    cartContainer.appendChild(productCart);
+  
+  
+  }
+}
+
+renderProducts(productList);
+
