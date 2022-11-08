dfcfc0352c41a921a4a00372f7f660bb2235a647
antonioVargas <vsavila.antonio@gmail.com>
2022-11-08 14:51:06 -0600

    Product cards setted

--- main.js
+++ main.js
@@ -1,8 +1,116 @@
 const emailmenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const mobileMenu = document.querySelector('.mobile-menu');
+const btn_burgerMenu = document.querySelector('.menu');
+
+const shopingCart = document.querySelector('.shopingCart');
+const btn_shoppingCart = document.querySelector('.navbar-shopping-cart');
+
+const productcardContainer = document.querySelector('.cards-container');
+
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+    
+    if (shopingCart.classList.contains('inactive')){
+        
+        desktopMenu.classList.toggle('inactive');
+        
+    } else { 
+        toggleShoppingCart();
+        desktopMenu.classList.toggle('inactive'); }
+        
+    }
+    
+function toggleShoppingCart() {
+        
+        desktopMenu.classList.add('inactive');
+
+
+    if (mobileMenu.classList.contains('inactive')){
+    
+        shopingCart.classList.toggle('inactive');
+
+    } else { 
+        toggleMobileMenu();
+        shopingCart.classList.toggle('inactive'); }
+}
+
+function toggleMobileMenu() {
+    if (shopingCart.classList.contains('inactive')){
+    
+        mobileMenu.classList.toggle('inactive');
+
+    } else { 
+        toggleShoppingCart();
+        mobileMenu.classList.toggle('inactive'); }
 }
 
 emailmenu.addEventListener('click', toggleDesktopMenu );
+btn_burgerMenu.addEventListener('click', toggleMobileMenu );
+btn_shoppingCart.addEventListener('click', toggleShoppingCart );
+
+// Adding PRODUCTOS to the array called productList
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: 12000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Computadora',
+    price: 37000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+// Website Layout HTML written by JavaScript
+
+function renderProducts(productsArray){
+    
+    for (const product of productsArray) {
+        
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        
+        const productImage = document.createElement('img');
+        productImage.setAttribute('src', product.image);
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        
+        const productPriceAndName = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+        
+        const shopCartButton = document.createElement('figure');
+        const shopCartIcon = document.createElement('img');
+        shopCartIcon.setAttribute('src', './icons/bt_add_to_cart.svg');
+        
+        productPriceAndName.append(productPrice, productName);
+        shopCartButton.appendChild(shopCartIcon);
+    
+        productInfo.append(productPriceAndName, shopCartButton);
+    
+        productCard.append(productImage, productInfo);
+    
+        productcardContainer.appendChild(productCard);
+    
+    }
+
+}
+
+renderProducts(productList);
+
+
