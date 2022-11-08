3c0cca8051daf597675a0d5a80ee8baf7c6eea0b
Humberto Vazquez <humbertoipnvz@gmail.com>
2022-11-08 12:22:12 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,138 @@
+const liMenuEmail = document.querySelector(".navbar-email");
+const divDesktopMenu = document.querySelector(".desktop-menu");
+
+const imgFaviconMenu = document.querySelector(".menu");
+const divMobileMenu = document.querySelector(".mobile-menu");
+
+const liIconCarrito = document.querySelector(".navbar-shopping-cart");
+const asideProductDetail = document.querySelector(".product-detail");
+
+const divCardsContainer = document.querySelector(".cards-container");
+
+const productList = [];
+
+productList.push({
+    name: "Bike",
+    price: 120,
+    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Computer",
+    price: 500,
+    img: "https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&tinysrgb&h=650&w=940",
+});
+
+productList.push({
+    name: "Mobile Phone",
+    price: 300,
+    img: "https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+    name: "Watch",
+    price: 250,
+    img: "https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+    name: "Sneakers",
+    price: 150,
+    img: "https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+    name: "New Nintendo 3DS XL",
+    price: 180,
+    img: "https://images.pexels.com/photos/3702458/pexels-photo-3702458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+    name: "Headphones",
+    price: 220,
+    img: "https://images.pexels.com/photos/205926/pexels-photo-205926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+    name: "Camera",
+    price: 250,
+    img: "https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+    name: "GameBoy Color",
+    price: 110,
+    img: "https://images.pexels.com/photos/2263816/pexels-photo-2263816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+liMenuEmail.addEventListener("click", toggleDesktopMenu);
+imgFaviconMenu.addEventListener("click", toggleMobileMenu);
+liIconCarrito.addEventListener("click", toggleProductDetail);
+
+function toggleDesktopMenu() {
+    const isAsideProductDetailOpen = !asideProductDetail.classList.contains("inactive");
+
+    if (isAsideProductDetailOpen) {
+        asideProductDetail.classList.toggle("inactive");
+    }
+
+    divDesktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+    const isAsideProductDetailOpen = !asideProductDetail.classList.contains("inactive");
+
+    if (isAsideProductDetailOpen) {
+        asideProductDetail.classList.toggle("inactive");
+    }
+    divMobileMenu.classList.toggle("slide-right");
+}
+
+function toggleProductDetail() {
+    const isDivDesktopMenuOpen = !divDesktopMenu.classList.contains("inactive");
+    const isDivMobileMenuOpen = divMobileMenu.classList.contains("slide-right");
+
+    if (isDivMobileMenuOpen) {
+        divMobileMenu.classList.toggle("slide-right");
+    }
+
+    if (isDivDesktopMenuOpen) {
+        divDesktopMenu.classList.toggle("inactive");
+    }
+    asideProductDetail.classList.toggle("inactive");
+}
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+
+        const imgProduct = document.createElement("img");
+        imgProduct.setAttribute("src", product.img);
+
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+
+        const productInfoDiv = document.createElement("div");
+
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+
+        const figureProductInfo = document.createElement("figure");
+
+        const productImgCart = document.createElement("img");
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+        productCard.append(imgProduct, productInfo);
+        productInfo.append(productInfoDiv, figureProductInfo);
+        productInfoDiv.append(productPrice, productName);
+        figureProductInfo.append(productImgCart);
+
+        divCardsContainer.append(productCard);
+    }
+}
+
+renderProducts(productList);
