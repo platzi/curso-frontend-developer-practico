91e2be9da47e50b0a8a237ced830d91818c77162
Diego Uriel Alvarez Almanza <diegouriel96@hotmail.com>
2022-11-07 20:56:53 -0600

    Avanze

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const menuEmail  = document.querySelector(".navbar-email")
-const desktopMenu  = document.querySelector(".desktop-menu")
+const menuEmail  = document.querySelector(".navbar-email");
+const desktopMenu  = document.querySelector(".desktop-menu");
+const hamb_menu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu")
+
 
 menuEmail.addEventListener('click',toogleDesktopMenu)
+hamb_menu.addEventListener('click',toogleMobileMenu)
 
 function toogleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function toogleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
