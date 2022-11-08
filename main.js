f4d48161789c623c83fd7236153ce84cdbb93ccf
lestherxm <lestherxm@gmail.com>
2022-11-07 23:40:48 -0600

    adding mobile menu

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-const navbarEmail = document.querySelector('.navbar-email');
+const navbarEmail = document.querySelector('.navbar-email'); //Text-Email que despliega un menu
 const desktopMenu = document.querySelector('.desktop-menu');
+const navbarMobile = document.querySelector('.menu'); // Imagen que despliega el menu en version mobile
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+navbarMobile.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu(){
     // Validar si este elemento contiene la clase especificada
     // True  > elimina
     // False > agrega
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
