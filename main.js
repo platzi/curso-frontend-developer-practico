545732dab23bf27edb37e6c21e39dba3bbab8067
Javier Cepeda <erjavierc@gmail.com>
2022-11-08 08:07:58 -0500

    toggleMenu

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-
+const menuCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
