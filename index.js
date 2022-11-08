6b8d9304ff7b516e4d11cad5de9d0b6aaf571194
sergioguzman <sergio.guzman1905@gmail.com>
2022-11-08 11:44:55 -0500

    adding menus and event driven in the .js

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 console.log('connected');
 
+// get elements of the html.
 mailButton = document.getElementById('email');
 AccountMenu = document.getElementById('accountmenu');
+hamMenu = document.getElementById('hamMenu');
+mobileMenu = document.getElementById('mobileMenu');
+shoppingCart = document.getElementById('shoppingCart')
+asideMen = document.getElementById('carAside');
 
-mailButton.addEventListener('click', toggleMenu)
+//create events listeners
+mailButton.addEventListener('click', toggleMenu);
+hamMenu.addEventListener('click', toggleMobile);
+shoppingCart.addEventListener('click', toggleShoppingCart);
 
+//create functions.
 function toggleMenu() {
-    AccountMenu.classList.toggle('inactive')
+    AccountMenu.classList.toggle('inactive');
+}
+
+function toggleMobile() {
+    let isShoppingCarClose = asideMen.classList.contains('inactive');
+    
+    if (!isShoppingCarClose) {
+        asideMen.classList.toggle('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart() {
+    let isMobileMenuClose = mobileMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClose) {
+        mobileMenu.classList.toggle('inactive');
+    }
+    asideMen.classList.toggle('inactive');
 }
