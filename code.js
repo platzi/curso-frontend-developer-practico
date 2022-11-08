f53172b5763b24dc339916ff221962a7b1e17ab4
frederic-morales <toko2005@DESKTOP-FGEL5IR.localdomain>
2022-11-08 17:19:22 -0600

    Fusion del menu en mobile 08 11 2022

--- code.js
+++ code.js
@@ -1,7 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+
+
+function toggleMobileMenu(){
+   console.log("click en menu mobile");
+   mobileMenu.classList.toggle("inactive");     
+};
 
 function toggleDesktopMenu(){
    desktopMenu.classList.toggle("inactive");     
