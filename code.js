06658d2d7f2419e68922feb14a1be73d09691dbb
frederic-morales <toko2005@DESKTOP-FGEL5IR.localdomain>
2022-11-09 17:31:27 -0600

    Agregando el Aside y haciendo que cuando le de click a un menu se cierren los demas 09 11 2022

--- code.js
+++ code.js
@@ -2,18 +2,53 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 
 function toggleMobileMenu(){
-   console.log("click en menu mobile");
+
+      //cerrar el Aside al abrir el Mobile Menu
+
+   const isAsideClosed = aside.classList.contains("inactive");
+   if(!isAsideClosed){
+      aside.classList.add("inactive");
+   }
    mobileMenu.classList.toggle("inactive");     
 };
 
+
 function toggleDesktopMenu(){
+
+      //cerrar el Aside al abrir el Desktop Menu
+
+   const isAsideClosed = aside.classList.contains("inactive");
+   if(!isAsideClosed){
+      aside.classList.add("inactive");
+   }
    desktopMenu.classList.toggle("inactive");     
 };
 
+
+function toggleCarritoAside(){
+   
+      //cerrar el Mobile menu al abrir el Aside
+
+   const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+   if(!isMobileMenuClosed){
+      mobileMenu.classList.add("inactive");
+   }
+
+      //cerrar el Menu desktop al abrir el Aside
+
+   const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+   if(!isDesktopMenuClosed){
+      desktopMenu.classList.add("inactive");
+   }
+
+   aside.classList.toggle("inactive");
+};
