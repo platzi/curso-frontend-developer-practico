a189bc48910b7f4810488f91819158867a177cc8
joseangelayalaz <ayalazjangel@gmail.com>
2022-11-10 08:34:35 -0600

    some changes on menu

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,31 @@
 const emailNavbar = document.querySelector(".navbar-email");
 const menuDesktop = document.querySelector(".desktop-menu");
+const menuHam = document.querySelector(".menu");
+const menuMobile = document.querySelector(".mobile-menu");
+const carritoMenu = document.querySelector(".carrito");
+const productDetail = document.querySelector(".product-detail");
+
+
 
 emailNavbar.addEventListener("click", toggleDesktopMenu);
+menuHam.addEventListener("click", toggleMobileMenu);
+carritoMenu.addEventListener("click", toggleCarritoMenu);
 
 function toggleDesktopMenu() {
     menuDesktop.classList.toggle("inactive");
+
 }
 
-console.log("Hola Mundo")
+function toggleMobileMenu() {
+    menuMobile.classList.toggle("menu-inactive");
+}
+function toggleCarritoMenu(){
+    const isMobileMenuClose = menuMobile.classList.contains("menu-inactive");
+
+    if (!isMobileMenuClose) {
+        menuMobile.classList.add("menu-inactive");
+    }
+
+    productDetail.classList.toggle("inactive");
+
+}
