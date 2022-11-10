3aa354d0c50e5feab8916bbfa720b1d696ab40dc
Felipe <felipepchv@gmail.com>
2022-11-10 17:04:33 -0600

    Menu mobile implementado

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Toogle del desktopmenu en el menuEmail
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
@@ -5,4 +6,14 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+//Toogle del HamMenu en el Icon
+const icon = document.querySelector('.menu');
+const HamMenu = document.querySelector('.mobile-menu');
+
+icon.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu(){
+    HamMenu.classList.toggle('inactive');
 }
