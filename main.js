56b7a0ef2749e8eed6976c3d332821c261374f9b
Juan Sebastian Paez Medina <juanpaez_9011@hotmail.com>
2022-11-10 08:44:11 +0100

    Solucion de colisiones con producto individual

--- main.js
+++ main.js
@@ -3,107 +3,50 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const aside = document.querySelector('.product-detail');
 const cardsContainer = document.querySelector('.cards-container');
-// const productDetailInd = document.querySelector('.product-detail-individual');
+const productDetailInd = document.querySelector('.product-detail-individual');
 
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 carIcon.addEventListener('click', toggleCarAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAsai);
 
 function toggleDesktopMenu() {
+    productDetailInd.classList.add('inactive');
     aside.classList.add("inactive");
     desktopMenu.classList.toggle("inactive");
 };
 
 function toggleMobileMenu() {
+    productDetailInd.classList.add('inactive');
     aside.classList.add("inactive");
     mobileMenu.classList.toggle("inactive");
 };
 
 function toggleCarAside() {
+    productDetailInd.classList.add('inactive');
     mobileMenu.classList.add("inactive");
     desktopMenu.classList.add("inactive");
     aside.classList.toggle("inactive");
 };
 
+function openProductDetailAsai() {
+    aside.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
+    productDetailInd.classList.remove('inactive')
+}
+
+function closeProductDetailAsai() {
+    productDetailInd.classList.add('inactive');
+}
+
+
+
 const productList = [];
-productList.push({
-    name: "bike",
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "pantalla",
-    price: 220,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "laptop",
-    price: 820,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-});
-productList.push({
-    name: "bike",
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "pantalla",
-    price: 220,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "laptop",
-    price: 820,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-});
-productList.push({
-    name: "bike",
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "pantalla",
-    price: 220,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "laptop",
-    price: 820,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-});
-productList.push({
-    name: "bike",
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "pantalla",
-    price: 220,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "laptop",
-    price: 820,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-});
-productList.push({
-    name: "bike",
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "pantalla",
-    price: 220,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "laptop",
-    price: 820,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-});
 productList.push({
     name: "bike",
     price: 120,
@@ -128,6 +71,7 @@ function renderProducts(arr) {
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAsai);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
