d51228a0f50f344cb47f67ad6de3e9a17262eff5
joseangelayalaz <ayalazjangel@gmail.com>
2022-11-10 12:40:18 -0600

    Open the menu and make them close in the moment you open an other one

--- JS/main.js
+++ JS/main.js
@@ -12,19 +12,33 @@ menuHam.addEventListener("click", toggleMobileMenu);
 carritoMenu.addEventListener("click", toggleCarritoMenu);
 
 function toggleDesktopMenu() {
+    const isProductDetailDesktopClose = productDetail.classList.contains("inactive");
+    if (!isProductDetailDesktopClose) {
+        productDetail.classList.add("inactive");
+    }
+
     menuDesktop.classList.toggle("inactive");
 
 }
 
 function toggleMobileMenu() {
+    const isProductDetailClose = productDetail.classList.contains("inactive");
+    if (!isProductDetailClose) {
+        productDetail.classList.add("inactive");
+    }
+
     menuMobile.classList.toggle("menu-inactive");
 }
 function toggleCarritoMenu(){
     const isMobileMenuClose = menuMobile.classList.contains("menu-inactive");
+    const isMenuDesktopClose = menuDesktop.classList.contains("inactive");
 
     if (!isMobileMenuClose) {
         menuMobile.classList.add("menu-inactive");
     }
+   if (!isMenuDesktopClose) {
+        menuDesktop.classList.add("inactive");
+   }
 
     productDetail.classList.toggle("inactive");
 
