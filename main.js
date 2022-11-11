36495f5d019bdb13c8a4240939c572b915d3b27c
mifannt <mifannt@gmail.com>
2022-11-11 09:22:35 -0400

    Identación del código

--- main.js
+++ main.js
@@ -36,11 +36,11 @@ function toogleDesktopMenu(){
 
 //Así lo había escrito 
 /* function toogleDesktopMenu(){
-   if (desktopMenu.classList.contains('inactive')){
+if (desktopMenu.classList.contains('inactive')){
     desktopMenu.classList.remove('inactive')
     } else {
     desktopMenu.classList.add('inactive');
-   }     
+}     
 }; */
 
 function toogleMobileMenu(){
@@ -129,13 +129,13 @@ productList.push({
 /*<div class="product-card">
 <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
 <div class="product-info">
-  <div>
+<div>
     <p>$120,00</p>
     <p>Bike</p>
-  </div>
-  <figure>
+</div>
+<figure>
     <img src="./icons/bt_add_to_cart.svg" alt="">
-  </figure>
+</figure>
 </div>
 </div>*/
 
