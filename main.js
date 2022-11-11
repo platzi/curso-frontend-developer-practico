02d92ca5de95e2069e2ebd451efd402dd3f1b017
Daniel Alejandro Leon Ortiz <dalobusiness01@gmail.com>
2022-11-11 15:53:40 -0500

    Finished navbar logic

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const email = document.querySelector('.navbar-email');
-const userMenu = document.querySelector('.desktop-menu');
+const email = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const burguerMenu = document.querySelector(".menu");
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const shoppingCartMenu = document.querySelector(".product-detail");
 
-email.addEventListener('click', toggleDesktopMenu);
+email.addEventListener("click", toggleDesktopMenu);
+burguerMenu.addEventListener("click", toggleMobileMenu);
+shoppingCart.addEventListener("click", toggleShoppingCard);
 
-function toggleDesktopMenu () {
-    userMenu.classList.toggle('inactive');
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  shoppingCartMenu.classList.add("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+  desktopMenu.classList.add("inactive");
+  shoppingCartMenu.classList.add("inactive");
+}
+
+function toggleShoppingCard() {
+  shoppingCartMenu.classList.toggle("inactive");
+  desktopMenu.classList.add("inactive");
+  mobileMenu.classList.add("inactive");
 }
