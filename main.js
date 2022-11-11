9a06908482ff0fbc6817ae8e17bf0b6b976ac4e7
rmassieug <rodmassieu97@icloud.com>
2022-11-10 21:56:17 -0600

    Clase 20, html menu mobile agregado al main.html

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuHamIcon= document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 //Cada vez que le den click al correo de arriba a la derecha,
 //esta funcion hara un toggle
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
