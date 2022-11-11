c0fc14376ea558b0117ca9dd27d86511d19b0984
CryselSal <CryselOgandoLizardo11>
2022-11-10 23:20:21 -0400

    fusion de boton hambur, cuenta en correo y carrito de compra movil y pc

--- main.js
+++ main.js
@@ -1,8 +1,94 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive");
+    }
+    aside.classList.toggle('inactive');
+    
+}
+
+const productList = [];
+productList.push({
+    name:"Bike",
+    price:120,
+    image:"https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=94"
+});
+productList.push({
+    name:"Pantalla",
+    price:200,
+    image:"https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=94"
+});
+productList.push({
+    name:"Computadora",
+    price:620,
+    image:"https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=94"
+});
+
+function renderProducts(arr){
+    for (product of productList){//el in en vez de of te da los indices?
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src",product.image);
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        const productInfoDiv = document.createElement("div");
+    
+        const producPrice = document.createElement("p");
+        producPrice.innerText = "$" + product.price;
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        
+    
+        const productInfoFigure = document.createElement("figure");
+        const productImgCart = document.createElement("img");
+        productImgCart.setAttribute('src',"./icons/bt_add_to_cart.svg");
+    
+        productInfoDiv.appendChild(producPrice);
+        productInfoDiv.appendChild(productName);
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        productInfoFigure.appendChild(productImgCart);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
