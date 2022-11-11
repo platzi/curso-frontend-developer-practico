921187145ed316863ac629ca7d9cb47b315ea216
DanielLenoz <danielstivenxx@outlook.com>
2022-11-10 21:05:43 -0500

    agregando el aside del carro de comprar y haciendo que solo una pestaña dure habierta

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive')
+    aside.classList.add('inactive')
 }
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive')
+    aside.classList.add('inactive')
+}
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive')
+    desktopMenu.classList.add('inactive')
+    mobileMenu.classList.add('inactive')
 }
