a787f9443d1b2dc460badecb067b8826a0053893
LuisCH10 <chicomaluis555@gmail.com>
2022-11-12 17:13:50 -0500

    mobile con JS

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',ToggleDesktopMenu);
+menuHamIcon.addEventListener('click',ToggleMobileMenu);
 
 function ToggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function ToggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
