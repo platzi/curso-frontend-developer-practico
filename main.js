2f04329087568aa93ce851f2dfdcf166b449da91
DanielLenoz <danielstivenxx@outlook.com>
2022-11-11 19:35:21 -0500

    agregando el producto detail, pero sin interactividad de JavaScript

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+menuCarritoIcon.addEventListener('click', toggleCarritoshoppingCartContainer);
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive')
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
 }
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive')
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
 }
-function toggleCarritoAside() {
-    aside.classList.toggle('inactive')
+function toggleCarritoshoppingCartContainer() {
+    shoppingCartContainer.classList.toggle('inactive')
     desktopMenu.classList.add('inactive')
     mobileMenu.classList.add('inactive')
 }
