a8db4a668ce8f4cdac44675eeac0dbf81ba08371
cathy <cathy@bubulazi.com>
2022-11-13 19:34:10 +0000

    Bugfix 2

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
