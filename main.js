5493f106ca44833b8bcc00922cde3dfea2c27759
IRojas0101 <pirry_ivan501@hotmail.com>
2022-11-13 19:34:19 -0500

    actualizacion v5

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const ShoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 const cardsContainer = document.querySelector('.cards-container');
 
@@ -14,13 +14,13 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 
-menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+menuCarritoIcon.addEventListener('click', toggleCarritoShoppingCartContainer);
 
 function toggleDesktopMenu() {
-    const  isAsideClosed = aside.classList.contains('inactive');
+    const  isShoppingCartContainerClosed = ShoppingCartContainer.classList.contains('inactive');
 
-    if (!isAsideClosed) {
-        aside.classList.add('inactive');
+    if (!isShoppingCartContainerClosed) {
+        ShoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
@@ -28,23 +28,23 @@ function toggleDesktopMenu() {
 
 
 function toggleMobileMenu() {
-    const  isAsideClosed = aside.classList.contains('inactive');
+    const  isShoppingCartContainerClosed = ShoppingCartContainer.classList.contains('inactive');
 
-    if (!isAsideClosed) {
-        aside.classList.add('inactive');
+    if (!isShoppingCartContainerClosed) {
+        ShoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCarritoAside() {
+function toggleCarritoShoppingCartContainer() {
     const  isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    ShoppingCartContainer.classList.toggle('inactive');
     
 }
 
