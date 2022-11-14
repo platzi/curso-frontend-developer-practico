cdb8b7d3c4aed6bce92e800933c549d18a32570e
Rafael Leonardo <rafamendo1994@gmail.com>
2022-11-14 18:18:10 +0100

    Agrgando JavaScript y sincronizando menús

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const removeMenu = document.querySelector('.body');
+const hamMenu = document.querySelector('.menu');
+const mobile = document.querySelector('.mobile-menu');
+const iconCar = document.querySelector('.carLogo');
+const carList = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamMenu.addEventListener('click', toggleMobileMenu);
+iconCar.addEventListener('click', toggleCarMenu)
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobileMenu(){
+    const isCarClosed = carList.classList.contains('inactive');
+
+    if (!isCarClosed){
+        carList.classList.add('inactive')
+    }
+
+    mobile.classList.toggle('inactive');
+}
+
+function toggleCarMenu(){
+    const isMobileClosed = mobile.classList.contains('inactive');
+    
+    if (!isMobileClosed){
+        mobile.classList.add('inactive');        
+    } 
+
+    carList.classList.toggle('inactive');
+}
