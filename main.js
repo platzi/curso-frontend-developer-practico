1190dab7603381f024c16fd01e0f12b4523b6422
Felipe Sanchez <alvarosanchez120@gmail.com>
2022-11-14 21:13:54 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -2,14 +2,38 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector(".product-detail")
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        aside.classList.add('inactive');   
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        aside.classList.add('inactive');   
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside() {
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+  
+    if (!isMobileMenuClose) {
+        mobileMenu.classList.add('inactive');   
+    }
+
+    aside.classList.toggle('inactive');
 }
