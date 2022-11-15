2e4d121293121cb3f0720b52c0f0cc583bf4bec1
Josue Contreras <estavesyano@gmail.com>
2022-11-15 08:33:57 -0500

    Tercera Clase - Fusionando el archivo clase13.html

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
-const hamburMenu = document.querySelector('.menu')
+const hamburMenuIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const asideCarrito = document.querySelector('.product-detail')
+const carritoIcon = document.querySelector('.navbar-shopping-cart')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-hamburMenu.addEventListener('click', toggleMobileMenu);
+hamburMenuIcon.addEventListener('click', toggleMobileMenu);
+carritoIcon.addEventListener('click', toggleCarritoMenu);
 
 function toggleDesktopMenu() {
+    let isAsideCarritoOpen = !asideCarrito.classList.toggle('inactive');
+    if(isAsideCarritoOpen) {
+        asideCarrito.classList.toggle('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    let isAsideCarritoOpen = !asideCarrito.classList.toggle('inactive');
+    if(isAsideCarritoOpen) {
+        asideCarrito.classList.toggle('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 }
 
+function toggleCarritoMenu() {
+    let isMobileMenu = !mobileMenu.classList.toggle('inactive');
+    let isDesktopMenu = !desktopMenu.classList.toggle('inactive');
+    if(isMobileMenu) {
+        mobileMenu.classList.toggle('inactive');
+    } else if(isDesktopMenu) {
+        desktopMenu.classList.toggle('inactive');
+    }
+    asideCarrito.classList.toggle('inactive');
+}
