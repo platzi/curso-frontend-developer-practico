1b2b5bcdf20694046396e2605502b18532440032
Ankush Bihal <ankush.bihal@triggle.app>
2022-11-16 14:37:42 +0100

    Add JavaScript to Project

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const clothes = document.getElementById("#clothes");
+// Variables:
+const deskTopEmail = document.querySelector(".navbar-email");
+const deskTopMenu = document.querySelector(".desktop-menu");
+const mobileMenuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-function onClickClothes() {
-  clothes.click;
+// arrow founction
+const toggleDeskMenu = () => deskTopMenu.classList.toggle("inactive");
+const toggleMobileMenu = () => mobileMenu.classList.toggle("inactive");
+
+// Escuchando eventos
+function lanzarListener(param1, param2) {
+  return param1.addEventListener("click", param2);
 }
+
+lanzarListener(deskTopEmail, toggleDeskMenu);
+lanzarListener(mobileMenuIcon, toggleMobileMenu);
