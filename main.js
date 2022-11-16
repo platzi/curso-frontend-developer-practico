bcb4f0164127ef022f001cb4331e9fa3d4d8fff0
Ankush Bihal <ankush.bihal@triggle.app>
2022-11-16 16:11:18 +0100

    Added Aside Shopping Cart Section

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
 // Variables:
 const deskTopEmail = document.querySelector(".navbar-email");
 const deskTopMenu = document.querySelector(".desktop-menu");
+const deskTopShoppingCartIcon = document.querySelector(".navbar-shopping-cart");
+
 const mobileMenuIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const asideDiv = document.querySelector(".product-detail");
 
-// arrow founction
-const toggleDeskMenu = () => deskTopMenu.classList.toggle("inactive");
-const toggleMobileMenu = () => mobileMenu.classList.toggle("inactive");
+// arrow founction:
+const toggleDeskMenu = () => {
+  const isAsideClose = asideDiv.classList.contains("inactive");
 
-// Escuchando eventos
+  if (!isAsideClose) {
+    asideDiv.classList.add("inactive");
+  }
+  deskTopMenu.classList.toggle("inactive");
+};
+
+const toggleMobileMenu = () => {
+  const isAsideClose = asideDiv.classList.contains("inactive");
+
+  if (!isAsideClose) {
+    asideDiv.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+};
+
+const toggleShoppingCartAside = () => {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDeskTopMenuClose = deskTopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  if (!isDeskTopMenuClose) {
+    deskTopMenu.classList.add("inactive");
+  }
+
+  asideDiv.classList.toggle("inactive");
+};
+// Escuchando eventos:
 function lanzarListener(param1, param2) {
   return param1.addEventListener("click", param2);
 }
 
 lanzarListener(deskTopEmail, toggleDeskMenu);
 lanzarListener(mobileMenuIcon, toggleMobileMenu);
+lanzarListener(deskTopShoppingCartIcon, toggleShoppingCartAside);
