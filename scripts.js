52992d558ce8445ca3fc6e53b45c5a2ddf382390
lauArciniega2797 <arciniega1497@hotmail.com>
2022-11-16 18:00:25 -0600

    cambios chidos

--- scripts.js
+++ scripts.js
@@ -32,6 +32,9 @@ carritoIconBtn.addEventListener('click', ()=>{
         menuMobileBox.classList.add('inactive')
     }
     carritoBox.classList.toggle('inactive')
+    if(!carritoBox.classList.contains('inactive')){
+        getCarItems()
+    }
 })
 productDetailCloseBtn.addEventListener('click', ()=>{
     asideDetailProduct.classList.remove('visible')
@@ -76,6 +79,9 @@ function renderProducts (productList) {
                     const productInfoFigure = document.createElement('figure')
                         const productInfoFigureImage = document.createElement('img')
                             productInfoFigureImage.src = "./icons/bt_add_to_cart.svg"
+                            productInfoFigureImage.addEventListener('click', function(){
+                                console.log('se agregará al carrito')
+                            })
     
             productInfoFigure.appendChild(productInfoFigureImage)
             productInfoText.append(productInfoTextPrice, productInfoTextName)
@@ -86,7 +92,7 @@ function renderProducts (productList) {
             productBox.appendChild(productCard)
         }
     }
-    xhttp.open("GET", 'https://api.escuelajs.co/api/v1/products');
+    xhttp.open("GET", 'https://api.escuelajs.co/api/v1/products?offset=0&limit=25');
     xhttp.send();
 
 }
@@ -97,6 +103,7 @@ function openProductDetail(productId, array){
     const aside_productInfo_price = document.querySelector('#detailProduct .product-info p:nth-child(1)')
     const aside_productInfo_name = document.querySelector('#detailProduct .product-info p:nth-child(2)')
     const aside_productInfo_descr = document.querySelector('#detailProduct .product-info p:nth-child(3)')
+    const aside_productInfo_btn = document.querySelector('.add-to-cart-button')
 
     array.forEach((element, index) => {
         if(productId == element.id) {
@@ -104,6 +111,60 @@ function openProductDetail(productId, array){
             aside_productInfo_name.innerHTML = element.title
             aside_productInfo_price.innerHTML = element.price
             aside_productInfo_descr.innerHTML = element.description
+            aside_productInfo_btn.addEventListener('click', function(){
+                addToCar(element.id, element.images[0], element.title, element.price)
+            })
         }
     })
+}
+let carProducts = []
+function addToCar(idProduct, imageProduct, nameProduct, priceProduct) {
+    carProducts.push({id:idProduct, image: imageProduct, title:nameProduct, price: priceProduct})
+
+    if(localStorage.getItem('productos_carrito')){
+        localStorage.removeItem('productos_carrito')
+    }
+    localStorage.setItem('productos_carrito', JSON.stringify(carProducts))
+}
+function getCarItems(){
+    const productCarBox = document.querySelector('#shoppingCartContainer .my-order-content #itemsContainer')
+    productCarBox.innerHTML = ''
+    let productInCart = JSON.parse(localStorage.getItem('productos_carrito'))
+
+    for (item of productInCart){
+                const shoppingCartItem = document.createElement('div')
+                    shoppingCartItem.classList.add('shopping-cart')
+                    const shoppingCartItem_figure = document.createElement('figure')
+                        const shoppingCartItem_figure_img = document.createElement('img')
+                            shoppingCartItem_figure_img.src = item.image
+                    const shoppingCartItem_name = document.createElement('p')
+                        shoppingCartItem_name.innerHTML = item.title
+                    const shoppingCartItem_price = document.createElement('p')
+                        shoppingCartItem_price.innerHTML = '$' + (item.price).toLocaleString('en-US')
+                    const shoppingCartItem_deleteBtn = document.createElement('img')
+                        shoppingCartItem_deleteBtn.src = './icons/icon_close.png'
+                        shoppingCartItem_deleteBtn.addEventListener('click', function(){
+                            deleteItemFromCart(item.id, productInCart)
+                        })
+
+            shoppingCartItem_figure.appendChild(shoppingCartItem_figure_img)
+            shoppingCartItem.append(shoppingCartItem_figure, shoppingCartItem_name,shoppingCartItem_price, shoppingCartItem_deleteBtn)
+            productCarBox.appendChild(shoppingCartItem)
+    }
+}
+function deleteItemFromCart(idProduct, array){
+    let productsInCart = Array.from(array)
+    productsInCart.forEach((element, index) => {
+        if(element.id == idProduct) {
+            productsInCart.splice(index, 1)
+            return false
+        }
+    })
+
+    if(localStorage.getItem('productos_carrito')){
+        localStorage.removeItem('productos_carrito')
+    }
+    localStorage.setItem('productos_carrito', JSON.stringify(productsInCart))
+
+    getCarItems()
 }
