07bdfef8070155f8979ca28a5e99ae6ffefe3f13
Marco Pretelin <marcoapretelin.igf@gmail.com>
2022-11-17 02:49:11 -0600

    Modification in index.html. styles.css and main.js, add product detail and shopping cart

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const prouctDetail = document.querySelector('.product-detail'); 
+const shopCart = document.querySelector('.navbar-shopping-cart'); 
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurgerIcon.addEventListener('click', toggleMobileMenu);
+shopCart.addEventListener('click', toggleProductDetail);
+
 
 function toggleDesktopMenu() {
     
@@ -12,5 +18,26 @@ function toggleDesktopMenu() {
 }
 
 function toggleMobileMenu() {
+
+    const isProductDetailClosed = prouctDetail.classList.contains('inactive'); 
+
+    if (!isProductDetailClosed) {
+            
+        prouctDetail.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+
+function toggleProductDetail() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+            
+        mobileMenu.classList.add('inactive');
+    }
+    prouctDetail.classList.toggle('inactive');  
+    
 }
