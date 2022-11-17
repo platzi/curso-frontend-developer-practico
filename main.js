af9968f0bda749c90f97e4a1d97cfb7815d63569
cathy <cathy@bubulazi.com>
2022-11-17 17:46:04 +0000

    Trabajando en la lista de producto: HTML a partir de arrays

--- main.js
+++ main.js
@@ -10,9 +10,48 @@ menuHamIcon.addEventListener('click', toggleMobileMenu)
 menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
+
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains('inactive')
+
+  if(!isAsideClosed) {
+    aside.classList.add('inactive')
+  }
+
   mobileMenu.classList.toggle('inactive')
 }
+
+
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+ 
+  if(!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive')
+  }
+  aside.classList.toggle('inactive')
+}
+
+const productList = []
+productList.push(
+  {
+    name: 'Bike',
+    price: 120,
+    imagge: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    name: 'Pantalla',
+    price: 220,
+    imagge: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    name: 'Compu',
+    price: 620,
+    imagge: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  }
+)
+console.log(productList)
