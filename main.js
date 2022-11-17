03e14cd403a11ae54f9abccf8ff727cf223a4c75
Brandon <70345941@continental.edu.pe>
2022-11-17 12:51:34 -0500

    Lista de productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -43,3 +43,74 @@ function toggleMenu(elemento) {
 //       break;
 //   }
 // }
+
+const productList = [
+  {
+    name: 'Bike',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Computer',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Speaker',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    name: 'Mouse',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+];
+const cardsContainer = document.querySelector('.cards-container');
+
+for (product of productList) {
+  const productCard = document.createElement('div');
+  productCard.classList.add('product-card');
+  
+  const productImg = document.createElement('img');
+  productImg.setAttribute('src', product.image);
+
+  const productInfo = document.createElement('div');
+  productInfo.classList.add('product-info');
+  
+  const productInfoDiv = document.createElement('div');
+  
+  const productPrice = document.createElement('p');
+  productPrice.innerText = '$' + product.price;
+  
+  const productName = document.createElement('p');
+  productName.innerText = product.name;
+  
+  const productInfoFigure = document.createElement('figure');
+  const productImgCard = document.createElement('img');
+  productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+  productInfoFigure.appendChild(productImgCard);
+  productInfoDiv.append(productPrice, productName);
+  productInfo.append(productInfoDiv, productInfoFigure);
+  productCard.append(productImg, productInfo);
+
+  cardsContainer.appendChild(productCard);
+}
+
+{/* 
+<div class="product-card">
+  <img
+    src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    alt="">
+  <div class="product-info">
+    <div>
+      <p>$120,00</p>
+      <p>Bike</p>
+    </div>
+    <figure>
+      <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+  </div>
+</div> 
+*/}
