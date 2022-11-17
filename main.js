206841612e27eef9c2bb15c3ba7be45eff625e67
Sergio Quintero <sergio.quinteroc@udea.edu.co>
2022-11-17 17:20:22 -0500

    Agregado detalle del producto

--- main.js
+++ main.js
@@ -5,11 +5,15 @@ const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const myOrderAside = document.querySelector(".product-detail");
+const myOrderAside = document.querySelector(".product-detail-cart");
+
+const productDetailContainer = document.querySelector(".product-detail");
+const productDetailCloseIcon = document.querySelector(".product-detail-close");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleMyOrderAside);
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside);
 
 const cardsContainer = document.querySelector(".cards-container");
 
@@ -21,12 +25,23 @@ function toggleDesktopMenu() {
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive");
     myOrderAside.classList.add("inactive");
+    productDetailContainer.classList.add("inactive");
 }
 
 function toggleMyOrderAside() {
     myOrderAside.classList.toggle("inactive");
     mobileMenu.classList.add("inactive");
     desktopMenu.classList.add("inactive");
+    productDetailContainer.classList.add("inactive");
+}
+
+function openProductDetailAside() {
+    productDetailContainer.classList.remove("inactive");
+    myOrderAside.classList.add("inactive");
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add("inactive");
 }
 
 const producList = [];
@@ -46,21 +61,6 @@ producList.push({
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 
-/* 
-<div class="product-card">
-    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-    <div class="product-info">
-          <div>
-            <p>$120,00</p>
-            <p>Bike</p>
-          </div>
-          <figure>
-            <img src="./icons/bt_add_to_cart.svg" alt="">
-          </figure>
-    </div>
-</div> 
-*/
-
 function renderProducts(producList) {
     for (product of producList) {
         const productCard = document.createElement("div");
@@ -68,6 +68,7 @@ function renderProducts(producList) {
 
         const productImg = document.createElement("img");
         productImg.setAttribute("src", product.image);
+        productImg.addEventListener("click", openProductDetailAside);
 
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
