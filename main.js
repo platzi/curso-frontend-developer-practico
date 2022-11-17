2a980c6610df74f365c166eba1d13f6c1ba12182
Vicki Avola <vickiavola7@gmail.com>
2022-11-17 05:37:39 +0100

    Clase 22 - Aside Product Detail

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -12,9 +12,9 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-     const isAsideClosed = aside.classList.contains('inactive');
+     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
      if (!isAsideClosed) {
-       aside.classList.add('inactive');
+      shoppingCartContainer.classList.add('inactive');
      }
 
      desktopMenu.classList.toggle('inactive');
@@ -22,10 +22,10 @@ function toggleDesktopMenu() {
    
 
 function toggleMobileMenu() {
-     const isAsideClosed = aside.classList.contains('inactive');
+     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
    
      if (!isAsideClosed) {
-       aside.classList.add('inactive'); 
+      shoppingCartContainer.classList.add('inactive'); 
      }
      mobileMenu.classList.toggle('inactive');
    }
@@ -35,7 +35,7 @@ function toggleCarritoAside(){
      if (!isMobileMenuClosed) {
           mobileMenu.classList.add('inactive');
      }
-     aside.classList.toggle('inactive');
+     shoppingCartContainer.classList.toggle('inactive');
 };
 
 const productList = [];
