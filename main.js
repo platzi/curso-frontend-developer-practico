4398f3ae5ed93c4f3c163520d24063cedde4196b
Sergio Quintero <sergio.quinteroc@udea.edu.co>
2022-11-16 21:58:47 -0500

    Añadido el carrito

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const myOrderAside = document.querySelector(".product-detail");
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
-menuHamIcon.addEventListener("click", toggleMobileMenu)
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleMyOrderAside);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
+    desktopMenu.classList.toggle('inactive');
+    myOrderAside.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle("inactive");
+    mobileMenu.classList.toggle('inactive');
+    myOrderAside.classList.add('inactive');
+
+/*     const isMyOrderAsideClosed = myOrderAside.classList.contains('inactive');
+
+    if (!isMyOrderAsideClosed){
+        myOrderAside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive'); */
+}
+
+function toggleMyOrderAside() {
+    myOrderAside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+/* 
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    } 
+    myOrderAside.classList.toggle('inactive');
+    */
 }
