737f9aa7507054ae80fb26a70680fb026c0f45bd
PepeCharly01 <jc.espinosa2@gmail.com>
2022-11-18 14:16:40 -0600

    se añadio el menu del carrito y con JS los menus se cierran cuando se abre otro

--- main.js
+++ main.js
@@ -2,14 +2,45 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileClosed) {
+        mobileMenu.classList.add('inactive');
+    } 
+
+    const isDesktopClosed = desktopMenu.classList.contains('inactive');
+    
+    if (!isDesktopClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
