f5f48a56197b22c7fcd8818c9297f795ceac0a77
puntoexe77 <dixman.17@gmail.com>
2022-11-18 11:14:39 -0500

    Se añadio el menu hamburguesa al index.html

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
 /*addEventListener('click', () =>{
     desktopMenu.classList.toggle('inactive')
 })*/;
 
 const showDesktopMenu=()=>{
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
+}
+const showMobileMenu=()=>{
+    mobileMenu.classList.toggle('inactive')
 }
 
+burguerMenu.addEventListener('click',showMobileMenu);
 menuEmail.addEventListener('click',showDesktopMenu);
 
 
