94c00a5c464a392bc14ff8195931b457a29a0f8f
puntoexe77 <dixman.17@gmail.com>
2022-11-18 17:06:13 -0500

    Agregue al archivo index.html, los detallesde los productos en el carrito, ademas que se mostraran todos los prodcutos

---
+++ base-datos-products.js
@@ -0,0 +1,12 @@
+let imagenes = [
+    'https://www.elcarrocolombiano.com/wp-content/uploads/2021/01/20210124-LOS-10-CARROS-MAS-VENDIDOS-DEL-MUNDO-EN-2020-01.jpg',
+    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+];
+let prices = [
+    '200',
+    '500'
+];
+let tipos = [
+    'motocicleta',
+    'bicicleta'
+];
--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burguerMenu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu')
-/*addEventListener('click', () =>{
-    desktopMenu.classList.toggle('inactive')
-})*/;
-
-const showDesktopMenu=()=>{
-    desktopMenu.classList.toggle('inactive');
-}
-const showMobileMenu=()=>{
-    mobileMenu.classList.toggle('inactive')
-}
+const mobileMenu = document.querySelector('.mobile-menu');
+const shopCar = document.querySelector('.navbar-shopping-cart');
+const detailProducto = document.querySelector('.product-detail');
+const containerCards = document.querySelector('.cards-container')
 
 burguerMenu.addEventListener('click',showMobileMenu);
 menuEmail.addEventListener('click',showDesktopMenu);
+shopCar.addEventListener('click',showDetailProduct)
+
+function showDesktopMenu(){
+    desktopMenu.classList.toggle('inactive');
+    detailProducto.classList.toggle('inactive',true);
+}
+function showMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    detailProducto.classList.toggle('inactive',true);
+}
+function showDetailProduct(){
+    detailProducto.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive',true);
+    mobileMenu.classList.toggle('inactive',true);
+}
 
 
+let descripProductos = [];
+for (let index = 0; index < imagenes.length; index++) {
+    let objeto = {imagen: imagenes[index],
+                  price:prices[index],
+                  tipo: tipos[index]}
+    descripProductos.push(objeto);    
+}
+
+function showProducts(){
+    descripProductos.forEach(element => {
+    let cardProduct = `<div class="product-card">
+                        <img src="${element.imagen}" alt="">
+                        <div class="product-info">
+                        <div>
+                            <p>$ ${element.price}</p>
+                            <p>${element.tipo}</p>
+                        </div>
+                        <figure>
+                            <img src="./icons/bt_add_to_cart.svg" alt="">
+                        </figure>
+                        </div>
+                        </div>`;
+    containerCards.innerHTML += cardProduct;
+});
+}
+showProducts();
