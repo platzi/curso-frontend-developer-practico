9696a7fd09ee4e1e06ba07a5de4ab248bb51f453
solicitudes.informatica <solicitudes.informatica@andar.com.co>
2022-11-18 10:47:25 -0500

    integrando js

---
+++ main.js
@@ -0,0 +1,6 @@
+const menuEmail = document.querySelector('.navbar-email');
+const menuDesktop = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', function(e) {
+    console.log('Hola');
+})
