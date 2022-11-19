36d1ddbaad86d244d380a378ef8b09e22ccce8a6
Andres Ulloa <andresguc@gmail.com>
2022-11-19 11:13:25 -0500

    Shopping cart module

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCartIcon.addEventListener('click', toggleCartAside);
+
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
     mobileMenu.classList.toggle('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
 }
+
+function toggleCartAside() {
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+
