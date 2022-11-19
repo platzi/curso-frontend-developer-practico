4e9e0645805a63c50cb8b87931e508abe6576572
atoledomargalef <agtoledomargalef@gmail.com>
2022-11-19 20:34:31 -0300

    Colocado el ProductDetail, modificada el css del shoping cart y productDetail y el menu mobile

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu')
-const aside = document.querySelector('.product-detail')
+const shoppingCartCont = document.querySelector('#shoppingCartCont')
+const asidePDetail = document.querySelector('.detail-aside')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
@@ -11,6 +12,7 @@ menuHamIcon.addEventListener('click', toggleMobileMenu)
 menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
+
     // const dmClasses = desktopMenu.getAttribute('class')
     // const inactive = 'inactive'
     // const dMClass = 'desktop-menu'
@@ -20,26 +22,27 @@ function toggleDesktopMenu(){
     // }   else {
     //     desktopMenu.setAttribute('class', dMClass)
     // }
-    const isAsideClosed = aside.classList.contains('inactive')
     
-    if(!isAsideClosed){
-        aside.classList.add('inactive')
+    const isShoppingCartContClosed = shoppingCartCont.classList.contains('inactive')
+    
+    if(!isShoppingCartContClosed){
+        shoppingCartCont.classList.add('inactive')
     }
     desktopMenu.classList.toggle('inactive')
 
 }
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive')
+    const isShoppingCartContClosed = shoppingCartCont.classList.contains('inactive')
     
-    if(!isAsideClosed){
-        aside.classList.add('inactive')
+    if(!isShoppingCartContClosed){
+        shoppingCartCont.classList.add('inactive')
     }
 
     mobileMenu.classList.toggle('inactive')
 
 }
 function toggleCarritoAside() {
-
+    console.log("click")
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
     
@@ -49,7 +52,7 @@ function toggleCarritoAside() {
     if(!isDesktopMenuClosed){
         desktopMenu.classList.add('inactive')
     }
-    aside.classList.toggle('inactive')
+    shoppingCartCont.classList.toggle('inactive')
 
 }
 
@@ -137,22 +140,6 @@ productList.push({
     price: 520,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
-/*
-<div class="product-card">
-<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-<div class="product-info">
-  <div>
-    <p>$120,00</p>
-    <p>Bike</p>
-  </div>
-  <figure>
-    <img src="./icons/bt_add_to_cart.svg" alt="">
-  </figure>
-</div>
-</div>
-*/
-
-
 
 renderProducts(productList)
 
