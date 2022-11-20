9a1d8bf45dacc7d998040b9d027bb27ad0b515c1
puntoexe77 <dixman.17@gmail.com>
2022-11-20 14:58:19 -0500

    Agregue dinamica al carrito de detalles para que se puedan ver el producto que se seleccione

--- base-datos-products.js
+++ base-datos-products.js
@@ -1,12 +1,17 @@
 let imagenes = [
     'https://www.elcarrocolombiano.com/wp-content/uploads/2021/01/20210124-LOS-10-CARROS-MAS-VENDIDOS-DEL-MUNDO-EN-2020-01.jpg',
-    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://www.elpais.com.co/files/article_main/uploads/2020/03/04/5e5fdf478b0ab.jpeg'
 ];
+
 let prices = [
     '200',
-    '500'
+    '500',
+    '600'
 ];
 let tipos = [
     'motocicleta',
-    'bicicleta'
+    'bicicleta',
+    'camioneta 4x4'
 ];
+
--- main.js
+++ main.js
@@ -4,24 +4,27 @@ const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shopCar = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
-const containerCards = document.querySelector('.cards-container')
+const containerCards = document.querySelector('.cards-container');
 
 burguerMenu.addEventListener('click',showMobileMenu);
 menuEmail.addEventListener('click',showDesktopMenu);
-shopCar.addEventListener('click',showDetailProduct)
+shopCar.addEventListener('click',showDetailProduct);
 
 function showDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
     shoppingCartContainer.classList.toggle('inactive',true);
+    productDetails.classList.toggle('inactive',true)
 }
 function showMobileMenu(){
     mobileMenu.classList.toggle('inactive');
     shoppingCartContainer.classList.toggle('inactive',true);
+    productDetails.classList.toggle('inactive',true)
 }
 function showDetailProduct(){
     shoppingCartContainer.classList.toggle('inactive');
     desktopMenu.classList.toggle('inactive',true);
     mobileMenu.classList.toggle('inactive',true);
+    productDetails.classList.toggle('inactive',true)
 }
 
 
@@ -32,14 +35,14 @@ for (let index = 0; index < imagenes.length; index++) {
                   tipo: tipos[index]}
     descripProductos.push(objeto);    
 }
-
+let serieClase = 1;
 function showProducts(){
     descripProductos.forEach(element => {
-    let cardProduct = `<div class="product-card">
+    let cardProduct = `<div class="product-card card-${serieClase}">
                         <img src="${element.imagen}" alt="">
                         <div class="product-info">
                         <div>
-                            <p>$ ${element.price}</p>
+                            <p>$ ${element.price},00</p>
                             <p>${element.tipo}</p>
                         </div>
                         <figure>
@@ -48,7 +51,54 @@ function showProducts(){
                         </div>
                         </div>`;
     containerCards.innerHTML += cardProduct;
+    serieClase++;
 });
 }
-console.log(containerCards)
 showProducts();
+
+
+
+console.log(serieClase);
+let a = []
+for (let index = 0; index < serieClase-1; index++) {
+    a[index] = document.querySelector(`.card-${index+1}`)
+    a[index].addEventListener('click', imagenUno)
+}
+
+let productDetails = document.querySelector('#productDetail');
+
+// let closeProductDetails;
+function imagenUno(x){
+    let a = x.path[1].querySelector('img').src;
+    console.log(a);
+    let index = imagenes.indexOf(a);
+    console.log(index);
+    let productDetalles = `<div class="product-detail-close">
+                            <img src="./icons/icon_close.png" alt="close">
+                          </div>
+                          <img src=${a} alt="bike">
+                          <div class="product-info-detail">
+                            <p>$ ${prices[index]},00</p>
+                            <p>${tipos[index]}</p>
+                            <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+                            <button class="primary-button add-to-cart-button">
+                            <img src="./icons/bt_add_to_cart.svg" alt="add to cart">
+                            Add to cart
+                            </button>
+                          </div>
+                          `
+    productDetails.innerHTML = productDetalles;
+    
+    let closeProductDetails = document.querySelector('.product-detail-close');
+    closeProductDetails.addEventListener('click', closeProductDetail); 
+    
+    productDetails.classList.toggle('inactive',false)
+    desktopMenu.classList.toggle('inactive',true);
+    mobileMenu.classList.toggle('inactive',true);
+    shoppingCartContainer.classList.toggle('inactive',true);
+}
+
+function closeProductDetail(){
+    productDetails.classList.toggle('inactive',true)
+}
+
