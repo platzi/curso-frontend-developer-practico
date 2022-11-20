af6c98dcd9a1a69b6df0bfe313b2b6e1385517c0
Stivenson <stivenson.sosa@gmail.com>
2022-11-19 23:24:18 -0500

    Se modifica el menu desktop y el menu mobile, se agregan mas estilos

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const btnBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+btnBurger.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
