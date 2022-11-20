6cf45da7711e6435a728efdc64fd1c3e01b65a7c
yoelR <yoelromero0501@gmail.com>
2022-11-20 17:12:00 -0600

    quitando menu, mostrando carrito y al revés

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
 const menuEmail = document.querySelector(".navbar-email");
-const DesktopMenu = document.querySelector(".desktop-menu");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-// function toggleDesktopMenu() {
-//     DesktopMenu.classList.remove("inactive");  //mi forma para que aparezca el menú
-// }
 
 function toggleDesktopMenu() {
-    DesktopMenu.classList.toggle("inactive");  //no sabia que existia toggle
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+
+    desktopMenu.classList.toggle("inactive");  //no sabia que existia toggle
 }
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle("inactive");  //esta linea es opcional, siempre funciona el algoritmo.
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopmenuClosed = desktopMenu.classList.contains('inactive')
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive");
+    }
+
+    if(!isDesktopmenuClosed){
+        desktopMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
+}
+
+
+
+
