257757126b2e2bfbf302da5277f28e071a51f78b
arturo4ever <darturosanchezg@gmail.com>
2022-11-21 18:54:53 +0000

    Funcionalidades de los menús de shopping-cart y navbar

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const desktopMenu = document.querySelector('.desktop-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu() {
+    const isCartMenuClosed = menuCart.classList.contains('inactive');
+
+    if (!isCartMenuClosed) {
+        menuCart.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
@@ -12,5 +17,30 @@ const mobileMenu = document.querySelector('.mobile-menu');
 burgerMenu.addEventListener('click', toggleBurgerMenu);
 
 function toggleBurgerMenu() {
+    const isCartMenuClosed = menuCart.classList.contains('inactive');
+
+    if (!isCartMenuClosed) {
+        menuCart.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const menuCart = document.querySelector('.product-detail');
+
+shoppingCart.addEventListener('click', toggleCartMenu);
+
+function toggleCartMenu() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    menuCart.classList.toggle('inactive');
 }
