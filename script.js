089062398127aa5ba5a4a24b0acf08bc156e8c01
Daniela Castro <dgonzalezcs@outlook.com>
2022-11-20 16:02:36 -0800

    versinnn previa al pull request

--- script.js
+++ script.js
@@ -2,23 +2,24 @@
 const menuEmail = document.querySelector('.navbar-email')
 const menuBurgerIcon = document.querySelector('.menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
+const productDetailContainer = document.querySelector('#productDetail')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuBurgerIcon.addEventListener('click', toggleMobileMenu)
 menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu(){
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
-
     if(!isAsideClosed) {
         shoppingCartContainer.classList.add('inactive')
     }  
-   
     desktopMenu.classList.toggle('inactive')
 }
 function toggleMobileMenu (){
@@ -28,6 +29,8 @@ function toggleMobileMenu (){
         shoppingCartContainer.classList.add('inactive')
     }  
     mobileMenu.classList.toggle('inactive')
+    
+    closeProductDetailAside()
 }
 function toggleCarritoAside (){
     
@@ -36,9 +39,22 @@ function toggleCarritoAside (){
     if(!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive')
     }    
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
+
+    if(!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive')
+    } 
     shoppingCartContainer.classList.toggle('inactive')
 }
+function openProductDetailAside () {
+    shoppingCartContainer.classList.add('inactive')
+    productDetailContainer.classList.remove('inactive')
+}
 
+function closeProductDetailAside () {
+    productDetailContainer.classList.add('inactive')
+}
 
 const productList = [];
 productList.push({
@@ -57,13 +73,13 @@ productList.push({
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 })
 
-
 for (product of productList) {
     const productCard = document.createElement("div")
     productCard.classList.add("product-card");
 
     const productImg = document.createElement("img")
     productImg.setAttribute("src", product.image)
+    productImg.addEventListener('click', openProductDetailAside)
 
     const productInfo = document.createElement("div")
     productInfo.classList.add("product-info");
