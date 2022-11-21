c368b53dd56f68caf4642a129c2557cf12e04ee7
Stivenson <stivenson.sosa@gmail.com>
2022-11-20 22:55:46 -0500

    Se modifica el carrito de compra y se ajusta el css, mobile first, y html

--- main.js
+++ main.js
@@ -2,15 +2,26 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const btnBurger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCar = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 btnBurger.addEventListener('click', toggleMobileMenu);
+menuCar.addEventListener('click', toggleCar);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+function toggleCar () {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
