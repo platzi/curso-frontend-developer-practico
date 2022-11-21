df26cab322677c2e50a57931562257e558baefa2
arturo4ever <darturosanchezg@gmail.com>
2022-11-21 17:52:05 +0000

    Navbar y menú, versiòn mobile

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-menuEmail.addEventListener('click', toggleDesktopMenu);
 
+menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+const burgerMenu = document.querySelector('#burgerMenu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+burgerMenu.addEventListener('click', toggleBurgerMenu);
+
+function toggleBurgerMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
