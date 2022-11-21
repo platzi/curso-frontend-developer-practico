c52119101de34071f2b9e3ca83793a3b202dd26a
camilos-byte <andressalgado742@gmail.com>
2022-11-20 23:43:13 -0500

    configuracion menu desktop y mobile

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 let menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobilMenu= document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', function(e) {
     e.preventDefault();
     //con toogle lo que hacemos es quitar o poner la clase inactive
     menuDesktop.classList.toggle('inactive')
 });
+
+
+menuHamIcon.addEventListener('click', function(e) {
+    e.preventDefault();
+    mobilMenu.classList.toggle('inactive')
+});
+
+
+
