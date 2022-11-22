ea7ac995bc37c0ae72af75fd237024beeacf73dd
Jesus Perez <jperezsiculaba@serempre.com>
2022-11-21 20:05:53 -0500

    Implementing the aside menu for order list

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const orderIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+orderIcon.addEventListener('click', toggleAsideMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
@@ -12,4 +15,16 @@ function toggleDesktopMenu(){
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive')
+
+    if (!aside.classList.contains('inactive')){
+        aside.classList.add('inactive')
+    }
+}
+
+function toggleAsideMenu(){
+    aside.classList.toggle('inactive')
+
+    if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive')
+    }
 }
