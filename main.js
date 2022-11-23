994242712534922e335d4826eb8cd7e22417c223
Hestiam <hestiam04@gmail.com>
2022-11-23 14:09:20 -0500

    Adicionando menus desplegables en mobile y y carrito

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Desktop Menu toggle
+
 const navEmail= document.querySelector(".navbar-email");
 const desktopMenu= document.querySelector(".desktop-menu");
 
@@ -10,6 +12,33 @@ function toggleDesktopMenu(){
     else{
         desktopMenu.className="desktop-menu inactive";
     } */
-
+    aside.classList.add('inactive')
     desktopMenu.classList.toggle("inactive");
-} 1
+}
+
+//Mobile Menu Toggle
+
+const menuHamIcon= document.querySelector(".menu");
+const mobileMenu= document.querySelector(".mobile-menu");
+
+menuHamIcon.addEventListener("click",toggleMobileMenu);
+
+function toggleMobileMenu(){    
+    aside.classList.add('inactive')
+    mobileMenu.classList.toggle("inactive");
+}
+
+//CArrito aside toggle
+
+const carritoIcon= document.querySelector(".navbar-shopping-cart");
+const aside= document.querySelector(".product-detail");
+
+carritoIcon.addEventListener("click",toggleCarritoAside);
+
+function toggleCarritoAside(){
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add("inactive");
+    aside.classList.toggle("inactive");
+}
+
+
