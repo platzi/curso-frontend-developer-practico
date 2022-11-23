568e87803f091222e6b5a6d7a699148e0563925e
Federico Hirsch Veloso <fhirsch@onfocusdev.com>
2022-11-23 13:32:29 -0300

    carrito de compras

--- main.js
+++ main.js
@@ -2,14 +2,36 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carritoMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+carritoMenu.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+    aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive')
 };
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+    aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')
 }
