a6b586cd31914b0a4f31fd7f9e4efc426cea35bc
Juan <juandiflow@gmail.com>
2022-11-22 19:00:18 -0500

    Agregamos BurgerMenu y sus apariciones responsivas

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const BurgerMenu = document.querySelector(".BurgerMenu")
+const mobilemenu = document.querySelector(".mobile-menu")
 
 navEmail.addEventListener("click", toggleMenu);
+BurgerMenu.addEventListener("click", toggleBurgerMenu)
 
 function toggleMenu () {
     desktopMenu.classList.toggle("inactivo"); //El classList accede a la clase inactivo y le otorga la propiedad toggle que la hace aparecer y desaparecer cuando demos click
-}
+};
+
+function toggleBurgerMenu () {
+    mobilemenu.classList.toggle("inactivo");
+};
