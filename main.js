acc9ea0005534c6d2f4ca65f1a66516488c2a3cd
Hestiam <hestiam04@gmail.com>
2022-11-24 16:21:38 -0500

    Agregando aside de detalles del producto

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function toggleDesktopMenu(){
     else{
         desktopMenu.className="desktop-menu inactive";
     } */
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
     desktopMenu.classList.toggle("inactive");
 }
 
@@ -24,21 +24,21 @@ const mobileMenu= document.querySelector(".mobile-menu");
 menuHamIcon.addEventListener("click",toggleMobileMenu);
 
 function toggleMobileMenu(){    
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
     mobileMenu.classList.toggle("inactive");
 }
 
-//Carrito aside toggle
+//Carrito shoppingCartContainer toggle
 
 const carritoIcon= document.querySelector(".navbar-shopping-cart");
-const aside= document.querySelector(".product-detail");
+const shoppingCartContainer= document.querySelector("#shoppingCartContainer");
 
-carritoIcon.addEventListener("click",toggleCarritoAside);
+carritoIcon.addEventListener("click",toggleCarritoshoppingCartContainer);
 
-function toggleCarritoAside(){
+function toggleCarritoshoppingCartContainer(){
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add("inactive");
-    aside.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList= [];
