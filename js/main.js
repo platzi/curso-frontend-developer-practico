35f5cc2ddfe9721dbc26a568bf3d10b14daba5a5
Miguel <maickys123@gmail.com>
2022-11-25 19:30:22 -0500

    finalziamos la seccion del nav y menu en desktop y mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu ");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
+    console.log("click");
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    console.log("click");
+    mobileMenu.classList.toggle('inactive');
 }
 
