22a3ba0e4cf26743539ad15898d9df374f7547fa
Brayan Galeano <bsgaleanol@correo.udistrital.edu.co>
2022-11-27 16:26:39 -0600

    menu mobile

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
+const burguerButton=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
 
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerButton.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
