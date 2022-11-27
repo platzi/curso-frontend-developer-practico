cb10b8f95952517cdcfb3b26f7f5c2aac346bbf4
Brayan Galeano <bsgaleanol@correo.udistrital.edu.co>
2022-11-27 17:16:38 -0600

    Detalle de productos

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-const menuEmail=document.querySelector('.navbar-email');
-const desktopMenu=document.querySelector('.desktop-menu');
-const burguerButton=document.querySelector('.menu');
-const mobileMenu=document.querySelector('.mobile-menu');
+const navbarEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 
-
-
-menuEmail.addEventListener('click', toggleDesktopMenu);
-burguerButton.addEventListener('click', toggleMobileMenu);
+navbarEmail.addEventListener("click", toggleDesktopMenu);
+menu.addEventListener("click", toggleMobileMenu);
+navbarShoppingCart.addEventListener("click", toggleProductDetail);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+  productDetail.classList.add("inactive");
+  desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+  const isProductDetailActive = !productDetail.classList.contains("inactive");
+  mobileMenu.classList.toggle("inactive");
+
+  if (isProductDetailActive) {
+    productDetail.classList.add("inactive");
+    console.log("product detail is active");
+  }
+}
+
+function toggleProductDetail() {
+  const isMobileMenuActive = !mobileMenu.classList.contains("inactive");
+  const isDesktopMenuActive = !desktopMenu.classList.contains("inactive");
+  productDetail.classList.toggle("inactive");
+  if (isMobileMenuActive) {
+    mobileMenu.classList.add("inactive");
+  } else if (isDesktopMenuActive) {
+    desktopMenu.classList.add("inactive");
+  }
 }
