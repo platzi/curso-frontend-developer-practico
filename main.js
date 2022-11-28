fa115e550901e330a61af30e9bee26963845959a
Brayan Galeano <bsgaleanol@correo.udistrital.edu.co>
2022-11-27 18:20:30 -0600

    Productos

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ navbarEmail.addEventListener("click", toggleDesktopMenu);
 menu.addEventListener("click", toggleMobileMenu);
 navbarShoppingCart.addEventListener("click", toggleProductDetail);
 
+const cardsContainer = document.querySelector(".cards-container");
+
 function toggleDesktopMenu() {
   productDetail.classList.add("inactive");
   desktopMenu.classList.toggle("inactive");
@@ -37,7 +39,59 @@ function toggleProductDetail() {
 
 const productList = [];
 productList.push({
-    name: "Product 1",
-    price: 100,
-    image: "https://picsum.photos/200/300",
+  name: "Switch",
+  price: 100,
+  image:
+    "https://www.abasteo.mx/out/productdescription/iframe/CISCO/SF110-24-NA/images/SF110-24-NA-1.png",
 });
+productList.push({
+  name: "Router",
+  price: 200,
+  image: "https://m.media-amazon.com/images/I/71CWnEDHVxL.jpg",
+});
+productList.push({
+  name: "Modem",
+  price: 300,
+  image: "https://m.media-amazon.com/images/I/81H48PPVpmL.jpg",
+});
+productList.push({
+  name: "Cable",
+  price: 400,
+  image: "https://m.media-amazon.com/images/I/61Nj6nHbCvL._AC_SL1500_.jpg",
+});
+productList.push({
+  name: "Adapter",
+  price: 500,
+  image: "https://m.media-amazon.com/images/I/61Yx9YT9gbL.jpg",
+});
+
+function renderProducts(productListArray) {
+  for (product of productListArray) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+    const img = document.createElement("img");
+    img.src = product.image;
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+    const div = document.createElement("div");
+    const price = document.createElement("p");
+    price.textContent = "$" + product.price;
+    const name = document.createElement("p");
+    name.textContent = product.name;
+    const figure = document.createElement("figure");
+    const img2 = document.createElement("img");
+    img2.src = "./icons/bt_add_to_cart.svg";
+
+    figure.appendChild(img2);
+    div.appendChild(price);
+    div.appendChild(name);
+    productInfo.appendChild(div);
+    productInfo.appendChild(figure);
+    productCard.appendChild(img);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productList);
