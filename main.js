f613bc804ed5d3f99532a8cf1cdfe0d7f6991ac2
Felipe Vanegas <vanegasfelipe1101@gmail.com>
2022-11-28 11:40:39 -0500

    Se corrige el menu para mobile y adicional se integra la vista detallada del producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -12,10 +12,10 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains ('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains ('inactive');
 
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
@@ -23,10 +23,10 @@ function toggleDesktopMenu() {
 
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains ('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains ('inactive');
 
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -44,7 +44,7 @@ function toggleCarritoAside() {
     }
     
 
-   aside.classList.toggle('inactive');
+   shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
