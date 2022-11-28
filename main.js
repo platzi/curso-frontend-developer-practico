7f1898ad15cbf4c5518923591d7dcfdca8c62a45
Brayan Galeano <bsgaleanol@correo.udistrital.edu.co>
2022-11-27 22:46:59 -0600

    Updates to info

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ navbarEmail.addEventListener("click", toggleDesktopMenu);
 menu.addEventListener("click", toggleMobileMenu);
 navbarShoppingCart.addEventListener("click", toggleProductDetail);
 
+const logo = document.querySelector(".logo");
+logo.addEventListener("click", openNewPage);
+function openNewPage() {
+  window.open("https://cursos.fie.umich.mx/");
+}
+
 const cardsContainer = document.querySelector(".cards-container");
 
 function toggleDesktopMenu() {
@@ -42,29 +48,44 @@ productList.push({
   name: "Switch",
   price: 100,
   image:
-    "https://www.abasteo.mx/out/productdescription/iframe/CISCO/SF110-24-NA/images/SF110-24-NA-1.png",
+    "https://cdn-icons-png.flaticon.com/512/1089/1089006.png",
 });
 productList.push({
   name: "Router",
   price: 200,
-  image: "https://m.media-amazon.com/images/I/71CWnEDHVxL.jpg",
+  image:
+    "https://cdn-icons-png.flaticon.com/512/2972/2972473.png",
 });
 productList.push({
   name: "Modem",
   price: 300,
-  image: "https://m.media-amazon.com/images/I/81H48PPVpmL.jpg",
+  image: "https://cdn-icons-png.flaticon.com/512/270/270516.png",
 });
 productList.push({
   name: "Cable",
   price: 400,
-  image: "https://m.media-amazon.com/images/I/61Nj6nHbCvL._AC_SL1500_.jpg",
+  image: "http://cdn.onlinewebfonts.com/svg/img_488919.png",
 });
 productList.push({
-  name: "Adapter",
+  name: "Adaptador",
   price: 500,
-  image: "https://m.media-amazon.com/images/I/61Yx9YT9gbL.jpg",
+  image: "https://cdn-icons-png.flaticon.com/512/1716/1716265.png",
 });
-
+productList.push({
+  name: "Servidor",
+  price: 600,
+  image: "https://cdn-icons-png.flaticon.com/512/1122/1122581.png",
+});
+productList.push({
+  name: "CV",
+  price: 700,
+  image: "https://cdn-icons-png.flaticon.com/512/3076/3076343.png",
+});
+productList.push({
+  name: "Seguridad",
+  price: 800,
+  image: "https://cdn-icons-png.flaticon.com/512/4803/4803145.png",
+});
 function renderProducts(productListArray) {
   for (product of productListArray) {
     const productCard = document.createElement("div");
