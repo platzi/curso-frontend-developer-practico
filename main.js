09527b20568f7fb94778951ee5b9f5ee338c27ef
Nicolás Bossi <nicofbossi@gmail.com>
2022-11-28 17:58:44 -0300

    desplegar los menú mobile, desktop y del carrito

--- main.js
+++ main.js
@@ -23,12 +23,42 @@ let carritoIconMenu = document.querySelector(".icon-product-detail");
 let productDetailAside = document.querySelector(".product-detail");
 
 
-const toogleDesktopMenu = function () { // quitar y agregar la clase inactive
-    desktopMenu.classList.toggle("inactive");
+const toogleDesktopMenu = function () { // agregar el menú de escritorio solo sí los otros están cerrados.
+    const isProductDetailOpen = productDetailAside.classList.contains("inactive");
+    const closedProductDetail = productDetailAside.classList.add("inactive");
+    const openDesktopMenu = desktopMenu.classList.toggle("inactive");
+
+    if (isProductDetailOpen) {
+        closedProductDetail;
+    }
+    openDesktopMenu;
 }
 
-const toogleMobileMenu = function () {
-    mobileMenu.classList.toggle("inactive");
+const toogleMobileMenu = function () { // agregar el menú de mobil solo sí los otros están cerrados.
+    const isProductDetailOpen = productDetailAside.classList.contains("inactive");
+    const closedProductDetail = productDetailAside.classList.add("inactive");
+    const openMobileMenu = mobileMenu.classList.toggle("inactive");
+
+    if (isProductDetailOpen) {
+        closedProductDetail;
+    }
+    openMobileMenu;
+}
+
+const toogleProductDetail = function () { // agregar el menu de articulos solo sí los otros están cerrados.
+    const isMobileMenuOpen = mobileMenu.classList.contains("inactive");
+    const isDesktopMenuOpen = desktopMenu.classList.contains("inactive");
+    const closedDesktopMenu = desktopMenu.classList.add("inactive");
+    const closedMobileMenu = mobileMenu.classList.add("inactive");
+    const openProductDetail = productDetailAside.classList.toggle("inactive");
+
+    if (isMobileMenuOpen) {
+        closedMobileMenu;
+    }
+    if (isDesktopMenuOpen) {
+        closedDesktopMenu;
+    }
+    openProductDetail;
 }
 
 navEmail.addEventListener("click", toogleDesktopMenu);
