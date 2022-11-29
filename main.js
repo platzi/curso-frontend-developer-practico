456abc274178b888403e09b05b4a70aa57131414
lmorozco <lorozco@valizant.com>
2022-11-29 15:58:37 -0800

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileNavOptions = document.querySelector(".menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+mobileNavOptions.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(){
   desktopMenu.classList.toggle("inactive");
 }
 
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+}
