4474c3154c1d470dd24a47a3a46750b2f05b0551
Pedro Alvear <pedroluisalvear@gmail.com>
2022-11-28 23:41:02 -0500

    Menu

---
+++ main.js
@@ -0,0 +1,43 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed){
+        aside.classList.add('inactive'); 
+    }
+    desktopMenu.classList.toggle('inactive');
+
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed){
+        aside.classList.toggle('inactive'); 
+    }
+    
+    mobileMenu.classList.toggle('inactive');
+}
+
+
+
+function toggleCarritoAside() {
+const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+if (!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive');
+} else { (!isDesktopMenuClosed) 
+    desktopMenu.classList.add('inactive');
+}
+    aside.classList.toggle('inactive');
+}
+
