8f60b64193d7e25dab984b364cbcf8c244c43a2d
Simon Rojas <sdavid.rojas@hotmail.com>
2022-11-29 13:08:31 -0500

    Implementación menu en mobile y ajuste de media querys

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 
-const menuEmail = document.querySelector('.navbar-email');
+const menuEmail   = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu  = document.querySelector('.mobile-menu');
+ 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     // toggle quita o pone la clase, dependiendo de si la tiene o no
-    desktopMenu.classList.toggle('inactive'); 
-    
+    desktopMenu.classList.toggle('inactive');     
+}
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
