884d9b5b4916dbffc0021fff4599957fa15d49ed
Simon Rojas <sdavid.rojas@hotmail.com>
2022-11-29 21:29:33 -0500

    Implementación lista de productos en un array

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu  = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
- 
+const cardContainer = document.querySelector('.cards-container');
+
+const productList = [];
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
@@ -24,4 +27,59 @@ function toggleMobileMenu(){
 function toggleCarritoAside() {
     aside.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
+}
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+for( product of productList ){
+    // Se crea el contenedor padre
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    cardContainer.appendChild(productCard);
+
+    // Se crea la etiqueta img y se le añade el src
+    const ProductImg = document.createElement('img');
+    ProductImg.setAttribute('src', product.image );
+    productCard.appendChild(ProductImg);
+
+    // Se crea el contenedor hijo
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+    productCard.appendChild(productInfo);
+
+    // Se crean los demás elementos de la card y se agregan los elementos a la card container
+    const childDivInfo = document.createElement('div');
+    productInfo.appendChild(childDivInfo);
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$'+product.price;
+    childDivInfo.appendChild(productPrice);
+
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+    childDivInfo.appendChild(productName);
+
+    const productFigure = document.createElement('figure');
+    productInfo.appendChild(productFigure);
+
+    const imgCar = document.createElement('img'); 
+    imgCar.setAttribute('src','./icons/bt_add_to_cart.svg' );
+    productFigure.appendChild(imgCar);    
+
 }
