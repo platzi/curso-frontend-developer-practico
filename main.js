bc350cdc374562ad016ffccf7d6fe0e9a310f052
carloandres11 <gomezh.carlos1@gmail.com>
2022-11-30 11:08:25 -0500

    Add Shopping and Logic

--- main.js
+++ main.js
@@ -2,13 +2,35 @@ const email = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const BurgerBtn = document.querySelector(".menu");
 const BurgerMenu = document.querySelector(".mobile-menu");
+const ShoppingBtn = document.querySelector(".navbar-shopping-cart");
+const shoppingMenu = document.querySelector(".product-detail");
 
 email.addEventListener("click", toggleDesktopMenu);
 BurgerBtn.addEventListener("click", toggleMobileMenu);
+ShoppingBtn.addEventListener("click", toggleShoppingMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+  let DesktopMenuOpen = shoppingMenu.classList.contains("inactive");
+  if (!DesktopMenuOpen) {
+    shoppingMenu.classList.add("inactive");
+  }
 }
 function toggleMobileMenu() {
   BurgerMenu.classList.toggle("inactive");
+  let BurgerOpen = shoppingMenu.classList.contains("inactive");
+  if (!BurgerOpen) {
+    shoppingMenu.classList.add("inactive");
+  }
+}
+function toggleShoppingMenu() {
+  shoppingMenu.classList.toggle("inactive");
+  let ShopOpen = BurgerMenu.classList.contains("inactive");
+  let ShopOpenforDesk = desktopMenu.classList.contains("inactive");
+  if (!ShopOpenforDesk) {
+    desktopMenu.classList.add("inactive");
+  }
+  if (!ShopOpen) {
+    BurgerMenu.classList.add("inactive");
+  }
 }
