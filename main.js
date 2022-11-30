bc6db6376cb3bc233a22a5d61cf701f804f77e75
Simon Rojas <sdavid.rojas@hotmail.com>
2022-11-29 19:13:56 -0500

    Implementación del carrito de compras

--- main.js
+++ main.js
@@ -3,16 +3,25 @@ const menuEmail   = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu  = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
  
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
     // toggle quita o pone la clase, dependiendo de si la tiene o no
-    desktopMenu.classList.toggle('inactive');     
+    desktopMenu.classList.toggle('inactive');    
+    aside.classList.add('inactive');   
 }
 
-menuHamIcon.addEventListener('click', toggleMobileMenu);
-
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+    mobileMenu.classList.toggle('inactive');  
+    aside.classList.add('inactive');  
+}
+
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
 }
