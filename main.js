3fe53f791ec8821c996fc6d76a986d0404c9a3e6
Amado Dubon <desshinra@gmail.com>
2022-11-29 20:37:47 -0600

    was created HTML tags with classes in JS  to create more practical products

--- main.js
+++ main.js
@@ -1,16 +1,109 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuShoppingCartIcon = document.querySelector('.navbar-shopping-cart')
 const mobileIconMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+mobileIconMenu.addEventListener('click', toggleMobileMenu);
+menuShoppingCartIcon.addEventListener('click', toggleAsideCart)
+
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+    if(!isAsideClosed) {
+        aside.classList.add('inactive') 
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
-mobileIconMenu.addEventListener('click', toggleMobileMenu);
-
 function toggleMobileMenu () {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')
 }
+
+
+function toggleAsideCart () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    
+    if(!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Television',
+    price: 320,
+    image: 'https://images.pexels.com/photos/6968875/pexels-photo-6968875.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Speaker',
+    price: 200,
+    image: 'https://images.pexels.com/photos/977296/pexels-photo-977296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+
+function renderProducts(arr) {
+    for (product of productList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        //a = {name, price, image, etc ...} -> product.image
+        const productImage = document.createElement('img');
+        productImage.setAttribute('src', product.image)
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price; 
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+        
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+        
+        
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+        
+        productInfoFigure.appendChild(productImgCart);
+    
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+    
+        productCard.appendChild(productImage);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
