93ca44689b503d91b7622e674e396e11b81eb998
Nicolás Bossi <nicofbossi@gmail.com>
2022-12-01 15:49:13 -0300

    Creación de contenedores de productos con contenido dinamico desde JavaScript

--- main.js
+++ main.js
@@ -21,46 +21,94 @@ let desktopMenu = document.querySelector(".desktop-menu");
 let mobileMenu = document.querySelector(".mobile-menu");
 let carritoIconMenu = document.querySelector(".icon-product-detail");
 let productDetailAside = document.querySelector(".product-detail");
+let cardsContainer = document.querySelector(".cards-container")
+const productList = [];
+productList.push({
+    name: "bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: "CPU",
+    price: 1500,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: "car",
+    price: 5000,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
 
+const showProducts = function (arr) { // Crear contenedor de los productos con contenido dinamico 
+    for (let product of arr ) {
+        const productCard = document.createElement( "div" );
+        const imagen = document.createElement("img");
+        const productInfo = document.createElement( "div" );
+        const productInfoContainer = document.createElement( "div" );
+        const productPrice = document.createElement( "p" );
+        const productName = document.createElement( "p" );
+        const iconContainer = document.createElement( "figure" );
+        const icon = document.createElement( "img" );
+
+        productCard.classList.add( "product-card" );
+        imagen.setAttribute( "src", product.image );
+        imagen.setAttribute( "alt", `Imagen de una ${product.name}` );
+        productInfo.setAttribute( "class", "product-info" );
+        icon.setAttribute( "src", "./icons/bt_add_to_cart.svg" );
+        icon.setAttribute( "alt", "icono" );
+        productPrice.innerText = "$" + product.price;
+        productName.innerText = "$" + product.name;
+
+        productCard.appendChild( imagen );
+        productCard.appendChild( productInfo );
+        productInfo.appendChild( productInfoContainer );
+        productInfoContainer.appendChild( productPrice );
+        productInfoContainer.appendChild( productName );
+        productInfo.appendChild( iconContainer );
+        iconContainer.appendChild( icon );
+        cardsContainer.appendChild( productCard );
+    }
+}
 
 const toogleDesktopMenu = function () { // agregar el menú de escritorio solo sí los otros están cerrados.
-    const isProductDetailOpen = productDetailAside.classList.contains("inactive");
-    const closedProductDetail = productDetailAside.classList.add("inactive");
-    const openDesktopMenu = desktopMenu.classList.toggle("inactive");
+    const isProductDetailOpen = productDetailAside.classList.contains( "inactive" );
+    const closedProductDetail = productDetailAside.classList.add( "inactive" );
+    const openDesktopMenu = desktopMenu.classList.toggle( "inactive" );
 
-    if (isProductDetailOpen) {
+    if ( isProductDetailOpen ) {
         closedProductDetail;
     }
     openDesktopMenu;
 }
 
 const toogleMobileMenu = function () { // agregar el menú de mobil solo sí los otros están cerrados.
-    const isProductDetailOpen = productDetailAside.classList.contains("inactive");
-    const closedProductDetail = productDetailAside.classList.add("inactive");
-    const openMobileMenu = mobileMenu.classList.toggle("inactive");
+    const isProductDetailOpen = productDetailAside.classList.contains( "inactive" );
+    const closedProductDetail = productDetailAside.classList.add( "inactive" );
+    const openMobileMenu = mobileMenu.classList.toggle( "inactive" );
 
-    if (isProductDetailOpen) {
+    if ( isProductDetailOpen ) {
         closedProductDetail;
     }
     openMobileMenu;
 }
 
 const toogleProductDetail = function () { // agregar el menu de articulos solo sí los otros están cerrados.
-    const isMobileMenuOpen = mobileMenu.classList.contains("inactive");
-    const isDesktopMenuOpen = desktopMenu.classList.contains("inactive");
-    const closedDesktopMenu = desktopMenu.classList.add("inactive");
-    const closedMobileMenu = mobileMenu.classList.add("inactive");
-    const openProductDetail = productDetailAside.classList.toggle("inactive");
+    const isMobileMenuOpen = mobileMenu.classList.contains( "inactive" );
+    const isDesktopMenuOpen = desktopMenu.classList.contains( "inactive" );
+    const closedDesktopMenu = desktopMenu.classList.add( "inactive" );
+    const closedMobileMenu = mobileMenu.classList.add( "inactive" );
+    const openProductDetail = productDetailAside.classList.toggle( "inactive" );
 
-    if (isMobileMenuOpen) {
+    if ( isMobileMenuOpen ) {
         closedMobileMenu;
     }
-    if (isDesktopMenuOpen) {
+    if ( isDesktopMenuOpen ) {
         closedDesktopMenu;
     }
     openProductDetail;
 }
 
+showProducts(productList);
 navEmail.addEventListener("click", toogleDesktopMenu);
 mobileIconMenu.addEventListener("click", toogleMobileMenu);
 carritoIconMenu.addEventListener("click", toogleProductDetail);
