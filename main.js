636b6b1215f5306716dcda2581d224cf9706cedd
carloandres11 <gomezh.carlos1@gmail.com>
2022-11-30 19:25:32 -0500

    Add ProductList

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const BurgerBtn = document.querySelector(".menu");
 const BurgerMenu = document.querySelector(".mobile-menu");
 const ShoppingBtn = document.querySelector(".navbar-shopping-cart");
 const shoppingMenu = document.querySelector(".product-detail");
+const containerCards = document.querySelector(".cards-container");
 
 email.addEventListener("click", toggleDesktopMenu);
 BurgerBtn.addEventListener("click", toggleMobileMenu);
@@ -34,3 +35,66 @@ function toggleShoppingMenu() {
     BurgerMenu.classList.add("inactive");
   }
 }
+
+const productList = [];
+
+productList.push({
+  name: "Bike",
+  price: "120",
+  addShopping:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Phone",
+  price: "820",
+  addShopping:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Monitor",
+  price: "320",
+  addShopping:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Stereo",
+  price: "420",
+  addShopping:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProduct(arr) {
+  for (product of arr) {
+    const divCard = document.createElement("div");
+    divCard.classList.add("product-card");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", product.addShopping);
+    divCard.appendChild(img);
+
+    const divInfo = document.createElement("div");
+    divInfo.classList.add("product-info");
+    divCard.appendChild(divInfo);
+
+    const divOnly = document.createElement("div");
+    divInfo.appendChild(divOnly);
+
+    const productPrice = document.createElement("p");
+    productPrice.innerHTML = "$" + product.price;
+    divOnly.appendChild(productPrice);
+
+    const productName = document.createElement("p");
+    productName.innerHTML = product.name;
+    divOnly.appendChild(productName);
+
+    const figure = document.createElement("figure");
+    divInfo.appendChild(figure);
+
+    const icon = document.createElement("img");
+    icon.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    figure.appendChild(icon);
+
+    containerCards.appendChild(divCard);
+  }
+}
+renderProduct(productList);
