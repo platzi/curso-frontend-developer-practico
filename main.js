8180022d1ceb29c172677a1b4f2196a03082f861
Rxgnxrz <difervm30@gmail.com>
2022-12-01 13:43:44 -0500

    fusion de menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector(".navbar-email");
-const desktopMenu= document.querySelector(".desktop-menu");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuhamburguesa = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuhamburguesa.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
