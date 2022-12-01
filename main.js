36e79ee1931144637f491c57a8b2cb37d2a8a3a8
MarianAlpha <mariana.paniagual@udea.edu.co>
2022-11-30 23:37:17 -0500

    Detalles de producto, correccion de aside

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarrito = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const aside = document.querySelector("#shopping-cart");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
