5cce14b9c313cfd2b8550f62abfed1cae3593b4f
Ernesto & Mely <ehvtrumpet2011@gmail.com>
2022-12-01 17:15:04 +0200

    Agregando Desktop y Mobile menu

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu') 
+const menuHamburg = document.querySelector('.menu') 
+const mobileMenu = document.querySelector('.mobile-menu') 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu)   //mouseover para que sea parandose encima con el mouse
+menuHamburg.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
+}
+
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+    if(!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
 }
