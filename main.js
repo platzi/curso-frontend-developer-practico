57191cd804562e0ea189a478c83c542a5126b217
Westhly Sarabia <westhly-carrillo@outlook.com>
2022-12-02 15:25:10 -0500

    fusion del carrito de compra, articulos

--- main.js
+++ main.js
@@ -1,15 +1,113 @@
 const menuEmail =document.querySelector('.navbar-email');
 const desktopMenu =document.querySelector('.desktop-menu');
+const menuCarritoIcon =document.querySelector('.navbar-shopping-cart');
 const menuHamIcon =document.querySelector('.menu');
 const mobileMenu =document.querySelector('.mobile-menu');
+const aside =document.querySelector('.product-detail');
+const cardsContainer =document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', togglemobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive'); 
+    
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    
+    mobileMenu.classList.toggle('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function togglemobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive'); 
+    
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    
     mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+  
+    if (!isMobileMenuClosed){
+            mobileMenu.classList.add('inactive')}
+
+    aside.classList.toggle('inactive');
+    }
+
+    const productList = [];
+    productList.push({
+        name: 'bicicleta',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    });
+    productList.push({
+        name: 'Pantalla',
+        price: 220,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    });
+    productList.push({
+        name: 'Computador',
+        price: 620,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    });
+    productList.push({
+        name: 'bicicleta',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    });
+    productList.push({
+        name: 'Pantalla',
+        price: 220,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    });
+    productList.push({
+        name: 'Computador',
+        price: 620,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    });
+
+
+    for (product of productList){
+       const productCard =document.createElement('div');
+       productCard.classList.add('product-card');
+       const productImg = document.createElement('img');
+       productImg.setAttribute('src', product.image);
+
+       const productInfo = document.createElement('div');
+       productInfo.classList.add('product-info');
+
+       const productInfoDiv =document.createElement('div');
+       const productprice = document.createElement('p');
+        productprice.innerText = '$' + product.price;
+       const productname = document.createElement('p');
+       productname.innerText = product.name;
+
+       productInfoDiv.appendChild(productprice);
+       productInfoDiv.appendChild(productname);
+
+       const productInfoFigure = document.createElement('figure');
+       const productImgCart = document.createElement('img');
+       productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+       productInfoFigure.appendChild(productImgCart);
+
+       productInfo.appendChild(productInfoDiv);
+       productInfo.appendChild(productInfoFigure);
+
+       productCard.appendChild(productImg);
+       productCard.appendChild(productInfo);
+       
+       cardsContainer.appendChild(productCard);
+    
+       
+
+       
+    }
