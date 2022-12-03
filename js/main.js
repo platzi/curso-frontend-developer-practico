addee0b29ab0e26a869f9102f158bf6b5cd8d5de
Luis David Cornejo <luisdavidcorbra24@gmail.com>
2022-12-03 13:49:17 -0500

    update main.js dinamismo entre menús
    - Se han agregado funciones de dinamismo entre menús, para alternar entre éllos.
    
    - Se ha creado una lista de productos. Mediante un for, cada producto es añadido al DOM desde js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,114 @@
 const menuEmail = document.getElementById('navbar-email')
 const desktopMenu = document.getElementById('desktop-menu')
 const menuHamIcon = document.getElementById('menu')
+const menuCarritoIcon = document.getElementById('navbar-shopping-cart')
 const mobileMenu = document.getElementById('mobile-menu')
+const aside = document.getElementById('product-detail')
+const cardsContainer = document.getElementById('cards-container')
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.toggle('inactive')
+    }
+
+    aside.classList.toggle('inactive')
+}
+
+const productList = []
+productList.push({
+    name: 'bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'computer',
+    price: 1200,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'car',
+    price: 10000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+renderProducts(productList)
+
+function renderProducts(array) {
+
+    for (product of array){
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+    
+        const productImg = document.createElement('img')
+        productImg.setAttribute('src', product.image)
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+    
+        const productInfoDiv = document.createElement('div')
+    
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price
+    
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+    
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+        const productInfoFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productImgCart)
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+    
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+    
+        cardsContainer.appendChild(productCard)
+    }
     
 }
