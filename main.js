0ae98dba1c7f681a4e88f82f54245cfcf1b403d6
Plunket01 <oskar@mastedoy.es>
2022-12-03 21:59:15 +0100

    Clase 20, Carrito de compras

--- main.js
+++ main.js
@@ -2,15 +2,39 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
+  const isAsideOpen = !aside.classList.contains("inactive");
+  if (isAsideOpen) {
+    aside.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
 }
 
-function toggleMobileMenu () {
+function toggleMobileMenu() {
+  const isAsideOpen = !aside.classList.contains("inactive");
+  if (isAsideOpen) {
+    aside.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuOpen = !mobileMenu.classList.contains("inactive");
+  const isDesktopMenuOpen = !desktopMenu.classList.contains("inactive");
+  if (isMobileMenuOpen) {
+    mobileMenu.classList.add("inactive");
+  }
+  if (isDesktopMenuOpen) {
+    desktopMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
 }
