3a617c5fbcbe064ccc060e0591665c574795bc00
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2022-12-03 21:47:08 -0300

    Agregue funcionalidad de al presionar la imagen del producto nos muestra la información correcta

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector('.cards-container');
 const productDetailContainer = document.querySelector('#productDetail');
-const closeBottonDetail = document.querySelector('.product-detail-close')
+const closeBottonDetail = document.querySelector('.product-detail-close');
+const imageProductDetail = document.querySelector('.image-product-detail');
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenu.addEventListener("click", toggleMobileMenu);
@@ -56,6 +57,7 @@ productList.push({
   price: 120,
   image:
     "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  description: 'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.'
 });
 
 productList.push({
@@ -63,6 +65,7 @@ productList.push({
   price: 220,
   image:
     "https://images.pexels.com/photos/2049411/pexels-photo-2049411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  description: 'Beautiful monitor.'
 });
 
 productList.push({
@@ -70,24 +73,29 @@ productList.push({
   price: 80,
   image:
     "https://images.pexels.com/photos/1772123/pexels-photo-1772123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  description: 'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.'
 });
 
 productList.push({
     name: 'Mouse',
     price: 50,
-    image: 'https://images.pexels.com/photos/2115256/pexels-photo-2115256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/2115256/pexels-photo-2115256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.'
 })
 
 /*CREACIÓN DE LAS PRODUCT CARDS HTML*/
 
 function renderProducts(arr){
-    for (product of arr) {
+    for (const [index,product] of arr.entries()) {
         const productCard = document.createElement("div");
         productCard.classList.add("product-card");
       
         const productImg = document.createElement("img");
         productImg.setAttribute("src", product.image);
-        productImg.addEventListener('click', openProductDetailContainer)
+        // productImg.addEventListener('click', openProductDetailContainer);
+        productImg.addEventListener("click", e => {
+          showProductDetailsContainer(e, productInfo, index, product)
+        });
       
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
@@ -114,6 +122,18 @@ function renderProducts(arr){
         cardsContainer.appendChild(productCard);
       }
       
+}
+
+function showProductDetailsContainer(e, productInfo, index, product){
+  productDetailContainer.classList.remove('inactive');
+  shoppingCartContainer.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+
+  imageProductDetail.setAttribute('src', e.target.src);
+  productDetailContainer.children[2].children[0].innerText = `$${productList[index].price}`;
+  productDetailContainer.children[2].children[1].innerText = productList[index].name;
+  productDetailContainer.children[2].children[2].innerText = productList[index].description;
 }
 
 renderProducts(productList);
