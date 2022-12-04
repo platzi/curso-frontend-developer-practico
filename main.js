4e406cc391b77b4bd6f72ec82b6794b555fa1e22
Diego patiño <diegoduvan321@gmail.com>
2022-12-04 17:16:38 -0500

    se trabajo con el carrito de compras

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-const menuEmain = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
-menuEmain.addEventListener('click', toggleDesktopMenu);
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 function toggleDesktopMenu() {
-    console.log('click');
- desktopMenu.classList.toggle('inactive');
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+  
+  desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive'); 
+  }
+  
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isDesktopMenu = desktopMenu.classList.contains('inactive');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.toggle('inactive'); 
+  }else if (!isDesktopMenu){
+    desktopMenu.classList.toggle('inactive');
+  }
+  
+  aside.classList.toggle('inactive');
 }
