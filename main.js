30bf29bd4184ddd9fac9315633e7f694d07d6ecd
bryhans <bryhansalazarpacsi@gmail.com>
2022-12-03 23:45:39 -0500

    mobile-menu

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
-menuEmail.addEventListener('click',toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toogleMobileMenu);
+
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
