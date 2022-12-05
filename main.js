354beeb28d1408c14d8239c44c3750de0fa1e79a
Olivel Cantoran Gonzalez <ocg101010@gmail.com>
2022-12-04 22:22:52 -0600

    Agregando funcionalidad de poner y quitar menu en Mobile Desktop

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Agregando quitar y poner Display: none para poner y quitar el menu
+//Agregando quitar y poner Display: none para poner y quitar el Menu Desktop
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
@@ -7,4 +7,15 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive')
+}
+
+
+//Agregando quitar y poner Display: none para poner y quitar el Menu Mobile
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive')
 }
