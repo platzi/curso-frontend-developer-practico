8785506b0640692a196bf2c1ca8c43e2cc6cdd3a
Fabian Beltran <joshefbeltran@gmail.com>
2022-12-05 16:00:00 -0500

    Finished the toggle menus

--- main.js
+++ main.js
@@ -4,14 +4,43 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const hamMenuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamMenuIcon.addEventListener('click', toggleMobileMenu);
+menuCarIcon.addEventListener('click', toggleCarrito);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarrito(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isMenuEmailClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMenuEmailClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
