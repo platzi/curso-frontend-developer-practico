3788ac197c362cfc2edee5d7a4a8588251fb6f0f
Olivel Cantoran Gonzalez <ocg101010@gmail.com>
2022-12-05 20:54:50 -0600

    Agregando funcionalidades para cerrar los menus

--- main.js
+++ main.js
@@ -2,20 +2,44 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const producDetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
+  
+  const isAsideMenuClosed = producDetail.classList.contains('inactive')
+
+  if(!isAsideMenuClosed) {
+    producDetail.classList.add('inactive')
+  } 
   desktopMenu.classList.toggle('inactive')
 }
 
-
-//Agregando quitar y poner Display: none para poner y quitar el Menu Mobile
-const menuHamIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+
 function toggleMobileMenu() {
+  const isAsideMenuClosed = producDetail.classList.contains('inactive')
+
+  if(!isAsideMenuClosed) {
+    producDetail.classList.add('inactive')
+  } 
   mobileMenu.classList.toggle('inactive')
 }
+
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside () {
+  const ismobilMenuClosed = mobileMenu.classList.contains('inactive')
+
+  if(!ismobilMenuClosed) {
+    mobileMenu.classList.add('inactive')
+  } 
+  
+  producDetail.classList.toggle('inactive'); 
+}
+
