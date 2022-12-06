0559c7d5457834778e91cacd5f44c0683de988ef
Paolo Rodriguez <calayo13@outlook.com>
2022-12-05 22:35:06 -0500

    HamburgerMenu

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobilMenu);
 
 function toggleDesktopMenu() {
-    
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobilMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
