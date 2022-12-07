4aa2011e38d65559a30761cd6dfe610132a5d5f8
pipemalz <sinister012@gmail.com>
2022-12-06 20:57:49 -0500

    Fusion del menu en desktop

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const navbar_email = document.querySelector('.navbar-email');
 const menu_desplegable = document.querySelector('.desktop-menu');
+const burger_menu = document.querySelector('.menu');
+
+burger_menu.addEventListener('click', () => {
+    menu_desplegable.classList.toggle('inactive');
+})
+
 navbar_email.addEventListener('click', () => {
-    menu_desplegable.classList.toggle('display');
+    menu_desplegable.classList.toggle('inactive');
 });
