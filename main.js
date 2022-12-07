e1f4c2028a1ead39fb4a0c388959792de9990130
esthedav <davidsdnoir@gmail.com>
2022-12-07 17:10:17 -0600

    Clase20: Carrito de compras

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.burguerMenu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+// Desplegables
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed || !isDesktopMenuClosed) {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive')
+}
+
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
 }
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive');
 }
