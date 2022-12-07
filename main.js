99eb0d1a78b82ac38a0d299d73366499475b4320
esthedav <davidsdnoir@gmail.com>
2022-12-07 07:40:44 -0600

    Fusion del menu en Mobile

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.burguerMenu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
+}
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
