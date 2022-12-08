cd86162ce5afb3514d54d07895f6724cb112d82a
Jhovani Aguirre <jhaguirre1107@gmail.com>
2022-12-07 19:52:58 -0800

    Añadi el menu desplegable a la version mobile

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
+
+burgerMenu.addEventListener('click', toggleMobileMenu);
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
-
     desktopMenu.classList.toggle("inactive");
    /*  if(desktopMenu.getAttribute("class")==="desktop-menu inactive"){
     desktopMenu.setAttribute("class", "desktop-menu");
@@ -12,3 +15,9 @@ function toggleDesktopMenu(){
     desktopMenu.setAttribute("class", "desktop-menu inactive");
     } */
 }
+
+function toggleMobileMenu(){
+    console.log("1");
+    mobileMenu.classList.toggle("inactive");
+}
+
