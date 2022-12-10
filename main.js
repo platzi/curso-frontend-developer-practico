f48bae25a909642fdbd923bc6dc2c5cb8b97bfa0
Robert Medina <csoperativo@gmail.com>
2022-12-09 22:48:45 -0400

    tod los navbar funcionan

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const navbarMobileMenu = document.querySelector('.mobile-menu');
+const navbarMobileMenuIcon = document.querySelector('.menu');
 
+navbarMobileMenuIcon.addEventListener('click', toggleMobileMenu);
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
-
     if (desktopMenu.classList.contains('inactive')) {
         navbarEmail.innerText = 'Open';
     } else {
         navbarEmail.innerText = 'Close';
     }
 }
+
+function toggleMobileMenu() {
+    navbarMobileMenu.classList.toggle('inactive');
+}
+
+
 console.log("funcionan js")
 
