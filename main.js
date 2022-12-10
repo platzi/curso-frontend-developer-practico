c6bcb159f3f9d31e72c8b8b1960d8e06f2c6d378
Robert Medina <csoperativo@gmail.com>
2022-12-09 23:26:25 -0400

    carrito listo

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const navbarMobileMenu = document.querySelector('.mobile-menu');
 const navbarMobileMenuIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuCarrito = document.querySelector('.product-detail');
 
 navbarMobileMenuIcon.addEventListener('click', toggleMobileMenu);
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuCarritoIcon.addEventListener('click', toggleCarrito);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
@@ -13,10 +16,28 @@ function toggleDesktopMenu() {
     } else {
         navbarEmail.innerText = 'Close';
     }
+    const isMenuCarritoClosed = menuCarrito.classList.contains('inactive');
+    if (!isMenuCarritoClosed) {
+        menuCarrito.classList.add('inactive');
+    }
 }
 
 function toggleMobileMenu() {
+    const isMenuCarritoClosed = menuCarrito.classList.contains('inactive');
+    if (!isMenuCarritoClosed) {
+        menuCarrito.classList.add('inactive');
+    }
     navbarMobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarrito() {
+    const isNavbarMobileMenuClosed = navbarMobileMenu.classList.contains('inactive');
+    if (!isNavbarMobileMenuClosed) {
+        navbarMobileMenu.classList.add('inactive');
+    }   
+    menuCarrito.classList.toggle('inactive');
+    
+    
 }
 
 
