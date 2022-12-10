8bcd02990091c819b7f64fff1d1d0c821b88bf25
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2022-12-10 19:04:08 -0300

    Con dinamica en menu mobile, desktop y carrito 10.12.2022#

--- main.js
+++ main.js
@@ -21,11 +21,74 @@ function noMostrarMenu() {
 
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const navMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const asideProductDetail = document.querySelector('.product-detail')
 
-navEmail.addEventListener('click', toggleDesktopMenu)
+/* Resuelto con dos funciones: 
+
+navEmail.addEventListener('click', toggleDesktopMenu);
+navMenu.addEventListener('click', toggleMobileMenu);
+
+*/
+
+/* Resuelto con 1 función */
+
+navEmail.addEventListener('click', toggleMenu);
+navMenu.addEventListener('click', toggleMenu);
+shoppingCart.addEventListener('click', toggleMenu);
 
 /* classList.toggle se usa para hacer aparecer o desaparecer la clase que está entre parentesis en función de si esta existe o no al momento de escucharse el evento. Entonces esto hace que cuando está desaparezca y al desaparecer el display none se borra del menu y viceversa. */
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+/* Resuelto con dos funciones: 
+
+function toggleDesktopMenu(event) {
+    console.log(event);
+    desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(event) {
+    console.log(event);
+    mobileMenu.classList.toggle('inactive');
+}
+*/
+
+/* Resuelto con 1 función */
+
+function toggleMenu(event) {
+    /* console.log(event); */
+    if (event.srcElement.className === 'menu') {
+        const isAsideProductDetailClose = asideProductDetail.classList.contains('inactive');
+
+        if (!isAsideProductDetailClose) {
+            asideProductDetail.classList.add('inactive');
+        }
+
+        mobileMenu.classList.toggle('inactive');
+    } else if (event.srcElement.className === 'navbar-email') {
+        const isAsideProductDetailClose = asideProductDetail.classList.contains('inactive');
+
+        if (!isAsideProductDetailClose) {
+            asideProductDetail.classList.add('inactive');
+        }
+
+        desktopMenu.classList.toggle('inactive');
+
+    } else if (event.srcElement.className === 'navbar-shopping-cart') {
+        const isMobileMenuClosed = mobileMenu.classList.contains('inactive'); /* True si el mobileMenu está inactive */
+        const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+
+        if (!isMobileMenuClosed) {
+            mobileMenu.classList.add('inactive');
+        } else if (!isDesktopMenuClosed) {
+            desktopMenu.classList.add('inactive');
+        }
+        
+        /* Si el menú mobile o el menú desktop no están cerrados entonces los cierro. */
+
+        asideProductDetail.classList.toggle('inactive'); /* Y luego hago toggle de inactive para el aside de product detail */
+    }
+}
+
