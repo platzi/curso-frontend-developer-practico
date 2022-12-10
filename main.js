0af1719c1d1058bb7951de942a8c4ea625a4d69f
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2022-12-10 12:20:01 -0500

    Fusion del menu en movile

---
+++ main.js
@@ -0,0 +1,15 @@
+const menuEmail = document.querySelector('.navbar-email');
+const menuDesktop = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleDesktopMenu() {
+    menuDesktop.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
