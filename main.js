7dedabcbaefc46ef7c93f070869cdc11a3b08793
Josue Zamora <josueazc120@hotmail.com>
2022-12-11 15:21:46 -0500

    Carrito de compras añadido y el error a la hora de abrir el menu y el carrito de compras al mismo tiempo solucionado

--- main.js
+++ main.js
@@ -2,14 +2,39 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuIcon = document.querySelector(".menu-icon");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuIcon.addEventListener("click", toggleMobileMenu);
+menuCarIcon.addEventListener("click", toggleCarroAside);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive"); //classList.toggle permite poner o quitar la clase inactive dependiendo si ya la tiene o no
 }
 
 function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
   mobileMenu.classList.toggle("inactive"); //classList.toggle permite poner o quitar la clase inactive dependiendo si ya la tiene o no
+}
+
+function toggleCarroAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive"); // classList.toggle permite poner o quitar la clase inactive dependiendo si ya la tiene o no
 }
