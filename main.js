2b09881d2018f63aaf57e0167f81dd9bebe93ad3
Lili Guerrero <lmguerrero1996@gmail.com>
2022-12-10 22:04:49 -0300

    carrito de compras: se agregan condiciones a las funciones toggleDesktopMenu, toggleMobileMenu y toggleCarritoAside para que no se solapen entre si cuando uno de ellos sea abierto por el usuario mientras que otro de los menu siga abierto en ese momento

--- main.js
+++ main.js
@@ -2,14 +2,36 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHambur = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const CarritoAside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHambur.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isCarritoAsideClosed = CarritoAside.classList.contains('inactive');
+
+    if (!isCarritoAsideClosed) {
+        CarritoAside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isCarritoAsideClosed = CarritoAside.classList.contains('inactive');
+
+    if (!isCarritoAsideClosed) {
+        CarritoAside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    CarritoAside.classList.toggle('inactive');
 }
