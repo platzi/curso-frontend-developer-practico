44f18bd5d44f11b0164fad0b32a565b4e3d2a3f8
Filiberto_Castro <fcastrob@miumg.edu.gt>
2022-12-11 13:55:34 -0600

    add product detail function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuIcon = document.querySelector('.menu');
 const carIconMain = document.querySelector('.navbar-shopping-cart');
+const shopContainerCloseIcon = document.querySelector(".close-shop-container");
 const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
@@ -8,6 +9,8 @@ const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const productDetailContainer = document.querySelector('#product-detail');
 const cardsContainer = document.querySelector('.cards-container');
 let isToggleElementMenu = false;
+
+// funciones para abrir y cerrar los contenedores
 
 function toggleElementMenu(element)
 {
@@ -43,68 +46,100 @@ productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 menuEmail.addEventListener('click', () => toggleElementMenu(desktopMenu));
 menuIcon.addEventListener('click', () => toggleElementMenu(mobileMenu));
 carIconMain.addEventListener('click', () => toggleElementMenu(shoppingCartContainer));
+shopContainerCloseIcon.addEventListener('click', () =>toggleElementMenu(shoppingCartContainer));
 
-//! AGREGANDO PRODUCTOS
+//! agregando productos
 
 const productList = [];
 productList.push({
     name: 'Bike',
     precio: 120,
-    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'La bicicleta es de color negro, cuenta con 20 cambios de velocidad, es completamente nuevo'
 });
 productList.push({
     name: 'Tv',
     precio: 4000,
-    imagen: 'https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'La tv es de color gris, es de marca X, una de las marcas mas antiguas'
 });
 productList.push({
     name: 'Laptop',
     precio: 6000,
-    imagen: 'https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'La laptop es de color gris, es una buena opción para quienes les gusta programar, para quienes aman el código'
 });
 productList.push({
     name: 'Bike',
     precio: 120,
-    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'La bicicleta es de color negro, cuenta con 20 cambios de velocidad, es completamente nuevo'
 });
 productList.push({
     name: 'Tv',
     precio: 4000,
-    imagen: 'https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'La tv es de color gris, es de marca X, una de las marcas mas antiguas'
 });
 productList.push({
     name: 'Laptop',
     precio: 6000,
-    imagen: 'https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'La laptop es de color gris, es una buena opción para quienes les gusta programar, para quienes aman el código'
 });
 productList.push({
     name: 'Bike',
     precio: 120,
-    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'La bicicleta es de color negro, cuenta con 20 cambios de velocidad, es completamente nuevo'
 });
 productList.push({
     name: 'Tv',
     precio: 4000,
-    imagen: 'https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'La tv es de color gris, es de marca X, una de las marcas mas antiguas'
 });
 productList.push({
     name: 'Laptop',
     precio: 6000,
-    imagen: 'https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'La laptop es de color gris, es una buena opción para quienes les gusta programar, para quienes aman el código'
 });
 
-//funcion de maquetacion - lista de productos
+//funcion que manda los datos del producto a la ventana de detalles
+
+const detailsProduct = product => {
+    openProductDetailAside();
+
+    const detailImage = document.querySelector('#product-detail>img');
+    const detailPrice = document.querySelector('.product-info p:nth-child(1)');
+    const detailName = document.querySelector('.product-info p:nth-child(2)');
+    const detailDescription = document.querySelector(
+        '.product-info p:nth-child(3)'
+    );
+
+    detailImage.setAttribute('src', product.imagen);
+    detailPrice.innerText = `$${product.precio}`;
+    detailName.innerText = product.name;
+    detailDescription.innerText = product.description;
+};
+
+//funcion de maquetacion - listar productos al
 
 function renderProducts(arr)
 {
-    for(product of arr)
+    for(let product of arr)
     {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.imagen);
-        productImg.addEventListener('click', openProductDetailAside);
+        productImg.setAttribute('alt', product.name);
+        
+        productImg.addEventListener('click', function(){
+            detailsProduct(product);
+        });
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -132,5 +167,6 @@ function renderProducts(arr)
     }
     
 }
+
 
 renderProducts(productList);
