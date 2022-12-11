a22785216a3b009da66927c248ad791590e616fe
Victor Urdaneta <victorurdaneta11@gmail.com>
2022-12-11 01:19:12 -0300

    mi tercer commit

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
+const productDetailContainer = document.querySelector('#productDetail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu(){
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -26,6 +29,8 @@ function toggleMobileMenu() {
       shoppingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
+
+    closeProductDetailAside();
 }
 
 function toggleCarritoAside() {
@@ -35,7 +40,23 @@ function toggleCarritoAside() {
         mobileMenu.classList.add('inactive');
     }
     shoppingCartContainer.classList.toggle('inactive');
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
+    if (!isProductDetailClosed) {
+      productDetailContainer.classList.add('inactive');
+    }
 }
+
+function openProductDetailAside() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside(){
+  productDetailContainer.classList.add('inactive');
+}
+
 
 const productList = [];
 productList.push({
@@ -70,7 +91,8 @@ productList.push({
       </div> 
       */
 
-for (product of productList) {
+function renderProducts(arr) {
+for (product of arr) {
    const productCard = document.createElement('div');
    productCard.classList.add('product-card');
 
@@ -78,6 +100,7 @@ for (product of productList) {
 
    const productImg = document.createElement('img');
    productImg.setAttribute('src', product.image);
+   productImg.addEventListener('click', openProductDetailAside);
 
    const productInfo = document.createElement('div');
    productInfo.classList.add('product-info');
@@ -108,4 +131,8 @@ productCard.appendChild(productInfo);
 
 cardsContainer.appendChild(productCard);
 
+  }
 }
+
+
+renderProducts(productList);
