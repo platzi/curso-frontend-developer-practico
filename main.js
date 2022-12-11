0899a139e2bf103466b95f30335316e9dd84ea78
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2022-12-11 11:42:11 -0500

    Cerrrando y abriendo para que no se crucen al abrir los botones

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
+// se seleccionan las etiquetas a manipular, tanto las que se les da click y las que muestran
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+// se agregan los eventos a las etiquetas y se le coloca la funcion que debe ejecutar al dar click
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+    }
     menuDesktop.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const iSmobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if (!iSmobileMenuClose) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
