70b9b0d0f67d965839a170fad19c4ab5a34bfeb7
Santyluito <santyluito@gmail.com>
2022-12-11 20:55:50 -0500

    Integrando mobile menu en el lado derecho

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const navEmail = document.querySelector('.navbar-email');
 const navEmailSubMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopmenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopmenu() {
   navEmailSubMenu.classList.toggle('inactive')
+};
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive')
 };
