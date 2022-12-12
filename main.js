e4b46d34dd9163b7db8fe1f6efa55925c6fae85c
AndresfGil <andresfgil98@outlook.com>
2022-12-12 17:30:56 -0500

    Add mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', togglemobileMenu);
 
 function toggleDesktopMenu() {
   console.log('Click');
   desktopMenu.classList.toggle('inactive');
+}
+
+function togglemobileMenu(){
+  mobileMenu.classList.toggle('inactive')
 }
