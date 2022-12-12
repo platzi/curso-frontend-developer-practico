ab5a4503c68de60125d644bd721b19c83af0ce85
Carlos Rosado <krlos_1594@yahoo.com>
2022-12-12 14:19:32 -0600

    Se agrego aside y el section cards container

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 
 function toggleDesktopMenu() {
-  console.log('Click');
+  const isAsideClosed = !mobileMenu.classList.contains('inactive');
+
+  if(!isAsideClosed){
+    aside.classList.add('inactive');
+  }
   desktopMenu.classList.toggle('inactive');
 }
+
+
+function toggleMobileMenu() {
+  const isAsideClosed = !mobileMenu.classList.contains('inactive');
+
+  if(!isAsideClosed){
+    aside.classList.add('inactive');
+  }
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+  if(!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+  aside.classList.toggle('inactive');
+}
+
+
+const productList = [];
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
