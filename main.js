d31c9952cfb0ca202e7a4741ec22b5e40f45698b
Andres Ulloa <andresguc@gmail.com>
2022-12-12 20:44:20 -0500

    organize the shopping cart menu

--- main.js
+++ main.js
@@ -13,32 +13,20 @@ menuCartIcon.addEventListener('click', toggleCartAside);
 
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive')
-
-    if (!isAsideClosed) {
-        aside.classList.add('inactive');
-    }
-
-    desktopMenu.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive')
-
+    desktopMenu.classList.add('inactive');
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
-
-    if (!isAsideClosed) {
-        aside.classList.add('inactive');
-    }
 }
 
 function toggleCartAside() {
-    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
-
-    if (!isDesktopMenuClosed) {
-        desktopMenu.classList.add('inactive');
-    }
-
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
     aside.classList.toggle('inactive');
 }
 
