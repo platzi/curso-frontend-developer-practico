e067e9c29a16437e75c7bc79303ceb28dff86d98
AndresfGil <andresfgil98@outlook.com>
2022-12-12 21:24:23 -0500

    Se agrego otro aside

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuCarrito = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('.shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -12,20 +12,20 @@ menuCarrito.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains('inactive')
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
   
   if(!isAsideClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }  
   console.log('Click');
   desktopMenu.classList.toggle('inactive');
 }
 
 function togglemobileMenu(){
-  const isAsideClosed = aside.classList.contains('inactive')
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
 
-  if(!isCarritoClosed) {
-    menuCarrito.classList.add('inactive');
+  if (!isAsideClosed)
+  {shoppingCartContainer.classList.add('inactive');
 }
 
   mobileMenu.classList.toggle('inactive')
@@ -43,7 +43,7 @@ if(!isdesktopClosed) {
     mobileMenu.classList.add('inactive');
 }
 
-aside.classList.toggle('inactive')
+shoppingCartContainer.classList.toggle('inactive')
 }
 
 const productList = []
