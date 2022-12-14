cfd7e6e9074c3e05cc9568ea397094ec9370590d
daylen86 <daylencita@gmail.com>
2022-12-14 00:43:33 -0500

    update menu

---
+++ main.js
@@ -0,0 +1,9 @@
+const navEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+navEmail.addEventListener('click', toogleDesktopMenu);
+
+function toogleDesktopMenu(){  
+    desktopMenu.classList.toggle('inactive');
+
+}
