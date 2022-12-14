ee76e523773f1ab407abeefab37d6fd040c8d1b0
Cesar Ochoa <cesar_8ab@hotmail.com>
2022-12-14 00:14:57 -0600

    Mobile menu component added

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const hamburgerMenu = document.querySelector('.menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+hamburgerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
