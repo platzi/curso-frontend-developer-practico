fd8e3514ef99aad2268c5510f215fccd6268ea79
[Guido] <[guidopr27@hotmail.com]>
2022-12-14 16:26:55 -0600

    Arreglando menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamButton = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamButton.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     console.log("Hey");
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
