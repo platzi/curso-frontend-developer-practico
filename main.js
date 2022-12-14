790d0f96bf09cc45fdf9f1a874fa27b30b21bdce
Felipe Reyes <frsinergia.18@gmail.com>
2022-12-14 12:05:53 -0500

    Fusion del menu en desktop

---
+++ main.js
@@ -0,0 +1,11 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDestopMenu);
+
+function toggleDestopMenu(){
+    console.log('click');
+    desktopMenu.classList.toggle ('inactive');
+    
+    
+}
