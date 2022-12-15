67740ecaba0d6302851cd1c4a0b2c1d157d7415c
MarkedOlive <olivdev4@gmail.com>
2022-12-14 21:44:10 -0400

    Empezando a integrar los archivos html en un solo archivo funcional con js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// Account menu display: 
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+function toggleDesktopMenu () {
+    desktopMenu.classList.toggle('inactive')
+}
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
