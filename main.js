bcf4da04225195e871da7ac567768caf29ec757c
Javier Gutierrez (Yav Gz) <gutierrez.alcantara.javier@gmail.com>
2022-12-15 17:16:23 -0600

    Shopping cart toggle

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const menuEmail =  document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
-
 const burguerMenu = document.querySelector(".burguer-menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const shoppingCart = document.querySelector(".sh-cart-img")
+const asideShCart = document.querySelector(".sh-cart-detail")
 
 menuEmail.addEventListener("click", toggleMenu)
 burguerMenu.addEventListener("click", toggleMenu)
+shoppingCart.addEventListener("click", toggleMenu)
 
 function toggleMenu (event) {
-  if (event.target.className == "navbar-email" ) {
+  if (event.target.className === "navbar-email" ) {
+    asideShCart.classList.add("inactive")
     desktopMenu.classList.toggle("inactive")
-  } else {
+  } else if (event.target.className === "burguer-menu") {
+    asideShCart.classList.add("inactive")
     mobileMenu.classList.toggle("inactive")
+  } else if (event.target.className === "sh-cart-img" ) {
+    desktopMenu.classList.add("inactive")
+    mobileMenu.classList.add("inactive")
+    asideShCart.classList.toggle("inactive")
   }
 }
