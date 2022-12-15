59cc33a41a4df06575943d03f912b343b56a4108
MarkedOlive <olivdev4@gmail.com>
2022-12-14 23:06:50 -0400

    Carrito funcionalidad de despliegue y sinergia con los demas menus

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
 function toggleDesktopMenu () {
+    const isAsideMenuClosed = aside.classList.contains('inactive')
+
+    if(!isAsideMenuClosed) {
+        aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive')
 }
 
@@ -14,7 +19,31 @@ const hamburgerMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 
 function toggleMobileMenu () {
+    const isAsideMenuClosed = aside.classList.contains('inactive')
+
+    if(!isAsideMenuClosed) {
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
 }
 
 hamburgerMenu.addEventListener('click', toggleMobileMenu)
+
+// Menu cart display: 
+const cartMenu = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+
+function toggleCart () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if(!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')
+}
+
+cartMenu.addEventListener('click', toggleCart)
