8193760a485e3ecebd7c960004580614f7d3e239
victicor1989 <losdelsur1947@hotmail.com>
2022-12-16 13:38:58 -0500

    v1.1 se comienza la fusion de las paginas del sitio web de YardSale

---
+++ main.js
@@ -0,0 +1,9 @@
+const navEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+navEmail.addEventListener('click', toggleDesktopMenu)
+
+function toggleDesktopMenu() {
+    console.log('click');
+    desktopMenu.classList.toggle('inactive')
+}
+console.log('js funciona');
