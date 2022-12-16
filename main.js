3e26a37e1f46a145f7a1f99db7c2651f32f96f21
victicor1989 <losdelsur1947@hotmail.com>
2022-12-16 14:22:40 -0500

    v1.2 Fusion del menu en mobile con y agregador de eventos para esconder y mostrar menu

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 navEmail.addEventListener('click', toggleDesktopMenu)
+menuHamIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
-    console.log('click');
     desktopMenu.classList.toggle('inactive')
 }
-console.log('js funciona');
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
