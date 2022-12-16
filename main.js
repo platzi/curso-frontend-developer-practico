af4794ed7db9210f58e79294f451e74e3d4c2eef
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2022-12-16 14:14:56 -0600

    Pequeño cambio de formatos

--- main.js
+++ main.js
@@ -3,14 +3,28 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.burgerMenu');
 const mobileBurgerMenu = document.querySelector('.mobile-menu');
+const shoppingCartBtn = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail')
 
+// Listeners para ventanas flotantes
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCartBtn.addEventListener('click', toggleShoppingCart);
 
+// desplegar menu de cuenta
 function toggleDesktopMenu() {
+    productDetail.classList.add('inactive'); // clase al 'product-detail'
     desktopMenu.classList.toggle('inactive');
 }
-
+// desplegar menu mobile
 function toggleMobileMenu(){
+    productDetail.classList.add('inactive');
     mobileBurgerMenu.classList.toggle('inactive');
+}
+
+// desplegar shopping cart
+function toggleShoppingCart(){
+    mobileBurgerMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive'); // inactive al 'desktop-menu'
+    productDetail.classList.toggle('inactive');
 }
