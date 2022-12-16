4b9b23b6757718d10023b71426f62489a4ff1852
Michael Meza <michalexis099@gmail.com>
2022-12-16 13:59:46 -0600

    Implementación mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+/*+++++++++++++++++++++Setup Desktop++++++++++++++++++++++++++*/
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+/*+++++++++++++++++++++Setup Mobile++++++++++++++++++++++++++*/
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+/*+++++++++++++++++++++addEventListener++++++++++++++++++++++++++*/
+menuEmail.addEventListener('click', toggleDesktopMenu);     /* Desktop menu */
+menuHamIcon.addEventListener('click', toggleMobileMenu);    /* Mobile menu */
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
-    
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
