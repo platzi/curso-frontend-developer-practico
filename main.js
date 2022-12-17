f26a0adc59cab0a1f2c83ffb631e45827ca0f554
Marcelo Rodriguez Gaete <marcelo20022010@hotmail.com>
2022-12-17 16:56:59 -0300

    Agregado de mobile-menu

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const navEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu() {
     DesktopMenu.classList.toggle('inactive')
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
+
+
+
