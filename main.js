f040ac0cfb9c5b5c754c85e3251ab7bb8c2ed0d1
WSquintero <santiagoquinteroperfil@gmail.com>
2022-12-16 21:09:42 -0500

    Agregando eventos click a nav bar y carrito de compras

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurguer = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuShopping = document.querySelector('.navbar-shopping-cart');
+const ordenMenu = document.querySelector('.my-order ');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurguer.addEventListener('click', toggleMobileMenu);
+menuShopping.addEventListener('click', toggleShoppingMenu);
+
+
 
 function toggleDesktopMenu(){
 desktopMenu.classList.toggle('inactive')
-}
+ordenMenu.classList.add('inactive');
+};
+
+
+function toggleMobileMenu(){
+mobileMenu.classList.toggle('inactive')
+ordenMenu.classList.add('inactive');
+};
+
+
+function toggleShoppingMenu(){
+  desktopMenu.classList.add('inactive');
+  ordenMenu.classList.toggle('inactive');
+  mobileMenu.classList.add('inactive');
+  };
+
+  //el método .add agrega automaticamente la clase "inactive", ocultando los menus conforme se van abriendo otros//
+
