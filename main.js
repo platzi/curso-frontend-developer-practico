cc1d0a328836588bf282ba4379a7a803e95a8b5e
Oscar Roman <oscarroman.pe@gmail.com>
2022-12-17 21:55:43 -0500

    Shopping cart added

--- main.js
+++ main.js
@@ -2,18 +2,43 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');    
+    }
 }
 
 function toggleMobileMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');    
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');    
+    }
+
+    aside.classList.toggle('inactive'); 
+    
 }
 
 /*Explicación Open-GPT
