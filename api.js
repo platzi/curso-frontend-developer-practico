b92e3aedc79362a6af4cbf2efcd16cc46c1ac4ad
DeproOs <klaudio_4ever@hotmail.com>
2022-12-19 11:54:30 -0300

    aparición del menu aSide y ejecución para ocultar menus si estan abiertos e interrumpen

--- api.js
+++ api.js
@@ -2,13 +2,25 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aSide = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 function toggleDesktopMenu(){
+    mobileMenu.classList.add("inactive");
+    aSide.classList.add("inactive");
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
+    aSide.classList.add("inactive");
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside(){
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
+    aSide.classList.toggle("inactive");
 }
