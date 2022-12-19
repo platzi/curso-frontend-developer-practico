bcac36ea38c5a0b44eeb9b2286a1acb9fd936c33
Oscar Rojas <oscarrojasb.tec@gmail.com>
2022-12-19 11:24:33 -0500

    Integración del carro de compras en la página y compatibilidad con los demás menu que se despliegan al dar click

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
-const menuEmail = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcom = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarroIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcom.addEventListener('click', toggleMobileMenu);
+menuCarroIcon.addEventListener('click', toogleCarAside);
 
 function toggleDesktopMenu (){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    };
+
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu (){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    };
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toogleCarAside (){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed && !isDesktopMenuClosed){
+        mobileMenu.classList.add('inactive')
+        desktopMenu.classList.add('inactive')
+    } else if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    } else if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    };
+
+    aside.classList.toggle('inactive')
+    
 }
