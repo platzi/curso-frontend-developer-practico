c56585afd90177e396097f55a7bdc68f4ed568b4
Johnny Barboza <barbozafj13@gmail.com>
2022-12-18 21:19:44 -0600

    Integrate produc-detail.html logic in index.html, styles.css and main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const accountMenu = document.querySelector('.account-menu');
+const mobileIconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartMenu = document.querySelector('.product-detail')
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart')
 
 // When user clicks on navbarEmail the idea is to show or hide the AccountMenu
 navbarEmail.addEventListener('click', toggleAccountMenu);
@@ -8,5 +12,22 @@ navbarEmail.addEventListener('click', toggleAccountMenu);
 function toggleAccountMenu(){
     // Using classList it will shows or hides class inactive depending on the current status
     accountMenu.classList.toggle('inactive');
+    shoppingCartMenu.classList.add('inactive');
 }
 
+// Add event when click in the Img menu to display the Mobile menu
+mobileIconMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    shoppingCartMenu.classList.add('inactive');
+}
+
+// Add windows for shopping cart icon
+shoppingCartIcon.addEventListener('click', toggleShoppingCartAside);
+
+function toggleShoppingCartAside(){
+    shoppingCartMenu.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    accountMenu.classList.add('inactive');
+}
