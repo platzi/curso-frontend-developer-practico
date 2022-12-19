89bdaa2db6576b24c9f552dcf5d815e10d8e13df
DeproOs <klaudio_4ever@hotmail.com>
2022-12-19 14:45:58 -0300

    se inserto detalle de la compra al HTML y se arreglaro errores que produjo

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const aSide = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -11,19 +11,19 @@ menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 function toggleDesktopMenu(){
     mobileMenu.classList.add("inactive");
-    aSide.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
-    aSide.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
     mobileMenu.classList.toggle("inactive");
 }
 
 function toggleCarritoAside(){
     mobileMenu.classList.add("inactive");
     desktopMenu.classList.add("inactive");
-    aSide.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
