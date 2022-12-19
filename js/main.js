3d2f107fc1233aff5aa9bf2abd222633fc5e816e
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2022-12-19 11:15:21 -0500

    Fusion menu mobile y carrito de compras, integrando con el resto de conjuntos al momento de cerrar o abrir, tanto en desktop como en mobil

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 //Fusión del menú en desktop
 let navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 navEmail.addEventListener('click',()=>{
-    if(desktopMenu.classList.contains('inactive')){
-        desktopMenu.classList.remove('inactive');
-    } else{
-        desktopMenu.classList.add('inactive');  
+    if(!aside.classList.contains('inactive')){
+        aside.classList.add('inactive');
     }
-   
+    desktopMenu.classList.toggle('inactive');  
+});
+
+
+// Fusion menu en mobile
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+menuHamIcon.addEventListener('click',()=>{
+    if(!aside.classList.contains('inactive')){
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');  
+});
+
+//MY ORDER
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+menuCarritoIcon.addEventListener('click',()=>{
+    if(!mobileMenu.classList.contains('inactive') ||!desktopMenu.classList.contains('inactive')){
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+  
+    aside.classList.toggle('inactive');
 });
