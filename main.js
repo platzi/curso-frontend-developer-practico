436ac808eb9eeae8890c4b7ec33ce13694afab82
SergioAlex2308 <semartinezc23@gmail.com>
2022-12-19 19:21:28 -0500

    Fusion menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuBurgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuBurgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
 	desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu() {
+	mobileMenu.classList.toggle('inactive');
 }
