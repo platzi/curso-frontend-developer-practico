788643186a25c276edfcf6a5084a5e107d97c165
Marcelo Rodriguez Gaete <marcelo20022010@hotmail.com>
2022-12-20 00:40:56 -0300

    Terminamos el carrito de compra

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 const navEmail = document.querySelector('.navbar-email');
-const DesktopMenu = document.querySelector('.desktop-menu')
-const menuHamIcon = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+const DesktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
     DesktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+    
     mobileMenu.classList.toggle('inactive')
 }
 
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    
+    if (!DesktopMenu.classList.contains('inactive')){
+        DesktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive');
+}
 
