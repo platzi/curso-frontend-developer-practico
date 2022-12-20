c6e8b832c3f706cc3c1e8ec0633ee6ad986e22e7
Edward Hernandez <edwardgta4@hotmail.com>
2022-12-20 11:03:57 -0500

    Some fixes

--- script.js
+++ script.js
@@ -1,8 +1,82 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerButton = document.querySelector('.menuMobileIcon');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shopCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsConstainer = document.querySelector('.cards-container');
  
-navbarEmail.addEventListener('click',toggleDesktopMenu);
+navbarEmail.addEventListener('click',() => toggleGeneral(desktopMenu));
+burgerButton.addEventListener('click',() => toggleGeneral(mobileMenu));
+shopCart.addEventListener('click',()=> toggleGeneral(aside));
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+
+function toggleGeneral(item){
+    switch(item){
+        case mobileMenu:
+            item.classList.toggle('inactive');
+            aside.classList.add('inactive');
+            break;
+        case aside:
+            item.classList.toggle('inactive');
+            mobileMenu.classList.add('inactive');
+            desktopMenu.classList.add('inactive');
+            break;
+        case desktopMenu:
+            item.classList.toggle('inactive');
+             aside.classList.add('inactive');
+            break;
+        default:
+            item.classList.toggle('inactive');
+            break;
+    }
 }
+
+const productList = [];
+productList.push({
+name: 'bike',
+price: 120,
+img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'});
+productList.push({
+    name: 'car',
+    price: 140,
+    img: 'https://images.pexels.com/photos/712618/pexels-photo-712618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'});
+productList.push({
+    name: 'motorbike',
+    price: 240,
+    img: 'https://images.pexels.com/photos/4577731/pexels-photo-4577731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'});
+productList.push({
+    name: 'boat',
+    price: 340,
+    img: 'https://images.pexels.com/photos/285937/pexels-photo-285937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'});
+
+function renderProducts(array){
+    for (product of array){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const img = document.createElement('img');
+        img.setAttribute('src',product.img);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$ '+product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.append(productImgCart);
+        productInfoDiv.append(productPrice,productName);
+        productInfo.append(productInfoDiv,productInfoFigure);
+        productCard.append(img, productInfo);
+        cardsConstainer.append(productCard);
+    }
+}
+renderProducts(productList);
