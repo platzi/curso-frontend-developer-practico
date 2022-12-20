e5181162cbcc6f3ec50070a7f8587492825b354a
Johnny Barboza <barbozafj13@gmail.com>
2022-12-19 18:42:58 -0600

    Add product-detail aside

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const navbarEmail = document.querySelector('.navbar-email');
 const accountMenu = document.querySelector('.account-menu');
 const mobileIconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const shoppingCartMenu = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const shoppingCartIcon = document.querySelector('.navbar-shopping-cart')
 const CardsContainer = document.querySelector('.cards-container');
 
@@ -13,7 +13,7 @@ navbarEmail.addEventListener('click', toggleAccountMenu);
 function toggleAccountMenu(){
     // Using classList it will shows or hides class inactive depending on the current status
     accountMenu.classList.toggle('inactive');
-    shoppingCartMenu.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 
 // Add event when click in the Img menu to display the Mobile menu
@@ -21,14 +21,14 @@ mobileIconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
-    shoppingCartMenu.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 
 // Add windows for shopping cart icon
 shoppingCartIcon.addEventListener('click', toggleShoppingCartAside);
 
 function toggleShoppingCartAside(){
-    shoppingCartMenu.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     accountMenu.classList.add('inactive');
 }
