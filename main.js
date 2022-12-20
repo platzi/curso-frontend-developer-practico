b6b1ce245a3c8cbe193e07b14901ed572f6db171
John Ocampo <john.devsigner@gmail.com>
2022-12-19 20:40:36 -0500

    Fusion component navbar and mobile-menu and working

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const burgerMenu = document.querySelector('#burger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 navbarEmail.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive');
+});
+
+burgerMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
 });
