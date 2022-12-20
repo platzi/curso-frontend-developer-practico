2250e593a24e692cdb913f9a5cedc544c5f3c792
daylen86 <daylencita@gmail.com>
2022-12-20 16:21:23 -0500

    interaccion con todas las paginas

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ const barMenu = document.querySelector('.menu');
 const mobileMenuL = document.querySelector('.mobile-menu');
 const car = document.querySelector('.navbar-shopping-cart');
 const ProductDetail = document.querySelector('.product-detail');
+const productDetailSecondary = document.querySelector('.product-detail-secondary');
+const productDetailClose = document.querySelector('.product-detail-close');
 
 barMenu.addEventListener('click', showMenu);
 navEmail.addEventListener('click', toogleDesktopMenu);
 car.addEventListener('click', toogleAddCar);
+productDetailClose.addEventListener('click', closeDetail);
 
 function showMenu(){
   const isCarClosed = ProductDetail.classList.contains('inactive');
   if(!isCarClosed){
       ProductDetail.classList.add('inactive')
   }
+  closeDetail();
    mobileMenuL.classList.toggle('menu-right');
 }
 function toogleAddCar(){
@@ -21,16 +25,26 @@ function toogleAddCar(){
      if(isIconBarClosed){
       mobileMenuL.classList.remove('menu-right')
     }
-    ProductDetail.classList.toggle('inactive');   
+    ProductDetail.classList.toggle('inactive'); 
+    productDetailSecondary.classList.add('inactive');
+
   }
 function toogleDesktopMenu(){  
     const isCarClosed = ProductDetail.classList.contains('inactive');
     if(!isCarClosed){
         ProductDetail.classList.add('inactive')
     }
+    
     desktopMenu.classList.toggle('inactive');
+    
 }
-
+function viewDetail(){    
+    productDetailSecondary.classList.remove('inactive');
+    ProductDetail.classList.add('inactive');   
+}
+function closeDetail(){
+    productDetailSecondary.classList.add('inactive');
+}
 const productList = [];
 productList.push({
     name:'Bike',
@@ -48,32 +62,40 @@ productList.push({
     image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
 
-for(product of productList){
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+function listProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        productCard.addEventListener('click', viewDetail);
 
-    const imgProduct = document.createElement('img');
-    imgProduct.setAttribute('src', product.image);
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
-
-    const details = document.createElement('div');
-    const price = document.createElement('p');
-    price.innerText = '$' + product.price;
-    const name = document.createElement('p');
-    name.innerText = product.name;
-     details.append(price, name);
-     const btnAdd = document.createElement('figure');
-     btnAdd.style.cursor='pointer';
-     const imgAdd = document.createElement('img');
-     imgAdd.setAttribute('src', './icons/bt_add_to_cart.svg');
-     btnAdd.appendChild(imgAdd);
-
-     details.append(price, name);
-     productInfo.append(details, btnAdd);
-     productCard.append(imgProduct, productInfo);
-
-     const cardContainer = document.querySelector('.cards-container');    
-     cardContainer.append(productCard);
-
+       
+        const imgProduct = document.createElement('img');
+        imgProduct.setAttribute('src', product.image);
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const details = document.createElement('div');
+        const price = document.createElement('p');
+        price.innerText = '$' + product.price;
+        const name = document.createElement('p');
+        name.innerText = product.name;
+         details.append(price, name);
+         const btnAdd = document.createElement('figure');
+         btnAdd.style.cursor='pointer';
+         const imgAdd = document.createElement('img');
+         imgAdd.setAttribute('src', './icons/bt_add_to_cart.svg');
+         btnAdd.appendChild(imgAdd);
+    
+         details.append(price, name);
+         productInfo.append(details, btnAdd);
+         productCard.append(imgProduct, productInfo);
+    
+         const cardContainer = document.querySelector('.cards-container');    
+         cardContainer.append(productCard);
+    
+    }
 }
+
+listProducts(productList);
+
+
