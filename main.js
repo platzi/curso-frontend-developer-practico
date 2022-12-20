ba816cc66ab5cc17c073cab704abca3a26c9dd2b
papita <many.d.r.o.2001@hotmail.com>
2022-12-20 16:08:52 -0500

    Se fusionó el componente my order y se arregló el error de sobreponerse con los otros menús

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const myOrder = document.querySelector('.my-order');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 burguerIcon.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleMyOrder);
 
 function toggleDesktopMenu(){
+    myOrder.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    myOrder.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMyOrder(){
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    myOrder.classList.toggle('inactive');
 }
