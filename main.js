501f7e9cac21465aa7e5828d84f26efa0e1aeff3
martincv <sonmartin94@gmail.com>
2022-12-20 16:13:46 -0500

    Integracion del componente del menú mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const iconMobileMenu = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+iconMobileMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
