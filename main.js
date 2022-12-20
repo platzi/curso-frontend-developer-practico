03259a2d6f0860b3fc2a718ec267642beee538f1
John Ocampo <john.devsigner@gmail.com>
2022-12-19 21:02:54 -0500

    added menu cart and product detail working

--- main.js
+++ main.js
@@ -4,10 +4,20 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('#burger-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+
 navbarEmail.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive');
 });
 
 burgerMenu.addEventListener('click', () => {
+    productDetail.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+});
+
+menuCart.addEventListener('click', () => {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    productDetail.classList.toggle('inactive');
 });
