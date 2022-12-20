8cdb9b6c193f206b23484ac0e7e68ba83bdbd644
SergioAlex2308 <semartinezc23@gmail.com>
2022-12-20 17:57:50 -0500

    Arreglo menu shopping y product detail

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const menuBurgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 const menuCartIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 const cardsContainer = document.querySelector('.cards-container');
 
@@ -31,10 +31,10 @@ menuBurgerIcon.addEventListener('click', toggleMobileMenu);
 menuCartIcon.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
-	const isAsideClosed = aside.classList.contains('inactive');
+	const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
 	if (!isAsideClosed) {
-		aside.classList.add('inactive');
+		shoppingCartContainer.classList.add('inactive');
 	}
 
 	desktopMenu.classList.toggle('inactive');
@@ -42,10 +42,10 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
 
-	const isAsideClosed = aside.classList.contains('inactive');
+	const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
 	if (!isAsideClosed) {
-		aside.classList.add('inactive');
+		shoppingCartContainer.classList.add('inactive');
 	}
 
 	mobileMenu.classList.toggle('inactive');
@@ -62,7 +62,7 @@ function toggleCartAside() {
 		desktopMenu.classList.add('inactive');
 	}
 
-	aside.classList.toggle('inactive');
+	shoppingCartContainer.classList.toggle('inactive');
 }
 
 function renderProducts(arr) {
