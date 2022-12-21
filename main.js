270b33e875a6837dd2836ce98aa3946557f173e8
mrluisperez <lkperezospino@gmail.com>
2022-12-21 12:31:27 -0600

    fusion with shopping chart

--- main.js
+++ main.js
@@ -2,13 +2,29 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shopCartIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingChart = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
+    desktopMenu.classList.toggle('inactive');
+
+    shoppingChart.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
 }
 
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle("inactive");
+    mobileMenu.classList.toggle('inactive');
+    
+    shoppingChart.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+}
+
+shopCartIcon.addEventListener('click', toggleShoppingChart);
+function toggleShoppingChart() {
+    shoppingChart.classList.toggle('inactive');
+
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
 }
