0d586d6b885fe512c27d56dafb97d588127ec2e2
leonardo <leogoca1300@gmail.com>
2022-12-21 08:21:57 -0500

    SE integro el prduct detail y se acomodo el css para los dos aside

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -12,11 +12,11 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu()
 {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     
     if(!isAsideClosed)
     {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
@@ -24,11 +24,11 @@ function toggleDesktopMenu()
 
 function toggleMobileMenu()
 {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     
     if(!isAsideClosed)
     {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -49,7 +49,7 @@ function toggleCarritoAside()
         desktopMenu.classList.add('inactive');
     }
     
-    aside.classList.toggle('inactive'); 
+    shoppingCartContainer.classList.toggle('inactive'); 
 }
 
 const productList = [];
