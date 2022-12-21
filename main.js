4176052d6b62d1bd045d567e4fa265331276b74e
cesarocbu <cesar_8ab@hotmail.com>
2022-12-21 17:17:19 -0600

    Added mobile and desktop menu, shopping cart and list of products

--- main.js
+++ main.js
@@ -1,15 +1,133 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-const hamburgerMenu = document.querySelector('.menu')
+const burgerMenu = document.querySelector('.menu')
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
-hamburgerMenu.addEventListener('click', toggleMobileMenu)
+burgerMenu.addEventListener('click', toggleMobileMenu)
+menuShoppingCart.addEventListener('click', toggleShoppingCart)
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
 }
+
+function toggleShoppingCart(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'Pantalla',
+    price: 3440,
+    image: 'https://resources.sears.com.mx/medios-plazavip/fotos/productos_sears1/original/3600792.jpg?scale=350'
+})
+
+productList.push({
+    name: 'Laptop',
+    price: 5200,
+    image: 'https://images.acer.com/is/image/acer/Nitro5_AN51555_WP_RGBKBUK_1_Bl-1?$Product-Cards-XL$'
+})
+
+productList.push({
+    name: 'Laptop',
+    price: 5200,
+    image: 'https://images.acer.com/is/image/acer/Nitro5_AN51555_WP_RGBKBUK_1_Bl-1?$Product-Cards-XL$'
+})
+
+productList.push({
+    name: 'Pantalla',
+    price: 3440,
+    image: 'https://resources.sears.com.mx/medios-plazavip/fotos/productos_sears1/original/3600792.jpg?scale=350'
+})
+
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'Laptop',
+    price: 5200,
+    image: 'https://images.acer.com/is/image/acer/Nitro5_AN51555_WP_RGBKBUK_1_Bl-1?$Product-Cards-XL$'
+})
+productList.push({
+    name: 'Pantalla',
+    price: 3440,
+    image: 'https://resources.sears.com.mx/medios-plazavip/fotos/productos_sears1/original/3600792.jpg?scale=350'
+})
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+
+        const productImg = document.createElement('img')
+        productImg.setAttribute('src', product.image)
+
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+
+        const productInfoDiv = document.createElement('div')
+
+        const productPrice = document.createElement('p')
+        productPrice.innerHTML = '$' + product.price
+
+        const productName = document.createElement('p')
+        productName.innerHTML = product.name
+
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+
+        const productInfoFigure = document.createElement('figure')
+
+        const productImgCard = document.createElement('img')
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg')
+
+        productInfoFigure.appendChild(productImgCard)
+
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList);
