5d5eae0be999e711b165f5df299b482a36bfbbda
Sergio Chaidez <sergioj.chaidez@gmail.com>
2022-12-20 23:13:32 -0600

    Integracion de los productos

--- main.js
+++ main.js
@@ -1,15 +1,81 @@
-const navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const burguerMenu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const navEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
-navEmail.addEventListener('click', toggleDesktopMenu);
-burguerMenu.addEventListener('click', toggleMobileMenu);
+navEmail.addEventListener("click", toggleDesktopMenu);
+burguerMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive');
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
+  desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  mobileMenu.classList.toggle('inactive');
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Pantalla",
+  price: 220,
+  image:"https://images.samsung.com/is/image/samsung/mx-t35f-lf27t350fhlxzx-frontblack-308825262?$650_519_PNG$",
+});
+productList.push({
+  name: "Computadora",
+  price: 620,
+  image:"https://m.media-amazon.com/images/I/61VcLC0G13L._AC_SL1500_.jpg",
+});
+
+/*
+<div class="product-card">
+  <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+  <div class="product-info">
+    <div>
+      <p>$120,00</p>
+      <p>Bike</p>
+    </div>
+    <figure>
+      <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+  </div>
+</div>
+*/
+
+for (product of productList) {
+  document.createElement('');
 }
