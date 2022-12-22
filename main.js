5099e7320c7997adcc56a53ee0f53185a4f6bc0b
Alex Nieblas <elalexnieblas@gmail.com>
2022-12-21 18:55:51 -0700

    Tercera clase, se agrego una funcion al carrito de compras

--- main.js
+++ main.js
@@ -2,16 +2,28 @@ let email = document.querySelector('.navbar-email')
 let desktopMenu = document.querySelector('.desktop-menu')
 let burguerMenu = document.querySelector('.burguerMenu')
 let mobileMenu = document.querySelector('.mobile-menu')
+let aside = document.querySelector('.product-detail')
+let carrito = document.querySelector('.navbar-shopping-cart')
 
 
 
 function quitarPonerClaseParaDesktop() {
+    aside.classList.add('inactive')
     desktopMenu.classList.toggle('inactive')
 }
 
 function quitarPonerClaseParaMobile(){
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive')
 }
+
+function quitarPonerClaseCarrito() {
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    aside.classList.toggle('inactive')
+}
+
+carrito.addEventListener('click', quitarPonerClaseCarrito)
 burguerMenu.addEventListener('click', quitarPonerClaseParaMobile)
 email.addEventListener('click', quitarPonerClaseParaDesktop)
 
