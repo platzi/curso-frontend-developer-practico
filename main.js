5578d6b1f92098a157d0ce8d2ae1f1a2c15e38c3
Gualberto <gumodi10@gmail.com>
2022-12-23 17:47:27 -0600

    Add function to menuhamburger

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+//selectores
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 // agrega el evento al elemento
 emailMenu.addEventListener('click',toggleMenuDesktop);
+menuHamburgerIcon.addEventListener('click',toogleMenuMobile);
 
-// agregamos para cambiar la clase al menu
+// funcion para cambiar la clase al menu
 function toggleMenuDesktop(){
-    console.log('Click');
     desktopMenu.classList.toggle('inactive');
+}
+
+// funcion para menuHamburgerIcon
+function toogleMenuMobile(){
+    mobileMenu.classList.toggle('inactive');
 }
