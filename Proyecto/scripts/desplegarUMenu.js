fb8a95ac143b894a56b2801428c5fb6d5124bb27
IvanRomero-io <ivandejesusrl2001@gmail.com>
2022-12-23 01:01:24 -0500

    todos los menus y bien optimizados

--- Proyecto/scripts/desplegarUMenu.js
+++ Proyecto/scripts/desplegarUMenu.js
@@ -1,14 +1,64 @@
 const menuEmail = document.querySelector('.navbar-email')
 const menudropdown = document.querySelector('.desktop-menu')
-menuEmail.addEventListener('click', toggleMenuEmail)
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const menuDeOrdenes = document.querySelector('.product-detail')
 
+menuEmail.addEventListener('click', toggleMenuEmail)
+menuCarritoIcon.addEventListener('click', toggleMenuOrder)
 
 function toggleMenuEmail(){
     menudropdown.classList.toggle('inactive')
+    menuDeOrdenes.classList.add('inactive')
 }
-//togle hace que por cada ejecucion de la funcion quite o añada la misma. como si fuera un swicher de luz
+
+function toggleMenuOrder(){
+    menuDeOrdenes.classList.toggle('inactive')
+    menudropdown.classList.add('inactive')
+    mobilemenu.classList.add('inactive')
+    
+    // const isMobileMenuClosed = mobilemenu.classList.contains('inactive')
+    
+    // if(!isMobileMenuClosed){
+    //    mobilemenu.classList.add('inactive')
+    // }
+    // menuDeOrdenes.classList.toggle('inactive')
+}
+
+// mobile //
+const burgerMenuMobile = document.querySelector('.menu')
+const mobilemenu = document.querySelector('.mobile-menu')
+
+burgerMenuMobile.addEventListener('click', toggleMenuMobile)
+
+function toggleMenuMobile(){
+    mobilemenu.classList.toggle('inactive')
+    menuDeOrdenes.classList.add('inactive')
+}
+
+//segunda forma de hacerlo
+// function toggleMenuMobile(){
+//     //creamos una variable que revise que un elemento en el html tenga una clase especifica
+//     const isMenuDeOrdenesClosed = menuDeOrdenes.classList.contains('inactive')
+    
+//     //revisa si no tiene la clase, osea que esta activo, se ejecuta, si si tiene la clase, no se ejecuta
+//     if(!isMenuDeOrdenesClosed){
+//        menuDeOrdenes.classList.add('inactive')
+//     }
+
+//     //Y al final se muestra el menu
+//     mobilemenu.classList.toggle('inactive')
+// }
 
 
+
+
+
+
+
+
+//toggle hace que por cada ejecucion de la funcion quite o añada la misma. como si fuera un swicher de luz
+
+//add añade una nueva clase al elemento, que puede ya estar definida en el css
 
 
 //ESTA FUE MI SOLUCION
--- Proyecto/scripts/menuMobile.js
+++
@@ -1,8 +0,0 @@
-const burgerMenuMobile = document.querySelector('.menu')
-const mobilemenu = document.querySelector('.mobile-menu')
-
-burgerMenuMobile.addEventListener('click', toggleMenuMobile)
-
-function toggleMenuMobile(){
-    mobilemenu.classList.toggle('inactive')
-}
