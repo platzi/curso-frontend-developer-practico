5dfde2e33b32c813acb9713a9e815a80a38999ca
Pedro Barrientos Voss <pedrobarrientosvoss@gmail.com>
2022-12-23 11:22:54 -0400

    Agregue el menu burger en Celular

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const menuEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu')
+const menuBurger=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',menuToggle);
 function menuToggle(){
     desktopMenu.classList.toggle('inactive')
+}
+menuBurger.addEventListener('click',menuToggleMobile);
+function menuToggleMobile(){
+    mobileMenu.classList.toggle('inactive');
 }
