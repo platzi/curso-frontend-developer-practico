561c2ba32af51139667b25ef5904436d3c7b64d3
Hector Maluy <hectormaluy@protonmail.com>
2022-12-22 21:07:52 -0600

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 //document.querySelector(".navbar-email");
 const navEmail = document.getElementsByClassName("navbar-email")[0]; 
 const divDesktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.getElementsByClassName("menu")[0];
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener("click", ()=> {
-    divDesktopMenu.classList.toggle("inactive");
+  divDesktopMenu.classList.toggle("inactive");
+});
+
+menuHamIcon.addEventListener("click", ()=> {
+  mobileMenu.classList.toggle('inactive');
 });
