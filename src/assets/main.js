bf0f105d997b8e2b57f035083e49d8b7ce766f83
Angel Duarte <angel.dtd.esc@gmail.com>
2022-12-24 08:54:33 -0600

    nav-bar & toggleMenu Integration on mobile

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuBurguer = document.querySelector('.menu');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleMenuDesktop);
+menuBurguer.addEventListener('click', toggleMenuMobile);
 
 function toggleMenuDesktop() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMenuMobile() {
+    mobileMenu.classList.toggle('inactive');
 }
