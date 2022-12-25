59fe2460f88764a9c304ff1021c57bc1d4f5c62e
Carlos EC <escribanoclemente@MacBook-Pro-de-Carlos.local>
2022-12-25 12:27:42 +0100

    Añadido modal de detalles del producto la lógica para cerrar los otros

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const buttonShoppingCart = document.querySelector('.navbar-shopping-cart');
 const asideOrders = document.querySelector('.cart-detail')
 const mainContent = document.querySelector('.main-content')
 const modals = document.querySelectorAll('.modals')
+const productCard = document.querySelector('.product-card');
+const productDetail = document.querySelector('.product-detail');
+const closeProductDetail = document.querySelector('.product-detail-close');
+
+
 
 
 // Array con Listado de productos
@@ -30,11 +35,14 @@ productList.push({
     image: "https://p.vitalmtb.com/photos/users/37957/setup_checks/30277/photos/45780/original_20151024_150427.jpg?1445696550",
 });
 
+
 function renderProducts(array){
     
     for (product of array){
-        const productCard = `
-        <div class="product-card">
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productInfo = `
             <img src=${product.image} alt="">
             <div class="product-info">
             <div>
@@ -45,34 +53,63 @@ function renderProducts(array){
                 <img src="./icons/bt_add_to_cart.svg" alt="">
             </figure>
             </div>
-        </div>
         `;
 
+        productCard.innerHTML = productInfo;
         const productsContainer = document.querySelector('.cards-container');
-        productsContainer.innerHTML += productCard;
+        productsContainer.appendChild (productCard);
+        productCard.addEventListener('click', openProductDetails);
     }
+  
+}
 
-    
+function openProductDetails(){
+    // Abrimos los detalles del producto
+    productDetail.classList.remove('inactive');
+
+    // Cerramos el resto de modales que puedan estar abierto, uno por uno
+    asideOrders.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+
+}
+
+function closeModalProductDetail(){
+    productDetail.classList.add('inactive');
 }
 
 function toggleMenuDesktop() {
-    asideOrders.classList.add('inactive');
+    // Abrimos el Menu Desktop si estuviera cerrado
     desktopMenu.classList.toggle('inactive');
+
+    // Cerramos el resto de modales que puedan estar abierto, uno por uno
+    asideOrders.classList.add('inactive');
+    productDetail.classList.add('inactive');
 }
 
 function toggleMenuMobile() {
-    asideOrders.classList.add('inactive');
+    // Abrimos el Menu Desktop si estuviera cerrado
     mobileMenu.classList.toggle('inactive');
+
+    // Cerramos el resto de modales que puedan estar abierto, uno por uno
+    asideOrders.classList.add('inactive');
+    productDetail.classList.add('inactive');
 }
 
 function toggleShoppingCart(){
-   
+   // Abrimos el Menu Desktop si estuviera cerrado
+    asideOrders.classList.toggle('inactive');
+
+    // Cerramos el resto de modales que puedan estar abierto, uno por uno
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
-    asideOrders.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
 }
 
+
+closeProductDetail.addEventListener('click', closeModalProductDetail);
 buttonShoppingCart.addEventListener('click', toggleShoppingCart);
 menuEmail.addEventListener('click', toggleMenuDesktop);
 buttonMenuMobile.addEventListener('click', toggleMenuMobile);
 renderProducts(productList);
+
+
