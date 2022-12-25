359d22d8a079d03d065d605fa520fe038ec636e1
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2022-12-25 11:41:59 -0500

    pequeños detalles

--- js/main.js
+++ js/main.js
@@ -47,63 +47,6 @@ closeIcon.addEventListener('click',()=>{
     productDetailContainer.classList.add('inactive');
 });
 
-//Lista de productos: HTML a partir de arrays
-const productList= [
-    {
-        id:1,
-        name:"Bike",
-        price:120,
-        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
-    },
-    {
-        id:2,
-        name:'Bicycle helmet',
-        price: 1600,
-        image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg',
-        desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
-
-    },
-    {
-        id:3,
-        name:'Seat',
-        price: 300,
-        image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg',
-        desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
-        
-    },
-    {
-        id:4,
-        name:'Tennis Montain Bike',
-        price: 2200,
-        image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg',
-        desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
-    },
-    {
-        
-
-    }
-    
-];
-
-productList.push ({
-   
-});
-productList.push ({
-    name:'Sunglasses',
-    price: 600,
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
-});
-productList.push ({
-    name:'Bicycle seat bag',
-    price: 876,
-    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
-}); 
-
-function openProductDetailAside(){
-    
-}
-
 function renderProducts(arr){
     for(product of arr){
         const productCard=document.createElement('div');
