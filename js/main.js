9da8c2feb2c94eeb714c597e5daa6a82dc370ea4
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2022-12-26 10:31:58 -0500

    dejar solo funcionalidades

--- js/main.js
+++ js/main.js
@@ -36,8 +36,7 @@ menuCarritoIcon.addEventListener('click',()=>{
     if(!mobileMenu.classList.contains('inactive') ||!desktopMenu.classList.contains('inactive') ||!productDetailContainer.classList.contains('inactive')){
         mobileMenu.classList.add('inactive');
         desktopMenu.classList.add('inactive');
-        productDetailContainer.classList.add('inactive');
-        
+        productDetailContainer.classList.add('inactive');  
     }
   
     aside.classList.toggle('inactive');
@@ -47,49 +46,13 @@ closeIcon.addEventListener('click',()=>{
     productDetailContainer.classList.add('inactive');
 });
 
-function renderProducts(arr){
-    for(product of arr){
-        const productCard=document.createElement('div');
-        productCard.classList.add('product-card');
-    
-        const productImg=document.createElement('img');
-        productImg.setAttribute('src',product.image);
-        productImg.addEventListener('click',()=>{
-            if(!mobileMenu.classList.contains('inactive') ||!desktopMenu.classList.contains('inactive') || !aside.classList.contains('inactive')){
-                mobileMenu.classList.add('inactive');
-                desktopMenu.classList.add('inactive');
-                aside.classList.add('inactive');
-                
-            }
-            productDetailContainer.classList.toggle('inactive')
-        });
-            
-    
-        const productInfo=document.createElement('div');
-        productInfo.classList.add('product-info');
-    
-        const productInfoDiv=document.createElement('div');
-    
-        const productPrice=document.createElement('p');
-        const productName=document.createElement('p');
-        productName.innerText=product.name;
-    
-        productInfoDiv.appendChild(productPrice);
-        productInfoDiv.appendChild(productName);
-        productPrice.innerText='$'+product.price;
-    
-        const productInfoFigure=document.createElement('figure');
-        const productImgCart=document.createElement('img');
-        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
-    
-        productInfoFigure.appendChild(productImgCart);
-        
-        productInfo.appendChild(productInfoDiv);
-        productInfo.appendChild(productInfoFigure);
-    
-        productCard.appendChild(productImg);
-        productCard.appendChild(productInfo);
-        cardsContainer.appendChild(productCard);
-    }
-}
- renderProducts(productList);
+productImg.addEventListener('click',()=>{
+  if(!mobileMenu.classList.contains('inactive') ||!desktopMenu.classList.contains('inactive') || !aside.classList.contains('inactive')){
+      mobileMenu.classList.add('inactive');
+      desktopMenu.classList.add('inactive');
+      aside.classList.add('inactive');
+      
+  }
+  productDetailContainer.classList.toggle('inactive')
+});
+
