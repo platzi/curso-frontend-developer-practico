43aa97e09182f85d73845331c40b5af2fd870d85
Gaston Rocha <gstnedgrdrch93@gmail.com>
2022-12-27 17:30:55 -0300

    Se agregaron comentarios para identar mejor

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+// Se colocan las variables en primer lugar
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 // Activa la funcion (toggleDesktorMenu) mediante el click
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -7,4 +11,14 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 // Funcion que verifica que tenga la clase (inactive), con toggle pone/quita la clase mencionada
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+};
+
+// ----------------------------------------------------------------------------------------------
+
+// Activa la funcion (toggleMobileMenu) mediante el click
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+// Funcion que verifica que tenga la clase (inactive), con toggle pone/quita la clase mencionada
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 };
