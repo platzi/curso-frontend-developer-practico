bec05d09e6d7c3f124aa22f70d11cdc31ceee379
Pedro Barrientos Voss <pedrobarrientosvoss@gmail.com>
2022-12-27 15:30:43 -0400

    Arreglo del carrito

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
+//Variables constantes
+
 const menuEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu')
 const menuBurger=document.querySelector('.menu');
 const mobileMenu=document.querySelector('.mobile-menu');
+const aside=document.querySelector('.navbar-shopping-cart');
+const asidedetail=document.querySelector('.product-detail')
 
+//Email Menu
 menuEmail.addEventListener('click',menuToggle);
 function menuToggle(){
     desktopMenu.classList.toggle('inactive')
 }
+
+//burger Menu
 menuBurger.addEventListener('click',menuToggleMobile);
 function menuToggleMobile(){
+  const carritoStatus=asidedetail.classList.contains('inactive')
+  if(!carritoStatus){
+    asidedetail.classList.add('inactive')
+  }
     mobileMenu.classList.toggle('inactive');
+}
+
+// Aside mobile
+
+aside.addEventListener('click',menuAside);
+function menuAside(){
+  const burgerStatus=mobileMenu.classList.contains('inactive');
+  if(!burgerStatus){
+    mobileMenu.classList.add('inactive')
+  }
+  asidedetail.classList.toggle('inactive');
 }
