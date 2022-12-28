6127bf062dd95a419cd101e5ef0ec37a4c0c3b9b
N40M10 <N40M10@disroot.org>
2022-12-28 17:19:51 -0600

    open/close menus added

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+const cartMenuIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 const layerMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-layerMenu.addEventListener('click', toggleMobileMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu)
+layerMenu.addEventListener('click', toggleMobileMenu)
+cartMenuIcon.addEventListener('click', toggleCartAside)
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+    desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCartAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
 }
