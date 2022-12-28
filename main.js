73e4fdaa19772e3d906917b10d2e13824db2ac09
George Abou Chanab <abuchegeorge@gmail.com>
2022-12-28 11:06:19 -0300

    fusuon mobile menu desktop menu

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const menuEail= document.querySelector(".navbar-email");
+const menuEmail= document.querySelector(".navbar-email");
 const desktopMenu=document.querySelector(".desktop-menu");
 
-menuEail.addEventListener("click",toggleDesktopMenu);
+menuEmail.addEventListener("click",toggleDesktopMenu);
 
 function toggleDesktopMenu() {
 desktopMenu.classList.toggle("inactive");
+}
 
+const mobileMenu=document.querySelector(".mobile-menu")
+const menuHamburgesa=document.querySelector(".menu")
+
+menuHamburgesa.addEventListener("click",toggleMobileMenu)
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
