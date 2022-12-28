8ab17c1538bc5d7dfe287637b6a5b0a46d35cdaf
Franco <fmangeleri@gmail.com>
2022-12-28 20:09:54 -0300

    Clase 19

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const iconMobileMenu = document.querySelector('#iconMobileMenu');
 
 menuEmail.addEventListener('click', () => {
-    desktopMenu.classList.toggle('inactive');;
+    desktopMenu.classList.toggle('inactive');
+});
+
+iconMobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
 });
