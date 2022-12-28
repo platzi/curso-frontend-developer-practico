f5e5cb33fb79495baa6e05bd3cf223e93cbda713
Juan Carrera <link4media@gmail.com>
2022-12-27 22:21:52 -0500

    Listo menu mobile y media queries de menu mail y menu mobile

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+//MENU MOBILE
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
 
 //MENU EMAIL
 menuEmail.addEventListener('click', toggleDesktopMenu);
