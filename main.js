0c249fc1b60cfdc3f6f0f4fcdee55275c65a8103
Giovanni Alexander <alexander.hh.cc@gmail.com>
2022-12-27 23:03:16 -0500

    [add] Integration with menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var [menuEmail] = document.getElementsByClassName("navbar-email");
 var [desktopMenu] = document.getElementsByClassName("desktop-menu");
+var menuHamIcon = document.querySelector(".menu");
+var mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
