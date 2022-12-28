b8c74c8e46b1d5a2ac3d0f98ee4afc5024fdf33d
anderDaZu <anderda-96@hotmail.com>
2022-12-27 21:48:18 -0500

    Add mobile menu to Index

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
-# se forkeo y luego se clono el repositorio desde Platzi
-luego se analizo cada archivo y se inicio con la fusión del menú en desktop
+# Se forkeo y luego se clono el repositorio desde Platzi
+Luego se analizo cada archivo y se inicio con la fusión del menú en desktop
 Es de resaltar que ya se aplico una parte de js donde se activa e inactiva una clase de css
+
+# Se fusiono el menú mobile al index 
+En esta ocasión se añadio el menú mobile al index, donde desde la parte de js se
+desarrollo el elemento que permitiese activar e inactivar las clase inactive
--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toogleShowDesktopMenu);
+menuHamIcon.addEventListener('click', toogleShowMobileMenu);
 
 function toogleShowDesktopMenu(event){
     event.preventDefault();
     desktopMenu.classList.toggle('inactive')
+}
+
+function toogleShowMobileMenu(event) {
+    event.preventDefault();
+    mobileMenu.classList.toggle('inactive');
 }
