7a31ab404a38879df5dc2199831d89b7f450937b
Giovanni Alexander <alexander.hh.cc@gmail.com>
2022-12-28 00:21:52 -0500

    [add] Product detail integration

--- main.js
+++ main.js
@@ -36,3 +36,137 @@ function toggleCarritoAside() {
   }
   aside.classList.toggle("inactive");
 }
+
+var productList = [];
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProducts(list) {
+  var cardsContainer = document.querySelector(".cards-container");
+
+  list.forEach(function (prod) {
+    var productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+    var image = document.createElement("img");
+    image.setAttribute("src", prod.image);
+    image.setAttribute("alt", "");
+    var productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+    var prodData = document.createElement("div");
+    var priceValue = prod.price.toFixed("2").replace(".", ",");
+    var price = document.createElement("p");
+    price.innerText = `$${priceValue}`;
+    var name = document.createElement("p");
+    name.innerText = prod.name;
+    var figure = document.createElement("figure");
+    var addToCartIcon = document.createElement("img");
+    addToCartIcon.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    addToCartIcon.setAttribute("alt", "");
+    figure.appendChild(addToCartIcon);
+    prodData.appendChild(price);
+    prodData.appendChild(name);
+    productInfo.appendChild(prodData);
+    productInfo.appendChild(figure);
+    productCard.appendChild(image);
+    productCard.appendChild(productInfo);
+    cardsContainer.appendChild(productCard);
+  });
+}
+
+renderProducts(productList);
