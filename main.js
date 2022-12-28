e70697e2963edfeb543ebbd47656afd46f35ea89
Juan Carrera <link4media@gmail.com>
2022-12-28 10:53:15 -0500

    Listo shopping cart, funciones para activar y desactivar menus segun su interaccion

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
+//VARIABLES MENU EMAIL
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
+//VARIABLES MENU MOBILE
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+//VARIABLES SHOPPING CART
+const shoppingCart = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
 
-//MENU MOBILE
-menuHamIcon.addEventListener('click', toggleMobileMenu);
-function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive')
-}
-
+///////////EVENTOS Y FUNCIONES/////////////////////////////////////////
 //MENU EMAIL
 menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+    productDetail.classList.add('inactive')//cierra
+    desktopMenu.classList.toggle('inactive')//abre
+}
+//MENU MOBILE
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+function toggleMobileMenu() {
+    productDetail.classList.add('inactive')//cierra
+    mobileMenu.classList.toggle('inactive')//abre
+}
+//SHOPPING CART
+shoppingCart.addEventListener('click', toggleShoppingCart);
+function toggleShoppingCart() {
+    desktopMenu.classList.add('inactive')//cierra
+    mobileMenu.classList.add('inactive')//cierra
+    productDetail.classList.toggle('inactive')//abre
 }
