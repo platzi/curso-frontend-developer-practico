2366a9c6e3e0490cbc06a25f609b426bc9b36d25
nicolasvaldelamar <nicolasvaldelamar.c@gmail.com>
2022-12-27 23:58:21 -0500

    Fusion de menu desktop

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+
+menuEmail.addEventListener('click', ()=>{
+    desktopMenu.classList.toggle('inactive');
+})
