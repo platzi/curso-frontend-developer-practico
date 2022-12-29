0fb35f18fae8c6d78480cb7c49ca818b24a40e07
Jafr200 <julianandredflorezrios@gmail.com>
2022-12-29 11:45:22 -0500

    Añadimos el aside de product detail y su comportamiento con JS

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
-let navbarEmail = document.querySelector(".navbar-email")
-let desktopMenu = document.querySelector(".desktop-menu")
+const navbarEmail = document.querySelector(".navbar-email")
+const desktopMenu = document.querySelector(".desktop-menu")
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
-navbarEmail.addEventListener('click', showDesktopMenu)
 
-function showDesktopMenu(){
+navbarEmail.addEventListener('click', toggleDesktopMenu)
+menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleAside)
+
+
+
+function toggleDesktopMenu(){
+    let isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive');
-    console.log("hola")
+
 }
 
+function toggleMobileMenu(){
+    let isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive');
+
+}
+function toggleAside(){
+    aside.classList.toggle('inactive');
+
+    let isMobileClosed = mobileMenu.classList.contains('inactive')
+    if(!isMobileClosed){
+        mobileMenu.classList.add('inactive')
+    }
+
+    let isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    }
+}
