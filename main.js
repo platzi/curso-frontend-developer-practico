449b04397094401c4a21331c71da2607c0847509
SoyAndresTrujillo <andymania.1301@gmail.com>
2022-12-29 14:51:22 -0500

    add shopping cart

--- main.js
+++ main.js
@@ -1,17 +1,57 @@
 // toogle = intercambiar
 
+// Menu desktop
 const menuEmail = document.querySelector('.navbar-email');
-const desktopmenu = document.querySelector('.desktop-menu');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+// Menu mobile
 const hamburguerIconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+// My order
+const carritoIconMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+// Menu desktop
 menuEmail.addEventListener('click', toogleDesktopMenu);
+
+// Menu mobile
 hamburguerIconMenu.addEventListener('click', toogleMobileMenu);
 
+// My order
+carritoIconMenu.addEventListener('click', toogleMyCarritoMenu);
+
 function toogleDesktopMenu() {
-    desktopmenu.classList.toggle('inactive');
+    const isMyCarritoClosed = aside.classList.contains('inactive');
+
+    if (!isMyCarritoClosed) {
+        aside.classList.add('inactive');
+    }
+
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toogleMobileMenu() {
+    // const isMyCarritoClosed = aside.classList.contains('inactive');
+
+    // if (!isMyCarritoClosed) {
+    //     aside.classList.add('inactive');
+    // }
+
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toogleMyCarritoMenu() {
+    // const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    // const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    // if (!isMobileMenuClosed || !isDesktopMenuClosed) {
+    //     mobileMenu.classList.add('inactive');
+    //     desktopMenu.classList.add('inactive');
+    // }
+
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
