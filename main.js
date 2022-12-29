b12303b6894233dfa150a3463d65c6d73921f0c4
SoyAndresTrujillo <andymania.1301@gmail.com>
2022-12-29 11:33:45 -0500

    @mediaQuerys mobile & desktop

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
+const hamburguerIconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+hamburguerIconMenu.addEventListener('click', toogleMobileMenu);
 
 function toogleDesktopMenu() {
     desktopmenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
