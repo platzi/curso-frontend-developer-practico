f0cafba5af4224df9ad19eb54483347e0eb3c9b2
vitomanjm <vitomanjm@gmail.com>
2022-12-29 01:23:24 -0500

    Añadiendo carrito de compras

--- main.js
+++ main.js
@@ -2,19 +2,51 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
 
-menuHamburguesa.addEventListener('click', toggleMobileMenu)
-
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDesktopMenu(){
     console.log('Click');
+
+    const isAsideclosed = aside.classList.contains('inactive');
+
+    if(!isAsideclosed)
+    {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
     console.log('Click');
+
+    const isAsideclosed = aside.classList.contains('inactive');
+
+    if(!isAsideclosed)
+    {
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    console.log('Click');
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+    if(!isMobileMenuClosed)
+    {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+    
 }
