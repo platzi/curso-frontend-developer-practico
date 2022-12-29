11acc50dd82b9fedf028f0759d8a6ff1a122f0d9
anderDaZu <anderda-96@hotmail.com>
2022-12-28 21:32:33 -0500

    Se agrego los elementos del carrito de compras a desktop y mobile

--- Readme.md
+++ Readme.md
@@ -5,3 +5,9 @@ Es de resaltar que ya se aplico una parte de js donde se activa e inactiva una c
 # Se fusiono el menú mobile al index 
 En esta ocasión se añadio el menú mobile al index, donde desde la parte de js se
 desarrollo el elemento que permitiese activar e inactivar las clase inactive
+
+# Se fusiono los elementos del carrito de compras
+En esta parte, se agrego un aside el cual corresponde a los elementos que tiene
+el carrito de compras, es de resaltar que con JS se agrego el dinamismo que
+permite hacer que un elemento aparezca o desaparezca dependiendo si otro elemento
+esta activo
--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toogleShowDesktopMenu);
 menuHamIcon.addEventListener('click', toogleShowMobileMenu);
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
+
 
 function toogleShowDesktopMenu(event){
     event.preventDefault();
-    desktopMenu.classList.toggle('inactive')
+    const isAsideOpen = !aside.classList.contains('inactive');
+    
+    if (isAsideOpen) {
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toogleShowMobileMenu(event) {
     event.preventDefault();
+    const isAsideOpen = !aside.classList.contains('inactive');
+    
+    if (isAsideOpen) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toogleCarritoAside(event) {
+    event.preventDefault();
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+    
+    if (isMobileMenuOpen) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (isDesktopMenuOpen) {
+        desktopMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
 }
