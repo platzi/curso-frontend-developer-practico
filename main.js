a145f9c1f6463fd14dd49f96ccc15fb1898ee51c
vitomanjm <vitomanjm@gmail.com>
2022-12-29 00:31:43 -0500

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const menuHamburguesa = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+
+menuHamburguesa.addEventListener('click', toggleMobileMenu)
+
+
 
 function toggleDesktopMenu(){
     console.log('Click');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    console.log('Click');
+    mobileMenu.classList.toggle('inactive');
 }
