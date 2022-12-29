0923107c666e1501a2e73165d38fdb19851a7ffa
nicolasvaldelamar <nicolasvaldelamar.c@gmail.com>
2022-12-28 19:41:16 -0500

    agregado nuevo aside

--- main.js
+++ main.js
@@ -3,24 +3,24 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const shoppingCardContainer = document.querySelector("#shoppingCardContainer");
 const cardsContainer = document.querySelector('.cards-container');
 let numProduct = 20;
 
 menuEmail.addEventListener("click", () => {
-  const isAsideClosed = aside.classList.contains("inactive");
-  if (!isAsideClosed) {
+  const isshoppingCardContainerClosed = shoppingCardContainer.classList.contains("inactive");
+  if (!isshoppingCardContainerClosed) {
     //si mobileMenu esta open, hay que cerrarlo
-    aside.classList.add("inactive");
+    shoppingCardContainer.classList.add("inactive");
   }
   desktopMenu.classList.toggle("inactive");
 });
 
 menuHamIcon.addEventListener("click", () => {
-  const isAsideClosed = aside.classList.contains("inactive");
-  if (!isAsideClosed) {
+  const isshoppingCardContainerClosed = shoppingCardContainer.classList.contains("inactive");
+  if (!isshoppingCardContainerClosed) {
     //si mobileMenu esta open, hay que cerrarlo
-    aside.classList.add("inactive");
+    shoppingCardContainer.classList.add("inactive");
   }
   mobileMenu.classList.toggle("inactive");
 });
@@ -32,7 +32,7 @@ menuCarritoIcon.addEventListener("click", () => {
     //si mobileMenu esta open, hay que cerrarlo
     mobileMenu.classList.add("inactive");
   }
-  aside.classList.toggle("inactive");
+  shoppingCardContainer.classList.toggle("inactive");
 });
 
 const productList = [];
