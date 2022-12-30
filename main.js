64b5bdce759f6f4fdef05991292594551cf7b066
Brayan Daza <brayandaza001@hotmail.com>
2022-12-29 22:14:02 -0500

    se añade la inteaccionde productos y su descripcion

--- main.js
+++ main.js
@@ -5,11 +5,18 @@ const hamburguerMenu = document.querySelector('.mobile-menu');
 const carShop = document.querySelector('.navbar-shopping-cart');
 const detailProduct = document.querySelector('.product-detail');
 const mainContainer =document.querySelector('.main-container');
+
 const cardsContainer = document.querySelector('.cards-container');
+
+const asideDescriptionProduct =document.querySelector('.product-detaill');
+const closeBoton=document.querySelector('.product-detaill-close');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 selectMenu.addEventListener('click', toggleMenu);
 carShop.addEventListener('click', toggleCar);
+cardsContainer.addEventListener('click', openDescription);
+closeBoton.addEventListener('click', closeDescription);
 
 
 
@@ -25,6 +32,7 @@ function toggleDesktopMenu(){
     // }
     detailProduct.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
+    asideDescriptionProduct.classList.add('inactive');
 
    
 }
@@ -32,6 +40,7 @@ function toggleMenu(){
 
     detailProduct.classList.add('inactive');
     hamburguerMenu.classList.toggle('inactive');
+    asideDescriptionProduct.classList.add('inactive');
 }
 function toggleCar(){
 
@@ -46,8 +55,18 @@ function toggleCar(){
     desktopMenu.classList.add('inactive');
     detailProduct.classList.toggle('inactive');
     hamburguerMenu.classList.add('inactive');
-
+    asideDescriptionProduct.classList.add('inactive');
     
+}
+function openDescription(){
+  asideDescriptionProduct.classList.remove('inactive');
+  hamburguerMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+}
+function closeDescription(){
+  asideDescriptionProduct.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
 }
 
 
@@ -76,36 +95,43 @@ const productsAll =[];
 productsAll.push({
     name:'Bike',
     price:120,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'as a bicycle to the means of transport that has two wheels, with pedals that allow movement to be transmitted to the rear wheel through a chain, a sprocket and a plate. It is a vehicle that moves by the propulsion of the user himself, who must pedal.'
 });
 productsAll.push({
     name:'Compu',
     price:220,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-
-productsAll.push({
-    name:'Compu',
-    price:220,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'as a bicycle to the means of transport that has two wheels, with pedals that allow movement to be transmitted to the rear wheel through a chain, a sprocket and a plate. It is a vehicle that moves by the propulsion of the user himself, who must pedal.'
 });
 
 productsAll.push({
     name:'Compu',
     price:220,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'as a bicycle to the means of transport that has two wheels, with pedals that allow movement to be transmitted to the rear wheel through a chain, a sprocket and a plate. It is a vehicle that moves by the propulsion of the user himself, who must pedal.'
+
+});
+
+productsAll.push({
+    name:'Compu',
+    price:220,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'as a bicycle to the means of transport that has two wheels, with pedals that allow movement to be transmitted to the rear wheel through a chain, a sprocket and a plate. It is a vehicle that moves by the propulsion of the user himself, who must pedal.'
 });
 productsAll.push({
     name:'Compu',
     price:220,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'as a bicycle to the means of transport that has two wheels, with pedals that allow movement to be transmitted to the rear wheel through a chain, a sprocket and a plate. It is a vehicle that moves by the propulsion of the user himself, who must pedal.'
 });
 
 
 productsAll.push({
     name:'Compu',
     price:220,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'as a bicycle to the means of transport that has two wheels, with pedals that allow movement to be transmitted to the rear wheel through a chain, a sprocket and a plate. It is a vehicle that moves by the propulsion of the user himself, who must pedal.'
 });
 for(product of productsAll){
 
@@ -116,6 +142,8 @@ for(product of productsAll){
     productCard.classList.add('product-card');
     productCardImg.setAttribute('src', product.image);
     productCard.appendChild(productCardImg);
+
+
 
     const productInfo=document.createElement('div');
     const productDiv = document.createElement('div');
@@ -145,5 +173,62 @@ for(product of productsAll){
     cardsContainer.appendChild(productCard);
    
 
-
 }
+
+
+
+// function prueba(){
+
+//   // <aside class="product-detaill inactive">
+//   //   <div class="product-detaill-close">
+//   //     <img src="./icons/icon_close.png" alt="close">
+//   //   </div>
+//   //   <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike">
+//   //   <div class="product-infoo">
+//   //     <p>$35,00</p>
+//   //     <p>Bike</p>
+//   //     <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+//   //     <button class="primary-buttonn add-to-cart-buttonn">
+//   //       <img src="./icons/bt_add_to_cart.svg" alt="add to cart">
+//   //       Add to cart
+//   //     </button>
+//   //   </div>
+//   // </aside>
+
+//   const cardDescription = document.createElement('div');
+//   const iconClose = document.createElement('img');
+//   cardDescription.classList.add('product-detaill-close');
+//   iconClose.setAttribute('src','./icons/icon_close.png');
+
+//   cardDescription.appendChild
+
+
+//   const imgProductDescription =document.createElement('img');
+//   imgProductDescription.setAttribute('src',productsAll.image);
+
+
+//   const infoProduct= document.createElement('div');
+//   const priceDescription = document.createElement('p');
+//   const nameDescription =document.createElement('p');
+//   const detallado = document.createElement('p');
+//   infoProduct.classList.add('product-infoo');
+//   priceDescription.innerText=productsAll.price;
+//   nameDescription.innerText=productsAll.name;
+//   detallado.innerText=productsAll.description;
+
+
+
+//   const boton = document.createElement('button');
+//   const imgboton = document.createElement('img');
+//   boton.classList.add('primary-buttonn add-to-cart-buttonn');
+//   imgboton.setAttribute('src','./icons/bt_add_to_cart.svg" alt="add to cart')
+//   boton.innerText('Add to cart');
+
+
+// }
+// prueba()
+// function togglePrueba(){
+
+//   asideDescriptionProduct.classList.toggle('inactive');
+ 
+// }
