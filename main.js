1e2f97067fdffbb60d3d4c54e7197edf423436a9
Cristo <cristo.platzi@gmail.com>
2022-12-30 02:05:00 +0100

    Add shopping car and hide a menu if it's stacking

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
+// DESKTOP - TOGGLE MENU FROM CLICKING EMAIL
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const burguerIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-burguerIcon.addEventListener('click', toggleMobilepMenu);
 
 function toggleDesktopMenu() {
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobilepMenu() {
+
+// MOBILE - TOGGLE MENU FROM CLICKING MENU ICON
+const burguerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+burguerIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+
+// D & M - TOGGLE SHOPPING CAR FROM CLICKING CAR ICON
+const carritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+carritoIcon.addEventListener('click', toggleShoppingCar);
+
+function toggleShoppingCar(){
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
 }
