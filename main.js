533a3ac780dbded214ecc6d22719b159d8d20ec0
Felipe Munoz <analista.felipe.m@gmail.com>
2022-12-30 04:54:55 -0300

    Se fusiona la clase 12. Se agregan nuevas funcionalidades

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const aside = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 /* escuchar el metodo click que al presionar un icono o texto, aparesca y desaparesca alguno de los menus. */
