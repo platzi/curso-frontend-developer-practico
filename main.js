a07b7f12bd466bdb88edb733115bcf73c7105777
Duvan Daza <duvandaza9@gmail.com>
2022-12-30 13:58:23 -0500

    ya quedo para celulares

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
-
+const menuHamIco = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+menuHamIco.addEventListener("click", toggleMobilMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobilMenu(){
+    mobileMenu.classList.toggle("inactive")
 }
