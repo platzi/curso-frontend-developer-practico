c5f559144a51c861dcc884fdf83190014929df93
Cristo <cristo.platzi@gmail.com>
2022-12-30 03:09:17 +0100

    Add static generated data through js

--- main.js
+++ main.js
@@ -28,8 +28,77 @@ const aside = document.querySelector('.product-detail');
 
 carritoIcon.addEventListener('click', toggleShoppingCar);
 
-function toggleShoppingCar(){
+function toggleShoppingCar() {
     aside.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
 }
+
+
+// DATA
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 240,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Ordenador',
+    price: 1200,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+function renderProductos(arr) {
+    const cardContainer = document.querySelector('.cards-container');
+    /*
+    <div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt="">
+        <div class="product-info">
+            <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+            </div>
+            <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="">
+            </figure>
+        </div>
+    </div>
+    */
+    for (product of arr) {
+        // product = {name, price, image}
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        const productFigure = document.createElement('figure');
+        const productCarImageFigure = document.createElement('img');
+        productCarImageFigure.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productCard.append(img, productInfo);
+        productInfo.appendChild(productInfoDiv);
+        productInfoDiv.append(productPrice, productName);
+        productInfo.appendChild(productFigure);
+        productFigure.appendChild(productCarImageFigure);
+
+        cardContainer.appendChild(productCard);
+    }
+}
+
+renderProductos(productList);
