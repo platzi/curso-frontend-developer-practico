5cf5359a11acb98b2f4ae3f1d97b9de5290b37a5
Freddyjcm <Freddyjcm@gmail.com>
2022-12-30 19:14:56 -0400

    agregado la funcion de menu hamburguesa mostrando el menu correspondiente

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const menuEmail = document.querySelector('#navbarEmail');
 const desktopMenu = document.querySelector('#desktopMenu');
+const menuIcon = document.querySelector('#menuIcon');
+const menuBurguer = document.querySelector('#menuBurguer')
+
 menuEmail.addEventListener('click', menuDeEmail);
+menuIcon.addEventListener('click', menuBurguerToggle);
 
 function menuDeEmail() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function menuBurguerToggle() {
+    menuBurguer.classList.toggle('inactive')
 }
