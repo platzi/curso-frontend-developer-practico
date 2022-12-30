810e1691b20f5513f03a914bafc8bf1cbfe05ecf
Levir Olea <Levir777@outlook.com>
2022-12-30 11:10:28 -0600

    maquetado de los productos con js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const MenuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 MenuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -40,6 +42,83 @@ function toggleCarritoAside(){
     } 
     
     aside.classList.toggle('inactive');
-
-
 }
+
+const productList = [];
+productList.push({
+    name: 'bike',
+    price: '130',
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Computer',
+    price: '2000',
+    image:'https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'Pantalla Retro',
+    price: '2000',
+    image:'https://images.pexels.com/photos/5721881/pexels-photo-5721881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'bike',
+    price: '130',
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Computer',
+    price: '2000',
+    image:'https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'Pantalla Retro',
+    price: '2000',
+    image:'https://images.pexels.com/photos/5721881/pexels-photo-5721881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+
+
+
+function renderProducts(arr){
+    for (product of arr) {
+        //Declarar los elementos y agregarlos con la constante
+       const productCard = document.createElement('div');
+       //Se agrega una clase al elemento que se creó en la constante
+        productCard.classList.add('product-card');
+    
+        
+       const productImg = document.createElement('img');
+       //se agrega el atributo de src a la etiqueta creada con la constante y lo que va dentro del atributo (product.image)
+        productImg.setAttribute('src', product.image);
+    
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText =  product.name;
+    
+        const productInfoFigure= document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        //se maquetan las etiquetas creadas agregandose dentro de sus contenedores especificos
+        productCard.append(productImg, productInfo)
+    
+        productInfo.append(productInfoDiv, productInfoFigure);
+        productInfoDiv.append(productPrice, productName);
+    
+        productInfoFigure.appendChild(productImgCard);
+    
+        //se mandan estos hacia la clase contenedora en html y así reflejarlos
+        cardsContainer.append(productCard);
+    
+    }
+}
+
+renderProducts(productList);
