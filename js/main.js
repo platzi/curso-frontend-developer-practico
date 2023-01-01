acd07eec89e0bb9a9575aab1d2d482d7a1a18a24
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2022-12-31 20:06:09 -0500

    error js

--- js/main.js
+++ js/main.js
@@ -1,84 +1,259 @@
-//Fusión del menú en desktop
-let navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const productList= [
+  
+  {
+      id:1,
+      name:"Bike",
+      category: 'Others',
+      price:120,
+      image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
+  },
+  {
+      id:2,
+      name:'Bicycle helmet',
+      category: 'Others',
+      price: 1600,
+      image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg',
+      desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
 
-navEmail.addEventListener('click',()=>{
-    if(!aside.classList.contains('inactive')){
-        aside.classList.add('inactive');
-    }
-    desktopMenu.classList.toggle('inactive');  
+  },
+  {
+      id:3,
+      name:'Seat',
+      category: 'Others',
+      price: 300,
+      image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg',
+      desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
+      
+  },
+  {
+    id:4,
+    name: 'Rokid Air AR',
+    category: 'Clothes',
+    price: 399.99,
+    image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/518WFzR2kGL._AC_UY218_.jpg',
+    desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
+}
+
+  ,
+  {
+      id:5,
+      name: 'Rokid Air AR',
+      category: 'Electronics',
+      price: 399.99,
+      image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/518WFzR2kGL._AC_UY218_.jpg',
+      desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
+  },
+  {
+      id:6,
+      name: 'Bocina Sound Link',
+      category: 'Electronics',
+      price: 89.99,
+      image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/61v+-9dV4PL._AC_UY218_.jpg',
+      desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
+  },
+  {
+      id:7,
+      name: 'Juego de mesa de cocina de 5 piezas',
+      category: 'Furnitures',
+      price: 256.99,
+      image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/813BJiM5DrL._AC_UL320_.jpg',
+      desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
+  },
+  {
+      id:8,
+      name: 'HOOBRO Mesa auxiliar',
+      category: 'Furnitures',
+      price: 59.99,
+      image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71Axy7YHLNL._AC_UL320_.jpg',
+      desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
+  },
+  {
+      id:9,
+      name: 'Serie RC con licencia oficial',
+      category: 'Toys',
+      price: 32.99,
+      image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71tICZPyOPL._AC_UL320_.jpg',
+      desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
+  },
+  {
+      id:10,
+      name: 'Elmo',
+      category: 'Toys',
+      price: 7.49,
+      image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71qqqhUGH3L._AC_UL320_.jpg',
+      desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus est minima nostrum voluptatibus nisi quis.`   
+  }
+];
+
+const cardsContainer = document.querySelector(".cards-container"),
+navbarLeft = document.querySelector(".navbar-left-ul"),
+mobilMenu = document.querySelector(".mobile-menu-ul"),
+productDetailContainer = document.querySelector('#product-detail'),
+productDetailAside = document.querySelector('.product-info'),
+background = document.querySelector('.product-detail-background');
+
+
+
+//Desplegar items
+window.addEventListener("DOMContentLoaded",function(){
+displayProductList(productList);
+displayMenuButtons();
 });
 
+function displayProductList(listItems) {
+let displayProducts = listItems.map(function (item) {
+  
+  return `
+  <div class="info-cards">
+  <div class="product-card" >
+  <img src=${item.image}  alt=${item.name} >
+  <div class="product-info" >
+    <div class= "info">
+      <p class="price">$${item.price}</p>
+      <p class="name">${item.name}</p>
+    </div>
+    <figure>
+      <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+  </div>
+  </div>
+  </div>`;
 
-// Fusion menu en mobile
+  });
+
+  displayProducts = displayProducts.join("");
+  cardsContainer.innerHTML = displayProducts;
+
+  const productImg = cardsContainer.querySelectorAll('.info-cards');
+  productImg.forEach(product=>{
+    product.addEventListener("click",event=>{
+      let url = event.target.src;
+      const productCategory = listItems.filter(function (productItem){
+        if(productItem.image === url){
+          let aside = `
+        <div class="product-detail-info">
+          
+          <img src=${productItem.image} alt="${productItem.name}" class="image-detail">
+          
+            <div class="product-info">
+              <p>$${productItem.price}</p>
+              <p>${productItem.name}</p>
+              <p>${productItem.desc}</p>
+              
+            </div> 
+          
+          `;
+          productDetailAside.innerHTML=aside;
+          //background.style.display='block';
+          productDetailContainer.classList.remove("inactive");
+
+          //productDetailContainer.classList.add("product-detail-background");
+        } 
+      });
+    }); 
+  });
+
+} 
+
+// desplegar menu botones
+function displayMenuButtons() {
+const categories = productList.reduce(
+  function (values, item) {
+    if (!values.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values;
+  },
+  ["All"]
+);
+const categoryBtns = categories
+  .map(function (category) {
+    return ` <li>
+    <li>
+    <a href="#" class="filter-btn" data-id="${category}">${category}</a>
+  </li>`
+  })
+  .join("");
+
+  navbarLeft.innerHTML = categoryBtns;
+  mobilMenu.innerHTML = categoryBtns;
+
+  /*Filtrar por categoría*/
+const filterBtns = navbarLeft.querySelectorAll(".filter-btn");
+const filterBtnsMb = mobilMenu.querySelectorAll(".filter-btn");
+//console.log(filterBtns);
+
+filterProducts(filterBtns);
+filterProducts(filterBtnsMb);
+
+function filterProducts(filter){
+  filter.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      //console.log(e.currentTarget.dataset.id);
+      
+      //Guardar id de la categoria
+      const category = e.currentTarget.dataset.id;
+      const productCategory = productList.filter(function (productItem) {
+       //console.log(productItem.category);
+    
+        if (productItem.category === category ) {
+          productDetailContainer.classList.add("inactive");
+          return productItem;
+        }
+      });
+      if (category === "All") {
+        displayProductList(productList);
+      } else {
+        displayProductList(productCategory);
+      }
+    });
+  });
+}
+}
+
+/*Email deskyop*/
+const desktopMenu = document.querySelector(".desktop-menu");
+const navEmail = document.querySelector('.navbar-email');
+navEmail.addEventListener('click',()=>{
+//console.log('navEmail clicked'); 
+if(!aside.classList.contains('inactive')|| !productDetailContainer.classList.contains('inactive')){
+  aside.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
+  
+}
+desktopMenu.classList.toggle('inactive'); 
+});
+
+/*Desplegar menu mobil*/
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 menuHamIcon.addEventListener('click',()=>{
-    if(!aside.classList.contains('inactive')){
-        aside.classList.add('inactive');
-    }
-    mobileMenu.classList.toggle('inactive');  
+if(!aside.classList.contains('inactive')||!productDetailContainer.classList.contains('inactive')){
+  aside.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
+}
+  mobileMenu.classList.toggle('inactive');  
 });
 
-//MY ORDER
+/*my order*/
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('#shopping-cart-container');
 
-const cardsContainer = document.querySelector('.cards-container');
-
 menuCarritoIcon.addEventListener('click',()=>{
-    if(!mobileMenu.classList.contains('inactive') ||!desktopMenu.classList.contains('inactive')){
-        mobileMenu.classList.add('inactive');
-        desktopMenu.classList.add('inactive');
-    }
-  
-    aside.classList.toggle('inactive');
+if(!mobileMenu.classList.contains('inactive') ||!desktopMenu.classList.contains('inactive') ||!productDetailContainer.classList.contains('inactive')){
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');  
+}
+
+aside.classList.toggle('inactive');
+
 });
 
-//Lista de productos: HTML a partir de arrays
-const productList= [];
-productList.push({
-    name:"Bike",
-    price:120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+/*Close Icon*/
+const closeIcon = productDetailContainer.querySelector('.product-detail-close');
+console.log(closeIcon);
+closeIcon.addEventListener('click',()=>{
+productDetailContainer.classList.add('inactive');
 });
-productList.push ({
-    name:'Bicycle helmet',
-    price: 1200,
-    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
-});
-productList.push ({
-    name:'Bicycle helmet',
-    price: 1600,
-    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
-});
-productList.push ({
-    name:'Bicycle helmet',
-    price: 1500,
-    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
-});
-productList.push ({
-    name:'Seat',
-    price: 300,
-    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
-});
-productList.push ({
-    name:'Tennis Montain Bike',
-    price: 2200,
-    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
-});
-productList.push ({
-    name:'Sunglasses',
-    price: 800,
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
-});
-productList.push ({
-    name:'Sunglasses',
-    price: 600,
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
-});
-productList.push ({
-    name:'Bicycle seat bag',
-    price: 876,
-    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
-}); 
