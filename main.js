43fa1550a50b37044a3595b0b175970f25c13616
Duvan Daza <duvandaza9@gmail.com>
2023-01-01 19:46:54 -0500

    quedo la lista de productos, ahora si

--- main.js
+++ main.js
@@ -64,56 +64,44 @@ productList.push({
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 })
 
-
-/* 
-<div class="product-card">
-        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-        <div class="product-info">
-          <div>
-            <p>$120,00</p>
-            <p>Bike</p>
-          </div>
-          <figure>
-            <img src="./icons/bt_add_to_cart.svg" alt="">
-          </figure>
-        </div>
-      </div> 
-*/
-
-
-for (product of productList){
-    const productCard = document.createElement("div")
-    productCard.classList.add("product-card")
-
-    const productImg = document.createElement("img")
-    productImg.setAttribute("src", product.image)
-
-    const productInfo = document.createElement("div")
-    productInfo.classList.add("product-info")
-
-    const productInfoDiv = document.createElement("div")
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement("div")
+        productCard.classList.add("product-card")
+    
+        const productImg = document.createElement("img")
+        productImg.setAttribute("src", product.image)
+    
+        const productInfo = document.createElement("div")
+        productInfo.classList.add("product-info")
+    
+        const productInfoDiv = document.createElement("div")
+            
+        const productPrice = document.createElement("p")
+        productPrice.innerText = "$" + product.price
+        const productName = document.createElement("p")
+        productName.innerText = product.name
+    
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+        const productInfoFigure = document.createElement("figure")
+        const productImgCart = document.createElement("img")
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
+    
+        productInfoFigure.appendChild(productImgCart)
         
-    const productPrice = document.createElement("p")
-    productPrice.innerText = "$" + product.price
-    const productName = document.createElement("p")
-    productName.innerText = product.name
-
-    productInfoDiv.appendChild(productPrice)
-    productInfoDiv.appendChild(productName)
-
-    const productInfoFigure = document.createElement("figure")
-    const productImgCart = document.createElement("img")
-    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
-
-    productInfoFigure.appendChild(productImgCart)
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
     
-    productInfo.appendChild(productInfoDiv)
-    productInfo.appendChild(productInfoFigure)
-
-    productCard.appendChild(productImg)
-    productCard.appendChild(productInfo)
-
-    cardsContainer.appendChild(productCard)
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
     
-} 
+        cardsContainer.appendChild(productCard)
+        
+    }
+}
+
+renderProducts(productList)
+ 
 
