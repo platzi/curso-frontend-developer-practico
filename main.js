0d0a5017b247ff33313000272a8f5d21971ac06c
Emzyel <Arenamisael.1997@gmail.com>
2023-01-01 23:17:17 -0400

    Se agrego la lista de productos

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ menuCarritoDeCompras.addEventListener('click' , showMyOrder)
 
 const porductDetail = document.querySelector('.product-detail')
 
+const cardsContainer = document.querySelector('.cards-container')
+
+
+
 
 function activeDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
@@ -33,3 +37,57 @@ function showMyOrder(){
     mobileMenu.classList.add('inactive')
     desktopMenu.classList.add('inactive')
 }
+
+
+/* Lista de productos dinamica */
+const listaDeProductos = [];
+listaDeProductos.push({
+    nombre: 'Bicicleta',
+    precio: 120,
+    img: 'https://falabella.scene7.com/is/image/FalabellaPE/881952255_1?wid=800&hei=800&qlt=70'
+});
+listaDeProductos.push({
+    nombre: 'Monitor',
+    precio: 200,
+    img: 'https://http2.mlstatic.com/D_NQ_NP_654285-MLV52559418174_112022-W.jpg'
+});
+listaDeProductos.push({
+    nombre: 'Pc Gamer',
+    precio: 1220,
+    img: 'https://m.media-amazon.com/images/I/51-FhVIYrVL.jpg'
+});
+
+/* Este for realiza un recorrido por el array de la lista de productos y a su ves, crea los elementos en el HTML para cada producto que se agregue a la tienda. */
+for (productos of listaDeProductos){
+
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', productos.img);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPierce = document.createElement('p');
+    productPierce.innerHTML = '$' + productos.precio;
+
+    const productName = document.createElement('p');
+    productName.innerHTML = productos.nombre;
+
+    productInfoDiv.append(productName, productPierce);
+
+    const productFigure = document.createElement('figure');
+    const productFigureimg = document.createElement('img');
+    productFigureimg.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productFigure.appendChild(productFigureimg);
+
+    productInfo.append(productInfoDiv, productFigure);
+
+    productCard.append(img, productInfo);
+
+    cardsContainer.append(productCard)
+};
