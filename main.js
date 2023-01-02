8ff4698d617eb9987ef8c05c4ba008b0c7e8eff8
Emzyel <Arenamisael.1997@gmail.com>
2023-01-01 22:29:09 -0400

    Se agrego el menu del carrito de compras y se ajusto en codigo para ocultar los demas menus si otros se abren

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
+/* Eventos del menu desktop */
 const menuEmail = document.querySelector('.navbar-email');
 menuEmail.addEventListener('click', activeDesktopMenu);
 
 const desktopMenu = document.querySelector('.desktop-menu');
 
+/* Eventos del menu mobile */
 const hamburMenu = document.querySelector('.menu')
 hamburMenu.addEventListener('click', activeMobileMenu)
 
 const mobileMenu = document.querySelector('.mobile-menu')
 
 
+/* Eventos del carrito de compra */
+const menuCarritoDeCompras = document.querySelector('.navbar-shopping-cart')
+menuCarritoDeCompras.addEventListener('click' , showMyOrder)
+
+const porductDetail = document.querySelector('.product-detail')
+
 
 function activeDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+    porductDetail.classList.add('inactive')
 }
 
 function activeMobileMenu() {
     mobileMenu.classList.toggle('inactive')
+    porductDetail.classList.add('inactive')
+}
+
+function showMyOrder(){
+    porductDetail.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
 }
