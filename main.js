bdd3e7a61a4d90d841d30f3e80cf1e50b467179f
Maxi08 <mnoc018@gmail.com>
2023-01-02 15:37:07 -0400

    Subida de fusion mobile y desktop

--- main.js
+++ main.js
@@ -2,6 +2,23 @@
 const email = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
 
+//Mobile constantes
+
+const menuBurger =document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+//Mobile menu events
+
+menuBurger.addEventListener('click',toggleMobileMenu);
+
+function toggleMobileMenu (){
+
+    mobileMenu.classList.toggle ('inactive');
+}
+
+
+
+//Desktop menu events
 email.addEventListener('click',toggleMenu);
 
 function toggleMenu(){
