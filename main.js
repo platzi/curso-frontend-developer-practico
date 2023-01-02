da7a771ece57cb9e18ea92ec301896591d00db6f
matias ongay <ongaymatias@gmail.com>
2023-01-02 11:24:27 -0300

    Agrego css del product detail ,cambio el nombre de las clases del aside por id

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHam = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarrito=document.querySelector('.navbar-shopping-cart');
-const aside=document.querySelector('.product-detail');
+const shoppingCartContainer=document.querySelector('#shoppingCartContainer');
 const cardsContainer=document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
@@ -11,16 +11,16 @@ menuHam.addEventListener('click',toggleMobileMenu)
 menuCarrito.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isAsideClosed=aside.classList.contains('inactive');
+    const isAsideClosed=shoppingCartContainer.classList.contains('inactive');
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu() {
-    const isAsideClosed=aside.classList.contains('inactive');
+    const isAsideClosed=shoppingCartContainer.classList.contains('inactive');
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -30,7 +30,7 @@ function toggleCarritoAside(){
     if(!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
     
    
 }
