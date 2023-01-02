4f7f63ad5dc20b0bafefcf2a1efd559408c5303e
Emzyel <Arenamisael.1997@gmail.com>
2023-01-01 20:02:07 -0400

    Cambios recientes, se agrgo el menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
-
-menuEmail.addEventListener('click', active);
+menuEmail.addEventListener('click', activeDesktopMenu);
 
 const desktopMenu = document.querySelector('.desktop-menu');
 
-function active(){
+const hamburMenu = document.querySelector('.menu')
+hamburMenu.addEventListener('click', activeMobileMenu)
+
+const mobileMenu = document.querySelector('.mobile-menu')
+
+
+
+function activeDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+
+function activeMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
