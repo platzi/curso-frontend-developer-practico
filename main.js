433ece009db322520f26c4460766fdc04026802c
Franz Minaya <franz.cristhians10@gmail.com>
2023-01-02 18:34:44 -0500

    Agregando el carrito de compras y condicionales para que se abra y se cierre otro elemneto

--- main.js
+++ main.js
@@ -2,14 +2,45 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu =document.querySelector('.mobile-menu');
+const menuCarritoIcon =document.querySelector('.navbar-shopping-cart');
+const asideProductDetail =document.querySelector('.product-detail');
+
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu(){ 
+    const isAsideProductDetailClosed = asideProductDetail.classList.contains('inactive');
+
+    if(!isAsideProductDetailClosed){
+        asideProductDetail.classList.add("inactive");
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
+    const isAsideProductDetailClosed = asideProductDetail.classList.contains('inactive');
+
+    if(!isAsideProductDetailClosed){
+        asideProductDetail.classList.add("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive");
+
+}
+
+function toggleCarritoAside(){
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    } else if (!isDesktopMenuClosed){
+        desktopMenu.classList.add("inactive");
+    }
+
+    asideProductDetail.classList.toggle("inactive");
 }
