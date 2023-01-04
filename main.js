cbd22a1f266c7083d1b3e31be1b7798112bac078
George Abou Chanab <abuchegeorge@gmail.com>
2023-01-04 10:49:02 -0300

    creacion de html com javascript

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ const mobileMenu=document.querySelector(".mobile-menu")
 const menuHamburgesa=document.querySelector(".menu")
 const carritoCompra=document.querySelector(".navbar-shopping-cart")
 const aside=document.querySelector(".product-detail")
+cardsContainer=document.querySelector(".cards-container")
+
 
 
 menuEmail.addEventListener("click",toggleDesktopMenu);
 menuHamburgesa.addEventListener("click",toggleMobileMenu)
-carritoCompra.addEventListener("click",toggleCarritoIcon)
+carritoCompra.addEventListener("click",toggleCarritoIcon) //aside
 
 function toggleDesktopMenu() {
     const isAsideclosed = aside.classList.contains("inactive")
@@ -40,3 +42,99 @@ function toggleCarritoIcon(){
 
     aside.classList.toggle("inactive");
 }
+
+const productList=[];
+productList.push({
+    name: "bike",
+    price:120,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+productList.push({
+    name: "pantalla",
+    price:300,
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNA2SH5mtSTJ7AYgVWjOvw3-gBYv2CbrUuXw&usqp=CAU"
+
+})
+
+productList.push({
+    name: "reloj-pared",
+    price:30,
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCUOr7suliYUxLDtn6ZJJTowm61HN3Z80eLA&usqp=CAU"
+
+})
+
+productList.push({
+    name: "cama",
+    price:200,
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIaqG_xBhT_GfmWssFNb5JuBwpSXje0P4kQw&usqp=CAU"
+
+})
+
+productList.push({
+    name: "mueble",
+    price:150,
+    image:"https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&w=600"
+
+})
+
+productList.push({
+    name: "radio",
+    price:100,
+    image:"https://images.pexels.com/photos/5211673/pexels-photo-5211673.jpeg?auto=compress&cs=tinysrgb&w=600"
+
+})
+
+productList.push({
+    name: "mesa-comedor",
+    price:440,
+    image:"https://images.pexels.com/photos/1457841/pexels-photo-1457841.jpeg?auto=compress&cs=tinysrgb&w=600"
+
+})
+
+productList.push({
+    name: "lavadora",
+    price:280,
+    image:"https://media.istockphoto.com/id/153593643/es/foto/m%C3%A1quina-de-lavado-aislado-en-blanco.jpg?s=1024x1024&w=is&k=20&c=XMC4xsx5lgI78JNL2aH2Lnjig8s7O-lWXX3QLzE3Piw="
+
+})
+
+function renderProducts(arr) {
+    for (product of arr) {
+      const productCard = document.createElement('div');
+      productCard.classList.add('product-card');
+    
+      // product= {name, price, image} -> product.image
+      const productImg = document.createElement('img');
+      productImg.setAttribute('src', product.image);
+    
+      const productInfo = document.createElement('div');
+      productInfo.classList.add('product-info');
+    
+      const productInfoDiv = document.createElement('div');
+    
+      const productPrice = document.createElement('p');
+      productPrice.innerText = '$' + product.price;
+      const productName = document.createElement('p');
+      productName.innerText = product.name;
+    
+      productInfoDiv.appendChild(productPrice);
+      productInfoDiv.appendChild(productName);
+    
+      const productInfoFigure = document.createElement('figure');
+      const productImgCart = document.createElement('img');
+      productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+      productInfoFigure.appendChild(productImgCart);
+    
+      productInfo.appendChild(productInfoDiv);
+      productInfo.appendChild(productInfoFigure);
+    
+      productCard.appendChild(productImg);
+      productCard.appendChild(productInfo);
+    
+      cardsContainer.appendChild(productCard);
+    }
+  }
+  
+  renderProducts(productList);
