eef6d66ec58edaa32640343e90279c670ab91095
Jared Ozono <jaredozono@gmail.com>
2023-01-04 15:51:56 -0800

    agrege los productos con js utilizando el dominio del DOM

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector (".navbar-shopping-cart");
 const menuHamIcon = document.querySelector (".menu");
 const mobileMenu = document.querySelector (".mobile-menu");
 const aside = document.querySelector (".product-detail");
+const cardsContainer = document.querySelector (".cards-container");
 
 menuEmail.addEventListener ("click", toggleDesktopMenu);
 menuHamIcon.addEventListener ("click", toggleMobileMenu);
@@ -43,3 +44,61 @@ function toggleCarritoAside () {
     
     aside.classList.toggle ("inactive")
 }
+
+const productList = [];
+productList.push ({
+    name: 'Bike',
+    Price: 120,
+    Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push ({
+    name: 'TV',
+    Price: 220,
+    Image: 'https://assets.sams.com.mx/image/upload/f_auto,q_auto:eco,w_350,c_scale,dpr_auto/mx/images/product-images/img_large/980000170l.jpg'
+});
+productList.push ({
+    name: 'Computer',
+    Price: 620,
+    Image: 'https://i.dell.com/sites/imagecontent/merchandizing/PublishingImages/xps_8900.png'
+});
+
+function renderProducts (arr) {
+for (product of productList) {
+
+   const productCard = document.createElement ('div');
+   productCard.classList.add ('product-card');
+
+   const productImg = document.createElement ("img") 
+   productImg.setAttribute ('src', product.Image);
+   
+   const productInfo = document.createElement ('div');
+   productInfo.classList.add ('product-info');
+
+   const productInfoDiv = document.createElement ('div');
+
+   const productPrice = document.createElement ('p');
+   productPrice.innerText = '$' + product.Price;
+
+   const productName = document.createElement ('p');
+   productName.innerText = '$' + product.name; 
+
+   productInfoDiv.appendChild (productPrice);
+   productInfoDiv.appendChild (productName);
+
+   const productInfoFigure = document.createElement ('figure');
+   const productImgCart = document.createElement ('img');
+   productImgCart.setAttribute ('src', './icons/bt_add_to_cart.svg');
+
+   productInfoFigure.appendChild (productImgCart);
+   
+   productInfo.appendChild (productInfoDiv);
+   productInfo.appendChild (productInfoFigure);
+
+   productCard.appendChild (productImg);
+   productCard.appendChild (productInfo);
+
+   cardsContainer.appendChild (productCard);
+}    
+}
+
+renderProducts(productList);
