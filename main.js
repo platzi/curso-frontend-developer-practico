c1598989a879452817a558285b1522b8dda4bde9
Genesis Patiño <genesisptn20@gmail.com>
2023-01-04 15:24:19 -0500

    fusión del iconBurgerMenu del navbar y su lista desblegable (menu-mobile), también agregamos media queries

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const iconBurgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navbarEmail.addEventListener("click", toggleDesktopMenu);
+iconBurgerMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("active");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("active");
 }
