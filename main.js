955f25be32f73dc3644d0850aeb502bcf43214ab
Genesis Patiño <genesisptn20@gmail.com>
2023-01-04 16:20:20 -0500

    fusión del carrito de compras con los detalles de compra, y funcionalidad para que las listas desplegables se muestren o no

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const iconBurgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 
 navbarEmail.addEventListener("click", toggleDesktopMenu);
 iconBurgerMenu.addEventListener("click", toggleMobileMenu);
+navbarShoppingCart.addEventListener("click", toggleProductDetail);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("active");
+  productDetail.classList.add("active");
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("active");
+  productDetail.classList.add("active");
+}
+
+function toggleProductDetail(){
+  productDetail.classList.toggle("active")
+  desktopMenu.classList.add("active");
+  mobileMenu.classList.add("active");
 }
