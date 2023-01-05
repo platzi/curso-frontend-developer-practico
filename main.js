2b5f31d22da8a498416e9e5a6a2ef0566b028258
Donovan Rojas <rojasdrm03@gmail.com>
2023-01-05 11:39:17 -0500

    JS de logica de quitar menus cuando hay conflictos-mejorar

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarrritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click',toggleMobileMenu)
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarrritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+    
+    if (!isAsideClose){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+    
+    if (!isAsideClose){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    
+
+    //aside.classList.toggle('inactive');
+    if (!isMobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
