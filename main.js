8e7414e761d3972cc9a811e466751db5507cc1ee
carlos melgarejo <melgarejocarlos02@gmail.com>
2023-01-05 15:04:26 -0600

    Segunda fusion del menu, fusionamos el menu-mobile

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDestopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDestopMenu (){
- desktopMenu.classList.toggle('inactive')
+desktopMenu.classList.toggle('inactive')
 }
 
+function toggleMobileMenu (){
+    mobileMenu.classList.toggle('inactive')
+}
