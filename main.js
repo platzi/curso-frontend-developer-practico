aae5b97dbf3688febc24e1cb2bd45ded803481e2
Genesis Patiño <genesisptn20@gmail.com>
2023-01-04 21:26:26 -0500

    Se creo la lista de productos desde js (base de datos), a la cual vamos a acceder y sera renderizada en el html.

--- main.js
+++ main.js
@@ -59,19 +59,6 @@ productsList.push({
   image: "https://i.postimg.cc/hXfTVS37/27acd65217afdaf67ab65f33b11d94bf.jpg",
 });
 
-/* <div class="product-card">
-    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-    <div class="product-info">
-      <div>
-        <p>$120,00</p>
-        <p>Bike</p>
-      </div>
-      <figure>
-        <img src="./icons/bt_add_to_cart.svg" alt="">
-      </figure>
-    </div>
-  </div> */
-
 function renderProducts(arr) {
   for (product of arr) {
     const productCard = document.createElement("div");
