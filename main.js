43d6cd9d58fd241297f51735a7175612f7d7b9cd
rafaelsolanogit config --global user.name rafaelsolanogit config --global user.name rafaelsolano <rafaelsolanol007@gmail.com>
2023-01-07 13:57:32 -0500

    Se crea la funcion de mostrar ocultar --> Shopping cart, y se agrago bg a menu hambur

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ const desktop_menu = document.querySelector('.desktop-menu');
 const mobile_menu =document.querySelector('.mobile-menu ');
 const menu = document.querySelector('.menu');
 
+const shopping_cart = document.querySelector('.shopping-cart-container');
+const navbar_shopping_cart = document.querySelector('.navbar-shopping-cart')
+
 
 menu_email.addEventListener('click', toggle_menu)
-
 menu.addEventListener('click', toggle_menu_hamburguer);
+
+navbar_shopping_cart .addEventListener('click', toggle_shopping_cart);
+
 
 function toggle_menu_hamburguer(){
   mobile_menu.classList.toggle('inactive');
@@ -17,4 +22,13 @@ function toggle_menu_hamburguer(){
 function toggle_menu(){
   desktop_menu.classList.toggle('inactive');
 
+}
+
+//Mostrar ocultar carrito de compras
+function toggle_shopping_cart(){
+
+  const is_mobile_menu_closed = 
+  shopping_cart.classList.toggle('inactive');
+  mobile_menu.classList.add('inactive')
+  
 }
