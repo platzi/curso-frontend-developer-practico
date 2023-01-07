da5a787b027c1266cdae9fabd359de9e5804b304
rafaelsolanogit config --global user.name rafaelsolanogit config --global user.name rafaelsolano <rafaelsolanol007@gmail.com>
2023-01-07 12:32:35 -0500

    se agrega funcionalidad --> menu hamburguesa para mobile

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const menu_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
 
+const mobile_menu =document.querySelector('.mobile-menu ');
+const menu = document.querySelector('.menu');
+
 
 menu_email.addEventListener('click', toggle_menu)
 
+menu.addEventListener('click', toggle_menu_hamburguer);
+
+function toggle_menu_hamburguer(){
+  mobile_menu.classList.toggle('inactive');
+
+}
+
 function toggle_menu(){
   desktop_menu.classList.toggle('inactive');
-
 
 }
