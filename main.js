fdc90366d6629febf1803836ff812cdddcde0208
Jared Ozono <jaredozono@gmail.com>
2023-01-06 16:26:39 -0800

    Agrege la informacion del producto y arregle los problemas con css

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector (".desktop-menu");
 const menuCarritoIcon = document.querySelector (".navbar-shopping-cart");
 const menuHamIcon = document.querySelector (".menu");
 const mobileMenu = document.querySelector (".mobile-menu");
-const aside = document.querySelector (".product-detail");
+const shoppingCartContainer = document.querySelector ("#shoppingCartContainer");
 const cardsContainer = document.querySelector (".cards-container");
 
 menuEmail.addEventListener ("click", toggleDesktopMenu);
@@ -11,20 +11,20 @@ menuHamIcon.addEventListener ("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener ("click", toggleCarritoAside);
 
 function toggleDesktopMenu () {
-    const isAsideColsed = aside.classList.contains ("inactive");
+    const isAsideColsed = shoppingCartContainer.classList.contains ("inactive");
 
     if (!isAsideColsed) {
-        aside.classList.add ("inactive");
+        shoppingCartContainer.classList.add ("inactive");
     }
 
     desktopMenu.classList.toggle ("inactive")
 }
 
 function toggleMobileMenu () {
-    const isAsideColsed = aside.classList.contains ("inactive");
+    const isAsideColsed = shoppingCartContainer.classList.contains ("inactive");
 
     if (!isAsideColsed) {
-        aside.classList.add ("inactive");
+        shoppingCartContainer.classList.add ("inactive");
     }
 
     mobileMenu.classList.toggle ("inactive");
@@ -42,7 +42,7 @@ function toggleCarritoAside () {
         mobileMenu.classList.add ("inactive");
     }
     
-    aside.classList.toggle ("inactive")
+    shoppingCartContainer.classList.toggle ("inactive")
 }
 
 const productList = [];
