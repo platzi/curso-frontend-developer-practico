b518fb348b6ab5891e52bfb4b80858225af787c5
iargotes <iargotes@gmail.com>
2023-01-06 21:57:41 -0500

    Se fusionan el menu de desktop de movil y el sidebar

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 const email = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobilMenu = document.querySelector('.mobile-menu');
+const burgerImg = document.querySelector('.menu');
+const cartContents = document.querySelector('.product-detail')
+const carrito = document.querySelector('.navbar-shopping-cart')
+
 email.addEventListener('click', toogleDesktopMenu);
+burgerImg.addEventListener('click',toogleMobilMenu)
+carrito.addEventListener('click',toogleCartContens);
 
 
-
-function toogleDesktopMenu(){
-    console.log("ve el click")
+function toogleDesktopMenu(){   
+    cartContents.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
-console.log("java funciona")
+function toogleMobilMenu(){
+    cartContents.classList.add('inactive');
+    mobilMenu.classList.toggle('inactive');
+}
+
+function toogleCartContens(){
+    desktopMenu.classList.add('inactive');
+    mobilMenu.classList.add('inactive');
+    cartContents.classList.toggle('inactive');
+}
