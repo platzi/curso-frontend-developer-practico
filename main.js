9271924c0f516c734896a3e150e534b4d1124aab
DevBeex <catsdevops@gmail.com>
2023-01-06 20:59:23 -0500

    pull request terminado

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
+const productDetailContainer = document.querySelector('#productDetail')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
+
 
 function toggleDesktopMenu (){
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
@@ -25,6 +30,8 @@ function toggleMobileMenu () {
     if (!isAsideClosed) {
         shoppingCartContainer.classList.add('inactive')
     }
+
+    closeProductDetailAside();
     mobileMenu.classList.toggle('inactive')
 }
 
@@ -38,8 +45,23 @@ function toggleCarritoAside (){
     if (!isDesktopMenuClosed){
         desktopMenu.classList.add('inactive')
     }
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
+
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive')
+    }
 
     shoppingCartContainer.classList.toggle('inactive')
+}
+
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive')
+
+    productDetailContainer.classList.remove('inactive')
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive')
 }
 
 const productList= [];
@@ -77,6 +99,7 @@ function renderProducts (arr){
         
         const productImg = document.createElement('img')
         productImg.setAttribute('src', product.image)
+        productImg.addEventListener('click', openProductDetailAside)
     
         const productInfo = document.createElement('div')
         productInfo.classList.add('product-info')
