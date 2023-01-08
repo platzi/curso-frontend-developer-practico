0dcbab9b7ce3f900019de0ece03745f0aac9dea4
Olga Acosta Bravo <olgaacostab@gmail.com>
2023-01-07 22:15:32 -0500

    adding menu options to the nav bar in the mobile view

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const menuEmail = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive')
-}
+  desktopMenu.classList.toggle('inactive');
+};
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
+};
