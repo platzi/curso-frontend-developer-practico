7dd631cb99db9dfb8f1e9cea069bf910886768cc
julminador <juliansosa021@gmail.com>
2023-01-07 21:42:37 -0500

    Integrated desktop and mobile menu

---
+++ main.js
@@ -0,0 +1,15 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+iconMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
+}
