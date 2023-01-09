6a01c29364a1c5527ab0573ccff2b4dda7bde121
Juan Castro <jcastro2433@gmail.com>
2023-01-09 17:57:27 -0500

    Clase 19: Adición de Toggle al menú de la izquierda en la vista mobile

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Aparece y desaparece el menu del email
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
@@ -5,4 +6,14 @@ navEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+//Aparece y desaparece el menu de la izquierda
+const HamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+HamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
