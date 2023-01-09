e3a94535af6d70fef4eb3dd6ba83ce579c09a06c
tottipensotti <ag.pensotti@gmail.com>
2023-01-09 16:42:48 -0300

    menus behaviour

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 const navMail = document.querySelector('li.navbar-email');
-const navMenu = document.querySelector('.desktop-menu');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const asideBar = document.querySelector('.product-detail')
+const cartIcon = document.querySelector('.navbar-shopping-cart')
 
-navMail.addEventListener('click', toggleMenu);
+navMail.addEventListener('click', toggleDesktopMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', toggleAsideBar);
 
-function toggleMenu(){
-    navMenu.classList.toggle ('inactive')
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle ('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
+    if (!asideBar.classList.contains('inactive')) {
+        asideBar.classList.add('inactive')
+    } else {
+        return;
+    }
+}
+
+function toggleAsideBar(){
+    asideBar.classList.toggle('inactive')
+    if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive')
+    } else {
+        return;
+    }
 }
