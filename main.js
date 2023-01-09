f011b6559a6dc054c829a5f3a84a4bbcd86391d2
Donovan Rojas <rojasdrm03@gmail.com>
2023-01-09 09:39:34 -0500

    mod css aside y js por el id nuevo y sus conexiones

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarrritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,20 +11,20 @@ menuHamIcon.addEventListener('click',toggleMobileMenu);
 menuCarrritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isAsideClose = aside.classList.contains('inactive');
+    const isAsideClose = shoppingCartContainer.classList.contains('inactive');
     
     if (!isAsideClose){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClose = aside.classList.contains('inactive');
+    const isAsideClose = shoppingCartContainer.classList.contains('inactive');
     
     if (!isAsideClose){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -37,7 +37,7 @@ function toggleCarritoAside() {
     if (!isMobileMenuClose){
         mobileMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
