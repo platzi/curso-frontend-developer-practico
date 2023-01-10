c538bd67388e0c760f2d95905c9c860add54495f
sebastian ovallos <juansebastianod@ufps.edu.co>
2023-01-10 00:01:38 -0500

    mejore

---
+++ index.js
@@ -0,0 +1,8 @@
+const email=document.querySelector(".navbar-email");
+const otracosa=document.querySelector(".desktop-menu")
+
+email.addEventListener('click',()=>{
+
+otracosa.classList.toggle('inactive')
+
+})
