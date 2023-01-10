6c633296a920da0f80e91c9fa49c1dd118949516
tottipensotti <ag.pensotti@gmail.com>
2023-01-09 21:45:36 -0300

    Created product cards

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const asideBar = document.querySelector('.product-detail')
 const cartIcon = document.querySelector('.navbar-shopping-cart')
+const productContainer = document.querySelector('.cards-container')
+
 
 navMail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
@@ -29,4 +31,29 @@ function toggleAsideBar(){
     } else {
         return;
     }
+}
+
+const products = [];
+
+for(i=0; i < 20; i++) {
+    products.push({
+        name: `Bike Modelo ${[i] + 1}`,
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    })
+}
+
+for (product of products) {
+    productContainer.innerHTML += 
+        `
+        <div class="product-card">
+                    <img src=${product.image} alt="">
+                    <div class="product-info">
+                        <div>
+                            <p>${product.price}</p>
+                            <p>${product.name}</p>
+                        </div>
+                        <figure><img src="./icons/bt_add_to_cart.svg" alt=""></figure>
+                    </div>
+        </div>`
 }
