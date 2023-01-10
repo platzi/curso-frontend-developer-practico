17c85187d7c93fabe1d3f0b31fc1e261265d5e1f
juaNgarci13 <97314300+juaNgarci13@users.noreply.github.com>
2023-01-10 12:15:59 -0500

    Finalizacion del curso practico de js

--- script.js
+++ script.js
@@ -1,56 +1,3 @@
-// SELECION DE LOS ELEMENTOS MEDIANTE UN QUERYSELECTOR 
-const desktopMenu = document.querySelector(".desktop-menu");
-const navbarEmail = document.querySelector(".navbar-email");
-const burgerIconMenu = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-const mainCarIcon = document.querySelector(".navbar-shopping-cart");
-const asideCartConatiner = document.querySelector(".product-detail-cart-container");
-const cardsContainer = document.querySelector('.cards-container')
-
-// ASIGNACION DE LOS EVENT LISTENERS 
-navbarEmail.addEventListener("click", togggleShowDesktopMenu);
-burgerIconMenu.addEventListener('click', togggleMobileMenu)
-mainCarIcon.addEventListener('click', togggleCarAside)
-
-
-// FUNCIONES DE ASIGNACION Y SELECION DE LAS CLASES
-function togggleCarAside() {
-    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    const isDDesktopMenuClosed = desktopMenu.classList.contains('inactive');
-
-    if (!isMobileMenuClosed) {
-        // si mobilemenu esta open, hay que cerrarlo
-        mobileMenu.classList.add('inactive')
-    }
-    if (!isDDesktopMenuClosed) {
-        // si mobilemenu esta open, hay que cerrarlo
-        desktopMenu.classList.add('inactive')
-    }
-
-    asideCartConatiner.classList.toggle('inactive')
-}
-
-function togggleShowDesktopMenu() {
-    const isAsideClosed = asideCartConatiner.classList.contains('inactive')
-
-    if (!isAsideClosed) {
-        asideCartConatiner.classList.add('inactive')
-    }
-
-    desktopMenu.classList.toggle("inactive");
-}
-
-function togggleMobileMenu() {
-    const isAsideClosed = asideCartConatiner.classList.contains('inactive');
-
-    if (!isAsideClosed) {
-        // si mobilemenu esta open, hay que cerrarlo
-        asideCartConatiner.classList.add('inactive')
-    }
-
-    mobileMenu.classList.toggle("inactive");
-}
-
 const listItems = []
 listItems.push(
     {
@@ -69,15 +16,104 @@ listItems.push(
         image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     });
 
+// SELECION DE LOS ELEMENTOS MEDIANTE UN QUERYSELECTOR 
+const desktopMenu = document.querySelector(".desktop-menu");
+const navbarEmail = document.querySelector(".navbar-email");
+const burgerIconMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mainCarIcon = document.querySelector(".navbar-shopping-cart");
+const cardsContainer = document.querySelector('.cards-container')
+const asideCartConatiner = document.querySelector(".product-detail-cart-container");
+const asideProductContainer = document.querySelector(".product-detail-container");
+const iconCloseProductContainer = document.querySelector(".product-detail-close-container");
+
+// ASIGNACION DE LOS EVENT LISTENERS 
+navbarEmail.addEventListener("click", togggleShowDesktopMenu);
+burgerIconMenu.addEventListener('click', togggleMobileMenu)
+mainCarIcon.addEventListener('click', togggleCarAside)
+iconCloseProductContainer.addEventListener('click', closeContainer)
+
+
+// FUNCIONES DE ASIGNACION Y SELECION DE LAS CLASES
+
+// CARRITO DE COMPRAS 
+function togggleCarAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetailClosed = asideProductContainer.classList.contains('inactive')
+
+    if (!isMobileMenuClosed) {
+        // si mobilemenu esta open, hay que cerrarlo
+        mobileMenu.classList.add('inactive')
+    }
+    if (!isDDesktopMenuClosed) {
+        // si mobilemenu esta open, hay que cerrarlo
+        desktopMenu.classList.add('inactive')
+    }
+    if (!isProductDetailClosed) {
+        // si mobilemenu esta open, hay que cerrarlo
+        asideProductContainer.classList.add('inactive')
+    }
+
+    asideCartConatiner.classList.toggle('inactive')
+}
+
+// EMAIL DEL NAVBAR
+function togggleShowDesktopMenu() {
+    const isAsideClosed = asideCartConatiner.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        asideCartConatiner.classList.add('inactive')
+    }
+
+    desktopMenu.classList.toggle("inactive");
+}
+
+// MENU HAMBURGUESA MOBILE
+function togggleMobileMenu() {
+    const isAsideClosed = asideCartConatiner.classList.contains('inactive');
+    const isAsideProductDetailClosed = asideProductContainer.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        // si mobilemenu esta open, hay que cerrarlo
+        asideCartConatiner.classList.add('inactive')
+    }
+    if (!isAsideProductDetailClosed) {
+        closeContainer();
+    }
+
+    mobileMenu.classList.toggle("inactive");
+}
+
+// DETALLE DE LOS PRODUCTOS
+function togggleProductContainer() {
+
+    const isCartMobileClosed = asideCartConatiner.classList.contains('inactive')
+
+    if (!isCartMobileClosed) {
+        asideCartConatiner.classList.add('inactive')
+    }
+
+    asideProductContainer.classList.remove('inactive')
+}
+
+// CERRAR DETALLE PRODUCTOS
+function closeContainer() {
+    asideProductContainer.classList.add('inactive')
+}
+
 
 function renderProducts(arr) {
     for (items of arr) {
+
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
 
         const img = document.createElement('img');
         img.setAttribute('src', items.image);
         img.setAttribute('alt', 'img card');
+
+        img.addEventListener('click', togggleProductContainer)
 
         // -------
 
@@ -109,9 +145,6 @@ function renderProducts(arr) {
         productInfo.appendChild(productInfoFigure)
         productCard.appendChild(img)
         productCard.appendChild(productInfo)
-
-        console.log(productCard);
-
 
         cardsContainer.appendChild(productCard)
     }
