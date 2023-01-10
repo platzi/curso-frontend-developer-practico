6660e9df4c85c9f5860a0db6830cfef1ed5b3236
Juan Castro <jcastro2433@gmail.com>
2023-01-09 19:31:03 -0500

    Clase 20: se agregó un toggleCarro, toggleMobileMenu y toggledesktopMenu cada uno con abriéndose o cerrándose cuando le toque

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const desktopMenu = document.querySelector('.desktop-menu');
 navEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
+    const isasideclosed = aside.classList.contains('inactive');
+
+    if (!isasideclosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
@@ -15,5 +21,33 @@ const mobileMenu = document.querySelector('.mobile-menu');
 HamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+    //Esta constante me dice cuándo el aside está abierto, y cuándo está cerrado (true=cerrado, false=abierto)
+    const isasideclosed = aside.classList.contains('inactive');
+
+    //Si el aside no está cerrado, hágalo
+    if (!isasideclosed) {
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+//Aparece y desaparece el carro de compras
+const CarroIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+CarroIcon.addEventListener('click', toggleCarro);
+
+function toggleCarro() {
+    //Esta constante me dice cuándo el mobileMenu está abierto, y cuándo está cerrado
+    const ismobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isdesktopclosed = desktopMenu.classList.contains('inactive');
+
+    //Si el menu mobile no ésta cerrado, hágalo
+    if (!ismobileMenuClosed || !isdesktopclosed)  {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
