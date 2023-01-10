358f03a4df4829d1e4f8fc8cedae9bc10455ea24
tottipensotti <ag.pensotti@gmail.com>
2023-01-10 14:08:13 -0300

    added more details

--- main.js
+++ main.js
@@ -7,14 +7,13 @@ const cartIcon = document.querySelector('.navbar-shopping-cart');
 const productContainer = document.querySelector('.cards-container');
 const productDetailCloseBtn = document.querySelector('.product-detail-close');
 const productDetail = document.querySelector('#product-detail');
-// const productImg = document.querySelector('.product-card img');
+const closeCartIcon = document.querySelector('#shopping-cart-menu .title-container img');
 
 navMail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
 cartIcon.addEventListener('click', toggleShoppingCart);
-productDetailCloseBtn.addEventListener('click', toggleProductDetail);
-productImg.addEventListener('click', toggleProductDetail)
-
+productDetailCloseBtn.addEventListener('click', closeProductDetail);
+closeCartIcon.addEventListener('click', closeShoppingCart);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle ('inactive')
@@ -51,8 +50,12 @@ function toggleShoppingCart(){
     }
 }
 
-function toggleProductDetail(){
-    productDetail.classList.toggle ('inactive')
+function closeShoppingCart() {
+    shoppingCart.classList.add('inactive')
+}
+
+function openProductDetail(){
+    productDetail.classList.remove ('inactive')
     if (!shoppingCart.classList.contains('inactive')) {
         shoppingCart.classList.add('inactive')
     } else if (!desktopMenu.classList.contains('inactive')) {
@@ -62,6 +65,55 @@ function toggleProductDetail(){
     } else {
         return;
     }
+}
+
+function closeProductDetail(){
+    productDetail.classList.add ('inactive')
+    if (!shoppingCart.classList.contains('inactive')) {
+        shoppingCart.classList.add('inactive')
+    } else if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive')
+    } else if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive')
+    } else {
+        return;
+    }
+}
+
+function addToCart(){
+    const items = document.querySelector('#items-on-cart');
+    const orderContent = document.querySelector('#cart-items');
+    orderContent.innerHTML += `
+        <div class="shopping-cart">
+                <figure><img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike"></figure>
+                <p>Bike</p>
+                <p>$120</p>
+                <img src="./icons/icon_close.png" alt="close" onclick="removeFromCart()">
+        </div>
+    `
+    new_quantity = Number(items.textContent) + 1;
+    items.innerText = new_quantity;
+
+    var totalAmount = document.querySelector('.order p:nth-child(2)')
+    totalAmount.innerText = `$ ${new_quantity * 120}`
+}
+
+function removeFromCart() {
+    const items = document.querySelector('#items-on-cart');
+    const orderContent = document.querySelector('#cart-items');
+    const lastItem = orderContent.lastChild
+    orderContent.removeChild(lastItem)
+    const totalAmount = document.querySelector('.order p:nth-child(2)')
+    if ((Number(items.textContent) - 1) < 0) {
+        new_quantity = 0;
+        new_amount = 0;
+    } else {
+        new_quantity =  (Number(items.textContent) - 1)
+        new_amount = new_quantity * 120
+    }
+        items.innerText = new_quantity;
+        
+        totalAmount.textContent = `$ ${new_amount}`
 }
 
 const products = [];
@@ -78,13 +130,13 @@ for (product of products) {
     productContainer.innerHTML += 
         `
         <div class="product-card">
-                    <img src=${product.image} alt="">
+                    <img src=${product.image} alt="" onclick="openProductDetail()">
                     <div class="product-info">
                         <div>
-                            <p>${product.price}</p>
+                            <p>$ ${product.price}</p>
                             <p>${product.name}</p>
                         </div>
-                        <figure><img src="./icons/bt_add_to_cart.svg" alt=""></figure>
+                        <figure onclick="addToCart()"><img src="./icons/bt_add_to_cart.svg" alt=""></figure>
                     </div>
         </div>`
 }
