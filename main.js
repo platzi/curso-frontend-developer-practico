a27322be02e993a479af8b3527d0557071e89750
César Almeida <cesaralmeida89@gmail.com>
2023-01-11 20:46:40 +0100

    :lipstick: add logic with class inactive to desktop, mobile and cart menu

--- main.js
+++ main.js
@@ -2,14 +2,38 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartMenu = document.querySelector('.navbar-shopping-cart');
+const shoppingCart = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCartMenu.addEventListener('click', toggleShoppingCartMenu);
 
 function toggleDesktopMenu() {
+    const isShoppingCartClosed = shoppingCart.classList.contains('inactive');
+    if (!isShoppingCartClosed) {
+        shoppingCart.classList.add('inactive')
+    }
 	desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isShoppingCartClosed = shoppingCart.classList.contains('inactive');
+    if (!isShoppingCartClosed) {
+        shoppingCart.classList.add('inactive')
+    }
 	mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCartMenu() {
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+    shoppingCart.classList.toggle('inactive');
 }
