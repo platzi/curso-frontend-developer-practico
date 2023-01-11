18dfd4933958b3f8a91ffd78105893b9fbac2598
irvinpinedaUCA <00024420@uca.edu.sv>
2023-01-11 09:25:53 -0600

    product list

---
+++ main.js
@@ -0,0 +1,118 @@
+const navBarEmail = document.querySelector(".navbar-email");
+const hamMenu = document.querySelector(".menu");
+const menuCar = document.querySelector(".navbar-shopping-cart");
+
+const navMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
+
+
+navBarEmail.addEventListener('click', toggleDesktopMenu);
+hamMenu.addEventListener('click', toggleMobileMenu);
+menuCar.addEventListener('click', toggleCarMenu);
+
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
+    navMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarMenu() {
+    const isMobileClosed = mobileMenu.classList.contains('inactive');
+    const isNavMenuClosed = navMenu.classList.contains('inactive');
+
+    if (!isMobileClosed) {
+        mobileMenu.classList.add("inactive");
+    }
+    if (!isNavMenuClosed) {
+        navMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
+}
+
+
+const productList = [];
+productList.push({
+    name: 'Byke',
+    price: 120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Comoutadora',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+
+function renderProduct(arr){
+    
+for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+    cardsContainer.appendChild(productCard);
+
+};
+};
+
+
+renderProduct(productList);
+/* <div class="product-card">
+      <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+     <div class="product-info">
+       <div>
+         <p>$120,00</p>
+         <p>Bike</p>
+       </div>
+       <figure>
+         <img src="./icons/bt_add_to_cart.svg" alt="">
+       </figure>
+     </div>
+   </div>
+
+   */
