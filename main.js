9750d80d22e846a0bde3360b5629f34a645a4232
jorpaz <giovanny.paz@outlook.com>
2023-01-10 21:59:19 -0600

    Union de menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggledesktopMenu);
+burguerMenuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggledesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
