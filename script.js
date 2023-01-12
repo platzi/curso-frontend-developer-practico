681fca83d59c0dcee8c40ef89804dd91b1a5ae8a
Maxi Torres <torres_maximiliano@hotmail.com>
2023-01-12 14:54:13 -0300

    Fusión del menú Mobile y fusión del aside-Carrito de compras

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+/*      Menú Desktop desplegable        */
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 
 function toggleDesktopMenu() {
+    asideOrder.classList.add('inactive')
     desktopMenu.classList.toggle('inactive')
-    // if (desktopMenu.classList.contains('inactive')) {
-    //     desktopMenu.classList.remove('inactive') 
-    // } else {
-    //     desktopMenu.classList.add('inactive') 
-    // }
 }
+
+/*      Menú Mobile desplegable        */
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+menuHamIcon.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu() {
+    asideOrder.classList.add('inactive')
+    mobileMenu.classList.toggle('inactive')
+ }
+
+/*      Menú Desktop desplegable        */
+const navShoppingCart = document.querySelector('.navbar-shopping-cart')
+const asideOrder = document.querySelector('.product-detail')
+
+navShoppingCart.addEventListener('click', toggleAsideOrder)
+
+ function toggleAsideOrder() {
+    desktopMenu.classList.add('inactive')
+    asideOrder.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+ }
