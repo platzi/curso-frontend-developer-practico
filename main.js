096defb35b26e5956776fea6b61bd603ae562f63
jorpaz <giovanny.paz@outlook.com>
2023-01-12 15:04:27 -0600

    Inicio de Product Cards

--- main.js
+++ main.js
@@ -40,3 +40,10 @@ function toggleCarritoAside(){
 
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
