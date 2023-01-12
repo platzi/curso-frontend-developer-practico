910b17dd014eff07854f8bc4fcde4992c9311d18
Olga Acosta Bravo <olgaacostab@gmail.com>
2023-01-11 21:57:36 -0500

    adding aside menu for desktop and closing navbar menus in case another is open

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurgerIcon = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart')
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurgerIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+  aside.classList.add('inactive')
 };
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle('inactive');
+  aside.classList.add('inactive')
+};
+
+function toggleCartAside() {
+  aside.classList.toggle('inactive');
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
 };
