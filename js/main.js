a2c7481279bfe5ec3cf4423f56c9b1aedec6d220
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2023-01-13 12:24:05 -0500

    Add product details

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 const emailMenu = document.querySelector('.navbar__email');
 const desktopMenu = document.querySelector('.desktop__menu');
+
 const burguerMenu = document.querySelector('.navbar__menu');
 const mobileMenu = document.querySelector('.mobile__menu');
 
+const menuCarritoIcon = document.querySelector('.navbar__shopping__cart');
+const aside = document.querySelector('.product__detail');
+
 emailMenu.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+  aside.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle('inactive');
+  aside.classList.add('inactive');
+}
+
+function toogleCarritoAside() {
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  aside.classList.toggle('inactive');
 }
