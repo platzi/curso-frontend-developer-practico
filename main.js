432dc548afad02df136f35087c1222abbccd7b44
gabrielravelo <ravelogabriel2@gmail.com>
2023-01-13 13:41:12 -0400

    ajustes al carrito desktop-mobile

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguer = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const asideCarrito = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguer.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleMenuCarrrito);
 
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive')
+  desktopMenu.classList.toggle('inactive');
+  asideCarrito.classList.add('inactive');
 };
 
 function toggleMobileMenu() {
-  mobileMenu.classList.toggle('inactive')
+  mobileMenu.classList.toggle('inactive');
+  asideCarrito.classList.add('inactive');
 };
+
+function toggleMenuCarrrito() {
+  asideCarrito.classList.toggle('inactive');
+  mobileMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+}
