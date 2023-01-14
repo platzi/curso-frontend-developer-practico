ddd16c6c58b6b6e93ffa888f7b6574d792ec7d1b
Olga Acosta Bravo <olgaacostab@gmail.com>
2023-01-13 20:08:59 -0500

    adding second aside for products details and changing some classes that were repeated

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuBurgerIcon = document.querySelector('.menu');
 const menuCartIcon = document.querySelector('.navbar-shopping-cart')
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -12,36 +12,66 @@ menuCartIcon.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
-  aside.classList.add('inactive')
+  shoppingCartContainer.classList.add('inactive')
 };
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle('inactive');
-  aside.classList.add('inactive')
+  shoppingCartContainer.classList.add('inactive')
 };
 
 function toggleCartAside() {
-  aside.classList.toggle('inactive');
+  shoppingCartContainer.classList.toggle('inactive');
   desktopMenu.classList.add('inactive');
   mobileMenu.classList.add('inactive');
 };
 
 const productList = [];
-productList.push({
-  name: 'Bike',
-  price: 120,
+productList.push ({
+  name:'Bike',
+  price: 12700,
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
-productList.push({
-  name: 'Pantalla',
-  price: 100,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+productList.push ({
+  name:'Bicycle helmet',
+  price: 1200,
+  image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
 });
-productList.push({
-  name: 'PC',
-  price: 110,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+productList.push ({
+  name:'Bicycle helmet',
+  price: 1600,
+  image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
 });
+productList.push ({
+  name:'Bicycle helmet',
+  price: 1500,
+  image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+  name:'Seat',
+  price: 300,
+  image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+productList.push ({
+  name:'Tennis Montain Bike',
+  price: 2200,
+  image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+  name:'Sunglasses',
+  price: 800,
+  image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+  name:'Sunglasses',
+  price: 600,
+  image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push ({
+  name:'Bicycle seat bag',
+  price: 876,
+  image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+}); 
 
 function renderProducts(arr) {
   for (product of arr) {
