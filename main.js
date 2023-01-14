c64b7958b865c717f785c6e42a0734d3af9c4299
Harold Rodriguez <haroldr-658@hotmail.com>
2023-01-14 15:45:18 -0500

    se agrego mobile menu se le dieron paraetros a css y js todo quedo ok

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const menuEmail =document.querySelector('.navbar-email');
 const desktopMenu =document.querySelector('.desktop-menu');
+const menuHamIcon =document.querySelector('.menu');
+const mobileMenu =document.querySelector('.mobile-menu');
+
+
+
 
 menuEmail.addEventListener('click' , togleDestokMenu);
+menuHamIcon.addEventListener('click' , togleMobileMenu);
+
+
 
 function togleDestokMenu(){
    desktopMenu.classList.toggle('inactive');
+}
+function togleMobileMenu(){
+   mobileMenu.classList.toggle('inactive');
+
 }
