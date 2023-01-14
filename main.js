a1a604d6daf9eac5347410df9f2ca9061c0e288c
latyn <carlosleandrolatyn@gmail.com>
2023-01-14 08:05:58 -0300

    [Add] Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuMail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuMail.addEventListener('click', toggleDesktopMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
