0cae6455ba009b505d2a2ea652e69cb83dca9d67
nicolascastro77 <nik_ced@hotmail.com>
2023-01-15 17:08:24 -0500

    Menu responsive

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
 
+const menuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click',toggleDekstopMenu)
+
+menuEmail.addEventListener('click',toggleDekstopMenu);
+menuIcon.addEventListener('click',toggleMobileMenu);
 
 
 
 
 function toggleDekstopMenu(){
     desktopmenu.classList.toggle ('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle ('inactive');
 }
