a7bcdf0ce5acd7535874cb841d043fa994305750
dudley delgado <arturobonilla0412@gmail.com>
2023-01-16 00:45:32 +0100

    create a product-detail container and fixed css problems

--- main.js
+++ main.js
@@ -3,34 +3,34 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu)
-menuCarritoIcon.addEventListener('click', toogleCarritoAside)
+menuCarritoIcon.addEventListener('click', toogleCarritoshoppingCartContainer)
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.toggle('inactive');
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.toggle('inactive');
 
-    if(!isAsideClosed) {
-        aside.classList.add('inactive');
+    if(!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.toggle('inactive');
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.toggle('inactive');
 
-    if(!isAsideClosed) {
-        aside.classList.add('inactive');
+    if(!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
 }
 
-function toogleCarritoAside() {
+function toogleCarritoshoppingCartContainer() {
     const isMobileMenuClosed = mobileMenu.classList.toggle('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.toggle('inactive');
     
@@ -40,7 +40,7 @@ function toogleCarritoAside() {
         desktopMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
