aea23b0aba3a71801c6a3ec4203df0a1540325ab
Gabriel Ravelo <ravelogabriel2@gmail.com>
2023-01-14 22:50:56 -0400

    lista de productos

--- main.js
+++ main.js
@@ -24,3 +24,100 @@ function toggleMenuCarrrito() {
   mobileMenu.classList.add('inactive');
   desktopMenu.classList.add('inactive');
 }
+
+const productList = [];
+const cardsContainer = document.querySelector('.cards-container');
+
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Pantalla',
+  price: 220,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Computadora',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Computadora',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Computadora',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Computadora',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Computadora',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Computadora',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Computadora',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Computadora',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProducts(arr) {
+  for (const product of arr) {
+  
+    const productCard = document.createElement('DIV');
+    productCard.classList.add('product-card');
+  
+    const productImg = document.createElement('IMG');
+    productImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('DIV');
+    productInfo.classList.add('product-info'); 
+  
+    const productInfoDiv = document.createElement('DIV');
+  
+    const productPrice = document.createElement('P');
+    productPrice.innerText = '$' + product.price;
+  
+    const productName = document.createElement('P');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+  
+    const productInfoFigure = document.createElement('FIGURE');
+  
+    const productImgCart = document.createElement('IMG');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+};
+
+renderProducts(productList);
