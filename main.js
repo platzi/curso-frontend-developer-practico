e5d3d5154d589684b82b690082151d5c81ac7989
nicolascastro77 <nik_ced@hotmail.com>
2023-01-16 13:11:23 -0500

    colores y componentes

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const ProductDetailCloseIcon = document.querySelector('.product-detail-close');
+
 
 const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shopingCartDetail = document.querySelector('#shopingCartDetail');
 const cardsContainer = document.querySelector('.cards-container');
+const ProductDetailContainer = document.querySelector('#productDetail');
+
+
 
 
 menuEmail.addEventListener('click',toggleDekstopMenu);
 menuIcon.addEventListener('click',toggleMobileMenu);
 menuCarritoIcon.addEventListener('click',toggleshopingCartDetail);
+ProductDetailCloseIcon.addEventListener('click',closeProductDetailAside);
 
 
 function toggleDekstopMenu(){
@@ -20,13 +26,16 @@ function toggleDekstopMenu(){
     }
 
     desktopmenu.classList.toggle ('inactive');
+    closeProductDetailAside();
 }
+
+
 function toggleMobileMenu(){
     const isAsideCLosed = isMobileMenuCLosed = shopingCartDetail.classList.contains('inactive');
     if(!isAsideCLosed){
         shopingCartDetail.classList.add('inactive');
     }
-
+    closeProductDetailAside();
     mobileMenu.classList.toggle ('inactive');
 
 }
@@ -39,7 +48,26 @@ function toggleshopingCartDetail(){
         mobileMenu.classList.add('inactive');
     }
 
+    const isProductDetailClosed = ProductDetailContainer.classList.contains('inactive');
+
+
+    if(!isProductDetailClosed){
+        ProductDetailContainer.classList.add('inactive');
+    }
+
+
+
+
     shopingCartDetail.classList.toggle ('inactive');
+}
+function openProductDetailAside(){
+    
+    shopingCartDetail.classList.add('inactive');
+
+    ProductDetailContainer.classList.remove('inactive')
+}
+function closeProductDetailAside(){
+    ProductDetailContainer.classList.add('inactive');
 }
 
 
@@ -67,12 +95,35 @@ productList.push({
 });
 
 productList.push({
-    name: 'Bicicleta Todo Terreno Rin29',
-    price: 1.199005,
+    name: 'Bicicleta de montaña raleigh',
+    price: 1.599005,
+    image: 'https://falabella.scene7.com/is/image/FalabellaCO/12206337_1?wid=800&hei=800&qlt=70',
+
+});
+productList.push({
+    name: 'Bicicleta De Montaña Mountain Gear',
+    price: 2.129005,
     image: 'https://i.linio.com/p/990d274108fcd416025f98303bd315f0-product.webp',
 
 });
+productList.push({
+    name: 'Bicicleta de montaña Hawk Mountain',
+    price: 3.199005,
+    image: 'https://falabella.scene7.com/is/image/FalabellaCO/12069671_1?wid=800&hei=800&qlt=70',
 
+});
+productList.push({
+    name: 'Bicicleta De Montaña Mountain Gear Raven',
+    price: 1.099005,
+    image: 'https://falabella.scene7.com/is/image/FalabellaCO/7590940_1?wid=800&hei=800&qlt=70',
+
+});
+productList.push({
+    name: 'Bicicleta de Montaña GW',
+    price: 5.099005,
+    image: 'https://falabella.scene7.com/is/image/FalabellaCO/3922919_1?wid=800&hei=800&qlt=70',
+
+});
 /* <div class="product-card">
 <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
 <div class="product-info">
@@ -110,6 +161,7 @@ function renderProducts(arr){
         
         const productImg = document.createElement('img');
         productImg.setAttribute('src',product.image);
+        productImg.addEventListener('click',openProductDetailAside)
         
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
