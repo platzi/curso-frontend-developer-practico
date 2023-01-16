277b009acc2a3d73c3a0a1c60184725b4342d071
jojourbina <jormanurbina1@gmail.com>
2023-01-16 14:16:44 -0300

    se agregaron archivos iniciales para practicar la fusion de elementos y el uso del dom para que sea interactivo

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const navEmail=document.querySelector(".navbar-email");
+const desktopMenu=document.querySelector(".desktop-menu");
+navEmail.addEventListener("click",toggleDesktopMenu)
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle("inactive");
+    console.log("me tocaste")
+}
