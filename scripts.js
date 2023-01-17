d417d159db71f138a96c95bd2c1eeb128b6bef1a
jojourbina <jormanurbina1@gmail.com>
2023-01-17 13:05:39 -0300

    se agrego el aside que se oculte o muestre sin necesidad de solapar los otros componentes

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
-const navEmail=document.querySelector(".navbar-email");
+const btnEmail=document.querySelector(".navbar-email");
+const btnBurg=document.querySelector(".menu");
+const btnCarrito=document.querySelector(".navbar-shopping-cart");
 const desktopMenu=document.querySelector(".desktop-menu");
-navEmail.addEventListener("click",toggleDesktopMenu)
+const mobileMenu=document.querySelector(".mobile-menu")
+const aside=document.querySelector(".product-detail")
+
+btnEmail.addEventListener("click",toggleDesktopMenu);
+btnBurg.addEventListener("click",toggleMobileMenu);
+btnCarrito.addEventListener("click",toggleAside);
+
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+    aside.classList.add("inactive")
     console.log("me tocaste")
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive")
+    aside.classList.add("inactive")
+    console.log("tocaste la hamburguesa")
+}
+function toggleAside(){
+    desktopMenu.classList.add("inactive")
+    mobileMenu.classList.add("inactive")
+    aside.classList.toggle("inactive")
+    console.log("tocaste la carrito de compras")
 }
