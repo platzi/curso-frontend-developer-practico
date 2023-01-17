420cb6526be3b6ad10698ea23dfb9a03c938c93a
crisLeo-P01 <cristianleopaiva@gmail.com>
2023-01-16 22:32:42 -0300

    Se realizo colision con las bombas y eso resta vidas. - Colision con el regalito y pasa de nivel.

--- 04-Practica Javascript VideoJuego/game.js
+++ 04-Practica Javascript VideoJuego/game.js
@@ -8,11 +8,19 @@ const btnLeft = document.querySelector('#left');
 
 let canvasSize;
 let elementSize;
+let level = 0;
+let lives = 3 ;
 
 const playerPosition = {
   x: undefined,
   y: undefined,
 }
+const giftPosition = {
+  x: undefined,
+  y: undefined,
+}
+
+let enemyPositions = [];
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
@@ -28,7 +36,7 @@ function setCanvasSize() {
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
 
-  elementSize = canvasSize / 10;
+  elementSize = canvasSize / 10.2;
 
   startGame();
 }
@@ -40,10 +48,20 @@ function startGame() {
   game.font = elementSize + 'px Verdana';
   game.textAlign = 'end';
 
-  const mapNivel = maps[1];
-  const mapRows = mapNivel.trim().split('\n');
-  const mapRowCol = mapRows.map(row => row.trim().split(''))
+  const mapNivel = maps[level];
 
+  if(!mapNivel) {
+    gameWin();
+    return;
+  }
+
+  const mapRows = mapNivel.trim().split('\n');
+  const mapRowCol = mapRows.map(row => row.trim().split(''));
+  console.log({mapNivel, mapRows, mapRowCol});
+
+  /* Estamos asignando un nuevo array vacío cada vez que el jugador mueve el personaje */
+  enemyPositions = [];
+  // Cada vez que inicia el juego, hace un recarga del mapa completo 👇🏼
   game.clearRect(0,0,canvasSize, canvasSize);
 
   //////////////////////// INICIO DE UBICACIÓN DE LOS ELEMENTOS
@@ -69,29 +87,84 @@ function startGame() {
           playerPosition.y = posY;
           console.log({playerPosition});
         }
+      } else if (col == 'I'){
+        giftPosition.x = posX;
+        giftPosition.y = posY;
+      } else if (col == 'X') {
+        enemyPositions.push ({
+          x: posX,
+          y: posY,
+        })
       }
 
       game.fillText(emoji, posX, posY);
     })
-
-    movePlayer();
   });
-
+  
   //////////////////////// FINAL DE UBICACIÓN DE LOS ELEMENTOS
-
+  
   // (x, y, width, height)
   // game.fillRect(0, 50, 150, 100);
   // game.clearRect(50, 50, 150, 50);
-
+  
   // game.font = '25px Verdana';
   // game.fillStyle = 'red';
   // game.textAlign = 'start';
   // game.fillText('Platzi', 60, 85);
+  movePlayer();
 }
 
 ///////////// Función del movimiento del jugador en pantalla
 function movePlayer() {
+  const giftPositionX = playerPosition.x == giftPosition.x;
+  const giftPositionY = playerPosition.y == giftPosition.y;
+  const giftCollision = giftPositionX && giftPositionY;
+
+  if( giftCollision ) {
+    levelWin();
+  }
+
+  const enemyCollision = enemyPositions.find(enemy => {
+    const enemyCollisionX = enemy.x == playerPosition.x;
+    const enemyCollisionY = enemy.y == playerPosition.y;
+    return enemyCollisionX && enemyCollisionY;
+  })
+
+  if( enemyCollision ) {
+    levelFail();
+  }
+
   game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
+}
+
+function levelWin() {
+  console.log('Pasaste de nivel');
+  level++;
+  startGame();
+}
+
+function levelFail() {
+  console.log('Chocaste contra un enemigo');
+  lives--;
+  console.log(lives);
+
+  if(lives <= 0) {
+    level = 0;
+    lives = 3;
+  }
+
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
+  startGame();
+}
+
+function gameWin() {
+  console.log('Ganaste el juego');
+}
+
+function gameOver() {
+  console.log('Pisaste bomba');
+  startGame();
 }
 
 ///////////// Movimientos de con sus respectivas TECLAS
@@ -109,23 +182,42 @@ function moveByKeys(event) {
   else if(event.key == 'ArrowLeft') moveLeft();
 }
 
+/* Con cada movimiento del jugador, ejecute la función startGame(),
+es decir, que recarga el mapa con cada movimiento */
+
 function moveUp() {
-  console.log('Me muevo hacia arriba');
-  playerPosition.y -= elementSize;
-  startGame();
+  /* Con if preguntamos que si en la resta del jugador menos el mapa
+  nos da un número negativo, evita que se salgue del mapa. Caso
+  contrario, el jugador puede seguir moviendose */
+
+  if( (playerPosition.y - elementSize) < elementSize ) {
+    console.log('OUT');
+  } else {
+    playerPosition.y -= elementSize;
+    startGame();
+  }
 }
 function moveRight() {
-  console.log('Me muevo hacia la derecha');
-  playerPosition.x += elementSize;
-  startGame();
+  if( (playerPosition.x + elementSize) > canvasSize ) {
+    console.log('OUT');
+  } else {
+    playerPosition.x += elementSize;
+    startGame();
+  }
 }
 function moveDown() {
-  console.log('Me muevo hacia abajo');
-  playerPosition.y += elementSize;
-  startGame();
+  if( (playerPosition.y + elementSize) > canvasSize ) {
+    console.log('OUT');
+  } else {
+    playerPosition.y += elementSize;
+    startGame();
+  }
 }
 function moveLeft() {
-  console.log('Me muevo hacia la izquierda');
-  playerPosition.x -= elementSize;
-  startGame();
+  if( (playerPosition.x - elementSize) < elementSize ) {
+    console.log('OUT');
+  } else {
+    playerPosition.x -= elementSize;
+    startGame();
+  }
 }
