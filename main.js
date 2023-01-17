1f2a5fb5ea5f7c98007f156c6f01c0d549e9bfe1
Francesc Xavier Bosch Janer <xavi2794@gmail.com>
2023-01-17 18:03:26 +0100

    Mobile Menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toogleDesktopMenu);
+burgerMenu.addEventListener('click', toogleBurgerMenu);
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleBurgerMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
