e522d7a33e2b017b706e00bd92dfad131047dd64
jojourbina <jormanurbina1@gmail.com>
2023-01-17 23:30:51 -0300

    se colocaron observaciones para pensar en un codigo futuro y que tenga desarrollo

---
+++ proyecto_estee.js
@@ -0,0 +1,11 @@
+//proyecto ESTE
+const nombreUsuario=prompt("indique su nombre de usuario")
+const preguntasAna=[{
+    opcion:1,
+    name:"deporte",
+    pregunta:`cual el deporte favorito de ${nombreUsuario}`,
+  
+},{
+    opcion:2,
+    name:"comida",
+    pregunta:`cual es la comida favorita de ${nombreUsuario}}`]
