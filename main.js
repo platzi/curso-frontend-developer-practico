35e7d9a828493869280a0b4b1bf8eb7f6d6fa05c
Dany Sandoval <dasandovalal@gmail.com>
2023-01-18 11:42:54 -0700

    Se agrego una archivo index, style y js general para las paginas, cambios en la nav bar para visualizar menus y el contenido principal

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.querySelector('.mobile-menu')
 //ASIDE CARRITO
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
+//PRODUCT LIST
+const cardsContainer = document.querySelector('.cards-container')
 
 //DESKTOP MENU
 menuEmail.addEventListener('click', toggleDesktopMenu)
@@ -51,3 +53,81 @@ function toggleCarritoAside(){
 
     aside.classList.toggle('inactive')
 }
+
+//PRODUCT LIST
+const productList = []
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+})
+productList.push({
+    name: 'Pantalla',
+    price: 1800,
+    image: 'https://images.pexels.com/photos/6976094/pexels-photo-6976094.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+
+})
+productList.push({
+    name: 'Laptop',
+    price: 600,
+    image: 'https://images.pexels.com/photos/18105/pexels-photo.jpg'
+
+})
+
+/*
+<div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>
+      */
+
+//POR CADA ELEMENTO DEL ARREGLO, SE HARA UN DIV CON EL CONTENIDO DEL PRODUCTO
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div') //CREAR UN ELEMENTO
+        productCard.classList.add('product-card') //AGREGAR LA CLASE AL ELEMENTO
+
+        const productImg = document.createElement('img')
+        productImg.setAttribute('src', product.image) //MODIDICAR EL VALOR DE UN ATRIBUTO
+        //product = {name, price, image} -> product.image
+
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+
+        const productInfoDiv = document.createElement('div')
+
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price //MODIFICAR EL TEXTO
+
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+
+        productInfoDiv.appendChild(productPrice) //AGREGAR UN ELEMENTO DENTRO DE OTRO
+        productInfoDiv.appendChild(productName)
+
+        const productInfoFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+
+        productInfoFigure.appendChild(productImgCart)
+
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList)
