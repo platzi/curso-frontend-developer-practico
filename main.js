603081c103b9c2339548e5e019eec09643ea3dfb
Raul S <raul.sr92@gmail.com>
2023-01-19 12:57:30 -0500

    Fusión componentes 8 y 11

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* Interacción de Menú desktop (componente 7)*/
+
 const menuEmail = document.querySelector(".navbar-email");
 console.log(menuEmail);
 
@@ -29,5 +31,39 @@ function toggleDesktopMenu(){
 
 function toggleDesktopMenu(){
 	menuDesktop.classList.toggle("inactive");
+	console.log("click")
+}
+
+
+/* Interacción de Menú Mobile (componente 8)*/
+
+const burguerMenu= document.querySelector(".menu");
+console.log(burguerMenu);
+const menuMobile= document.querySelector(".mobile-menu");
+console.log(menuMobile);
+
+burguerMenu.addEventListener("click",toggleMobileMenu);
+
+ //FORMA 1 - RAUL
+/*
+function toggleMobileMenu(){
+	if (menuMobile.classList.length==2){
+		menuMobile.classList.remove("inactive");
+
+		console.log(menuMobile.classList);
+		console.log(menuMobile.classList.length);
+
+	} else if (menuMobile.classList.length==1){
+		menuMobile.classList.add("inactive")
+
+		console.log(menuMobile.classList);
+		console.log(menuMobile.classList.length);
+}
+}
+*/
+ //FORMA 2 - PROFESOR
+
+ function toggleMobileMenu(){
+	menuMobile.classList.toggle("inactive");
 	console.log("click")
 }
