a264ff5ad5a76b196c17724c918d9ad584017fd7
codefarz <codefarz@gmail.com>
2023-01-20 08:20:26 -0500

    menu mobile add to index.html. Event click on iconMenu toggle 'inactive' on js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const navMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
+const iconMenu = document.querySelector('.menu')
+const menuMobile = document.querySelector('.mobile-menu')
 
 navMenu.addEventListener('click', toggleMenuDesktop);
 
 function toggleMenuDesktop() {
     desktopMenu.classList.toggle('inactive') 
+} 
+
+iconMenu.addEventListener('click', toggleIconMenu);
+
+function toggleIconMenu() {
+    menuMobile.classList.toggle('inactive') 
 } 
