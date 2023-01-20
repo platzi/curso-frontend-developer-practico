671dcf94ad25b4bcc7428faac279a22ce2397fba
codefarz <codefarz@gmail.com>
2023-01-20 10:08:51 -0500

    add my order menu

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ const navMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
 const iconMenu = document.querySelector('.icon-menu-mobile')
 const menuMobile = document.querySelector('.mobile-menu')
+const iconShoppingCart = document.querySelector('.navbar-shopping-cart')
+const myOrder = document.querySelector('.product-detail')
 
 navMenu.addEventListener('click', toggleMenuDesktop);
 
 function toggleMenuDesktop() {
-    desktopMenu.classList.toggle('inactive') 
+    myOrder.classList.add('inactive');
+    menuMobile.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive');
 } 
 
 iconMenu.addEventListener('click', toggleIconMenu);
 
 function toggleIconMenu() {
-    menuMobile.classList.toggle('inactive') 
+    myOrder.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    menuMobile.classList.toggle('inactive'); 
+} 
+
+iconShoppingCart.addEventListener('click', toggleMyOrder);
+
+function toggleMyOrder() {
+    desktopMenu.classList.add('inactive');
+    menuMobile.classList.add('inactive');
+    myOrder.classList.toggle('inactive');
 } 
