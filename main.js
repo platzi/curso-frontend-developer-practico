cbf9628e94bb62d65f1a67b5dccf74e3b5526e37
CristobalDF <cdelfierro@dt.gob.cl>
2023-01-20 19:08:51 -0300

    a medias c20

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const menuEmail = document.querySelector(".navbar-email");
 const menuHamIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
   //   desktopMenu.className === "desktop-menu inactive"
@@ -14,5 +17,21 @@ function toggleDesktopMenu() {
 }
 
 function toggleMobileMenu() {
+  isAsideOpen = !aside.classList.contains("inactive");
+
+  if (isAsideOpen) {
+    aside.classList.toggle("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.toggle("inactive");
+  }
+
+  aside.classList.toggle("inactive");
 }
