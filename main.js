e29dc6cd8ea7ad71e82f3a6101bbfb9c346b4dc5
Cesar Villamizar <107062808+CKEISERV@users.noreply.github.com>
2023-01-20 18:09:36 -0500

    Clase 19 JS

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
 
+const mobileMenu = document.querySelector(".mobile-menu")
+const menuIcon = document.querySelector(".menu")
+
 menuEmail.addEventListener("click", toggleMenu); 
+menuIcon.addEventListener("click", toggleMenuMobile)
 
 function toggleMenu(){
 
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMenuMobile(){
+
+    mobileMenu.classList.toggle("inactive")
 }
