e6b90d301733e34c76a3a54b3c0438a5cd063fe0
Halil Velasco <halilvelasco@gmail.com>
2023-01-20 15:20:07 -0600

    integración del menu mibile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const botonMemuMB = document.querySelector('.menu');
+const menumobile = document.querySelector('.mobile-menu');
 //fin de variables
 
 function menuDesplegable() {
     menuDesktop.classList.toggle('inactive');
 }
 menuEmail.addEventListener('click', menuDesplegable);
+
+function toggleMenuMobile() {
+    menumobile.classList.toggle('inactive');
+}
+botonMemuMB.addEventListener('click', toggleMenuMobile);
