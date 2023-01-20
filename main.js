f5693f5c786bf134cfaaa354d2dbfbcc11172b9f
Halil Velasco <halilvelasco@gmail.com>
2023-01-20 16:44:34 -0600

    integración de carrito de compras

--- main.js
+++ main.js
@@ -2,14 +2,34 @@ const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
 const botonMemuMB = document.querySelector('.menu');
 const menumobile = document.querySelector('.mobile-menu');
+const iconCarrito = document.querySelector('.navbar-shopping-cart');
+const BarraCarrito = document.querySelector('.product-detail');
 //fin de variables
-
+menuEmail.addEventListener('click', menuDesplegable);
+botonMemuMB.addEventListener('click', toggleMenuMobile);
+iconCarrito.addEventListener('click', Carritotoggle);
 function menuDesplegable() {
+    let closeBarraCarrito = BarraCarrito.classList.contains('inactive');
+    if (!closeBarraCarrito) {
+        BarraCarrito.classList.add('inactive')
+    }
     menuDesktop.classList.toggle('inactive');
 }
-menuEmail.addEventListener('click', menuDesplegable);
-
 function toggleMenuMobile() {
+    let closeBarraCarrito = BarraCarrito.classList.contains('inactive');
+    if (!closeBarraCarrito) {
+        BarraCarrito.classList.add('inactive')
+    }
     menumobile.classList.toggle('inactive');
 }
-botonMemuMB.addEventListener('click', toggleMenuMobile);
+function Carritotoggle(){
+    let closemenumobile = menumobile.classList.contains('inactive');
+    let closemenuDesktop  = menuDesktop.classList.contains('inactive');
+    if (!closemenumobile) {
+        menumobile.classList.add('inactive')
+    }
+    if (!closemenuDesktop) {
+        menuDesktop.classList.add('inactive')
+    }
+    BarraCarrito.classList.toggle('inactive');
+}
