187773bcab4bb939321c4e38648a9d513e20764c
CristobalDF <cdelfierro@dt.gob.cl>
2023-01-20 18:43:06 -0300

    clase menu mobile

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const menuEmail = document.querySelector(".navbar-email");
+const menuHamIcon = document.querySelector(".menu");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   //   desktopMenu.className === "desktop-menu inactive"
   //     ? (desktopMenu.className = "desktop-menu")
   //     : (desktopMenu.className = "desktop-menu inactive");
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
