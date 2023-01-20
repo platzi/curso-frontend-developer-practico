61be3da6f4cb2a858e1ef79033d04146e7c5efa9
caballo31 <versallialejandro@gmail.com>
2023-01-20 19:22:52 -0300

    Add mobile menu to index, work fine

--- mains.js
+++ mains.js
@@ -1,8 +1,17 @@
+
+// Elements
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
 
 emailMenu.addEventListener('click', toggleMenuDesktop);
+burgerMenu.addEventListener('click', toggleBurgerMenu);
 
 function toggleMenuDesktop() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleBurgerMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
