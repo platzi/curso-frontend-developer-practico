277898475d9761be7a3ba411f2ced38517e6e6e4
Raul S <raul.sr92@gmail.com>
2023-01-21 01:14:01 -0500

    Fusion componentes 13 y 11

--- main.js
+++ main.js
@@ -29,11 +29,12 @@ function toggleDesktopMenu(){
 
  //FORMA 2 - PROFESOR
 
+/*
 function toggleDesktopMenu(){
 	menuDesktop.classList.toggle("inactive");
 	console.log("click")
 }
-
+*/
 
 /* Interacción de Menú Mobile (componente 8)*/
 
@@ -63,7 +64,118 @@ function toggleMobileMenu(){
 */
  //FORMA 2 - PROFESOR
 
+ /*
  function toggleMobileMenu(){
 	menuMobile.classList.toggle("inactive");
 	console.log("click")
-}
+}*/
+
+
+
+/* Interacción de Carrito de compra (componente 13)*/
+
+const shoppingCartIcon= document.querySelector(".navbar-shopping-cart");
+console.log(shoppingCartIcon);
+
+const shoppingCartDetail=document.querySelector(".product-detail");
+console.log(shoppingCartDetail);
+
+shoppingCartIcon.addEventListener("click", toggleCartDetail);
+
+ //FORMA 1 - RAUL
+/*
+ function toggleCartDetail(){
+	if(shoppingCartDetail.classList.length==2){
+		shoppingCartDetail.classList.remove("inactive");
+		console.log(shoppingCartDetail.classList)
+		console.log(shoppingCartDetail.classList.length)
+	}
+		else if (shoppingCartDetail.classList.length==1){
+			shoppingCartDetail.classList.add("inactive");
+			console.log(shoppingCartDetail.classList)
+			console.log(shoppingCartDetail.classList.length)
+		}
+	};*/
+
+	 //FORMA 2 - PROFESOR
+/*
+	 function toggleCartDetail(){
+		shoppingCartDetail.classList.toggle("inactive");
+		console.log("click")
+	}
+	*/
+
+
+	//SOLUCION DE ERRORES - RAUL
+
+	/*
+	function toggleCartDetail(){
+		if (menuMobile.classList.length==1){
+			menuMobile.classList.add("inactive")
+		} else if (menuDesktop.classList.length==1){
+			menuDesktop.classList.add("inactive")
+		}
+		shoppingCartDetail.classList.toggle("inactive");
+	};
+
+	function toggleMobileMenu(){
+		if(shoppingCartDetail.classList.length==1){
+			shoppingCartDetail.classList.add("inactive");
+		}
+		menuMobile.classList.toggle("inactive");
+	};
+
+	function toggleDesktopMenu(){
+		if(shoppingCartDetail.classList.length==1){
+			shoppingCartDetail.classList.add("inactive");
+		}
+		menuDesktop.classList.toggle("inactive");
+	}
+*/
+
+	//SOLUCION DE ERRORES - JUAN DC
+
+	function toggleCartDetail(){
+		const ismenuMobileClosed= menuMobile.classList.contains("inactive");
+		console.log(ismenuMobileClosed)
+
+		const ismenuDesktopClosed= menuDesktop.classList.contains("inactive");
+		console.log(ismenuDesktopClosed)
+
+		
+		if(!ismenuMobileClosed){
+			menuMobile.classList.add("inactive")
+		} else if (!ismenuDesktopClosed){
+			menuDesktop.classList.add("inactive")
+		}
+		shoppingCartDetail.classList.toggle("inactive");
+	};
+
+
+
+	function toggleMobileMenu(){
+		const isCartDetailClosed= shoppingCartDetail.classList.contains("inactive")
+		console.log(isCartDetailClosed)
+
+		if(!isCartDetailClosed){
+			shoppingCartDetail.classList.add("inactive")
+		}
+
+		menuMobile.classList.toggle("inactive");
+	};
+
+
+
+	function toggleDesktopMenu(){
+		const isCartDetailClosed= shoppingCartDetail.classList.contains("inactive")
+		console.log(isCartDetailClosed)
+
+		if(!isCartDetailClosed){
+			shoppingCartDetail.classList.add("inactive")
+		}
+
+		menuDesktop.classList.toggle("inactive");
+	}
+
+	
+	
