e5503ff3a29948ed8b6c3aa6cc7541a7782d950a
Cesar Villamizar <107062808+CKEISERV@users.noreply.github.com>
2023-01-21 15:58:38 -0500

    Clase 20 JS

--- main.js
+++ main.js
@@ -2,17 +2,46 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
 
 const mobileMenu = document.querySelector(".mobile-menu")
-const menuIcon = document.querySelector(".menu")
+const menuHamIcon = document.querySelector(".menu")
+
+const menuCarIcon = document.querySelector(".navbar-shopping-cart")
+const shoppingCar = document.querySelector(".product-detail")
 
 menuEmail.addEventListener("click", toggleMenu); 
-menuIcon.addEventListener("click", toggleMenuMobile)
+menuHamIcon.addEventListener("click", toggleMenuMobile)
+menuCarIcon.addEventListener("click", toggleShoppingCar)
 
 function toggleMenu(){
 
-    desktopMenu.classList.toggle("inactive")
+    const isShoppingCarClose = shoppingCar.classList.contains("inactive"); 
+
+    if(!isShoppingCarClose){
+        desktopMenu.classList.remove("inactive")
+        shoppingCar.classList.add("inactive")
+    } else{
+        desktopMenu.classList.toggle("inactive")
+    }
 }
 
 function toggleMenuMobile(){
 
-    mobileMenu.classList.toggle("inactive")
+    const isShoppingCarClose = shoppingCar.classList.contains("inactive"); 
+
+    if(!isShoppingCarClose){
+        mobileMenu.classList.remove("inactive")
+        shoppingCar.classList.add("inactive")
+    } else{
+        mobileMenu.classList.toggle("inactive")
+    }
+}
+
+function toggleShoppingCar(){
+    const isMobileMenuClose = mobileMenu.classList.contains("inactive"); 
+
+    if(!isMobileMenuClose){
+        mobileMenu.classList.add("inactive")
+        shoppingCar.classList.remove("inactive")
+    } else{
+        shoppingCar.classList.toggle("inactive")
+    }
 }
