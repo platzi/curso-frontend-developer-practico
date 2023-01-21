bfea8b3336422602b0a319124e935652c9129876
Tatiadev <tamapehe@gmail.com>
2023-01-20 22:22:53 -0500

    Estructuramos el carrito de compras y fusión de menú

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email');
 const destktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+      }
     destktopMenu.classList.toggle('inactive');
 }
 
-console.log('JS funcionando');
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+      }
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+      }
+    
+      aside.classList.toggle('inactive');
+
+}   
