8b22f3dea08948e6bf535dfb2564580336c4b8bf
Cesar Villamizar <107062808+CKEISERV@users.noreply.github.com>
2023-01-21 16:48:56 -0500

    clase 21 JS

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const menuHamIcon = document.querySelector(".menu")
 const menuCarIcon = document.querySelector(".navbar-shopping-cart")
 const shoppingCar = document.querySelector(".product-detail")
 
+const cardsContainer = document.querySelector(".cards-container")
+
 menuEmail.addEventListener("click", toggleMenu); 
 menuHamIcon.addEventListener("click", toggleMenuMobile)
 menuCarIcon.addEventListener("click", toggleShoppingCar)
@@ -45,3 +47,64 @@ function toggleShoppingCar(){
         shoppingCar.classList.toggle("inactive")
     }
 }
+
+const productList = []
+
+productList.push({
+    name: "Bike",
+    price: 120, 
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+})
+
+productList.push({
+    name: "Pantalla",
+    price: 220, 
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+})
+
+productList.push({
+    name: "Computador",
+    price: 620, 
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+})
+
+renderProducts(productList)
+
+function renderProducts(arr){
+    for(product of productList){
+
+        const productCard = document.createElement("div")
+        productCard.classList.add("product-card")
+    
+        const productImg = document.createElement("img")
+        productImg.setAttribute("src", product.image)
+    
+        const productInfo = document.createElement("div")
+        productInfo.classList.add("product-info")
+    
+        const productInfoDiv = document.createElement("div")
+        
+        const productPrice = document.createElement("p")
+        productPrice.innerHTML = product.price
+    
+        const productName = document.createElement("p")
+        productName.innerHTML = product.name
+    
+        const productInfoFigure = document.createElement("figure")
+    
+        const productImgCar = document.createElement("img")
+        productImgCar.setAttribute("src", "./icons/bt_add_to_cart.svg")
+    
+        productInfoFigure.appendChild(productImgCar); 
+        productInfoDiv.append(productPrice,productName)
+        productInfo.append(productInfoDiv, productInfoFigure)
+        productCard.append(productImg, productInfo)
+    
+        cardsContainer.appendChild(productCard)
+    
+    }
+}
+
