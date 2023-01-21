3175e2b73a5014b6cd36e52b5827560b367a28e1
caballo31 <versallialejandro@gmail.com>
2023-01-21 10:43:39 -0300

    Add product-detail aside to index, css works fine

--- mains.js
+++ mains.js
@@ -5,27 +5,27 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cartMenu = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shopping-cart');
 const cardsContainer = document.querySelector('.cards-container')
 
 emailMenu.addEventListener('click', toggleMenuDesktop);
 burgerMenu.addEventListener('click', toggleBurgerMenu);
-cartMenu.addEventListener('click', toggleAside);
+cartMenu.addEventListener('click', toggleshoppingCartContainer);
 
 // Interaccion UI 
 
 function toggleMenuDesktop() {
     desktopMenu.classList.toggle('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 
 function toggleBurgerMenu(){
     mobileMenu.classList.toggle('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 
-function toggleAside() {
-    aside.classList.toggle('inactive');
+function toggleshoppingCartContainer() {
+    shoppingCartContainer.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
 
