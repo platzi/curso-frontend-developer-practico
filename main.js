e331dc64f612b757df2cb4be652805b22506bc5a
BradlyGutierrez <bradly.gutierrez14520@est.uca.edu.ni>
2023-01-22 00:34:58 -0600

    some changes

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const navEmail = document.querySelector(".navbar-email");
 
 
 navEmail.addEventListener('click', () => {
-    desktopMenu.classList.toggle("inactive");
+    if(!productDetail.classList.contains('inactive')){
+        productDetail.classList.toggle('inactive')
+    }
+    desktopMenu.classList.toggle('inactive')
+    
 });
 
 //mobile menu
@@ -13,5 +17,27 @@ const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector('.mobile-menu');
 
 menuHamIcon.addEventListener('click',() => {
+    if(!productDetail.classList.contains('inactive')){
+        productDetail.classList.toggle('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
+    
 });
+
+//Shopping car
+const productDetail = document.querySelector('.product-detail');
+const ShoppingCarIcon =  document.querySelector('.navbar-shopping-cart')
+
+ShoppingCarIcon.addEventListener('click', ShoppingCarIconn);
+
+function ShoppingCarIconn(){
+
+    if(menuHamIcon.classList.contains('inactive') === false){
+        menuHamIcon.classList.add('inactive')
+    }
+    if(desktopMenu.classList.contains('inactive') == false){
+        desktopMenu.classList.add('inactive');
+    }
+   
+    productDetail.classList.toggle('inactive')
+}
