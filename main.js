d4c3e299e487fb665ddee001d080e7382c2ad153
BradlyGutierrez <bradly.gutierrez14520@est.uca.edu.ni>
2023-01-21 23:55:01 -0600

    merge menu mobile with index

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Desktop Menu
 const desktopMenu = document.querySelector(".desktop-menu");
 const navEmail = document.querySelector(".navbar-email");
 
@@ -5,4 +6,12 @@ const navEmail = document.querySelector(".navbar-email");
 
 navEmail.addEventListener('click', () => {
     desktopMenu.classList.toggle("inactive");
+});
+
+//mobile menu
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuHamIcon.addEventListener('click',() => {
+    mobileMenu.classList.toggle('inactive')
 });
