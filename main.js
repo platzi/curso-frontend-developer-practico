202049eaef377251f1ec90dcf7c370699886d5bb
Lucas Frezzini <luquiitas.f@gmail.com>
2023-01-22 21:17:01 -0300

    Agregamos al flujo el Mobile Menu. Fix bugs in desktop menu.

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ const desktop_menu = document.querySelector('.desktop-menu');
 nav_email.addEventListener('click', () => {
     desktop_menu.classList.toggle('inactive');
 });
+
+// Mobile Menu
+
+const menu_icon = document.querySelector('.menu');
+const menu_mobile = document.querySelector('.mobile-menu');
+
+menu_icon.addEventListener('click', () => {
+    menu_mobile.classList.toggle('inactive');
+})
