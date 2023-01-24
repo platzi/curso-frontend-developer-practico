bb06cd95046a1e06321996cfc5017c5b42d567cd
Ismael Cohen <cohen1987@gmail.com>
2023-01-24 18:59:12 -0500

    Creamos lista de detalles con elementos emparejados a los elementos de la lista de productos a traves del data-index.

--- main.js
+++ main.js
@@ -28,13 +28,18 @@ const iconoCarrito = document.querySelector('.navbar-shopping-cart');
 
 //Menu asociado al Carrito
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
-//shoppingCartContainer
+
+//Contenedor de detalles
+const productDetailContainer = document.querySelector('#productDetail');
+//Icono de cerrar contenedor de detalles
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 
 
 
 
 
-//ADICION DE EVENTOS
+
+//ASIGNACIÓN DE EVENTOS
 
 //click al menu desktop
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -47,7 +52,17 @@ burguerMenu.addEventListener('click', toggleMobileMenu);
 iconoCarrito.addEventListener('click', toggleCarritoAside);
 
 
-//DEFINICION DE FUNCIONES
+//Cerrar contenedor de detalles
+productDetailCloseIcon.addEventListener('click', closeProductDetail);
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////
+
+//DEFINICION DE FUNCIONES/////////////////////////////////////
 function toggleDesktopMenu(){
 
     //Solapamiento con Carrito
@@ -55,6 +70,11 @@ function toggleDesktopMenu(){
     if(!isAsideClosed){
         shoppingCartContainer.classList.add('inactive');
     }
+
+
+    //Solapamiento con details
+    productDetailContainer.classList.add('inactive');
+
     
     deskTopMenu.classList.toggle('inactive');
 }
@@ -73,6 +93,10 @@ function toggleMobileMenu() {
         shoppingCartContainer.classList.add('inactive');
     }
  
+    //Cerramos detail
+    closeProductDetail();
+
+    
     mobileMenu.classList.toggle('inactive');
 }
 
@@ -91,8 +115,44 @@ function toggleCarritoAside() {
         deskTopMenu.classList.add('inactive');
     }
 
+    //Solapamiento con details, al dar click en carrito
+    //se cierra el detail
+    const isProductDetailClosed = productDetailContainer.
+    classList.contains('inactive');
+
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
+    }
+
     shoppingCartContainer.classList.toggle('inactive');
 }
+
+
+function openProductDetail() {
+
+    //Siempre que abramos details sampamos inactive al carrito
+    shoppingCartContainer.classList.add('inactive');
+
+
+    //solapamiento con desktop-menu
+    deskTopMenu.classList.add('inactive');
+
+
+    //para que aparezca quitamos clase inactive
+    productDetailContainer.classList.remove('inactive');
+}
+
+
+function closeProductDetail() {
+
+    //para que desaparezca agregamos clase inactive
+    productDetailContainer.classList.add('inactive');
+}
+
+
+
+////////////////////////////////////////////////////////////
+
 
 
 
@@ -104,18 +164,25 @@ const productList = [];
 productList.push({
     name: 'Bike',
     price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: '1 With its practical position, this bike also fulfills a decorative function, add your hall or workspace'
 });
 productList.push({
     name: 'Pantalla',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    price: 600,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: '2 With its practical position, this bike also fulfills a decorative function, add your hall or workspace'
 });
 productList.push({
     name: 'Producto 3',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    price: 1000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: '3 With its practical position, this bike also fulfills a decorative function, add your hall or workspace'
 });
+
+
+
+
 
 
 //Fabricacion de tarjetas
@@ -123,15 +190,40 @@ productList.push({
 //Aplicamos el for a cada elemento del productList
 function renderProducts(array){
 
+    indexControl = -1;
     for (product of array) {
-    
+        
+        //Para emparejar con detailCard
+        indexControl += 1;
+        
+        
+        //ELEMENTOS LISTA DE PRODUCTOS
+
         //creamos div contenedor de clase product-card
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
-    
+        
+
+        //Controlamos el .dataset.index
+        productCard.setAttribute("data-index", indexControl);
+
+        
+
+
+
+
         //creamos la imagen asociada al producto
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        
+        //Asignamos clase para el pointer
+        productImg.classList.add('productImg');
+        
+        //Asignamos evento click a la imagen
+        productImg.addEventListener('click', openProductDetail);
+
+
+
     
         //En la estruc base, creamos el product-info
         const productInfo = document.createElement('div');
@@ -170,7 +262,7 @@ function renderProducts(array){
         productCard.appendChild(productImg);
         productCard.appendChild(productInfo);
     
-    
+
         //Aca terminamos fabricacion de tarjeta ahora la
         //Ingresamos al contenedor de tarjetas en el html
         cardsContainer.appendChild(productCard);
@@ -179,6 +271,96 @@ function renderProducts(array){
 }
 
 renderProducts(productList);
+
+
+const detailContainer = document.querySelector('.detail-container');
+
+function createDetailCards(array){
+    
+    indexControl = -1;
+    for (product of array) {        
+        
+        indexControl += 1;
+
+        //ASIDE
+        const detailCard = document.createElement('aside');
+        detailCard.classList.add('inactive');
+        detailCard.id = "productDetail";
+        //Completo
+
+
+
+        //CONTROL EMPAREJAMIENTO
+        detailCard.setAttribute("data-index", indexControl);
+
+
+        //PARTE 1
+        const detailCloseContainer = document.createElement('div');
+        detailCloseContainer.classList.add('product-detail-close');
+
+        const detailCloseIcon = document.createElement('img');
+        detailCloseIcon.setAttribute('src', "./icons/icon_close.png");
+        detailCloseIcon.setAttribute('alt', "close");
+
+        //Completo
+        detailCloseContainer.appendChild(detailCloseIcon);
+
+
+
+
+        //PARTE 2
+        const detailProductImg = document.createElement('img');
+        detailProductImg.setAttribute('src', product.image);
+        
+
+
+
+        //PARTE 3
+        const detailInfo = document.createElement('div');
+        detailInfo.classList.add('product-info');
+
+        //Precio
+        const detailPrice = document.createElement('p');
+        detailPrice.innerText = product.price;
+        detailInfo.appendChild(detailPrice);
+
+        //Name
+        const detailName = document.createElement('p');
+        detailName.innerText = product.name;
+        detailInfo.appendChild(detailName);
+        
+        //Description
+        const detailDescription = document.createElement('p');
+        detailDescription.innerText = product.description;
+        detailInfo.appendChild(detailDescription);
+        
+        //Button;////////////////////////////////
+        const detailButton = document.createElement('button');
+        detailButton.classList.add('primary-button');
+        detailButton.classList.add('add-to-cart-button');
+        
+
+        detailButton.innerHTML = '<img src="./icons/bt_add_to_cart.svg" alt="add to cart"> Add to cart';
+
+        detailInfo.appendChild(detailButton);
+        /////////////////////////////////////////////////////
+
+
+        //INTODUCIMOS PARTE 1, 2 y 3
+        detailCard.appendChild(detailCloseContainer);
+        detailCard.appendChild(detailProductImg);
+        detailCard.appendChild(detailInfo);
+
+        //Metemos tarjeta en el html
+        detailContainer.appendChild(detailCard);
+    }
+}
+
+createDetailCards(productList);
+
+
+
+function eventClickProd_Detail(){}
 
 
 
