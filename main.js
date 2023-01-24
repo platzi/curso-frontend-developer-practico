e231a1a00bc024c438162685d448fd07f538f0cc
Oscar Poveda <oscarpoveda679@gmail.com>
2023-01-24 09:27:17 -0500

    Se agrego el carrito de compras y su funcionalidad con los demas elementos

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 let menuEmail = document.querySelector('.navbar-email');
 let desktop = document.querySelector('.desktop-menu');
-
+let menuMobile = document.querySelector('.mobile-menu');
+let logo = document.querySelector('.menu');
+let carritoCompras = document.querySelector('.shopping-card');
+let aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener("click",toggleMenu);
-
+logo.addEventListener("click",toggleMenuMobile);
+carritoCompras.addEventListener('click',toggleProductDetail);
 function toggleMenu(){
-    desktop.classList.toggle('inactive');
+    let isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+      }
+    
+        desktop.classList.toggle('inactive'); 
 }
+function toggleMenuMobile(){
+    let isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+      }
+    
+        menuMobile.classList.toggle('inactive'); 
+}
+function toggleProductDetail(){
+    let isMobileMenuClosed = carritoCompras.classList.contains('inactive');
+
+      if(!isMobileMenuClosed){
+        menuMobile.classList.add('inactive');
+      }
+    
+        aside.classList.toggle('inactive'); 
+    }
+    
