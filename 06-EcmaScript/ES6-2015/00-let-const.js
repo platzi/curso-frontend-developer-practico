9103c665ffdace031e432db51519cb8d4ef5bafd
crisLeo-P01 <cristianleopaiva@gmail.com>
2023-01-24 01:43:22 -0300

    Aprendiendo sus versiones de ECMAScript

---
+++ 06-EcmaScript/ES6-2015/00-let-const.js
@@ -0,0 +1,28 @@
+var lastName = 'David';
+lastName = 'Oscar';
+console.log(lastName);
+
+///////////////////////
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+///////////////////////
+
+const animal = 'Dog';
+animal = 'Cat';
+console.log(animal);
+
+///////////////////////
+
+const fruits = () => {
+  if(true) {
+    var fruit1 = 'Apple';
+    let fruit2 = 'Banana';
+    const fruit3 = 'Kiwi';
+  }
+  console.log(fruit1);
+  console.log(fruit2);
+  console.log(fruit3);
+}
