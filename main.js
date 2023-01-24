e2c27259ff27faccba14f108b4f6878794c6bb9c
Dario <dariolmedo2@gmail.com>
2023-01-24 12:00:21 -0300

    Lista de productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
@@ -43,3 +44,58 @@ function toggleCarritoAside() {
 
 	aside.classList.toggle('inactive');
 }
+
+
+const productList = [];
+
+productList.push({
+	name:'bike',
+	price: 120,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+	name:'pantalla',
+	price: 220,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+	name:'computadora',
+	price: 520,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+const renderProduct = (arr) => {
+	for (product of arr){
+		const productCard = document.createElement('div');
+		productCard.classList.add('product-card')
+	
+		const img = document.createElement('img');
+		img.setAttribute('src', product.image);
+	
+		const productInfo = document.createElement('div');
+		productInfo.classList.add('product-info')
+	
+		const productInfoDiv = document.createElement('div');
+	
+		const productInfoPrice = document.createElement('p');
+		productInfoPrice.innerText = '$' + product.price;
+		const productInfoName = document.createElement('p');
+		productInfoName.innerText = product.name;
+	
+		productInfoDiv.append(productInfoPrice, productInfoName);
+	
+		const productInfoFigure = document.createElement('figure');
+	
+		const productImgCard = document.createElement('img');
+		productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+	
+		productInfoFigure.append(productImgCard);
+		productInfo.append(productInfoDiv, productInfoFigure);
+		productCard.append(img, productInfo);
+		cardContainer.append(productCard);
+	}
+}
+
+renderProduct(productList);
