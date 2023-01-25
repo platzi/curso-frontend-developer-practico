3121db9a0bae889040ca0a7bf033b143eddccf9f
camilabv14 <116381831+camilabv14@users.noreply.github.com>
2023-01-25 13:55:05 -0500

    menu product detail added

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector('.mobile-menu')
+const productDetail = document.querySelector(".product-detail")
+const navbarCart = document.querySelector(".navbar-shopping-cart")
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+navbarCart.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
