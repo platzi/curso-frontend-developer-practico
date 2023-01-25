17d1821299b58281b6153437ca66e3f5484ee0ca
juandavid0859 <juandavid0859@gmail.com>
2023-01-25 11:49:17 -0500

    unir product-detail

--- main.js
+++ main.js
@@ -3,41 +3,41 @@
  const mobileMenu = document.querySelector('.mobile-menu');
  const menuHamIcon = document.querySelector('.menu');
  const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
- const aside = document.querySelector('.product-detail');
+ const shoppingCartContainer = document.querySelector('.product-detail');
  const cardsContainer = document.querySelector('.cards-container');
 
 
  menuEmail.addEventListener('click', toggleDesktopMenu);
  menuHamIcon.addEventListener('click', toggleMobileMenu);
- menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+ menuCarritoIcon.addEventListener('click', toggleCarritoshoppingCartContainer);
 
  function toggleDesktopMenu () {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if (!isAsideClosed){
-        aside.classList.add('inactive');
+    if (!isshoppingCartContainerClosed){
+        shoppingCartContainer.classList.add('inactive');
     }
 
      desktopMenu.classList.toggle('inactive');
  }
 
  function toggleMobileMenu () {
-     const isAsideClosed = aside.classList.contains('inactive');
+     const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
 
-     if (!isAsideClosed){
-         aside.classList.add('inactive');
+     if (!isshoppingCartContainerClosed){
+         shoppingCartContainer.classList.add('inactive');
      }
 
      mobileMenu.classList.toggle('inactive');
  }
 
- function toggleCarritoAside () {
+ function toggleCarritoshoppingCartContainer () {
      const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     
      if (!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
      }
-     aside.classList.toggle('inactive');
+     shoppingCartContainer.classList.toggle('inactive');
  }
 
  const productList = [];
