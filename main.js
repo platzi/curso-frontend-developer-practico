ee31a4a32e43d17b63e7c5d52e79d3e0db45a73c
agusdigiam <agustindigiambatista@gmail.com>
2023-01-25 11:51:43 -0300

    terminado el 20

---
+++ main.js
@@ -0,0 +1,16 @@
+const menuEmail = document.querySelector(".navbar-email")
+const desktopMenu = document.querySelector(".desktop-menu")
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle("inactive");
+
+
+}
+
+
+
+
+
+
+menuEmail.addEventListener("click" , toggleDesktopMenu);
+
