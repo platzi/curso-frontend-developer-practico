1a0b6c1d4f12ef42fe7f338fff118cd49da408fb
john.jejen <john.jejen@ptaginc.com>
2023-01-25 17:52:59 -0500

    fusion between index, css and javascript, menu works

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+  console.log('Click');
+  desktopMenu.classList.toggle('inactive');
+}
