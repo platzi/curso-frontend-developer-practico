d0a15caf8330bde6aca047f77d86f8716b6b1dc7
SebaMoya92 <sebamoya92@gmail.com>
2023-01-26 12:27:18 -0300

    fusion de menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var menuEmail = document.querySelector(".navbar-email");
 var desktopMenu = document.querySelector(".desktop-menu");
+var mainMenu = document.querySelector(".menu");
+var mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+mainMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
