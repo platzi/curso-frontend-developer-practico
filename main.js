39515de5d410ebdb45a3cf746ad1fb3aed8af015
fparramat <f.parra.matamala@gmail.com>
2023-01-25 21:48:07 -0300

    mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const desktopMenuToggle = document.querySelector("#desktop-menu-toggle");
+const mobileMenuToggle = document.querySelector("#mobile-menu-toggle");
 const desktopMenu = document.querySelector("#desktop-menu");
+const mobileMenu = document.querySelector("#mobile-menu");
 
-desktopMenuToggle.addEventListener("click", () => {
-    desktopMenu.classList.toggle("inactive");
-})
+function toggleElement(element, toggleButton){
+    toggleButton.addEventListener("click", () => {
+    element.classList.toggle("inactive");
+})}
+
+toggleElement(desktopMenu, desktopMenuToggle);
+toggleElement(mobileMenu, mobileMenuToggle);
