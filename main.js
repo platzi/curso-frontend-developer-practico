2cb6b654d7a2bc05f9361076847588b36f5bde35
Gfrey <gfreycode@gmail.com>
2023-01-26 10:02:55 -0500

    Shopping cart

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
+// Variables
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menu = document.querySelector('.menu');
+const buttonCartShopping = document.querySelector('.navbar-shopping-cart');
+const cartShopping = document.querySelector('.product-detail');
 
+// Funciones
+function closeCartShopping () {
+    if (cartShopping.classList.contains('product-detail--open')) { 
+        cartShopping.classList.toggle('product-detail--open');
+    }
+}
 function openAndCloseMenu () {
     desktopMenu.classList.toggle('desktop-menu--open');
+    closeCartShopping();
 }
-
 function openAndCloseMenuMobile () {
     mobileMenu.classList.toggle('mobile-menu--open');
+    closeCartShopping();
+}
+function openAndCloseCartShopping () {
+    cartShopping.classList.toggle('product-detail--open');
+
+    if (desktopMenu.classList.contains('desktop-menu--open')) { 
+        desktopMenu.classList.toggle('desktop-menu--open');
+    } else if (mobileMenu.classList.contains('mobile-menu--open')) {
+        mobileMenu.classList.toggle('mobile-menu--open');
+    }
 }
 
-navbarEmail.addEventListener('click', openAndCloseMenu);
 
+// Ejecuciones
+navbarEmail.addEventListener('click', openAndCloseMenu);
 menu.addEventListener('click', openAndCloseMenuMobile);
+buttonCartShopping.addEventListener('click', openAndCloseCartShopping);
