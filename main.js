0d3ea5818469d08f1586018dde46e645fd6df46c
AlbertoVillanueva10 <betoangel100@gmail.com>
2023-01-25 20:19:59 -0600

    Se actualizan par de variables en archivo main

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
-
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 
 //Se agrega la clase inactive o quita cada que el usuario da click al "correo"
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
+console.log("Esta haciendo algo");
