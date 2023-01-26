27ae3c4ce857bca565c8212c6c06f979a9d4b5e2
SebaMoya92 <sebamoya92@gmail.com>
2023-01-26 20:50:37 -0300

    carrito de compras agregado

--- main.js
+++ main.js
@@ -2,14 +2,39 @@ var menuEmail = document.querySelector(".navbar-email");
 var desktopMenu = document.querySelector(".desktop-menu");
 var mainMenu = document.querySelector(".menu");
 var mobileMenu = document.querySelector(".mobile-menu");
+var carritoMenu = document.querySelector(".navbar-shopping-cart");
+var aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 mainMenu.addEventListener("click", toggleMobileMenu);
+carritoMenu.addEventListener("click", toggleCarritoMenu);
 
 function toggleDesktopMenu() {
+  var isCarritoMenuClosed = aside.classList.contains("inactive");
+
+  if (!isCarritoMenuClosed) {
+    aside.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  var isCarritoMenuClosed = aside.classList.contains("inactive");
+
+  if (!isCarritoMenuClosed) {
+    aside.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoMenu() {
+  var isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
 }
