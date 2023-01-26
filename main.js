6e01e94007f7f098f35873b59d20f2e631734d76
jonathanmaster <davidrodriguezcarvajal686@gmail.com>
2023-01-26 12:15:48 -0500

    version 1 navbar

---
+++ main.js
@@ -0,0 +1,18 @@
+const menuEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+
+
+
+menuEmail.addEventListener('click', ()=>{
+    desktopMenu.classList.toggle('inactive')
+
+})
+
+
+
+
+
+
+
+
+
