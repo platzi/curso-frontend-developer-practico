d145a4b9d2f6645f6859b96e78585f53f37ea2c0
CarlosErnestoEM <carlos@carlosernestoem.live>
2023-01-26 21:58:11 -0600

    Added shop item

--- script.js
+++ script.js
@@ -6,17 +6,24 @@ const menuIcon = document.querySelector(".menu")
 
 const email = document.querySelector(".navbar-email")
 
-
+const shopButton = document.querySelector(".navbar-shopping-cart")
+const shopMenu = document.querySelector(".product-detail")
 
 email.addEventListener("click", toggleDesktopMenu)
 menuIcon.addEventListener("click",toggleMobileMenu)
-
+shopButton.addEventListener("click", toggleShopMenu)
 
 function toggleDesktopMenu(event){
     desktopMenu.classList.toggle("inactive")
 }
 
 function toggleMobileMenu(event) {
-    console.log("click")
+    shopMenu.classList.add("inactive")
     mobileMenu.classList.toggle("inactive")
+}
+
+function toggleShopMenu(event){
+    mobileMenu.classList.add("inactive")
+    desktopMenu.classList.add("inactive")
+    shopMenu.classList.toggle("inactive")
 }
