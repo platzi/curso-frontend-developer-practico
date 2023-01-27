f2ee0cb9834c7e9a36bdda0b06806f63c0d62b60
agusdigiam <agustindigiambatista@gmail.com>
2023-01-27 09:37:13 -0300

    clase 22 actualizacion

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
-const menuEmail = document.querySelector(".navbar-email")
-const desktopMenu = document.querySelector(".desktop-menu")
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
 
 
 }
+burguerMenu.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+    const isasideClosed = aside.classList.contains("inactive");
+    if(!isasideClosed) {
+        aside.classList.add("inactive");
+        }
+    
+    mobileMenu.classList.toggle("inactive");
+}
+
+/*terminaste el video 20 comenzar con el 21*/ */
 
 
 
 
 
+
+function togglemenuCarritoIcon() {
+    const ismobileMenuClosed = mobileMenu.classList.contains("inactive");
+    
+     if(!ismobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+        }
+    aside.classList.toggle("inactive");
+
+}
+
+menuCarritoIcon.addEventListener("click" , togglemenuCarritoIcon)
 
 menuEmail.addEventListener("click" , toggleDesktopMenu);
-
