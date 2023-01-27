5c174fcf656648ba4ee20531c85464af5dc2e765
Andrea <lauralme96@gmail.com>
2023-01-26 23:54:20 -0500

    Carrito y menu mobile

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
  const navEmail = document.querySelector('.navbar-email')
- const nav = document.querySelector('.inactive')
+ const nav = document.querySelector('.desktop-menu')
+ const menu = document.querySelector('.menu')
+ const mobilemenu = document.querySelector('.mobile-menu')
+ const menuCarritoIcon = document.querySelector('.iconocarrito')
+ const productDetail = document.querySelector('.product-detail')
+ 
 
  navEmail.addEventListener('click', taggledesktopmenu);
 
  function taggledesktopmenu(){
     nav.classList.toggle('inactive')
+ }
+
+menu.addEventListener('click', togglemobiledesktop);
+function togglemobiledesktop(){
+    mobilemenu.classList.toggle('inactive')
+ }
+
+ menuCarritoIcon.addEventListener('click', tagglecarritomenu);
+
+ function tagglecarritomenu(){
+    productDetail.classList.toggle('inactive')
  }
