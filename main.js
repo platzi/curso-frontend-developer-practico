1e4b9ca3abdef42a03609ad03d5edbedf72d0acb
Gfrey <gfreycode@gmail.com>
2023-01-27 15:58:54 -0500

    interaction between components

--- main.js
+++ main.js
@@ -7,50 +7,76 @@ const buttonCartShopping = document.querySelector('.navbar-shopping-cart');
 const cartShopping = document.querySelector('.purchase-detail');
 const cardsContainer = document.querySelector('.cards-container');
 const productsList = [];
+const productDetail = document.querySelector('.product-detail');
+const productDetailClose = document.querySelector('.product-detail-close');
 
+
+// productCard.addEventListener('click', productDetail);
 
 // Funciones 🌎🟢
+function openProductDetail () {
+    productDetail.classList.add('product-detail--open');
+    closeCartShopping();
+    closeDesktopMenu();
+}
+function closeProductDetail () {
+    productDetail.classList.remove('product-detail--open');
+}
 function closeCartShopping () {
     let isCartShoppingOpen = cartShopping.classList.contains('purchase-detail--open');
     if (isCartShoppingOpen) { 
-        cartShopping.classList.toggle('product-detail--open');
+        cartShopping.classList.toggle('purchase-detail--open');
     }
 }
-function openAndCloseMenu () {
+function closeDesktopMenu () {
+    let isDesktopMenuOpen = desktopMenu.classList.contains('desktop-menu--open');
+    if (isDesktopMenuOpen) { 
+        desktopMenu.classList.toggle('desktop-menu--open');
+    }
+}
+function openAndCloseMenuDesktop () {
     desktopMenu.classList.toggle('desktop-menu--open');
     closeCartShopping();
+    closeProductDetail();
 }
 function openAndCloseMenuMobile () {
     mobileMenu.classList.toggle('mobile-menu--open');
     closeCartShopping();
+    closeProductDetail();
 }
 function openAndCloseCartShopping () {
     cartShopping.classList.toggle('purchase-detail--open');
 
     let isDesktopMenuOpen = desktopMenu.classList.contains('desktop-menu--open');
     let isMobileMenuOpen = mobileMenu.classList.contains('mobile-menu--open');
+    let isProductDetailOpen = productDetail.classList.contains('product-detail--open');
 
     if (isDesktopMenuOpen) { 
         desktopMenu.classList.toggle('desktop-menu--open');
     } else if (isMobileMenuOpen) {
         mobileMenu.classList.toggle('mobile-menu--open');
+    } else if (isProductDetailOpen) {
+        closeProductDetail();
     }
 }
 function setProduct (name, price, image) {
     productsList.push({
         name: name,
         price: price,
-        img: image
+        img: image,
+        id: productsList.length + 1
     })
 }
 function getAndPrintProduct (arr) {
     for (product of arr) {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
+        productCard.setAttribute('id', `${product.id}`)
 
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.img);
         productCard.appendChild(productImg);
+        productImg.addEventListener('click', openProductDetail);
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -77,16 +103,19 @@ function getAndPrintProduct (arr) {
 
 
 // Ejecuciones 🌎🟢
-navbarEmail.addEventListener('click', openAndCloseMenu);
+navbarEmail.addEventListener('click', openAndCloseMenuDesktop);
 menu.addEventListener('click', openAndCloseMenuMobile);
 buttonCartShopping.addEventListener('click', openAndCloseCartShopping);
+productDetailClose.addEventListener('click', closeProductDetail);
 {
-setProduct('Bike', 120.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-setProduct('Bike', 120.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-setProduct('Bike', 120.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-setProduct('Bike', 120.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-setProduct('Bike', 120.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-setProduct('Bike', 120.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+setProduct('Bike1', 240.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+setProduct('Bike2', 550.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+setProduct('Bike3', 840.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+setProduct('Bike4', 120.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+setProduct('Bike5', 320.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+setProduct('Bike6', 190.00 , 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
 }
 getAndPrintProduct(productsList);
+
+// proff 🤩😓
 
