edd1740de6b2a2c0a2d63c939e753823856397d8
fparramat <f.parra.matamala@gmail.com>
2023-01-27 15:46:30 -0300

    add shopping cart aside

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const desktopMenuToggle = document.querySelector("#desktop-menu-toggle");
 const mobileMenuToggle = document.querySelector("#mobile-menu-toggle");
+const shoppingCartToggle = document.querySelector("#shopping-cart-toggle");
 const desktopMenu = document.querySelector("#desktop-menu");
 const mobileMenu = document.querySelector("#mobile-menu");
+const productDetails = document.querySelector("#product-details");
 
-function toggleElement(element, toggleButton){
+function toggleElement(element, toggleButton, otherElementToHide){
     toggleButton.addEventListener("click", () => {
-    element.classList.toggle("inactive");
-})}
+        element.classList.toggle("inactive");
+        if (otherElementToHide){
+            if (!element.classList.contains("inactive")){
+                otherElementToHide.classList.add("inactive");
+            }
+        }
+    })
+}
+
 
 toggleElement(desktopMenu, desktopMenuToggle);
-toggleElement(mobileMenu, mobileMenuToggle);
+toggleElement(mobileMenu, mobileMenuToggle, productDetails);
+toggleElement(productDetails, shoppingCartToggle, mobileMenu);
+
