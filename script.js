4e2bb1bf2dfe822aed98cc13e2666a3bf485ec43
CarlosErnestoEM <carlos@carlosernestoem.live>
2023-01-26 19:08:53 -0600

    Finished navbar and menu for desktop and mobile

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
 
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu")
+const menuIcon = document.querySelector(".menu")
+
 const email = document.querySelector(".navbar-email")
 
 
-email.addEventListener("click", toggleDesktopMenu)
 
+email.addEventListener("click", toggleDesktopMenu)
+menuIcon.addEventListener("click",toggleMobileMenu)
 
 
 function toggleDesktopMenu(event){
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu(event) {
+    console.log("click")
+    mobileMenu.classList.toggle("inactive")
 }
