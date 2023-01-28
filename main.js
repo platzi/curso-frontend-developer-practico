38a74ae3ffd6a097fbe5e88e2af6774b668eba09
Eduard Galeano <eduardestivengs@gmail.com>
2023-01-28 03:56:57 -0500

    Se fuciona  la clase7 con la clase 11 en el index

---
+++ main.js
@@ -0,0 +1,13 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktoMenu = document.querySelector('.desktop-menu')
+
+menuEmail.addEventListener('click', toggleDesktopMenue)
+
+function toggleDesktopMenue(){
+
+    //console.log('click')
+
+    desktoMenu.classList.toggle('inactive');
+
+
+}
