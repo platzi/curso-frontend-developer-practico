755c274da78653311ce194b9ef6c574bf4dfe66d
guido2288 <guido2288@gmail.com>
2023-01-29 16:55:11 -0300

    menu mobile added

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu ');
+const menuHamburger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 const toggleDesktopMenu = () => {
 
-  desktopMenu.classList.toggle('inactive')
+  desktopMenu.classList.toggle('inactive');
+
+}
+
+const toggleMobileMenu = () => {
+
+  mobileMenu.classList.toggle('inactive');
 
 }
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburger.addEventListener('click', toggleMobileMenu);
 
