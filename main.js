86e1705ac07778d1a893a0519d8c9dc69829ff85
jorge luis <martinezmonajorgeluis@gmail.com>
2023-01-29 16:10:19 -0500

    En este commit se a desarrollado segun el programa de estudios del curso practico de JS en platzi la clase # 20 (Carrito de compras)que esta dedicada a engranar el correcto funcionamiento del carrito de compras esto de logra desarrollando unas lineas de codigo para poder conjugar el funcionamiento del portal (en verdad me siento entusiasmado con el aprendisaje del desarrollo de codigo es para mi  gratificante y realisardo)

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu')
-
+const aside = document.querySelector('.product-detail')
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
+    const isAsideClose = aside.classList.contains('inactive')
+    if(!isAsideClose){
+        aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu(){
+    const isAsideClose = aside.classList.contains('inactive')
+    if(!isAsideClose){
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')
 }
