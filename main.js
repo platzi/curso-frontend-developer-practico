4908582695ac8c9c04adf90f968f56b05ba11523
leonelmarinaro <marinaro.leonel@tecnica7.edu.ar>
2023-01-30 18:27:02 -0300

    Funcionalidades entre los menus, colocando clase inactive

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  aside.classList.add("inactive");
 }
 
 function toggleMobileMenu() {
+  desktopMenu.classList.add("inactive");
   mobileMenu.classList.toggle("inactive");
+  aside.classList.add("inactive");
+}
+
+function toggleCarritoAside() {
+  desktopMenu.classList.add("inactive");
+  mobileMenu.classList.add("inactive");
+  aside.classList.toggle("inactive");
 }
