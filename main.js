670f38b23d13d48d6a638f21a88fac1d4f47d605
dmarinv <dmarinv@users.noreply.github.com>
2023-01-30 17:56:32 -0500

    Fusion clase8 add media query toogle desktop and mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const deskTopMenu = document.querySelector('.desktop-menu');
+const hamburgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     deskTopMenu.classList.toggle('inactive');
 }
-console.log("JS")
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
