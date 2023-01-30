2ac94af5e17a0f0c0434318053b886132da711a0
Gianmarco Cueva <giancuap@gmail.com>
2023-01-30 12:31:21 -0500

    Menú de categorias en mobile añadido

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu =document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const categories = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu() {
+    categories.classList.toggle('inactive');
 }
