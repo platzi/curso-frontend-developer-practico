fd0dd0a6f7c3a607a9510cb726ac31fd3369338d
Danifanton <danielrxd0912@gmail.com>
2023-01-30 14:41:47 -0400

    Añadimos el Aside de resumen de compra

--- main.js
+++ main.js
@@ -5,23 +5,48 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const burgerIcon = document.querySelector(".burgerIcon");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const cartNavIcon = document.querySelector(".navbar-shopping-cart");
+const orderResumeAside = document.querySelector(".order-resume-aside");
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuFlecha.addEventListener("click", toggleDesktopMenu);
 
 burgerIcon.addEventListener("click", toggleMobileMenu);
 
+cartNavIcon.addEventListener("click", toggleOrderResumeAside);
+
 function toggleDesktopMenu(){
-    if(desktopMenu.className == "desktop-menu inactive"){
-        desktopMenu.className = "desktop-menu";
+    let isOrderResumeAsideOpen = !orderResumeAside.classList.contains("inactive"); 
+    
+    if(isOrderResumeAsideOpen){
+        orderResumeAside.classList.toggle("inactive");
     }
-    else{
-        desktopMenu.className = "desktop-menu inactive";
-    }
-    /* function toggleDesktopMenu(){
-        desktopMenu.classList.toggle("inactive");
-    } METODO DE JUAN*/
+
+    desktopMenu.classList.toggle("inactive");
+
 }
 
 function toggleMobileMenu(){
+    let isOrderResumeAsideOpen = !orderResumeAside.classList.contains("inactive"); 
+    
+    if(isOrderResumeAsideOpen){
+        orderResumeAside.classList.toggle("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleOrderResumeAside(){
+    let isMobileMenuOpen = !mobileMenu.classList.contains("inactive");  
+    let isEmailMenuOpen = !desktopMenu.classList.contains("inactive");
+
+    if(isMobileMenuOpen){
+        mobileMenu.classList.toggle("inactive");
+    }
+
+    if(isEmailMenuOpen){
+        desktopMenu.classList.toggle("inactive");
+    }
+
+    orderResumeAside.classList.toggle("inactive");
 }
