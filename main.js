72f7c7b85ede418449f427070895295e6d4f436d
Brayan Baron Amaya <brayan.baron@hotmail.com>
2023-01-29 22:58:41 -0500

    implementacion cambios responsive

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
 const menuEmail = document.querySelector ('.navbar-email');
+const BurguerIcon = document.querySelector('.menu');
+const menuCarritoIcon =  document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside =  document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+BurguerIcon.addEventListener('click', togglemobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
 
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
-    console.log("Click");
+    //console.log("Click");
+}
+
+function togglemobileMenu(){
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside (){
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
