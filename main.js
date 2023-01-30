ac074a07044c13c323ad31f67c5483804f330803
Brayan Camilo Bautista Arias <brayanbauti01@gmail.com>
2023-01-30 17:31:37 -0500

    Class:23 last changes

--- main.js
+++ main.js
@@ -6,11 +6,15 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon =document.querySelector('.product-detail-close');
+
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu(){
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -30,6 +34,7 @@ function toggleMobileMenu(){
         shotapingCartContainer.classList.add('inactive');
     
     }
+    closeProductDetailAside();
     
    mobileMenu.classList.toggle('inactive');
 
@@ -43,9 +48,31 @@ function toggleCarritoAside(){
     mobileMenu.classList.add('inactive');
 
 }
+
+const isProductDetailClosed= productDetailContainer.classList.contains('inactive');
+   
+   
+   
+   if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive');
+
+}
+
 shoppingCartContainer.classList.toggle('inactive');
  
 }
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive');
+    
+    productDetailContainer.classList.remove('inactive');
+
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
+
+}
+
 
 const productList = [];
 
@@ -80,12 +107,14 @@ productList.push ({
     price: 600,
     image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
 });
+
 for(product of productList){
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
 
     const productImg= document.createElement('img');
     productImg.setAttribute('src' , product.image);
+    productImg.addEventListener('click', openProductDetailAside);
     // product= {name, price, image} --> product.image
 
     const productInfo = document.createElement('div');
