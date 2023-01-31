dd3a2fa75492febaab8a2bbe7fd35976ac532db4
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-01-31 18:14:38 -0500

    Shopping Cart added and states setted to active or not depending on what I want

--- main.js
+++ main.js
@@ -1,19 +1,45 @@
-// alert("Verificando que JS fxne")
+// alert("Chacking that JS is working")
 
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const shoppingCartMenu = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
 menuHamIcon.addEventListener("click", toggleMobileMenu)
+shoppingCartMenu.addEventListener("click", toggleShoppingCartAside)
 
 function toggleDesktopMenu() {
-    // Con ".toggle" quito o pongo clase "inactive" dependiendo de si la tiene o no
+    // Creating constants in order to evaluate if the tags are closed
+    const isAsideClosed = aside.classList.contains("inactive")
+    
+    // If the tag "is not" (!) closed... then its open, so please close it
+    if(!isAsideClosed) {
+        aside.classList.add("inactive")
+    }
+    
+    // ".toggle" allows me to remove or set the class "inactive"
     desktopMenu.classList.toggle("inactive")
 }
 
-function toggleMobileMenu() {
-    // Con ".toggle" quito o pongo clase "inactive" dependiendo de si la tiene o no
+function toggleMobileMenu() { 
+    const isAsideClosed = aside.classList.contains("inactive")
+    
+    if(!isAsideClosed) {
+        aside.classList.add("inactive")
+    }
+    
     mobileMenu.classList.toggle("inactive")
+}
+
+function toggleShoppingCartAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
+
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive")
+    }
+    
+    aside.classList.toggle("inactive");
 }
