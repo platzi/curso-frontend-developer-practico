9396f44eea8243345179de32f79ce7c0ee147f39
Carlos Guerrero <c.j.guerrero.a@gmail.com>
2023-01-31 19:58:49 +0100

    ajustando colapsos entre menus

--- main.js
+++ main.js
@@ -6,18 +6,62 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const MobileMenuIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 
-//escuchando el evento click del menu desktop
+//Selectores para el carrito
+const menuCarritoICon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+
+//escuchando el evento click en el menu desktop
 menuEmail.addEventListener('click', toggleDesktopMenu)
 
-//escuchando el evento click del menu mobile
+//escuchando el evento click en el menu mobile
 MobileMenuIcon.addEventListener('click', toggleMobileMenu)
 
+//escuchando el evento click en el carrito
+menuCarritoICon.addEventListener('click', toggleCarritoAside)
+
 //funcion toggle para el menu en desktop
+
 function toggleDesktopMenu() {
+
+    //Cerrando el Menu del carrito
+    const isAsideClosed = aside.classList.contains('incavtive')
+
+    if (!isAsideClosed){
+            aside.classList.add('inactive')
+    }
+    
     desktopMenu.classList.toggle('inactive')
 }
 
 //funcion toggle para el menu en mobile
 function toggleMobileMenu(){
+
+    //Cerrando el Menu del carrito
+    const isAsideClosed = aside.classList.contains('incavtive')
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
+    //Abriendo el menu mobile
     mobileMenu.classList.toggle('inactive')
+}
+
+//funcion toggle para el menu del carrito
+function toggleCarritoAside(){
+
+    //Cerrando el mobile menu y el desktop menu
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    else if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    }
+
+    //Abriendo el menu del carrito
+    aside.classList.toggle('inactive')
+
 }
