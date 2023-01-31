f0bb263019fc4f31d0b4ee7f1aca27f14684c80e
“smuel14” <smuel14.finca@gmail.com>
2023-01-30 21:07:13 -0500

    Agregamos la lista de elementos

--- main.js
+++ main.js
@@ -1,12 +1,136 @@
-const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
+/**
+ * Seleccionando del DOM lo que necesitamos
+ */
+const menuEmail = document.querySelector(".navbar-email"); //Email
+const desktopMenu = document.querySelector(".desktop-menu"); //Menu desktop
+
+const menuIcon = document.querySelector(".menu"); //Icono de mobile
+const mobileMenu = document.querySelector(".mobile-menu"); //Menu mobile
+
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart"); //Icono carrito
+const aside = document.querySelector(".product-detail") //Menu carrito
+
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
+/**
+ * Funciones par desplegar menus
+ */
 // Aparecer desktop menu, presionar email
 function toggleDesktopMenu() {
-    console.log("Click");
+    const isAsideOpen = !aside.classList.contains("inactive");
+
+    if(isAsideOpen){
+        aside.classList.add("inactive");
+    }
     desktopMenu.classList.toggle("inactive"); //if inactive is set remove it, otherwise add it
 }
+// Aparecer mobile menu, presionar icono
+function toggleMobileMenu(){
+    const isAsideOpen = !aside.classList.contains("inactive");
 
-console.log("JS funcionando");
+    if(isAsideOpen){
+        aside.classList.add("inactive");
+    }
+    mobileMenu.classList.toggle("inactive");
+}
+
+// Aparece menu carrito
+function toggleCarritoAside(){
+    const isMobileMenuOpen = !mobileMenu.classList.contains("inactive");
+    const isDesktopMenuOpen = !desktopMenu.classList.contains("inactive");
+
+    //Si mobile esta open lo cerramos.
+    if(isMobileMenuOpen){
+        mobileMenu.classList.add("inactive");
+    }
+    //Si desktop esta open lo cerramos
+    if(isDesktopMenuOpen){
+        desktopMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
+}
+
+/**
+ * Agarran elemento como si lo hubieramos tráido desde el backend
+ */
+const productList = []; //Haga de cuenta que lo traemos de una base de datos
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+})
+productList.push({
+    name: "Pantalla",
+    price: 220,
+    image: "https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+productList.push({
+    name: "Computadora",
+    price: 620,
+    image: "https://images.pexels.com/photos/5380642/pexels-photo-5380642.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+//Insertar en HTML
+/**
+ * 1. Utilizar un ciclo for
+ * 2. Utilizar sintaxis for prod of productList
+ */
+/*
+<div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+        <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+        </div>
+        <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div>
+*/
+
+function renderProducts(arr){
+    for( product of arr){
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.image);
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+    
+    
+        const productInfoDiv = document.createElement("div");
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement("figure");
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }   
+}
+
+renderProducts(productList);
