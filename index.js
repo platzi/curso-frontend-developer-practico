8768493a4e8a4823b202f1c1c62a8804f3790bb5
Roberto Ramirez <robert.624@hotmail.com>
2023-01-31 17:05:06 -0500

    combined menu mobile

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const menuEmail = document.querySelector(".navbar-email")
 const hiddenMenu = document.querySelector(".desktop-menu")
 
 
-menuEmail.addEventListener("click", toggleMenu)
+menuEmail.addEventListener("click", toggleDesktopMenu)
 
 
-function toggleMenu () {
-
+function toggleDesktopMenu () {
     hiddenMenu.classList.toggle("inactive")
+}
+
+
+// Toggle menu mobile only when mobile and when burguer Menu is clicked
+
+const menuHamburger = document.querySelector(".menu");
+const menuMobile = document.querySelector(".mobile-menu");
+
+menuHamburger.addEventListener("click", toggleMobileMenu)
+
+function toggleMobileMenu() {
+    // console.log(menuMobile)
+    menuMobile.classList.toggle("inactive")
 }
