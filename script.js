99fae97d66f9a754189e97ed961e87e1adb4adb7
micton-code <antho_ul@hotmail.com>
2023-01-31 19:03:40 -0400

    agregamos el burgerMenu a la version mobile y se mejoro el mediaqueries del navbar

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-// desplegar menu al dar click al email
+// var desplegar menu al dar click al email
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+// var Desplegar menu hamburguesa al dar click al icono 
+const burgerIcon = document.querySelector('.burgerIcon');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+// desplegar menu al dar click al email
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     console.log('click');
-    // al ejecutar la funcion classList.toggle va a quitar o colocr la clase inactive dependiendo del evento click a*adido anteriormente
+    // al ejecutar la funcion classList.toggle va a quitar o colocar la clase inactive dependiendo del evento click a*adido anteriormente
     desktopMenu.classList.toggle('inactive');
 }
 console.log('js funcionando');
+
+// Desplegar menu hamburguesa al dar click al icono 
+burgerIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    console.log('click');
+    // al ejecutar la funcion classList.toggle va a quitar o colocar la clase inactive dependiendo del evento click a*adido anteriormente
+    mobileMenu.classList.toggle('inactive');
+}
