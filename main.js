e9fed3d471ad89df930eb751459efd198e32d74d
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-01-31 17:07:03 -0500

    Mobile menu has been merged into general files index.html/styles.css/main.js

--- main.js
+++ main.js
@@ -2,10 +2,18 @@
 
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+const menuHamIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+menuHamIcon.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu() {
-    // Con esto, quito o pongo clase "inactive" dependiendo de si la tiene o no
+    // Con ".toggle" quito o pongo clase "inactive" dependiendo de si la tiene o no
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu() {
+    // Con ".toggle" quito o pongo clase "inactive" dependiendo de si la tiene o no
+    mobileMenu.classList.toggle("inactive")
 }
