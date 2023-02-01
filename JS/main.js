7002043419926fe93aa35eeedf8e62b062b857f4
Carlos Rodríguez <racm1698@gmail.com>
2023-01-31 18:16:24 -0600

    Trabajo terminado en la barra de navegacion tanto para la vista mobile y desktop se agrego la funcionalidad total en cada uno de los iconos correspondientes

--- JS/main.js
+++ JS/main.js
@@ -4,8 +4,12 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburgeIcon=document.querySelector('.menuHamburger');
 const mobileMenu=document.querySelector('.mobile-menu');
 
+const shoppingCartIcon=document.querySelector('.navbar-shoppingCart-icon')
+const detailShoppingCart=document.querySelector('.product-detail');
+
 navEmail.addEventListener('click',toogleDesktopMenu);
 menuHamburgeIcon.addEventListener('click',toogleMobileMenu);
+shoppingCartIcon.addEventListener('click',toogleShoppingCart);
 
 function toogleDesktopMenu(){
     // forma larga
@@ -17,9 +21,28 @@ function toogleDesktopMenu(){
     // }
 
     //forma profesional
+    const isdetailShoppingCartClosed=detailShoppingCart.classList.contains('inactive');
+    if(!isdetailShoppingCartClosed){
+        detailShoppingCart.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 
 }
 function toogleMobileMenu(){
+    const isdetailShoppingCartClosed=detailShoppingCart.classList.contains('inactive');
+    if(!isdetailShoppingCartClosed){
+        detailShoppingCart.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+function toogleShoppingCart(){
+    const isMenuMobileClosed=mobileMenu.classList.contains('inactive');
+    const isMenuDesktopClosed=desktopMenu.classList.contains('inactive');
+    if(!isMenuMobileClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if(!isMenuDesktopClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    detailShoppingCart.classList.toggle('inactive');
 }
