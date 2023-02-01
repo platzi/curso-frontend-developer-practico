851974a219480ee06b1a3d6684f1c9480f9cbe28
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-01-31 23:24:19 -0500

    Product cards created through JS instead of HTML

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const shoppingCartMenu = document.querySelector(".navbar-shopping-cart")
 const aside = document.querySelector(".product-detail")
+const cardsContainer = document.querySelector(".cards-container") 
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
 menuHamIcon.addEventListener("click", toggleMobileMenu)
 shoppingCartMenu.addEventListener("click", toggleShoppingCartAside)
 
+// The next functions are created to show or hide certain cards of the code, and while doing, closing or opening other elements
 function toggleDesktopMenu() {
-    // Creating constants in order to evaluate if the tags are closed
+    // Creating constants in order to represent when the tags are closed
     const isAsideClosed = aside.classList.contains("inactive")
     
     // If the tag "is not" (!) closed... then its open, so please close it
@@ -36,10 +38,105 @@ function toggleMobileMenu() {
 
 function toggleShoppingCartAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
+    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive")
 
     if(!isMobileMenuClosed) {
         mobileMenu.classList.add("inactive")
     }
     
     aside.classList.toggle("inactive");
+
+    if(!isDesktopMenuClosed) {
+        desktopMenu.classList.add("inactive")
+    }
 }
+
+
+// Array created with the products to be shown, so that is not necessary to have them in HTML:
+const productList = []
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productList.push({
+    name: "Screen",
+    price: 310,
+    image: "https://images.pexels.com/photos/5861325/pexels-photo-5861325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+});
+
+productList.push({
+    name: "Computer",
+    price: 2400,
+    image: "https://images.pexels.com/photos/259091/pexels-photo-259091.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+});
+
+productList.push({
+    name: "Stationery",
+    price: 80,
+    image: "https://images.pexels.com/photos/7718747/pexels-photo-7718747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+});
+
+productList.push({
+    name: "Lamp",
+    price: 437,
+    image: "https://images.pexels.com/photos/6029034/pexels-photo-6029034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+});
+
+productList.push({
+    name: "Bench",
+    price: 689,
+    image: "https://images.pexels.com/photos/4559954/pexels-photo-4559954.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+});
+
+productList.push({
+    name: "Armchair",
+    price: 550,
+    image: "https://images.pexels.com/photos/5749125/pexels-photo-5749125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+});
+
+
+// Function for creating a card
+function renderProducts(arr) {
+    // Iterating through the array to manipulate the DOM
+    for (product of productList) {
+        // console.log(product.name)
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.image)
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        const productInfoDiv = document.createElement("div");
+        
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+    
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+        const productInfoFigure = document.createElement("figure");
+        const productImgCart = document.createElement("img");
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+// Iterating the array through the function created for this purpose
+renderProducts(productList);
