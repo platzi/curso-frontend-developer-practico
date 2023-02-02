dacf5e9e9830eb6e9e070ce841fe79b0de63d52f
unknown <moyaroy@gmail.com>
2023-02-01 23:26:09 -0500

    Se realizó la unión con la clase 13 (Aside de detalle de productos), se realizó las configuraciones para que no se muestren en simultaneo los elementos anteriores (Menu Mobile, y Desktop Menu)

--- main.js
+++ main.js
@@ -4,14 +4,36 @@ const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
 const menuHamIcon = $('.menu');
 const mobilemenu=$('.mobile-menu');
+const menuCarritoIcon=$('.navbar-shopping-cart')
+const asideProductDetails =$('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
+    const asideProductDetailsClose=asideProductDetails.classList.contains('inactive')
+
+    if(!asideProductDetailsClose){
+        asideProductDetails.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive')
 } 
 
 function toggleMobileMenu(){
+    const asideProductDetailsClose=asideProductDetails.classList.contains('inactive')
+
+    if(!asideProductDetailsClose){
+        asideProductDetails.classList.add('inactive')
+    } 
     mobilemenu.classList.toggle('inactive')
+} 
+
+function toggleCarritoAside(){
+    const isMobileMenuClose=mobilemenu.classList.contains('inactive')
+
+    if(!isMobileMenuClose){
+        mobilemenu.classList.add('inactive')
+    } 
+    asideProductDetails.classList.toggle('inactive')
 } 
