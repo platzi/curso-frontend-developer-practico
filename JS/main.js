97e70ccb2ca8e462ee8b91ed717550eb49a01866
Carlos Rodríguez <racm1698@gmail.com>
2023-02-01 18:53:42 -0600

    Se creo una fucion en el archivo main.js que simula el consumo de una API Rest para hacer dinamica la lista de productos. El DOM se manipulo usando JS

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,9 @@ const mobileMenu=document.querySelector('.mobile-menu');
 const shoppingCartIcon=document.querySelector('.navbar-shoppingCart-icon')
 const detailShoppingCart=document.querySelector('.product-detail');
 
+const mainContainerCards=document.querySelector('.cards-container');
+
+
 navEmail.addEventListener('click',toogleDesktopMenu);
 menuHamburgeIcon.addEventListener('click',toogleMobileMenu);
 shoppingCartIcon.addEventListener('click',toogleShoppingCart);
@@ -46,3 +49,74 @@ function toogleShoppingCart(){
     }
     detailShoppingCart.classList.toggle('inactive');
 }
+
+const productList=[];
+productList.push({
+    name:'Bike',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name:'Camera',
+    price:720,
+    image:'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name:'Bag of coffee',
+    price:100,
+    image:'https://images.pexels.com/photos/4829069/pexels-photo-4829069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name:'Soap',
+    price:80,
+    image:'https://images.pexels.com/photos/3735149/pexels-photo-3735149.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name:'Jeans',
+    price:320,
+    image:'https://images.pexels.com/photos/7679454/pexels-photo-7679454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+function showProductList(productLists){
+    productLists.forEach(productList => {
+        const {image,price,name}=productList;
+        const productContainer=document.createElement('div');
+        productContainer.classList.add('product-card');
+        const imgProdut=document.createElement('img');
+        imgProdut.classList.add('product-card-image');
+        imgProdut.src=image;
+
+        const productInfoContainer=document.createElement('div');
+        productInfoContainer.classList.add('product-info');
+
+        const priceNameContainer=document.createElement('div');
+        const priceProduct=document.createElement('p');
+        priceProduct.classList.add('product-info-price');
+        priceProduct.innerText="$"+price+",00";
+        const nameProduct=document.querySelector('p');
+        nameProduct.classList.add('product-info-name');
+        nameProduct.innerText=name;
+        priceNameContainer.appendChild(priceProduct);
+        priceNameContainer.appendChild(nameProduct);
+
+        const iconContainer=document.createElement('figure');
+        const iconAddShoppingCart=document.createElement('img');
+        iconAddShoppingCart.classList.add('product-info-icon');
+        iconAddShoppingCart.src="./icons/bt_add_to_cart.svg";
+        iconContainer.appendChild(iconAddShoppingCart);
+        priceNameContainer.appendChild(iconContainer);
+
+        productInfoContainer.appendChild(priceNameContainer);
+        productInfoContainer.appendChild(iconContainer);
+
+
+        productContainer.appendChild(imgProdut);
+        productContainer.appendChild(productInfoContainer);
+
+        /*Vista en el HMTL */
+        mainContainerCards.appendChild(productContainer);
+    });
+
+}
+
+showProductList(productList);
