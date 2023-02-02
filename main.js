2830d7ce35791ece5a49a7ed02be2164528a38b2
Ronald Moreno <ronald.moreno1214@hotmail.com>
2023-02-02 03:03:22 -0500

    3.5

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuHamIcon =document.querySelector('.menu');
 const mobileMenu =document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon =document.querySelector('.product-detail-close');
 const cardsContainer =document.querySelector('.cards-container');
 
 
@@ -12,6 +14,8 @@ const cardsContainer =document.querySelector('.cards-container');
 menuEmail.addEventListener('click' ,toggleDesktopMenu);
 menuHamIcon.addEventListener('click', togglemobileMenu);
 menuCarritoIcon.addEventListener('click' ,toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
+
 
 function toggleDesktopMenu(){
     const isAsideClosed =shoppingCartContainer.classList.contains('inactive');
@@ -24,27 +28,42 @@ function toggleDesktopMenu(){
 }
 
 function togglemobileMenu(){
-    const isAsideClosed =shoppingCartContainer.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if (!isAsideClosed){
-       shoppingCartContainer.classList.add('inactive');
-     
-         }
-     
+    if (!isAsideClosed) {
+      shoppingCartContainer.classList.add('inactive'); 
+    }
+  
+    closeProductDetailAside();
+    
     mobileMenu.classList.toggle('inactive');
-}
+  }
 
 function toggleCarritoAside(){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    
-    if (!isMobileMenuClosed){
-   mobileMenu.classList.add('inactive');
-
+  
+    if (!isMobileMenuClosed) {
+      mobileMenu.classList.add('inactive');
     }
-
-shoppingCartContainer.classList.toggle('inactive');
-}
-
+  
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    
+    if (!isProductDetailClosed) {
+      productDetailContainer.classList.add('inactive'); 
+    }
+    
+    shoppingCartContainer.classList.toggle('inactive');
+  }
+  
+  function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+  }
+  
+  function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
+  }
+  
  const productList = [];
  productList.push({
    name: 'Bike',
@@ -82,6 +101,7 @@ shoppingCartContainer.classList.toggle('inactive');
           // product= {name, price, image} -> product.image
           const productImg = document.createElement('img');
           productImg.setAttribute('src', product.image);
+          productImg.addEventListener('click',openProductDetailAside)
         
           const productInfo = document.createElement('div');
           productInfo.classList.add('product-info');
