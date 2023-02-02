c31019769d75f1dbb0c32ebb3f67fddcb6b0f4c5
unknown <moyaroy@gmail.com>
2023-02-01 22:30:12 -0500

    Se realizó la unión con la clase 08, y se modificó para que trabaje webresponsive en mobiles

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ const $ = (selector) => document.querySelector(selector);
 
 const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
+const menuHamIcon = $('.menu');
+const mobilemenu=$('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuHamIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
-}
+} 
+
+function toggleMobileMenu(){
+    mobilemenu.classList.toggle('inactive')
+} 
