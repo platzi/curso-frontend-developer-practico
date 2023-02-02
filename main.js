3895b8a130352652133be550c4267129e0e5f67a
unknown <moyaroy@gmail.com>
2023-02-01 21:32:51 -0500

    Se ha mejorado el uso de variable $ en reemplazo de document.querySelector

---
+++ main.js
@@ -0,0 +1,10 @@
+const $ = (selector) => document.querySelector(selector);
+
+const menuEmail = $('.navbar-email');
+const desktopMenu = $('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
+
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive')
+}
