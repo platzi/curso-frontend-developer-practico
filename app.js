6b5da779af3bfffda80a9a726ebe587aaa6e0218
Andrea <lauralme96@gmail.com>
2023-02-02 22:29:10 +0000

    lista de productos

--- app.js
+++ app.js
@@ -31,3 +31,31 @@ function togglemobiledesktop(){
    }
     productDetail.classList.toggle('inactive')
  }
+
+ const productList = [];
+ productList.push({
+   name: "Bike",
+   price: 200,
+   imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ })
+ productList.push({
+   name: "Tv",
+   price: 500,
+   imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ })
+ productList.push({
+   name: "tablet",
+   price: 400,
+   imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ })
+
+ for (product of productList){
+   const productCard = document.createElement('div');
+   productCard.classList('product-card');
+
+   const img = document.createElement('img')
+   img.setAttribute('src', product.imagen)
+
+   const productInfo = document.createElement('div');
+   productInfo.classList('product-info');
+ }
