b0a97c5d1da0890e3f3ff652f9f0e69463fa37ba
YonatanA <YonathanArroyave>
2023-02-02 23:42:17 -0500

    esconder y mostrar el menu mobile en el menu hambur

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMEnu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
-function toggleDesktopMenu() {
-    console.log('clickod');
+function toggleDesktopMenu() { 
     desktopMenu.classList.toggle('inactive'); //esta parte nos ayuda a poner o ocultar pedazos de css
 }
 
+function toggleMobileMenu() {
+    mobileMEnu.classList.toggle('inactive'); 
+
+}   
