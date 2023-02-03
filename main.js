62f8314f862b18d34c3cf386e71c2fc89c85cd01
Sochoag <santiagodav@hotmail.es>
2023-02-03 12:16:17 -0500

    Clase 20

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const menuEmail = document.querySelector(".navbar-email");
+const menuBurger = document.querySelector(".menu");
+const cartList = document.querySelector(".navbar-shopping-cart");
+
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const menuBurger = document.querySelector(".menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuBurger.addEventListener("click", toggleMobileMenu);
+cartList.addEventListener("click", toggleCartList);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+  aside.classList.add("inactive");
 }
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
+  aside.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
+}
+function toggleCartList() {
+  mobileMenu.classList.add("inactive");
+  aside.classList.toggle("inactive");
+  desktopMenu.classList.add("inactive");
 }
