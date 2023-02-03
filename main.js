ba08871b0f6f8f58af3d1aa55f9633cc0b6a672f
Sochoag <santiagodav@hotmail.es>
2023-02-03 13:00:16 -0500

    Clase 21

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const cartList = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuBurger.addEventListener("click", toggleMobileMenu);
@@ -24,3 +25,63 @@ function toggleCartList() {
   aside.classList.toggle("inactive");
   desktopMenu.classList.add("inactive");
 }
+
+const productList = [];
+productList.push({
+  name: "Cellphone",
+  price: "520.00",
+  image:
+    "https://www.fda.gov/files/styles/main_image_medium/public/smartphone.jpg?itok=Iqc_wjqn",
+});
+productList.push({
+  name: "Pantalla",
+  price: "220.00",
+  image:
+    "https://oasis.opstatics.com/content/dam/oasis/page/frankfurt/27-share.jpg",
+});
+productList.push({
+  name: "Compu",
+  price: "620.00",
+  image:
+    "https://i.pcmag.com/imagery/reviews/02lcg0Rt9G3gSqCpWhFG0o1-2..v1656623239.jpg",
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.append(productCard);
+  }
+}
+
+renderProducts(productList);
