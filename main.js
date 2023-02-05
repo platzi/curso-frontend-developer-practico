5af9dcce9bdd12d3db1196d48a201cced0e56633
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2023-02-05 15:07:31 -0600

    Adding mobile menu and media queries to show or hide mobile and desktop menu

--- main.js
+++ main.js
@@ -2,8 +2,11 @@
 // creation of a const in charge to open and close the desktop-menu
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
 
@@ -17,4 +20,8 @@ function toggleDesktopMenu(){
 
     //The teacher method:
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
