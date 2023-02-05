14bdfdb049cf752f57a6b33d9943189180cfe3d3
Brayan Baron Amaya <brayan.baron@hotmail.com>
2023-02-04 23:32:02 -0500

    identacion

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function toggleDesktopMenu() {
   if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive');
   }
-  
+
   desktopMenu.classList.toggle('inactive');
 }
 
@@ -24,19 +24,19 @@ function toggleMobileMenu() {
   const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
-    shoppingCartContainer.classList.add('inactive'); 
+    shoppingCartContainer.classList.add('inactive');
   }
-  
+
   mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside() {
   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-  
+
   if (!isMobileMenuClosed) {
-    mobileMenu.classList.add('inactive'); 
+    mobileMenu.classList.add('inactive');
   }
-  
+
   shoppingCartContainer.classList.toggle('inactive');
 }
 
@@ -61,36 +61,36 @@ function renderProducts(arr) {
   for (product of arr) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
-  
+
     // product= {name, price, image} -> product.image
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
-  
+
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
-  
+
     const productInfoDiv = document.createElement('div');
-  
+
     const productPrice = document.createElement('p');
     productPrice.innerText = '$' + product.price;
     const productName = document.createElement('p');
     productName.innerText = product.name;
-  
+
     productInfoDiv.appendChild(productPrice);
     productInfoDiv.appendChild(productName);
-  
+
     const productInfoFigure = document.createElement('figure');
     const productImgCart = document.createElement('img');
     productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-  
+
     productInfoFigure.appendChild(productImgCart);
-  
+
     productInfo.appendChild(productInfoDiv);
     productInfo.appendChild(productInfoFigure);
-  
+
     productCard.appendChild(productImg);
     productCard.appendChild(productInfo);
-  
+
     cardsContainer.appendChild(productCard);
   }
 }
