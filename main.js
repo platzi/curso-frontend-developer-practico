e994ee8ba96ac31a6d5d861b077035b593af64e3
Ovett <ovettmora17@gmail.com>
2023-02-05 17:51:27 -0600

    Version finalizada, con todas las funcionalidades

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
+
+const myOrders = document.querySelector('.title');
+myOrders.addEventListener('click', toggleShoppingCartAside);
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 iconMenu.addEventListener('click', toggleMobileMenu);
 shoppingCartIcon.addEventListener('click', toggleShoppingCartAside);
@@ -23,16 +27,15 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu (){
     shoppingCartContainer.classList.add('inactive');
-
     closeProductDetailAside();
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleShoppingCartAside (){
+function toggleShoppingCartAside (e){
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
     productDetailContainer.classList.add('inactive');
-    renderShoppingCart();
+    // addToShoppingCart(e);
     shoppingCartContainer.classList.toggle('inactive');
 }
 
@@ -40,6 +43,7 @@ function openProductDetailAside (e) {
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
     shoppingCartContainer.classList.add('inactive');
+    
     productDetailContainer.innerHTML = '';
 
     for (product of productList) {
@@ -70,18 +74,21 @@ function openProductDetailAside (e) {
             const button = document.createElement('button');
             button.classList.add('primary-button');
             button.classList.add('add-to-cart-button');
+            button.setAttribute('data-id', product.id);
+            
+            button.addEventListener('click', addToShoppingCart, {once: true});
+            /* "{once: true}" hace que el evento se ejecute una sola vez, 
+            así no tener problema con que se repeta el mismo producto.
+            Claro, se puede mejorar para que se compren mas del mismo producto */
     
             const buttonImg = document.createElement('img');
             buttonImg.setAttribute('src', './icons/bt_add_to_cart.svg');
             buttonImg.setAttribute('alt', 'add to cart');
-    
-            const buttonInfo = document.createElement('p');
-            buttonInfo.classList.add('btn-info');
-            buttonInfo.innerText = 'Add to cart';
+            buttonImg.setAttribute('data-id', product.id);
             
+            button.innerText = "Add to Cart";
             button.appendChild(buttonImg);
-            button.appendChild(buttonInfo);
-    
+            
             productDetailContainer.appendChild(productImgInfo);
             
             productInfo_Container.appendChild(productPriceInfo);
@@ -92,15 +99,15 @@ function openProductDetailAside (e) {
             productDetailContainer.appendChild(productDetailClosed);
             productDetailContainer.appendChild(productInfo_Container);
             
-            productDetailContainer.classList.remove('inactive');  
+            productDetailContainer.classList.remove('inactive'); 
         }
     }
     scrollToTop();
-    e.stopPropagation();
+    
 }
 function scrollToTop(){
     window.scrollTo({top: 0, behavior: 'smooth'});
-  }
+}
 function closeProductDetailAside(){
     productDetailContainer.classList.add('inactive');
 }
@@ -108,7 +115,7 @@ var productList = [];
 
 productList.push({
     id: 1,
-    name: 'motorcycle',
+    name: 'Bike',
     price: 11000,
     Image: './images/image1.jpeg',
     description: 'Amazing motorcycle'
@@ -191,12 +198,16 @@ function renderProducts(arr) {
         productInfoDiv.appendChild(productName);
     
         const productInfoFigure = document.createElement('figure');
-        const productImgCart = document.createElement('img');
-        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-        productImgCart.setAttribute('data-id', product.id);
-        productImgCart.addEventListener('click', openProductDetailAside);
+        const productIconCart = document.createElement('img');
+        productIconCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+        productIconCart.setAttribute('data-id', product.id);
+        productIconCart.classList.add('add-to-cart-button');
 
-        productInfoFigure.appendChild(productImgCart);
+        productIconCart.addEventListener('click', addToShoppingCart, {once: true});
+        /* "{once: true}" hace que el evento se ejecute una sola vez, 
+        asi no tener problema con repetir el mismo producto */
+
+        productInfoFigure.appendChild(productIconCart);
     
         productInfo.appendChild(productInfoDiv);
         productInfo.appendChild(productInfoFigure);
@@ -206,77 +217,118 @@ function renderProducts(arr) {
     
         cardsContainer.appendChild(productCard);
     }
-    
-    
 }
 
 renderProducts(productList);
 
-//shoppingCart
-function renderShoppingCart() {
-    shoppingCartContainer.innerHTML = '';
+// shopping cart container
+const titleContainer = document.createElement('div');
+titleContainer.classList.add('title-container');
 
-    const titleContainer = document.createElement('div');
-    titleContainer.classList.add('title-container');
-    const titleImg = document.createElement('img');
-    titleImg.setAttribute('src', './icons/flechita.svg');
-    titleImg.classList.add('product-detail--img');
-    titleImg.addEventListener('click', toggleShoppingCartAside);
-    const titleText = document.createElement('p');
-    titleText.classList.add('title');
-    titleText.innerText = 'My order';
-    titleContainer.appendChild(titleImg);
-    titleContainer.appendChild(titleText);
+const titleImg = document.createElement('img');
+titleImg.setAttribute('src', './icons/flechita.svg');
+titleImg.classList.add('product-detail--img');
 
-    const orderContent = document.createElement('div');
-    orderContent.classList.add('my-order-content');
-    for (product of productList) {
-        
-        const cartDiv = document.createElement('div');
-        cartDiv.classList.add('shopping-cart');
+// event to close the shopping cart
+titleImg.addEventListener('click', toggleShoppingCartAside);
 
-        const cartFigure = document.createElement('figure');
-        const cartImg = document.createElement('img');
-        cartImg.setAttribute('src', product.Image);
-        cartFigure.appendChild(cartImg);
+const titleText = document.createElement('p');
+titleText.classList.add('title');
+titleText.innerText = 'My Orders';
 
-        const cartItemName = document.createElement('p');
-        cartItemName.innerText = product.name; 
-        
-        const cartItemPrice = document.createElement('p');
-        cartItemPrice.innerText = '$ ' + product.price; 
+titleContainer.appendChild(titleImg);
+titleContainer.appendChild(titleText);
 
-        const DeleteItem = document.createElement('img');
-        DeleteItem.setAttribute('src', './icons/icon_close.png');
-        DeleteItem.classList.add('delete-product');
+const orderContent = document.createElement('div');
+orderContent.classList.add('my-order-content');
 
-        cartDiv.appendChild(cartFigure);
-        cartDiv.appendChild(cartItemName);
-        cartDiv.appendChild(cartItemPrice);
-        cartDiv.appendChild(DeleteItem);
+const totalCart = document.createElement('div');
+totalCart.classList.add('order');
 
-        orderContent.appendChild(cartDiv);
+const totalText = document.createElement('p');
+const totalTextSpan = document.createElement('span');
+totalTextSpan.innerText = 'Total';
+totalText.appendChild(totalTextSpan);
+totalCart.appendChild(totalText);
+
+//total price
+var total = 0;
+const totalPaying = document.createElement('p');
+totalPaying.innerText = '$ ' + total;
+totalCart.appendChild(totalPaying);
+
+// checkout button
+const primaryButton = document.createElement('button');
+primaryButton.classList.add('primary-button');
+primaryButton.innerText = 'Checkout';
+
+// structuring the shopping cart
+shoppingCartContainer.appendChild(titleContainer);
+shoppingCartContainer.appendChild(orderContent);
+shoppingCartContainer.appendChild(totalCart);
+shoppingCartContainer.appendChild(primaryButton);
+
+
+//add to shopping cart
+function addToShoppingCart (e) {
+    // console.log(e.target.classList.contains('add-to-cart-button'));
+    // console.log(e.target.dataset.id);
+    if (e.target.classList.contains('add-to-cart-button')) {
+        renderShoppingCart(e);
     }
-    const totalCart = document.createElement('div');
-    totalCart.classList.add('order');
+}
+//render shopping cart
+const cartAmount = document.querySelector('.shopping-cart--amount');
+var  cartAmountItem = 0;
+cartAmount.textContent = cartAmountItem;
 
-    const totalText = document.createElement('p');
-    const totalTextSpan = document.createElement('span');
-    totalTextSpan.innerText = 'Total';
-    totalText.appendChild(totalTextSpan);
+function renderShoppingCart(e) {
+    for (product of productList) {
+        if (e.target.dataset.id == product.id) {
+            //incrementamos antes para que se refleje al entrar al condicional y no despues.
+            cartAmount.textContent = ++cartAmountItem;
+            
+            const cartDiv = document.createElement('div');
+            cartDiv.classList.add('shopping-cart');
 
-    const totalPaying = document.createElement('p');
-    totalPaying.innerText = '$ ' + product.price;
+            const cartFigure = document.createElement('figure');
+            const cartImg = document.createElement('img');
+            cartImg.setAttribute('src', product.Image);
+            cartFigure.appendChild(cartImg);
 
-    totalCart.appendChild(totalText);
-    totalCart.appendChild(totalPaying);
+            const cartItemNameDiv = document.createElement('div');
+            cartItemNameDiv.classList.add('shopping-cart-info');
 
-    const primaryButton = document.createElement('button');
-    primaryButton.classList.add('primary-button');
-    primaryButton.innerText = 'Checkout';
+            const cartItemName = document.createElement('p');
+            cartItemName.innerText = product.name; 
+            
+            const cartItemPrice = document.createElement('p');
+            cartItemPrice.innerText = '$ ' + product.price; 
+            total = total + product.price;
+            totalPaying.innerText = '$ ' + total;
+            
+            const deleteItem = document.createElement('img');
+            deleteItem.setAttribute('src', './icons/icon_close.png');
+            deleteItem.classList.add('delete-product');
+            deleteItem.setAttribute('data-id', product.id);
+            deleteItem.addEventListener('click', (e) => {
+                for (product of productList) {
+                    if (e.target.dataset.id == product.id) {
+                        total = total - product.price;
+                        totalPaying.innerText = '$ ' + total;
+                        cartAmount.textContent = --cartAmountItem;
+                    }
+                }
+                //Aca removemos el contenido del producto que seleccionamos
+                orderContent.removeChild(cartDiv);
+            });
+            
+            cartDiv.appendChild(cartFigure);
+            cartDiv.appendChild(cartItemName);
+            cartDiv.appendChild(cartItemPrice);
+            cartDiv.appendChild(deleteItem);
 
-    shoppingCartContainer.appendChild(titleContainer);
-    shoppingCartContainer.appendChild(orderContent);
-    shoppingCartContainer.appendChild(totalCart);
-    shoppingCartContainer.appendChild(primaryButton);
+            orderContent.appendChild(cartDiv);
+        }
+    }   
 }
