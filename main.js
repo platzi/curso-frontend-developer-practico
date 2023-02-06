bd31e20499d56e6bec4d77a89c861d15b3087801
Alan Felix <alangiovani123@gmail.com>
2023-02-06 16:57:52 -0700

    Agregamos AsideMenu, usamos condiciones para que se cierren los menús y arreglamos animaciones

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
+// Desktop Menu
 const email = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const burguerIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-const 
 
 email.addEventListener('click', ToggleDesktopMenu);
-burguerIcon.addEventListener('click', ToggleMobileMenu);
 
 function ToggleDesktopMenu(){
+  const isAsideClosed = !asideMenu.classList.contains('inactive');
+
+  if(isAsideClosed){
+    asideMenu.classList.add('ocultoMenuCart');
+  }
   desktopMenu.classList.toggle('ocultoMenuDesktop');
 }
 
+// Burguer Menu
+
+const burguerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+burguerIcon.addEventListener('click', ToggleMobileMenu);
+
 function ToggleMobileMenu(){
+  const isAsideClosed = !asideMenu.classList.contains('inactive');
+
+  if(isAsideClosed){
+    asideMenu.classList.add('ocultoMenuCart');
+  }
   mobileMenu.classList.toggle('ocultoMenuMobile');
+}
+
+// Cart Menu
+
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const asideMenu = document.querySelector('.product-detail');
+
+cartIcon.addEventListener('click', ToggleCartMenu);
+
+function ToggleCartMenu(){
+  const isMobileClosed = !mobileMenu.classList.contains('ocultoMenuMobile');
+  const isDesktopClosed = !desktopMenu.classList.contains('ocultoMenuDesktop');
+
+  if(isMobileClosed){
+    mobileMenu.classList.add('ocultoMenuMobile');
+  }else if(isDesktopClosed){
+    desktopMenu.classList.add('ocultoMenuDesktop');
+  }
+
+  asideMenu.classList.toggle('ocultoMenuCart');
 }
