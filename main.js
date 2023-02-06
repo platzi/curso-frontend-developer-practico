7090209b59e151eb1a71a53a545014ba9ddc3fd0
David Castellanos Zayas <david383812@gmail.com>
2023-02-06 16:39:45 -0500

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMovileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMovileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
