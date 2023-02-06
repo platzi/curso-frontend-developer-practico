6b2945521130d6afdc8216ef9b6a1b3ac0152d5c
Diego Reyes <diegorele13@gmail.com>
2023-02-06 15:52:21 -0500

    merging menu mobile

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 const toggleDesktopMenu = () => {
 	console.log("Toggle desktop menu");
@@ -7,3 +9,9 @@ const toggleDesktopMenu = () => {
 };
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+
+const toggleMobileMenu = () => {
+	mobileMenu.classList.toggle("inactive");
+};
+
+menuHamIcon.addEventListener("click", toggleMobileMenu);
