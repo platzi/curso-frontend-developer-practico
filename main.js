8e707d1aa8ef23ae1ad4f5a7a2543d378ab78f4e
Jorge Suárez <suarezjorge758@gmail.com>
2023-02-06 14:54:47 -0400

    hola

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-menuEmail.addEventListener('mouseenter' , toggleDesktopMenu);
+menuEmail.addEventListener('click' , toggleDesktopMenu);
 
  function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
