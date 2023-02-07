db2cea06ec03579ca1f9232b7eba04e27798dcf5
Jorge Suárez <suarezjorge758@gmail.com>
2023-02-07 15:12:57 -0400

    clase 19

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuMobile = document.querySelector('.mobile-menu');
+const menuBurger = document.querySelector('.menu');
 
 menuEmail.addEventListener('click' , toggleDesktopMenu);
 
  function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+ }
+ 
+ menuBurger.addEventListener('click' , toggleMobileMenu);
+
+ function toggleMobileMenu() {
+    menuMobile.classList.toggle('inactive');
  }
