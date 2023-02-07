48e40cf4f5e3f163e797a577a8c779e92b7d674f
juanjoramos <jjramos@redesomnivida.org>
2023-02-07 16:38:54 -0500

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconmenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+iconmenu.addEventListener('click', toggleMobilepMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobilepMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
