678cf209c73a6e398b273a929a02158db50a9e08
micton-code <antho_ul@hotmail.com>
2023-02-06 20:14:50 -0400

    agregue la lista de productos, maquetando en JS el html de cada producto

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-// var desplegar menu al dar click al email
+// #01# var desplegar menu al dar click al email
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-// var Desplegar menu hamburguesa al dar click al icono 
+// #02# var Desplegar menu hamburguesa al dar click al icono 
 const burgerIcon = document.querySelector('.burgerIcon');
 const mobileMenu = document.querySelector('.mobile-menu');
-// var Desplegar menu carrito de compras al dar click al icono 
+
+// #03# var Desplegar menu carrito de compras al dar click al icono 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+// #04# var lista de productos 
+const cardsContainer = document.querySelector('.cards-container');
 
 
-
-// desplegar menu al dar click al email
+// #01# desplegar menu al dar click al email
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
@@ -29,7 +31,7 @@ console.log('js funcionando');
 
 
 
-// Desplegar menu hamburguesa al dar click al icono 
+// #02# Desplegar menu hamburguesa al dar click al icono 
 burgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
@@ -47,7 +49,7 @@ function toggleMobileMenu() {
 
 
 
-// Desplegar menu carrito de compras al dar click al icono 
+// #03# Desplegar menu carrito de compras al dar click al icono 
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleCarritoAside() {
@@ -64,3 +66,130 @@ function toggleCarritoAside() {
     
     aside.classList.toggle('inactive'); 
 }
+
+
+
+// #04# lista de productos 
+const productList = []; 
+        // dentro del Array vamos a agregar un objeto
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Reloj',
+    price: 50,
+    image: 'https://images.pexels.com/photos/210528/pexels-photo-210528.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+productList.push({
+    name: 'Vaso',
+    price: 2,
+    image: 'https://media.istockphoto.com/id/1183424538/es/foto/agua-que-vierte-en-el-vidrio.jpg?b=1&s=612x612&w=0&k=20&c=wwAqjo8wowpSQuggOtxFRW_Jhj1wU2nuvndeUmdb3_c=',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 70,
+    image: 'https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+productList.push({
+    name: 'Telefono',
+    price: 220,
+    image: 'https://images.pexels.com/photos/13039078/pexels-photo-13039078.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+productList.push({
+    name: 'Laptop',
+    price: 300,
+    image: 'https://images.pexels.com/photos/40185/mac-freelancer-macintosh-macbook-40185.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Reloj',
+    price: 50,
+    image: 'https://images.pexels.com/photos/210528/pexels-photo-210528.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+productList.push({
+    name: 'Vaso',
+    price: 2,
+    image: 'https://media.istockphoto.com/id/1183424538/es/foto/agua-que-vierte-en-el-vidrio.jpg?b=1&s=612x612&w=0&k=20&c=wwAqjo8wowpSQuggOtxFRW_Jhj1wU2nuvndeUmdb3_c=',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 70,
+    image: 'https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+productList.push({
+    name: 'Telefono',
+    price: 220,
+    image: 'https://images.pexels.com/photos/13039078/pexels-photo-13039078.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+productList.push({
+    name: 'Laptop',
+    price: 300,
+    image: 'https://images.pexels.com/photos/40185/mac-freelancer-macintosh-macbook-40185.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+        /*
+        <div class="product-card">
+                <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+                <div class="product-info">
+                  <div>
+                    <p>$120,00</p>
+                    <p>Bike</p>
+                  </div>
+                  <figure>
+                    <img src="./icons/bt_add_to_cart.svg" alt="">
+                  </figure>
+                </div>
+              </div>
+        */
+        
+        // DENTRO DE ESTE "FOR" vamos a recrear el html de cada producto
+            const productCard = document.createElement('div');
+            productCard.classList.add('product-card');
+            
+        
+            //para mdificar su propiedad src usamos  el siguiente codigo
+            //product = {name, price, image} -> product.image
+            const productImg = document.createElement('img');
+            productImg.setAttribute('src', product.image);
+        
+        
+            const productInfo = document.createElement('div');
+            productInfo.classList.add('product-info');
+            
+        
+            const productInfoDiv = document.createElement('div');
+            const productPrice = document.createElement('p');
+            productPrice.innerText = '$' + product.price;
+            const productName = document.createElement('p');
+            productName.innerText = product.name;
+            //NOTA: Luego de replicar el html integramos todos los elementros dentro de su contenedor padre
+            productInfoDiv.appendChild(productPrice);
+            productInfoDiv.appendChild(productName);
+        
+            const productInfoFigure = document.createElement('figure');
+            const productImgCart = document.createElement('img');
+            productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+            //NOTA: Luego de replicar el html integramos todos los elementros dentro de su contenedor padre
+            productInfoFigure.appendChild(productImgCart);
+        
+        
+            //NOTA: Luego de replicar el html integramos todos los elementros dentro de su contenedor padre
+            productInfo.appendChild(productInfoDiv);
+            productInfo.appendChild(productInfoFigure);
+            
+            productCard.appendChild(productImg);
+            productCard.appendChild(productInfo);
+        
+            cardsContainer.appendChild(productCard);
+        }
+}
+
+renderProducts(productList);
