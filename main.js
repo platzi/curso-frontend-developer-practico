620482236d035ebbfb30884a01aa13ce69f44603
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2023-02-06 21:29:01 -0600

    add shopping Cart view & interaction show/hide depending if other menus are open to just have one menu open

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenuIcon.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toogleshoppingCartAside);
 
 function toggleDesktopMenu(){
-
+    aside.classList.add('inactive');
     // The commented section was the challenge in the class and I did it in that way
 
     // if (desktopMenu.classList.contains('inactive')){
@@ -23,5 +26,12 @@ function toggleDesktopMenu(){
 }
 
 function toggleMobileMenu(){
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toogleshoppingCartAside(){
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
