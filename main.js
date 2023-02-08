381daff60f872f73b63796555dc04cb8d86cf163
[felipe] <[felipearara123@gmail.com]>
2023-02-07 19:51:21 -0500

    se realizo el trbajo al carrito en mobile con js

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuHamIcon = document.querySelector('.menu');
 const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu );
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+
 
 function toggleDesktopMenu(){
-   desktopMenu.classList.toggle('inactive');
+   const isAsideClosed = aside.classList.contains('inactive');
+
+   if (!isAsideClosed){
+      aside.classList.add('inactive');
+   }
+
+  desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+   const isAsideClosed = aside.classList.contains('inactive');
+
+   if (!isAsideClosed){
+      aside.classList.add('inactive');
+   }
+
      mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+if (!isMobileMenuClosed){
+   mobileMenu.classList.add('inactive');
+}
+
+   aside.classList.toggle('inactive');
 }
