cbea6478d82bb48fef46da32c99b7a7cc65bde6c
IvanEDLCP <ivanedlcp7@gmail.com>
2023-02-08 11:57:52 -0500

    Fusiono el navbar con el menu para mobile

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const hamburgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
+    console.log('Click Desktop Menu');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    console.log('Click Mobile Menu');
+    mobileMenu.classList.toggle('inactive');
 }
