c15e8124abe26a12468dbdd6578210b279483182
Adrian Coronel Mendoza <adrianonel78@gmail.com>
2023-02-08 15:04:41 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -1,20 +1,52 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+const iconFlechaProduc = document.querySelector('#icon-flechita-produc-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+iconFlechaProduc.addEventListener('click', toogleFlechaProduc);
 
 function toggleDesktopMenu()
 {
+  const isCarritoAsideOpen = aside.classList.contains('inactive');
+
+  if (!isCarritoAsideOpen)
+    aside.classList.toggle('inactive');
+
   // toggle('class') pone o quita la clase dependiendo si existe o no
   desktopMenu.classList.toggle('inactive');
+
+
 }
 
 function toggleMobileMenu()
 {
+  const isCarritoAsideOpen = aside.classList.contains('inactive');
+  
+  if (!isCarritoAsideOpen)
+  aside.classList.toggle('inactive');
+  
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside()
+{
+  const isMobileMenuOpen = mobileMenu.classList.contains('inactive');
+
+  if (!isMobileMenuOpen)
+    mobileMenu.classList.toggle('inactive');
+
+  aside.classList.toggle('inactive');
+}
+
+function toogleFlechaProduc()
+{
+  aside.classList.toggle('inactive');
 }
 
 
