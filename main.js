e9d0e58d3fc733e18468303030e337fe3168606c
Tob Stoic <rafaeltobass@gmail.com>
2023-02-08 18:54:51 -0500

    Renderizando html a  traves de javascript con la funcion renderproducts

--- main.js
+++ main.js
@@ -4,19 +4,94 @@ const hamburguerMenuButton = document.querySelector("#hamButton");
 const verticalMobileMenu = document.querySelector(".mobile-menu");
 const menuCart = document.querySelector(".navbar-shopping-cart");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
+const itemCard = document.querySelector(".product-card") //Cualquier articulo mostrado
+const productDetail = document.querySelector("#productDetail");
+const cardsContainer = document.querySelector(".cards-container");
+const productList = [];
+
+productList.push(
+    {
+        name: "Iphone 13",
+        price: 2900,
+        image: "https://images.pexels.com/photos/9667337/pexels-photo-9667337.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    }
+);
+
+productList.push(
+    {
+        name: "Samsung Galaxy s22",
+        price: 3450,
+        image: "https://images.pexels.com/photos/13780425/pexels-photo-13780425.jpeg",
+    }
+);
+
+productList.push(
+    {
+        name: "PowerBank",
+        price: 140,
+        image: "https://images.pexels.com/photos/4072683/pexels-photo-4072683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+);
+
+productList.push(
+    {
+        name: "Bike",
+        price: 120,
+        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    }
+);
+
+productList.push(
+    {
+        name: "Iphone 13",
+        price: 2900,
+        image: "https://images.pexels.com/photos/9667337/pexels-photo-9667337.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    }
+);
+
+productList.push(
+    {
+        name: "Micrófono Sehnheiser",
+        price: 2500,
+        image: "https://images.pexels.com/photos/347700/pexels-photo-347700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    }
+);
+
+productList.push(
+    {
+        name: "Amplificador",
+        price: 450,
+        image: "https://images.pexels.com/photos/1706694/pexels-photo-1706694.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    }
+);
+
+productList.push(
+    {
+        name: "Mouse",
+        price: 105,
+        image: "https://images.pexels.com/photos/5082573/pexels-photo-5082573.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    }
+);
 
 emailFromNavBar.addEventListener("click", toggleDropMenu);
 hamburguerMenuButton.addEventListener("click", toggleMobileMenu);
 menuCart.addEventListener("click", toggleMenuCart);
+//itemCard.addEventListener("click", toggleitemDetails)
+
 
 //-----------------------------FUNCIONES DE DESPLIEGUE DE MENU 
 function toggleMenuCart()
 {
     const  verticalMobileMenuClosed = verticalMobileMenu.classList.contains("inactive")
- 
+    const  productDetailClosed = productDetail.classList.contains("inactive")
     if (!verticalMobileMenuClosed) 
     {
         verticalMobileMenu.classList.add("inactive")
+    }
+
+    if (!productDetailClosed) 
+    {
+        productDetail.classList.add("inactive")
     }
 
     shoppingCartContainer.classList.toggle("inactive")
@@ -26,9 +101,15 @@ function toggleMenuCart()
 function toggleDropMenu ()
 {
     const asideClosed = shoppingCartContainer.classList.contains("inactive")
+    const  productDetailClosed = productDetail.classList.contains("inactive")
+
     if (!asideClosed) 
     {
         shoppingCartContainer.classList.toggle("inactive")
+    }
+    if (!productDetailClosed) 
+    {
+        productDetail.classList.add("inactive")
     }
     dropMenuFromEmail.classList.toggle("inactive")
 
@@ -43,59 +124,25 @@ function toggleMobileMenu ()
     }
     verticalMobileMenu.classList.toggle("inactive")
 }
+
+function toggleitemDetails()
+{
+
+}
 //-----------------------------------
-
-
-
-const productList = [];
-
-productList.push(
-    {
-        name: "Iphone 13",
-        price: 2900,
-        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    }
-);
-
-productList.push(
-    {
-        name: "Samsung Galaxy s22",
-        price: 3450,
-        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    }
-);
-
-productList.push(
-    {
-        name: "PowerBank",
-        price: 140,
-        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    }
-);
-
-productList.push(
-    {
-        name: "bike",
-        price: 120,
-        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    }
-);
-
-
-
-
 
 
 
 function renderProduct(arr)
 {
 
-    for (const product of arr) {
+    for (const product of arr) 
+    { // FUNCIÓN PARA RENDERIZAR CADA ELEMENTO DEL ARRAY EN UNA ESTRUCTURA DE HTML 
         const individualProductCard = document.createElement("div");
         individualProductCard.classList.add("product-card");
     
-        const img = document.createElement("img")
-        img.setAttribute("src", product.image)
+        const productImg = document.createElement("img")
+        productImg.setAttribute("src", product.image)
         
         const individualProductInfo = document.createElement("div");
         individualProductInfo.classList.add("product-info");
@@ -106,12 +153,29 @@ function renderProduct(arr)
         individualProductPrice.innerText = "$" + product.price
         const individualProductName = document.createElement("p");
         individualProductName.innerText = product.name
+
+        individualProductInfoInnerDiv.appendChild(individualProductPrice);
+        individualProductInfoInnerDiv.appendChild(individualProductName);
+
     
         const individualProductFigureTag = document.createElement("figure");
         const individualProductCartIcon = document.createElement("img")
-        img.setAttribute("src", "./icons/bt_add_to_cart.svg")
+        individualProductCartIcon.setAttribute("src", "./icons/bt_add_to_cart.svg")
     
-    
+        individualProductFigureTag.appendChild(individualProductCartIcon);    //ÍCONO DE CARRITO DENTRO DE LA ETIQUETA FIGHURE
+        
+        individualProductInfoInnerDiv.appendChild(individualProductPrice) // PRECIO Y NOMBRE DEL PRODUCTO DENTRO DE DIV CONTENEDOR 
+        individualProductInfoInnerDiv.appendChild(individualProductName)
+
+        individualProductInfo.appendChild(individualProductInfoInnerDiv)
+        individualProductInfo.appendChild(individualProductFigureTag)
+
+        individualProductCard.appendChild(productImg)
+        individualProductCard.appendChild(individualProductInfo)
+
+        cardsContainer.appendChild(individualProductCard)
     }
 
 }
+
+renderProduct(productList)
