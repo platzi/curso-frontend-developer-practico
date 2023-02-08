0779139767eae53818254e445a7ae668d7ac3680
Franci <tupy77@hotmail.com>
2023-02-08 20:05:45 +0100

    renderizado productos

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail')
 
+const cardsContainer = document.querySelector('.cards-container')
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
@@ -42,3 +44,63 @@ function toggleCarritoAside(){
     
     aside.classList.toggle('inactive')
 }
+
+const productList = [];
+productList.push({
+    name: 'Pantalla',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: 'Computadora',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+    
+        const productImg= document.createElement('img')
+        productImg.setAttribute('src', product.image)
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+    
+        const productInfoDiv = document.createElement('div')
+    
+        const productPrice = document.createElement('p')
+        productPrice.innerHTML = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerHTML = product.name;
+    
+        const productFigure = document.createElement('figure');
+    
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', "./icons/bt_add_to_cart.svg")
+    
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+        productFigure.appendChild(productImgCart)
+        
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productFigure)
+    
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+    
+        cardsContainer.appendChild(productCard)
+    } 
+    /*Con Element.append() podemos agregar varios nodos y texto mientras que con Element.appendChild() solo podemos agregar un nodo.
+
+        productInfoDiv.append(productPrice, productName) */
+}
+
+renderProducts(productList)
