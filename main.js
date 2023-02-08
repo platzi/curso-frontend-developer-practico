f87ea3289ea1076f8df72ab7af40034915d85b9e
Jorge Suárez <suarezjorge758@gmail.com>
2023-02-08 17:28:12 -0400

    clase 21 fin

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuMobile = document.querySelector('.mobile-menu');
 const menuBurger = document.querySelector('.menu');
 const menuCarrito = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click' , toggleDesktopMenu);
 menuBurger.addEventListener('click' , toggleMobileMenu);
@@ -42,3 +43,145 @@ function toggleMobileMenu() {
    
    aside.classList.toggle('inactive');
  }
+
+const productList = [];
+
+productList.push ({
+   name:'Bike',
+   price: 120.00 ,
+   image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+})
+
+productList.push ({
+   name:'Nintendo Switch',
+   price: 300.00 ,
+   image: "https://images.pexels.com/photos/4523030/pexels-photo-4523030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push ({
+   name:'TV',
+   price: 1696.99 ,
+   image: "https://images.pexels.com/photos/1682519/pexels-photo-1682519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+productList.push ({
+   name:'Bike',
+   price: 120.00 ,
+   image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+})
+
+productList.push ({
+   name:'Nintendo Switch',
+   price: 300.00 ,
+   image: "https://images.pexels.com/photos/4523030/pexels-photo-4523030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push ({
+   name:'TV',
+   price: 1696.99 ,
+   image: "https://images.pexels.com/photos/1682519/pexels-photo-1682519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+productList.push ({
+   name:'Bike',
+   price: 120.00 ,
+   image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+})
+
+productList.push ({
+   name:'Nintendo Switch',
+   price: 300.00 ,
+   image: "https://images.pexels.com/photos/4523030/pexels-photo-4523030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push ({
+   name:'TV',
+   price: 1696.99 ,
+   image: "https://images.pexels.com/photos/1682519/pexels-photo-1682519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+productList.push ({
+   name:'Bike',
+   price: 120.00 ,
+   image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+})
+
+productList.push ({
+   name:'Nintendo Switch',
+   price: 300.00 ,
+   image: "https://images.pexels.com/photos/4523030/pexels-photo-4523030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push ({
+   name:'TV',
+   price: 1696.99 ,
+   image: "https://images.pexels.com/photos/1682519/pexels-photo-1682519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+productList.push ({
+   name:'Bike',
+   price: 120.00 ,
+   image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+})
+
+productList.push ({
+   name:'Nintendo Switch',
+   price: 300.00 ,
+   image: "https://images.pexels.com/photos/4523030/pexels-photo-4523030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+productList.push ({
+   name:'TV',
+   price: 1696.99 ,
+   image: "https://images.pexels.com/photos/1682519/pexels-photo-1682519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+})
+
+
+
+// <div class="product-card">
+//                 <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+//                     alt="">
+//                 <div class="product-info">
+//                     <div>
+//                         <p>$120,00</p>
+//                         <p>Bike</p>
+//                     </div>
+//                     <figure>
+//                         <img src="./icons/bt_add_to_cart.svg" alt="">
+//                     </figure>
+//                 </div>
+//             </div>
+
+function renderProduct(arr){
+   for (product of arr) {
+   const productCard = document.createElement('div');
+   productCard.classList.add('product-card');
+
+   const productImg = document.createElement('img');
+   productImg.setAttribute('src', product.image);
+   
+   const productInfo = document.createElement('div');
+   productInfo.classList.add('product-info');
+   
+   const productInfoDiv = document.createElement('div');
+   
+   const productPrice = document.createElement('p');
+   productPrice.innerText = '$' + product.price;
+   const productName = document.createElement('p');
+   productName.innerText = product.name;
+   
+   productInfoDiv.append(productPrice, productName);
+   
+   const productInfoFigure = document.createElement('figure');
+   const productImgCart = document.createElement('img');
+   productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+   
+   productInfoFigure.append(productImgCart);
+
+   productInfo.append(productInfoDiv, productInfoFigure);
+
+   productCard.append(productImg , productInfo);
+   
+   cardsContainer.append(productCard);
+
+}
+}
+
+
+renderProduct(productList);
