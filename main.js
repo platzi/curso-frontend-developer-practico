99df0943b26c99e6a9fbfc659a22f32a3c10bcac
juanjoramos <jjramos@redesomnivida.org>
2023-02-08 14:52:45 -0500

    Interaccion entre todos los componentes

--- main.js
+++ main.js
@@ -1,43 +1,63 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const iconmenu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
-const cardsContainer = document.querySelector('.cards-container');
+const menuEmail=document.querySelector('.navbar-email');
+const menuHamIcon=document.querySelector('.menu');
+const menuCarritoIcon=document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon=document.querySelector('.product-detail-close')
+const desktopMenu=document.querySelector('.desktop-menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+const shoppingCartContainer=document.querySelector('#shoppingCartContainer');
+const productDetailContainer=document.querySelector('#productDetail');
+const cardsContainer=document.querySelector('.cards-container');
 
-
-menuEmail.addEventListener('click', toggleDesktopMenu);
-iconmenu.addEventListener('click', toggleMobilepMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
 }
 
-function toggleMobilepMenu() {
+function toggleMobileMenu() {
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        shoppingCartContainer.classList.add('inactive');
+    }
+
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle('inactive')
 }
 
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive')
+
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
+}
+
 function toggleMobilepMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
      // aside.classList.toggle('inactive')
 
      if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 //si el carrito esta abierto para que cuando abramos el menu de salir se cierre
 function toggleMobilepMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
      // aside.classList.toggle('inactive')
 
      if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -51,39 +71,33 @@ function toggleCarritoAside() {
      if (!ismobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive');
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
+    // aside.classList.toggle('inactive')
+
+    if (!isProductDetailClosed) {
+       productDetailContainer.classList.add('inactive');
+   }
+
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
 productList.push({
     name: 'Bike',
-    price: 123,
+    price: 100,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push ({
-    name: 'Pantalla',
-    price: 1200,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    name: 'Balanza cocina',
+    price: 30,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTNRld_jlI7JG4-Ft152Y-4tGd_rW_Twmyew&usqp=CAU'
 });
 productList.push ({
-    name: 'Compu',
-    price: 1600,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Bike',
-    price: 123,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-productList.push({
-    name: 'Bike',
-    price: 123,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-productList.push({
-    name: 'Bike',
-    price: 123,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name: 'Camara',
+    price: 300,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnnB0Rqo9h5IwZapmk8Elyf8zb2Cu7FvcXag&usqp=CAU'
 });
 
 /*
@@ -108,6 +122,7 @@ function renderProducts(arr) {
         // product = {name, price, image}
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
