71d4d28e84bb2868e40028a9bbd6b6bc934859cc
RoiFuries <roi.furies@gmail.com>
2023-02-08 19:11:12 -0600

    integration burger menu and fixed responsive querys

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+
 menuEmail.addEventListener('click', toggleDesktop)
 
 function toggleDesktop (){
-    console.log('clickk')
+    console.log('click')
     desktopMenu.classList.toggle('inactive');
+}
+
+const burger = document.querySelector('.menu')
+const movileMenu = document.querySelector('.mobile-menu')
+
+burger.addEventListener('click', toggleMovileMenu)
+
+function toggleMovileMenu(){
+    console.log('burger click')
+    movileMenu.classList.toggle('inactive')
 
 }
+
