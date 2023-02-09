d812caa47473514750a0c0e52026aacb1ac54266
Reylan Lugo <ClockR13@gmail.com>
2023-02-08 23:03:59 -0400

    Integramos el menu del carrito

--- exe.js
+++ exe.js
@@ -11,6 +11,23 @@ function toggleDesktopMenu() {
 const mobileicon = document.querySelector('.menu');
 const mobilemenu = document.querySelector(".mobile-menu");
 
+const shoppingicon = document.querySelector(".navbar-shopping-cart");
+const shoppingmenu = document.querySelector(".product-detail");
+
+
 mobileicon.addEventListener("click",()=>{
-    mobilemenu.toggleAttribute("hidden")
+    if (!shoppingmenu.hasAttribute("hidden")) {  
+        shoppingmenu.toggleAttribute("hidden");
+    } 
+    mobilemenu.toggleAttribute("hidden");
+});
+
+
+
+shoppingicon.addEventListener("click", ()=>{
+    if (!mobilemenu.hasAttribute("hidden")) {
+        mobilemenu.toggleAttribute("hidden");
+    }
+    shoppingmenu.toggleAttribute("hidden");
+     
 });
