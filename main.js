1d08a6e8128277ac41dd6b1133760ced3c71fd14
Franci <tupy77@hotmail.com>
2023-02-09 18:53:49 +0100

    Detalles de un producto

--- main.js
+++ main.js
@@ -3,32 +3,31 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail')
-
-const cardsContainer = document.querySelector('.cards-container')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+function toggleDesktopMenu() {
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if (!isAsideClosed) {
-        aside.classList.toggle('inactive')
-    }
-
-    desktopMenu.classList.toggle('inactive')
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive');
+  }
+  
+  desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+function toggleMobileMenu() {
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if (!isAsideClosed) {
-        aside.classList.toggle('inactive')
-    }
-
-    mobileMenu.classList.toggle('inactive')
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive'); 
+  }
+  
+  mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside(){
@@ -42,65 +41,65 @@ function toggleCarritoAside(){
         mobileMenu.classList.add('inactive')
     }
     
-    aside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
 }
 
 const productList = [];
 productList.push({
-    name: 'Pantalla',
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  name: 'Pantalla',
+  price: 220,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push({
-    name: 'Computadora',
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  name: 'Compu',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
-function renderProducts(arr){
-    for (product of arr){
-        const productCard = document.createElement('div')
-        productCard.classList.add('product-card')
-    
-        const productImg= document.createElement('img')
-        productImg.setAttribute('src', product.image)
-    
-        const productInfo = document.createElement('div')
-        productInfo.classList.add('product-info')
-    
-        const productInfoDiv = document.createElement('div')
-    
-        const productPrice = document.createElement('p')
-        productPrice.innerHTML = '$' + product.price;
-    
-        const productName = document.createElement('p');
-        productName.innerHTML = product.name;
-    
-        const productFigure = document.createElement('figure');
-    
-        const productImgCart = document.createElement('img')
-        productImgCart.setAttribute('src', "./icons/bt_add_to_cart.svg")
-    
-        productInfoDiv.appendChild(productPrice)
-        productInfoDiv.appendChild(productName)
-        productFigure.appendChild(productImgCart)
-        
-        productInfo.appendChild(productInfoDiv)
-        productInfo.appendChild(productFigure)
-    
-        productCard.appendChild(productImg)
-        productCard.appendChild(productInfo)
-    
-        cardsContainer.appendChild(productCard)
-    } 
-    /*Con Element.append() podemos agregar varios nodos y texto mientras que con Element.appendChild() solo podemos agregar un nodo.
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    // product= {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+      /*Con Element.append() podemos agregar varios nodos y texto mientras que con Element.appendChild() solo podemos agregar un nodo.
 
         productInfoDiv.append(productPrice, productName) */
 }
 
-renderProducts(productList)
+renderProducts(productList);
