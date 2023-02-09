4bc9ac6df31de053d0074f45f9857ce5694ff8d0
CHAMATRONIC <alejorm@gmail.com>
2023-02-09 08:21:59 -0300

    cart order added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const aside = document.querySelector('.product-detail');
 // mobile desktop
 const arrowMenuLeft = document.getElementById('email-arrow-menu');
 const desktopMenu = document.querySelector('.desktop-menu');
@@ -6,6 +7,11 @@ const mailAndArrowMenu = document.querySelector('.navbar-email');
 mailAndArrowMenu.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
+    const isAside = !aside.classList.contains('inactive');
+    if(isAside){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
     arrowMenuLeft.classList.toggle('arrow-down');
 }
@@ -17,5 +23,30 @@ const mobileMenu = document.querySelector('.mobile-menu');
 menuBurger.addEventListener('click',toggleMobileMenu);
 
 function toggleMobileMenu(){
+    const isAside = !aside.classList.contains('inactive');
+    if(isAside){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+//cart
+const cartBtn = document.querySelector('.navbar-shopping-cart');
+
+cartBtn.addEventListener('click',toggleCartAside)
+
+function toggleCartAside() {
+    const isMobileMenu = !mobileMenu.classList.contains('inactive');
+    const isDestopMenu = !desktopMenu.classList.contains('inactive');
+    
+    if(isMobileMenu){
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (isDestopMenu) {
+        desktopMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
 }
