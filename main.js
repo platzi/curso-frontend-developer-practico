99ebcbe6776907d8fd645ed1ff238fe90f897ac2
oskar656 <oscarjhenaoc@gmail.com>
2023-02-10 12:20:52 -0500

    Mobile menus added

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burgerMenuIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
 
     desktopMenu.classList.toggle("inactive");
+
+}
+
+
+function toggleMobileMenu() {
+
+    mobileMenu.classList.toggle("inactive");
 
 }
 
