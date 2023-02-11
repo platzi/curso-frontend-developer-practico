18d351d69ce9b267f90a8325e77f78fed62a41fd
Elkin Jaramillo <elkin.dev22@gmail.com>
2023-02-11 18:21:55 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -2,14 +2,36 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobilepMenu);
+menuCarIcon.addEventListener("click", toggleCarAside);
 
 function toggleDesktopMenu() {
-  //Si existe la clase inactive muestra el menu de la clase desktopMenu
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
 }
+
 function toggleMobilepMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
 }
