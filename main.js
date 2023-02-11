384c51d1459df5dceffb8181ff266525f24f0997
Elkin Jaramillo <elkin.dev22@gmail.com>
2023-02-11 00:18:19 -0500

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
-const destopMenu = document.querySelector(".desktop-menu");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobilepMenu);
 
 function toggleDesktopMenu() {
-  destopMenu.classList.toggle("inactive");
+  //Si existe la clase inactive muestra el menu de la clase desktopMenu
+  desktopMenu.classList.toggle("inactive");
+}
+function toggleMobilepMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
