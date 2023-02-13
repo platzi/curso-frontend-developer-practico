4f8ff9aaa4fd3d6997aa6f747d2cfa4decdbabec
ricardo940 <electronica0506@gmail.com>
2023-02-12 20:28:09 -0500

    Se agrego mobile-menu y asideshoppingCar y se hizo la logica toogle con js

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const carIcon = document.querySelector('.navbar-shopping-cart');
+const asideShoppingCar = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+menuHamIcon.addEventListener('click', toogleMobileMenu);
+carIcon.addEventListener('click',toogleAsideShoppingCar);
 
 function toogleDesktopMenu(){
+    asideShoppingCar.classList.add('inactive');
+
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu(){
+    desktopMenu.classList.add('inactive');
+    asideShoppingCar.classList.add('inactive');
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toogleAsideShoppingCar(){
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+
+    asideShoppingCar.classList.toggle('inactive');
 }
