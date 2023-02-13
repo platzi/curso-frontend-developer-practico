cbd66a680d79e2b0d492f502e2cde7a8c02a540b
Dario Viasus <darioviasus73@gmail.com>
2023-02-13 11:38:32 -0500

    se arreglo una parte del comentario del archivo main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Con esta logica hacemos que el menu aprezca y desaparezca cuando damos click sobre el correo, primero llamamos a los elementos HTML con el query selector y luego con el addEventListener los escuchamos y le decimos el evento y lo que debe hacer con la funcion que creamos
+//Con esta logica hacemos que el menu aparezca y desaparezca cuando damos click sobre el correo, primero llamamos a los elementos HTML con el query selector y luego con el addEventListener los escuchamos y le decimos el evento y lo que debe hacer con la funcion que creamos
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
