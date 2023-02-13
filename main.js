94e68d685c0019068c3b5ea1f16a79cbc5801d1d
Dario Viasus <darioviasus73@gmail.com>
2023-02-13 16:12:24 -0500

    Se completo de anclar el menu mobile y el navbar y ademas se trabajo en que fueran responsive

--- main.js
+++ main.js
@@ -2,10 +2,19 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburguerMenu.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
 
