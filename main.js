1daf87174eda5b10ad7d85ed612537087ca6c0ef
Dario Viasus <darioviasus73@gmail.com>
2023-02-13 18:02:45 -0500

    Se agrego el componente de aside para el carrito de compras y se agregaron unas lineas de logica pra que se ocultaran los diferentes menu desplegables al momento de que se hiciera click en cada uno de ellos

--- main.js
+++ main.js
@@ -3,18 +3,75 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hamburguerMenu = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamburguerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
+/*
+// esta es la forma que se plantea con condicionales
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');    
+    }
+
     desktopMenu.classList.toggle('inactive');
+
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');    
+    }
+
+    mobileMenu.classList.toggle('inactive');    
+
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');    
+    }
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');    
+    }
+
+    aside.classList.toggle('inactive');
+
+}*/
+
+// no es necesario aplicar la dependencia entre el mobileMenu y el desktopMenu ya que por los media query estos menus nunca estan al mismo tiempo.
+
+//Basicamente primero garantizo que el resto de menus esten ocultos para luego mostrar el menu que debe salir.
+
+function toggleDesktopMenu() 
+{
+    aside.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu()
+{
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
 
+function toggleCarritoAside()
+{
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
+}
