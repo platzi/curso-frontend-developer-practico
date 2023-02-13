54fb3c8cd43bf74f645bcaef30e5e13cdf9900b1
Pablo <pablo.david.vega1@gmail.com>
2023-02-12 21:11:51 -0500

    add js to menus in mediaqueries

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuMail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuMail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {    
     desktopMenu.classList.toggle('inactive');
+}
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {    
+    mobileMenu.classList.toggle('inactive');
 }
