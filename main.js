5d5dc1505da8bf1e475ef7ecadb78dc83dc415fb
Caortega9 <caortega.sk@gmail.com>
2023-02-14 16:59:50 -0500

    agregue carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+
 const burgerMenu = document.querySelector(".menu");
-const desktopMobile = document.querySelector(".mobile-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const shoppingCart = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenu.addEventListener("click", toggleMobileMenu);
+menuCartIcon.addEventListener("click", toggleProductDetail);
 
 function toggleDesktopMenu() {
+  shoppingCart.classList.add("inactive");
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  desktopMobile.classList.toggle("inactive");
+  shoppingCart.classList.add("inactive");
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleProductDetail() {
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
+  shoppingCart.classList.toggle("inactive");
 }
