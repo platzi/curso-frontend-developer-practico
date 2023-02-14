0ba00769431b2882777a7b6a02229fa031cab3b5
Santiago Triana <sgoth27@gmail.com>
2023-02-13 21:23:59 -0500

    se agrega componente de menu hamburguesa con ajuste de mediaquerys

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 let navbarEmail = document.querySelector('.navbar-email');
 let desktopMenu = document.querySelector('.desktop-menu');
+let hamburgerIcon = document.querySelector('.menu');
+let mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleMenu);
 
 function toggleMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+hamburgerIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
