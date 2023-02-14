32235f4df9da613af2445da8dc56d5c874355b37
ogat17 <ogat2011@gmail.com>
2023-02-14 13:43:21 -0500

    Clase JS incluir componentes menu mobile

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const mobileMenu = document.querySelector('.mobile-menu');
+const imgMenuMobile = document.querySelector('.menu');
+
 navEmail.addEventListener('click', toogleDesktopMenu)
 
 function toogleDesktopMenu(){
@@ -8,3 +11,15 @@ function toogleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
 
 }
+
+imgMenuMobile.addEventListener('click', toogleMobileMenu)
+
+function toogleMobileMenu(){
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+
+
+
+
