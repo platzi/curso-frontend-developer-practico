1aff44b026e14c009830f24b4d0b4f8537e0b11a
Caortega9 <caortega.sk@gmail.com>
2023-02-14 12:31:01 -0500

    fusion de mobile menu clase 8 con el index html

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".menu");
+const desktopMobile = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burgerMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  desktopMobile.classList.toggle("inactive");
 }
