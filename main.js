9b315ff213a8bd836dc054b76fe77c24b7e11d4a
JairMora21 <jairantonio21@hotmail.com>
2023-02-14 11:25:52 -0600

    Añadimos funcoanlidad en js al abrir el menu

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email')
 const deskopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 
 menuEmail.addEventListener('click', showDesktopMenu)
+menuHamIcon.addEventListener('click', toggleMobileMenu)
+
 
 function showDesktopMenu(){
 deskopMenu.classList.toggle('inactive')
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
+    }
