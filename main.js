9779bd3a44c6ee2464570dac7804a45be05438d0
Sebastian Jimenez <sebasjilo@hotmail.com>
2023-02-15 17:38:27 -0500

    Agregue el product detail que es un aside con la descripcion del producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuHamIcon = document.querySelector(".menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 
@@ -13,16 +13,16 @@ menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu () { 
     desktopMenu.classList.toggle("inactive");
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
 }
 
 function toggleMobileMenu () {
     mobileMenu.classList.toggle("inactive");
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
 }
 
 function toggleCarritoAside () {
-    aside.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
     desktopMenu.classList.add("inactive");
     mobileMenu.classList.add("inactive");
 }
