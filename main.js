1abdd77ebba0a2800611782fd22a453bbc4bc93d
adgarzon <angelduvan20@gmail.com>
2023-02-14 22:28:39 -0500

    fusion carrito de compras

--- main.js
+++ main.js
@@ -1,19 +1,45 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-
+const aside = document.querySelector(".product-detail")
 
 
 menuEmail.addEventListener("click",toggleDesktopMenu);
 menuHamIcon.addEventListener("click",toggleMobileMenu);
+menuCarritoIcon.addEventListener("click",toogleCarritoAside);
 
 
 function toggleDesktopMenu(){
-desktopMenu.classList.toggle("inactive")
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+    desktopMenu.classList.toggle("inactive")
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive")
+
+}
+
+function toogleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive");
+    }
+    
+    aside.classList.toggle("inactive");
+       
 
 }
