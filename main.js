6c708a59aed662d3dd0068288a067cb814a72c30
Delsart Ivan <ivandelsart@gmail.com>
2023-02-15 18:12:01 -0300

    clase 13 incorporada

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
 const desplegableMobileMenu = document.querySelector('.mobile-menu');
-const bottonHamMobileMenu = document.querySelector('.menu')
+const bottonHamMobileMenu = document.querySelector('.menu');
+
+const navbarShoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+
 navbarEmail.addEventListener('click', ()=>{
     desktopMenu.classList.toggle('showActive')
+    productDetail.classList.add('showActive');
 });
 bottonHamMobileMenu.addEventListener('click', ()=>{
     desplegableMobileMenu.classList.toggle('showActive')
+    productDetail.classList.add('showActive');
+})
+
+navbarShoppingCart.addEventListener('click', () => {
+    productDetail.classList.toggle('showActive');
+    desplegableMobileMenu.classList.add('showActive')
 })
 
