6447a3f3daea0902b0695bcf12a2de022658e28f
nestorrg18 <neriga10@hotmail.com>
2023-02-14 19:25:26 -0600

    Clase 19: Fusión del menú en mobile

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burguerIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
+menuEmail.addEventListener("click", ()=> {blockOrNone(desktopMenu)});
+burguerIcon.addEventListener("click", ()=> {blockOrNone(mobileMenu)});
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
+function blockOrNone(element) {//https://platzi.com/comentario/3856000/
+    element.classList.toggle("inactive")
 }
