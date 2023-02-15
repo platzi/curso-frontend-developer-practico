d6e735a2ad75ef48e9ab4e5b98510f286e0a8599
graffyterlaptop <alejorpo97@gmail.com>
2023-02-15 12:22:57 -0500

    terminamos el despliegue del la cuenta en desk y en mobile y añadimos el despliegue a al carrito falta la version mobile del carrito LT

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email')
 const DesktopMenu = document.querySelector('.desktop-menu')
+const menuhambur = document.querySelector('.menu')
+const MobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuhambur.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
     DesktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    MobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside(){
+    aside.classList.toggle('inactive')
 }
