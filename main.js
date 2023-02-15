2cadddf8fa36e2bb7a054908aa19c7202d3f6c1d
David R <davidalexrodriguez@hotmail.com>
2023-02-14 19:13:15 -0600

    Agregar el menu de carrito de compras

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burguerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burguerMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
@@ -12,4 +15,10 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
+  aside.classList.add("inactive");
+}
+
+function toggleCarritoMenu() {
+  aside.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
 }
