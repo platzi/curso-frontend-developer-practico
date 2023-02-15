8b1d0dc027651437f73f9320a7b4b3b8c0efb155
Delsart Ivan <ivandelsart@gmail.com>
2023-02-14 22:58:00 -0300

    clase11y7

---
+++ main.js
@@ -0,0 +1,7 @@
+const navbarEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+navbarEmail.addEventListener('click', ()=>{
+    desktopMenu.classList.toggle('showActive')
+});
+
