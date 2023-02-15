ad8cd99b2b03dfa5f49b03aa483fbd2f949c658a
Delsart Ivan <ivandelsart@gmail.com>
2023-02-15 00:34:54 -0300

    integrando clase 8

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const desplegableMobileMenu = document.querySelector('.mobile-menu');
+const bottonHamMobileMenu = document.querySelector('.menu')
 navbarEmail.addEventListener('click', ()=>{
     desktopMenu.classList.toggle('showActive')
 });
+bottonHamMobileMenu.addEventListener('click', ()=>{
+    desplegableMobileMenu.classList.toggle('showActive')
+})
 
