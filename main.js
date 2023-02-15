e62fcd422ded830427bd4acf48b93ef7d870bbe3
DiegoMP34 <diegomarcosportugal7@gmail.com>
2023-02-14 22:08:19 -0500

    se inserto el menu para mobile y su funcionalidad toggle, y se modifico las media queries

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+//mobile menu
+const burguerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', function(){
     desktopMenu.classList.toggle('inactive')
 })
+
+//mobile menu
+burguerMenu.addEventListener('click', function(){
+    mobileMenu.classList.toggle('inactive')
+})
+
