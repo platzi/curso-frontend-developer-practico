caeaa38fcb30437387ddf9f8251ef654f9dfbe34
Delsart Ivan <ivandelsart@gmail.com>
2023-02-16 12:51:06 -0300

    clase 6 implementada

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const bottonHamMobileMenu = document.querySelector('.menu');
 const navbarShoppingCart = document.querySelector('.navbar-shopping-cart');
 const productDetail = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container');
 navbarEmail.addEventListener('click', ()=>{
     desktopMenu.classList.toggle('showActive')
     productDetail.classList.add('showActive');
@@ -21,3 +22,71 @@ navbarShoppingCart.addEventListener('click', () => {
     desplegableMobileMenu.classList.add('showActive')
 })
 
+
+// ------- AGREGANDO PRODUCTOS ----------
+const productList = [];
+
+productList.push({
+    name: 'bicicleta',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'bicicleta',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'bicicleta',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'pc',
+    price: 120,
+    image: 'https://www.venex.com.ar/products_images/1659379310_1656441054_ew.jpg'
+})
+
+
+
+function productsRender(arr){
+    for(products of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', products.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = products.price;
+        const productName = document.createElement('p');
+
+        const productFigure = document.createElement('figure');
+        const productFigureImg = document.createElement('img');
+        productFigureImg.setAttribute('src', "./icons/bt_add_to_cart.svg");
+
+        productName.innerText = products.name;
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        productFigure.appendChild(productFigureImg);
+
+        cardsContainer.appendChild(productCard);
+
+      };
+}
+
+productsRender(productList);
