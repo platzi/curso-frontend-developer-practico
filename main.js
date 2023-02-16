06e25185e28a73f69556f7d005d48198ed22256e
Pablo Fajardo <pablofajardoruano@gmail.com>
2023-02-16 18:31:22 -0500

    Hice un aside q nos sirve para varias etiquetas del mismo tipo

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu ");
 const carritoIcon = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -12,16 +12,16 @@ carritoIcon.addEventListener("click", toggleAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
-  aside.classList.add("inactive");
+  shoppingCartContainer.classList.add("inactive");
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
-  aside.classList.add("inactive");
+  shoppingCartContainer.classList.add("inactive");
 }
 
 function toggleAside() {
-  aside.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
   mobileMenu.classList.add("inactive");
   desktopMenu.classList.add("inactive");
 }
