ea4e5ca8fd39d6ef6ce6bc48b397115986bdf3e9
Pablo Fajardo <pablofajardoruano@gmail.com>
2023-02-16 16:59:15 -0500

    Agregamos un array para los productos e hicimos las etiquetas desde javascript

--- main.js
+++ main.js
@@ -1,9 +1,112 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu ");
+const carritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+const cardContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+carritoIcon.addEventListener("click", toggleAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+  aside.classList.add("inactive");
 }
-console.log("no me funciona");
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+  aside.classList.add("inactive");
+}
+
+function toggleAside() {
+  aside.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
+}
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Bicycle helmet",
+  price: 1200,
+  image:
+    "https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto",
+});
+productList.push({
+  name: "Bicycle helmet",
+  price: 1600,
+  image: "https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg",
+});
+productList.push({
+  name: "Bicycle helmet",
+  price: 1500,
+  image:
+    "https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto",
+});
+productList.push({
+  name: "Seat",
+  price: 300,
+  image: "https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg",
+});
+productList.push({
+  name: "Tennis Montain Bike",
+  price: 2200,
+  image:
+    "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg",
+});
+productList.push({
+  name: "Sunglasses",
+  price: 800,
+  image:
+    "https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602",
+});
+productList.push({
+  name: "Sunglasses",
+  price: 600,
+  image:
+    "https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603",
+});
+productList.push({
+  name: "Bicycle seat bag",
+  price: 876,
+  image: "https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg",
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const producImage = document.createElement("img");
+    producImage.setAttribute("src", product.image);
+    const producInfo = document.createElement("div");
+    producInfo.classList.add("product-info");
+
+    const producInfoDiv = document.createElement("div");
+
+    const producPrice = document.createElement("p");
+    producPrice.innerText = "$" + product.price;
+    const producName = document.createElement("p");
+    producName.innerText = product.name;
+
+    const producInfoFigure = document.createElement("figure");
+
+    const imgClosed = document.createElement("img");
+    imgClosed.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    cardContainer.appendChild(productCard);
+    productCard.append(producImage, producInfo);
+    producInfo.append(producInfoDiv, producInfoFigure);
+    producInfoDiv.append(producPrice, producName);
+    producInfoFigure.appendChild(imgClosed);
+  }
+}
+
+renderProducts(productList);
