19748fbc463de004e44cca9e3cece340c2d10cf5
Mayra Denhoff <mayradenhoff@gmail.com>
2023-02-17 12:22:34 -0300

    Se agrego funcion del menú

---
+++ main.js
@@ -0,0 +1,9 @@
+const navEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu")
+
+navEmail.addEventListener("click", toggleDesktopMenu) 
+
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle("inactive")
+}
