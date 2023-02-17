b83d66e7631a8f50d8dd4b81782080e6cf8439fd
DiegoMP34 <diegomarcosportugal7@gmail.com>
2023-02-17 13:05:25 -0500

    Agregamos funcionalidad a mobile menu y productDetail, mostrando solo uno de ellos sin que se sobre pongan

--- main.js
+++ main.js
@@ -9,24 +9,36 @@ const productDetail = document.querySelector('.product-detail')
 
 //Eventos
 menuEmail.addEventListener('click', function(){
+    const isProductDetailOpen = !productDetail.classList.contains('inactive')
     desktopMenu.classList.toggle('inactive')
-    //hide desktopmenu when click on product detail
-    if (!productDetail.classList.contains('inactive')) {
+    //hide product detail when click on desktopmenu
+    if (isProductDetailOpen) {
         productDetail.classList.add('inactive')
     }
+
 })
 
 // mobile menu
 burguerMenu.addEventListener('click', function(){
+    const isProductDetailOpen = !productDetail.classList.contains('inactive')
     mobileMenu.classList.toggle('inactive')
+    //hide product detail when click on mobile menu
+    if (isProductDetailOpen) {
+        productDetail.classList.add('inactive')
+    }
 })
 
 // show products detail
 shoppingBasket.addEventListener('click', function(){
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive')
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive')
     productDetail.classList.toggle('inactive')
-    //hide product detail when click on desktopmenu
-    if (!desktopMenu.classList.contains('inactive')) {
+    //hide desktopmenul when click on product detail
+    if (isDesktopMenuOpen) {
         desktopMenu.classList.add('inactive')
+    }
+    if (isMobileMenuOpen) {
+        mobileMenu.classList.add('inactive')
     }
 })
 
