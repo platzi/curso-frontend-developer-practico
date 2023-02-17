844c367a4bf1065fe12ddb75f1c46f48079d4ca7
BorDevle <edgareddborja@gmail.com>
2023-02-16 21:11:13 -0500

    se icorpora la parte lateral izquierda de las opciones del nav

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+//varibles// 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+// events
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
 
+
+// functions
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
