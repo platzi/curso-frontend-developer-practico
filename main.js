ad199546db1461b1d9ea738a2cb696f594e746b3
Cajero de Cambio Sunrise <cajacambiomps@palace-resorts.local>
2023-02-17 15:50:17 -0500

    Se agrega el menu en mobile, y se configuran los media quieries

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const menu = document.querySelector('.desktop-menu');
+const desktop_menu = document.querySelector('.desktop-menu');
 const navbar_email = document.querySelector('.navbar-email');
+const mobile_menu = document.querySelector('.mobile-menu');
+const icon_mobile_menu = document.querySelector('.menu');
 
 navbar_email.addEventListener('click', clickEmail);
+icon_mobile_menu.addEventListener('click', clickIconMenu);
 
 function clickEmail(){
-    menu.classList.toggle('inactive');
+    desktop_menu.classList.toggle('inactive');
+}
+
+function clickIconMenu(){
+    mobile_menu.classList.toggle('inactive');
+    console.log("OK");
 }
