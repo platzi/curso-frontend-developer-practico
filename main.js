7a02a5839c20adbcc438feb2c926010dcb7e5610
Jose Luis <jose.lmy2004@gmail.com>
2023-02-17 15:00:06 -0500

    Se unió el archivo del menu para mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+const burguerIcon = document.querySelector(".menu")
 
-menuEmail.addEventListener("click", toggDesktopMenu)
+menuEmail.addEventListener("click", toggleDesktopMenu)
+burguerIcon.addEventListener("click", toggleMobileMenu)
 
-function toggDesktopMenu(){
+function toggleDesktopMenu(){
     desktopMenu.classList.toggle("ver")
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("ver")
+    console.log(mobileMenu)
 }
