136a7f0fad6924d62d86923792c12c4461bb0c3a
Luis Manuel Martinez <luismtgg@gmail.com>
2023-02-18 15:55:21 -0600

    Añadí la lista de productos desde javascript para evitar el uso de HTML

---
+++ main.js
@@ -0,0 +1,116 @@
+const navEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+
+const iconMobile = document.querySelector(".menu");
+const menuMobile = document.querySelector(".mobile-menu");
+
+const shoppingCartIcon = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
+
+const cardsContainer = document.querySelector(".cards-container");
+
+
+navEmail.addEventListener("click", showDesktopmenu);
+function showDesktopmenu(){
+    const isProductDetailClose = productDetail.classList.contains("inactive");
+    if(!isProductDetailClose){
+        productDetail.classList.add("inactive");
+    }
+    desktopMenu.classList.toggle("inactive");
+}
+
+iconMobile.addEventListener("click", showMobileMenu);
+
+function showMobileMenu(){
+    const isProductDetailClose = productDetail.classList.contains("inactive");
+    if(!isProductDetailClose){
+        productDetail.classList.add("inactive");
+    }
+    menuMobile.classList.toggle("inactive");
+}
+
+shoppingCartIcon.addEventListener("click", showProductDetail);
+
+function showProductDetail(){
+    const isMobileMenuclose = menuMobile.classList.contains("inactive");
+    const isDesktopclose = desktopMenu.classList.contains("inactive")
+    if(!isMobileMenuclose){
+        menuMobile.classList.add("inactive")
+    }
+    if(!isDesktopclose){
+        desktopMenu.classList.add("inactive")
+    }
+    productDetail.classList.toggle("inactive")
+}
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"});
+productList.push({
+    name: "Pantalla",
+    price: 3000,
+    image: 'https://www.lg.com/mx/images/televisores/md07531108/gallery/D-01.jpg',
+});
+productList.push({
+    name: "Laptop",
+    price: 15000,
+    image: 'https://m.media-amazon.com/images/I/71rmymgVWVL._AC_UF894,1000_QL80_.jpgcir',
+});
+
+
+ /*<section class="main-container">
+<div class="cards-container">
+
+  <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+      <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+      </div>
+      <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+      </figure>
+    </div>
+  </div>
+</section> */
+
+for(product of productList){
+const productCard = document.createElement("div");
+productCard.classList.add("product-card");
+
+const ProductImg = document.createElement("img");
+ProductImg.setAttribute('src', product.image);
+
+const productInfo = document.createElement("div");
+productInfo.classList.add("product-info");
+
+
+
+const productInfoDiv = document.createElement("div");
+
+const productPrice = document.createElement("p");
+productPrice.innerText = "$ " + product.price;
+const productName = document.createElement("p");
+productName.innerText = product.name;
+
+productInfoDiv.appendChild(productPrice);
+productInfoDiv.appendChild(productName);
+
+const productInfoFigure = document.createElement("Figure");
+const productImgCart = document.createElement("img");
+productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg" )
+
+productInfoFigure.appendChild(productImgCart)
+
+productInfo.appendChild(productInfoDiv);
+productInfo.appendChild(productInfoFigure);
+
+productCard.appendChild(ProductImg);
+productCard.appendChild(productInfo);
+
+cardsContainer.appendChild(productCard);
+
+}
