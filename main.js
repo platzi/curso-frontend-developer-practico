95edbbec744336fdb701e5dff67ecb1d6cd79ce9
Cajero de Cambio Nizuc <cajacambiompn@palace-resorts.local>
2023-02-18 16:30:27 -0500

    Se agregó Carrito de compras

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 const desktop_menu = document.querySelector('.desktop-menu');
 const navbar_email = document.querySelector('.navbar-email');
+const navbar_cart = document.querySelector('.navbar-shopping-cart');
 const mobile_menu = document.querySelector('.mobile-menu');
 const icon_mobile_menu = document.querySelector('.menu');
+const aside = document.querySelector('.product-detail');
 
 navbar_email.addEventListener('click', clickEmail);
 icon_mobile_menu.addEventListener('click', clickIconMenu);
+navbar_cart.addEventListener('click', clickIconCart);
 
 function clickEmail(){
+    isEnable(false, aside);
     desktop_menu.classList.toggle('inactive');
 }
 
 function clickIconMenu(){
+    isEnable(false, aside);
     mobile_menu.classList.toggle('inactive');
-    console.log("OK");
+}
+
+function clickIconCart(){
+    isEnable(false, mobile_menu);
+    isEnable(false, desktop_menu);
+    aside.classList.toggle('inactive');
+}
+
+function isEnable(status, item){
+    if (status==true){
+        item.classList.remove('inactive');
+    } else {
+        item.classList.add('inactive');
+    }
 }
