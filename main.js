75e2d2fc70d973d497a990cde41a2814bb45ad40
Victor Hugo <viccorrt@gmail.com>
2023-02-18 01:09:51 -0800

    Agregamos el componente de la clase6.html el cual consiste en mostranos una lista de productos, con una imagen, nombre y precio del producto. Esto se hizo maquetando html desde javascript, creando la lista sin necesidad de repetir demasiado html para cada articulo.

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const asideCarritoIcon = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoIcon);
@@ -50,3 +52,90 @@ function toggleCarritoIcon() {
   mobileMenu.classList.add('inactive');
   asideCarritoIcon.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Pantalla',
+  price: 220,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: 'Computadora',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    // product = {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+  
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$ ' + product.price;
+    
+    // Cuando usamos append para ingresar varios nodos y no solo appendChild que ingresa un solo nodo
+    // Mejor opcion de desarrollo
+    productInfoDiv.append(productName, productPrice);
+    // productInfoDiv.appendChild(productName);
+    // productInfoDiv.appendChild(productPrice);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+    
+    // Mejor opcion de desarrollo
+    productInfo.append(productInfoDiv, productInfoFigure);
+    // productInfo.appendChild(productInfoDiv);
+    // productInfo.appendChild(productInfoFigure);
+  
+    // Mejor opcion de desarrollo
+    productCard.append(productImg, productInfo);
+    // productCard.appendChild(productImg);
+    // productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+// funcion alternativa
+function renderProductsAlt(arr) {
+  for (product of arr) {
+    const cardsProduct = `
+      <div class="product-card">
+        <img src=${product.image} alt="">
+        <div class="product-info">
+          <div>
+            <p>${product.name}</p>
+            <p>$${product.price}</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>`
+    cardsContainer.innerHTML += cardsProduct;
+  }
+}
+
+renderProducts(productList);
+
+// renderProductsAlt(productList);
+
