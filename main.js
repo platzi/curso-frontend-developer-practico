3972507a3dd35ed4f3b8759267c04697fc75a47f
Jose Luis <jose.lmy2004@gmail.com>
2023-02-17 20:56:13 -0500

    Se creó la funcion renderProducts y se guardo en esta el cicloo for creado anteriormente para los productos.

--- main.js
+++ main.js
@@ -47,35 +47,39 @@ productList.push({
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 })
 
-for(product of productList){
-    const productCard = document.createElement("div")
-    productCard.classList.add("product-card")
-
-    const productImg = document.createElement("img")
-    productImg.setAttribute("src", product.image)
-
-    const productInfo = document.createElement("div")
-    productInfo.classList.add("product-info")
-
-    const productInfoDiv = document.createElement("div")
-
-    const productInfoPrice = document.createElement("p")
-    productInfoPrice.innerText = "$" + product.price
-    const productInfoName = document.createElement("p")
-    productInfoName.innerText = product.name
-
-    const productInfoFigure = document.createElement("figure")
-    const productImgCart = document.createElement("img")
-    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
-
-    productCard.appendChild(productImg)
-    productInfoFigure.appendChild(productImgCart)
-    productInfo.append(productInfoDiv, productInfoFigure)
-    productInfoDiv.append(productInfoPrice, productInfoName)
-    productCard.append(productInfo)
-
-    cardsContainer.appendChild(productCard)
+function renderProducts(arr) {
+    for(product of productList){
+        const productCard = document.createElement("div")
+        productCard.classList.add("product-card")
+    
+        const productImg = document.createElement("img")
+        productImg.setAttribute("src", product.image)
+    
+        const productInfo = document.createElement("div")
+        productInfo.classList.add("product-info")
+    
+        const productInfoDiv = document.createElement("div")
+    
+        const productInfoPrice = document.createElement("p")
+        productInfoPrice.innerText = "$" + product.price
+        const productInfoName = document.createElement("p")
+        productInfoName.innerText = product.name
+    
+        const productInfoFigure = document.createElement("figure")
+        const productImgCart = document.createElement("img")
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
+    
+        productCard.appendChild(productImg)
+        productInfoFigure.appendChild(productImgCart)
+        productInfo.append(productInfoDiv, productInfoFigure)
+        productInfoDiv.append(productInfoPrice, productInfoName)
+        productCard.append(productInfo)
+    
+        cardsContainer.appendChild(productCard)
+    }
 }
+
+renderProducts()
 
 // <div class="product-card">
         //   <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
