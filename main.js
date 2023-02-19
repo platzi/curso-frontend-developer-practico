f0566c5f86196f9e221fa71ac977d17e3a05b672
juankaruna <juankaruna@gmail.com>
2023-02-18 18:40:22 -0600

    Grid of products added to index, Products created & rendered from JS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const iconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cartIcon = document.querySelector('.navbar-shopping-cart');
 const cartProductDetail = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 iconMenu.addEventListener('click', toggleMobileMenu);
@@ -42,3 +43,54 @@ function toggleCartProductDetail(){
 
     cartProductDetail.classList.toggle('inactive');
 }
+
+const productList = [];
+
+/*Fill the product list whit examples*/
+function CreateProducts(num){
+    for(var i = 0; i < num; i++){
+        productList.push({
+            name: 'bike',
+            price: 120,
+            image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        });
+    }
+}
+
+/*Create the products cards*/
+function renderProducts(arr){
+    for(let product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImage = document.createElement('img');
+        productImage.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        const productInfoFigure = document.createElement('figure');
+    
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+        productInfoDiv.append(productPrice,productName);
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        productCard.appendChild(productImage);
+        productCard.appendChild(productInfo);
+        cardsContainer.appendChild(productCard);
+    
+    }
+}
+
+CreateProducts(10);
+renderProducts(productList);
