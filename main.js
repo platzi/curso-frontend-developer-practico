b1e123a35885543424ca5e883083058be186b6e6
manuelcamacho <manuelcamacho.developer@gmail.com>
2023-02-19 12:53:16 -0800

    se fusionó menuMobile y se agregaron media querys para mostrar/ocultar menus

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurguer = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurguer.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
