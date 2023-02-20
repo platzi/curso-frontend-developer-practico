464bbcd6b694d24d07a33d2e2d79eac0ebf70ce9
MaGpeGarciaCastellanos <lupita_castellanos12@hotmail.com>
2023-02-20 10:53:04 -0600

    Agregando js para ocultar elementos y asi no se encimen unos con otros

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 const menuEmail=document.querySelector('.navbar-email');
-const desktopMenu=document.querySelector('.desktop-menu')
+const desktopMenu=document.querySelector('.desktop-menu');
+const burguerMenu=document.querySelector('.menu')
+const MenuCarritoIcon=document.querySelector('.navbar-shopping-cart')
+const mobileMenu=document.querySelector('.mobile-menu')
+const aside=document.querySelector('.product-detail')
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu)
+burguerMenu.addEventListener('click', toggleMobileMenu)
+MenuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 
 function toggleDesktopMenu(){
+    const isAsideClosed=aside.classList.contains('inactive')
+
+   if(!isAsideClosed){
+    aside.classList.add('inactive');
+   }
     desktopMenu.classList.toggle('inactive')
+}
+
+
+function toggleMobileMenu(){
+    const isAsideClosed=aside.classList.contains('inactive')
+
+   if(!isAsideClosed){
+    aside.classList.add('inactive');
+   }
+   
+    
+    mobileMenu.classList.toggle('inactive')
+}
+
+
+function toggleCarritoAside(){
+    const ismobileMenuClosed=mobileMenu.classList.contains('inactive')
+    
+    // aside.classList.toggle('inactive')
+
+   if(!ismobileMenuClosed){
+    mobileMenu.classList.add('inactive');
+   }
+   
+    aside.classList.toggle('inactive')
+   
 }
