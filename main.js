bada9686ece4256d50a1202abe513e88d3a661dc
duvanpalomino <duvanpalomino@gmail.com>
2023-02-20 17:30:56 -0500

    Fusion de carrito de compra con los componentes que llevamos

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
 const menuEmail = document.querySelector('.navbar-email');
-const deskTopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const deskTopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     deskTopMenu.classList.toggle('inactive');
+
 };
 
 function toggleMobileMenu() {
+
+    const isAsideClosed = aside.classList.contains('inactive'); 
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+
+};
+
+function toggleCarritoAside() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = deskTopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!isDesktopMenuClosed) {
+        deskTopMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
 };
