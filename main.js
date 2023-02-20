8f9adee804d54ad09635e9cad8995820c4dc78c0
duvanpalomino <duvanpalomino@gmail.com>
2023-02-20 15:03:00 -0500

    Fusion del menu en decktop

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuEmail = document.querySelector('.navbar-email');
+const deskTopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    deskTopMenu.classList.toggle('inactive');
+};
