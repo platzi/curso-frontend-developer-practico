4c333d2275597742bb8b02c62e6dcda3642084c8
santodev <dseosantos76@gmail.com>
2023-02-20 19:27:21 -0400

    carrito de compras

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const asideProduct = document.querySelector('.product-detail');
 
 // desplegar desktopMenu al hacer click a navbarEmai
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 // mobile menu
 burgerMenu.addEventListener('click', toggleMobilepMenu);
+// aside
+menuCarritoIcon.addEventListener('click', togglemenuCarritoIcon);
 
 function toggleDesktopMenu(event){
-    // metodo de JuanDC
+    const isAsideClosed = asideProduct.classList.contains('inactive');
+    if(!isAsideClosed){
+        asideProduct.classList.add('inactive')
+    } 
     desktopMenu.classList.toggle('inactive')
-    // metodo de stackOverflow
-    // desktopMenu.style = 'display:block'
 }
 
 function toggleMobilepMenu(){
+    const isAsideClosed = asideProduct.classList.contains('inactive');
+    if(!isAsideClosed){
+        asideProduct.classList.add('inactive')
+    } 
+
     mobileMenu.classList.toggle('inactive')
+}
+
+function togglemenuCarritoIcon(){
+    const isMenuMobileClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMenuMobileClosed){
+        mobileMenu.classList.add('inactive')
+    } 
+    desktopMenu.classList.add('inactive')    
+    asideProduct.classList.toggle('inactive');
 }
