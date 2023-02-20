27d589fa981395441f6eb53acf80b8c8412dd3fb
santodev <dseosantos76@gmail.com>
2023-02-20 13:12:00 -0400

    fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 // desplegar desktopMenu al hacer click a navbarEmai
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+// mobile menu
+burgerMenu.addEventListener('click', toggleMobilepMenu);
 
 function toggleDesktopMenu(event){
     // metodo de JuanDC
     desktopMenu.classList.toggle('inactive')
     // metodo de stackOverflow
     // desktopMenu.style = 'display:block'
+}
+
+function toggleMobilepMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
