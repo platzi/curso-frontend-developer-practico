c3f04927885173e158f08767899c89462528899f
duvanpalomino <duvanpalomino@gmail.com>
2023-02-20 15:34:18 -0500

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const deskTopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu() {
     deskTopMenu.classList.toggle('inactive');
+};
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 };
