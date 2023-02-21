f913d411be94cae2f6254d2af64a3ce62649c231
Walter Miranda <mirandawalter100@gmail.com>
2023-02-20 21:52:59 -0600

    Cambio de responsibe a menu telefono

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHmIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHmIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
