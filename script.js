2ebc7a61cebea0bd25cfd55715d4fe6a545a3dcf
MaGpeGarciaCastellanos <lupita_castellanos12@hotmail.com>
2023-02-20 22:28:24 -0600

    Acomodando las clases de detalle de producto

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const desktopMenu=document.querySelector('.desktop-menu');
 const burguerMenu=document.querySelector('.menu')
 const MenuCarritoIcon=document.querySelector('.navbar-shopping-cart')
 const mobileMenu=document.querySelector('.mobile-menu')
-const aside=document.querySelector('.product-detail')
+const shoppingCartContainer=document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container') 
 
 
@@ -13,20 +13,20 @@ MenuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 
 function toggleDesktopMenu(){
-    const isAsideClosed=aside.classList.contains('inactive')
+    const isAsideClosed=shoppingCartContainer.classList.contains('inactive')
 
    if(!isAsideClosed){
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
    }
     desktopMenu.classList.toggle('inactive')
 }
 
 
 function toggleMobileMenu(){
-    const isAsideClosed=aside.classList.contains('inactive')
+    const isAsideClosed=shoppingCartContainer.classList.contains('inactive')
 
    if(!isAsideClosed){
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
    }
    
     
@@ -43,7 +43,7 @@ function toggleCarritoAside(){
     mobileMenu.classList.add('inactive');
    }
    
-    aside.classList.toggle('inactive')
+   shoppingCartContainer.classList.toggle('inactive')
    
 }
 
