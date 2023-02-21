6b59411f0a73d9d8e974ee6c6e347283d61030e7
Yínerson Urina <yi.ur@hotmail.com>
2023-02-21 18:39:06 -0500

    Se añadió lista de productos por medio de un array en JS

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const burguerMenu =$('.menu');
 const mobileMenu =$('.mobile-menu');
 const menuCart =$('.navbar-shopping-cart');
 const productNav =$('.product-detail');
+const cardsContainer =$('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -28,5 +29,54 @@ function toggleproductNav() {
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'TV',
+    price: '1000',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: "Xbox Series X",
+    price: '500',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+        
+        productInfoFigure.appendChild(productImgCart);
+        productInfoDiv.append(productPrice, productName);
+        productInfo.append(productInfoDiv, productInfoFigure);
+        productCard.append(img, productInfo);
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
 
 console.log('JS JALA');
