0248adad407faa27594b0fac5f82105809689eb7
Yínerson Urina <yi.ur@hotmail.com>
2023-02-20 21:42:08 -0500

    fusión del menú mobile

--- main.js
+++ main.js
@@ -2,10 +2,21 @@ const $ = (selector) => document.querySelector(selector);
 
 const desktopMenu = $('.desktop-menu')
 const menuEmail =$('.navbar-email');
+const burguerMenu =$('.menu');
+const mobileMenu =$('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobilepMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobilepMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
+
+
+console.log('JS JALA');
