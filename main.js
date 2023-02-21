6c14d6690cca8c74944cd43e871599e2ebe8b41f
SantiagoDoval <danieldovals@gmail.com>
2023-02-20 19:29:43 -0500

    Shopping Card

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu=document.querySelector('.desktop-menu');
 const menuHamIcon=document.querySelector('.menu');
 const mobileMenu=document.querySelector('.mobile-menu');
 const menuCarritoIcon=document.querySelector('.navbar-shopping-cart');
-const aside=document.querySelector('.product-detail');
+const shoppingCartContainer=document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 MenuEmail.addEventListener('click', toggleDesktopMenu );
@@ -13,20 +13,20 @@ menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
 
-    const isAsideClosed=aside.classList.contains('inactive');    
+    const isAsideClosed=shoppingCartContainer.classList.contains('inactive');    
     
     if(!isAsideClosed){
-        aside.classList.add('inactive');        
+        shoppingCartContainer.classList.add('inactive');        
     }   
 
     desktopMenu.classList.toggle('inactive');    
 }
 
 function toggleMobilepMenu(){
-    const isAsideClosed=aside.classList.contains('inactive');    
+    const isAsideClosed=shoppingCartContainer.classList.contains('inactive');    
     
     if(!isAsideClosed){
-        aside.classList.add('inactive');        
+        shoppingCartContainer.classList.add('inactive');        
     }   
 
     mobileMenu.classList.toggle('inactive');    
@@ -39,7 +39,7 @@ function toggleCarritoAside(){
         mobileMenu.classList.add('inactive');        
     }
     
-    aside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
 }
 
 function openProductDetailAside() {
