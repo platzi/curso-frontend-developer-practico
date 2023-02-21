cacc74df8f9c24063f9a9607f0c3b32490c5a57d
jgomero <josegomerov90@gmail.com>
2023-02-20 19:19:06 -0500

    Se modifico css, html y js para menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu"); 
 
-const clickMenu = menuEmail.addEventListener("click", toggleDesktopMenu);
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu)
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+};
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive"); /*hacemos llamado al atributo inactive que es display none*/
 };
