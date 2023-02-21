50c519599098327f56ef33662ea8cf198a2cec86
Yínerson Urina <yi.ur@hotmail.com>
2023-02-20 22:49:27 -0500

    Se agregó el aside del shoping cart

--- main.js
+++ main.js
@@ -4,19 +4,29 @@ const desktopMenu = $('.desktop-menu')
 const menuEmail =$('.navbar-email');
 const burguerMenu =$('.menu');
 const mobileMenu =$('.mobile-menu');
+const menuCart =$('.navbar-shopping-cart');
+const productNav =$('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobilepMenu);
+menuCart.addEventListener('click', toggleproductNav);
+
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    productNav.classList.add('inactive');
 }
 
 function toggleMobilepMenu() {
     mobileMenu.classList.toggle('inactive');
+    productNav.classList.add('inactive');
 }
 
-
+function toggleproductNav() {
+    productNav.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+}
 
 console.log('JS JALA');
