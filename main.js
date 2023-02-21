906958fdce72ec844b736c4849137b065d82f47d
Caja de Cambio The Grand <cajacambiotg@palace-resorts.local>
2023-02-20 22:34:41 -0500

    Se agregó ventana detalles del producto

--- main.js
+++ main.js
@@ -3,27 +3,34 @@ const navbar_email = document.querySelector('.navbar-email');
 const navbar_cart = document.querySelector('.navbar-shopping-cart');
 const mobile_menu = document.querySelector('.mobile-menu');
 const icon_mobile_menu = document.querySelector('.menu');
-const aside = document.querySelector('.product-detail');
+const shopping_cart = document.querySelector('#shopping_cart');
 const cards_containter = document.querySelector('.cards-container');
+const product_detail = document.querySelector('.product-detail');
+const btnClose = document.querySelector('.product-detail-close')
 
-navbar_email.addEventListener('click', clickEmail);
-icon_mobile_menu.addEventListener('click', clickIconMenu);
+navbar_email.addEventListener('click', clickDesktopMenu);
+icon_mobile_menu.addEventListener('click', clickMobileMenu);
 navbar_cart.addEventListener('click', clickIconCart);
 
-function clickEmail(){
-    isEnable(false, aside);
+function clickDesktopMenu(){
+    isEnable(false, product_detail);
+    isEnable(false, shopping_cart);
+    isEnable(false, mobile_menu);
     desktop_menu.classList.toggle('inactive');
 }
 
-function clickIconMenu(){
-    isEnable(false, aside);
+function clickMobileMenu(){
+    isEnable(false, shopping_cart);
+    isEnable(false, desktop_menu);
+    isEnable(false, product_detail);
     mobile_menu.classList.toggle('inactive');
 }
 
 function clickIconCart(){
     isEnable(false, mobile_menu);
     isEnable(false, desktop_menu);
-    aside.classList.toggle('inactive');
+    isEnable(false, product_detail);
+    shopping_cart.classList.toggle('inactive');
 }
 
  /*Disable menu forms function*/
@@ -98,33 +105,17 @@ productsList.push({
     img: 'https://images.pexels.com/photos/1330638/pexels-photo-1330638.jpeg?auto=compress&cs=tinysrgb&w=400'
 });
 
-
-/*
-<div class="product-card">
-      <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-      <div class="product-info">
-        <div>
-          <p>$120,00</p>
-          <p>Bike</p>
-        </div>
-        <figure>
-          <img src="./icons/bt_add_to_cart.svg" alt="">
-        </figure>
-      </div>
-    </div> 
-    */
-
     for (eachItem of productsList){
 
-        const div_main = document.createElement('div');
-        div_main.classList.add('product-card');
+        const product_card = document.createElement('div');
+        product_card.classList.add('product-card');
     
         const img = document.createElement('img');
         img.setAttribute('src', eachItem.img)
         img.setAttribute('alt','Imagen de articulo')
     
         const div_info = document.createElement('div');
-        div_info.classList.add('product-info');
+        div_info.classList.add('product-info_PC');
     
         const div = document.createElement('div');
     
@@ -145,10 +136,24 @@ productsList.push({
         figure.appendChild(icon_cart);
         div_info.appendChild(div);
         div_info.appendChild(figure);
-        div_main.appendChild(img);
-        div_main.appendChild(div_info);
+        product_card.appendChild(img);
+        product_card.appendChild(div_info);
     
-    cards_containter.appendChild(div_main);
+    cards_containter.appendChild(product_card);
+
+    product_card.addEventListener('click', clickProductCard);
+    btnClose.addEventListener('click', clickBtnClose);
     }
 
+    function clickProductCard(){
+        isEnable(false, shopping_cart);
+        isEnable(false, mobile_menu);
+        isEnable(false, desktop_menu);
+        product_detail.classList.toggle('inactive');
+        console.log("FUncionando");
+    }
+
+    function clickBtnClose(){
+        product_detail.classList.add('inactive');
+    }
 
