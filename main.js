39484bf7734dbd7a5022f6c220f774e7c889bb0c
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-02-22 12:24:04 -0300

    agregamos el carrito de opciones y lo configuramos para que no se superponga con el otro

--- main.js
+++ main.js
@@ -12,13 +12,16 @@ menuMobileIcon.addEventListener('click', mostrarMobileMenu);
 carrito.addEventListener('click', mostrarProductDetail);
 
 function mostrarBarraEmail(){
-    
+    const asideClosed=asideProductDetailCarrito.classList.contains('inactive');
+    if(!asideClosed){
+        asideProductDetailCarrito.classList.add('inactive')
+    }
     DesktopMenu.classList.toggle('inactive');
 }
 function mostrarMobileMenu(){
 
     // LA CONSTANTE CREADA ES PARA SABER SI EL ELEMENTO MENCIONADO CONTIENE LA CLASE "INACTIVE"
-
+    
     const asideClosed=asideProductDetailCarrito.classList.contains('inactive');
     // EL IF CHECKEA SI EL ELEMENTO TIENE O NO ESA CLASE, Y EN CASO DE NO TENERLA SE LA AGREGA PARA QUE NO SE MUESTRE SOBRE EL ELEMENTO QUE QUEREMOS MOSTRAR EN LA FUNCTION. LUEGO LE QUITA LA CLASE INACTIVE AL ELEMENTO A MOSTRAR PARA QUE ESTE APAREZCA
     if(!asideClosed){
@@ -27,6 +30,10 @@ function mostrarMobileMenu(){
     mobileMenu.classList.toggle('inactive');
 }
 function mostrarProductDetail(){
+    const desktopMenuClosed=DesktopMenu.classList.contains('inactive')
+    if (!desktopMenuClosed){
+        DesktopMenu.classList.add('inactive')
+    }
     const mobileMenuClosed=mobileMenu.classList.contains('inactive');
     if (!mobileMenuClosed){
         mobileMenu.classList.add('inactive');
