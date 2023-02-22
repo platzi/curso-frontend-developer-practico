b75838d70989fb82b598c41b8522cd6346a3db4d
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-02-22 12:11:41 -0300

    agregado del carrito y configuración para que no se superponga con el otro elemento a mostrar

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-const barraEmail=document.querySelector('.navbar-email')
-const DesktopMenu=document.querySelector('.desktop-menu')
-const mobileMenu=document.querySelector('.mobile-menu')
-const menuMobileIcon=document.querySelector('.menu')
+const barraEmail=document.querySelector('.navbar-email');
+const DesktopMenu=document.querySelector('.desktop-menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+const menuMobileIcon=document.querySelector('.menu');
 
+const carrito=document.querySelector('.navbar-shopping-cart');
+const asideProductDetailCarrito=document.querySelector('.product-detail');
 
 
 barraEmail.addEventListener('click', mostrarBarraEmail);
 menuMobileIcon.addEventListener('click', mostrarMobileMenu);
+carrito.addEventListener('click', mostrarProductDetail);
 
 function mostrarBarraEmail(){
+    
     DesktopMenu.classList.toggle('inactive');
 }
 function mostrarMobileMenu(){
+
+    // LA CONSTANTE CREADA ES PARA SABER SI EL ELEMENTO MENCIONADO CONTIENE LA CLASE "INACTIVE"
+
+    const asideClosed=asideProductDetailCarrito.classList.contains('inactive');
+    // EL IF CHECKEA SI EL ELEMENTO TIENE O NO ESA CLASE, Y EN CASO DE NO TENERLA SE LA AGREGA PARA QUE NO SE MUESTRE SOBRE EL ELEMENTO QUE QUEREMOS MOSTRAR EN LA FUNCTION. LUEGO LE QUITA LA CLASE INACTIVE AL ELEMENTO A MOSTRAR PARA QUE ESTE APAREZCA
+    if(!asideClosed){
+        asideProductDetailCarrito.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive');
+}
+function mostrarProductDetail(){
+    const mobileMenuClosed=mobileMenu.classList.contains('inactive');
+    if (!mobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    asideProductDetailCarrito.classList.toggle('inactive');
 }
