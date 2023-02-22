43a69546e87337d010471041293c16fb1f3e49fd
Yínerson Urina <yi.ur@hotmail.com>
2023-02-22 12:03:42 -0500

    interaccón entre todos los componentes

--- main.js
+++ main.js
@@ -7,28 +7,46 @@ const mobileMenu =$('.mobile-menu');
 const menuCart =$('.navbar-shopping-cart');
 const productNav =$('.product-detail');
 const cardsContainer =$('.cards-container');
+const productDetailContainer =$('.product-detail-2')
+const productDetailClose =$('.product-detail-close');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobilepMenu);
 menuCart.addEventListener('click', toggleproductNav);
+productDetailClose.addEventListener('click', closeProductDetail)
 
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
     productNav.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 }
 
 function toggleMobilepMenu() {
     mobileMenu.classList.toggle('inactive');
     productNav.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 }
 
 function toggleproductNav() {
     productNav.classList.toggle('inactive');
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 }
+
+function openProductDetail() {
+    productDetailContainer.classList.remove('inactive');
+    desktopMenu.classList.add('inactive');
+    productNav.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+}
+
+function closeProductDetail() {
+    productDetailContainer.classList.add('inactive');
+}
+
 
 const productList = [];
 productList.push({
@@ -37,14 +55,14 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push({
-    name: 'TV',
+    name: 'TV LG OLED CS',
     price: '1000',
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://www.digitalexperience.co.za/wp-content/uploads/2022/10/OLED55CS6LA-z.png',
 })
 productList.push({
     name: "Xbox Series X",
     price: '500',
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://images.wallpapersden.com/image/download/4k-xbox-series-x_bGVnbmqUmZqaraWkpJRqZWWtamVl.jpg',
 })
 
 function renderProducts(arr) {
@@ -54,6 +72,7 @@ function renderProducts(arr) {
     
         const img = document.createElement('img');
         img.setAttribute('src', product.image);
+        img.addEventListener('click', openProductDetail);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
