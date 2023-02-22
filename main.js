5800509264df7e07a2168307946e2625ed8aea5f
gerardo camejo <gerardocam154@gmail.com>
2023-02-22 17:23:43 +0100

    agregado mediante maquetacion de html utilizando javascript, la lista de productos del yardsale

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shopCar = document.querySelector('.navbar-shopping-cart');
 const shopList = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container')
 
 menuHamMenu.addEventListener('click', toggleMobileMenu);
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -48,3 +49,68 @@ function toggleShopList()
 
    shopList.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push(
+    {
+        name: 'Bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    });
+
+productList.push(
+    {
+        name: 'TV',
+        price: 100,
+        image: 'https://images.pexels.com/photos/6976094/pexels-photo-6976094.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    });
+
+productList.push(
+     {
+        name: 'Laptop',
+        price: 80,
+        image: 'https://images.pexels.com/photos/5629144/pexels-photo-5629144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    });
+    
+function renderProducts(arr)
+{
+    for (product of arr) 
+{
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+
+
+}
+}
+
+renderProducts(productList);
