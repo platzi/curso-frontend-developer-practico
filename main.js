3ddaf604cbae26e8747ddd4bb467707e56575219
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-02-22 16:54:05 -0300

    Acabado lo del render de productos

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const menuMobileIcon=document.querySelector('.menu');
 const carrito=document.querySelector('.navbar-shopping-cart');
 const asideProductDetailCarrito=document.querySelector('.product-detail');
 
+const cardsContainer=document.querySelector('.cards-container')
+
 
 barraEmail.addEventListener('click', mostrarBarraEmail);
 menuMobileIcon.addEventListener('click', mostrarMobileMenu);
@@ -39,4 +41,87 @@ function mostrarProductDetail(){
         mobileMenu.classList.add('inactive');
     }
     asideProductDetailCarrito.classList.toggle('inactive');
+}
+
+const productList=[]
+productList.push({
+    name:'Bike',
+    price: 120.00,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+}
+)
+productList.push({
+    name:'Nova SS',
+    price: 30000,
+    image: 'https://www.kindpng.com/picc/m/736-7363802_chevy-nova-ss-1969-png-transparent-png.png'
+}
+)
+productList.push({
+    name:'ToyotaCorolla',
+    price: 12000,
+    image: 'https://www.pngitem.com/pimgs/m/638-6380605_toyota-corolla-car-png-transparent-png.png'
+}
+)
+productList.push({
+    name:'lambo',
+    price: 54000,
+    image: 'https://www.pngitem.com/pimgs/m/2-25960_lamborghini-png-image-lamborghini-gallardo-lp560-4-transparent.png'
+}
+)
+
+
+{/* 
+<div class="cards-container">
+    
+    <div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+    <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+        </div>
+        <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+        </div>
+    </div>
+</div> 
+*/
+}
+
+for (product of productList){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    
+    const productImg=document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo=document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const infoDiv=document.createElement('div')
+    
+    const productPrice=document.createElement('p')
+    productPrice.innerText = '$' + product.price
+    const productName=document.createElement('p')
+    productName.innerText = product.name
+    
+    infoDiv.appendChild(productPrice)
+    infoDiv.appendChild(productName)
+
+
+    const productFigure=document.createElement('figure')
+    const productImgFigure=document.createElement('img')
+    productImgFigure.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+    productFigure.appendChild(productImgFigure)
+
+    productInfo.appendChild(infoDiv)
+    productInfo.appendChild(productFigure)
+
+    productCard.appendChild(productImg)
+    productCard.appendChild(productInfo)
+
+    cardsContainer.appendChild(productCard)
+
 }
