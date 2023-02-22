1d778f4d7509b4800351386f0d9f312e063352d7
Eliana Corrales Vanegas <Elitm0330@gmail.com>
2023-02-22 07:37:49 -0500

    Config menu hamburguesa

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const navMenuImail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 
 navMenuImail.addEventListener('click',toggleDesktopMenu)
+menuHamIcon.addEventListener('click',togglemenu)
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+
+}
+
+function togglemenu(){
+    mobileMenu.classList.toggle('inactive');
 
 }
