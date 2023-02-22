a45a221e348f9c67567f52f4a29d431505c9b334
Eliana Corrales Vanegas <Elitm0330@gmail.com>
2023-02-22 08:27:44 -0500

    Cerrar listados desplegables

--- main.js
+++ main.js
@@ -2,19 +2,46 @@ const navMenuImail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 
 navMenuImail.addEventListener('click',toggleDesktopMenu)
 menuHamIcon.addEventListener('click',togglemenu)
+menuCarritoIcon.addEventListener('click',toggleCarritoCompras)
 
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
-
 }
 
 function togglemenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 
 }
+
+function toggleCarritoCompras(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed ){
+        mobileMenu.classList.add('inactive');   
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+     aside.classList.toggle('inactive');
+
+}
+    
