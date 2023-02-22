b8ca40a85154661029e6f64d60d11d7bb37ae0e0
johnSanchezCoronado <jhon.sanchez@optimus3d.com>
2023-02-22 13:10:39 -0500

    add menu and product list

---
+++ main.js
@@ -0,0 +1,105 @@
+const menuMail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHam = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCart = document.querySelector(".navbar-shopping-cart");
+const asideCart = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
+
+menuMail.addEventListener("click", toggleMenuDesktop);
+menuHam.addEventListener("click", toggleMenuMobile);
+menuCart.addEventListener("click", toggleMenuCart);
+
+function toggleMenuDesktop() {
+  const isAsideCartClosed = asideCart.classList.contains("inactive");
+
+  if (!isAsideCartClosed) {
+    asideCart.classList.add("inactive");
+  }
+
+  desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMenuMobile() {
+  const isAsideCartClosed = asideCart.classList.contains("inactive");
+
+  if (!isAsideCartClosed) {
+    asideCart.classList.add("inactive");
+  }
+
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleMenuCart() {
+  const isMobileMenuClose = mobileMenu.classList.contains("inactive");
+  const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClose) {
+    mobileMenu.classList.add("inactive");
+  }
+  if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add("inactive");
+  }
+
+  asideCart.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Pantalla",
+  price: 220,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Compu",
+  price: 620,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    // product= {name, price, image} -> product.image
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productList);
