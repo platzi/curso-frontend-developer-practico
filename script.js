81871eaf16f246f8a8014a5991e52e9c6970f148
nestorrg18 <neriga10@hotmail.com>
2023-02-21 19:57:14 -0600

    Clase 21: Lista de productos: HTML a partir de arrays

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//estaticos
+const cardsContainer = document.querySelector(".cards-container")
+//accionables
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burguerIcon = document.querySelector(".menu");
@@ -7,14 +10,21 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 
 const interactiveElements = document.querySelectorAll(".interactive");
 console.log(interactiveElements);
+//
+const productList = [];
 
 menuEmail.addEventListener("click", ()=> {blockOrNone(desktopMenu)});
 burguerIcon.addEventListener("click", ()=> {blockOrNone(mobileMenu)});
 menuCarritoIcon.addEventListener("click", ()=> {blockOrNone(menuCarrito)});
 
 function blockOrNone(element) {//https://platzi.com/comentario/3856000/
-    displayNoneAll()
-    element.classList.toggle("inactive")
+    if (element.classList.contains("inactive") === true) {
+        displayNoneAll()
+        element.classList.remove("inactive")
+    } else {
+        displayNoneAll()
+        element.classList.add("inactive")
+    }
 }
 function displayNoneAll() {
     interactiveElements.forEach(function(interactiveElement){
@@ -23,3 +33,40 @@ function displayNoneAll() {
         }
     })
 }
+
+class Product {
+    constructor(name, price, image){
+        this.name = name,
+        this.price = price,
+        this.image = image
+    }
+}
+const bike = new Product("Bike", 120, "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+const bicycleHelmet = new Product("Bicycle helmet", 1200, "https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto" )
+const seat = new Product("Seat", 300, "https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg")
+const tennisMountainBike = new Product("Tennis Montain Bike", 2200, "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg")
+const sunglasses = new Product("Sunglasses", 800, "https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602")
+
+productList.push(bike, bicycleHelmet, seat, tennisMountainBike, sunglasses)
+console.log(productList);
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement("div")
+        productCard.classList.add("product-card")
+        productCard.innerHTML = `
+            <img src="${product.image}" alt="${product.name}">
+            <div class="product-info">
+            <div>
+                <p>$${product.price}</p>
+                <p>${product.name}</p>
+            </div>
+            <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="">
+            </figure>
+            </div>`;
+        console.log(productCard);
+        cardsContainer.append(productCard);
+    }
+}
+renderProducts(productList);
