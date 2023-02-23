07dbb4509e314509ef612aae3afb4eacc06da047
Elío <eliodiez@hotmail.com>
2023-02-23 15:06:30 +0100

    mi segundo commit con los menus mobile y desktop integrados

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconoMenuMobil = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener ('click', toggleDesktopMenu);
+iconoMenuMobil.addEventListener ('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+};
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+    console.log('Hello')
 };
 console.log('I am working');
