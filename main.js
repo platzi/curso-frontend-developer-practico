dbf5e182fffbf178825d4eb45fc4b6cea2995c98
juang-10 <judagosa8520@gmail.com>
2023-02-23 18:09:19 -0500

    feat: mostrar y ocultar el carrito de compras

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
-const navEmail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
-navEmail.addEventListener('click', function() {
+menuEmail.addEventListener('click', function() {
+  const isAsideClosed = aside.classList.contains('inactive');
+  
+  if(!isAsideClosed) {
+    aside.classList.add('inactive')
+  }
+
   desktopMenu.classList.toggle('inactive')
 })
 
 menuHamIcon.addEventListener('click', function() {
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if(!isAsideClosed) {
+    aside.classList.add('inactive')
+  }
+
   mobileMenu.classList.toggle('inactive')
+})
+
+menuCarritoIcon.addEventListener('click', function() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+  if(!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive')
+  }
+
+  // Do it for me
+
+  if(!isDesktopMenuClosed) {
+    desktopMenu.classList.add('inactive')
+  }
+  
+
+  aside.classList.toggle('inactive')
+
 })
