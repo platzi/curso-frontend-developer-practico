8483404885205e40a82d2d6039539c551ee3ebba
juang-10 <judagosa8520@gmail.com>
2023-02-23 16:11:33 -0500

    feat: mostrar y ocultar menu mobile

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 navEmail.addEventListener('click', function() {
   desktopMenu.classList.toggle('inactive')
+})
+
+menuHamIcon.addEventListener('click', function() {
+  mobileMenu.classList.toggle('inactive')
 })
