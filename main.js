87287b0b713fc93e68a8708cbea1eececf94c5c9
carlossg88 <carlossg88@hotmail.com>
2023-02-23 08:05:12 -0500

    Adding mobile menu

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const menuEmail =document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggledesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 
 function toggledesktopMenu(){
   desktopMenu.classList.toggle('inactive');
   console.log('click')
-  
 }
-console.log('funciona');
+
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('inactive')
+}
