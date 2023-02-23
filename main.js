e99299f2f316c2a00292d1e908795e8b82ecd1d8
luiscanate <luiscanate@gmail.com>
2023-02-22 22:49:15 -0500

    se adiciona modulo final

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
 menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu(){
   const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -27,6 +30,8 @@ function toggleMobileMenu(){
     shoppingCartContainer.classList.add('inactive');
   }
 
+  closeProductDetailAside();
+  
   mobileMenu.classList.toggle('inactive'); 
   
 }
@@ -37,8 +42,25 @@ function toggleCarritoAside(){
   if (!isMobileMenuClosed){
     mobileMenu.classList.add('inactive');
   }
+
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+  
+  if (!isProductDetailClosed){
+    productDetailContainer.classList.add('inactive');
+  }
+
   shoppingCartContainer.classList.toggle('inactive');
- 
+   
+}
+
+function openProductDetailAside(){
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+
+}
+
+function closeProductDetailAside(){
+  productDetailContainer.classList.add('inactive');
 }
 const productList = [];
 productList.push({
@@ -68,6 +90,7 @@ function renderProducts(arr){
      // product = {name, price, image} -> product.image 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside);
   
    
     const productInfo = document.createElement('div');
