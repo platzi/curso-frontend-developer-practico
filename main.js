48e426aa2022f7facc8ac3f37a2eb631e26a7b9c
carlosrmgpc <carlosmg023@gmail.com>
2023-02-23 08:41:33 -0500

    insertando-html-desde-javascript-lista-de-producto

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -42,3 +44,88 @@ function toggleCarritoAside () {
 
 
 }
+
+const productList = [];
+productList.push({
+    name: 'bike',
+    pricie: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'pantalla',
+    pricie: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'computador',
+    pricie: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'bike',
+    pricie: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'pantalla',
+    pricie: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'computador',
+    pricie: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+
+/*for(product of prodeuctList) {
+    console.log(product.name)
+} en caso de querer llamer solo el nombre de el aticulo que necesitamos*/
+
+//para tener un codigo mas organisado creamos esto dentro de una funcion co un paramero  arr asi podemos reutilisar  y desirle cual esel array con el que quiero trabajar 
+
+function renderProduct(arr) {
+    for(product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        //product = {name, price, image} -> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerHTML = '$' + product.Price;
+        const productName = document.createElement('p');
+        productName.innerHTML = product.Name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName)
+    
+    
+        const productInfofigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfofigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfofigure)
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    
+    }
+}
+
+//ej
+
+renderProduct(productList);
+
