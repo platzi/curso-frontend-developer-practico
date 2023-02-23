eef2b108dbcf79a30703935a4b8a92c852b1f759
juang-10 <judagosa8520@gmail.com>
2023-02-23 13:43:51 -0500

    feat: mostrar y ocultar menu

---
+++ main.js
@@ -0,0 +1,6 @@
+const navEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu')
+
+navEmail.addEventListener('click', function() {
+  desktopMenu.classList.toggle('inactive')
+})
