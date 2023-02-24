5116c49d60a7054cee91fcd791460e14f4ab24d4
RamsesGV <senkustone15@gmail.com>
2023-02-24 11:06:38 -0600

    agregar productos en un array de objetos

--- main.js
+++ main.js
@@ -1,10 +1,158 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
+const MenuDesplegableHam = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
-const toggleDesktopMenu = () => DesktopMenu.classList.toggle('inactive')
+
+const toggleMenuHam = () => { 
+const IsAsideClose = aside.classList.contains('inactive')
+if (!IsAsideClose){ 
+aside.classList.add('inactive')
+}
+mobileMenu.classList.toggle('inactive')
+} 
+
+
+
+const toggleDesktopMenu = () => { 
+const IsAsideClose = aside.classList.contains('inactive')
+if(!IsAsideClose) 
+aside.classList.add('inactive')
+DesktopMenu.classList.toggle('inactive')
+}
+
+
+
+const toggleMenuCarrito = () => { 
+const IsMobileMenuClosed = mobileMenu.classList.contains('inactive')
+const IsDesktopMenuClosed = DesktopMenu.classList.contains('inactive')
+if (!IsDesktopMenuClosed) { 
+    DesktopMenu.classList.add('inactive')
+}
+if (!IsMobileMenuClosed){ 
+ mobileMenu.classList.add('inactive')
+}
+aside.classList.toggle('inactive'); 
+}
+
+
+
+/* <div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+
+</div>
+</div> */
+
+
+
+const renderProductos = (arr) => { 
+for ( product of arr) {
+const productCard =   document.createElement('div');
+productCard.classList.add('product-card');
+
+const productImg = document.createElement('img');
+productImg.setAttribute('src', product.image);
+
+const productInfo =   document.createElement('div');
+productInfo.classList.add('product-info');
+ 
+const productInfoDiv = document.createElement('div');
+
+const producPrice = document.createElement('p');
+producPrice.innerText = '$' + product.price;
+const productName = document.createElement('p');
+productName.innerText = product.name;
+
+productInfoDiv.appendChild(producPrice);
+productInfoDiv.appendChild(productName);
+
+
+const productInfoFigure = document.createElement('figure');
+const productImgCart = document.createElement('img');
+productImgCart.setAttribute('src', "./icons/bt_add_to_cart.svg");
+
+productInfoFigure.appendChild(productImgCart);
+
+productInfo.appendChild(productInfoDiv);
+productInfo.appendChild(productInfoFigure);
+
+productCard.appendChild(productImg)
+productCard.appendChild(productInfo)
+
+cardsContainer.appendChild(productCard)
+
+}
+}
+
+
+
+const productList = []
+
+productList.push({
+    name:'Bike',
+    price:120,
+    image:'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' 
+})
+
+
+productList.push({
+    name:'Pantalla',
+    price:220,
+    image:'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+
+productList.push({
+    name:'compu',
+    price:620,
+    image:'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name:'Bike',
+    price:120,
+    image:'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' 
+})
+
+
+productList.push({
+    name:'Pantalla',
+    price:220,
+    image:'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+
+productList.push({
+    name:'compu',
+    price:620,
+    image:'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+    
+
+    
+
+
+renderProductos(productList)
+
+
+
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
-
+MenuDesplegableHam.addEventListener('click', toggleMenuHam);
+menuCarritoIcon.addEventListener('click', toggleMenuCarrito);
 /*
 Primero, se crea una constante llamada navbarEmail que utiliza el método querySelector para buscar en el documento HTML un elemento que tenga la clase navbar-email.
 
