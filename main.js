48bffeadc3629ba40c0a31a9f8cdd0c65d83cd19
Dan R <danorhl09120@gmail.com>
2023-02-24 00:55:31 -0500

    Maquetacion de la lista de productos en Html pero usando JavaScript, ahora son dinamicos

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -41,3 +42,79 @@ function toggleCarritoAside(){
         }
             aside.classList.toggle('inactive');
 }
+
+
+const productList = [];
+productList.push({
+    name: 'Bicicleta',
+    price: 450,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Patineta',
+    price: 220,
+    image: 'https://images.pexels.com/photos/1018484/pexels-photo-1018484.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'Patines',
+    price: 129,
+    image: 'https://images.pexels.com/photos/2005992/pexels-photo-2005992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'Mat de Yoga',
+    price: 99,
+    image: 'https://images.pexels.com/photos/6246398/pexels-photo-6246398.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'Tetera para Té',
+    price: 69,
+    image: 'https://images.pexels.com/photos/8329955/pexels-photo-8329955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'Cafetera',
+    price: 139,
+    image: 'https://images.pexels.com/photos/6766549/pexels-photo-6766549.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+
+function renderProducts(arr){
+
+    for(product of arr){
+
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+    
+        const productImg = document.createElement('img');
+        // En caso de img :  product = {name, price, image} -> product.image
+        // Los demás es muy parecido
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+    
+        const productInfoDiv  = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.append(productPrice, productName);
+    
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.append(productInfoDiv, productInfoFigure);
+    
+        productCard.append(productImg, productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
