c4914b518c0e36f33fd63edfebd7d91ed0a7868b
Manuel Niño <manuestudi2391@hotmail.com>
2023-02-24 09:54:54 -0500

    Ya creamos Lista de Productos con Arrays

--- main.js
+++ main.js
@@ -1,9 +1,113 @@
 const navBarEmail = document.querySelector(".navbar-email");
-const desktopMenu =document.querySelector(".desktop-menu");
+const desktopMenu = document.querySelector(".desktop-menu");
+const iconMenuCarrito = document.querySelector(".navbar-shopping-cart");
+const iconHamMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
+
 
 navBarEmail.addEventListener("click", toggleDesktopMenu);
+iconHamMenu.addEventListener("click", toggleMobileMenu);
+iconMenuCarrito.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu()
     {
+        const isAsideClosed = aside.classList.contains("inactive");
+
+        if (!isAsideClosed)
+            {
+                aside.classList.add("inactive");
+            }
+
         desktopMenu.classList.toggle("inactive");
     }
+function toggleMobileMenu()
+    {
+        const isAsideClosed = aside.classList.contains("inactive");
+
+        if (!isAsideClosed)
+            {
+                aside.classList.add("inactive");
+            }
+
+        mobileMenu.classList.toggle("inactive");
+    }
+function toggleCarritoAside()
+    {
+        const isMobileMenuclosed = mobileMenu.classList.contains("inactive");
+        const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+        if (!isMobileMenuclosed)
+            {
+                mobileMenu.classList.add("inactive");
+            }
+        
+        if (!isDesktopMenuClosed)
+            {
+                desktopMenu.classList.add("inactive");
+            }
+
+        aside.classList.toggle("inactive");
+    }
+
+const productList = [];
+productList.push(
+    {
+        name:"Bike",
+        price: 120,
+        image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    });
+productList.push(
+    {
+        name:"pantalla",
+        price: 220, 
+        image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    });
+productList.push(
+    {
+        name:"Compu",
+        price: 620,
+        image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    });
+
+function renderProducts(arr)
+    {
+        for (product of arr)
+            {
+                const productCard = document.createElement("div");
+                productCard.classList.add("product-card");
+
+                const productImg = document.createElement("img");
+                productImg.setAttribute("src", product.image);
+
+                const productInfo = document.createElement("div");
+                productInfo.classList.add("product-info");
+
+                const productInfoDiv = document.createElement("div");
+
+                const productPrice = document.createElement("p");
+                productPrice.innerText = "$" + product.price;
+
+                const productName = document.createElement("p");
+                productName.innerText = product.name;
+
+                productInfoDiv.appendChild(productPrice);
+                productInfoDiv.appendChild(productName);
+
+                const productInfoFigure = document.createElement("figure");
+                const productImgCart = document.createElement("img");
+                productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+                productInfoFigure.appendChild(productImgCart);
+
+                productInfo.appendChild(productInfoDiv);
+                productInfo.appendChild(productInfoFigure);
+
+                productCard.appendChild(productImg);
+                productCard.appendChild(productInfo);
+
+                cardsContainer.appendChild(productCard);
+            }
+    }
+
+renderProducts(productList);
