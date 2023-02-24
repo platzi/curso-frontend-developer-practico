05540f5113949561c2e06e5d3ae00282d9e80f6f
Justin Becerra <jube2928@gmail.com>
2023-02-24 02:23:07 -0500

    Agregué interacción al menú mobile

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const menuCel = document.querySelector(".menu")
+const selectorMobile = document.querySelector(".mobile-menu")
+
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+menuCel.addEventListener("click", toggleMobileMenu)
+
 
 function toggleDesktopMenu(){
 
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu(){
+    selectorMobile.classList.toggle("inactive")
 }
