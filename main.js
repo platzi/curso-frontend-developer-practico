02c694d6c5c9fb49224b6dd0f373c39052a9ef17
JeisonPC <jeida16@yahoo.es>
2023-02-23 20:57:24 -0500

    Add changes of class 19

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const $ = (select) => document.querySelector(select);
+
+const menuEmail = $('.navbar-email');
+const desktopMenu = $('.desktop-menu');
+const hamburgerIcon = $('.menu');
+const mobileMenu = $('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
+hamburgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
     
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+
 }
