476bb3f9ededfb772cc9f2e96e46ff53e2a956e5
Jorge Manrique <georgemanc.28@gmail.com>
2023-02-24 11:41:06 -0500

    Se intregro la barra de navegación con el menu mobile interactivo JavaScript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const email = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menu = document.querySelector(".menu");
 
 email.addEventListener("click", toggleDesktopMenu);
+menu.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
