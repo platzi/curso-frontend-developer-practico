da1f935e201b6a7dea84f94a26948e7aae0acf65
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-02-27 11:23:50 -0500

    agrego más productos

--- main.js
+++ main.js
@@ -85,34 +85,19 @@ productList.push({
     image: 'https://images.pexels.com/photos/5793953/pexels-photo-5793953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
 productList.push({
-    name: 'Bicicleta',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name: 'Sofa',
+    price: 580,
+    image: 'https://images.unsplash.com/photo-1540574163026-643ea20ade25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
 })
 productList.push({
-    name: 'Televisor',
-    price: 220,
-    image: 'https://images.pexels.com/photos/6316063/pexels-photo-6316063.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    name: 'Lámpara',
+    price: 40,
+    image: 'https://images.unsplash.com/photo-1612736228052-80e7f381690c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=381&q=80',
 })
 productList.push({
-    name: 'Portatil',
-    price: 620,
-    image: 'https://images.pexels.com/photos/5793953/pexels-photo-5793953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-    name: 'Bicicleta',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
-productList.push({
-    name: 'Televisor',
-    price: 220,
-    image: 'https://images.pexels.com/photos/6316063/pexels-photo-6316063.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-})
-productList.push({
-    name: 'Portatil',
-    price: 620,
-    image: 'https://images.pexels.com/photos/5793953/pexels-photo-5793953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    name: 'Estéreo',
+    price: 180,
+    image: 'https://images.unsplash.com/photo-1612241143917-d05c85c43751?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
 });
 
 function renderProducts (arr) {
