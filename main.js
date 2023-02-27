929c9ade93f30fd0b4954a6ecc3afc2c3de44a22
carlossg88 <carlossg88@hotmail.com>
2023-02-27 07:24:03 -0500

    Added Aside menu

--- main.js
+++ main.js
@@ -2,15 +2,36 @@ const menuEmail =document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggledesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+
 
 function toggledesktopMenu(){
+  const IsAsideClosed = aside.classList.contains('inactive')
+  if (!IsAsideClosed){
+    aside.classList.add('inactive')
+  }
+
   desktopMenu.classList.toggle('inactive');
-  console.log('click')
 }
 
 function toggleMobileMenu(){
-  mobileMenu.classList.toggle('inactive')
+  const IsAsideClosed = aside.classList.contains('inactive')
+  if (!IsAsideClosed){
+    aside.classList.add('inactive')
+  }
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive')
+  }
+  aside.classList.toggle('inactive');
 }
