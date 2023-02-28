ac0cd4adc20e8a6c1c8a074489d84ace43987135
edu2122 <eduardorafaellm@gmail.com>
2023-02-28 11:01:31 -0400

    Se agrego el menu mobile integrado con JavaScript y se modificaron 2 media query para que los menu de mobile y desktop se oculten.

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurger.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
