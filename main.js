4447d811346d738c5c40fc75036a4f78b0254528
Jessica <jessiica_om97@hotmail.com>
2023-02-28 16:55:37 -0600

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuBtnMobileIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
-
+//* Event Listeners
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuBtnMobileIcon.addEventListener('click', toggleMobileMenu)
 
+//* Functions
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
