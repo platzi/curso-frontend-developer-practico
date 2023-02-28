a7bb5548e1eeac0dac641df28928cf2639198f17
Raul Salinas <salinasraul883@gmail.com>
2023-02-28 07:26:35 -0600

    Union de menus

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 const navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuMobileIcon = document.querySelector('.menu-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const shoppingMenu = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuMobileIcon.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleShoppingMenu);
+
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+    const isShoppingMenuClosed = shoppingMenu.classList.contains('inactive');
+
+    if (!isShoppingMenuClosed) {
+        shoppingMenu.classList.add('inactive');
+    }
+
+
+    desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    const isShoppingMenuClosed = shoppingMenu.classList.contains('inactive');
+
+    if (!isShoppingMenuClosed) {
+        shoppingMenu.classList.add('inactive');
+    }
+
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingMenu() {
+    const isMovileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+    if (!isMovileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    } else if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    shoppingMenu.classList.toggle('inactive');
+    
+}
+
