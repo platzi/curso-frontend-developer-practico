4e508e5d170b9d991eb99dcaa21558424afb55b0
Comayocode <ronalc726@gmail.com>
2023-02-27 19:17:55 -0500

    feat: Fusion clase7.html a clase11.html. Fusion del menu mobile
    - Ocultar o mostrar el menu en mobile al dar clic sobre menu hamburguesa
    - Ocultar menu mobile en resolucion desktop
    - Ocultar menu desktop en resolucion mobile

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 // ----- SELECCIONES ------
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHam = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 // ----- AÑADIR ESCUCHA DE EVENTOS -----
-navbarEmail.addEventListener('click', cambiarDesktopMenu);
+navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuHam.addEventListener('click', toggleMobileMenu)
 
-// ----- Función que oculta o muestra la clase inactive -----
-function cambiarDesktopMenu() {
+// ----- Cambiar clase correo desktopMeu -----
+function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive'); //la clase no se le pone el punto
+}
+
+// ----- Cambiar clase mobileMenu -----
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
