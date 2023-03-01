79d299fd4dd5a542910d81839419297d324146fe
Gerson Ep <gerson21millones@gmail.com>
2023-02-28 23:10:48 -0600

    C19 - Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 
 const menuEmail = document.querySelector('.navbar-email'); /*Seleccion de elementos html por su clase */
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);/*Escuchar su evento de click */
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive'); /*Quita o pone la clase independiente si ya la tiene o no */
-console.log('aparece el menu')
 }
 
-
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
 
