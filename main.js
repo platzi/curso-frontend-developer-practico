a944c60fe731d812633ab54a505ad3897cba79ac
Valeria <vale.bedoya@outlook.com>
2023-03-01 17:24:10 -0500

    Carrito de compras y menus listos

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu );
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu() {
-desktopMenu.classList.toggle('inactive')
+    const isAsideClosed = aside.classList.contains('inactive');
+
+if(!isAsideClosed){
+    aside.classList.add('inactive');
+}
+desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+const isAsideClosed = aside.classList.contains('inactive');
+
+if(!isAsideClosed){
+    aside.classList.add('inactive');
+}
+
+mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+if(!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive');
+}
+else if(!isDesktopMenuClosed){
+    desktopMenu.classList.add('inactive');
+}
+
+aside.classList.toggle('inactive');
 }
