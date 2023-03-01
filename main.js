ad3a4797032a7c158de300f1aa562669451f8380
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2023-02-28 21:47:29 -0600

    Adding the feature which allow to see a product in the product d etail window

--- main.js
+++ main.js
@@ -49,7 +49,46 @@ function toogleshoppingCartAside(){
     shoppingCartContainer.classList.toggle('inactive');
 }
 
-function openProductDetail(){
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 500,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Motorcyle',
+    price: 7000,
+    image: 'https://images.unsplash.com/photo-1471466054146-e71bcc0d2bb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
+});
+productList.push({
+    name: 'Scooter',
+    price: 2000,
+    image: 'https://images.unsplash.com/photo-1519750292352-c9fc17322ed7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=985&q=80'
+});
+productList.push({
+    name: 'Book: The Subtle Art of Not Giving a F*ck',
+    price: 40,
+    image: 'https://joneytalks.com/wp-content/uploads/2018/09/IMG_4763-2-768x1024.jpg'
+});
+productList.push({
+    name: 'Coffe Table',
+    price: 1120,
+    image: 'https://images.unsplash.com/photo-1585264550248-1778be3b6368?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1363&q=80'
+});
+productList.push({
+    name: 'Toy: Buzz Lightyear',
+    price: 20,
+    image: 'https://images.unsplash.com/photo-1581557991964-125469da3b8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1433&q=80'
+});
+productList.push({
+    name: 'Max steel',
+    price: 50,
+    image: 'https://i.etsystatic.com/37183910/r/il/82ccfd/4450133782/il_fullxfull.4450133782_kgla.jpg'
+});
+
+
+function openProductDetail(product){
 
     // closing all open menus
     desktopMenu.classList.add('inactive');
@@ -59,59 +98,38 @@ function openProductDetail(){
 
     // Opening Product Detail
     productDetailContainer.classList.remove('inactive');
+
+    // Assigning the product selected and bringing the corresponded info
+
+    const productImg = document.querySelector('.product-image');
+    productImg.setAttribute('src', product.image);
+
+    const productPrice = document.querySelector('.product-price');
+    productPrice.innerText = '$' +product.price;
+
+    const productName = document.querySelector('.product-name');
+    productName.innerText = product.name;
+    console.log(product.name);
 }
 
 function closeProductDetail(){
     productDetailContainer.classList.add('inactive');
 }
-
-const productList = [];
-productList.push({
-    name: 'Bike',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Motorcyle',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Scooter',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Book',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Coffe Table',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Toy',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Max steel',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-
-
 function renderProducts(arr) {
-    for (product of productList){
+    for (let product = 0; product < productList.length; product++){
+    // let counterArray = 0;
+    // for (product of productList){
+        // counterArray += 1;
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
     
         // product = {name, price, image} -> product.image
         const productImg = document.createElement('img');
-        productImg.setAttribute('src', product.image);
-        productImg.addEventListener('click', openProductDetail);
+        productImg.setAttribute('src', productList[product].image);
+        productImg.addEventListener('click', function(){
+            // console.log(productList[product]);
+        openProductDetail(productList[product]);
+        });
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -119,9 +137,9 @@ function renderProducts(arr) {
         const productInfoDiv = document.createElement('div');
     
         const productPrice = document.createElement('p');
-        productPrice.innerText = '$' + product.price;
+        productPrice.innerText = '$' + productList[product].price;
         const productName = document.createElement('p');
-        productName.innerText = product.name;
+        productName.innerText = productList[product].name;
     
         productInfoDiv.append(productPrice, productName); // .append allows to add several elements
         
