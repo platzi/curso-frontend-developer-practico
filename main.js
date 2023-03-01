26a4884a86daed75becd408d5464fe0d332ffb3e
Dan R <danorhl09120@gmail.com>
2023-02-28 21:09:00 -0500

    Ordenamiento de los detalles de productos dentro de la aplicación, css

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -12,21 +12,21 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    const isAsideClose = aside.classList.contains('inactive');
+    const isAsideClose = shoppingCartContainer.classList.contains('inactive');
 
     if(!isAsideClose){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     } 
     
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isAsideClose = aside.classList.contains('inactive');
+    const isAsideClose = shoppingCartContainer.classList.contains('inactive');
     
     if(!isAsideClose){
         // Saber si mobile menu esta abierto, para primero cerrarlo
-            aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
         mobileMenu.classList.toggle('inactive');
 }
@@ -40,7 +40,7 @@ function toggleCarritoAside(){
             mobileMenu.classList.add('inactive');
             desktopMenu.classList.add('inactive');
         }
-            aside.classList.toggle('inactive');
+        shoppingCartContainer.classList.toggle('inactive');
 }
 
 
