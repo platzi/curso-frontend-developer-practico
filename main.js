abece75fc405aadba915865a7e0474056ddeb3cf
TenZ <08wTenZ@gmail.com>
2023-03-01 07:58:39 -0500

    adding shopping-cart

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamburguesa.addEventListener('click', toggleMobileMenu)
+shoppingCart.addEventListener('click', toggleProductDetail)
+
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
@@ -20,4 +24,8 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleProductDetail(){
+    productDetail.classList.toggle('inactive')
 }
