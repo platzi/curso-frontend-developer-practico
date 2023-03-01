ce610ba3307f527852cbdf1673f4146196ceb931
TenZ <08wTenZ@gmail.com>
2023-03-01 08:17:43 -0500

    fixing menu overlap issue

--- main.js
+++ main.js
@@ -11,21 +11,30 @@ shoppingCart.addEventListener('click', toggleProductDetail)
 
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
-    //alternative without inactive class
-    /*if(desktopMenu.style.display === "none"){
-        desktopMenu.style.display = "block"
-    } else {
-        desktopMenu.style.display = "none"
+    if (!(productDetail.classList.contains('inactive'))){
+        productDetail.classList.toggle('inactive')
     }
-    */
-   
+    desktopMenu.classList.toggle('inactive')
+
+
 }
 
 function toggleMobileMenu() {
+    if (!(productDetail.classList.contains('inactive'))){
+        productDetail.classList.toggle('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
 }
 
 function toggleProductDetail(){
+    mobileMenuIsOpen = !(mobileMenu.classList.contains('inactive'))
+    desktopMenuIsOpen = !(desktopMenu.classList.contains('inactive'))
+
+    if (mobileMenuIsOpen){
+        mobileMenu.classList.toggle('inactive')
+    }
+    if (desktopMenuIsOpen){
+        desktopMenu.classList.toggle('inactive')
+    }
     productDetail.classList.toggle('inactive')
 }
