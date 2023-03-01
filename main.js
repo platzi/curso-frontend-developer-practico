2c1deb57321b0c86a7450341b3a51495987a9f83
edu2122 <eduardorafaellm@gmail.com>
2023-03-01 10:09:45 -0400

    Se agrego el carrito de compras con funcionalidades de JavaScript

--- main.js
+++ main.js
@@ -4,14 +4,38 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuBurger.addEventListener('click', toggleMobileMenu)
+menuBurger.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = !aside.classList.contains('inactive');
+  if (isAsideClosed) {
+    aside.classList.add('inactive');
+  }
   desktopMenu.classList.toggle('inactive');
+  
 }
 
 function toggleMobileMenu() {
+  const isAsideClosed = !aside.classList.contains('inactive');
+  if (isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+
   mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = !mobileMenu.classList.contains('inactive');
+  // aside.classList.toggle('inactive');
+    if (isMobileMenuClosed) {
+      mobileMenu.classList.add('inactive');
+    }
+    // si el mobileMenu esta abierto, hay que cerrarlo
+    aside.classList.toggle('inactive');
+}
+
