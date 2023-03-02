459911b48c7b0a7047219589fa54e633fa588db6
speak1ngs <juanmavillalbaovelar@gmail.com>
2023-03-02 20:09:18 -0300

    funcionamiento de toggle, open y close

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ const menuMobile = document.querySelector('.mobile-menu');
 const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardContainer = document.querySelector('.cards-container');
+const productDetailContainer = document.querySelector('#product-detail');
+const productDetailClosedIcon = document.querySelector('.product-detail-close');
 
 navEmail.addEventListener('click',toggleDesktopMenu);
 menuMobileImg.addEventListener('click', toggleMenuMobile);
 menuCartIcon.addEventListener('click', toggleProductDetail);
+productDetailClosedIcon.addEventListener('click', closeProductDetailIconAside);
 
 function toggleDesktopMenu(){
 	const isProductDetailClosed = shoppingCartContainer.classList.contains('inactive');
-	
+
 	if( !isProductDetailClosed)
 		shoppingCartContainer.classList.add('inactive');
+
+
 
 	desktopMenu.classList.toggle('inactive');
 };
@@ -25,22 +30,31 @@ function toggleMenuMobile() {
 	if( !isProductDetailClosed)
 		shoppingCartContainer.classList.add('inactive');
 
+		closeProductDetailIconAside();
+
 	menuMobile.classList.toggle('inactive');
 }
 
 function toggleProductDetail() {
 	const isMobileMenuClosed = menuMobile.classList.contains('inactive');
 	const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+	const isProductDetailSelectClosed = productDetailContainer.classList.contains('inactive');
 
 	if(!isDesktopMenuClosed)
 		desktopMenu.classList.add('inactive');
 
 	if( !isMobileMenuClosed)
-			menuMobile.classList.add('inactive');
+		menuMobile.classList.add('inactive');
+
+	if(!isProductDetailSelectClosed)
+		productDetailContainer.classList.add('inactive');
 
 	shoppingCartContainer.classList.toggle('inactive');
 
 }
+
+
+
 
 const productList = [];
 productList.push({
@@ -61,6 +75,20 @@ productList.push({
 	image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 
 });
+function openProductDetailAside() {
+	// const isProductDetailClosed = shoppingCartContainer.classList.contains('inactive');
+	
+	// if( !isProductDetailClosed)
+		shoppingCartContainer.classList.add('inactive');
+
+
+	productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailIconAside() {
+	productDetailContainer.classList.add('inactive');
+	
+}
 
 function renderProduct(arr){
 	for(product of arr){
@@ -69,7 +97,7 @@ function renderProduct(arr){
 	
 		const img = document.createElement('img');
 		img.setAttribute('src',product.image);
-	
+		img.addEventListener('click', openProductDetailAside);
 	
 	
 		const productInfo = document.createElement('div');
