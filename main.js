73f2cea491ef8ff0fcd63b48d71773764cc2cd11
Hector Valenzuela <francisco1495@gmail.com>
2023-03-02 06:53:53 -0700

    Terminamos el header y pusimos los menus, desktop, mobile, la lista de ordenes y ademas pusimos para que estos desaparescan en un media query eespecifico y que algun menu desaparesco si otro menu es abierto

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 var menuEmail = document.querySelector('.navbar-email');
 var desktopMenu = document.querySelector('.desktop-menu');
+var menubtn = document.querySelector('.menu');
+var mobileMenu = document.querySelector('.mobile-menu');
+var menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+var myOrdermenu = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopmenu);
-
 function toggleDesktopmenu(){
- desktopMenu.classList.toggle('inactive')
+    var isMenuEmailClosed = myOrdermenu.classList.contains('inactive')
+
+    if(!isMenuEmailClosed){
+        myOrdermenu.classList.add('inactive')
+    }
+
+    desktopMenu.classList.toggle('inactive')
 };
 
-console.log('funcionando');
+
+menubtn.addEventListener('click', toggleMobilemenu);
+function toggleMobilemenu(){
+    var isMyOrderMenuClosed = myOrdermenu.classList.contains('inactive')
+
+    if(!isMyOrderMenuClosed){
+        myOrdermenu.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive')
+};
+
+menuCarritoIcon.addEventListener('click', togglemyOrdermenu);
+function togglemyOrdermenu(){
+    var isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    myOrdermenu.classList.toggle('inactive');
+}
