1224ce9e07051019c3428a694f7ff8ee5b6c10db
john Cotamo <johncotamo@hotmail.com>
2023-03-02 07:52:42 -0500

    message

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const menuEmail = document.querySelector('.navbar-email');
 
 const desktopMenu = document.querySelector('.desktop-menu');
 
-const menuHamIcon = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-const menuCarritoIcon = document.querySelector('.navbar-shoping-cart')
-const aside = document.querySelector('.product-detail')
+const menuCarritoIcon = document.querySelector('.navbar-shoping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', togglecarritoMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoMenu);
 
 
 function toggleDesktopMenu() {
@@ -20,6 +20,6 @@ function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive')
 }
 
-function togglecarritoMenu() {
+function toggleCarritoMenu() {
     aside.classList.toggle('inactive')
 }
