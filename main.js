270d795114047452d999ae7e8bc0333b4234f4c4
= <victoriess77@gmail.com>
2023-03-01 21:54:56 -0500

    Menu: burger menu organization

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+// Desktop menu
 const navLink = document.querySelector(".navbar-email");
 const desktopInactive = document.querySelector(".desktop-menu");
-
+// Movile menu
+const burger = document.querySelector(".menu-burger");
+const mobileInactive = document.querySelector(".mobile-menu");
 
 navLink.addEventListener("click", toggleMenu);
+burger.addEventListener("click", toggleBurger)
 
 function toggleMenu() {
     desktopInactive.classList.toggle("inactive");
+}
+function toggleBurger() {
+    mobileInactive.classList.toggle("inactive");
 }
