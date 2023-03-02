4a91b5c15eea33ccd2d1b3609fa4b380907d8f0d
00lar <eduardani84@gmail.com>
2023-03-02 02:00:16 -0700

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const BurguerMenu = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+BurguerMenu.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
     console.log("prueba")
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
 }
