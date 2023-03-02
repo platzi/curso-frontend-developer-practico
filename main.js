6783dbf836a2993b4dbc75f53450001c217d277a
edu2122 <eduardorafaellm@gmail.com>
2023-03-02 11:20:25 -0400

    Se agrego el nuevo aside en el html, entonces ahora tenemos dos aside a los cuales se les borro la clase product-details y se les agrego un id respectivo para cada aside, aparte de eso modificamos algunos componentes en el CSS y HTML para que todo se viera correctamente

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const menuBurger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 const cardsContainer = document.querySelector('.cards-container')
 
@@ -14,18 +14,18 @@ menuBurger.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = !aside.classList.contains('inactive');
+  const isAsideClosed = !shoppingCartContainer.classList.contains('inactive');
   if (isAsideClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
   desktopMenu.classList.toggle('inactive');
   
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = !aside.classList.contains('inactive');
+  const isAsideClosed = !shoppingCartContainer.classList.contains('inactive');
   if (isAsideClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
 
   mobileMenu.classList.toggle('inactive');
@@ -33,12 +33,12 @@ function toggleMobileMenu() {
 
 function toggleCarritoAside() {
   const isMobileMenuClosed = !mobileMenu.classList.contains('inactive');
-  // aside.classList.toggle('inactive');
+  // shoppingCartContainer.classList.toggle('inactive');
     if (isMobileMenuClosed) {
       mobileMenu.classList.add('inactive');
     }
     // si el mobileMenu esta abierto, hay que cerrarlo
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
