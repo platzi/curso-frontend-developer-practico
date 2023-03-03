04d9494c5dc8b6a98837d0117cd2628202d64be5
Jorge Gutierrez <jorgeluis.gutierrez91@gmail.com>
2023-03-03 17:15:39 -0600

    agregar el aside del product-detail y separarlo del shopping-cart

--- main.js
+++ main.js
@@ -4,27 +4,27 @@ const menuCartIcon = document.querySelector(".navbar-shopping-cart")
 
 const desktopMenu = document.querySelector(".desktop-menu")
 const mobileMenu = document.querySelector(".mobile-menu")
-const cartMenu = document.querySelector(".product-detail")
+const cartDetail = document.querySelector("#cartDetail")
 const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
 menuIcon.addEventListener("click", toggleMobileMenu)
-menuCartIcon.addEventListener("click", toggleCartMenu)
+menuCartIcon.addEventListener("click", togglecartDetail)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive")
-    if(!(cartMenu.classList.contains("inactive"))){
-        cartMenu.classList.toggle("inactive")}
+    if(!(cartDetail.classList.contains("inactive"))){
+        cartDetail.classList.toggle("inactive")}
 }
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle("inactive")
-    if(!(cartMenu.classList.contains("inactive"))){
-        cartMenu.classList.toggle("inactive")}
+    if(!(cartDetail.classList.contains("inactive"))){
+        cartDetail.classList.toggle("inactive")}
 }
 
-function toggleCartMenu(){
-    cartMenu.classList.toggle("inactive")
+function togglecartDetail(){
+    cartDetail.classList.toggle("inactive")
     if(!(desktopMenu.classList.contains("inactive"))){
         desktopMenu.classList.toggle("inactive")}
     if(!(mobileMenu.classList.contains("inactive"))){
