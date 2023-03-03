439e3bcf4d4e21488b844a81290be06472263dce
Arley <aguerrero0615@gmail.com>
2023-03-03 12:33:12 -0500

    Feat: Se ajustan los toggle para aparecer y desaparecer los menus en dispositivos desktop y mobile

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIconMobile = document.querySelector('.menuHamMobile');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 
 menuEmail.addEventListener('click', toggleDeskopMenu);
+menuHamIconMobile.addEventListener('click', toggleMobileMenu)
 
 function toggleDeskopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
