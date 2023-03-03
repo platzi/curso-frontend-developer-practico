b4c06a584867a14d45d53cd12a4c60c752dc9dcd
mathiasgodoydev <mathiasgodoy019@gmail.com>
2023-03-03 11:12:59 -0300

    Segunda modificación

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu(){
-    console.log('click');
     desktopMenu.classList.toggle('inactive');
-};
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
