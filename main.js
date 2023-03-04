01dbbc965b00b61932f1edd6684960f5349d099c
Hugo López <hugolopez4791@gmail.com>
2023-03-04 11:37:55 +0100

    Product Detail sin animación y Sombras a los menus

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
-const aside = document.querySelector(".product-detail")
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer")
 const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
@@ -12,17 +12,17 @@ menuCarritoIcon.addEventListener("click", toggleCarritoAside)
 
 // Menus
 function toggleDesktopMenu() {
-    aside.classList.add("inactive")
+    shoppingCartContainer.classList.add("inactive")
     desktopMenu.classList.toggle("inactive")
 }
 function toggleMobileMenu() {
-    aside.classList.add("inactive")
+    shoppingCartContainer.classList.add("inactive")
     mobileMenu.classList.toggle("inactive")
 }
 function toggleCarritoAside() {
     mobileMenu.classList.add("inactive")
     desktopMenu.classList.add("inactive")
-    aside.classList.toggle("inactive")
+    shoppingCartContainer.classList.toggle("inactive")
 } 
 
 // Products
