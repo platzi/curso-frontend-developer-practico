c2bb832d3bafb88049d29e507adb6bb62e1b33aa
Santi Rojo <santi.rojo.franco@gmail.com>
2023-03-04 18:07:35 -0300

    Generando cards de productos escribiendo html  desde un recorrido de array

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -17,7 +18,7 @@ function toggleDesktopMenu() {
     }
 
     desktopMenu.classList.toggle('inactive');
-}
+};
 
 function toggleMobileMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
@@ -27,7 +28,7 @@ function toggleMobileMenu() {
     }
 
     mobileMenu.classList.toggle('inactive');
-}
+};
 
 function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
@@ -43,4 +44,69 @@ function toggleCarritoAside() {
 
     aside.classList.toggle('inactive');
     
+};
+
+function renderProducts(arr){
+    for (product of arr) {
+
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const img = document.createElement('img')
+        img.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        
+        const productInfoDiv = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText = `$ ${product.price}`
+        
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+        
+        
+        const productInfoFigure = document.createElement('figure');
+        
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+        
+        
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+        productInfoFigure.appendChild(productImgCart);
+        
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        
+        productCard.appendChild(img);
+        productCard.appendChild(productInfo);
+        
+        cardsContainer.appendChild(productCard);
+    }
 }
+
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: '230',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Compu',
+    price: '620',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+renderProducts(productList);         
