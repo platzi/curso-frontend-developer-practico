e17c5e551da7f8259b517997779be72fa0852493
Hugo López <hugolopez4791@gmail.com>
2023-03-04 09:03:09 +0100

    Simpleza en el JS del nav interactivo

--- main.js
+++ main.js
@@ -10,34 +10,15 @@ menuHamIcon.addEventListener("click", toggleMobileMenu)
 menuCarritoIcon.addEventListener("click", toggleCarritoAside)
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains("inactive")
-
-    if (!isAsideClosed) {
-        aside.classList.add("inactive")
-    }
-
+    aside.classList.add("inactive")
     desktopMenu.classList.toggle("inactive")
 }
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains("inactive")
-    
-    if (!isAsideClosed) {
-        aside.classList.add("inactive")
-    }
-
+    aside.classList.add("inactive")
     mobileMenu.classList.toggle("inactive")
 }
 function toggleCarritoAside() {
-    const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
-    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive")
-    
-    if (!isMobileMenuClosed) {
-        mobileMenu.classList.add("inactive")
-    }
-    
-    if (!isDesktopMenuClosed) {
-        desktopMenu.classList.add("inactive")
-    }
-
+    mobileMenu.classList.add("inactive")
+    desktopMenu.classList.add("inactive")
     aside.classList.toggle("inactive")
 } 
