b10a3c2c7eb4297b3249258b2ec0e6c815eea02a
hgutierrez1 <hgutierrezva123@gmail.com>
2023-03-04 00:00:10 -0500

    added mobile menu finctionality,class 8 fused

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const menuEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
+const iconHamMenu=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click',toggleDesktopMenu);
+iconHamMenu.addEventListener('click',toggleMobileMenu);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+/*     console.log("click")
+ */    desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu(){
+/*     console.log("click")
+ */    mobileMenu.classList.toggle('inactive');
 }
