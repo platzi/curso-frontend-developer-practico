f2076aa76d2802d26047e76167630efd805ba8db
Gerson Ep <gerson21millones@gmail.com>
2023-03-05 10:21:29 -0600

    C21 - Lista de Productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container');
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);/*Escuchar su evento de click */
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
@@ -51,4 +54,82 @@ function toggleCarritoAside(){
     aside.classList.toggle('inactive');
 }
 
+//Craeacion de array
+const productList =[];
+productList.push({
+    name:'Bike',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name:'Pantalla',
+    price:220,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name:'Compu',
+    price:620,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
 
+
+
+//Maqueta HTML en Javascript Se necesita la referencia de los elementos
+{/*
+
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>  */}
+
+
+//Maqueta HTML en Javascript
+//Insercion de arrays de productos en nuestro HTML: necesario tener la referencia HTML
+function renderProducts(arr){
+for(product of arr){ //da automaticamente el elemento del array. //for(product in productList){ //da el indice del elemento del arrays
+  const productCard =  document.createElement('div');
+  productCard.classList.add('product-card');
+
+  //product= {name,price,image} -> product.image
+  const productImg=document.createElement('img');
+  productImg.setAttribute('src',product.image);
+  
+  const productInfo =  document.createElement('div');
+  productInfo.classList.add('product-info')
+
+
+  const productInfoDiv = document.createElement('div');
+
+  const productPrice =  document.createElement('p');
+  productPrice.innerText = '$'+ product.price;
+
+  const productName = document.createElement('p');
+  productName.innerText=product.name;
+ 
+  productInfoDiv.appendChild(productPrice);
+  productInfoDiv.appendChild(productName);
+
+  const productInfoFigure = document.createElement('figure');
+  const productImgCart = document.createElement('img');
+  productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg')
+
+  productInfoFigure.appendChild(productImgCart);
+
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard);
+}
+}
+renderProducts(productList);
