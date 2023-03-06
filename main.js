7c7050e076459957fd2ecce1e07f74918a39e882
00lar <eduardani84@gmail.com>
2023-03-06 01:15:43 -0700

    Lista de productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const BurguerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const Carrito_icon = document.querySelector(".navbar-shopping-cart");
 const aside_carrito = document.querySelector(".product-detail");
+const cards_container = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 BurguerMenu.addEventListener("click", toggleMobileMenu);
@@ -21,8 +22,8 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
-  const IsCarritoClosed = aside_carrito.classList.contains("inactive");
 
+  const IsCarritoClosed = aside_carrito.classList.contains("inactive");
   if (!IsCarritoClosed) {
     aside_carrito.classList.add("inactive");
   }
@@ -36,3 +37,107 @@ function toggleCar() {
     mobileMenu.classList.add("inactive");
   }
 }
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Pantalla",
+  price: 520,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push ({
+  name:'Bike',
+  price: 12700,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push ({
+  name:'Bicycle helmet',
+  price: 1200,
+  image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+  name:'Bicycle helmet',
+  price: 1600,
+  image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+});
+productList.push ({
+  name:'Bicycle helmet',
+  price: 1500,
+  image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+  name:'Seat',
+  price: 300,
+  image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+productList.push ({
+  name:'Tennis Montain Bike',
+  price: 2200,
+  image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+  name:'Sunglasses',
+  price: 800,
+  image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+  name:'Sunglasses',
+  price: 600,
+  image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push ({
+  name:'Bicycle seat bag',
+  price: 876,
+  image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+}); 
+
+
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    const img = document.createElement('img')
+    img.setAttribute('src',product.image)
+    
+    const productInfo = document.createElement("div")
+    productInfo.classList.add("product-info")
+  
+    const productInfoDiv = document.createElement("div")
+  
+    const p1 = document.createElement("p");
+    p1.innerText="$" + product.price;
+  
+    const p2 = document.createElement("p");
+    p2.innerText = product.name;
+  
+    const figure = document.createElement("figure");
+  
+    const imgcarrito = document.createElement("img")
+    imgcarrito.setAttribute("src","./icons/bt_add_to_cart.svg")
+  
+  
+    productCard.append(img,productInfo);
+    productInfo.append(productInfoDiv,figure)
+    productInfoDiv.append(p1,p2);
+    figure.append(imgcarrito);
+  
+    cards_container.appendChild(productCard);
+    
+  }
+}
+
+renderProducts(productList);
