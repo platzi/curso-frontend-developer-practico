734f6285a7ca44680fea9fdc803edb7af5391181
mathiasgodoydev <mathiasgodoy019@gmail.com>
2023-03-06 16:36:14 -0300

    Cuarta modificación del proyecto

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -40,3 +41,71 @@ function toggleCarritoAside(){
 
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://www.digitalavmagazine.com/wp-content/uploads/2020/03/samsung-t55-a.jpg',
+});
+productList.push({
+    name: 'Computadora',
+    price: 620,
+    image: 'https://png.pngitem.com/pimgs/s/199-1994223_gaming-computer-png-transparent-png.png',
+});
+
+productList.push({
+    name: 'Remera del Club Olimpia',
+    price: 150,
+    image: 'https://img.planetafobal.com/2023/01/olimpia-camiseta-nike-2023-local-trv.jpg',
+});
+productList.push({
+    name: 'Botines Nike Phantom',
+    price: 150,
+    image: 'https://static.nike.com/a/images/t_default/6de4b02f-718c-46cf-a548-da944cf3e836/gripknit-phantom-gx-elite-fg-botas-de-futbol-terreno-firme-C0D3tn.png',
+});
+
+function renderProducts (arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image );
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList);
