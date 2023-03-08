b16a28252603f1404fbe301e90271c14d2fd67ef
arthurchina92 <arthurchinaa@gmail.com>
2023-03-07 20:17:19 -0600

    Merged mobile-menu into nav-bar, made elements responsive using @media query

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+burgerMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
