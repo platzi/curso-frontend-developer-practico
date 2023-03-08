5dfb065de039804c877177c176b8fb234a160e14
Lucxcuy <tkd1709@gmail.com>
2023-03-08 03:29:56 -0300

    Agrego el menu del carrito de compras donde podemos ver la ordy le agrego que los menus no se superpongan al momento de abrirlos

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const burgerMenuIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenuIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+
 
 function toggleDesktopMenu() {
+    aside.classList.add("inactive");
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+    aside.classList.add("inactive");
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+    aside.classList.toggle("inactive");
 }
