698f56482b8a350b7df890f5031bd6169998b0bc
cesar rios <cesar.riosg25@gmail.com>
2023-03-09 01:14:56 -0500

    se agrega menu mobile al icono hamburguesa

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 // SE AGREGA LA FUNCION CLICK AL CORREO DEL USUARIO, PARA APARECER Y DESAPARECER EL MENU DE INGRESO.
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+const menuHamIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
+
+
+
+
+
+//codigo para navbar lado derecho-perfil usuario.
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+//codigo para icono hamburguesa lado izquierdo.
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
+
 }
