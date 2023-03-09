dada69bfe36e9398be7245000bf0c1e14873bf88
Kristian Cifuentes <kdcv2007@hotmail.com>
2023-03-08 19:02:14 -0500

    Agregando mobile menu

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const mailMenu = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 
+const menuHamb = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+
 mailMenu.addEventListener("click",toggleDesktopMenu)
+menuHamb.addEventListener("click",toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive")
 }
