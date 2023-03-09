fbf1585b2eab6a6f0dbbf168c242fa60993263b7
camilo rios <camilototus180@gmail.com>
2023-03-09 11:30:36 -0500

    se organizo en Menu en mobil

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamburger = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamburger.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
 }
- 
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
+}
