3f75015a5783ff71097fea044f570f40e5ab1530
Brian Rodriguez <brian.rodrigueza.23@gmail.com>
2023-03-09 14:45:50 -0500

    Se empieza la fusion de HTML y css por separado junto con JS

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuEmail = document.querySelector(".navbar-email");
+const destktopMenu = document.querySelector(".desktop-menu");
+
+
+menuEmail.addEventListener("click", toggleDesktopMenu)
+
+function toggleDesktopMenu () {
+    destktopMenu.classList.toggle ("inactive");
+}
