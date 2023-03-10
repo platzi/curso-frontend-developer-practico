196ea1a7cfdc37d7bc26776f6c97fd0805a56647
Brian Rodriguez <brian.rodrigueza.23@gmail.com>
2023-03-10 00:48:42 -0500

    Menus destktop integrados con mobile sin conflictos

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
 const menuEmail = document.querySelector(".navbar-email");
 const destktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
 menuHamIcon.addEventListener("click", toggleMobileMenu)
+menuCarritoIcon.addEventListener("click", toggleCarritoAside)
 
 function toggleDesktopMenu () {
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add ("inactive");
+    }
+
     destktopMenu.classList.toggle ("inactive");
 }
 
 function toggleMobileMenu () {
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add ("inactive");
+    }
+
     mobileMenu.classList.toggle ("inactive");
+}
+
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isMenuDestktopClosed = destktopMenu.classList.contains("inactive");
+ 
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add ("inactive");
+    }
+
+    if (!isMenuDestktopClosed) {
+        destktopMenu.classList.add ("inactive");
+    }
+
+    aside.classList.toggle ("inactive");
 }
