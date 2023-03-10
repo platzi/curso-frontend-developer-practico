a4b56b64cabebf01d85fea04061fe69e336ea171
Brian Rodriguez <brian.rodrigueza.23@gmail.com>
2023-03-09 23:41:20 -0500

    Agregado los menus tanto de desktop como de mobile c:

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const destktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+menuHamIcon.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu () {
     destktopMenu.classList.toggle ("inactive");
+}
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle ("inactive");
 }
