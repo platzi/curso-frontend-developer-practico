0543ae1084de2e5115f4c2ef463aa123b6161058
camilo rios <camilototus180@gmail.com>
2023-03-10 17:26:10 -0500

    Se integro el Menu de carrito de compras y se organizo que cuando se abre uno cierra el otro

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamburger = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamburger.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClose = aside.classList.contains("inactive");
+
+  if (!isAsideClose) {
+    aside.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle("inactive")
+  const isAsideClose = aside.classList.contains("inactive");
+
+  if (!isAsideClose) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDesktopMenu = desktopMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  } else if (!isDesktopMenu) {
+    desktopMenu.classList.add("inactive")
+  }
+  aside.classList.toggle("inactive");
 }
