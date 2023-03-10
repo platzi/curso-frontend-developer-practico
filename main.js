6240565643902f7725884dea4d37d4db379325bd
gipiampa <gipiampa_@hotmail.com>
2023-03-10 18:32:55 -0300

    adaptamos el menu mobile con javascript

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ let navbarEmail = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu"); 
 navbarEmail.addEventListener("click", mostrarMenu);
 
+let mobileMenu = document.querySelector(".mobile-menu");
+let imgMobileMenu = document.querySelector(".menu");
+imgMobileMenu.addEventListener("click", mostrarMenuMobile);
+
+
 function mostrarMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function mostrarMenuMobile(){
+    mobileMenu.classList.toggle("inactive");
 }
