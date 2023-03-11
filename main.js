5d1c4cb8c783ddfe4282ad1923f3f1d1eb011105
gipiampa <gipiampa_@hotmail.com>
2023-03-11 11:54:31 -0300

    haciendo cositas

--- main.js
+++ main.js
@@ -6,11 +6,23 @@ let mobileMenu = document.querySelector(".mobile-menu");
 let imgMobileMenu = document.querySelector(".menu");
 imgMobileMenu.addEventListener("click", mostrarMenuMobile);
 
+let carritoDeCompras = document.querySelector(".navbar-shopping-cart");
+let productDetail = document.querySelector(".product-detail");
+carritoDeCompras.addEventListener("click", mostrandoCarrito);
 
 function mostrarMenu(){
     desktopMenu.classList.toggle("inactive");
+    productDetail.classList.add("inactive");
 }
 
 function mostrarMenuMobile(){
     mobileMenu.classList.toggle("inactive");
+    productDetail.classList.add("inactive");
+}
+
+function mostrandoCarrito(){
+    productDetail.classList.toggle("inactive");
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+    
 }
