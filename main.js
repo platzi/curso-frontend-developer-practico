3db0b4a3f0d09db37e663b7d5c3e10cf43e9944d
Brian Rodriguez <brian.rodrigueza.23@gmail.com>
2023-03-12 01:21:33 -0500

    Agregue carrito de compras y arreglamos cosas de CSS

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const destktopMenu = document.querySelector(".desktop-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container")
 
 
@@ -22,10 +22,10 @@ function toggleDesktopMenu () {
 }
 
 function toggleMobileMenu () {
-    const isAsideClosed = aside.classList.contains("inactive");
+    const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
     if (!isAsideClosed) {
-        aside.classList.add ("inactive");
+        shoppingCartContainer.classList.add ("inactive");
     }
 
     mobileMenu.classList.toggle ("inactive");
@@ -43,7 +43,7 @@ function toggleCarritoAside () {
         destktopMenu.classList.add ("inactive");
     }
 
-    aside.classList.toggle ("inactive");
+    shoppingCartContainer.classList.toggle ("inactive");
 }
 
 const productList = [];
