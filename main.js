2e0d314d750c25ce4e34ac28ca4f96bf8259df0a
edison <ebg.0495@gmail.com>
2023-03-12 15:25:47 -0500

    Fusion del menu en mobile y corrección de media query

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',togglemobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+
+function togglemobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
