94282c7141951599df90cd710ab33eca2edb8bcd
AndresMCardenas <andrescar18@gmail.com>
2023-03-12 16:20:19 -0500

    agragado carrito a JS en mobile y desktop

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+  
+  const isAsideMenuClose = aside.classList.contains('inactive');
+
+  if (!isAsideMenuClose) {
+    aside.classList.add('inactive');
+  }
  }
 
 function toggleMobileMenu() {
- mobileMenu.classList.toggle('inactive');
+  const isAsideMenuClose = aside.classList.contains('inactive');
+
+  if (!isAsideMenuClose) {
+    aside.classList.add('inactive');
+  }
+  mobileMenu.classList.toggle('inactive');
+
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClose) {
+    mobileMenu.classList.add('inactive');
+  }
+  
+  aside.classList.toggle('inactive');
+
 }
