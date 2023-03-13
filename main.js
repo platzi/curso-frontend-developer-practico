7aad32b6f8e034abd23dd5c83dd3232085842bb4
Julen Alvaro <julenalvaro@gmail.com>
2023-03-13 18:28:44 +0100

    menús de carrito y menú de la izquierda

--- main.js
+++ main.js
@@ -3,14 +3,32 @@ console.log('main.js loaded');
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const botonMenuMovil = document.querySelector('#boton-menu-movil');
+const menuMovil = document.querySelector('.mobile-menu');
 
-// si se hace click sobre navbarEmail, se le quita la clase .inactive a desktopMenu
+const botonCarrito = document.querySelector('.navbar-shopping-cart');
+const menuCarrito = document.querySelector('.product-detail');
 
-navbarEmail.addEventListener('click', () => {
-  console.log('click en navbarEmail')
-    desktopMenu.classList.toggle('inactive');
-});
+navbarEmail.addEventListener('click', toggleDesktopMenu);
+botonMenuMovil.addEventListener('click', toggleMobileMenu);
+botonCarrito.addEventListener('click', toggleCarrito);
 
+function toggleDesktopMenu(){
+  desktopMenu.classList.toggle('inactive');
+}
 
+function toggleMobileMenu(){
+  // si el menuCarrito esta abierto, lo cierro
+  if(!menuCarrito.classList.contains('inactive')){
+    toggleCarrito();
+  }
+  menuMovil.classList.toggle('inactive');
+}
 
-
+function toggleCarrito(){
+  // si el menuMovil esta abierto, lo cierro
+  if(!menuMovil.classList.contains('inactive')){
+    toggleMobileMenu();
+  }
+  menuCarrito.classList.toggle('inactive');
+}
