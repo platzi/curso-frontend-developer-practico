e4d6d56afc87208c713b07748441b0f749e25e68
ginogiorgi <ginorubengiorgi@gmail.com>
2023-03-14 14:02:10 -0300

    Clase 21 curso de practica de Javascript

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuHamburguer = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCart = document.querySelector('.navbar-shopping-cart');
 const productDetail = document.querySelector('.product-detail');
+const productList = [];
+const cardsContainer = document.querySelector('.cards-container')
 
 function toggleDesktopMenu(){
     const isProductDetailOpen = !productDetail.classList.contains('inactive');
@@ -30,8 +32,60 @@ function toggleMobileMenu(){
     }
     mobileMenu.classList.toggle('inactive');
 }
+function renderProducts(arr){
+    for (product of productList){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        
+        cardsContainer.appendChild(productCard);
+    }
+}
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 shoppingCart.addEventListener('click', toggleProductDetail);
 menuHamburguer.addEventListener('click', toggleMobileMenu);
+productList.push({
+    name: 'Bike',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Computadora',
+    price: '620',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: '220',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+renderProducts(productList);
+
 
