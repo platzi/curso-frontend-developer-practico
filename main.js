7b4d1848a493adf92154dffafba602f8da23e063
Alex Camarillo <alex7camarillo@gmail.com>
2023-03-13 21:31:30 -0600

    Agregando funcionalidad MenuMobile

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+
 
 navEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu () {
     console.log('click');
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu () {
+    console.log('click');
+    mobileMenu.classList.toggle('inactive');
 }
