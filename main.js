3ec2255badc561f95b08f7a969549f8436183fd5
germa099 <germainramirez11@gmail.com>
2023-03-14 11:08:23 -0600

    Agregamos el produc detail

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const userMenu = document.querySelector(".desktop-menu");
 const burguerIcon = document.querySelector(".menu");
 const burguerMenu = document.querySelector(".mobile-menu");
 const cartIcon = document.querySelector(".navbar-shopping-cart");
-const cartDetail = document.querySelector(".product-detail");
+const cartDetail = document.querySelector("#cart-detail");
 const cardsContainer = document.querySelector(".cards-container");
+const asaideProductDetail = document.querySelector("#product-detail");
 
 navbarEmail.addEventListener("click", togglerUserMenu);
 
@@ -58,12 +59,14 @@ function togglerUserMenu() {
 function togglerCartDetail() {
   userMenu.classList.add("inactive");
   burguerMenu.classList.add("inactive");
+  asaideProductDetail.classList.add("inactive");
 
   cartDetail.classList.toggle("inactive");
 }
 
 function togglerBurguerMenu() {
   cartDetail.classList.add("inactive");
+  asaideProductDetail.classList.add("inactive");
 
   burguerMenu.classList.toggle("inactive");
 }
@@ -75,51 +78,69 @@ productList.push({
   price: 12700,
   image:
     "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  description:
+    "Bicicleta Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vel? Expedita in consequuntur praesentium architecto error dolore veritatis numquam, unde perspiciatis ipsa, qui culpa iste eveniet vero sit sed iure.",
 });
 productList.push({
   name: "Bicycle helmet",
   price: 1200,
   image:
     "https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto",
+  description:
+    "Casco Bici Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vel? Expedita in consequuntur praesentium architecto error dolore veritatis numquam, unde perspiciatis ipsa, qui culpa iste eveniet vero sit sed iure.",
 });
 productList.push({
   name: "Bicycle helmet",
   price: 1600,
   image: "https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg",
+  description:
+    "Casco Bici 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vel? Expedita in consequuntur praesentium architecto error dolore veritatis numquam, unde perspiciatis ipsa, qui culpa iste eveniet vero sit sed iure.",
 });
 productList.push({
   name: "Bicycle helmet",
   price: 1500,
   image:
     "https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto",
+  description:
+    "Casco Bici 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vel? Expedita in consequuntur praesentium architecto error dolore veritatis numquam, unde perspiciatis ipsa, qui culpa iste eveniet vero sit sed iure.",
 });
 productList.push({
   name: "Seat",
   price: 300,
   image: "https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg",
+  description:
+    "Silla Bici Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vel? Expedita in consequuntur praesentium architecto error dolore veritatis numquam, unde perspiciatis ipsa, qui culpa iste eveniet vero sit sed iure.",
 });
 productList.push({
   name: "Tennis Montain Bike",
   price: 2200,
   image:
     "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg",
+  description:
+    "TENIS Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vel? Expedita in consequuntur praesentium architecto error dolore veritatis numquam, unde perspiciatis ipsa, qui culpa iste eveniet vero sit sed iure.",
 });
 productList.push({
   name: "Sunglasses",
   price: 800,
   image:
     "https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602",
+  description:
+    "GAFAS Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vel? Expedita in consequuntur praesentium architecto error dolore veritatis numquam, unde perspiciatis ipsa, qui culpa iste eveniet vero sit sed iure.",
 });
 productList.push({
   name: "Sunglasses",
   price: 600,
   image:
     "https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603",
+  description:
+    "GAFAS 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vel? Expedita in consequuntur praesentium architecto error dolore veritatis numquam, unde perspiciatis ipsa, qui culpa iste eveniet vero sit sed iure.",
 });
 productList.push({
   name: "Bicycle seat bag",
   price: 876,
   image: "https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg",
+  description:
+    "SACO RARO Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vel? Expedita in consequuntur praesentium architecto error dolore veritatis numquam, unde perspiciatis ipsa, qui culpa iste eveniet vero sit sed iure.",
 });
 
 createCards(productList);
@@ -131,6 +152,11 @@ function createCards(array) {
 
     const imgProduct = document.createElement("img");
     imgProduct.setAttribute("src", product.image);
+    imgProduct.classList.add("product-card-img");
+
+    imgProduct.addEventListener("click", () => {
+      togglerProductDetail(array);
+    });
 
     const productInfo = document.createElement("div");
     productInfo.classList.add("product-info");
@@ -160,5 +186,23 @@ function createCards(array) {
     productCard.append(productInfo);
 
     cardsContainer.append(productCard);
+
+    function togglerProductDetail(i) {
+      console.log(i);
+      const long = i.length;
+      console.log(long);
+
+      //ABRIR Y CERRAR
+      const asaideProductDetail = document.querySelector("#product-detail");
+      const productDetailClose = document.querySelector(
+        ".product-detail-close"
+      );
+      productDetailClose.addEventListener("click", () => {
+        asaideProductDetail.classList.add("inactive");
+      });
+      asaideProductDetail.classList.toggle("inactive");
+      burguerMenu.classList.add("inactive");
+      cartDetail.classList.add("inactive");
+    }
   }
 }
