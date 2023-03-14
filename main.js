dc0651dbc5604743688a82b2e6fc90a0cc110fe5
jstevenfm <jstevenfm@gmail.com>
2023-03-14 09:27:47 -0500

    feat - Mobile menu fusion

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+iconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
