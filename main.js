9e0b9e3a3a66361e573239fae98e0ae46870a1be
Luis Cantor <creacionfinanciera@gmail.com>
2023-03-14 16:50:20 -0500

    Esta es la fusion del menu mobile

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ const menuEmail = document.querySelector('.navbar-email');
 //declaramos una variable para llamar la clase que va a aparecer cuando el usuario de clic
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 //con este comando, indicamos que cuando el usuario de click, aparezca o desaparezca según lo que el usuario haga
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 //pero antes debo crear la función que llamará el anterior comando, la función lo que hace es ejecutar la función classList.toggle que quita o pone la clase inactive, dependiendo de si ya la tiene o no la tiene
 function toggleDesktopMenu() {
-    console.log('Click');
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('JS funcionando');
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
