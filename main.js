811554084995f592690bd7a83db8e6c4eb2946c2
Alex Camarillo <alex7camarillo@gmail.com>
2023-03-14 08:23:50 -0600

    Cambios en el menú

--- main.js
+++ main.js
@@ -2,18 +2,38 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu () {
-    console.log('click');
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.toggle('inactive');
+    }  
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu () {
-    console.log('click');
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.toggle('inactive');
+    }      
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.toggle('inactive');
+    }     
+    aside.classList.toggle('inactive');
 }
