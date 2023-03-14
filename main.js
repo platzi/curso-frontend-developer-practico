0b575c7f1c0af591d48bf12aab12abef6845e3b7
camilo rios <camilototus180@gmail.com>
2023-03-14 09:57:20 -0500

    unimos el Aside Detalles del producto con su CSS Y HTML y organizamos la Info de ambps aside

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamburger = document.querySelector(".menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -11,19 +11,19 @@ menuHamburger.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  const isAsideClose = aside.classList.contains("inactive");
+  const isAsideClose = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClose) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  const isAsideClose = aside.classList.contains("inactive");
+  const isAsideClose = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClose) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
   mobileMenu.classList.toggle("inactive");
 }
@@ -37,7 +37,7 @@ function toggleCarritoAside() {
   } else if (!isDesktopMenu) {
     desktopMenu.classList.add("inactive");
   }
-  aside.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
