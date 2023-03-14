fb9d0ed3f42e5e68eedbca6db87ea3fadd046f8a
ginogiorgi <ginorubengiorgi@gmail.com>
2023-03-14 11:13:57 -0300

    Clase 19 curso practico Javascript

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburguer = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguer.addEventListener('click', toggleMobileMenu);
 
