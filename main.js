0b5c41d1183b961c45c08f90e91f78ed2da869f4
Anderson <anderson.asm201@gmail.com>
2023-03-14 13:07:09 -0500

    Fusion de menu mobile, despliegue con js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const navEmail = document.querySelector('.navbar-email')
-const DesktopMenu = document.querySelector('.desktop-menu')
+const navEmail = document.querySelector('.navbar-email');
+const DesktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-navEmail.addEventListener('click' , toggleDesktopMenu)
+navEmail.addEventListener('click' , toggleDesktopMenu);
+menuHamIcon.addEventListener('click' , toggleMobileMenu);
 
 function toggleDesktopMenu(){
-    DesktopMenu.classList.toggle('inactive')
-}
+    DesktopMenu.classList.toggle('inactive');
+};
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+};
