73bdad4b03e31d6dd86979a67e3d207489734376
negagl <nega_gl@outlook.com>
2023-03-15 00:55:05 -0500

    Added mobile menu

--- code/main.js
+++ code/main.js
@@ -2,9 +2,18 @@ const $ = (selector) => document.querySelector(selector);
 
 const menuOpt = $(".desktop-menu");
 const navRight = $(".navbar-email");
+const burgerMenu = $(".burgerMenu");
+const menuMobile = $(".mobile-menu");
+
 
 function toggleMenu(){
     !menuOpt.hasAttribute("hidden") ? menuOpt.setAttribute("hidden", "") : menuOpt.removeAttribute("hidden");
+    // menuOpt.classList.toggle('clase'); coloca o quita la clase
 }
 
-const clickOpt = navRight.addEventListener("click", toggleMenu);
+function toggleMenuMobile(){
+    menuMobile.toggleAttribute("hidden");
+}
+
+const clickOptGral = navRight.addEventListener("click", toggleMenu);
+const clickOptMob = burgerMenu.addEventListener("click", toggleMenuMobile);
