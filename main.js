376eee3a510e1548bdad299f1d035a38c82be282
Saitamah07 <matias279ah@gmail.com>
2023-03-15 09:40:37 -0500

    iteraccion entre todos los componentes

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const burguerMenu = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -27,6 +30,8 @@ if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive');
 }
 
+closeProductDetailAside();
+
 mobileMenu.classList.toggle('inactive');
 }
 
@@ -37,7 +42,22 @@ if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');
 }
 
+const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
+if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive');
+}
+
 shoppingCartContainer.classList.toggle('inactive');
+}
+
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -77,9 +97,12 @@ function renderProducts(arr){
     for (product of arr){
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
+
+
           // Product = {name, price, image} --> product.image
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside)
 
 
         const productInfo = document.createElement('div');
