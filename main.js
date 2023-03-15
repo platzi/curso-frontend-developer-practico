97e1b55de39ab8245cc243bb7961d9123555fb29
Luis Cantor <creacionfinanciera@gmail.com>
2023-03-15 11:52:20 -0500

    Esta es la fusion del carrito de compras

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 //con este comando, indicamos que cuando el usuario de click, aparezca o desaparezca según lo que el usuario haga
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 //pero antes debo crear la función que llamará el anterior comando, la función lo que hace es ejecutar la función classList.toggle que quita o pone la clase inactive, dependiendo de si ya la tiene o no la tiene
 function toggleDesktopMenu() {
@@ -17,6 +20,23 @@ function toggleDesktopMenu() {
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');    
+    }
+    
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.contains('inactive')    
+    }
+
+    aside.classList.toggle('inactive');
+   
 }
 
