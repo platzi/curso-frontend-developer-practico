08a61709bba8f322271eb042f32f62ea6bfda6b4
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2023-03-14 21:52:37 -0500

    Pendiente cierre de ventanas desplegadas al intentar abrir otra

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const cartMenu = document.querySelector('.navbar-shopping-cart');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const asideMenu = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
-burguerMenu.addEventListener('click', toggleMobileMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
+cartMenu.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
@@ -13,4 +16,9 @@ function toggleDesktopMenu(){
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCartAside(){
+    asideMenu.classList.toggle('inactive');
+}
+
 
