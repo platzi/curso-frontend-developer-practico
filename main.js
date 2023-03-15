6b401099076264a3376d16ec4b1354194cb6b88c
renzobuendia <renzobuendia@gmail.com>
2023-03-14 22:11:20 -0400

    Carrito de compras agregado con CSS y JavaScript

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
  
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+
+    const isAsideOpen = !aside.classList.contains('inactive');
+    if (isAsideOpen) {
+        aside.classList.add('inactive');
+    }  
 }
 
 function toggleMobileMenu(){
+    const isAsideOpen = !aside.classList.contains('inactive');
+
     mobileMenu.classList.toggle('inactive');
+    if (isAsideOpen) {
+        aside.classList.add('inactive');
+    }  
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+
+    aside.classList.toggle('inactive');
+    if (isMobileMenuOpen) {
+        mobileMenu.classList.add('inactive');
+    }
+    if (isDesktopMenuOpen) {
+        desktopMenu.classList.add('inactive');
+    }
 }
