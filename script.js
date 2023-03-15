caebaaea5d8de279f47487aaadaad8a9af02501b
Sebastian Varon <sebastianvaron96@gmail.com>
2023-03-15 13:10:38 -0400

    se crea funcion para el ciclo que recorre el array

--- script.js
+++ script.js
@@ -53,32 +53,59 @@ producList.push({
     name: 'Bike',
     price: 120,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});producList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});producList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});producList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});producList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});producList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});producList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
-
-for (const product of producList) {
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
-    const productImg = document.createElement('img')
-    productImg.setAttribute('src', product.image);
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
-    const productInfoDiv = document.createElement('div');
-    const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price;
-    const productName = document.createElement('p');
-    productName.innerText = product.name;
-
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
-
-    const productInfoFigure =  document.createElement('figure');
-    const productImgCart =  document.createElement('img');
-    productImgCart.setAttribute('src', "./icons/bt_add_to_cart.svg");
-
-    productInfoFigure.appendChild(productImgCart);
-    productInfo.appendChild(productInfoDiv);
-    productInfo.appendChild(productInfoFigure);
-    productCard.appendChild(productImg);
-    productCard.appendChild(productInfo);
-    cardsContainer.appendChild(productCard);
+function renderProducts(objetc) {
+    for (const product of objetc) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        const productImg = document.createElement('img')
+        productImg.setAttribute('src', product.image);
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure =  document.createElement('figure');
+        const productImgCart =  document.createElement('img');
+        productImgCart.setAttribute('src', "./icons/bt_add_to_cart.svg");
+    
+        productInfoFigure.appendChild(productImgCart);
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        cardsContainer.appendChild(productCard);
+    }
 }
+
+renderProducts(producList);
