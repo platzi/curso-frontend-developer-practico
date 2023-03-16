0ffe0f53f45aade6578d3e29f28fe4d3c4250091
negagl <nega_gl@outlook.com>
2023-03-16 00:45:01 -0500

    Added Cart. Items list in progress

--- code/main.js
+++ code/main.js
@@ -4,16 +4,29 @@ const menuOpt = $(".desktop-menu");
 const navRight = $(".navbar-email");
 const burgerMenu = $(".burgerMenu");
 const menuMobile = $(".mobile-menu");
+const cart = $(".navbar-shopping-cart");
+const cartList = $(".product-detail");
 
+// Events
+const clickOptGral = navRight.addEventListener("click", toggleMenu);
+const clickOptMob = burgerMenu.addEventListener("click", toggleMenuMobile);
+const clickCartGral = cart.addEventListener("click", toggleCart);
 
 function toggleMenu(){
     !menuOpt.hasAttribute("hidden") ? menuOpt.setAttribute("hidden", "") : menuOpt.removeAttribute("hidden");
+    menuMobile.setAttribute("hidden", "");
+    cartList.setAttribute("hidden", "");
     // menuOpt.classList.toggle('clase'); coloca o quita la clase
 }
 
 function toggleMenuMobile(){
     menuMobile.toggleAttribute("hidden");
+    cartList.setAttribute("hidden", "");
+    menuOpt.setAttribute("hidden", "");
 }
 
-const clickOptGral = navRight.addEventListener("click", toggleMenu);
-const clickOptMob = burgerMenu.addEventListener("click", toggleMenuMobile);
+function toggleCart(){
+    cartList.toggleAttribute("hidden");
+    menuMobile.setAttribute("hidden", "");
+    menuOpt.setAttribute("hidden", "");
+}
