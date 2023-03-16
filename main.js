700fa8a709960d405c21188c4bf1ac14cac5e96d
juanpa0180 <102774270+juanpa0180@users.noreply.github.com>
2023-03-15 20:55:14 -0500

    anadi toggle a menu hamburg, carrito y email

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-const menuemail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuIzquierda = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-menuemail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
+}
+
+menuIzquierda.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopClosed = desktopMenu.classList.contains('inactive');
+    
+    if(!isMobileMenuClosed || !isDesktopClosed){
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
