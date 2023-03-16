7904bd3bf7b91188965f4fd9f5ff9ae7a1d02205
Anderson <anderson.asm201@gmail.com>
2023-03-16 15:53:28 -0500

    Agregado el aside con los estilos CSS}

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const DesktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click' , toggleDesktopMenu);
@@ -12,20 +12,20 @@ menuCarritoIcon.addEventListener('click' , toggleCarritoAside);
 
 
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if(!isAsideClosed){
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
     
     DesktopMenu.classList.toggle('inactive');
 };
 
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if(!isAsideClosed){
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
     
     mobileMenu.classList.toggle('inactive');
@@ -41,7 +41,7 @@ function toggleCarritoAside(){
         DesktopMenu.classList.add('inactive')
     }
 
-    aside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
     
 };
 
