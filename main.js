a5e9cffcdeca9cfe2100e8a0f8dd9f44ea91d0d4
Anderson <anderson.asm201@gmail.com>
2023-03-16 15:17:04 -0500

    Creacion de cards apartir de arrays

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click' , toggleDesktopMenu);
 menuHamIcon.addEventListener('click' , toggleMobileMenu);
@@ -43,3 +44,84 @@ function toggleCarritoAside(){
     aside.classList.toggle('inactive')
     
 };
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Tv',
+    price: 180,
+    image: 'https://images.samsung.com/is/image/samsung/p6pim/co/qn55qn85bakxzl/gallery/co-qled-qn85b-qn55qn85bakxzl-532188131?$650_519_PNG$',
+});
+productList.push({
+    name: 'Celular',
+    price: 300,
+    image: 'https://itechcolombia.co/wp-content/uploads/2022/10/261987_ys8mqf.png',
+});
+productList.push({
+    name: 'Computador',
+    price: 620,
+    image: 'https://exitocol.vtexassets.com/arquivos/ids/15994985/COMPUTADOR-PORTATIL-HP-AMD-Ryzen-3-5500U-12-GB-256-GB-SSD-15-ef2504la-3249875_a.jpg?v=638072554528430000',
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Tv',
+    price: 180,
+    image: 'https://images.samsung.com/is/image/samsung/p6pim/co/qn55qn85bakxzl/gallery/co-qled-qn85b-qn55qn85bakxzl-532188131?$650_519_PNG$',
+});
+productList.push({
+    name: 'Celular',
+    price: 300,
+    image: 'https://itechcolombia.co/wp-content/uploads/2022/10/261987_ys8mqf.png',
+});
+productList.push({
+    name: 'Computador',
+    price: 620,
+    image: 'https://exitocol.vtexassets.com/arquivos/ids/15994985/COMPUTADOR-PORTATIL-HP-AMD-Ryzen-3-5500U-12-GB-256-GB-SSD-15-ef2504la-3249875_a.jpg?v=638072554528430000',
+});
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const ProductImg = document.createElement('img');
+        ProductImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productInfoPrice = document.createElement('p');
+        productInfoPrice.innerText = '$' + product.price;
+        const productInfoName = document.createElement('p');
+        productInfoName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productInfoPrice);
+        productInfoDiv.appendChild(productInfoName);
+    
+        const productInfoFigure = document.createElement('figure');
+        
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src' , './icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productImgCard);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(ProductImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard)
+    } 
+}  
+renderProducts(productList);
