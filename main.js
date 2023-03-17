84bfb49b1b933ab8eedee982825705c3a096e1e7
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2023-03-16 21:50:32 -0500

    Se agregan más funcionalidades a la página (home, regresar, entre otros)

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
+const homeMenu = document.querySelector('.logo')
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const cartMenu = document.querySelector('.navbar-shopping-cart');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const asideMenu = document.querySelector('.product-detail');
-const productDetailContainer = document.querySelector('.product-detail-main')
-const productDetailCloseIcon = document.querySelector('.product-detail-close')
-const cardsContainer= document.querySelector('.cards-container')
+const productDetailContainer = document.querySelector('.product-detail-main');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
+const cardsContainer= document.querySelector('.cards-container');
+const titleMyOrderMenu = document.querySelector('#arrow-image');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
 cartMenu.addEventListener('click', toggleCartAside);
 productDetailCloseIcon.addEventListener('click',closeProductDetailMain)
+homeMenu.addEventListener('click',goHomePage)
+titleMyOrderMenu.addEventListener('click',toggleCartAside)
 
+
+function goHomePage(){
+    location.href='./index.html';
+}
 
 function toggleDesktopMenu(){
 
     const IsAsideMenuClosed= asideMenu.classList.contains('inactive');
+    
 
     if (!IsAsideMenuClosed){
         //abrir el menu mobile
@@ -52,7 +61,8 @@ function toggleCartAside(){
 
     const IsMobileMenuClosed= mobileMenu.classList.contains('inactive');
     const IsDesktopMenuClosed= desktopMenu.classList.contains('inactive');
-    const IsProductDetailClosed= productDetailContainer.classList.contains('inactive')
+    const IsProductDetailClosed= productDetailContainer.classList.contains('inactive');
+    /* const IsTitleMyOrderMenuClosed = titleMyOrderMenu.classList.contains('inactive'); */
 
     if (!IsMobileMenuClosed){
         //abrir el aside
@@ -84,6 +94,7 @@ function toggleCartAside(){
 }
 
 function openProductDetailMain(){
+
 
 
     asideMenu.classList.add('inactive');
