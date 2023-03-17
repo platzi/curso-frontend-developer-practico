02425cd452c1242c66f3c7069c221c2861152693
Cosmo <johan.amador200@gmail.com>
2023-03-17 06:37:44 -0500

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var menuEmail = document.querySelector('.navbar-email');
 var desktopMenu = document.querySelector('.desktop-menu');
+var menuHamIcon = document.querySelector('.menu');
+var mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
