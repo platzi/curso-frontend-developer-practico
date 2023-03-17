62f644be7c35ffe8f3828e6afab6f03c24b3eb60
= <victoriess77@gmail.com>
2023-03-16 21:50:01 -0500

    New: shopping cart running

--- main.js
+++ main.js
@@ -4,13 +4,23 @@ const desktopInactive = document.querySelector(".desktop-menu");
 // Movile menu
 const burger = document.querySelector(".menu-burger");
 const mobileInactive = document.querySelector(".mobile-menu");
+// Shopping car
+const productDetail = document.querySelector(".product-detail")
+const shoppingCar = document.querySelector(".navbar-shopping-cart")
 
 navLink.addEventListener("click", toggleMenu);
 burger.addEventListener("click", toggleBurger)
+shoppingCar.addEventListener("click", toggleShoppingCar);
 
 function toggleMenu() {
     desktopInactive.classList.toggle("inactive");
 }
 function toggleBurger() {
+    productDetail.classList.add("inactive");
     mobileInactive.classList.toggle("inactive");
+}
+
+function toggleShoppingCar() {
+    mobileInactive.classList.add("inactive");
+    productDetail.classList.toggle("inactive");
 }
