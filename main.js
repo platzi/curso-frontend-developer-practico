22825aee3dad94c6ff3f3bac1d63353361e37180
Julián Gómez <juliuis23@gmail.com>
2023-03-17 12:13:13 -0500

    Hamburguer menu responsive

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleBurguerMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
-    console.log("click")
+}
+
+function toggleBurguerMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
