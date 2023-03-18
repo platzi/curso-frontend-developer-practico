02aa63b584994e93771366d4dd1d7c8f499520ec
lucho <lavgamer777@gmail.com>
2023-03-18 18:43:39 -0500

    Olvide hacer commit antes pero añadi el menu de la cuenta desplegable con un link y lo mismo pero con el menu burgir en mobile

--- matrix.js
+++ matrix.js
@@ -1,8 +1,33 @@
 const navEmail = document.querySelector('.navbar-email')
-const navMenu = document.querySelector('.desktop-menu')
+const navMenuEmail = document.querySelector('.desktop-menu')
+const menuBurger = document.querySelector('.menu-burger')
+const mobileMenu = document.querySelector('.mobile-menu')
+const carritoDesktopLogo = document.querySelector('.navbar-shopping-cart')
+const carritoDesktopList = document.querySelector('.product-detail')
+
+
 navEmail.addEventListener('click', toggleMenuVar)
+menuBurger.addEventListener('click', toggleMobileMenu)
+carritoDesktopLogo.addEventListener('click', toggleCarritoCompras)
 
 function toggleMenuVar() {
-    navMenu.classList.toggle('inactive');
+    if (!carritoDesktopList.classList.contains('inactive')) {
+        carritoDesktopList.classList.toggle('inactive');
+    }
+    
+    navMenuEmail.classList.toggle('inactive');
 }
-console.log('sds');
+function toggleMobileMenu() {
+    if (!carritoDesktopList.classList.contains('inactive')) {
+        carritoDesktopList.classList.toggle('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoCompras() {
+    if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.toggle('inactive');
+    }
+
+    carritoDesktopList.classList.toggle('inactive');
+}
