a8db3ad7408fca84d2b3ed4da8b7e259c6f7fc9d
yerson castro <yertb12345@gmail.com>
2023-03-18 18:47:56 -0500

    agregando seccion de carrito de compras y funciones para que no haya interferencia en las secciones

--- js/main.js
+++ js/main.js
@@ -1,14 +1,49 @@
+
+// CONST
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurger = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const shoppingCar = document.querySelector('.navbar-shopping-cart')
+const productDetails = document.querySelector('.product-detail')
 
+//NAME FUNCION
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurger.addEventListener('click', toggleMobileMenu);
+shoppingCar.addEventListener('click', showMyOrders)
+
+//DESKTOP MENU
 
 function toggleDesktopMenu(){
+    const isProductDetailsClosed =  productDetails.classList.contains('inactive');
+
+    if (!isProductDetailsClosed){
+        productDetails.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
+
+// MOBILE MENU
 function toggleMobileMenu(){
+
+    const isProductDetailsClosed =  productDetails.classList.contains('inactive');
+
+    if (!isProductDetailsClosed){
+        productDetails.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
 }
+
+
+// MY CAR DESPLEGABLE
+
+
+
+function showMyOrders(){
+    mobileMenu.classList.add('inactive')
+    productDetails.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+
+};
+
