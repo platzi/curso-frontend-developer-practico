bbe3fed1731b5c3e95d481dbbc230ac597ec760e
G4 <mperez@daabon.com.co>
2023-03-18 15:27:32 -0500

    Menu mobile funcional

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const nav_email = document.querySelector(".navbar-email");
 const DesktopMenu = document.querySelector(".desktop-menu");
+const MenuIcon = document.querySelector(".menu");
+const MobileMenu = document.querySelector(".mobile-menu");
 
 nav_email.addEventListener('click', ToggleDesktopMenu);
+MenuIcon.addEventListener('click', ToggleMobileMenu);
+
 
 function ToggleDesktopMenu()
 {
     DesktopMenu.classList.toggle('inactive'); //El toggle quita y coloca la clase inactive dependiendo si la tiene o no
+}
+
+function ToggleMobileMenu()
+{
+    MobileMenu.classList.toggle('inactive'); //El toggle quita y coloca la clase inactive dependiendo si la tiene o no
 }
