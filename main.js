dc085a4074f2a9b028a06ed4e742c34f60e3d48e
Lachicagradiadora <200379@unsaac.edu.pe>
2023-03-20 13:17:53 -0500

    mobile and desktop menu interaction with cart

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 const menuEmail = document.querySelector('.navbar-email'); 
 const desktopMenu = document.querySelector('.desktop-menu'); 
 const menuHamIcon = document.querySelector('.menu'); 
+const menuCartIcon = document.querySelector('.navbar-shopping-cart'); 
 const mobileMenu = document.querySelector('.mobile-menu'); 
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCarAside)
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    // aside.classList.toggle('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    } 
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarAside(){
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    if (!isDesktopMenuClosed) desktopMenu.classList.add('inactive');
+
+    // aside.classList.toggle('inactive');
+    
+    if (!isMobileMenuClosed) mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
