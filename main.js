7dbdc3fd7c4d592db26a6978d55244e48cf72d9f
JosueXDa <josueloy12@gmail.com>
2023-03-21 16:45:48 -0500

    Se agrego tres archivos para agrupar todos los html

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('todo good');
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
