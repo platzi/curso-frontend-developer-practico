dd5c988310cf5f9f6d99ad2a6e2227cc9446a61b
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-03-20 21:15:53 -0500

    feat: 🎸 Clase 20: Fusión del menú en mobile

--- main.js
+++ main.js
@@ -2,8 +2,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+const iconBurger = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
+iconBurger.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
