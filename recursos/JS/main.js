6a0342d64df1a014ea8e9e371d77e6f080374cf3
Raul-madero <r.madero.ramirez@gmail.com>
2023-03-22 11:21:20 -0600

    Se integraron los menus en la aplicacion para que funcionen de manera adecuada

--- recursos/JS/main.js
+++ recursos/JS/main.js
@@ -1,9 +1,23 @@
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamburguesa = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarrito = document.querySelector('.navbar-shopping-cart')
+const shoppingDetail = document.querySelector('.product-detail')
 
 navEmail.addEventListener('click', toggleDesktopMenu)
+menuHamburguesa.addEventListener('click', toggleMobileMenu)
+menuCarrito.addEventListener('click', toggleMenuCarrito)
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    shoppingDetail.classList.add('inactive')
 }
-
-console.log('JS funcionando')
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+    shoppingDetail.classList.add('inactive')
+}
+function toggleMenuCarrito() {
+    shoppingDetail.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+}
