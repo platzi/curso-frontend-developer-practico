23fcea49665d9272ded97b70508796749aa8a72b
anthony paiva <adpaguero1992@gmail.com>
2023-03-22 18:31:21 -0500

    dandole funcionalidad a el menu desktop y mobile

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const desktopMenu = document.querySelector('.desktop-menu')
-const hamburMenu = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+const hamburMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside =document.querySelector('.product-detail')
+
 
 menuEmail.addEventListener('click', toggleDeskotMenu)
 hamburMenu.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
-function toggleDeskotMenu(event) {
-    desktopMenu.classList.toggle('inactive')
+function toggleDeskotMenu() {
+
+    const  isAsideClose = aside.classList.contains('inactive')
+
+    if(!isAsideClose){
+    aside.classList.add('inactive'); 
 }
 
-function toggleMobileMenu(event) {
-   mobileMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+   const  isAsideClose = aside.classList.contains('inactive')
+
+        if(!isAsideClose){
+    aside.classList.add('inactive'); 
+}
+
+mobileMenu.classList.toggle('inactive')
+}
+   
+function toggleCarritoAside() {
+
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+   
+  if(!isMobileMenuClose){
+
+        mobileMenu.classList.add('inactive');   
+    }
+    aside.classList.toggle('inactive');
+    
+ }
