acddfcfcb63c5f159a8ed1c331a512fcaf01e9e2
Raul-madero <r.madero.ramirez@gmail.com>
2023-03-22 13:43:07 -0600

    se creo una maqueta de HTML desde JS para crear la lista de productos

--- recursos/JS/main.js
+++ recursos/JS/main.js
@@ -4,10 +4,12 @@ const menuHamburguesa = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuCarrito = document.querySelector('.navbar-shopping-cart')
 const shoppingDetail = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
 navEmail.addEventListener('click', toggleDesktopMenu)
 menuHamburguesa.addEventListener('click', toggleMobileMenu)
 menuCarrito.addEventListener('click', toggleMenuCarrito)
+
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
     shoppingDetail.classList.add('inactive')
@@ -21,3 +23,66 @@ function toggleMenuCarrito() {
     mobileMenu.classList.add('inactive')
     desktopMenu.classList.add('inactive')
 }
+const productList = [];
+productList.push({
+    name: 'Bicicleta',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Computadora',
+    price: 280,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+function renderProducts(arr) {
+    for (product of productList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+       
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+       
+        const productInfodiv = document.createElement('div');
+       
+        const productPrice = document.createElement('p');
+        const productNAme = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        productNAme.innerText = product.name;
+       
+        const productInfoFigure = document.createElement('figure');
+       
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', '/recursos/icons/bt_add_to_cart.svg');
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfodiv.append(productNAme, productPrice);
+    
+        productInfo.append(productInfodiv, productInfoFigure);
+        productCard.append(productImg, productInfo)
+        cardsContainer.appendChild(productCard)
+    }
+}
+renderProducts(productList)
