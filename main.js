c5884935ab299c38c7d5dbf57000a8ccc7d4a36e
chelyvanis1987 <ara_vic37@hotmail.com>
2023-03-22 12:51:14 -0700

    modificaciones clase 20

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     console.log ('click');
     desktopMenu.classList.toggle('inactive');
 
 }
-console.log ('funcionando');
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
