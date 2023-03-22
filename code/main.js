99c9639bbc960a7ac91e869f4a673d7e6f198f6e
negagl <nega_gl@outlook.com>
2023-03-21 22:30:36 -0500

    Changed items images and layout form

--- code/main.js
+++ code/main.js
@@ -11,58 +11,64 @@ const productDetail = $(".product-detail");
 // ItemsList
 const productsList = [];
 productsList.push({
-    name: "Carro",
+    name: "Blood Moon Thresh",
     price: 110,
-    image:
-        "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    image: "https://th.bing.com/th/id/OIP.o39c92qthWolZ8ifeBqANgHaEo?pid=ImgDet&w=1680&h=1050&rs=1",
 });
 productsList.push({
-    name: "Casa de juguete",
+    name: "Blood Moon Twisted Fate",
     price: 500,
-    image:
-        "https://images.unsplash.com/photo-1605012464390-45820d1f7bdf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    image: "https://th.bing.com/th/id/R.3b7bd4ea26a8ee3b94695de01d675959?rik=B9PdrafUpeBssA&pid=ImgRaw&r=0",
 });
 productsList.push({
-    name: "Teléfono",
+    name: "Gentleman Gnar",
     price: 1000,
-    image:
-        "https://images.unsplash.com/photo-1565849904461-04a58ad377e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80",
+    image: "https://cdnen.samurai-gamers.com/wp-content/uploads/2017/07/17000345/Gnar_2.jpg",
 });
 productsList.push({
-    name: "Carro",
+    name: "Star Guardian Jinx",
     price: 110,
-    image:
-        "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    image: "https://th.bing.com/th/id/R.c011d457375b7cd1ee02857935f280e1?rik=hHQt7I2fJWa1NQ&pid=ImgRaw&r=0",
 });
 productsList.push({
-    name: "Casa de juguete",
+    name: "Eternal Sword Master Yi",
     price: 500,
-    image:
-        "https://images.unsplash.com/photo-1605012464390-45820d1f7bdf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    image: "https://th.bing.com/th/id/R.226f7c9235f0446cdb23286b526a44a0?rik=wphX25sAckVdxg&pid=ImgRaw&r=0",
 });
 productsList.push({
-    name: "Teléfono",
+    name: "Cosmic Devourer Vladimir",
     price: 1000,
-    image:
-        "https://images.unsplash.com/photo-1565849904461-04a58ad377e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80",
+    image: "https://th.bing.com/th/id/R.85eddf950359e3a457ad377c2c26e260?rik=uYFb2arFiGCvtw&pid=ImgRaw&r=0",
 });
 productsList.push({
-    name: "Carro",
+    name: "Battle Academy Caitlyn",
     price: 110,
-    image:
-        "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    image: "https://steamuserimages-a.akamaihd.net/ugc/1767071895191909189/D9332E51B43CF37EFEF96AE945B2AB573BBEA79B/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
 });
 productsList.push({
-    name: "Casa de juguete",
+    name: "Omega Squad Tristana",
     price: 500,
-    image:
-        "https://images.unsplash.com/photo-1605012464390-45820d1f7bdf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    image: "https://www.mobafire.com/images/champion/skins/landscape/tristana-omega-squad.jpg",
 });
 productsList.push({
-    name: "Teléfono",
+    name: "Resistance jayce",
     price: 1000,
-    image:
-        "https://images.unsplash.com/photo-1565849904461-04a58ad377e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80",
+    image: "https://th.bing.com/th/id/R.7841d7dccfdbdba77c629202e3d2468b?rik=owS%2f2K6a9qmUJw&pid=ImgRaw&r=0",
+});
+productsList.push({
+    name: "Spirit Blossom Lilia",
+    price: 110,
+    image: "https://th.bing.com/th/id/R.a17176f4e84946bbb7d36c6358df78f2?rik=gnvNqMMYYnNhGw&pid=ImgRaw&r=0",
+});
+productsList.push({
+    name: "Spirit Blossom Cassiopeia",
+    price: 500,
+    image: "https://th.bing.com/th/id/R.b95319222f4145913893c1a4ac84fa3a?rik=yryb6fqt0Uq1TQ&pid=ImgRaw&r=0",
+});
+productsList.push({
+    name: "Resistance Illaoi",
+    price: 1000,
+    image: "https://cdn.vox-cdn.com/thumbor/AXcNCqlKeDM4VzviOJtC3J2VdEs=/0x0:1920x1080/1200x800/filters:focal(747x264:1053x570)/cdn.vox-cdn.com/uploads/chorus_image/image/58595011/Res_Illaoi_splash.0.jpg",
 });
 
 // Display/Hide options menu PC
@@ -96,10 +102,10 @@ function createProductsList(array) {
         // Creating all elements
         const productCard = document.createElement("div");
         productCard.classList.add("product-card");
-        productCard.setAttribute("onclick", `openProductDetail(${i})`);
 
         const productImg = document.createElement("img");
         productImg.setAttribute("src", product.image);
+        productImg.setAttribute("onclick", `openProductDetail(${i})`);
 
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
@@ -107,7 +113,7 @@ function createProductsList(array) {
         const productInfoDiv = document.createElement("div");
 
         const productPrice = document.createElement("p");
-        productPrice.innerText = "$" + product.price;
+        productPrice.innerText = "$" + product.price + " RP";
 
         const productName = document.createElement("p");
         productName.innerText = product.name;
