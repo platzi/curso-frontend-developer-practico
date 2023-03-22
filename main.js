45e8ebedc004be86bfe6f4025e0f4877b88963ff
JosueXDa <josueloy12@gmail.com>
2023-03-22 09:57:59 -0500

    Se agrego el menu en mobile y se limito cuando aparece uno y otro con media querys

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 console.log('todo good');
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobilemenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    mobilemenu.classList.toggle('inactive');
+}
+
