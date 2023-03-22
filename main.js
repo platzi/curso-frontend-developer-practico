0abb5ef7d2e373624dd7cb69b68fa542c8089cfa
anthony paiva <adpaguero1992@gmail.com>
2023-03-22 13:38:25 -0500

    fusionando y haciendo responsive ek menu mobile y el desktop

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
+const hamburMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDeskotMenu)
+hamburMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDeskotMenu(event) {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(event) {
+   mobileMenu.classList.toggle('inactive')
 }
