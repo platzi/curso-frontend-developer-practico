86dd0146f36f6c9e2719c38be03be81aa80bdf19
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-03-21 20:08:09 -0500

    feat: 🎸 Clase 21 Carrito de compras (Mobile y Desktop)

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-//fusión de menú en desktop. (ver ordenes, cuenta, sign outs)
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+// menuEmail.addEventListener("click", toggleDesktopMenu);
 
 const iconBurger = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+// iconBurger.addEventListener("click", toggleMobileMenu);
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
-iconBurger.addEventListener("click", toggleMobileMenu);
+const iconShopingCart = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+// iconShopingCart.addEventListener("click", toggleIconShopingCart);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleIconShopingCart() {
+  desktopMenu.classList.add("inactive");
+
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
 }
