b953c4a12aa3fa6e7487292ee58a412d4d4aef6e
SantiagoAG <santiagowe98@gmail.com>
2023-03-23 14:49:31 -0500

    Added mobile-menu and media querys

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
   console.log('click');
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
