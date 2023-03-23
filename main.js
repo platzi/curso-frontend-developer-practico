6d296fb656bb11990a6292d4152283576d184839
SantiagoAG <santiagowe98@gmail.com>
2023-03-23 15:43:42 -0500

    Toggled desktopMenu, mobileMenu and the aside

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurgerIcon = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurgerIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
-  console.log('click');
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+  
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+
+  aside.classList.toggle('inactive');
 }
