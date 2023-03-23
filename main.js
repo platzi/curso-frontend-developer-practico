b7b451a213e4734ce6dcb97aed1bccc86d6ad034
anthony paiva <adpaguero1992@gmail.com>
2023-03-22 21:57:55 -0500

    se hizo la funcion para agregar HTML con javascript para las casilas de productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const hamburMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside =document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
 
 menuEmail.addEventListener('click', toggleDeskotMenu)
@@ -43,3 +44,65 @@ function toggleCarritoAside() {
     aside.classList.toggle('inactive');
     
  }
+
+ const productList = [];
+ productList.push({
+    name:'Bike',
+    precio: 120,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+ })
+
+ productList.push({
+    name:'Patineta',
+    precio: 100,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+ })
+
+ productList.push({
+    name:'Triciclo',
+    precio: 80,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+ })
+ 
+
+ 
+function renderProduts(array){
+    for (product of array){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+      
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.imagen);
+      
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+      
+        const productInfoDiv = document.createElement('div');
+      
+        const productPrice = document.createElement('p');
+        productPrice.innerText ='$' + product.precio;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+      
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+      
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img')
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg')
+      
+        productInfoFigure.appendChild(productImgCard)
+      
+      
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+      
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+      
+        cardsContainer.appendChild(productCard)
+       }
+}
+
+
+renderProduts(productList);
