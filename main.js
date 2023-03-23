a438c4e40555749795bc696d1f215af10d2c7c94
Roberto Cardenas <drcp_cardenas@hotmail.com>
2023-03-23 04:31:26 -0600

    Agregando carrito de compras, poniendo condiciones de despligue de secciones de la pagina con condicionales en jva escript

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 const menuEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
+const menuCarritoIcon= document.querySelector('.navbar-shopping-cart');
+const aside= document.querySelector('.product-detail');
 const menuHamMenu=document.querySelector('.menu');
 const mobileMenu=document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamMenu.addEventListener('click',toggleMobileManu);
+menuCarritoIcon.addEventListener('click',toggleCarrritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!isAsideClosed)
+    {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileManu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!isAsideClosed)
+    {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarrritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    // aside.classList.toggle('inactive');
+
+    if (!isMobileMenuClosed)
+    {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
