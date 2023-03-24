3810fd4baa8345de03b0971c005a3d52c2a98423
Facundo Melgarejo Roma <facundomelgarejor@gmail.com>
2023-03-24 18:55:11 -0300

    Agregando carrito de compras, con su js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let menuEscritorio = document.querySelector('.desktop-menu')
 
 btnMail.addEventListener('click', function(){
     menuEscritorio.classList.toggle('inactive')
+    
 })
 
 let btnMenuCategoriasMobile = document.querySelector('#btn-menu-categorias-mobile');
@@ -11,4 +12,14 @@ let menuMobile = document.querySelector('.mobile-menu')
 
 btnMenuCategoriasMobile.addEventListener('click', function(){
     menuMobile.classList.toggle('inactive')
+    menuCarritodeCompras.classList.add('inactive')
+})
+
+
+let btnCarritodeCompras = document.querySelector('.navbar-shopping-cart');
+let menuCarritodeCompras = document.querySelector('.product-detail');
+
+btnCarritodeCompras.addEventListener('click', function(){
+    menuCarritodeCompras.classList.toggle('inactive')
+    menuMobile.classList.add('inactive')
 })
