a2839996473f150483de24db420548047cc76234
Facundo Melgarejo Roma <facundomelgarejor@gmail.com>
2023-03-24 18:32:01 -0300

    fusión de menu lateral categorias, con su js

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let btnMail = document.querySelector('.navbar-email');
 let menuEscritorio = document.querySelector('.desktop-menu')
 
+
 btnMail.addEventListener('click', function(){
     menuEscritorio.classList.toggle('inactive')
+})
+
+let btnMenuCategoriasMobile = document.querySelector('#btn-menu-categorias-mobile');
+let menuMobile = document.querySelector('.mobile-menu')
+
+btnMenuCategoriasMobile.addEventListener('click', function(){
+    menuMobile.classList.toggle('inactive')
 })
