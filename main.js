f5f104cfc245a5a1024efc9c02f051e1afc1aa6f
chelyvanis1987 <ara_vic37@hotmail.com>
2023-03-25 11:32:27 -0700

    modificaciones detalle de un producto clas 23

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -15,14 +15,14 @@ function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
     
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
 }
 function toggleMobileMenu() {
-const isAsideClosed = aside.classList.contains('inactive')
+const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -35,7 +35,7 @@ if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');   
 }
 
-aside.classList.toggle('inactive');
+shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
