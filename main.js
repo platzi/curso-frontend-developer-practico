6d857afcd3f9d3424cb9747d309b7f2b4eb5609e
Max Santos <maxsantos9@gmail.com>
2023-03-24 20:13:28 -0600

    integracion del detalle

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuCarrito = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
+const aside = document.querySelector('#shoppingCartContainer')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
