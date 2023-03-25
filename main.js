671563a5b61f7baa65e27e3a3a6f64e9b0d825e4
Jusched <juans-betancurt@unilibre.edu.co>
2023-03-24 21:38:38 -0500

    merged navbar and navbar-email menu

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+
+menuEmail.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  desktopMenu.classList.toggle("inactive");
+  // adds or removes .inactve to toggle the desktop menu
+}
