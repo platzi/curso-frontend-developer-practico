8e3c6aca116654aef67490deaa582af9a9691530
Daniel Muñoz <daniel20025febrero29@gmail.com>
2023-03-26 14:02:48 -0500

    Fusión Menú Mobile

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
 
 function toggleDesktopMenu(){
-    console.log('Click')
     desktopMenu.classList.toggle('inactive');
 }
 
