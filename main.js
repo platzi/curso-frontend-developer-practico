910648d0c322ab7f8489ffb58dc7e0936a2be502
nicolasco <cabezasn628@gmail.com>
2023-03-26 11:20:40 -0500

    js email functional

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("js funcionando");
+
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle("inactive");
+}
