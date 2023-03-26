ade9583a5f1eb2a936c24a991f73a967d424251c
Alvaro Johel Condori <alvarojohelcondori@gmail.com>
2023-03-26 11:32:58 -0600

    Carrito de compras (integrado tanto en el mobile y desktop)

--- main.js
+++ main.js
@@ -2,14 +2,39 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuHamIcon.addEventListener('click', toogleMobileMenu);
+menuCarIcon.addEventListener('click', toogleCarAside);
 
 function toogleDesktopMenu() {
+	const isAsideClosed = aside.classList.contains('inactive');
+
+	if (!isAsideClosed) {
+		aside.classList.add('inactive');
+	}
+
 	desktopMenu.classList.toggle('inactive');
 }
 
 function toogleMobileMenu() {
+	const isAsideClosed = aside.classList.contains('inactive');
+
+	if (!isAsideClosed) {
+		aside.classList.add('inactive');
+	}
+
 	mobileMenu.classList.toggle('inactive');
+}
+
+function toogleCarAside() {
+	const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+	if (!isMobileMenuClosed) {
+		mobileMenu.classList.add('inactive');
+	}
+
+	aside.classList.toggle('inactive');
 }
