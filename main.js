937bd5eb157a957b50e69cd3978a11e0134826f7
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-03-25 19:39:57 -0500

    feat: 🎸 Clase 22 Lista de productos: HTML con arrays

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const iconShopingCart = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
 // iconShopingCart.addEventListener("click", toggleIconShopingCart);
 
+const cardsContainer = document.querySelector(".cards-container");
+
 function toggleDesktopMenu() {
   const isAsideClosed = aside.classList.contains("inactive");
 
@@ -41,3 +43,71 @@ function toggleIconShopingCart() {
 
   aside.classList.toggle("inactive");
 }
+
+const productsList = [];
+productsList.push({
+  name: "Monitor",
+  price: 150,
+  imageProducts:
+    "https://images.pexels.com/photos/2661929/pexels-photo-2661929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productsList.push({
+  name: "Keyboard",
+  price: 50,
+  imageProducts:
+    "https://images.pexels.com/photos/163117/keyboard-white-computer-keyboard-desktop-163117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productsList.push({
+  name: "Mouse",
+  price: 30,
+  imageProducts:
+    "https://images.pexels.com/photos/4034473/pexels-photo-4034473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+// <div class="product-card">
+//             <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+//             <div class="product-info">
+//               <div>
+//                 <p>$120,00</p>
+//                 <p>Bike</p>
+//               </div>
+//               <figure>
+//                 <img src="./icons/bt_add_to_cart.svg" alt="">
+//               </figure>
+//             </div> --></img>
+
+function renderProducts(array) {
+  for (product of array) {
+    // maquetar HTML con JS
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.imageProducts);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = `$${product.price}`;
+    const productName = document.createElement("p");
+    productName.innerText = `${product.name}`;
+
+    productInfoDiv.append(productPrice, productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImageCart = document.createElement("img");
+    productImageCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImageCart);
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImg, productInfo);
+    cardsContainer.appendChild(productCard);
+  }
+}
+renderProducts(productsList);
