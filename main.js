103895585a30ab9743013713b9731839dc0baf48
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-03-25 20:26:29 -0500

    feat: 🎸 Clase 23 Detalles de un producto (product-detail)

--- main.js
+++ main.js
@@ -7,26 +7,26 @@ const mobileMenu = document.querySelector(".mobile-menu");
 // iconBurger.addEventListener("click", toggleMobileMenu);
 
 const iconShopingCart = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 // iconShopingCart.addEventListener("click", toggleIconShopingCart);
 
 const cardsContainer = document.querySelector(".cards-container");
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClosed) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
 
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClosed) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
 
   mobileMenu.classList.toggle("inactive");
@@ -41,7 +41,7 @@ function toggleIconShopingCart() {
     mobileMenu.classList.add("inactive");
   }
 
-  aside.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productsList = [];
