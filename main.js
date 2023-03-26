a56c4b5cdc2f08bad7b544393422b0b58186e446
nicolasco <cabezasn628@gmail.com>
2023-03-26 11:44:34 -0500

    union y fusion del menu desktop y mobile

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ console.log("js funcionando");
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
