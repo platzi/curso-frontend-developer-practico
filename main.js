21b502cf5c66d21f0f9150337af370672dc7c926
unknown <joseroniz18@gmail.com>
2023-03-27 14:17:08 -0700

    logramos hacer que el aside product detail se abra cuandose de click y se cierre cuando se de click al icono de la x

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHanIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCart = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -13,10 +13,10 @@ menuHanIcon.addEventListener("click", toggleMobileMenu);
 menuCart.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClosed) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
 
   desktopMenu.classList.toggle("inactive");
@@ -25,10 +25,10 @@ function toggleDesktopMenu() {
 //* Funcion del mobile menu
 
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClosed) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
 
   mobileMenu.classList.toggle("inactive");
@@ -43,7 +43,7 @@ function toggleCarritoAside() {
     mobileMenu.classList.add("inactive");
   }
 
-  aside.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 
