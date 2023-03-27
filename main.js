e96c4d7360ce97ca97cc444dc7d0c0cf867c905a
Andrey Macias <carlos.macias@inoutdelivery.com>
2023-03-27 10:21:48 -0500

    se mezclo la clase 12

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const cardContainer = document.querySelector('.cards-container')
 
 
@@ -12,17 +12,17 @@ menuHamIcon.addEventListener('click', toggleMobileMenu)
 menuCarritoIcon.addEventListener('click',toggleCarritoAside )
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains('inactive')
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
   if(!isAsideClosed){
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
   }
   desktopMenu.classList.toggle('inactive')
 }
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains('inactive')
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
 
   if(!isAsideClosed){
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
   }
   mobileMenu.classList.toggle('inactive')
 }
@@ -33,7 +33,7 @@ function toggleCarritoAside() {
     mobileMenu.classList.add('inactive')
   }
 
-  aside.classList.toggle('inactive')
+  shoppingCartContainer.classList.toggle('inactive')
 }
 
 const productList = []
