7e2afc3fa4803df3d32f3350f5c7da962e6a8a1a
razec2112 <razec.cz@gmail.com>
2023-03-27 21:55:31 -0400

    se agrega el menu de compra y se resuelve interferencia entre los menus

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const menuEmail = document.querySelector(".navbar-email");
 const menuDesktop = document.querySelector(".desktop-menu");
 const menuHamburguer = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const carrito = document.querySelector(".navbar-shopping-cart");
+const compra = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamburguer.addEventListener("click", toggleMobileMenu);
+carrito.addEventListener("click", toggleCompraMenu);
+
 
 function toggleDesktopMenu(){
     // if (menuDesktop.classList[1] == "inactive") {
@@ -13,8 +17,16 @@ function toggleDesktopMenu(){
     //     menuDesktop.classList.add("inactive")
     // }
     menuDesktop.classList.toggle("inactive");
+    compra.classList.add("inactive");
 };
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle("inactive");
+    compra.classList.add("inactive");
 };
+
+function toggleCompraMenu (){
+    compra.classList.toggle("inactive");
+    mobileMenu.classList.add("inactive");
+    menuDesktop.classList.toggle("inactive");
+}
