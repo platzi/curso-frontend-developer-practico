a09f5bfe7d67be3a3b0409173aaef212a4e36b1f
razec2112 <razec.cz@gmail.com>
2023-03-27 20:49:42 -0400

    se agrega el menu mobil para dispositivos

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const menuEmail = document.querySelector(".navbar-email");
 const menuDesktop = document.querySelector(".desktop-menu");
-
+const menuHamburguer = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamburguer.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(){
     // if (menuDesktop.classList[1] == "inactive") {
@@ -11,4 +13,8 @@ function toggleDesktopMenu(){
     //     menuDesktop.classList.add("inactive")
     // }
     menuDesktop.classList.toggle("inactive");
+};
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 };
