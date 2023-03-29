dac7908ba9aa0ecad120bd743f24528b97ccbb75
Decker316 <diegoq1998@gmail.com>
2023-03-28 19:33:23 -0600

    Integrate the Mobile menu, add comments to CSS and fix some display conflicts

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMEnu);
+menuIcon.addEventListener('click', toggleMobileMEnu);
 
 function toggleDesktopMEnu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMEnu(){
+    mobileMenu.classList.toggle('inactive');
 }
