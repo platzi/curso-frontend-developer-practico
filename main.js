cdff54212cf9a6d3840cca80d296e4691a3c5a60
Decker316 <diegoq1998@gmail.com>
2023-03-28 21:07:11 -0600

    Shopping Cart and interaction between menus

--- main.js
+++ main.js
@@ -2,14 +2,34 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const asideShoppingCart = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMEnu);
-menuIcon.addEventListener('click', toggleMobileMEnu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
+menuShoppingCart.addEventListener('click', toggleAsideShopingMenu);
 
-function toggleDesktopMEnu(){
+function toggleDesktopMenu(){
+    
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMEnu(){
+function toggleMobileMenu(){
+    const isShoppingCartMenuClosed = asideShoppingCart.classList.contains('inactive');
+
+    if (!isShoppingCartMenuClosed) {
+        asideShoppingCart.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAsideShopingMenu(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    asideShoppingCart.classList.toggle('inactive');
 }
