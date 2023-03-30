f3c65a581abeabc2a81354a2a0d40984ed9c6f39
Franco Suarez <fsuarez@gdpsalud.com.ar>
2023-03-29 23:16:33 -0300

    add listeners to hide elements

--- main.js
+++ main.js
@@ -2,35 +2,68 @@ const navEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
 const menuBurguerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const asideShopping = document.querySelector('.product-detail');
+const shoppingCartDetail = document.querySelector('#shoppingCartContainer');
 const shoppingIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailContainer = document.querySelector('#productDetail');
+const buttonProductDetailClosed = document.querySelector('.product-detail-close');
 const cardsContainer = document.querySelector('.cards-container');
 
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuBurguerIcon.addEventListener('click', toggleMobileMenu);
-shoppingIcon.addEventListener('click', toggleAsideProductDetail);
+shoppingIcon.addEventListener('click', toggleAsideShoppingCart);
+buttonProductDetailClosed.addEventListener('click', hideProductDetailContainer);
+
+function hideProductDetailContainer(){
+    productDetailContainer.classList.toggle('inactive');
+}
+
+function toggleProductDetail(){
+    const isShoppingCartDetailOpen = !shoppingCartDetail.classList.contains('inactive');
+    const isMenuDesktopOpen = !menuDesktop.classList.contains('inactive');
+    const isProductDetailHiden = productDetailContainer.classList.contains('inactive');
+
+    if(isMenuDesktopOpen){
+        menuDesktop.classList.toggle('inactive');
+    }
+
+    if(isShoppingCartDetailOpen){
+        shoppingCartDetail.classList.toggle('inactive');
+    }
+
+    if(isProductDetailHiden){
+        productDetailContainer.classList.toggle('inactive');  
+    }
+}
 
 function toggleMobileMenu(){
-    const isAsideOpen = !asideShopping.classList.contains('inactive');
+    const isAsideOpen = !shoppingCartDetail.classList.contains('inactive');
     if(isAsideOpen){
-        asideShopping.classList.toggle('inactive');
+        shoppingCartDetail.classList.toggle('inactive');
     }
     mobileMenu.classList.toggle('inactive');   
     
 }
  
 function toggleDesktopMenu(){
-    const isAsideOpen = !asideShopping.classList.contains('inactive');
+    const isAsideOpen = !shoppingCartDetail.classList.contains('inactive');
+    const isProductDetailOpen = !productDetailContainer.classList.contains('inactive');
+
     if(isAsideOpen){
-        asideShopping.classList.toggle('inactive');
-    }    
+        shoppingCartDetail.classList.toggle('inactive');
+    }
+    
+    if(isProductDetailOpen){
+        productDetailContainer.classList.toggle('inactive');   
+    }
+
     menuDesktop.classList.toggle('inactive');   
 }
 
-function toggleAsideProductDetail(){
+function toggleAsideShoppingCart(){
     const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
     const isDesktopMenuOpen = !menuDesktop.classList.contains('inactive');
+    const isProductDetailOpen = !productDetailContainer.classList.contains('inactive');
     
     if(isMobileMenuOpen){
         mobileMenu.classList.toggle('inactive');
@@ -39,7 +72,11 @@ function toggleAsideProductDetail(){
     if(isDesktopMenuOpen){
         menuDesktop.classList.toggle('inactive');
     }
-    asideShopping.classList.toggle('inactive');
+
+    if(isProductDetailOpen){
+        productDetailContainer.classList.toggle('inactive');   
+    }
+    shoppingCartDetail.classList.toggle('inactive');
 
 }
 
@@ -48,7 +85,7 @@ addProducts();
 addHtmlProducts(productList);
 
 function addProducts(){
-    for(let i= 0; i < 100; i++){
+    for(let i= 0; i < 36; i++){
         productList.push({
             name : 'Bike',
             price: 120.00,
@@ -65,6 +102,7 @@ function addHtmlProducts(products){
 
         const img = document.createElement('img');
         img.setAttribute('src', product.img);
+        img.addEventListener('click', toggleProductDetail);
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
