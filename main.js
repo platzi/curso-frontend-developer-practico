e1517f7bd33f45dec84b5dde3bb7f0580c85699c
Decker316 <diegoq1998@gmail.com>
2023-03-30 23:33:32 -0600

    Add product detail aside, fix CSS problems and some visual bugs.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
-const asideShoppingCart = document.querySelector('.product-detail');
+const asideShoppingCart = document.querySelector('.shopping-cart-product');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
