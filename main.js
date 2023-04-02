caf3dbbf5753d218e29c3244ff9783c72a61eec3
ItsCamilo95 <106566378+ItsCamilo95@users.noreply.github.com>
2023-04-02 17:04:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function API(){
         const cardsContenedor = document.querySelector('.cards-container');
         for (let i = 0; i < data['results'].length; i++) {
             cardsContenedor.innerHTML+=`<div class="product-card">
-            <img src="${data['results'][i].image}" alt="" onclick="openProductDetailAside('${data['results'][i].image}', '${data['results'][i].name}', '${data['results'][i]['origin'].name}' ,'${data['results'][i].gender}','${data['results'][i].species}', '${data['results'][i].status}', '${data['results'][i]['location'].name}')">
+            <img src="${data['results'][i].image}" href="product-detail" alt="" onclick="openProductDetailAside('${data['results'][i].image}', '${data['results'][i].name}', '${data['results'][i]['origin'].name}' ,'${data['results'][i].gender}','${data['results'][i].species}', '${data['results'][i].status}', '${data['results'][i]['location'].name}')">
             <div class="product-info">
             <div>
                 <p>${data['results'][i].name}</p>
