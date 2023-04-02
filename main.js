145c9220114cb7921b886c849d4269264b7d4ba9
JacoboGL <jaco401@hotmail.com>
2023-04-01 21:39:25 -0500

    Added menus interaction for mobile and desktop. Also added the checkout menu

--- main.js
+++ main.js
@@ -1,8 +1,58 @@
+// LLAMAR ELEMENTOS DESDE EL CSS
+
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+const burguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const shoppingCart = document.querySelector(".navbar-shopping-cart")
+const checkoutMenu = document.querySelector(".product-detail")
+
+
+// INTERACTIVIDAD
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
+burguerMenu.addEventListener("click", toggleMobileMenu);
+
+shoppingCart.addEventListener("click", toggleShoppingCart);
+
+
+// FUNCIONES
+
 function toggleDesktopMenu() {
+    const isShoppingCartOpen = checkoutMenu.classList.contains('inactive');
+
+    if (!isShoppingCartOpen){
+        checkoutMenu.classList.add("inactive");
+    }
+
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isShoppingCartOpen = checkoutMenu.classList.contains('inactive');
+
+    if (!isShoppingCartOpen){
+        checkoutMenu.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart() {
+    const isMobileMenuOpen = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuOpen){
+        mobileMenu.classList.add("inactive");
+    }
+
+    const isEmailMenuOpen = desktopMenu.classList.contains('inactive');
+
+    if (!isEmailMenuOpen){
+        desktopMenu.classList.add("inactive");
+    }
+
+    checkoutMenu.classList.toggle('inactive');
 }
