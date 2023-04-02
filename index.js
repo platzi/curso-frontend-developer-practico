f2b12b1468b4f31bceb058809383529106bba322
Giovanny Moreno <giovannysocas@gmail.com>
2023-04-02 15:29:03 -0500

    add: Component mobileMenu and responsive reaction

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const $menuEmail = document.querySelector('.navbar-email');
 const $desktopMenu = document.querySelector('.desktop-menu');
+const $iconMenu = document.querySelector('.iconMenu');
+const $mobileMenu = document.querySelector('.mobile-menu');
+
+
 
 $menuEmail.addEventListener('click', toggleDesktopMenu);
+$iconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
   $desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+  $mobileMenu.classList.toggle('inactive');
 }
