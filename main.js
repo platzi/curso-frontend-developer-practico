b435df9e5f2d1a04a51801fbd5aa30a7a5a7cbf2
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2023-04-02 13:31:58 -0500

    Lista de productos HTML a partir de arrays

--- main.js
+++ main.js
@@ -1,8 +1,96 @@
+// variables
 const menuEmail = document.querySelector(".navbar-email");
+const menuHamIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 // toggle me permite intercambiar
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  desktopMenu.classList.toggle("inactive");
 }
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+function toggleCarritoAside() {
+  // esta abierto el menu mobile
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  // si tiene la clase inactive no tenemos que hacer nada
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120.0,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Pantalla",
+  price: 220.0,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Comput",
+  price: 620.0,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+
+const renderProduct = (arr) => {
+    for (product of arr) {
+      const productCard = document.createElement("div");
+      productCard.classList.add("product-card");
+    
+      const productImg = document.createElement("img");
+      productImg.setAttribute("src", product.image);
+    
+      const productInfo = document.createElement("div");
+      productInfo.classList.add("product-info");
+    
+      const productInfoDiv = document.createElement("div");
+    
+      const productPrice = document.createElement("p");
+      productPrice.innerText = "$ " + product.price;
+      const productName = document.createElement("p");
+      productName.innerText = product.name;
+    
+      productInfoDiv.appendChild(productPrice);
+      productInfoDiv.appendChild(productName);
+    
+      const productInfoFigure = document.createElement("figure");
+      const productImgCart = document.createElement("img");
+      productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+      // Metedos un elemento dentro de otro elemento
+      productInfoFigure.appendChild(productImgCart);
+    
+      productInfo.appendChild(productInfoDiv);
+      productInfo.appendChild(productInfoFigure);
+    
+      productCard.appendChild(productImg);
+      productCard.appendChild(productInfo);
+    
+      cardsContainer.appendChild(productCard);
+    }
+}
+renderProduct(productList)
+// Implementación del menú en responsive
