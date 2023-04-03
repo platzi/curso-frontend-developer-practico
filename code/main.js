0d3bc726fc7a465fc032d88d81f544e01e07a25d
alejandro911pa <alejandro.paez39@unach.mx>
2023-04-02 21:32:22 -0600

    lista de productos

--- code/main.js
+++ code/main.js
@@ -7,19 +7,16 @@ const mobileMenu = document.querySelector(".mobile-menu")
 
 const menuCarrito = document.querySelector(".navbar-shopping-cart")
 const menuMyorder = document.querySelector(".product-detail")
+
+const cardsContainer = document.querySelector(".cards-container")
  
-
-
 menuEmail.addEventListener('click',correoMenu)
 
 function correoMenu(){
     DesktopMenu.classList.toggle('inactive')
     menuMyorder.classList.add("inactive")
 
-
-
 }
-
 
 MenuBurger.addEventListener("click",toogleMobile)
 function toogleMobile(){
@@ -30,10 +27,7 @@ function toogleMobile(){
 
 }
 
-
-
 menuCarrito.addEventListener('click',toogleMyOrder)
-
 function toogleMyOrder() {
     menuMyorder.classList.toggle("inactive")
 
@@ -43,3 +37,50 @@ function toogleMyOrder() {
 
 
 }
+
+
+const productList = []
+productList.push({
+    //imagen precio nombre
+    name: "higado",
+    price: 120,
+    image:"https://images.pexels.com/photos/8272928/pexels-photo-8272928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+})
+
+productList.push({
+    //imagen precio nombre
+    name: "Pulmones",
+    price: 100000,
+    image:"https://imgs.search.brave.com/dh_Mjj5R_AIHMtR5BhT3_FsN5DdckfpmwqInba19GQU/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/bHVuZ3NhbmR5b3Uu/Y29tL3N0YXRpYy9p/bWFnZXMvcHJvZ3Jl/c3Npb24vbm9ybWFs/LWx1bmdzLmpwZw"
+})
+
+productList.push({
+    //imagen precio nombre
+    name: "Riñones",
+    price: 120,
+    image:"https://e7.pngegg.com/pngimages/779/986/png-clipart-kidney-kidney-failure-membranoproliferative-glomerulonephritis-minimal-change-disease-membranous-glomerulonephritis-kidney-miscellaneous-hand.png"
+})
+
+function renderProducts(arr){
+    for (product of productList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        productCard.innerHTML = `
+        <img src="${product.image}" alt="">
+        <div class="product-info">
+            <div>
+                <p>$ ${product.price}</p>
+                <p>${product.name}</p>
+            </div>
+            <figure>
+                <img src="../icons/bt_add_to_cart.svg" alt="">
+            </figure>
+        </div>
+        `
+        cardsContainer.appendChild(productCard);
+    }
+
+}
+
+renderProducts(productList)
