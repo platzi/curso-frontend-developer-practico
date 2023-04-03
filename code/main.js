ef3cd43b64febf440b4784d58d0e828e893d9570
alejandro911pa <alejandro.paez39@unach.mx>
2023-04-02 23:09:36 -0600

    detalles productos

--- code/main.js
+++ code/main.js
@@ -6,37 +6,58 @@ const MenuBurger = document.querySelector(".menuBurger")
 const mobileMenu = document.querySelector(".mobile-menu")
 
 const menuCarrito = document.querySelector(".navbar-shopping-cart")
-const menuMyorder = document.querySelector(".product-detail")
+const menuMyorder = document.querySelector("#shoppingCartContainer")
 
 const cardsContainer = document.querySelector(".cards-container")
- 
-menuEmail.addEventListener('click',correoMenu)
 
-function correoMenu(){
-    DesktopMenu.classList.toggle('inactive')
-    menuMyorder.classList.add("inactive")
+const detalles = document.querySelector("#productDetail")
+const detallesPicture = document.querySelector(".cards-container")
+const cerrardetalles =document.querySelector(".close")
 
-}
+detallesPicture.addEventListener("click", detallesproduct)
+cerrardetalles.addEventListener("click", detallesproduct)
 
-MenuBurger.addEventListener("click",toogleMobile)
-function toogleMobile(){
-
-    mobileMenu.classList.toggle("inactive")
+function detallesproduct() {
+    detalles.classList.toggle('inactive')
+    // cerrardetalles.toggle('inactive')
 
     menuMyorder.classList.add("inactive");
 
 }
 
-menuCarrito.addEventListener('click',toogleMyOrder)
-function toogleMyOrder() {
-    menuMyorder.classList.toggle("inactive")
+menuEmail.addEventListener('click', correoMenu)
+function correoMenu() {
+    DesktopMenu.classList.toggle('inactive')
 
-   mobileMenu.classList.add("inactive")
+    detalles.classList.add('inactive')
+    menuMyorder.classList.add("inactive")
 
-   DesktopMenu.classList.add('inactive')
+}
+
+MenuBurger.addEventListener("click", toogleMobile)
+function toogleMobile() {
+
+    mobileMenu.classList.toggle("inactive")
+
+    menuMyorder.classList.add("inactive");
+    detalles.classList.add('inactive')
 
 
 }
+
+menuCarrito.addEventListener('click', toogleMyOrder)
+function toogleMyOrder() {
+    menuMyorder.classList.toggle("inactive")
+
+    mobileMenu.classList.add("inactive")
+    detalles.classList.add('inactive')
+    DesktopMenu.classList.add('inactive')
+
+
+}
+
+
+
 
 
 const productList = []
@@ -44,30 +65,30 @@ productList.push({
     //imagen precio nombre
     name: "higado",
     price: 120,
-    image:"https://images.pexels.com/photos/8272928/pexels-photo-8272928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    image: "https://images.pexels.com/photos/8272928/pexels-photo-8272928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 })
 
 productList.push({
     //imagen precio nombre
     name: "Pulmones",
     price: 100000,
-    image:"https://imgs.search.brave.com/dh_Mjj5R_AIHMtR5BhT3_FsN5DdckfpmwqInba19GQU/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/bHVuZ3NhbmR5b3Uu/Y29tL3N0YXRpYy9p/bWFnZXMvcHJvZ3Jl/c3Npb24vbm9ybWFs/LWx1bmdzLmpwZw"
+    image: "https://imgs.search.brave.com/dh_Mjj5R_AIHMtR5BhT3_FsN5DdckfpmwqInba19GQU/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/bHVuZ3NhbmR5b3Uu/Y29tL3N0YXRpYy9p/bWFnZXMvcHJvZ3Jl/c3Npb24vbm9ybWFs/LWx1bmdzLmpwZw"
 })
 
 productList.push({
     //imagen precio nombre
     name: "Riñones",
     price: 120,
-    image:"https://e7.pngegg.com/pngimages/779/986/png-clipart-kidney-kidney-failure-membranoproliferative-glomerulonephritis-minimal-change-disease-membranous-glomerulonephritis-kidney-miscellaneous-hand.png"
+    image: "https://e7.pngegg.com/pngimages/779/986/png-clipart-kidney-kidney-failure-membranoproliferative-glomerulonephritis-minimal-change-disease-membranous-glomerulonephritis-kidney-miscellaneous-hand.png"
 })
 
-function renderProducts(arr){
+function renderProducts(arr) {
     for (product of productList) {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
-    
+
         productCard.innerHTML = `
-        <img src="${product.image}" alt="">
+        <img class = "imagenDetalle" src="${product.image}" alt="">
         <div class="product-info">
             <div>
                 <p>$ ${product.price}</p>
