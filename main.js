9a7625b20cdc0e3f43cbcd4aa22388bce80aad21
jesusserpa <jesusdavidserpa@hotmail.com>
2023-04-03 16:16:29 -0500

    Agregamos cambio de imagen bike

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ const productList = [];
 productList.push({
   name: 'Bike',
   price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
 productList.push({
   name: 'Pantalla',
