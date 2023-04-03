cf6378c32f37a03eec4b537268668eb942163584
alejandro911pa <alejandro.paez39@unach.mx>
2023-04-02 19:58:12 -0600

    correccion menu mobile y menu desplegable

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 const menuEmail = document.querySelector(".navbar-email")
 const DesktopMenu = document.querySelector(".desktop-menu")
 
+const MenuBurger = document.querySelector(".menuBurger")
+const mobileMenu = document.querySelector(".mobile-menu")
+
+
 menuEmail.addEventListener('click',toogleDesktopMenu)
 
 function toogleDesktopMenu(){
@@ -9,3 +13,9 @@ function toogleDesktopMenu(){
 }
 
 
+
+MenuBurger.addEventListener("click",toogleMobile)
+
+function toogleMobile(){
+    mobileMenu.classList.toggle("inactive")
+}
