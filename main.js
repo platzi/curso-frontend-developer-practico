6cc9ecfe055afbf01d3208ce1b80f192f36ac473
DiegoRueda <diegocsgow@gmail.com>
2023-04-03 12:55:33 -0400

    Menu agregado del carrito de comprar y se involucra logica de programacion para abrir y cerrar menus cuando uno este abierto y el otro no

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobMenu = document.querySelector('.mobile-menu');
+const shcar = document.querySelector('.navbar-shopping-cart');
+const detailP = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
+    const isdetailOpen = detailP.classList.contains('inactive');
+    if(!isdetailOpen){
+        detailP.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
-
 }
-
-const burguerMenu = document.querySelector('.menu');
-const mobMenu = document.querySelector('.mobile-menu');
 
 burguerMenu.addEventListener('click', toggleMobMenu);
 
 function toggleMobMenu(){
-    mobMenu.classList.toggle('none')
+    const isdetailOpen = detailP.classList.contains('inactive');
+    //Cuando esta abierto es False y cuando esta cerrado es True
+    if(!isdetailOpen){
+        detailP.classList.add('inactive');
+    }
+
+    mobMenu.classList.toggle('none'); 
+}
+
+shcar.addEventListener('click', segnal);
+
+function segnal(){
+    /*cuando esta abierto False y cuando esta cerrado True*/
+    const ismobileMenuOpen = mobMenu.classList.contains('none');
+    const isDeksMenuOpen = desktopMenu.classList.contains('inactive');
+
+    if(!isDeksMenuOpen){
+        desktopMenu.classList.add('inactive');
+    }
+
+    if(!ismobileMenuOpen){         //Se cambia de False a True para ejecutar
+        mobMenu.classList.add('none'); //Se agrega la clase nuevamente.
+    }
+
+    detailP.classList.toggle('inactive'); // y como consecuencia se abre la nueva pestaña
+    
 }
