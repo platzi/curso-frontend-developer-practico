fa0f51d9e837ff0c5d466dff7b952e1df17668ce
carlitoxe <cerc-12@hotmail.com>
2023-04-04 12:23:12 -0400

    added list of products and some feature to show menus

--- main.js
+++ main.js
@@ -1,8 +1,128 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const orderShoppingCart = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleOrder);
 
 function toggleDesktopMenu() {
+/*     const isOrderClosed = orderShoppingCart.classList.contains('inactive')
+    if (!isOrderClosed) {
+        orderShoppingCart.classList.add('inactive');
+    } */
+    orderShoppingCart.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+/*     const isOrderClosed = orderShoppingCart.classList.contains('inactive')
+
+    if (!isOrderClosed) {
+        orderShoppingCart.classList.add('inactive');
+    } */
+    orderShoppingCart.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleOrder() {
+/*     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');  
+    } */
+    mobileMenu.classList.add('inactive');  
+    orderShoppingCart.classList.toggle('inactive');
+
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Smart TV',
+    price: 300,
+    image: 'https://www.sencor.com/getmedia/f58c499b-6cf0-47e3-851b-5f0ef546e565/SLE-55US800TCSB-front.jpg.aspx?width=2100&height=2100&ext=.jpg ',
+});
+productList.push({
+    name: 'Laptop',
+    price: 700,
+    image: 'https://www.laserprintsoluciones.com/wp-content/uploads/2022/08/X512JA-211VBGB.png',
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Smart TV',
+    price: 300,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Laptop',
+    price: 700,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+/* 
+<div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+            <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+            </div>
+            <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="">
+            </figure>
+        </div>
+</div> 
+*/
+
+function renderProducts(arr) {
+
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const figureImg = document.createElement('img');
+        figureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+        productInfoFigure.appendChild(figureImg);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+
+}
+
+renderProducts(productList);
