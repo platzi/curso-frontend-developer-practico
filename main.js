5fde8fd75277aaa01e696ac77a5e6d5cb7b4de46
IDMB10 <ivandavid.martinez10@gmail.com>
2023-04-04 10:29:24 -0500

    Se crea función para hacer render de productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuBurger = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const iconoCarritoCompras = document.querySelector(".navbar-shopping-cart");
 const asideProductDetail = document.querySelector(".product-detail");
+const productCardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
@@ -44,3 +45,79 @@ function toggleAsideProductDetail() {
 
     asideProductDetail.classList.toggle('inactive');
 }
+
+function renderProducts(arrayProducts) {
+    for (const product of arrayProducts) {
+
+        const productCart = document.createElement('div');
+        productCart.classList.add("product-card");
+
+        const ProductImage = document.createElement('img');
+        //ProductImage.src = product.image;
+        ProductImage.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add("product-info");
+
+        const divData = document.createElement('div');
+
+        const ParrafoProductPrice = document.createElement('p');
+        ParrafoProductPrice.innerHTML = '$' + product.price;
+
+        const ParrafoProductoName = document.createElement('p');
+        ParrafoProductoName.innerHTML = product.name;
+
+        const productFigure = document.createElement('figure');
+
+        const productAddCartImg = document.createElement('img');
+        productAddCartImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productFigure.appendChild(productAddCartImg);
+        divData.append(ParrafoProductPrice, ParrafoProductoName);
+        productInfo.append(divData, productFigure);
+        productCart.append(ProductImage, productInfo);
+
+        productCardsContainer.appendChild(productCart);
+    }
+}
+
+function renderProducts2(arrayProducts) {
+    for (const product of arrayProducts) {
+        const html = `<div class="product-card">
+                        <img src=${product.image}
+                            alt="">
+                            <div class="product-info">
+                                <div>
+                                    <p>$${product.price}</p>
+                                    <p>${product.name}</p>
+                                </div>
+                                <figure>
+                                    <img src="./icons/bt_add_to_cart.svg" alt="">
+                                </figure>
+                            </div>
+                    </div>`;
+        
+        productCardsContainer.innerHTML += html;
+    }
+}
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: "Computer",
+    price: 1000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: "Keyboard",
+    price: 300,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+renderProducts(productList);
