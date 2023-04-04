53aeeb967108b6ddb371ad91649cf3d957e3aa6e
JacoboGL <jaco401@hotmail.com>
2023-04-04 11:49:00 -0500

    Created the JS component to push product cards generated from list

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// LLAMAR ELEMENTOS DESDE EL CSS
+// CALL CSS ELEMENTS
 
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
@@ -6,11 +6,13 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const burguerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
-const shoppingCart = document.querySelector(".navbar-shopping-cart")
-const checkoutMenu = document.querySelector(".product-detail")
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const checkoutMenu = document.querySelector(".product-detail");
+
+const cardsContainer = document.querySelector(".cards-container");
 
 
-// INTERACTIVIDAD
+// INTERACTION
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
@@ -19,7 +21,7 @@ burguerMenu.addEventListener("click", toggleMobileMenu);
 shoppingCart.addEventListener("click", toggleShoppingCart);
 
 
-// FUNCIONES
+// FUNCTIONS FOR THE INTERACTIONS
 
 function toggleDesktopMenu() {
     const isShoppingCartOpen = checkoutMenu.classList.contains('inactive');
@@ -56,3 +58,74 @@ function toggleShoppingCart() {
 
     checkoutMenu.classList.toggle('inactive');
 }
+
+// GENERATE PRODUCT CARDS AND THE HTML FOR THEM
+
+const productList = [];
+
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productList.push({
+    name: "Computer",
+    price: 1100,
+    image: "https://images.pexels.com/photos/5552789/pexels-photo-5552789.jpeg?auto=compress&cs=tinysrgb&w=1600"
+});
+
+productList.push({
+    name: "Fridge",
+    price: 750,
+    image: "https://media.istockphoto.com/id/1344561356/es/foto/lado-a-lado-refrigerador-lleno-refrigerador-muestra-interior.jpg?s=612x612&w=0&k=20&c=oixNLthMLEYJb7RxjE8SDrg0kKc214V7JSdy9Eg1VuE="
+});
+
+productList.push({
+    name: "Headphones",
+    price: 200,
+    image: "https://images.pexels.com/photos/15840650/pexels-photo-15840650.jpeg?auto=compress&cs=tinysrgb&w=1600"
+});
+
+productList.push({
+    name: "Plants",
+    price: 30,
+    image: "https://images.pexels.com/photos/4484184/pexels-photo-4484184.jpeg?auto=compress&cs=tinysrgb&w=1600"
+});
+
+for (product of productList){
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    productInfoDiv.append(productPrice, productName);
+
+    const productInfoFigure = document.createElement("figure");
+
+    const productImgCard = document.createElement("img");
+    productImgCard.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCard);
+
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImg, productInfo);
+
+    cardsContainer.appendChild(productCard);
+}
+
+
+
