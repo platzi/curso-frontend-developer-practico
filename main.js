d85469295ef98fa5985085448b3f750e97f7a2d5
Cerdan31 <jamoreno33@misena.edu.co>
2023-04-06 18:20:31 -0500

    carrito de compras

--- main.js
+++ main.js
@@ -2,15 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const  desktopMenu= document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const MenuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click',toggleDesktopMenu);
-menuHamIcon.addEventListener('click',toggleMobileMenu)
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+MenuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
+
+    const isasideMenuClosed = aside.classList.contains('inactive'); 
+    if(!isasideMenuClosed)
+    {
+        aside.classList.add('inactive');
+
+    }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isasideMenuClosed = aside.classList.contains('inactive'); 
+    if(!isasideMenuClosed)
+    {
+        aside.classList.add('inactive');
 
+    }
     mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside(){
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive'); 
+   // 
+    //
+    if(!isMobileMenuClosed){
+    
+            mobileMenu.classList.add('inactive');
+        }
+        aside.classList.toggle('inactive');
+    }
+    
