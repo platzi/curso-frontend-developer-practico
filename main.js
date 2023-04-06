c8e6c8801bc261e507ef157c4395a2d2763940b4
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-04-06 00:20:56 -0600

    Integración de los menus para Desktop y Mobile (con animación)

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconBurgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+// Despliegue de menú para Desktop
 menuEmail.addEventListener('click', toggleMenuDesktop); // "toggle" significa intercambiar  
 
 function toggleMenuDesktop(){
-    console.log('Click')
-    desktopMenu.classList.toggle('inactive');           // El método ".classList.toggle()" se encarga de poner y quitar la clase ".inactive" (Es como un botón de una lampára)
+    desktopMenu.classList.toggle('inactive');           // El método ".classList.toggle()" se encarga de añadir y quitar la clase ".inactive" (Es como un botón de una lampára)
 }
 
-console.log('js funcionando');
+
+// Despliegue de menú para Mobile
+iconBurgerMenu.addEventListener('click', toggleMenuMobile);
+
+function toggleMenuMobile(){
+    mobileMenu.classList.toggle('oculto');              // Acá usé otra transición para mostra el menú para mobile
+}
+
