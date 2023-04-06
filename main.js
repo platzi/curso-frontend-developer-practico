e4a41432faa5d75b7086af0577377c9ae99bc8b6
alejo <alejandroleakov@gmail.com>
2023-04-06 11:02:32 -0400

    funciones primerias finalizadas

--- main.js
+++ main.js
@@ -4,68 +4,90 @@ const burguerMenu = document.querySelector('.menu');
 const mobMenu = document.querySelector('.mobile-menu');
 const shcar = document.querySelector('.navbar-shopping-cart');
 const detailP = document.querySelector('.product-detail');
+const productAside = document.querySelector('.product-detail-p2');
+const asideBtn = document.querySelector('.product-detail-close');
 const cardsContainer = document.querySelector('.cards-container');
 const productList = [];
-
+const productImgDetail = document.querySelector('.product-detail-image'); 
+    
 //LISTA DE PRODUCTO A PARTIR DE UN ARRAY
-productList.push({
-    name: 'Bike',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 
-});
-productList.push({
-    name: 'Pantalla',
-    price: 220,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+function construction(product, price, image){
+    this.name = product;
+    this.price = price;
+    this.image = image;
+}
 
-});
-productList.push({
-    name: 'Computador',
-    price: 620,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+let newProduct = new construction('Bike', 12700, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
 
-});
+let newProduct2 = new construction('Bicycle helmet', 1200, 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto');
+
+let newProduct3 = new construction('Seat', 300, 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg');
+
+let newProduct4 = new construction('Tennis Montain Bike', 2200, 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg');
+
+let newProduct5 = new construction('Bicycle seat bag', 800, 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg');
+
+
+productList.push(newProduct, newProduct2, newProduct3, newProduct4, newProduct5);
+
+//1 INTEGRACION DESPLIEGUE ASIDE POR CLICK A UN IMAGEN PRODUCT PARA COMPRAR
+function closeAside(){
+    productAside.classList.add('inactive');
+}
+
+asideBtn.addEventListener('click', closeAside); //1
+  
+function openAside(event){
+    const isdetailPOpen = detailP.classList.contains('inactive');
+    productImgDetail.setAttribute('src', event.target.src);
+
+    if(!isdetailPOpen){
+        detailP.classList.add('inactive');
+    }
+    productAside.classList.remove('inactive');
+}
+
 
 
 for(product of productList){
-    const productCard = document.createElement('div'); //Se crea el div padre
-    productCard.classList.add('product-card') // Se le agrega la clase al div padre
-    const productImg = document.createElement('img'); //Se crea un etiqueta de img
-    productImg.setAttribute('src', product.image);//se integra la direccion de la img que proviene del objeto
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card') 
+    const productImg = document.createElement('img'); 
 
-    const productInfo = document.createElement('div'); //Se crea el div segundo padre
-    productInfo.classList.add('product-info'); //se crea una clase al div segundo padre
+    productImg.setAttribute('src', product.image); //1
+    
+    productImg.addEventListener('click', openAside);
 
-    const productInfoDiv = document.createElement('div');  //Se crea el elemento Div solo
+    const productInfo = document.createElement('div'); 
+    productInfo.classList.add('product-info'); 
 
-    const productPrice = document.createElement('p'); //se crea el parrafo que contiene el precio
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
     productPrice.innerText = '$' + product.price;
-    const productName = document.createElement('p'); //se crea el parrafo que contiene el nombre
+    const productName = document.createElement('p');
     productName.innerText = product.name;
 
-    productInfoDiv.appendChild(productPrice); //Se integra el precio al div que esta solo
-    productInfoDiv.appendChild(productName);  //Se integra el nombre al div que esta solo
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
 
-    const productInfoFigure = document.createElement('figure'); //Se crea la etiqueta figure
+    const productInfoFigure = document.createElement('figure');
 
-    const productImgCart = document.createElement('img'); //Se crea la etiqueta img
-    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg'); //Se integra la direccion de la imagen
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
 
-    productInfoFigure.appendChild(productImgCart); //Se integra la imagen a la etiqueta figure
+    productInfoFigure.appendChild(productImgCart); 
     
-    productInfo.append(productInfoDiv, productInfoFigure);//Se integra el div dos parrafos al segundo padre //Se integra la etiqueta figure al segundo padre 
+    productInfo.append(productInfoDiv, productInfoFigure);
 
-    productCard.append(productImg, productInfo);//Se integra la imagen y div segundo padre, al primer padre
+    productCard.append(productImg, productInfo);
 
-    cardsContainer.appendChild(productCard); //Se integra dentro de HTML el div primer padre contenedor de todo.
+    cardsContainer.appendChild(productCard);
+
 }
    
-
-//INTEGRACION DE FUNCION DESKTOP MENU AL CLICK DEL CORREO
-
-navEmail.addEventListener('click', toggleDesktopMenu);
-
+//2 INTEGRACION DE FUNCION DESKTOP MENU AL CLICK DEL CORREO
 function toggleDesktopMenu(){
     const isdetailOpen = detailP.classList.contains('inactive');
     if(!isdetailOpen){
@@ -74,38 +96,44 @@ function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
-
-//INTEGRACION DEL DESPLIEGUE DE BOTON HAMBURGUESA MOBILE
-
-burguerMenu.addEventListener('click', toggleMobMenu);
-
+//3 INTEGRACION DEL DESPLIEGUE DE BOTON HAMBURGUESA MOBILE
 function toggleMobMenu(){
     const isdetailOpen = detailP.classList.contains('inactive');
-    //Cuando esta abierto es False y cuando esta cerrado es True
+    const isproductAsideOpen = productAside.classList.contains('inactive');
+
     if(!isdetailOpen){
         detailP.classList.add('inactive');
+    }
+    if(!isproductAsideOpen){
+        productAside.classList.add('inactive');
     }
 
     mobMenu.classList.toggle('none'); 
 }
 
-
-// INTEGRACION DEL DESPLIEGUE MENU AL CLICK DEL CARRO DE COMPRAS
-
-shcar.addEventListener('click', segnal);
-
-function segnal(){
-    /*cuando esta abierto False y cuando esta cerrado True*/
+//4 INTEGRACION DEL DESPLIEGUE MENU AL CLICK DEL CARRO DE COMPRAS
+    function segnal(){
     const ismobileMenuOpen = mobMenu.classList.contains('none');
     const isDeksMenuOpen = desktopMenu.classList.contains('inactive');
+    const isproductAsideOpen = productAside.classList.contains('inactive');
 
-    if(!isDeksMenuOpen){
-        desktopMenu.classList.add('inactive');
+    switch(true){
+        case !isproductAsideOpen:
+            productAside.classList.add('inactive');
+            break;
+        case !isDeksMenuOpen:
+            desktopMenu.classList.add('inactive');
+            break;
+        case !ismobileMenuOpen:
+            mobMenu.classList.add('none');
+            break;
     }
-
-    if(!ismobileMenuOpen){         //Se cambia de False a True para ejecutar
-        mobMenu.classList.add('none'); //Se agrega la clase nuevamente.
-    }
-
-    detailP.classList.toggle('inactive'); // y como consecuencia se abre la nueva pestaña
+    detailP.classList.toggle('inactive'); 
 }
+
+//2
+navEmail.addEventListener('click', toggleDesktopMenu);
+//3
+burguerMenu.addEventListener('click', toggleMobMenu);
+//4
+shcar.addEventListener('click', segnal);
