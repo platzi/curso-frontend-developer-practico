6e69efd7f883573fd37e5db732494a8f08b8a0f5
Hamilton Posada Serna <posadahamilton@gmail.com>
2023-04-06 14:14:25 +0200

    added products to the webshop

--- script.js
+++ script.js
@@ -1,8 +1,103 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-
-menuEmail.addEventListener("click", toggleDesktopMenu);
+const menuHamIcon = document.querySelector(".menu");
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 function toggleDesktopMenu() {
+  const isAsideClose = aside.classList.contains("inactive");
+
+  if (!isAsideClose) {
+    aside.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
+
+function toggleMobileMenu() {
+  const isAsideClose = aside.classList.contains("inactive");
+
+  if (!isAsideClose) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCartAside() {
+  const isMobileMenuClose = mobileMenu.classList.contains("inactive");
+  const isDesktopMenuClose = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClose) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  if (!isDesktopMenuClose) {
+    desktopMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Monitor",
+  price: 220,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Laptop",
+  price: 580,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProducts(array) {
+  array.forEach((product) => {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.textContent = `$${product.price}`;
+    const productName = document.createElement("p");
+    productName.textContent = `${product.name}`;
+
+    productInfoDiv.append(productPrice, productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImg, productInfo);
+
+    cardsContainer.appendChild(productCard);
+  });
+}
+
+renderProducts(productList);
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCartIcon.addEventListener("click", toggleCartAside);
