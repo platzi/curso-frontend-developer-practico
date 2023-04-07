b7b03c6c4487a4f2f9b9091f16d3bbf3676c6442
Juanjo Mayotte <juanjomayotte@gmail.com>
2023-04-07 13:39:01 +0200

    integramos el menu mobile y lo hicimos responsive para que aparezca en tamaños de pantalla menores a 640px

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu-ham-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu); //toggle = palanca
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
