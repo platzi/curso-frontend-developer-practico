9039dd36644ea467f6a26be8ec38ff36b8dfa0f4
Cerdan31 <jamoreno33@misena.edu.co>
2023-04-07 09:32:57 -0500

    Lista de productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const MenuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
@@ -43,3 +44,85 @@ function toggleCarritoAside(){
         aside.classList.toggle('inactive');
     }
     
+const productList =[];
+productList.push({
+name:'Bike',
+price: 120,
+Image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+
+})  ;  
+productList.push({
+    name:'Carro',
+    price: 1000,
+    Image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    
+    })  ;  
+productList.push({
+        name:'televisor',
+        price: 30000,
+        Image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        
+        })  ;      
+productList.push({
+            name:'televisor',
+            price: 30000,
+            Image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            
+            })  ;      
+productList.push({
+                name:'televisor',
+                price: 30000,
+                Image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                
+                })  ;      
+productList.push({
+                    name:'televisor',
+                    price: 30000,
+                    Image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                    
+                    })  ;      
+                        
+       
+function renderProducts(arr){
+
+    for(product of arr){
+        const producCard = document.createElement('div');
+        
+        producCard.classList.add('product-card');
+        const productImg =document.createElement('img');
+        // product = {mane , price image}
+        productImg.setAttribute('src',product.Image);
+
+        const producInfo = document.createElement('div');
+        producInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+       // producInfoDiv.classList.add('product-info');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' +product.price;
+        const productName = document.createElement('p');
+        productName.innerText =product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+     ;
+        producInfo.appendChild(productInfoDiv);
+        producInfo.appendChild(productInfoFigure);
+        producCard.appendChild(productImg);
+        producCard.appendChild(producInfo);
+       
+      //  cardsContainer.appendChild(producCard);
+        cardsContainer.appendChild(producCard);
+
+
+     }
+     renderProducts(productList);
+
+}
