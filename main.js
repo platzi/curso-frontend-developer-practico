b5712040b78ad86cea2980a8add90f8fcff3d11e
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-04-06 23:51:17 -0600

    Mejoras de legibilidad

--- main.js
+++ main.js
@@ -58,23 +58,10 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 })
 
-{/* <div class="product-card">
-    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-    <div class="product-info">
-      <div>
-        <p>$120,00</p>
-        <p>Bike</p>
-      </div>
-      <figure>
-        <img src="./icons/bt_add_to_cart.svg" alt="">
-      </figure>
-    </div>
-</div> */}
-
 
 function renderProducts(arrayProducts){                                          // Función que despliega los elementos de un Array para los productos de la tienda   
     for(product of arrayProducts){                                               // Voy a recorrer los elementos de mi Array de Objetos, y generar elementos HTML con sus class de CSS usando JavaScript
-        // En esta sección por a generar mis elementos HTML 
+        // En esta sección por a generar mis elementos HTML                      // Para maquetar este diseño, use de referencia el archivo HTML de origen de las imagenes 
         const productCard =  document.createElement('div');                      // Creo un elemento <div> 
         productCard.classList.add('product-card');                               // Le agrego su class correspondiente 'product-card'
      
