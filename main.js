ca92307d27f492218df7b4a47ad2aa3bde8156ce
Xanarchy820 <55326894sg@gmail.com>
2023-04-08 02:24:03 -0600

    agregando menu del carrito

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burguerIcon = document.querySelector(".menu");
+const carritoIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burguerIcon.addEventListener("click", toggleMobileMenu);
+carritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    aside.classList.add("inactive");
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+    aside.classList.add("inactive");
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+    aside.classList.toggle("inactive");
 }
