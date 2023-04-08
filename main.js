341b62b54b251e499c63ad80f67641c7f3f64555
Xanarchy820 <55326894sg@gmail.com>
2023-04-08 14:05:19 -0600

    agregando la lista de productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const burguerIcon = document.querySelector(".menu");
 const carritoIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burguerIcon.addEventListener("click", toggleMobileMenu);
@@ -24,3 +25,66 @@ function toggleCarritoAside() {
     mobileMenu.classList.add("inactive");
     aside.classList.toggle("inactive");
 }
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productList.push({
+    name: "Motorcycle",
+    price: 1400,
+    image: "./images/bike-g0f75bc46d_1280.jpg"
+});
+
+productList.push({
+    name: "MacBook",
+    price: 800,
+    image: "./images/macbook-g6d3523b9f_1280.jpg"
+});
+
+productList.push({
+    name: "Xbox",
+    price: 1000,
+    image: "./images/video-game-console-g7eaa97af7_1280.jpg"
+});
+
+productList.push({
+    name: "Motorcycle helmet",
+    price: 80,
+    image: "./images/motorbike-g884dc8432_1280.jpg"
+});
+
+function renderProducts(array) {
+    for(product of array) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.image);
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        const productInfoDiv = document.createElement("div");
+    
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        const productInfoFigure = document.createElement("figure");
+        const productImgCart = document.createElement("img");
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    
+        productInfoFigure.appendChild(productImgCart);
+        productInfoDiv.append(productPrice, productName);
+        productInfo.append(productInfoDiv, productInfoFigure);
+        productCard.append(productImg, productInfo);
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
