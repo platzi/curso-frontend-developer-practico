2271debd40ec0a9a3f208851b55579a6c2409211
misterdan100 <danielmca60@gmail.com>
2023-04-08 20:24:10 -0500

    Clase 22 lista de productos escribiendo html desde JS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHumIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHumIcon.addEventListener("click", toggleMobileMenu);
@@ -41,3 +42,61 @@ function toggleCarritoAside() {
     aside.classList.toggle('inactive');
 }
 
+//renderizar productos ----------------------
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://source.unsplash.com/xwBZl79AJBo"
+})
+productList.push({
+    name: "Pantalla",
+    price: 220,
+    image: "https://source.unsplash.com/rmHTCPXUk9g"
+})
+productList.push({
+    name: "Compu",
+    price: 620,
+    image: "https://source.unsplash.com/_qOf3MkCrhk"
+})
+
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImg = document.createElement("img") 
+        productImg.setAttribute("src", product.image)
+    
+        const productInfo = document.createElement("div")
+        productInfo.classList.add("product-info")
+    
+        const productInfoDiv = document.createElement("div");
+        
+        const productPrice = document.createElement("p")
+        productPrice.innerText = "$ " + product.price;
+        const productName = document.createElement("p")
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+        const productInfoFigure = document.createElement("figure")
+        
+        const productImgCart = document.createElement("img");
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg" )
+    
+        productInfoFigure.appendChild(productImgCart)
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+    
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList)
