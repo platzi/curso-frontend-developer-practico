253d14f73a6a302fd640e47f7a6432549e041990
Luis R Neme <luisrneme91@gmail.com>
2023-04-08 23:57:17 -0300

    Agregamos la lista de producto con JS

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const menuEmail = document.querySelector('.navbar-email');//creo una variable 
 const desktopMenu = document.querySelector('.desktop-menu');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);/* toggleDesktopMenu es una funcion */
 
@@ -48,7 +49,78 @@ function toggleCarritoAside(){
 
 }
 
+/* Lista de productos */
 
+const productList = [];//Array
+productList.push({//Agrego un producto al Array 
+    name:'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+});
+productList.push({//Agrego un producto al Array 
+    name:'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+});
+productList.push({//Agrego un producto al Array 
+    name:'Computadora',
+    price: 720,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+});
+
+
+//Insertar el Array en HTML y para eso debemos recorrer el Arrays
+
+
+function renderProducts(arr){
+    for(product of productList){//product es el objeto que tengamos de nuestro arrays
+    
+        //unimoso con productCard que es div principal el cual esta contenido por cards-container
+       
+        
+        const productCard = document.createElement('div');//creamos un div
+        productCard.classList.add('product-card');//creamos la clase de div class='product-card'
+           
+        const productImg = document.createElement('img');//creamos una imagen
+        productImg.setAttribute('src', product.image);//le asignamos una imagen
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+         
+        const productInfoDiv = document.createElement('div');//div sin clase
+    
+        const productPrice = document.createElement('p');//creamos los parrafos
+        productPrice.innerText='$' + product.price;//Le asignamos el texto al parrafo
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        const productInfoFigures = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+        //Le agregamos a productInfoFigures la productImgCart
+        productInfoFigures.appendChild(productImgCart);
+        
+        //agregamos los parrafos a el segundo div
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        //agregamos el div y el figure al contenedor principal div
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigures);
+        
+        productCard.appendChild(productInfo);
+        productCard.appendChild(productImg);
+        
+        cardsContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProducts(productList);//Llamo a la funcion
 
 
 
