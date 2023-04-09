f3b966cc4934ddfe9921f5f180db284a4e832fdb
Oscar Quinones <oscardqq@hotmail.com>
2023-04-08 21:47:25 -0500

    Se realiza union del navbarEmailconel DesktopMenu, adicional se realizan cambios en css, html y js para que funcione al hacer el llamado de la funcion

---
+++ main.js
@@ -0,0 +1,10 @@
+const navbarEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu")
+
+
+navbarEmail.addEventListener('click', toggleDesktopMenu)
+
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive');
+}
+
