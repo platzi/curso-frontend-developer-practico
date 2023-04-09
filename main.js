ff40f0b94a153624e6cd8bfab03d43a11c23a3fb
Oscar Quinones <oscardqq@hotmail.com>
2023-04-09 09:58:39 -0500

    Se agrega despliegue del menu mobil, adicional se le agrega mmax-width y min-width en cuanto aplique el caso

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const navbarEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu")
+const desktopMenu = document.querySelector(".desktop-menu");
+const logoDeMenu = document.querySelector(".logoMenu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 
-navbarEmail.addEventListener('click', toggleDesktopMenu)
+navbarEmail.addEventListener('click', toggleDesktopMenu);
+logoDeMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
-}
+};
 
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+};
