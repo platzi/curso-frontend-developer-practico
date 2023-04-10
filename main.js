7f343e6b0f9edb32428099086b0ebf2a654220d8
Luis Diaz <luisdiaz_1706@hotmail.com>
2023-04-09 18:13:06 -0600

    Se agrega una nueva funcionalidad al menu.

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 let menuEmail = document.querySelector('.navbar-email');
 let desktopMenu = document.querySelector('.desktop-menu');
+let menuIconH = document.querySelector('.menu');
+let mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuIconH.addEventListener('click',toggleMobilepMenu);
 
 function toggleDesktopMenu(){
-
     desktopMenu.classList.toggle('inactive');
+}
 
+function toggleMobilepMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
