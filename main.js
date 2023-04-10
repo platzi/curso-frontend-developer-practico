f56bd65f52a1b2a293743374584a27ac74cd13d7
Juan sebastian reyes vasquez <jsreyes979@gmail.com>
2023-04-10 16:04:58 -0500

    Primera parte de la Integracion de detalle del producto

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-const aside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 
@@ -15,22 +15,22 @@ menuHamIcon.addEventListener('click', toggleMobileMenu)
 menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
-	const isAsideClosed = aside.classList.contains('inactive')
+	const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
 	
 	
 	if(!isAsideClosed){
-		aside.classList.add('inactive')
+		shoppingCartContainer.classList.add('inactive')
 	}
 	desktopMenu.classList.toggle('inactive')
 }
 
 
 function toggleMobileMenu(){
-	const isAsideClosed = aside.classList.contains('inactive')
+	const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
 	
 	
 	if(!isAsideClosed){
-		aside.classList.add('inactive')
+		shoppingCartContainer.classList.add('inactive')
 	}
 	mobileMenu.classList.toggle('inactive')
 
@@ -44,7 +44,7 @@ function toggleCarritoAside(){
 		mobileMenu.classList.add('inactive')
 	}
 	
-	aside.classList.toggle('inactive')
+	shoppingCartContainer.classList.toggle('inactive')
 }
 
 
