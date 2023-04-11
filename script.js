7c191c1f21381b0fb37b77e82c22db497f6cdbad
Arturo Hernandez <earturohl@gmail.com>
2023-04-11 03:49:56 -0500

    mobileMenu and bug fixes(responsiveDesing)

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const compressedMenu = document.querySelector('.compressedMobileMenu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 navEmail.addEventListener('click', toggleDesktopMenu)
+compressedMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
+}
+
