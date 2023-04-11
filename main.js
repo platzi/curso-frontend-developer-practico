bc1da57189e80acd87733f774c2ea4288f047921
wilgar <wilenri341@hotmai.com>
2023-04-11 17:37:45 -0500

    en este paso agregamos el menu en mobile y agregamos el los productos del carrito de compra. Ademas generamos interactividad al hacer click en los menu de desktop, mobile y el carrito de compras, para que no permanecieran ambos abiertos al mismo tiempo (manipulando css, main e index)

--- main.js
+++ main.js
@@ -2,16 +2,41 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const iconMenuMob = document.querySelector(".menu"); //boton para menu en mobile
 const mobileMenu = document.querySelector(".mobile-menu");
-
+const iconMenuCar = document.querySelector(".shoppin-cart");//boton para mostrar product-detail en el carrito
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 iconMenuMob.addEventListener("click", toggleMobileMenu);
+iconMenuCar.addEventListener("click", toggleProductCarrito);
 
 
 function toggleDesktopMenu(){
+    const isAsideClose = aside.classList.contains("inactive");
+    if (!isAsideClose){
+        aside.classList.add("inactive");
+    }
+    
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
+    const isAsideMobileClose = aside.classList.contains("inactive");
+    if (!isAsideMobileClose){
+        aside.classList.add("inactive");
+    }
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleProductCarrito(){
+    const isMobileMenuClose = mobileMenu.classList.contains("inactive");
+    if (!isMobileMenuClose){
+        mobileMenu.classList.add("inactive");
+    }
+    
+    const isDesktopMenuClose = desktopMenu.classList.contains("inactive");
+    if (!isDesktopMenuClose){
+        desktopMenu.classList.add("inactive");
+    }
+    
+    aside.classList.toggle("inactive");
 }
