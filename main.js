58b3b21a763fc9f72b64fbd89cf92029c7310a0f
Maria <mariaf7458@gmail.com>
2023-04-10 20:59:25 -0500

    Se agrego menu hamburgesa a mobile menu

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburguesa = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu (){
     //desktopMenu default = inactive 
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    //menu hamburguesa
+  mobileMenu.classList.toggle('inactive');
+}
+
 
   
