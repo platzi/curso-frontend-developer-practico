f4a0efd83ba69348e1f19cae4608e62ef984d5ea
Maria <mariaf7458@gmail.com>
2023-04-10 21:31:05 -0500

    Se agrego carrito de compras desplegable, ningunode los  menus desplegables interfieren entre ellos

--- main.js
+++ main.js
@@ -2,18 +2,30 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carritoIcon = document.querySelector('.carrito');
+const carritocontent = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguesa.addEventListener('click', toggleMobileMenu);
+carritoIcon.addEventListener('click', toggleCartContent);
 
+//Desktop menu
 function toggleDesktopMenu (){
-    //desktopMenu default = inactive 
+    carritocontent.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
+//Mobile menu
 function toggleMobileMenu(){
     //menu hamburguesa
+  carritocontent.classList.add('inactive');
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartContent(){
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    carritocontent.classList.toggle('inactive');
 }
 
 
