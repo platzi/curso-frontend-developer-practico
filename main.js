e795372000dd6d279ba0b9e70aa49beaf36e6c90
Aleja Ramirez <aleja2107@yahoo.com>
2023-04-10 22:55:22 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsaidClosed = aside.classList.contains ('inactive');
+
+    if (!isAsaidClosed) {
+        aside.classList.add ('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsaidClosed = aside.classList.contains ('inactive');
+
+    if (!isAsaidClosed) {
+        aside.classList.add ('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const ismobileMenuClosed = mobileMenu.classList.contains ('inactive');
+    
+    if (!ismobileMenuClosed) {
+        mobileMenu.classList.add ('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
