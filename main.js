737a1bb02f6ce1ea61ee88749e0887899524096f
phillip191 <127802102+phillip191@users.noreply.github.com>
2023-04-11 18:19:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const DesktopMenu = document.querySelector('.desktop-menu')
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
+    
+    const IsCarritoDetailClosed = CarritoDetail.classList.contains('inactive');
+    if(!IsCarritoDetailClosed){
+
+        CarritoDetail.classList.add('inactive')
+    }
+    
     DesktopMenu.classList.toggle('inactive');
 }
 
@@ -14,5 +21,32 @@ const MobileMenu = document.querySelector('.mobile-menu')
 menuburguer.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu(){
+
+    const IsCarritoDetailClosed = CarritoDetail.classList.contains('inactive');
+    if(!IsCarritoDetailClosed){
+
+        CarritoDetail.classList.add('inactive')
+    }
+    
     MobileMenu.classList.toggle('inactive');
+}
+
+
+const CarritoDetail = document.querySelector('.product-detail')
+const menuCarrito = document.querySelector('.navbar-shopping-cart')
+
+menuCarrito.addEventListener('click', toggleCarritoDetail);
+
+function toggleCarritoDetail(){
+
+    const IsMobileMenuClosed = MobileMenu.classList.contains('inactive');
+   
+    if(!IsMobileMenuClosed){
+        MobileMenu.classList.add('inactive')
+    }
+    
+   
+    CarritoDetail.classList.toggle('inactive');
+
+  
 }
