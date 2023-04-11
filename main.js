11de27a6ed7c2df4988a9fea988b44922d674e65
wilgar <wilenri341@hotmai.com>
2023-04-11 15:47:59 -0500

    Despues de agregar el navbar y el menu desktop, en este paso agregamos el menu desktop a los archivos principales (clase 8).

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const iconMenuMob = document.querySelector(".menu"); //boton para menu en mobile
+const mobileMenu = document.querySelector(".mobile-menu");
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+iconMenuMob.addEventListener("click", toggleMobileMenu);
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
 }
- 
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+}
