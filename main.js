faaa0a4a1a408cf92a56eda3f3602dbba9b275ad
wendy Arcia <wendy_342@hotmail.com>
2023-04-10 22:45:08 -0300

    Mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const navEmail = document.getElementsByClassName("navbar-email")[0];
 const desktopMenu = document.getElementsByClassName("desktop-menu")[0];
-console.log(desktopMenu[0]);
+const hamburgerMenuIcon = document.getElementsByClassName("menu")[0];
+const mobileMenu = document.getElementsByClassName("mobile-menu")[0];
 
 navEmail.addEventListener("click", toggleDesktopNav);
+hamburgerMenuIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopNav() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
