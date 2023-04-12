dc935e74d7aa7d7e4908e61ad0fc59a514393816
Aleja Ramirez <aleja2107@yahoo.com>
2023-04-11 23:24:11 -0500

    Detalle del producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -12,19 +12,19 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isAsaidClosed = aside.classList.contains ('inactive');
+    const isAsaidClosed = shoppingCartContainer.classList.contains ('inactive');
 
     if (!isAsaidClosed) {
-        aside.classList.add ('inactive');
+        shoppingCartContainer.classList.add ('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsaidClosed = aside.classList.contains ('inactive');
+    const isAsaidClosed = shoppingCartContainer.classList.contains ('inactive');
 
     if (!isAsaidClosed) {
-        aside.classList.add ('inactive');
+        shoppingCartContainer.classList.add ('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -35,7 +35,7 @@ function toggleCarritoAside() {
     if (!ismobileMenuClosed) {
         mobileMenu.classList.add ('inactive');
     }
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList =[];
