eeab3c4a7081119bcec1a81e13dfff16ffe5e703
Tzuriel López <tzuriel.lm@hotmail.com>
2023-04-12 18:36:35 -0500

    Fusión del Menú en Mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuIcon = document.querySelector('.menu')
+const menuMobile = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive')
+}
+
+menuIcon.addEventListener('click', toggleDesktopMobile);
+
+function toggleDesktopMobile () {
+    menuMobile.classList.toggle('inactive')
 }
