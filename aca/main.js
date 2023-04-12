a38de072186c034e32664c532e7c4f6890152974
LKlober <klobermdq@gmail.com>
2023-04-12 10:57:34 -0300

    new day

---
+++ aca/main.js
@@ -0,0 +1,8 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopEmail = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+    
+}
