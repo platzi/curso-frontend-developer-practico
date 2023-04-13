324307891b9d264a9cc1dd21871508631c563f35
Tzuriel López <tzuriel.lm@hotmail.com>
2023-04-13 18:50:59 -0500

    Lista de productos HTML a partir de un array

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const menuMobile = document.querySelector('.mobile-menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
 
+const cardsContainer = document.querySelector('.cards-container')
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu () {
@@ -43,3 +45,84 @@ if (!isMobileMenuClosed) {
 
 aside.classList.toggle('inactive');
 }
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Screen',
+    price: 330,
+    image: 'https://www.wesellit.ph/content/images/thumbs/0004877_samsung-24-flat-screen-led-monitor.jpeg',
+})
+
+productList.push({
+    name: 'Laptop',
+    price: 1400,
+    image: 'https://i.pcmag.com/imagery/roundups/02naaOkVLe7DIiejFUyDPJp-31..v1588859331.jpg',
+})
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Screen',
+    price: 330,
+    image: 'https://www.wesellit.ph/content/images/thumbs/0004877_samsung-24-flat-screen-led-monitor.jpeg',
+})
+
+productList.push({
+    name: 'Laptop',
+    price: 1400,
+    image: 'https://i.pcmag.com/imagery/roundups/02naaOkVLe7DIiejFUyDPJp-31..v1588859331.jpg',
+})
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        // product = {name, price, image}
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+        
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        const productInfoFigure = document.createElement('figure')
+    
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList)
