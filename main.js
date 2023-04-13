3c9246ae43399244d151bc25cacf2606b61df774
Tzuriel López <tzuriel.lm@hotmail.com>
2023-04-12 23:28:59 -0500

    Carrito de Compras y Menus añadidos

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+
 const menuIcon = document.querySelector('.menu')
 const menuMobile = document.querySelector('.mobile-menu')
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    } 
+
     desktopMenu.classList.toggle('inactive')
 }
 
-menuIcon.addEventListener('click', toggleDesktopMobile);
+menuIcon.addEventListener('click', toggleMobileMenu);
 
-function toggleDesktopMobile () {
+function toggleMobileMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+if (!isAsideClosed) {
+    aside.classList.add('inactive')
+}
+
     menuMobile.classList.toggle('inactive')
+}
+
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside () {
+const isMobileMenuClosed = menuMobile.classList.contains('inactive');
+const isNavBarEmailClosed = menuEmail.classList.contains('inactive')
+
+if (!isMobileMenuClosed) {
+    menuMobile.classList.add('inactive')
+} else {desktopMenu.classList.add('inactive')}
+
+aside.classList.toggle('inactive');
 }
