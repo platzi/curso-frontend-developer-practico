da8f1c7afa3e792e3faa3df4566b40264e2a1c60
Oscar Quinones <oscardqq@hotmail.com>
2023-04-12 21:56:13 -0500

    se despliega el carro de compras, adicional se modifica js con la finalidad de que al momento de abrir una de nuestras secciones no choque con otra

--- main.js
+++ main.js
@@ -2,15 +2,25 @@ const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const logoDeMenu = document.querySelector(".logoMenu");
 const mobileMenu = document.querySelector(".mobile-menu");
-
+const carritoDecompras = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 logoDeMenu.addEventListener('click', toggleMobileMenu);
+carritoDecompras.addEventListener('click',toggleCarritoAside );
 
 function toggleDesktopMenu(){
+    aside.classList.add('inactive')
     desktopMenu.classList.toggle('inactive');
 };
 
 function toggleMobileMenu(){
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
 };
+
+function toggleCarritoAside(){
+    navbarEmail.classList.add('ínactive')
+    mobileMenu.classList.add('inactive')
+    aside.classList.toggle('inactive');
+}
