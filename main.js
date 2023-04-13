edc93f675f99968aff44e90b6cab97c3fc4f651b
jusched <juans-betancurt@unilibre.edu.co>
2023-04-12 22:22:13 -0500

    added product card function and improved it

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ const mobileMenuIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const cartIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 mobileMenuIcon.addEventListener("click", toggleMobileMenu);
 cartIcon.addEventListener("click", toggleCart);
 
+// If the aside is open, close it
 function toggleDesktopMenu() {
   const isAsideOpen = !aside.classList.contains("inactive");
-  // If the aside is open, close it
+
   if (isAsideOpen) {
     aside.classList.add("inactive");
   }
@@ -27,8 +29,8 @@ function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
 }
 
+// whenever we open our shopping cart, mobileMenu and desktopMenu are closed
 function toggleCart() {
-  // whenever we open our shopping cart, mobileMenu and desktopMenu are closed
   const isDesktopMenuOpen = !desktopMenu.classList.contains("inactive");
   const isMobileMenuOpen = !mobileMenu.classList.contains("inactive");
 
@@ -44,12 +46,36 @@ function toggleCart() {
   aside.classList.toggle("inactive");
 }
 
-// List of products
+// List of products to be displayed in the cards-container
 // Format: {name: String, price: int, image: url}
 const productList = [];
 productList.push({
   name: "Bike",
   price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Computer",
+  price: 200,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Car",
+  price: 1200,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Computer",
+  price: 200,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Car",
+  price: 1200,
   image:
     "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
@@ -82,21 +108,56 @@ productList.push({
           </div>
         </div> */
 
-for (product of productList) {
-  const productCard = document.createElement("div");
-  productCard.classList.add("product-card");
+// This function creates each product-card in the cards-container instead of having
+// to write the HTML code for each product card
+function renderProducts(arr) {
+  for (product of arr) {
+    // We create the first div
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
 
-  const img = document.createElement("img");
-  // product = {name, price, img}
-  img.setAttribute("src", product.image);
+    // We create the image that gets data from the productList array
+    const productImg = document.createElement("img");
+    // product = {name, price, img}
 
-  const productInfo = document.createElement("div");
-  productInfo.classList.add("product-info");
+    productImg.setAttribute("src", product.image);
 
-  const productInfoDiv = document.createElement("div");
+    // We create the second div which contains the product info together with the
+    // add to cart button
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
 
-  const productName = document.createElement("p");
-  productPrice.innerText = product.name;
-  const productPrice = document.createElement("p");
-  productPrice.innerText = "$" + product.price;
+    // We create the div where the product name and price is
+    const productInfoDiv = document.createElement("div");
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+
+    // We append the name and price of each product to the div where the info is.
+    productInfoDiv.appendChild(productName);
+    productInfoDiv.appendChild(productPrice);
+
+    // We create and append the add to cart button
+    const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCart);
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    // Finally, we add every product card created into the main cardContainer
+    cardsContainer.appendChild(productCard);
+  }
 }
+// This recreates the product card for each product in the productList
+// based on the HTML code above which represents a product card
+
+// We only call the function with the product list with the format we have
+// which is {name: String, price: int, image: url}
+renderProducts(productList);
