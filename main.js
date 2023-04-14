603216596ec2804d33e2ef3a9c2c0acc65e486bd
Luis Diaz <luisdiaz_1706@hotmail.com>
2023-04-14 15:40:51 -0600

    Se añade funcionalidad al carrito de compras.

--- main.js
+++ main.js
@@ -2,14 +2,33 @@ let menuEmail = document.querySelector('.navbar-email');
 let desktopMenu = document.querySelector('.desktop-menu');
 let menuIconH = document.querySelector('.menu');
 let mobileMenu = document.querySelector('.mobile-menu');
+let menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+let aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuIconH.addEventListener('click',toggleMobilepMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    let isAsideClsed = aside.classList.contains('inactive');
+    if (!isAsideClsed) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobilepMenu(){
+    let isAsideClsed = aside.classList.contains('inactive');
+    if (!isAsideClsed) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside() {
+    let isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
