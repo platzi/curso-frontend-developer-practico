55a55cf426019ff6679a87a196ac00661c1dc22b
unknown <alvaromenachojs@gmail.com>
2023-04-13 19:16:31 -0500

    uni dos archivos html el del menu y la pagina principal. agregue el js respectivo

---
+++ main.js
@@ -0,0 +1,9 @@
+const navEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+navEmail.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+
+    desktopMenu.classList.toggle('inactive')
+}
