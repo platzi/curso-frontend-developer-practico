488e4f548ed0cbbb04a3e882f7f0ad209609ab3f
Luis Manuel Chávez Gutiérrez <luismanuelchavezgutierrez@gmail.com>
2023-04-15 10:54:50 -0600

    union de clase 8 (mobile menu)

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let navbarEmail = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
+let mobileIconMenu = document.querySelector(".menu");
+let mobileMenu = document.querySelector(".mobile-menu");
 
 function toggleEmailMenu() {
     desktopMenu.classList.toggle("inactive") 
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive") 
 }
 
 navbarEmail.addEventListener('click',toggleEmailMenu)
 console.log(navbarEmail)
 
 desktopMenu.addEventListener('mouseout',toggleEmailMenu)
+
+mobileIconMenu.addEventListener('click',toggleMobileMenu)
