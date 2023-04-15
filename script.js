6f0745fd659ca2a0adf2c8a1802c20501f142f81
Luis Manuel Chávez Gutiérrez <luismanuelchavezgutierrez@gmail.com>
2023-04-15 12:01:39 -0600

    union de clase 13 (aside menu productos)

--- script.js
+++ script.js
@@ -2,18 +2,33 @@ let navbarEmail = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
 let mobileIconMenu = document.querySelector(".menu");
 let mobileMenu = document.querySelector(".mobile-menu");
+let navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
+let asideProductDetailMenu = document.querySelector(".product-detail");
 
 function toggleEmailMenu() {
     desktopMenu.classList.toggle("inactive") 
+    asideProductDetailMenu.classList.contains("inactive") == false ? asideProductDetailMenu.classList.add("inactive") : ''
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive") 
+    asideProductDetailMenu.classList.contains("inactive") == false ? asideProductDetailMenu.classList.add("inactive") : ''
+
+}
+
+function toggleAsideProductDetailMenu() {
+    if(asideProductDetailMenu.classList.contains("inactive") ){
+        if (! mobileMenu.classList.contains("inactive")){
+            mobileMenu.classList.add("inactive") 
+        }
+        if(! desktopMenu.classList.contains("inactive")){
+            desktopMenu.classList.add("inactive") 
+        }
+    }
+    asideProductDetailMenu.classList.toggle("inactive") 
 }
 
 navbarEmail.addEventListener('click',toggleEmailMenu)
-console.log(navbarEmail)
-
 desktopMenu.addEventListener('mouseout',toggleEmailMenu)
-
 mobileIconMenu.addEventListener('click',toggleMobileMenu)
+navbarShoppingCart.addEventListener('click',toggleAsideProductDetailMenu)
