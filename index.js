2f36eba1af72df600ba2e21a4939884c8ea20fdc
EvilAFAS <evilafas99@gmail.com>
2023-04-16 16:06:51 -0500

    Mobile  menu ready

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const email = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const bntMenuMobile = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 email.addEventListener('click',()=>{
     desktopMenu.classList.toggle("toggle");
+})
+
+bntMenuMobile.addEventListener("click",()=>{
+    mobileMenu.classList.toggle("toggle");
 })
