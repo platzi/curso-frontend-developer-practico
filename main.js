058c94db76ee8383281c5759401dc9bd96699c05
Carlos Betancourt <carlosmario110995@gmail.com>
2023-04-15 22:33:37 -0500

    Se consolido la clase 8, se ajusto el mobileMenu con sus respectivas @media

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector ('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIco = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIco.addEventListener('click', togglemobileMenu);
 
 function toggleDesktopMenu() {
-    console.log('click');
     desktopMenu.classList.toggle('inactive');
+}
+
+function togglemobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
