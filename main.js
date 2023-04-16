eb46f8787ddc375c84b105711a8f42d03b2f4611
Francisco J Vite <jeshuavhen@hotmail.com>
2023-04-15 19:03:31 -0600

    Detalles de un producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurgerIcon = document.querySelector('.menu');
 const menuCArritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -14,10 +14,10 @@ menuCArritoIcon.addEventListener('click', toggleCarritoAside);
 
 //* Función que revisa si el contenido del carrito está abierto para cerrarlo al dar click al "Menú en version Desktop"
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');    
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');    
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');            
+        shoppingCartContainer.classList.add('inactive');            
     }
 
     desktopMenu.classList.toggle('inactive');
@@ -25,10 +25,10 @@ function toggleDesktopMenu(){
 
 //* Función que revisa si el contenido del carrito está abierto para cerrarlo al dar click al "Menú en version Mobile"
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');    
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');    
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');            
+        shoppingCartContainer.classList.add('inactive');            
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -45,7 +45,7 @@ function toggleCarritoAside(){
         desktopMenu.classList.add('inactive')   ;     
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 
 }
 
