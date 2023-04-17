9a3b6977dc6a318d3f2cb1ad6912b93a35a93e17
EvilAFAS <evilafas99@gmail.com>
2023-04-16 21:07:36 -0500

    interacciones de los elementos del menú, listos!

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ const email = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const bntMenuMobile = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-
+const productDetail = document.querySelector(".product-detail");
+const navbarShopingCart = document.querySelector(".navbar-shopping-cart");
+  
 email.addEventListener('click',()=>{
-    desktopMenu.classList.toggle("toggle");
+    const cartMenuClosed = productDetail.classList.contains("inactive");
+    if(!cartMenuClosed){
+        productDetail.classList.add("inactive")
+    }
+    desktopMenu.classList.toggle("inactive");
+
 })
 
 bntMenuMobile.addEventListener("click",()=>{
-    mobileMenu.classList.toggle("toggle");
+    const cartMenuClosed = productDetail.classList.contains("inactive");
+    if(!cartMenuClosed){
+        productDetail.classList.add("inactive")
+    }
+    mobileMenu.classList.toggle("inactive");
+})
+
+navbarShopingCart.addEventListener("click",()=>{
+
+    const mobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const emailClosed = desktopMenu.classList.contains("inactive");
+    if(!emailClosed){
+        desktopMenu.classList.add("inactive");
+    }
+    if(!mobileMenuClosed){
+        mobileMenu.classList.add("inactive");
+    }
+    productDetail.classList.toggle("inactive");
+    
 })
