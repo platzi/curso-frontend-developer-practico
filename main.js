336c7311cabe726dfef25f69379715aefa032df9
higi <cdramirezh@unal.edu.co>
2023-04-17 09:40:12 -0500

    22 Lista de productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const mobileMenu = document.querySelector('.mobile-menu')
 const burgerMenu = document.querySelector('.menu')
 const shoppingCart = document.querySelector('.navbar-shopping-cart')
 const productDetail = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
 const toggleElement = (e) => {
 	const elements = [mobileMenu, productDetail, desktopMenu]
@@ -16,6 +17,54 @@ const toggleElement = (e) => {
 	}
 }
 
+const productList = []
+productTemplate = {
+	img_source: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+	price: 120,
+	name: 'Bike',
+}
+
+const renderProducts = () => {
+	for (product of productList) {
+
+		const productCard = document.createElement('div')
+		productCard.classList.add('product-card')
+	
+		const img = document.createElement('img')
+		img.setAttribute('src', product.img_source)
+	
+		const productInfo = document.createElement('div')
+		productInfo.classList.add('product-info')
+	
+		const price = document.createElement('p')
+		price.innerText = `$ ${product.price}`
+		
+		const name = document.createElement('div')
+		name.innerText = product.name
+		
+		const productInfoDiv = document.createElement('div')
+		productInfoDiv.appendChild(price)
+		productInfoDiv.appendChild(name)
+		
+		const img2 = document.createElement('img')
+		img2.setAttribute('src', './icons/bt_add_to_cart.svg')
+		img2.setAttribute('alt', '')
+		const fig = document.createElement('figure')
+		fig.appendChild(img2)
+	
+		productInfo.appendChild(productInfoDiv)
+		productInfo.appendChild(fig)
+	
+		productCard.appendChild(img)
+		productCard.appendChild(productInfo)
+	
+		cardsContainer.appendChild(productCard)
+	}
+}
+
+for (i of Array(9)) { productList.push(productTemplate) }
+
 menuEmail.addEventListener('click', toggleElement(desktopMenu))
 burgerMenu.addEventListener('click', toggleElement(mobileMenu))
 shoppingCart.addEventListener('click', toggleElement(productDetail))
+renderProducts()
