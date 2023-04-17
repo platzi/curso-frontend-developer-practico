e6ead317bae78a945994490a09a985063b49ee44
LKlober <klobermdq@gmail.com>
2023-04-17 11:21:26 -0300

    add fix in menu mobile

--- aca/main.js
+++ aca/main.js
@@ -2,11 +2,13 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menuIcon');
 const mobileMenu = document.querySelector('.mobile-menu');
-
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', togglemobileMenu);
+shoppingCart.addEventListener('click', toggleproductDetail);
 
 
 function toggleDesktopMenu(){
@@ -14,4 +16,7 @@ function toggleDesktopMenu(){
 }
 function togglemobileMenu(){
     mobileMenu.classList.toggle('inactive');
+}
+function toggleproductDetail(){
+    productDetail.classList.toggle('inactive');
 }
