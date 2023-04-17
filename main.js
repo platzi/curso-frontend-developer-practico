9b632427de0db98a35b23ad7fdd5d27ba69021ef
wendy Arcia <wendy_342@hotmail.com>
2023-04-16 22:36:33 -0300

    Interaction among all components

--- main.js
+++ main.js
@@ -3,19 +3,18 @@ const desktopMenu = document.getElementsByClassName("desktop-menu")[0];
 const hamburgerMenuIcon = document.getElementsByClassName("menu")[0];
 const mobileMenu = document.getElementsByClassName("mobile-menu")[0];
 const cartMenuIcon = document.getElementsByClassName("navbar-shopping-cart")[0];
-const cartMenu = document.getElementsByClassName("shoppingCart")[0];
+const cartMenu = document.getElementsByClassName("shoppingCartContainer")[0];
 const cardsContainer = document.getElementsByClassName("cards-container")[0];
-const productDetail = document.getElementsByClassName("product-detail")[0];
-const productDetails = document.getElementsByClassName("product-card")[0];
-const iconProductDetailClosed = document.getElementsByClassName(
-  "product-detail-closed"
+const productDetailContainer =
+  document.getElementsByClassName("product-detail")[0];
+const productDetailsClosedIcon = document.getElementsByClassName(
+  "product-detail-close"
 )[0];
 
 navEmail.addEventListener("click", toggleDesktopNav);
 hamburgerMenuIcon.addEventListener("click", toggleMobileMenu);
 cartMenuIcon.addEventListener("click", toggleCartMenu);
-
-// iconProductDetailClosed.addEventListener("click", closedIconProductDetail);
+productDetailsClosedIcon.addEventListener("click", closedProductDetails);
 
 /* Creo el evento para invocar a la función renderProductList cuando se 
 cargue el DOM y cuando invoco   renderProductList le paso como argumento el array productList;
@@ -26,23 +25,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function toggleDesktopNav() {
   const isCartMenuClosed = cartMenu.classList.contains("inactive");
-  const isproductDetailsClosed = productDetail.classList.contains("inactive");
+  const isproductDetailsClosed =
+    productDetailContainer.classList.contains("inactive");
+  const isProductDetailClosed =
+    productDetailContainer.classList.contains("inactive");
   if (!isCartMenuClosed) {
     cartMenu.classList.add("inactive");
   } else if (!isproductDetailsClosed) {
-    productDetail.classList.add("inactive");
+    productDetailContainer.classList.add("inactive");
+  } else if (!isProductDetailClosed) {
+    productDetailContainer.classList.add("inactive");
   }
-
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
   const isCartMenuClosed = cartMenu.classList.contains("inactive");
-  const isproductDetailsClosed = productDetail.classList.contains("inactive");
+  const isproductDetailsClosed =
+    productDetailContainer.classList.contains("inactive");
+  const isproductDetailClosed =
+    productDetailContainer.classList.contains("inactive");
   if (!isCartMenuClosed) {
     cartMenu.classList.add("inactive");
   } else if (!isproductDetailsClosed) {
-    productDetail.classList.add("inactive");
+    productDetailContainer.classList.add("inactive");
+  } else if (!isproductDetailClosed) {
+    productDetailContainer.classList.add("inactive");
   }
 
   mobileMenu.classList.toggle("inactive");
@@ -51,15 +59,28 @@ function toggleMobileMenu() {
 function toggleCartMenu() {
   const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
   const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
-  const isproductDetailsClosed = productDetail.classList.contains("inactive");
+  const isproductDetailClosed =
+    productDetailContainer.classList.contains("inactive");
   !isMobileMenuClosed
     ? mobileMenu.classList.add("inactive")
     : !isDesktopMenuClosed
     ? desktopMenu.classList.add("inactive")
-    : !isproductDetailsClosed
-    ? productDetail.classList.add("inactive")
+    : !isproductDetailClosed
+    ? productDetailContainer.classList.add("inactive")
     : null;
   cartMenu.classList.toggle("inactive");
+}
+
+function openProductDetail() {
+  cartMenu.classList.add("inactive");
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
+
+  productDetailContainer.classList.remove("inactive");
+}
+
+function closedProductDetails() {
+  productDetailContainer.classList.add("inactive");
 }
 
 const productList = [];
@@ -120,6 +141,7 @@ function renderProductList(arr) {
 
     const img = document.createElement("img");
     img.setAttribute("src", product.image);
+    img.addEventListener("click", openProductDetail);
 
     const productInfo = document.createElement("div");
     productInfo.classList.add("product-info");
