32b32cea301071d7d9ca3bb029c829a9df8c7c1d
LKlober <klobermdq@gmail.com>
2023-04-17 09:16:12 -0300

    new day

--- aca/main.js
+++ aca/main.js
@@ -1,7 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const movileMenu = document.querySelector('.movile-menu');
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', togglebuguerMenu);
+
+
+function toggle(){
+    desktopMenu.classList.toggle('inactive');
+}
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
