6d4bc1362f765b39c0dd5f4e6e86c0791a73ac64
Lucio Peñaloza <penalozalucio@hotmail.com>
2023-04-17 21:17:40 -0600

    Menu desktop, menu mobile y carrito

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCartMenu = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobilepMenu);
+shoppingCartIcon.addEventListener('click', toggleCartMenu);
 
 function toggleDesktopMenu() {
 	desktopMenu.classList.toggle('inactive');
+	shoppingCartMenu.classList.add('inactive');
 }
 
 function toggleMobilepMenu() {
 	mobileMenu.classList.toggle('inactive');
+	shoppingCartMenu.classList.add('inactive');
+}
+
+function toggleCartMenu() {
+	shoppingCartMenu.classList.toggle('inactive');
+	mobileMenu.classList.add('inactive');
+	desktopMenu.classList.add('inactive');
 }
