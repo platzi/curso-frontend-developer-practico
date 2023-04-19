a77deb447ca1bd465cd69bd5fc346dcd82b5822e
EvilAFAS <evilafas99@gmail.com>
2023-04-19 09:27:40 -0500

    funcionalidad terminada

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const email = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const bntMenuMobile = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const productDetail = document.querySelector(".product-content");
+const productContent= document.querySelector(".product-content");
 const navbarShopingCart = document.querySelector(".navbar-shopping-cart");
-
+const productDetail = document.querySelector(".product-detail");
+const productDetailClose = document.querySelector(".product-detail-close");
 email.addEventListener('click', () => {
-    const cartMenuClosed = productDetail.classList.contains("inactive");
+    const cartMenuClosed = productContent.classList.contains("inactive");
     if (!cartMenuClosed) {
-        productDetail.classList.add("inactive")
+        productContent.classList.add("inactive")
     }
     desktopMenu.classList.toggle("inactive");
 
 })
 
 bntMenuMobile.addEventListener("click", () => {
-    const cartMenuClosed = productDetail.classList.contains("inactive");
+    const productDetailClosed = productDetail.classList.contains("inactive");
+    if(!productDetailClosed){
+        productDetail.classList.add("inactive");
+    }
+    const cartMenuClosed = productContent.classList.contains("inactive");
     if (!cartMenuClosed) {
-        productDetail.classList.add("inactive")
+        productContent.classList.add("inactive")
     }
     mobileMenu.classList.toggle("inactive");
 })
@@ -26,14 +31,25 @@ navbarShopingCart.addEventListener("click", () => {
 
     const mobileMenuClosed = mobileMenu.classList.contains("inactive");
     const emailClosed = desktopMenu.classList.contains("inactive");
+    
     if (!emailClosed) {
         desktopMenu.classList.add("inactive");
     }
     if (!mobileMenuClosed) {
         mobileMenu.classList.add("inactive");
     }
-    productDetail.classList.toggle("inactive");
 
+    const isProductDetailClosed = productDetail.classList.contains("inactive");
+    
+    if(!isProductDetailClosed){
+        productDetail.classList.add("inactive");
+    }
+    productContent.classList.toggle("inactive");
+
+})
+
+productDetailClose.addEventListener("click", ()=>{
+    productDetail.classList.add("inactive");
 })
 
 const productList = [];
@@ -55,16 +71,7 @@ productList.push({
     img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 })
 
-productList.push({
-    name: "Phone",
-    price: 990,
-    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
-productList.push({
-    name: "Phone",
-    price: 990,
-    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
+
 
 
 function renderProducts(arr){
@@ -74,6 +81,12 @@ productList.forEach((productos) => {
 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', productos.img);
+    
+    productImg.addEventListener("click",()=>{
+        productContent.classList.add("inactive");
+        productDetail.classList.remove("inactive");
+
+    })
 
     const productInfo = document.createElement('div');
     productInfo.classList.add("product-info");
