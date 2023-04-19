f78e7805884fe500ab8eaa8893bed827a308466d
Julian Guacaneme <julianguacaneme@gmail.com>
2023-04-19 10:10:01 +0200

    interaccion entre todos los componentes

--- main.js
+++ main.js
@@ -1,51 +1,70 @@
 const menuEmail = document.querySelector(".navbar-email");
 const menuHamIcon = document.querySelector(".menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const productDetailCloseIcon = document.querySelector(".product-detail-close");
+
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const shoppingCartContainer = document.querySelector("#shopping-cart-container");
-const cardContainer = document.querySelector('.cards-container')
+const shoppingCartContainer = document.querySelector(
+  "#shopping-cart-container"
+);
+const productDetailContainer = document.querySelector("#product-detail");
+const cardContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggCarritoAside);
-
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside);
 
 function toggleDesktopMenu() {
-
   const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
-   if (!isAsideClosed) {
-     shoppingCartContainer.classList.add("inactive");
-   }
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
   const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
   if (!isAsideClosed) {
-   shoppingCartContainer.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
-
+  closeProductDetailAside();
   mobileMenu.classList.toggle("inactive");
 }
 
-function toggCarritoAside (){
-  const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+function toggCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
 
   if (!isMobileMenuClosed) {
-  mobileMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+  }
+
+  const isProductDetailClosed =
+    productDetailContainer.classList.contains("inactive");
+
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add("inactive");
+  }
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
-shoppingCartContainer.classList.toggle("inactive");
-  
+function openProductDetailAside() {
+  shoppingCartContainer.classList.add("inactive");
+  productDetailContainer.classList.remove("inactive");
+}
+
+function closeProductDetailAside() {
+  productDetailContainer.classList.add("inactive");
 }
 
 const productList = [];
 productList.push({
-  name: 'Bike',
-  price: '120',
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
+  name: "Bike",
+  price: "120",
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
 productList.push({
   name: "Monitor",
   price: "280",
@@ -59,13 +78,14 @@ productList.push({
     "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 
-function renderProducts(arr){
+function renderProducts(arr) {
   for (product of productList) {
     const productCard = document.createElement("div");
     productCard.classList.add("product-card");
 
     const productImg = document.createElement("img");
     productImg.setAttribute("src", product.image);
+    productImg.addEventListener("click", openProductDetailAside);
 
     const productInfo = document.createElement("div");
     productInfo.classList.add("product-info");
@@ -93,4 +113,4 @@ function renderProducts(arr){
   }
 }
 
-renderProducts(productList)
+renderProducts(productList);
