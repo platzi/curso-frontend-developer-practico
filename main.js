18f446c81cb9644bd06723da5029b23c4ba05116
Robert <robertmdinca@proton.me>
2023-04-19 18:25:29 +0200

    Añadir menus desplegables y mediaqueries correspondientes

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBurger = document.querySelector('.menu-burger');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurger.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive'); 
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive'); 
 }
