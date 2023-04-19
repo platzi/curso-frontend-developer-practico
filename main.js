ff58d1ff50e4d07a94e927e4614b0d618b36f46e
th3Ryo <fabian_b.g@hotmail.com>
2023-04-19 00:52:43 -0500

    tercera clase, menu oculto icono

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-const menuEmail = document.querySelector('.navbar-email')
-const deskopMenu = document.querySelector('.desktop-menu')
+const menuEmail = document.querySelector('.navbar-email') //donde debo dar click
+const deskopMenu = document.querySelector('.desktop-menu') // donde quiero que cambie con el click
+const menuImg = document.querySelector('.menu')
+const movileMenu = document.querySelector ('.mobile-menu')
 
 menuEmail.addEventListener ('click',toggleDeskotMenu);
+menuImg.addEventListener ('click',toggleMobileMenu);
+
+/* otra forma de capturar datos
+
+email.addEventListener("click", () => toggleElement(desktopMenu));
+
+menu.addEventListener("click", () => toggleElement(mobileMenu));
+
+const toggleElement = (e) => {
+  e.classList.toggle("inactive");
+};
+ */
 
 function toggleDeskotMenu () {
     console.log("click");
     deskopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu () {
+    console.log("click");
+    movileMenu.classList.toggle('inactive');
+}
+
 console.log("sin js");
