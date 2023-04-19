06b8d191b323d49221280b2112e545f8662a10e6
keryum <alejandro_suarezp@outlook.com>
2023-04-18 22:42:32 -0500

    Fusión my shopping cart y navbar además de arreglar la superposición de los menús

--- main.js
+++ main.js
@@ -2,16 +2,40 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const iconMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+iconMenu.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
 }
 
-
-iconMenu.addEventListener('click', toggleMobileMenu);
-
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive')
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+        aside.classList.toggle('inactive'); 
+    }else if(!isDesktopMenuClosed); {
+        desktopMenu.classList.add('inactive');
+        }
+        aside.classList.toggle('inactive');
 }
