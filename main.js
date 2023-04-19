fe5a82b4c3ace39cea64d6d49e3909faab9360f3
keryum <alejandro_suarezp@outlook.com>
2023-04-18 19:18:01 -0500

    Fusión navbar con mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+
+iconMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
