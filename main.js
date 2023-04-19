428b9b5d616abe1bf206f20231effaa137f76cbf
MolinAnimation <molinanimation@gmail.com>
2023-04-19 17:12:19 -0500

    Clase 20 aside

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu")
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
 menuHamIcon.addEventListener("click", toggleMobileMenu)
+menuCarritoIcon.addEventListener("click", togggleCarritoAside)
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu(){//prender y apagar menu correo
     desktopMenu.classList.toggle("inactive")
     
+    if(!aside.classList.contains("inactive")){//apagar carrito menu
+        aside.classList.toggle("inactive")
+    }
 }
 
-function toggleMobileMenu(){
+function toggleMobileMenu(){//prender y apagar menu mobile
     mobileMenu.classList.toggle("inactive")
+
+    if(!aside.classList.contains("inactive")){//apagar carrito menu
+        aside.classList.toggle("inactive")
+    }
 }
+
+function togggleCarritoAside(){//prender y apagar carrito
+    aside.classList.toggle("inactive")
+
+    if(!mobileMenu.classList.contains("inactive")){//apagar mobile menu 
+        mobileMenu.classList.toggle("inactive")
+    }
+
+    if(!desktopMenu.classList.contains("inactive")){//apagar menu correo
+        desktopMenu.classList.toggle("inactive")
+        console.log("cierre");
+    }
+}
+
