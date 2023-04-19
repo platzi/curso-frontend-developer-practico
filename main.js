08d41a12833b80df7bb764c386e9d01a7a441a51
nikogoio <nico.94lta@gmail.com>
2023-04-19 16:56:23 -0400

    Integracion de menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector ('.navbar-email');
 const desktopMenu = document.querySelector ('.desktop-menu');
+const categoriesMenu = document.querySelector ('.menu');
+const categories = document.querySelector ('.mobile-menu')
 
 menuEmail.addEventListener ('click', toggleDeskMenu);
+categoriesMenu.addEventListener ('click', toggleCatMenu)
 
 function toggleDeskMenu () {
    desktopMenu.classList.toggle('inactive')
 }
+
+function toggleCatMenu () {
+   categories.classList.toggle('inactive')
+}
+
