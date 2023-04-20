ea84d9b72f350efbfdf55639a033564990b043d9
Tzuriel López <tzuriel.lm@hotmail.com>
2023-04-20 17:13:06 -0500

    Product Details

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ const menuIcon = document.querySelector('.menu')
 const menuMobile = document.querySelector('.mobile-menu')
 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu () {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     } 
 
     desktopMenu.classList.toggle('inactive')
@@ -24,10 +24,10 @@ function toggleDesktopMenu () {
 menuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu () {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
 if (!isAsideClosed) {
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
 }
 
     menuMobile.classList.toggle('inactive')
@@ -43,7 +43,7 @@ if (!isMobileMenuClosed) {
     menuMobile.classList.add('inactive')
 } else {desktopMenu.classList.add('inactive')}
 
-aside.classList.toggle('inactive');
+shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
