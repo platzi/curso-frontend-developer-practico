5e131dc83494a8555c119b33e92321322c56853c
williamAVM <william18_z95@hotmail.com>
2023-04-20 19:11:17 -0400

    add a function so that when you click on any position on the page, all menus will be hidden

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-
-navbarEmail.addEventListener('click', toggleDesktopMenu);
-
-function toggleDesktopMenu(){
-
-    desktopMenu.classList.toggle("inactive");
-    productDetail.classList.add("inactive");
-
-}
-
-
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-menuHamIcon.addEventListener("click", toggleMobileMenu);
-
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle("inactive");
-    productDetail.classList.add("inactive");
-}
-
 const shoppingCart = document.querySelector(".navbar-shopping-cart");
 const productDetail = document.querySelector(".product-detail");
 
-shoppingCart.addEventListener("click", toggleProductDetail);
+navbarEmail.addEventListener("click", toggleDesktopMenu);
 
-function toggleProductDetail(){
-    productDetail.classList.toggle("inactive");
-    mobileMenu.classList.add("inactive");
-    desktopMenu.classList.add("inactive");
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle("inactive");
+  productDetail.classList.add("inactive");
 }
 
-// agregando funcionlidad para el activado/descativado de todos los menus cuando se selecciona uno 
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+  productDetail.classList.add("inactive");
+}
+
+shoppingCart.addEventListener("click", toggleProductDetail);
+
+function toggleProductDetail() {
+  productDetail.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
+}
+
+document.addEventListener("mousedown", function (event) {
+  if (event.target !== navbarEmail && !desktopMenu.contains(event.target)) {
+    desktopMenu.classList.add("inactive");
+  }
+  
+  if (event.target !== menuHamIcon && !mobileMenu.contains(event.target)) {
+    mobileMenu.classList.add("inactive");
+  }
+
+   if (event.target !== shoppingCart && !productDetail.contains(event.target)) {
+     productDetail.classList.add("inactive");
+   }
+});
