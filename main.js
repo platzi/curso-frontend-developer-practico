aec4272c86f0865d99773cde5d090172f2f502e4
williamAVM <william18_z95@hotmail.com>
2023-04-20 17:41:07 -0400

    class twenty one complete

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
-
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
