b7d728c9b1f6806d3b406a629e17bbad7aa509e2
williamAVM <william18_z95@hotmail.com>
2023-04-20 01:01:35 -0400

    Class twety complete, the previos commit is the nineten class

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ const desktopMenu = document.querySelector(".desktop-menu");
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 
-function toggleDesktopMenu(event){
+function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
