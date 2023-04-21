ff434326c9fe794ddfe3e90ddc3dd009ff377a46
Alberto Chacon <albertojosechacon@gmail.com>
2023-04-20 22:23:59 -0500

    .gitignore modificado

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 // ------------------------Variables-----------------------------
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuHamIcon = document.querySelector('.menu');
-const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
-const aside=document.querySelector('.aside-order')
-//------------------variables para cierres y aperturas de aside y mobile-menu---
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside=document.querySelector('.aside-order');
+
+
+
+
 
 // -------------------Llamando Eventos-------------------------------
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -49,3 +54,21 @@ function toggleCarritoAside(){
   }
   
   aside.classList.toggle('inactive');}
+
+
+  const productlist=[];
+  productlist.push({
+    name:'Bike',
+    price: 120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  })
+  productlist.push({
+    name:'telefono',
+    price: 200,
+    image:'https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2023/04/Pixel-Fold-scaled.jpg?resize=1500%2C844&quality=50&strip=all&ssl=1'
+  })
+  productlist.push({
+    name:'Pantalla',
+    price: 220,
+    image:'https://www.mayoristatecnologico.com.co/images/companies/7/UN58TU6900KXZL%201.jpg?1669070456334'
+  })
