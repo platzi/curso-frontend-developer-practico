80327944be396400fa253655c878ad4725543ea1
David Mauricio Grajales Duran <davidgrajales111@gmail.com>
2023-04-21 21:06:47 -0500

    En este commit agrege el HTML y CSS del archivo clase8.html al archivo index.html (donde quedara todo el proyecto). Agrege un media queries al CSS. Por ultimo agrege JS al archivo main.js para hacer interactivo todo el contido que se uso de clase8.html

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 let menuEmail = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
+let menuHamburgerIcon = document.querySelector(".menu");
+let mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamburgerIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
