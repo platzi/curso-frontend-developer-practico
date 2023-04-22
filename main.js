acef6ecc1d4ed183b03eb4742ed23a5c668fa2b3
Lucas Sebastian Cabrera <lucasebastian89@MacBook-Air-de-Lucas.local>
2023-04-22 17:34:45 -0300

    Fusion Navbar-Menu-Mobile

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const desktopMenu = document.querySelector('.desktop-menu');
 const navbarEmail = document.querySelector('.navbar-email');
+const iconBurguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+iconBurguerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
