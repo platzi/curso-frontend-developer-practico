95ce174bbe16a318f39f9994d3db96deb35c25fb
David Mauricio Grajales Duran <davidgrajales111@gmail.com>
2023-04-22 15:08:44 -0500

    Esta clase estuvo de locos, el nivel de lore que se tiro fue magnifico. Lo que hicimos fue agregar el contenido de clase13.html al index.html, obvio hacer JS y un porquito de CSS.       En lo que respecta HTML agregamos el HTML de clase13.html a index.html por fuera de la etiqueta nav.  Lo unico que se hizo due agregar la clase inactive a la etiqueta aside.       En CSS copiamos todo el CSS de clase13.html y lo pusimos en styles.css. A las clases product-detail y mobile-menu les pusimos un background color var (—white). Y en l aparte de media queries pusimos el media queries de clase13.html en el media queries de max-width: 640px de styles.css.       En la parte de JavaScript fue la mas difícil (pero se logra con esfuerzo). Al igual que en las 2 ultimas clases hicimos que el aside aparezca y desaparezca cuando el usuario le da CLICK. Lo que fue nuevo es que tuvimos que hacer que cuando aparezcan unas etiquetas desaparezcan otras que ya esten abiertas para que no se vean las 2 al mismo tiempo (Porque se ve feo y mal echo).

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 let menuEmail = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
 let menuHamburgerIcon = document.querySelector(".menu");
+let menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 let mobileMenu = document.querySelector(".mobile-menu");
+let aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamburgerIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    let isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+    let isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+    let isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
 }
