0de2c4234db6bac9c713b6a871015f051f38282a
Edgrreach <edgrreach@yahoo.com>
2023-04-22 20:45:40 -0600

    SE AGREGA MENU BURGUER

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const burguerMenu = document.querySelector('.menu')
+const mobileMenu  = document.querySelector('.mobile-menu')
+
 navEmail.addEventListener('click', toggleDesktopMenu)
+burguerMenu.addEventListener('click', toggleBurguerMenu)
 
 function toggleDesktopMenu() {
     console.log('es un click')
     desktopMenu.classList.toggle('inactive')
+    
+}
+
+function toggleBurguerMenu() {
+   
+    mobileMenu.classList.toggle('inactive')
     
 }
