1225367a18b0ef931728ed734ca8898eee9d66bc
Alvaro Menacho <alvaromenachojs@gmail.com>
2023-04-24 11:29:14 -0500

    estoy agregando la lista de productos con js

--- main.js
+++ main.js
@@ -64,3 +64,62 @@ function toggleMenu() {
 
 
 }
+
+var productList = [];
+productList.push({
+    name:'bicicleta',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>
+*/
+for(product of productList){
+ // Maquetacion de carta del producto (elemento html)
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImage = document.createElement("img");
+    productImage.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+    
+    const productInfoDiv = document.createElement("div");
+    
+    const productName = document.createElement("p");
+    productName.innerText(product.name);
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText(product.price);
+
+    const productFigure = document.createElement("figure");
+    
+    const productFigureImage = document.createElement("img");
+    productFigureImage.setAttribute("src", "./icons/bt_add_to_cart.svg")
+
+
+    //Colocacion dentro de sus clases
+
+    productFigure.appendChild(productFigureImage);
+    productInfoDiv.appendChild(productName);
+    productInfoDiv.appendChild(productPrice);
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productFigure);
+
+    
+
+
+
+}
