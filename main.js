ac6ef8695e10818e985382376c3f7577f9c2c9c8
Zaprovic <jazaro2001@gmail.com>
2023-04-25 12:04:36 -0500

    changes added to feature branch

--- main.js
+++ main.js
@@ -5,43 +5,76 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const cartMenu = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector("#shopping-cart");
 const cardsContainer = document.querySelector(".cards-container");
+const asideDetail = document.querySelector("#product-detail");
+const productDetailClose = document.querySelector(".product-detail-close");
+const product = document.querySelector(".product-card");
 
 email.addEventListener("click", toggleDesktopMenu);
 burguerMenu.addEventListener("click", toggleMobileMenu);
 cartMenu.addEventListener("click", toggleCartAside);
+productDetailClose.addEventListener("click", removeDetail);
+desktopMenu.addEventListener("click", removeDetail);
 
 function toggleDesktopMenu() {
 	const isAsideClosed = aside.classList.contains("inactive");
-
+	const isProductDetailClosed = asideDetail.classList.contains("inactive");
+	
 	if (!isAsideClosed) {
 		aside.classList.add("inactive");
+	} else if (!isProductDetailClosed) {
+		asideDetail.classList.add("inactive");
 	}
-
+	
 	desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
 	const isAsideClosed = aside.classList.contains("inactive");
-
+	
 	if (!isAsideClosed) {
 		aside.classList.add("inactive");
 	}
-
+	
+	removeDetail();
 	mobileMenu.classList.toggle("inactive");
 }
 
 function toggleCartAside() {
 	const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
 	const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
-
+	const isProductDetailClosed =
+	productDetailClose.classList.contains("inactive");
+	
 	if (!isMobileMenuClosed) {
 		mobileMenu.classList.add("inactive");
 	} else if (!isDesktopMenuClosed) {
 		desktopMenu.classList.add("inactive");
+	} else if (!isProductDetailClosed) {
+		asideDetail.classList.add("inactive");
 	}
 	aside.classList.toggle("inactive");
 }
 
+function openAsideDetail() {
+	aside.classList.add("inactive");
+	asideDetail.classList.toggle("inactive"); // toggle or remove works fine
+}
+
+function removeDetail() {
+	asideDetail.classList.add("inactive");
+}
+
+function toggleDetailProduct() {
+	const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+	
+	if (!isDesktopMenuClosed) {
+		desktopMenu.classList.add("inactive");
+	}
+}
+
+function removeDesktop() {
+	desktopMenu.classList.add("inactive");
+}
 
 const productList = [];
 productList.push({
@@ -55,7 +88,7 @@ productList.push({
 	name: "Computer",
 	price: 850,
 	image:
-		"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		"https://images.pexels.com/photos/930530/pexels-photo-930530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 });
 
 productList.push({
@@ -94,6 +127,7 @@ function renderProduct(arr) {
 
 		const img = document.createElement("img");
 		img.setAttribute("src", i.image);
+		img.addEventListener("click", openAsideDetail); //////
 
 		const productInfo = document.createElement("div");
 		productInfo.classList.add("product-info");
