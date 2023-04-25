737b2607c70d3226149710a347e18dd1c8070891
StarDev44 <dylanestrella2@gmail.com>
2023-04-25 01:37:57 -0500

    Adding JS to Menu

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const desktopMenu = document.getElementById("desktopMenu");
+const navbarRight = document.getElementById("navbarRight");
+
+navbarRight.addEventListener("click",()=>{
+    desktopMenu.classList.toggle("display-none");
+
+})
