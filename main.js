6e4f7fd3b19a9ac55711dfca12504f34aa8f4f44
Jhon Duarte <jhonny.jdt18@gmail.com>
2023-04-25 21:19:09 +0200

    Fusion del menu en mobil

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurger = document.querySelector('.menu');
+const mobilMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuBurger.addEventListener('click',toggleMobilMenu);
+
+
 
 function toggleDesktopMenu(){
-
     desktopMenu.classList.toggle('inactive');
+}
 
+function toggleMobilMenu(){
+    mobilMenu.classList.toggle('inactive');
 }
