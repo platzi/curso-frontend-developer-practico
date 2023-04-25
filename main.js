69aa05884f40df2e8dfff3cd2cb9a6d39aa22432
Eduardo Razo <esse_rios_e@hotmail.com>
2023-04-25 07:06:29 -0600

    Fusión del menú en mobile 20/29

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 /* creamos variable apuntando al elemento */
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu );
+menuHamIcon.addEventListener('click', toggleMobileMenu );
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive'); /* toogle activa e inactiva una clase */
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive'); /* toogle activa e inactiva una clase */
 }
