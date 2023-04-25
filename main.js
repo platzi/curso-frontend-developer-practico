8566776093ca70f675ad20f4b84473abc05cb009
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-04-25 18:36:56 -0300

    se fusiono menu desktop con navbar email

---
+++ main.js
@@ -0,0 +1,15 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+
+function toogleDesktopMenu(){
+    console.log('click');
+    desktopMenu.classList.toggle('inactive');
+}
+
+
+menuEmail.addEventListener('click', toogleDesktopMenu);
+
+console.log('Js funciona')
+
+
