6a4f2cb44528c545e7a1c3bc17639669910214db
daresnlefepi <felipe6668.afgg@gmail.com>
2023-04-25 17:49:41 -0500

    Clase 22

--- main.js
+++ main.js
@@ -1,8 +1,103 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburguesa = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const navbarShopCar = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container'); /* todoesto para poder pegar los productos al html */
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click', toggleMibileMenu);
+navbarShopCar.addEventListener('click', toggleShopCard);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
     desktopMenu.classList.toggle('inactive');/* coloca y quita la clase */  
 }
+
+function toggleMibileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive'); 
+  }
+    mobileMenu.classList.toggle('inactive');/* Q y C Menu Mobile */
+}
+
+function toggleShopCard() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive'); 
+  } else if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add('inactive');
+  }
+    aside.classList.toggle('inactive');/* Qy C My Order */
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120.00,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Nintendo',
+    price: 299.00,
+    image: 'https://http2.mlstatic.com/D_NQ_NP_909032-MLA47920360780_102021-O.webp',
+});
+productList.push({
+    name: 'Portatil',
+    price: 1200.00,
+    image: 'https://falabella.scene7.com/is/image/FalabellaCO/35129542_8?wid=800&hei=800&qlt=70',
+});
+productList.push({
+    name: 'Optimus',
+    price: 100.00,
+    image: 'https://falabella.scene7.com/is/image/FalabellaCO/35129542_8?wid=800&hei=800&qlt=70',
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+      const productCard = document.createElement('div');
+      productCard.classList.add('product-card');
+    
+      // product= {name, price, image} -> product.image
+      const productImg = document.createElement('img');
+      productImg.setAttribute('src', product.image);
+    
+      const productInfo = document.createElement('div');
+      productInfo.classList.add('product-info');
+    
+      const productInfoDiv = document.createElement('div');
+    
+      const productPrice = document.createElement('p');
+      productPrice.innerText = '$' + product.price;
+      const productName = document.createElement('p');
+      productName.innerText = product.name;
+    
+      productInfoDiv.appendChild(productPrice);
+      productInfoDiv.appendChild(productName);
+    
+      const productInfoFigure = document.createElement('figure');
+      const productImgCart = document.createElement('img');
+      productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+      productInfoFigure.appendChild(productImgCart);
+    
+      productInfo.appendChild(productInfoDiv);
+      productInfo.appendChild(productInfoFigure);
+    
+      productCard.appendChild(productImg);
+      productCard.appendChild(productInfo);
+    
+      cardsContainer.appendChild(productCard);
+    }
+  }
+  
+  renderProducts(productList);
