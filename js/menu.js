1044b45771409a6ddc319d1324fd82a59e21c52c
StarDev44 <dylanestrella2@gmail.com>
2023-04-25 10:15:20 -0500

    Adding Mobile Menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,32 @@
 const desktopMenu = document.getElementById("desktopMenu");
 const navbarRight = document.getElementById("navbarRight");
+const menuBtn     = document.getElementById("menuBtn");
+const mobileMenu  = document.getElementById("mobileMenu");
 
 navbarRight.addEventListener("click",()=>{
     desktopMenu.classList.toggle("display-none");
 
+})
+
+menuBtn.addEventListener("click",()=>{
+    
+    mobileMenu.classList.toggle("menu-animation");
+    mobileMenu.classList.toggle("menu-animation-reverse");
+
+    console.log(mobileMenu.classList)
+
+    const state = mobileMenu.classList.contains('display-none');
+    
+    if(!state)
+    {
+        setTimeout(()=>{
+            mobileMenu.classList.toggle("display-none")
+        },300);
+        console.log("Si")
+    }
+    else
+    {
+        console.log("No")
+        mobileMenu.classList.toggle("display-none")
+    }
 })
