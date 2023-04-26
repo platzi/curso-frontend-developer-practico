2639e2c6339a957047ce183e1ef14a0bbad04f3d
CRKNO <cristiancanosantiago@hotmail.com>
2023-04-25 22:54:17 -0300

    mejoro la interacción cuando estamos en modo mobile o en desktop

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ const mobileMenuIcon = document.querySelector(".menu")
 
 mobileMenuIcon.addEventListener("click", ()=>{
     mobileMenu.classList.toggle("inactive");
+    desktopMenu.classList.add("inactive");
 })
 navEmail.addEventListener("click", ()=>{ //Uso una funcion de flecha para el evento ya que la funcion no se volvera a usar.
 
     desktopMenu.classList.toggle("inactive");
+    mobileMenu.classList.add("inactive");
 });
