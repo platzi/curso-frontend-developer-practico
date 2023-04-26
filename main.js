62b6d6dc96130d3f39bc3b5b8f4d4bc9a8a71af9
Jhon Duarte <jhonny.jdt18@gmail.com>
2023-04-26 16:35:18 +0200

    Arreglos al carrito de compras y al menu en mobil y desktop

--- main.js
+++ main.js
@@ -2,16 +2,44 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurger = document.querySelector('.menu');
 const mobilMenu = document.querySelector('.mobile-menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuBurger.addEventListener('click',toggleMobilMenu);
+menuCarrito.addEventListener('click',toggleCarritoAside);
 
 
 
 function toggleDesktopMenu(){
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobilMenu(){
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     mobilMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+
+    const isMobileMenuClosed = mobilMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobilMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
