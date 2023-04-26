4dc52b888f7d38cef797d1511ffd12853c68f0b8
CRKNO <cristiancanosantiago@hotmail.com>
2023-04-25 22:40:39 -0300

    doy funcionalidad al menu en version mobile desde main.js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
 
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuIcon = document.querySelector(".menu")
 
+mobileMenuIcon.addEventListener("click", ()=>{
+    mobileMenu.classList.toggle("inactive");
+})
 navEmail.addEventListener("click", ()=>{ //Uso una funcion de flecha para el evento ya que la funcion no se volvera a usar.
 
     desktopMenu.classList.toggle("inactive");
