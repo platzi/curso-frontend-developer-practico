bf6edc4f2263b0d88afd7f81ee94d8cb29ad53f0
Law <victorestrada2006@gmail.com>
2023-04-26 19:17:57 +0200

    Añadiendo un nuevo elemento al HTML y dandole interacción con Javsacript para dispositivos móviles. Se actualizaron los estilos del CSS y añadieron comentarios explicativos de las funcionalidades.

--- main.js
+++ main.js
@@ -3,7 +3,12 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+//Añadiendo los menús para dispositivos móviles
+const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 navEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 //Solución del profesor
 
@@ -23,4 +28,10 @@ function toggleDesktopMenu(){
     }else{
         desktopMenu.classList.remove('inactive');
     }
+}
+
+//Función que nos permite hacer que desaparezcan o aparezcan los menús.
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
