203cf645421eacebb843b41af297d5d74b4a014c
NikoGox <nico.94lta@gmail.com>
2023-04-25 22:48:11 -0400

    Revert "prueba"
    This reverts commit 0301440358625dbb4ec01d23d543a3a421b563fc.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector ('.desktop-menu');
 const categoriesMenuIcon = document.querySelector ('.menu');
 const carritoMenuIcon = document.querySelector ('.navbar-shopping-cart');
 const categories = document.querySelector ('.mobile-menu');
-const aside = document.querySelector ('.product-detail-cart');
+const aside = document.querySelector ('.product-detail');
 const cardContainer = document.querySelector ('.cards-container')
 
 menuEmail.addEventListener ('click', toggleUserMenu);
