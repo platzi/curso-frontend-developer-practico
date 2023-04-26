a3f2acc646ddd6a45cecf1b499b031afc0b531c9
Luis <luisandrino3@gmail.com>
2023-04-26 16:49:59 -0600

    añadidos menus

--- main.js
+++ main.js
@@ -1,13 +1,50 @@
 const user = document.querySelector(".navbar-email");
 const menu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu")
+const burguerMenu = document.querySelector(".menu");
+const market = document.querySelector(".navbar-shopping-cart");
+const productos = document.querySelector(".product-detail")
 
-user.addEventListener("click", menus);
+user.addEventListener("click", toggleDesktopMenu);
 
-function menus(){
 
-    if(menu.classList.contains("inactive")){
-        menu.classList.remove("inactive");
+function toggleDesktopMenu(){
+    let isProductsClosed = productos.classList.contains("inactive");
+
+    if(isProductsClosed){
+        menu.classList.toggle("inactive");
     } else {
-        menu.classList.add("inactive")
+        productos.classList.toggle("inactive");
+        menu.classList.toggle("inactive");
     }
+}
+
+burguerMenu.addEventListener("click" , toggleMobileMenu);
+
+function toggleMobileMenu() {
+    let isProductsClosed = productos.classList.contains("inactive");
+    if(isProductsClosed){
+        mobileMenu.classList.toggle("inactive");
+    } else {
+        productos.classList.toggle("inactive");        
+        mobileMenu.classList.toggle("inactive");
+    }
+}
+
+market.addEventListener("click", toggleMarket);
+
+function toggleMarket(){
+    let isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    let isDesktopClosde = menu.classList.contains("inactive");
+
+    if(isMobileMenuClosed || isDesktopClosde){
+        productos.classList.toggle("inactive");
+        
+    } else {
+        mobileMenu.classList.toggle("inactive");
+        productos.classList.toggle("inactive");
+        menu.classList.toggle("inactive");    
+    }
+
+    
 }
