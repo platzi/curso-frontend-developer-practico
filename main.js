c6bd22e930f293e6dc6907c11713bd54c4e0fe25
CRKNO <cristiancanosantiago@hotmail.com>
2023-04-26 22:53:54 -0300

    doy funcionalidad al icono del carrito y mejoro la interacción de los menus con este

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const navEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu")
+const desktopMenu = document.querySelector(".desktop-menu");
 
 const mobileMenu = document.querySelector(".mobile-menu");
-const mobileMenuIcon = document.querySelector(".menu")
+const mobileMenuIcon = document.querySelector(".menu");
+
+const shoppingCartAside = document.querySelector(".product-detail");
+const navShoppingCart = document.querySelector(".navbar-shopping-cart");
+
+navShoppingCart.addEventListener("click", ()=>{
+    shoppingCartAside.classList.toggle("inactive");
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+})
 
 mobileMenuIcon.addEventListener("click", ()=>{
     mobileMenu.classList.toggle("inactive");
     desktopMenu.classList.add("inactive");
+    shoppingCartAside.classList.add("inactive");
 })
-navEmail.addEventListener("click", ()=>{ //Uso una funcion de flecha para el evento ya que la funcion no se volvera a usar.
 
+navEmail.addEventListener("click", ()=>{ //Uso una funcion de flecha para el evento ya que la funcion no se volvera a usar.
+    
     desktopMenu.classList.toggle("inactive");
     mobileMenu.classList.add("inactive");
+    shoppingCartAside.classList.add("inactive");
 });
