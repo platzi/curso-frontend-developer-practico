fe950adc637ebfb5db35d1ac02a77a9ba3d17163
Law <victorestrada2006@gmail.com>
2023-04-27 15:47:29 +0200

    Añadiendo el carrito de compras y detalles de los productos. Además, también se añade la interacción con Javascript para evitar conflictos entre los diferentes menús.

--- main.js
+++ main.js
@@ -7,8 +7,16 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const burgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+//Añadiendo el carrito de compras y los detalles de los productos.
+
+const carrito = document.querySelector(".navbar-shopping-cart");
+const productDetails = document.querySelector(".product-detail");
+
 navEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+
+//Añadiendo el addEventListener al carrito para mandar a llamar a los detalles de los productos.
+carrito.addEventListener('click', toggleProductDetail);
 
 //Solución del profesor
 
@@ -28,10 +36,24 @@ function toggleDesktopMenu(){
     }else{
         desktopMenu.classList.remove('inactive');
     }
+
+    productDetails.classList.add('inactive')
 }
 
 //Función que nos permite hacer que desaparezcan o aparezcan los menús.
 
 function toggleMobileMenu(){
+    //Manda a agregar la clase "inactive" a los detalles de los productos para que desaparezcan cada vez que se abre el menú de móvil.
+    productDetails.classList.add("inactive")
     mobileMenu.classList.toggle('inactive');
+}
+
+//Función que nos permite que aparezcan o desaparezcan los detalles de los productos.
+
+function toggleProductDetail(){
+    productDetails.classList.toggle("inactive");
+
+    //Le añade la clase 'inactive' al menú de teléfono y al menú de pc cada vez que se abran los detalles de los productos.
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
 }
