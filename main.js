c03aa1c5768e989147bd41651e505adf4d5681fb
daresnlefepi <felipe6668.afgg@gmail.com>
2023-04-27 18:40:51 -0500

    Clase 25 - Fucionando la sincronizacion de todos las ventanas emergentes

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const navbarShopCar = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container'); /* todoesto para poder pegar los productos al html */
+const productDetailContainer = document.querySelector('#productDetail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguesa.addEventListener('click', toggleMibileMenu);
 navbarShopCar.addEventListener('click', toggleShopCard);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
-function toggleDesktopMenu() {
+function toggleDesktopMenu() {/* toggle abre y cierra */
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive');
-  }
+  } else if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive'); 
+  } 
     desktopMenu.classList.toggle('inactive');/* coloca y quita la clase */  
 }
 
 function toggleMibileMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive'); 
-  }
+  } else if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive'); 
+  } 
     mobileMenu.classList.toggle('inactive');/* Q y C Menu Mobile */
 }
 
@@ -37,29 +47,45 @@ function toggleShopCard() {
   } else if (!isDesktopMenuClosed) {
     desktopMenu.classList.add('inactive');
   }
-  shoppingCartContainer.classList.toggle('inactive');/* Qy C My Order */
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive'); 
+  } 
+    shoppingCartContainer.classList.toggle('inactive');/* Qy C My Order */
+}
+
+function openProductDetailAside() {
+  shoppingCartContainer.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
 productList.push({
-    name: 'Bike',
+    name: 'Resident Evil 4',
     price: 120.00,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://image.api.playstation.com/vulcan/ap/rnd/202210/0712/cWZlv5HCWi4sGKuwVRO4c8Xg.png',
 });
 productList.push({
-    name: 'Nintendo',
+    name: 'Mario y Luigi',
     price: 299.00,
-    image: 'https://http2.mlstatic.com/D_NQ_NP_909032-MLA47920360780_102021-O.webp',
+    image: 'https://www.egames.news/__export/1676300180942/sites/debate/img/2023/02/13/super-mario-bros-movie-comercial-2.jpg_1902800913.jpg',
 });
 productList.push({
-    name: 'Portatil',
-    price: 1200.00,
-    image: 'https://falabella.scene7.com/is/image/FalabellaCO/35129542_8?wid=800&hei=800&qlt=70',
+    name: 'God of War 4',
+    price: 59.00,
+    image: 'https://media.vandal.net/i/1200x630/27407/god-of-war-201711112176_1.jpg',
 });
 productList.push({
-    name: 'Optimus',
+    name: 'Optimus Prime',
     price: 100.00,
-    image: 'https://falabella.scene7.com/is/image/FalabellaCO/35129542_8?wid=800&hei=800&qlt=70',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/94/Optimus_prime_bumblebee-1200x983.jpg',
 });
 
 function renderProducts(arr) {
@@ -70,6 +96,7 @@ function renderProducts(arr) {
       // product= {name, price, image} -> product.image
       const productImg = document.createElement('img');
       productImg.setAttribute('src', product.image);
+      productImg.addEventListener('click', openProductDetailAside);/* hace que si doy click en una imagen apresca su detalle en pantalla */
     
       const productInfo = document.createElement('div');
       productInfo.classList.add('product-info');
@@ -99,5 +126,4 @@ function renderProducts(arr) {
       cardsContainer.appendChild(productCard);
     }
   }
-  
   renderProducts(productList);
