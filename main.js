dd6b48353da224d61e8b64353732ae35312b17ff
RicoCardenas <julianalejandro132465798@gmail.com>
2023-04-27 17:48:00 -0500

    Hicimos que al tener la vista de mobile, al momento de presionar el menu hamburguesa aparezca el menumobile, y hicimos algunos cambio en css

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu= document.querySelector('.desktop-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuBurguer = document.querySelector('.menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuBurguer.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
