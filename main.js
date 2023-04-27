2602c11cc8d7d278bc028e63474f57ddc3d476d0
BreidysG <breidysgutierrez8@gmail.com>
2023-04-27 15:08:10 -0400

    Se fusiono el menu mobile

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ menuEmail.addEventListener("click", toggleDesktopMenu);
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle("inactive"); //Pone y quita la clase
 }
+
+const hamburguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburguerMenu.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+}
