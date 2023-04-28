d383d89e62dd0d75abeebed74ce5415972a40451
badr <bernardoantoniod@gmail.com>
2023-04-28 15:57:16 -0400

    se junto la pagina de productos pero se elimino del HTML y se hace desde JS

--- README.md
+++ README.md
@@ -42,3 +42,27 @@ El aside si lo queremos dejar por fuera del navbar (aunque no entiendo el criter
 El mediaqueri del aside se mete dentro de los anteriores poque hace lo mismo.
 
 Se hicieron condicionales y se utilizo .contains y .add para hacer que se cierre uno mientras se habre otro. Ojo, no es la mejor solición, en los comentarios hay una mejor solución, la cual voy a implementar la proxima clase.
+
+
+
+Clase 22/29
+Lista de productos: HTML a partir de arrays
+
+La lista de productos esta en Clase6.
+Se agrega como siempre el HTML y CSS. Se hacen los ajustes necesarios.
+
+Ahora se van a borrar todos los divs de product card menos uno. Para que con JS lo hagamos aparecer todas las veces que lo necesitemos. De hecho se va a dejar comentado este div y no quedara ninguno porque se va a hacer desde JS. Ojo, en la vide real estos divs se hacen en backend no como lo haremos aqui.
+Se crea una lista de productos vacia.
+Se le haran push de objetos (dicts) a esa lista vacia, con las caracteristicas que necesitan los productos.
+Despues de agregar varios productos se aplica un ciclo para que se agregen en el HTML. Dentro de este ciclo se maquetaran como estaba en el HTML y con los estilos de CSS para que vayan quedando igual. De esta manera:
+    * Crear un div que tenga la clase product-card.
+    * Ahora hay que crear la imagen. A la imagen hay que agregarle el src, peor no debe ser el link de la imagen sino la propiedad image que tienen los objetos.
+    *Despues se crea el siguiente div con clase product-info.
+    *Se sigue construyendo la misma maquetación completa.
+    *Ahora hay que devolverse, como asi? -- meter la imagen (cerrito) dentro del figure, meter los parrafos dentro del div y meter al figure y el div dentro del div product-info.
+    * Ahora quedaria insertar el product-info y la imagen dentro del div profuct-card.
+    * Ahora todavia queda insertar el productCart dentro del div cards-container. Para esto tengo que crear la variable cardsContainer fuera del ciclo for. Pero se le agrega el productCard dentro del ciclo.
+    * Funcccccccccciiiiiiiiiiiiiiiooooooooooonaaaaaaaaaaaaaaaaaaaaaaaaaa!
+Ahora con este codigo si se meten mas productList.push se acomodaran directamente en el HTML!!!
+Ahora lo que queda es meter ese ciclo for en una funcion para ordenar y poder utilizarlo mas eficientemente.
+En el futuro esto se puede conectar con el backend para traer los productos y solo hacer un llamado desde el JS usando API 
--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const menuFromEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-
 const menuBurgerIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
-
 const shoppingCartIcon = document.querySelector(".navbar-shopping-cart");
-const productDetailSummary = document.querySelector(".product-detail")
+const productDetailSummary = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
+
 
 menuFromEmail.addEventListener("click", toggleDesktopMenu);
 menuBurgerIcon.addEventListener("click", toggleMenuBurger);
 shoppingCartIcon.addEventListener("click", toggleSummaryFromShoppingCart);
-
 
 function toggleDesktopMenu() {
 
@@ -18,16 +17,88 @@ function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
 
 };
-
 function toggleMenuBurger() {
 
     productDetailSummary.classList.add("inactive");
     mobileMenu.classList.toggle("inactive");
 };
-
 function toggleSummaryFromShoppingCart() {
 
     mobileMenu.classList.add("inactive");
     desktopMenu.classList.add("inactive");
     productDetailSummary.classList.toggle("inactive");
 };
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "Helment",
+    price: 50,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "Gloves",
+    price: 20,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+// <!-- <div class="product-card">
+// <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+// <div class="product-info">
+//   <div>
+//     <p>$120,00</p>
+//     <p>Bike</p>
+//   </div>
+//   <figure>
+//     <img src="./icons/bt_add_to_cart.svg" alt="">
+//   </figure>
+// </div>
+// </div> -->
+
+function renderProducts (list) {
+
+    for (product of list) {
+
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.image);
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        // productInfo.appendChild(productInfoDiv)
+        // productInfo.appendChild(productInfoFigure)
+    
+        const productInfoDiv = document.createElement("div");
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement("figure");
+        const productInfoCart = document.createElement("img");
+        productInfoCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    
+        productInfoFigure.appendChild(productInfoCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productInfo);
+        productCard.appendChild(productImg);
+    
+        cardsContainer.appendChild(productCard);
+    
+    };
+};
+
+renderProducts(productList);
