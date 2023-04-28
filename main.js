53a1be689f8b0fd3c98c490e0674ac78d4ace0d3
RicoCardenas <julianalejandro132465798@gmail.com>
2023-04-28 17:07:22 -0500

    Hice que cuando abriese mi carrito de compras se cerrar el menu de hamburguesa y cuando abriera el destock se cerrar el carrito de comprar y viceversa

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
-const menuEmail = document.querySelector('.navbar-email')
-const desktopMenu= document.querySelector('.desktop-menu')
-const mobileMenu = document.querySelector('.mobile-menu')
-const menuBurguer = document.querySelector('.menu')
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu= document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBurguer = document.querySelector('.menu');
+const productDetail = document.querySelector('.product-detail');
+const carShopping = document.querySelector('.navbar-shopping-cart');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
-menuBurguer.addEventListener('click', toggleMobileMenu)
+carShopping.addEventListener('click', toggleProductDetail);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurguer.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+    const isProductDetailClosed = productDetail.classList.contains('inactive');
+    
+    if (!isProductDetailClosed) {
+        productDetail.classList.add('inactive')
+    }
+
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    const isProductDetailClosed = productDetail.classList.contains('inactive');
+
+    if (!isProductDetailClosed) {
+        productDetail.classList.add('inactive')
+    }
+    
+    mobileMenu.classList.toggle('inactive');
+
+}
+
+function toggleProductDetail(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    
+    productDetail.classList.toggle('inactive');
+
+    const isDestockMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isDestockMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
 }
