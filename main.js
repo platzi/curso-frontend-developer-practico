20224d621e461fa00fb9e1083adafa065448c354
Eduardo Razo <esse_rios_e@hotmail.com>
2023-04-28 22:12:19 -0600

    Lista de productos: HTML a partir de arrays 22/29

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu );
 menuHamIcon.addEventListener('click', toggleMobileMenu );
@@ -40,3 +41,64 @@ function toggleCartAside() {
  
 
 }
+
+const productList = [];
+productList.push({
+    name:'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name:'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name:'Computer',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+
+
+function renderProducts(arr) {
+    for (product of arr){
+        //crear cada elemento de html pero en js
+        const productCard = document.createElement('div');  
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');  
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div'); 
+    
+        const productPrice = document.createElement('p'); 
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name; 
+    
+        productInfoDiv.appendChild(productPrice);//anidar elemento
+        productInfoDiv.appendChild(productName);//anidar elemento
+    
+        const productInfoFigure = document.createElement('figure'); 
+        const productImgCart = document.createElement('img'); 
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart); //anidar elemento
+    
+        productInfo.appendChild(productInfoDiv);//anidar elemento
+        productInfo.appendChild(productInfoFigure);//anidar elemento
+    
+        productCard.appendChild(productImg);//anidar elemento
+        productCard.appendChild(productInfo);//anidar elemento
+        
+        cardsContainer.appendChild(productCard);//anidar elemento
+    
+        /* 18:57 22/29*/
+    }
+}
+
+renderProducts(productList);
