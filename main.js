5dc60520476c75af473987a9f537d8c0df256b11
a9leom <a9leom@gmail.com>
2023-04-29 17:49:18 -0500

    Adding the mobile menu and configuring media queries.

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+// Elementos HTML
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+// Escuchar eventos
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
+// Funciones tras los eventos
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+
 }
