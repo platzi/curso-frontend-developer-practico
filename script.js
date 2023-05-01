4c44ee9e336ec5ffd181a47b0f075eb44f0d43ff
AleisterFox <ericaguirrez@hotmail.com>
2023-04-30 21:11:50 -0600

    integrar el menu mobile y agregar media queries

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const $ = (selector) => document.querySelector(selector); // Simplifica crear las constantes de los elementos html
 const navbarEmail = $(".navbar-email");
 const desktopMenu = $(".desktop-menu");
+const hamMenu = $(".menu");
+const mobileMenu = $(".mobile-menu");
 
-navbarEmail.addEventListener('click', toggleDesktopMenu)
+navbarEmail.addEventListener('click', toggleDesktopMenu);
+hamMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     if (desktopMenu.classList.contains('inactive')) {
@@ -12,6 +15,18 @@ function toggleDesktopMenu() {
         desktopMenu.style.animation = "disappear 600ms";
         setTimeout (function() {
             desktopMenu.classList.toggle('inactive');
+        },590);
+    }
+}
+
+function toggleMobileMenu() {
+    if (mobileMenu.classList.contains('inactive')) {
+        mobileMenu.style.animation = "slide 600ms";
+        mobileMenu.classList.toggle('inactive');
+    } else {
+        mobileMenu.style.animation = "unslide 600ms";
+        setTimeout (function() {
+            mobileMenu.classList.toggle('inactive');
         },590);
     }
 }
