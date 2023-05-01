27e7f46342f29d41f1dfed17924323e4ce92966c
unknown <alvaromenachojs@gmail.com>
2023-04-30 22:40:26 -0500

    no hice nada

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const carritoIcon = document.querySelector('.navbar-shopping-cart')
 const carritoMenu = document.querySelector('.product-detail')
 const cardConteiner = document.querySelector('.cards-container')
 const Clothes = document.querySelector('.Clothes')
-const pagina = document.getElementById('titulo')
 
 var mostrarProduct = false;
 
@@ -23,19 +22,15 @@ function toggleCarrito() {
     const MenuDesktopClose = desktopMenu.classList.contains('inactive')
 
     if(MenuMovileClose){
-        carritoMenu.classList.remove('inactive')
+        carritoMenu.classList.toggle('inactive')
     }else{
         movileMenu.classList.add('inactive')
-        carritoMenu.classList.remove('inactive')
+        carritoMenu.classList.toggle('inactive')
     }
 
     if(MenuDesktopClose){
         carritoMenu.classList.remove('inactive')
-    }else{
-        desktopMenu.classList.add('inactive')
-        carritoMenu.classList.remove('inactive')
     }
-  
 
 
 }
@@ -74,28 +69,24 @@ function toggleMenu() {
 
 
 }
-function alvaro(){
 
-    var productList = [];
-    productList.push({
-        name:'bicicleta',
-        price:120,
-        image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-    });
-    productList.push({
-        name:'tornamesa',
-        price:240,
-        image:'https://img.clasf.pe/2021/10/05/Maleta-tocadiscos-Tornamesa-Bluetooth-Parlante-Altavoz-20211005093127.7862520015.jpg'
-    });
-    productList.push({
-        name:'mesa',
-        price:300,
-        image:'https://neeflouis.nl/wp-content/uploads/2020/05/TS180927C-0-H-Divan-tafel-Gerrit-Rietveld-1923-Dutch-De-Stijl-kunstbeweging-bauhaus-bijzettafel-Schroeder-huis-Utrecht-Divantafel-Divantafeltje-design-vintage.jpg'
-    });
-    
+var productList = [];
+productList.push({
+    name:'bicicleta',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name:'tornamesa',
+    price:240,
+    image:'https://img.clasf.pe/2021/10/05/Maleta-tocadiscos-Tornamesa-Bluetooth-Parlante-Altavoz-20211005093127.7862520015.jpg'
+});
+productList.push({
+    name:'mesa',
+    price:300,
+    image:'https://neeflouis.nl/wp-content/uploads/2020/05/TS180927C-0-H-Divan-tafel-Gerrit-Rietveld-1923-Dutch-De-Stijl-kunstbeweging-bauhaus-bijzettafel-Schroeder-huis-Utrecht-Divantafel-Divantafeltje-design-vintage.jpg'
+});
 
-    renderProduct(productList);
-}
 
 
 /*
@@ -155,37 +146,40 @@ function renderProduct(lib){
        }
 }
 
+renderProduct(productList);
 
 
 function productClothes(){   
-   alvaro();
-}
-
-function mayra(){
+   
     var productListClothes = [];
-    productListClothes.push({
-        name:'hello Kanye',
-        price:70,
-        image:'https://cdn.shopify.com/s/files/1/0235/4475/8319/products/hellokanye.jpg?v=1667000969'
-    });
-    productListClothes.push({
-        name:'t-shirt Ralph Lauren',
-        price:100,
-        image:'https://static.standard.co.uk/2022/02/11/13/PAUL%20SMITH%20ARTIST%20STRIPE%20POLO.jpg'
-    });
-    productListClothes.push({
-        name:'BBC Jacket Lollipop Red',
-        price:300,
-        image:'https://i.ebayimg.com/images/g/XXEAAOSwxkhhu5gM/s-l1600.jpg'
-    });
+productListClothes.push({
+    name:'hello Kanye',
+    price:70,
+    image:'https://cdn.shopify.com/s/files/1/0235/4475/8319/products/hellokanye.jpg?v=1667000969'
+});
+productListClothes.push({
+    name:'t-shirt Ralph Lauren',
+    price:100,
+    image:'https://static.standard.co.uk/2022/02/11/13/PAUL%20SMITH%20ARTIST%20STRIPE%20POLO.jpg'
+});
+productListClothes.push({
+    name:'BBC Jacket Lollipop Red',
+    price:300,
+    image:'https://i.ebayimg.com/images/g/XXEAAOSwxkhhu5gM/s-l1600.jpg'
+});
 
-    if(mostrarProduct == false) {
-        renderProduct(productListClothes);
-        mostrarProduct = true;
-    }
 
-    
+if(!mostrarProduct) {
+    renderProduct(productListClothes);
+    mostrarProduct = true;
 }
+}
+
+
+
+
+
+
 
 
 
