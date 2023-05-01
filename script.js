146a132ec66c58d871d409163d2d2997024b102a
manu <manu.lazcano17@gmail.com>
2023-05-01 16:17:34 -0300

    Mobile menu function and media querys for desktop and mobile

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const navEmail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-navEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
