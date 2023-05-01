3c3149d0da25c06ab85371a2961d1bb2248eedf6
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-04-30 22:49:07 -0300

    integración de menues completada.

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const BurgerIcon = document.querySelector('.menu');
+const mobileDesktop = document.querySelector('.mobile-menu');
+const shoppingCarIcon = document.querySelector('.navbar-shopping-cart');
+const asideProductDetail = document.querySelector('.product-detail');
 
 function toogleDesktopMenu(){
-    console.log('click');
+    const isCartClosed = asideProductDetail.classList.contains('inactive');
+
+    if(!isCartClosed){
+        asideProductDetail.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
+function toogleMobile(){
+    const isAsideClosed = asideProductDetail.classList.contains('inactive');
 
+    if(!isAsideClosed){
+        asideProductDetail.classList.add('inactive');
+    }
+    mobileDesktop.classList.toggle('inactive');
+}
+
+function hideShowShoppingCart(){
+    const isMobileMenuClosed= mobileDesktop.classList.contains('inactive');
+    const isAsideClosed= asideProductDetail.classList.contains('inactive');
+    
+    
+    /* condiconales para comprabar como estan */
+    //tambien si el mobile menu esta cerrado para cerrarlos
+    if(!isMobileMenuClosed){
+        mobileDesktop.classList.add('inactive');
+    }
+    asideProductDetail.classList.toggle('inactive');
+}
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+BurgerIcon.addEventListener('click', toogleMobile);
+shoppingCarIcon.addEventListener('click', hideShowShoppingCart);
 
 console.log('Js funciona')
 
