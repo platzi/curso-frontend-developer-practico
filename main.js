e49f9dc02c97bd6cd7221610f2ae2583598a6d1b
Jeferson Garzón <jefersong5854@gmail.com>
2023-05-02 14:18:35 -0500

    intregación lista de productos con ayuda de JS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const mobileMenuIco     = document.querySelector('.menu');
 const mobileMenu        = document.querySelector('.mobile-menu');
 const ShopCartMenuIco   = document.querySelector('.navbar-shopping-cart');
 const ShopCartMenu      = document.querySelector('.product-detail');
+const cardsContainer    = document.querySelector('.cards-container');
 
 // Escuchador despliegue menu email.
 menuEmail.addEventListener('click',toggleDesktopMenu);
@@ -55,3 +56,90 @@ function toggleShopCartMenuIco()
     }    
     ShopCartMenu.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Bike2',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Tv',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Computer',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+/*<div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+    <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+    </div>
+    <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+    </div>
+</div>
+
+<div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+    <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+    </div>
+    <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+    </div>
+</div>*/
+
+function renderProducts(array)
+{
+    for (product of productList)
+    {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv    = document.createElement('div');
+        const productPrice      = document.createElement('p');
+        productPrice.innerText  = '$' + product.price;
+        const productName       = document.createElement('p');
+        productName.innerText   = product.name;
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const ProductFigureImg  = document.createElement('img');
+        ProductFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+        productInfoFigure.appendChild(ProductFigureImg);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
+
