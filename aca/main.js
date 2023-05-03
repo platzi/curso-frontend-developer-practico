f14f7fd1394280208822b4d7e859acedd868191e
LKlober <klobermdq@gmail.com>
2023-05-03 11:11:08 -0300

    new day work

--- aca/main.js
+++ aca/main.js
@@ -38,3 +38,10 @@ function toggleproductDetail(){
 
     productDetail.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
