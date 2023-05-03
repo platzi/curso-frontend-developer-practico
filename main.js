a4e439deee83a6c54bf3f8ef2e3e11917c7a9d6b
ccortesm <ccortesm2000@gmail.com>
2023-05-03 16:01:13 -0500

    Se agregan al index.html el menu de carrito con sus estilos y se deja trabajando sin ninfun conflicto con los demas menus desplegables

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const iconMenuMobile = document.querySelector('.menu')
-
+const iconMenuMobile = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 iconMenuMobile.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', togglecarritoAside)
 
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 
+}
+
+function togglecarritoAside() {
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
 }
