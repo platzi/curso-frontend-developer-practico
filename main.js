b891172d933ccf370aebf6224a7c170b9b91bd8f
Franco Caraffa <francocaraffa2005@gmail.com>
2023-05-04 20:50:41 -0300

    Menu mobile interactivo con js y transiciones agregadas

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const menuEmail = document.querySelector(".navbar-email");
 const menu = document.querySelector(".desktop-menu");
+const menuHam = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 menuEmail.addEventListener("click", toggleMenu);
+menuHam.addEventListener("click", toggleMobileMenu);
 
 function toggleMenu() {
-  menu.classList.toggle("inactive");
+  menu.classList.toggle("slide-desktop");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("slide");
 }
