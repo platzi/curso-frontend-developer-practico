dd160a7860fc3243d7a67a145065fd358fc02018
LKlober <klobermdq@gmail.com>
2023-05-04 18:16:08 -0300

    add function js productcards

--- aca/main.js
+++ aca/main.js
@@ -40,12 +40,12 @@ function toggleproductDetail(){
 }
 
 const productList = [];
+
 productList.push({
     name: 'Bike',
     price: 120,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });  
-
 productList.push({
     name: 'Coso',
     price: 11220,
@@ -56,3 +56,42 @@ productList.push({
     price: 13440,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 }); 
+
+/* <div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="../icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>  */
+
+
+for (product of productList){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + productPrice;
+
+    const productName = document.createElement('p');
+    productName.innerText = productName;
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    img.setAttribute('src', "../icons/bt_add_to_cart.svg" );
+
+
+}
