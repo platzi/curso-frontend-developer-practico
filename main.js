0b1e83bd4ac68e63ec877843979f21218520ab15
dvlpr <vicmauuu@gmail.com>
2023-05-04 15:48:36 -0600

    Fusión menú mobile

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const clickMail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuHamburguesa = document.querySelector('.menu');
 
 clickMail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click', toogleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
-
-console.log(desktopMenu.classList);
+function toogleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
