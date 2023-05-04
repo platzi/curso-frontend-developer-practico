141ef9215f4963cc39ccd770e94b6652c19fb0f2
aldoberrios01 <39415375+aldoberrios01@users.noreply.github.com>
2023-05-03 22:51:23 -0600

    menu mobil

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobilMenu = document.querySelector('.mobile-menu');
+
+
+
 menuEmail.addEventListener('click',toggleDesktopMenu)
+menuHamIcon.addEventListener('click',toggleMobilMenu)
 
 function toggleDesktopMenu()
 {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobilMenu()
+{
+    mobilMenu.classList.toggle('inactive');
 }
