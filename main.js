c1077e908a7ab91ac5592e3f3cc6407647410190
hys-soluciones <hugoeduardo07@hotmail.com>
2023-05-04 18:16:18 -0500

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*.***Navbar desktop Menu*** */
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
@@ -5,4 +6,14 @@ menuEmail.addEventListener("click", toogleDesktopMenu);
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+/* *** Menu en movile *** */
+const menuAmburgesa = document.querySelector(".menu");
+const movileMenu = document.querySelector(".mobile-menu");
+
+menuAmburgesa.addEventListener("click", toogleMovileMenu);
+
+function toogleMovileMenu() {
+    movileMenu.classList.toggle("inactive");
 }
