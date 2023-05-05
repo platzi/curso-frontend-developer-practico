87a41bf1223e0fc06941e6f6c35997d10de4501f
hys-soluciones <hugoeduardo07@hotmail.com>
2023-05-05 08:52:00 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const desktopMenu = document.querySelector(".desktop-menu");
 menuEmail.addEventListener("click", toogleDesktopMenu);
 
 function toogleDesktopMenu() {
+    const isAsiceClose = aside.classList.contains("inactive");
+
+    if (!isAsiceClose) {
+        aside.classList.add("inactive");
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
@@ -15,5 +20,31 @@ const movileMenu = document.querySelector(".mobile-menu");
 menuAmburgesa.addEventListener("click", toogleMovileMenu);
 
 function toogleMovileMenu() {
+    const isAsiceClose = aside.classList.contains("inactive");
+
+    if (!isAsiceClose) {
+        aside.classList.add("inactive");
+    }
     movileMenu.classList.toggle("inactive");
+}
+
+/* ***** Carrito de compras***** */
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
+menuCarritoIcon.addEventListener("click", toogleProductDetail);
+
+function toogleProductDetail() {
+    const isMovilMenuClose = movileMenu.classList.contains("inactive");
+    const isDesktopMenu = desktopMenu.classList.contains("inactive");
+
+    if (!isDesktopMenu) {
+        desktopMenu.classList.add("inactive");
+    }
+
+    if (!isMovilMenuClose) {
+        movileMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
 }
