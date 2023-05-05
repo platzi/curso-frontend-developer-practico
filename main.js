4eb486e64f0d7c6aeb29e9cf9ed4c3cfbce3d0c7
dvlpr <vicmauuu@gmail.com>
2023-05-04 18:46:10 -0600

    Fusión del menú carrito de compra y lógica para que no se encimen los menús

--- main.js
+++ main.js
@@ -2,13 +2,30 @@ const clickMail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuHamburguesa = document.querySelector('.menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
 clickMail.addEventListener('click', toggleDesktopMenu);
 menuHamburguesa.addEventListener('click', toogleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarrito);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    const menuCarritoClosed = productDetail.classList.contains('inactive');
+    // Si product detail no tiene la clase active (o sea que está abierto)
+    if(!menuCarritoClosed){
+       productDetail.classList.add('inactive');
+    }
 }
 function toogleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
+}
+function toggleCarrito(){
+    productDetail.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    const menuDesktopClosed = desktopMenu.classList.contains('inactive');
+    if(!menuDesktopClosed){
+       desktopMenu.classList.add('inactive');
+    }
 }
