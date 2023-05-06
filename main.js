4c3893a565cac51744c6accd5553daa6c5e48386
jaracar84 <andresjaramillofranco@gmail.com>
2023-05-06 12:50:51 -0500

    Listo para hacer pullrequest

--- main.js
+++ main.js
@@ -5,26 +5,42 @@ const menuSmallIcon = document.querySelector(".menuSmall");
 const menuMobile = document.querySelector(".mobile-menu");
 const shopingCartContainer = document.querySelector("#shopingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
-const mainContainer = document.querySelector('.main-container');
+const mainContainer = document.querySelector(".main-container");
+const asideProductDetail = document.querySelector("#productDetail");
+const productDetailCloseIcon = document.querySelector(".product-detail-close");
 
 menuEmail.addEventListener("click", mostrarMenu);
 menuSmallIcon.addEventListener("click", mostrarMenuMobile);
 menuCarrito.addEventListener("click", mostrarMenuCarrito);
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside);
 
 function mostrarMenu() {
-  const isMenuCarritoClosed = shopingCartContainer.classList.contains("inactive");
+  const isMenuCarritoClosed =
+    shopingCartContainer.classList.contains("inactive");
+  const isProductDetailClosed =
+    asideProductDetail.classList.contains("inactive");
 
   if (!isMenuCarritoClosed) {
     shopingCartContainer.classList.add("inactive");
   }
+  if (!isProductDetailClosed) {
+    asideProductDetail.classList.add("inactive");
+  }
+
   menuDesktop.classList.toggle("inactive");
 }
 
 function mostrarMenuMobile() {
-  const isMenuCarritoClosed = shopingCartContainer.classList.contains("inactive");
+  const isMenuCarritoClosed =
+    shopingCartContainer.classList.contains("inactive");
+  const isProductDetailClosed =
+    asideProductDetail.classList.contains("inactive");
 
   if (!isMenuCarritoClosed) {
     shopingCartContainer.classList.add("inactive");
+  }
+  if (!isProductDetailClosed) {
+    asideProductDetail.classList.add("inactive");
   }
   menuMobile.classList.toggle("inactive");
 }
@@ -32,32 +48,53 @@ function mostrarMenuMobile() {
 function mostrarMenuCarrito() {
   const isMobileMenuClosed = menuMobile.classList.contains("inactive");
   const isDesktopMenuClosed = menuDesktop.classList.contains("inactive");
+  const isProductDetailClosed =
+    asideProductDetail.classList.contains("inactive");
 
   if (!isMobileMenuClosed) {
     menuMobile.classList.add("inactive");
   }
 
-    if (!isDesktopMenuClosed) {
-      menuDesktop.classList.add("inactive");
+  if (!isDesktopMenuClosed) {
+    menuDesktop.classList.add("inactive");
+  }
+  if (!isProductDetailClosed) {
+    asideProductDetail.classList.add("inactive");
   }
   shopingCartContainer.classList.toggle("inactive");
 }
 
+function openProductDetailAside() {
+  asideProductDetail.classList.remove("inactive");
+  const isMenuCarritoClosed =
+    shopingCartContainer.classList.contains("inactive");
+  if (!isMenuCarritoClosed) {
+    shopingCartContainer.classList.add("inactive");
+  }
+}
+
+function closeProductDetailAside() {
+  asideProductDetail.classList.add("inactive");
+}
+
 const producList = [];
 producList.push({
-  name:'Bike',
-  price:'120',
-  image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  name: "Bike",
+  price: "120",
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 producList.push({
-  name:'Pantalla',
-  price:'220',
-  image:'https://images.pexels.com/photos/1714204/pexels-photo-1714204.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  name: "Pantalla",
+  price: "220",
+  image:
+    "https://images.pexels.com/photos/1714204/pexels-photo-1714204.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 });
 producList.push({
-  name:'Compu',
-  price:'620',
-  image:'https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  name: "Compu",
+  price: "620",
+  image:
+    "https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 });
 
 /*
@@ -75,43 +112,43 @@ producList.push({
     </div>
   </div> */
 
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
 
-  function renderProducts(arr) {
-    for (product of arr) {
-      const productCard = document.createElement('div');
-      productCard.classList.add('product-card');
-  
-      const productImg = document.createElement('img');
-      productImg.setAttribute('src', product.image);
-  
-      const productInfo = document.createElement('div');
-      productInfo.classList.add('product-info');
-  
-      const productInfoDiv = document.createElement('div');
-  
-      const productPrice = document.createElement('p');
-      productPrice.innerText = '$' + product.price;
-      const productName = document.createElement('p');
-      productName.innerText = product.name;
-  
-      productInfoDiv.appendChild(productPrice);
-      productInfoDiv.appendChild(productName);
-  
-      const productInfofigure = document.createElement('figure');
-      const figureImgCart = document.createElement('img');
-      figureImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
-  
-      productInfofigure.appendChild(figureImgCart);
-  
-      productInfo.appendChild(productInfoDiv);
-      productInfo.appendChild(productInfofigure);
-  
-      productCard.appendChild(productImg);
-      productCard.appendChild(productInfo);
-  
-      cardsContainer.appendChild(productCard);
-  
-    }
+    //product= {name, price, image} -> product.image
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+    productImg.addEventListener("click", openProductDetailAside);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfofigure = document.createElement("figure");
+    const figureImgCart = document.createElement("img");
+    figureImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfofigure.appendChild(figureImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfofigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
   }
+}
 
-  renderProducts(producList);
+renderProducts(producList);
