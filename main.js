651a961e6e7fe7c2bbe43ca4b5d4143c12b571fe
Sebas Vasquez <sebitas0489@gmail.com>
2023-05-05 23:55:22 -0500

    Adecuacion de estilos y eventos a  menu mobile, se agreaga mediaquery a ambos menus

--- main.js
+++ main.js
@@ -3,11 +3,18 @@
 //1.Creamos las variables de los elementos que vamos a seleccionar***
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-//2.Utilizamos la propiedada o eventeto al darle click***
+//2.Utilizamos la propiedada o evento al darle click***
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
-//3.Creamos la funcion**
+//3.Creamos las funciones**
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")//llamamos a la variable del elemento y le indicamos que debe hacer***
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
