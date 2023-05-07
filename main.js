26fce4027244c2da48d633efb10fd2429986fee0
Franco Caraffa <francocaraffa2005@gmail.com>
2023-05-07 11:25:21 -0300

    Agregando el Product Details (clase 12), solucionando reescritura de estilos css

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuDesktop = document.querySelector(".desktop-menu");
 const menuHam = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const carroIcon = document.querySelector(".navbar-shopping-cart");
-const productDetail = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const container = document.querySelector(".container");
 const cardsContainer = document.querySelector(".cards-container");
 
@@ -14,18 +14,20 @@ container.addEventListener("click", closeAllMenus);
 
 function toggleMenuDesktop(event) {
   event.stopPropagation();
-  const isProducDetailsClosed = productDetail.classList.contains("inactive");
+  const isProducDetailsClosed =
+    shoppingCartContainer.classList.contains("inactive");
   if (!isProducDetailsClosed) {
-    productDetail.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
   menuDesktop.classList.toggle("slide-desktop");
 }
 
 function toggleMobileMenu(event) {
   event.stopPropagation();
-  const isProducDetailsClosed = productDetail.classList.contains("inactive");
+  const isProducDetailsClosed =
+    shoppingCartContainer.classList.contains("inactive");
   if (!isProducDetailsClosed) {
-    productDetail.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
   mobileMenu.classList.toggle("slide");
 }
@@ -39,17 +41,18 @@ function productDetails(event) {
     mobileMenu.classList.remove("slide");
     menuDesktop.classList.remove("slide-desktop");
   }
-  productDetail.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 function closeAllMenus() {
   const isMobileMenuClosed = !mobileMenu.classList.contains("slide");
   const isDesktopMenuClosed = !menuDesktop.classList.contains("slide-desktop");
-  const isProducDetailsClosed = productDetail.classList.contains("inactive");
+  const isProducDetailsClosed =
+    shoppingCartContainer.classList.contains("inactive");
   if (!isMobileMenuClosed || !isDesktopMenuClosed || !isProducDetailsClosed) {
     mobileMenu.classList.remove("slide");
     menuDesktop.classList.remove("slide-desktop");
-    productDetail.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
 }
 
