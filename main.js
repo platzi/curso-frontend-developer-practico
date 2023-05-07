856ae39b7bd27ad274e9d67538dd0d19bef9541b
dvlpr <vicmauuu@gmail.com>
2023-05-06 22:06:23 -0600

    Fusión lista de productos, creación del for y englobado dentro de una función con un parámetro sencillo

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const menuCarrito = document.querySelector('.navbar-shopping-cart');
 const productDetail = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 clickMail.addEventListener('click', toggleDesktopMenu);
 menuHamburguesa.addEventListener('click', toogleMobileMenu);
@@ -48,6 +49,52 @@ productList.push({
     image: 'https://images.pexels.com/photos/1229861/pexels-photo-1229861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
 
-for (product of productList){
-    console.table(product.price);
+
+/* <div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div> */
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const divDatos = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$'+ product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        const figure = document.createElement('figure');
+
+        const cartImg = document.createElement('img');
+        cartImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        // Colocación de cada elemento hijo dentro del padre.
+        productCard.append(img, productInfo);
+        productInfo.append(divDatos, figure);
+        divDatos.append(productPrice, productName);
+        figure.append(cartImg);
+        cardsContainer.append(productCard);
+    }
 }
+
+renderProducts(productList);
+
