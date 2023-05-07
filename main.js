a209ad33aa5dc9a47cc574425e4a12fd483ab350
veronica mendez <you@example.com>
2023-05-06 19:53:17 -0500

    added toogle to mobile and desktop menu and made changes to css and html

--- main.js
+++ main.js
@@ -1,20 +1,55 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHambIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
-
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHambIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 // aqui vamos a hacer que aparezca o desaparezca el menu
 // en desktop:
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle("inactive");
 };
 
 // en mobile:
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle("inactive");
+};
+
+// carrito de compras 
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isMenuDesktopClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!isMenuDesktopClosed) {
+        desktopMenu.classList.add('inactive');
+        
+    }
+
+    // abrir el aside
+
+    aside.classList.toggle("inactive");
+
+    // si el mobileMenu esta open, hay que cerrarlo
+
 };
