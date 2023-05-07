c0745ea5c88be07ab8c42333a38d698a130a94f3
AngelGM7 <angelgaitan89@gmail.com>
2023-05-07 18:07:34 -0500

    clase7 y clase11 añadidos al index

---
+++ main.js
@@ -0,0 +1,17 @@
+/* const menuEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+
+menuEmail.addEventListener ('click', toggleDesktopMenu)
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toogle('inactive');
+} */
+const menuEmail=document.querySelector('.navbar-email');
+const desktopMenu=document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click',toggleDesktopMenu);
+function toggleDesktopMenu(){
+    console.log('Click');
+    desktopMenu.classList.toggle('inactive');}
+    
+console.log('JS funcionando');
