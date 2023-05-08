a50b794f922bc0851abb60c28740e7b9c9a9f229
Alejandro Valencia <alejandrovalenciarendon@gmail.com>
2023-05-08 17:04:06 -0500

    Se integró el menú desplegable para el botón hamburguesa en la versión móvil

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const menuIcon = document.querySelector(".menu-icon");
 const menuEmail = document.querySelector(".navbar-email");
-const menuDesktop = document.querySelector(".inactive");
+const menuMobile = document.querySelector(".mobile-menu");
+const menuDesktop = document.querySelector(".desktop-menu");
 
+menuIcon.addEventListener("click", toggleMenuMobile);
 menuEmail.addEventListener("click", toggleMenuDesktop);
+
+function toggleMenuMobile() {
+    menuMobile.classList.toggle("inactive");
+}
 
 function toggleMenuDesktop() {
     menuDesktop.classList.toggle("inactive");
