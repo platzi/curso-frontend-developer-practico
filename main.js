7150f4d1a5f52008013d969d2b336d10675f4c71
yerti <yerti0818@gmail.com>
2023-05-08 18:23:20 -0500

    itearciones entre todos los componentes

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu');
 const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailIcon = document.querySelector('.product-detail-close');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const producDetailContainer = document.querySelector('#productDetail')
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 shoppingCartIcon.addEventListener('click',toggleCartAside);
+productDetailIcon.addEventListener('click',closeProductDetailAsside);
 
 function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.toggle('inactive');
@@ -26,6 +29,8 @@ function toggleMobileMenu() {
     if(!isAsideClosed) {
         shoppingCartContainer.classList.add('inactive');
     }
+    
+    closeProductDetailAsside();
 
     mobileMenu.classList.toggle('inactive');
 }
@@ -38,6 +43,24 @@ function toggleCartAside() {
     }
 
     shoppingCartContainer.classList.toggle('inactive');
+
+    const isProductDetailClose = producDetailContainer.classList.contains('inactive');
+    
+    if(!isProductDetailClose) {
+        producDetailContainer.classList.add('inactive');
+    }
+
+    productDetailIcon.classList.toggle('inactive');
+}
+
+function openPoductDetailAside() {
+    shoppingCartContainer.classList.add('inactive');
+    producDetailContainer.classList.remove('inactive');
+    
+}
+
+function closeProductDetailAsside() {
+    producDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -49,12 +72,12 @@ productList.push({
 productList.push({
     name: "bracalet",
     price: 40,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="'
+    image: 'https://i.pinimg.com/564x/33/3f/74/333f742618de4fc8e5dfa0228be29f85.jpg'
 });
 productList.push({
     name: "celular",
     price: 2550,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="'
+    image: 'https://i.pinimg.com/564x/99/b9/c5/99b9c564a70fba21cf050eb60ce317c8.jpg'
 });
 
 function renderProduct(arr) {
@@ -64,6 +87,7 @@ function renderProduct(arr) {
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openPoductDetailAside);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
