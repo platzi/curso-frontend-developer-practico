1893b0e27febfdc849bf7a98ed280312fdb46464
Alejandro Valencia <alejandrovalenciarendon@gmail.com>
2023-05-08 17:41:43 -0500

    Se integró el carrito de compras en las dos vistas, desktop y movil, armonizando su comportamiento con el de los menús del email en desktop y del icono hamburguesa en movil.

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-const menuIcon = document.querySelector(".menu-icon");
-const menuEmail = document.querySelector(".navbar-email");
+const btnBurgerMenu = document.querySelector(".menu-icon");
+const btnEmailMenu = document.querySelector(".navbar-email");
+const btnShopCar = document.querySelector(".navbar-shopping-cart");
 const menuMobile = document.querySelector(".mobile-menu");
 const menuDesktop = document.querySelector(".desktop-menu");
+const menuProdDetail = document.querySelector(".product-detail");
 
-menuIcon.addEventListener("click", toggleMenuMobile);
-menuEmail.addEventListener("click", toggleMenuDesktop);
+btnBurgerMenu.addEventListener("click", toggleMenuMobile);
+btnEmailMenu.addEventListener("click", toggleMenuDesktop);
+btnShopCar.addEventListener("click", toggleMenuProdDetail);
 
 function toggleMenuMobile() {
     menuMobile.classList.toggle("inactive");
+
+    if(!menuMobile.classList.contains("inactive") &&
+            !menuProdDetail.classList.contains("inactive")) {
+        toggleMenuProdDetail();
+    }
 }
 
 function toggleMenuDesktop() {
     menuDesktop.classList.toggle("inactive");
+
+    if(!menuDesktop.classList.contains("inactive") &&
+            !menuProdDetail.classList.contains("inactive")) {
+        toggleMenuProdDetail();
+    }
+}
+
+function toggleMenuProdDetail() {
+    menuProdDetail.classList.toggle("inactive");
+
+    if(!menuProdDetail.classList.contains("inactive")) {
+        if(!menuMobile.classList.contains("inactive")) {
+            toggleMenuMobile();
+        }
+        if(!menuDesktop.classList.contains("inactive")) {
+            toggleMenuDesktop();
+        }
+    }
 }
