0620e884752e53ac4271e24b98f378ebcdb68540
Angel Gutierrez <gutierrez.cantuta.luis@gmail.com>
2023-05-09 19:11:54 -0400

    fusion del carrito de compras

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-const menuEmail=document.querySelector('.navbar-email');
-const desktopMenu=document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    aside.classList.add('inactive');
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+
+    aside.classList.toggle('inactive');
 }
