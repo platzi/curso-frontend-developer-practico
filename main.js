4e6418377e87a440c9a4275d0e0304c23080f01c
Yeison Esteban Pretel Puentes <43614257+yeprepue@users.noreply.github.com>
2023-05-09 10:39:07 -0500

    Menú Mobile, Menú Hamburguesa, Menú Carrito de compras
    Se fusiona HTML en index.html con JS manipulando el DOM y dando estilos en el nav bar manipulando eventos click  botones con funciones

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDestockMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
-function toggleDestockMenu() {
+function toggleDesktopMenu() {
+
+    const isAsideClose = aside.classList.contains('inactive'); 
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+        
+    }
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClose = aside.classList.contains('inactive'); 
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+        
+    }
+    mobileMenu.classList.toggle('inactive');
+
+
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.toggle('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+
 }
