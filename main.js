36fd8bad216f0c7dcdbfc0732611b32ad0da1d47
Lester <lestercobon@gmail.com>
2023-05-11 00:24:05 -0600

    Menu responsivo

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 
 const desplegable = document.querySelector('.desktop-menu');
 const correo = document.querySelector('.navbar-email');
+const hamburger = document.querySelector('.menu');
+const menuView = document.querySelector('.mobile-menu');
 
 correo.addEventListener('click', toogleClass);
 
+
 function toogleClass(){
     desplegable.classList.toggle('inactive');
+}
+
+hamburger.addEventListener('click', menuHamburger);
+
+function menuHamburger(){
+    menuView.classList.toggle('inactive');
 }
