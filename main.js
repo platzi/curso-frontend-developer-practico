75e0b041dae3b50dc7da6872fadf1a25eeffa0d0
Marcos Day <marcosday90@gmail.com>
2023-05-10 22:32:40 -0300

    Fusión aside carrito de compras

--- main.js
+++ main.js
@@ -2,23 +2,51 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const manuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
+    const isAsideMenuClosed = aside.classList.contains('inactive');
+
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive');
+     }
 
     desktopMenu.classList.toggle('inactive');// Esta función agrega o remueve una clase
-
-    // desktopMenu.classList.forEach(clase => {
-    //     clase == "inactive"?desktopMenu.classList.remove('inactive'):desktopMenu.classList.add('inactive');
-    // })
 }
 
 
 manuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu(){
-    console.log("hola")
+    const isAsideMenuClosed = aside.classList.contains('inactive');
+
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive');
+     }
+
     mobileMenu.classList.toggle('inactive');// Esta función agrega o remueve una clase
+}
+
+
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+    
+    if(!isMobileMenuClosed){
+       mobileMenu.classList.add('inactive');
+    }
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+     }
+
+    aside.classList.toggle('inactive');
 }
