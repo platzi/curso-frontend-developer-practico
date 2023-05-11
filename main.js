50f429e61c6fb2973f31b54cff20d0aacab1405c
jesusvite <nicolasvitejesus@gmail.com>
2023-05-11 11:23:41 -0600

    agregue mejoras en el munu mobie con html css y javascript

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const varEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const varMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-varEmail.addEventListener('click',showMenu);
 
-function showMenu(){
+varEmail.addEventListener('click',showDesktopMenu);
+varMenu.addEventListener('click',showMobileMenu);
+
+
+function showMobileMenu(){
+        mobileMenu.classList.toggle('inactive')
+}
+function showDesktopMenu(){
         desktopMenu.classList.toggle('inactive');
 };
-console.log('Js funcionando')
