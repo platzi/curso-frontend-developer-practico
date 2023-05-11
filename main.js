d4fc7336d4dd90c7ddc07e33b536e52204f29ae7
jesusvite <nicolasvitejesus@gmail.com>
2023-05-11 17:03:49 -0600

    en Java script cree una foncion que contiene un for para que al agregar productos pueda agregarlos sin siguiendo un patron.

--- main.js
+++ main.js
@@ -2,15 +2,120 @@ const varEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const varMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const navCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardContainer = document.querySelector('.cards-container');
 
 
-varEmail.addEventListener('click',showDesktopMenu);
-varMenu.addEventListener('click',showMobileMenu);
+varEmail.addEventListener('click',togleDesktopMenu);
+varMenu.addEventListener('click',togleMobileMenu);
+navCarrito.addEventListener('click',togleCarMenu);
 
 
-function showMobileMenu(){
+function togleMobileMenu(){
+        const isCarMenuClose = aside.classList.contains('inactive');
+        if(!isCarMenuClose){
+                aside.classList.add('inactive');
+        }
         mobileMenu.classList.toggle('inactive')
+
 }
-function showDesktopMenu(){
+function togleDesktopMenu(){
+        const isCarMenuClose = aside.classList.contains('inactive');
+        if(!isCarMenuClose){
+                aside.classList.add('inactive');
+        }
         desktopMenu.classList.toggle('inactive');
 };
+function togleCarMenu(){
+        const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+        const isDesktopMenuClode = desktopMenu.classList.contains('inactive');
+        if(!isMobileMenuClose){
+                mobileMenu.classList.add('inactive');
+        }
+        if(!isDesktopMenuClode){
+                desktopMenu.classList.add('inactive');
+        }
+        aside.classList.toggle('inactive');
+}
+
+const productsContent = [];
+productsContent.push({
+name:'compu',
+price:'$120,00',
+image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productsContent.push({
+name:'compu',
+price:'$120,00',
+image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productsContent.push({
+name:'compu',
+price:'$120,00',
+image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productsContent.push({
+name:'compu',
+price:'$120,00',
+image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+function productList(arr){
+  for (product of arr){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');    
+  
+    const img = document.createElement('img');
+    img.setAttribute('src', product.image);
+  // -------------------
+    const productInfo = document.createElement('div')
+    productInfo.classList.add('product-info');
+  
+  
+         // ---------------------
+    const productInfoDiv = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerHTML = product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice)
+    productInfoDiv.appendChild(productName)
+  
+         // -----------------------
+    const productFigure = document.createElement('figure');
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', './icons/bt_add_to_cart.svg' );
+    
+    productFigure.appendChild(productImg);
+        // -----------------------
+  // ------------------------
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productFigure);
+    
+    productCard.append(img, productInfo);
+  
+    cardContainer.appendChild(productCard);
+    
+  }
+}
+productList(productsContent);
+
+
+
+{/*
+<div class="product-card">
+  <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+  <div class="product-info">
+    <div>
+      <p>$120,00</p>
+      <p>Bike</p>
+    </div>
+    <figure>
+      <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+  </div>
+</div>
+ */}
