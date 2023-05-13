2f54dd632c590e805b1dc8425fbbd25ac280de66
unknown <javif8@gmail.com>
2023-05-13 07:19:13 -0400

    third commit

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const burgerMenu = document.querySelector(".menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenu.addEventListener("click", toggleMobileMenu);
+menuCarIcon.addEventListener("click", toggleCarAside);
 
 function toggleDesktopMenu () {
+    const isAsideClose = aside.classList.contains("inactive");
+
+    if(!isAsideClose) {
+        aside.classList.add("inactive");
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu () {
+    const isAsideClose = aside.classList.contains("inactive");
+    
+    if(!isAsideClose) {
+        aside.classList.add("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarAside () {
+    const isMobileMenuClose = mobileMenu.classList.contains("inactive");
+    
+    if (!isMobileMenuClose) {
+        mobileMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
 }
