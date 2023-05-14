4be5afe47fc24a62faa843aec1d5d56dd9723f7f
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-05-14 17:21:49 -0400

    nuevos estilos y agregar aside de product detail

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon =document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarIcon =document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 // toggleDestokMenu
 menuEmail.addEventListener('click', () => {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
@@ -18,10 +18,10 @@ menuEmail.addEventListener('click', () => {
 
 // toggleMobileMenu
 menuHamIcon.addEventListener('click', () => {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -39,7 +39,7 @@ menuCarIcon.addEventListener('click', () => {
     if (isDesktopMenuOpen) {
         desktopMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 });
 
 const productList = [];
