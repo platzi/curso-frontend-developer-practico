2ff3413cc5b48b5d0ae47a28468294cb95c9d41a
EdwinMolina <edwin96.molina@outlook.es>
2023-05-14 11:54:32 -0600

    Segunda fusion de mis componentes - menu mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const iconHambur = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 function toggleMenu(){
     menuDesktop.classList.toggle('inactive');
 }
+function toggleMobilMenu(){
+    menuMobile.classList.toggle('inactive');
+}
 menuEmail.addEventListener('click', toggleMenu);
+iconHambur.addEventListener('click', toggleMobilMenu);
