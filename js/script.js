f70cb30fbd1d923ebf60b872ee40187f8c65ef4b
EdwinMolina <edwin96.molina@outlook.es>
2023-05-14 16:35:47 -0600

    Tercera fusion de mis componentes - Carrito de compras

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+
 const iconHambur = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const detalleProductos = document.querySelector('.product-detail');
+
 function toggleMenu(){
+    //El detalle de carrito esta cerrado cuando tiene la clase inactive
+    const isdetallecarrito = detalleProductos.classList.contains('inactive');
+    //si el detalle carrito NO esta cerrado(esta abierto)
+    if(!isdetallecarrito){
+        detalleProductos.classList.add('inactive');
+    }
     menuDesktop.classList.toggle('inactive');
+
 }
-function toggleMobilMenu(){
+function toggleMobileMenu(){
+    //El detalle de carrito esta cerrado cuando tiene la clase inactive
+    const isdetallecarrito = detalleProductos.classList.contains('inactive');
+    //si el detalle carrito NO esta cerrado
+    if(!isdetallecarrito){
+        detalleProductos.classList.add('inactive');
+    }
     menuMobile.classList.toggle('inactive');
 }
+function toggleDetallesProductos(){    
+    //El menu mobile esta cerrado cuando tiene la clase inactive
+    const isMobileMenuClosed = menuMobile.classList.contains('inactive');
+    //si el menu NO esta cerrado
+    if(!isMobileMenuClosed){
+        menuMobile.classList.add('inactive');
+    }
+
+    detalleProductos.classList.toggle('inactive');
+}
+
+
 menuEmail.addEventListener('click', toggleMenu);
-iconHambur.addEventListener('click', toggleMobilMenu);
+iconHambur.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleDetallesProductos);
