77eca878547aff5c55f8da231f6799aac01521e7
Luiscodify <lmgonzalezj@outlook.com>
2023-05-16 13:50:04 -0400

    Se agrego el carrito de compras en la clase 21

--- main.js
+++ main.js
@@ -2,15 +2,30 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const MenuHamIcon = document.querySelector('.menu');
 const MovilMenu = document.querySelector('.mobile-menu');
+const ShoppingCart = document.querySelector('.navbar-shopping-cart');
+const ProductDetail = document.querySelector('.product-detail');
 
-
+ShoppingCart.addEventListener('click', toggleProductDetail)
 navEmail.addEventListener('click', togglemenu);
 MenuHamIcon.addEventListener('click', togglemobilemenu);
 
 function togglemenu() {
     desktopMenu.classList.toggle('inactive')
+    if (!ProductDetail.classList.contains('inactive')){
+        ProductDetail.classList.add('inactive')
+    }
 }
 
 function togglemobilemenu() {
     MovilMenu.classList.toggle('inactive')
+    if (!ProductDetail.classList.contains('inactive')){
+        ProductDetail.classList.add('inactive')
+    }
+}
+
+function toggleProductDetail() {
+    ProductDetail.classList.toggle('inactive')
+    if (!MovilMenu.classList.contains('inactive')) {
+        MovilMenu.classList.add('inactive')
+    }
 }
