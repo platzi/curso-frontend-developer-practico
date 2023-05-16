9b291df2197200b933c826457cdd8793dd013c10
Luiscodify <lmgonzalezj@outlook.com>
2023-05-16 16:47:56 -0400

    Se agrego el product datalles en la clase 23

--- main.js
+++ main.js
@@ -3,30 +3,30 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const MenuHamIcon = document.querySelector('.menu');
 const MovilMenu = document.querySelector('.mobile-menu');
 const ShoppingCart = document.querySelector('.navbar-shopping-cart');
-const ProductDetail = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 
-ShoppingCart.addEventListener('click', toggleProductDetail)
+ShoppingCart.addEventListener('click', toggleshoppingCartContainer)
 navEmail.addEventListener('click', togglemenu);
 MenuHamIcon.addEventListener('click', togglemobilemenu);
 
 function togglemenu() {
     desktopMenu.classList.toggle('inactive')
-    if (!ProductDetail.classList.contains('inactive')){
-        ProductDetail.classList.add('inactive')
+    if (!shoppingCartContainer.classList.contains('inactive')){
+        shoppingCartContainer.classList.add('inactive')
     }
 }
 
 function togglemobilemenu() {
     MovilMenu.classList.toggle('inactive')
-    if (!ProductDetail.classList.contains('inactive')){
-        ProductDetail.classList.add('inactive')
+    if (!shoppingCartContainer.classList.contains('inactive')){
+        shoppingCartContainer.classList.add('inactive')
     }
 }
 
-function toggleProductDetail() {
-    ProductDetail.classList.toggle('inactive')
+function toggleshoppingCartContainer() {
+    shoppingCartContainer.classList.toggle('inactive')
     if (!MovilMenu.classList.contains('inactive')) {
         MovilMenu.classList.add('inactive')
     }
