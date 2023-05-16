cbd3b6b81ececc21a784dd06e3df218b0af7f0d4
Luiscodify <lmgonzalezj@outlook.com>
2023-05-16 12:21:26 -0400

    Se agrego el menu en mibile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const MenuHamIcon = document.querySelector('.menu');
+const MovilMenu = document.querySelector('.mobile-menu');
+
 
 navEmail.addEventListener('click', togglemenu);
+MenuHamIcon.addEventListener('click', togglemobilemenu);
 
 function togglemenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function togglemobilemenu() {
+    MovilMenu.classList.toggle('inactive')
 }
