b519de210ded13389597259e60cc45e75ad9c521
Yeison Esteban Pretel Puentes <43614257+yeprepue@users.noreply.github.com>
2023-05-17 13:35:24 -0500

    Arreglando CSS y HTML de platilla. Se agrega los elementos de productDetail

--- main.js
+++ main.js
@@ -1,88 +1,70 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
-const cardContainer = document.querySelector('.cards-container');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
-    const isAsideClose = aside.classList.contains('inactive');
-
-    if (!isAsideClose) {
-        aside.classList.add('inactive');
-
+    if (!isAsideClosed) {
+        shoppingCartContainer.classList.add('inactive');
     }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClose = aside.classList.contains('inactive');
-    if (!isAsideClose) {
-        aside.classList.add('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
+    if (!isAsideClosed) {
+        shoppingCartContainer.classList.add('inactive');
     }
+
     mobileMenu.classList.toggle('inactive');
-
-
 }
 
 function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    const isdesktopMenuClosed = desktopMenu.classList.contains('inactive');
 
-    if (!isMobileMenuClosed && isdesktopMenuClosed) {
-
-        mobileMenu.classList.toggle('inactive');
-        desktopMenu.classList.toggle('inactive');
-
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
     }
 
-
-
-
-    aside.classList.toggle('inactive');
-
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
-
 productList.push({
     name: 'Bike',
     price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
-
 productList.push({
-    name: 'TV',
-    price: 180,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
-
 productList.push({
-    name: 'Xbox xs',
-    price: 240,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
-//recorre lista de objetos del array y con se genera codigo JS para insertar HTML de los objetos.
-function rederProducts(arr) {
+function renderProducts(arr) {
     for (product of arr) {
-
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
 
+        // product= {name, price, image} -> product.image
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
-        // product ={name, price, image} 
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -91,7 +73,6 @@ function rederProducts(arr) {
 
         const productPrice = document.createElement('p');
         productPrice.innerText = '$' + product.price;
-
         const productName = document.createElement('p');
         productName.innerText = product.name;
 
@@ -110,10 +91,8 @@ function rederProducts(arr) {
         productCard.appendChild(productImg);
         productCard.appendChild(productInfo);
 
-        cardContainer.appendChild(productCard);
-
-
+        cardsContainer.appendChild(productCard);
     }
 }
 
-rederProducts(productList);
+renderProducts(productList);
