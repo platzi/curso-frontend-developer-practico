e600a3f616586439b9be6892a42ddbd370e1c564
Katherine <angiecruzo.o@gmail.com>
2023-05-17 19:25:19 -0500

    cuarto commit con js

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const menuCarritoIcon= document.querySelector('.navbar-shopping-cart');
 const desktopMenu= document.querySelector('.desktop-menu');
 const mobileMenu= document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -35,4 +37,61 @@ function toggleAside() {
         mobileMenu.classList.add('inactive');
     }
     aside.classList.toggle('inactive'); 
+}
+
+const productList =[]
+productList.push({
+    name: 'bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=',
+});
+
+productList.push({
+    name: 'screen',
+    price: 220,
+    image: 'https://media.wired.com/photos/624cdf50b340f55b37084fda/191:100/w_1280,c_limit/WFH-Dual-Monitors-Gear-1199427514.jpg',
+});
+
+productList.push({
+    name: 'computer',
+    price: 620,
+    image: 'https://media.istockphoto.com/id/516508345/es/vector/fondo-con-%C3%A1rea-de-trabajo.jpg?s=170667a&w=0&k=20&c=PXZM2NSgHNA_vMgg9bMP8ig_my86aJxjCef4krKIEQw=',
+});
+
+for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    // product ={name, prime, image} -> product.image
+    const productImg= document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+
 }
