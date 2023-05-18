19e6549618c18ffbe8144dce713c19c45bc9157d
Diego Grueso <goidesdesign@gmail.com>
2023-05-17 22:21:45 -0400

    update projects with css and js

--- main.js
+++ main.js
@@ -6,22 +6,43 @@ document.addEventListener('DOMContentLoaded', function(){
     const menuMobile = document.querySelector('.mobile-menu')
     const iconCart = document.querySelector('.navbar-shopping-cart')
     const aside = document.querySelector('aside')
+    const asideAdd = document.querySelector('.product-detail--add')
+    const closeButton = document.querySelector('.product-detail-close')
+    const imgAddCart = document.querySelector('.product-detail--add > img')
+
     
     showMenuUser.addEventListener('click', onClickShowMenuUser)
     iconMenumobile.addEventListener('click', onClickShowMenuMobile)
     iconCart.addEventListener('click', onClickCartShopping)
+    closeButton.addEventListener('click', onClickCloseButton)
     
     function onClickShowMenuUser(){
         const isAsideClose = aside.classList.contains('hide--element')
+        const isAsideAdd = asideAdd.classList.contains('hide--element')
 
         if(!isAsideClose){
             aside.classList.add('hide--element')
+        }
+
+        if(!isAsideAdd){
+            asideAdd.classList.add('hide--element')
         }
 
         menuEmail.classList.toggle('hide--element')
     }
 
     function onClickShowMenuMobile(){
+        const isAsideAddClose = asideAdd.classList.contains('hide--element')
+        const isAsideClose = aside.classList.contains('hide--element')
+
+        if(!isAsideAddClose){
+            asideAdd.classList.add('hide--element')
+        }
+
+        if(!isAsideClose){
+            aside.classList.add('hide--element')
+        }
+
         menuMobile.classList.toggle('hide--element')
 
         if(!menuMobile.classList.contains('hide--element')){
@@ -31,9 +52,19 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function onClickCartShopping(){
         const isDesktopClose = menuEmail.classList.contains('hide--element')
+        const isAsideAddClose = asideAdd.classList.contains('hide--element')
+        const isMenuMobileClose = menuMobile.classList.contains('hide--element')
 
         if(!isDesktopClose){
             menuEmail.classList.add('hide--element')
+        }
+
+        if(!isAsideAddClose){
+            asideAdd.classList.add('hide--element')
+        }
+
+        if(!isMenuMobileClose){
+            menuMobile.classList.add('hide--element')
         }
         
         if(!aside.classList.contains('hide--element')){
@@ -41,6 +72,10 @@ document.addEventListener('DOMContentLoaded', function(){
         }
 
         aside.classList.toggle('hide--element')
+    }
+
+    function onClickCloseButton(){
+        asideAdd.classList.add('hide--element')
     }
 
     const productList = []
@@ -57,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function(){
     productList.push({
         name: 'Skate',
         price: 100,
-        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        img: 'https://hips.hearstapps.com/hmg-prod/images/2023-mclaren-artura-101-1655218102.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     })
 
     function generateProducts(arr){
@@ -68,6 +103,7 @@ document.addEventListener('DOMContentLoaded', function(){
             // img product
             const img = document.createElement('img')
             img.src = product.img
+            img.addEventListener('click', onClickImageProduct)
             // div product info
             const divProductInfo = document.createElement('div')
             divProductInfo.classList.add('product-info')
@@ -98,6 +134,17 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     generateProducts(productList)
+
+    function onClickImageProduct(e){
+        const isMenuUserClose = menuEmail.classList.contains('hide--element')
+        const isAsideClose = aside.classList.contains('hide--element')
+
+        if(!isAsideClose) aside.classList.add('hide--element')
+        if(!isMenuUserClose) menuEmail.classList.add('hide--element')
+
+        asideAdd.classList.remove('hide--element')
+        imgAddCart.src = e.target.currentSrc
+    }
 
 
 })
