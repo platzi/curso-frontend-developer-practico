397316f6e929d1ae2a7bdbafd7030a34e8e60bfc
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-05-19 18:29:54 -0400

    mostrar productos por cada imagen cliqueada

--- main.js
+++ main.js
@@ -59,37 +59,62 @@ productDetailCloseIcon.addEventListener('click', function () {
     productDetailContainer.classList.add('inactive');
 });
 
-const productList = [];
-productList.push({
-    name: 'Bike',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Pantalla',
-    price: 240,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Compu',
-    price: 450,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Bike',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Pantalla',
-    price: 240,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Compu',
-    price: 450,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
+const productList = [
+    {
+        name: 'Bike',
+        description: 'With ',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Macbook Pro',
+        description: 'With its practical',
+        price: 400,
+        image: 'https://images.pexels.com/photos/6446709/pexels-photo-6446709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Computer Desktop',
+        description: 'With its practical position, this',
+        price: 450,
+        image: 'https://images.pexels.com/photos/6804098/pexels-photo-6804098.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Mouse Ergonimico',
+        description: 'With its practical position, this bike ',
+        price: 20,
+        image: 'https://images.pexels.com/photos/5241055/pexels-photo-5241055.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Ram hddr4 16gb',
+        description: 'With its practical position, this bike also',
+        price: 30,
+        image: 'https://images.pexels.com/photos/2582928/pexels-photo-2582928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Laptop Hp',
+        description: 'With its practical position, this bike also fulfills',
+        price: 240,
+        image: 'https://images.pexels.com/photos/11129922/pexels-photo-11129922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Control de play',
+        description: 'With its practical position, this bike also fulfills a ',
+        price: 30,
+        image: 'https://images.pexels.com/photos/1371985/pexels-photo-1371985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Xbox',
+        description: 'With its practical position, this bike also fulfills a decorative',
+        price: 300,
+        image: 'https://images.pexels.com/photos/10997582/pexels-photo-10997582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Playstation 1',
+        description: 'With its practical position, this bike also fulfills a decorative function, ',
+        price: 250,
+        image: 'https://images.pexels.com/photos/4219892/pexels-photo-4219892.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+];
 
 const cardsContainer = document.querySelector('.cards-container');
 
@@ -101,7 +126,12 @@ function renderProducts(arr) {
         // product = {name, price,image} ->product.image
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.setAttribute('id', `${product.name}`)
         productImg.addEventListener('click', function () {
+
+            // Cambiar productos por nombre
+            changeProductInfo(this.getAttribute('id'), productList);
+
             // ocultar cartContainer 
             shoppingCartContainer.classList.add('inactive');
 
@@ -142,3 +172,32 @@ function renderProducts(arr) {
 }
 
 renderProducts(productList);
+
+function changeProductInfo(name, arr) {
+
+    for (product of arr) {
+        if (name != product.name) {
+            continue;
+        }else{
+            const img = document.querySelector('#productDetail > img');
+            img.setAttribute('src', product.image);
+            
+            const pPrice = document.querySelector('#productDetail .product-info p:nth-child(1)');
+            const pName = document.querySelector('#productDetail .product-info p:nth-child(2)');
+            const pDescription = document.querySelector('#productDetail .product-info p:nth-child(3)');
+            pPrice.innerText = product.name;
+
+            pName.innerText = '$' + product.price;
+
+            pDescription.innerText = product.description;
+        }
+    }
+}
+
+function desestructuracion(arr) {
+    for(const {name: nombre, description: descripcion, price: precio} of arr) {
+        console.log(`${nombre}, 
+        descripcion: ${descripcion}, 
+        precio: ${precio}`);
+    }
+}
