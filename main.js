ed6318cb7614bd720f54cf3c40ef2cd5117982d4
c.castro.protel <c.castro.protel>
2023-05-19 10:38:46 -0500

    [IMP] onclick navbar to show menu in mobile version

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var navbarEmail = document.querySelector('.navbar-email');
+var menuHamIcon = document.querySelector('.menu');
+
+var desktopMenu = document.querySelector('.desktop-menu');
+var mobileMenu = document.querySelector('.mobile-menu');
 
 function toggleDesktopMenu(event){
-    var desktopMenu = document.querySelector('.desktop-menu');
-
     desktopMenu.classList.toggle('inactive')
 }
 
+function toggleMobileMenu(event){
+    mobileMenu.classList.toggle('inactive')
+}
+
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
