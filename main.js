29cb9cc35403f442ce5dfea18dbb0eba301cc8c3
jaec1146 <jaec1146@gmail.com>
2023-05-20 16:47:38 -0700

    En index agremos el archivo main.js en styles hicimos modificaciones en cursor y hover de meni email. Principalmente se relizo un evento click con toggle para activar y desactivar la clase inactve que pone en display none.

---
+++ main.js
@@ -0,0 +1,33 @@
+//cree esta constante para no tener que estar escribiendo document.querySelector(selector) cada vez.
+const $ = (selector) => document.querySelector(selector);
+
+const menuEmail = $('.navbar-email');
+const desktopMenu = $('.desktop-menu');
+
+
+/* como la escribió el profesor
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}*/
+
+//Una forma mas comprimida de escribir el  evento de la función es:
+/*
+--------------------------------------------------
+- menuEmail.addEventListener('click', ()=> {     -   
+-     desktopMenu.classList.toggle('inactive')   -
+- });                                            -
+--------------------------------------------------
+Pero al parecer no es una buena práctica ya que si te ves en la necesidad de remover el evento, al ser anónimo, no podrás hacerlo.
+
+
+Para agregar la función flecha ()=> seria recomendable hacerlo así.*/
+
+const toggleDesktopMenu = () => {
+    desktopMenu.classList.toggle('inactive');
+}
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+// Para que funcione tube que poner el evento click después de la función.
