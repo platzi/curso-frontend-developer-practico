031f7f42c48e231a1c659b0c925250676a4aafca
Amparo Guillén Requena <ampguireq@gmail.com>
2023-05-20 12:23:28 +0100

    Lista de productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuShopCart = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shopCart = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurguerIcon.addEventListener('click', toggleMobileMenu);
@@ -43,3 +44,56 @@ function toggleShopCartMenu() {
 
     shopCart.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'SkateBoard',
+    price: 160,
+    image: 'https://img.freepik.com/psd-gratis/mockup-skateboard_1310-876.jpg?size=626&ext=jpg&ga=GA1.2.510947045.1681300511&semt=sph',
+});
+productList.push({
+    name: 'RollerSkate',
+    price: 90,
+    image: 'https://img.freepik.com/foto-gratis/mujer-llevando-rollerskates-equitacion-pavimento_23-2147654934.jpg?size=626&ext=jpg&ga=GA1.1.510947045.1681300511&semt=sph',
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info'); 
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '€' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfoDiv.append(productPrice, productName);
+
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImg, productInfo);
+
+    cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
