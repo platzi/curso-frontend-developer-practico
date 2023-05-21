5521ce6533ba97474582f2413b34c503d37e4582
Walter Llanos <walterdll@hotmail.com>
2023-05-21 07:55:19 -0500

    added master html, js and css

---
+++ main.js
@@ -0,0 +1,7 @@
+const emailMenu = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+emailMenu.addEventListener('click', toggleDesktopMenu);
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive');
+};
