8f9c7b84287c58c2f04078e0b4112a3b3b8c26c6
c.castro.protel <c.castro.protel>
2023-05-23 16:06:44 -0500

    [IMP] Menu Mobile Responsive

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 var navbarEmail = document.querySelector('.navbar-email');
 var menuHamIcon = document.querySelector('.menu');
+var menuCarIcon = document.querySelector('.navbar-shopping-cart');
 
 var desktopMenu = document.querySelector('.desktop-menu');
 var mobileMenu = document.querySelector('.mobile-menu');
+var carMenu = document.querySelector('.product-detail');
+
+
+navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarIcon.addEventListener('click', toggleCarMenu);
 
 function toggleDesktopMenu(event){
     desktopMenu.classList.toggle('inactive')
+    carMenu.classList.add('inactive')
 }
 
 function toggleMobileMenu(event){
     mobileMenu.classList.toggle('inactive')
+    carMenu.classList.add('inactive')
 }
 
-navbarEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', toggleMobileMenu);
+function toggleCarMenu(event){
+    carMenu.classList.toggle('inactive')
+    desktopMenu.classList.add('inactive')
+    mobileMenu.classList.add('inactive')
+}
