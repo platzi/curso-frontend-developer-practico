ee353cac18bb4e5d8d4beb3b2ab5bd25109df15a
jaec1146 <jaec1146@gmail.com>
2023-05-22 18:45:01 -0700

    Agregue la parte de JS para carrito de compras y unos cambnios en los togglets para que no interfirieran uno con el otro

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
 const menuHamIcon = $('.menu');
 const mobileMenu = $('.mobile-menu');
+const menuCarritoIcon = $('.navbar-shopping-cart');
+const aside = $('.product-detail');
 
 
 /* como la escribió el profesor
@@ -27,12 +29,39 @@ Pero al parecer no es una buena práctica ya que si te ves en la necesidad de re
 Para agregar la función flecha ()=> seria recomendable hacerlo así.*/
 
 const toggleDesktopMenu = () => {
+    const isCarritoClosed = aside.classList.contains('inactive');
+
+    if (!isCarritoClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 const toggleMobileMenu = () => {
+    const isCarritoClosed = aside.classList.contains('inactive');
+
+    if (!isCarritoClosed) {
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+const toggleCarrito = () => {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarrito);
 // Para que funcione tube que poner el evento click después de la función.
