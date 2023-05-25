44fb4e6920a2f0c6dff3ed5786434f8c5f7b872f
Jason Sepulveda <jasonsepulvedas@gmail.com>
2023-05-25 16:13:40 -0500

    product detail added

--- main.js
+++ main.js
@@ -6,29 +6,45 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const iconShoppingCart = document.querySelector('.navbar-shopping-cart');
 const shoppingCart = document.querySelector('.aside-shopping-cart');
 const cardsContainer = document.querySelector('.cards-container');
+const productDetailContainer = document.querySelector('.product-detail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
+
 
 // 2- Listen the even and declare the function, in this case we created a script where user to click on an element and then a function will be run
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 iconMobileMenu.addEventListener('click', toggleMobileMenu);
 iconShoppingCart.addEventListener('click', toggleShoppingCart);
+productDetailCloseIcon.addEventListener('click', closeProductDetail);
+
 
 // 3- create the function. The mayority of the functions are same, beacuse we wanted to hide or display some elements. Even we applied a logic without if, for example, when click on the shopping cart we hide the rest of the element like a desktop menu or mobile menu.
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
     shoppingCart.classList.add('inactive'); // closing ShoppingCart
+    productDetailContainer.classList.add("inactive"); // closing Product Detail
 }
 
 function toggleMobileMenu () {
     mobileMenu.classList.toggle('inactive');
     shoppingCart.classList.add('inactive'); // closing ShoppingCart
+    productDetailContainer.classList.add("inactive"); // closing Product Detail
 }
 
 function toggleShoppingCart () {
     shoppingCart.classList.toggle('inactive');
     mobileMenu.classList.add('inactive'); // closing Mobile Menu
     desktopMenu.classList.add('inactive'); // closing Desktop Menu
+    productDetailContainer.classList.add("inactive"); // closing Product Detail
+}
+
+function openProductDetail () {
+    productDetailContainer.classList.remove("inactive");
+}
+
+function closeProductDetail () {
+    productDetailContainer.classList.add('inactive')
 }
 
 const ProductList = [];
@@ -70,6 +86,7 @@ function renderProduct (ArrayProducts) {
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetail);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
