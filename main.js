c06449f367c700c76caf7d02fd86a118e4c8fd82
lupeMorales <morales.lupe@gmail.com>
2023-05-25 21:53:09 +0200

    add mobile menu

--- main.js
+++ main.js
@@ -2,9 +2,24 @@
 
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
 }
 
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+}
+
 navEmail.addEventListener("click", toggleDesktopMenu);
+burgerMenu.addEventListener("click", toggleMobileMenu);
+
+/* refactor code */
+/* const toggleElement = (ev) => {
+  ev.classList.toggle("inactive");
+};
+
+navEmail.addEventListener("click", () => toggleElement(desktopMenu));
+burgerMenu, addEventListener("click", () => toggleElement(mobileMenu)); */
