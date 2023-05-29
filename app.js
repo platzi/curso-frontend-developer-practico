371183aa63a024f4d3f10a5a69470144e70f5c0a
Raudy Garcia <16gbrailius16@gmail.com>
2023-05-29 03:11:23 -0400

    Agregando los menus tanto de menu mobile como el del carrito y colocando su javascript para hacerlo dinamico

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon= document.querySelector('.navbar-shopping-cart');
+const carrito = document.querySelector('.product-detail');
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
+burguerIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoMenu);
 
 function toggleDesktopMenu(){
 
@@ -15,5 +21,17 @@ function toggleDesktopMenu(){
     // }
 
     // solucion mas corta
-    desktopMenu.classList.toggle('inactive')
+    carrito.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    carrito.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoMenu(){
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    carrito.classList.toggle('inactive');
 }
