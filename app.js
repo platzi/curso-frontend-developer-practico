c24f7f971c833dc5ebadee85c0496988158ea6e6
Raudy Garcia <16gbrailius16@gmail.com>
2023-05-29 09:09:50 -0400

    Agregando todos los productos generados con javaScript

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const burguerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon= document.querySelector('.navbar-shopping-cart');
 const carrito = document.querySelector('.product-detail');
+const cardContainer = document.querySelector('.cards-container');
 
 
 
@@ -35,3 +36,107 @@ function toggleCarritoMenu(){
     desktopMenu.classList.add('inactive');
     carrito.classList.toggle('inactive');
 }
+
+const productList = [];
+
+function backend(){
+    productList.push({
+        name:'Bike',
+        precio:120,
+        imagen:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    });
+    productList.push({
+        name:'Pantalla',
+        precio:220,
+        imagen:'https://images.unsplash.com/photo-1593305841991-05c297ba4575?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1057&q=80'
+    });
+    productList.push({
+        name:'Computadora',
+        precio:620,
+        imagen:'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80'
+    });
+    productList.push({
+        name:'Bike',
+        precio:120,
+        imagen:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    });
+    productList.push({
+        name:'Pantalla',
+        precio:220,
+        imagen:'https://images.unsplash.com/photo-1593305841991-05c297ba4575?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1057&q=80'
+    });
+    productList.push({
+        name:'Computadora',
+        precio:620,
+        imagen:'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80'
+    });
+    productList.push({
+        name:'Bike',
+        precio:120,
+        imagen:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    });
+    productList.push({
+        name:'Pantalla',
+        precio:220,
+        imagen:'https://images.unsplash.com/photo-1593305841991-05c297ba4575?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1057&q=80'
+    });
+    productList.push({
+        name:'Computadora',
+        precio:620,
+        imagen:'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80'
+    });
+    productList.push({
+        name:'Bike',
+        precio:120,
+        imagen:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    });
+    productList.push({
+        name:'Pantalla',
+        precio:220,
+        imagen:'https://images.unsplash.com/photo-1593305841991-05c297ba4575?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1057&q=80'
+    });
+    productList.push({
+        name:'Computadora',
+        precio:620,
+        imagen:'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80'
+    });
+}
+backend();
+
+function renderProduct(array){
+    array.forEach( product => {
+        console.log(product.name)
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const img = document.createElement('img');
+        img.setAttribute('src', product.imagen);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText='$' + product.precio;
+    
+        const productName = document.createElement('p');
+        productName.innerText= product.name;
+    
+        productInfoDiv.append(productPrice, productName);
+    
+        const productFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+        productFigure.appendChild(productImgCard);
+    
+        productInfo.append(productInfoDiv, productFigure)
+    
+        productCard.append(img, productInfo);
+    
+        cardContainer.appendChild(productCard)
+    })
+};
+
+renderProduct(productList);
