1c133fc4f7d25f0410c12ecf10561c10c7e537cc
juanmunoz816 <juanmunozrozo@gmail.com>
2023-05-30 18:13:15 -0500

    Se agrego la funcionalidad del carrito de compras, ademas de eso se mejoró la lógica del JS

--- msin.js
+++ msin.js
@@ -4,14 +4,28 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const shopingCar = document.querySelector('.navbar-shopping-cart');
+const orderDetail = document.querySelector('.product-detail');
+
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobilepMenu);
+shopingCar.addEventListener('click', toggleOrderDetail);
 
 
 function toggleDesktopMenu() {
+    orderDetail.classList.add('inactive')
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobilepMenu() {
+    orderDetail.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleOrderDetail() {
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive');
+    orderDetail.classList.toggle('inactive');
 }
