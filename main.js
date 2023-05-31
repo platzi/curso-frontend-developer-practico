5e40c3023576cff5b0cbedb9bec5971ae053a469
Ckevn <cknicolas@icloud.com>
2023-05-31 09:08:39 -0300

    Agregamos los menus faltantes, tanto a desktop como a mobile, usando javascript. Usamos métodos como toggle, add, contains. A su ves utilizamos condicionales para poder ocultar un menú en el caso que el otro esté abierto y no se superpongan.

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 const menuEmail = document.querySelector('.navbar-email'); //seleccionamos la clase, y al hacer click en esa clase queremos que aparezca el menu email.
-
+const menuHamburgerMobile = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
+
+// Eventos
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburgerMobile.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
+
+
+// Funciones
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){ 
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+
+    if(!isMobileMenuClosed){
+        //preguntamos si el mobileMenu está open, hay que cerrarlo. 
+        mobileMenu.classList.add('inactive');
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+     aside.classList.toggle('inactive');
 }
