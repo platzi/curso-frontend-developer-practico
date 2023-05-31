8d62bb212a6f20a7800da4099ec019553c82b429
SergioPenaloza <pesergioro@gmail.com>
2023-05-31 01:09:24 -0500

    Integración de menú mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHanIco = document.querySelector('.menu');
+const movileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHanIco.addEventListener('click', toggleMovileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMovileMenu(){
+    movileMenu.classList.toggle('inactive');
 }
