6cb314110a3a9a55ec3bac9b6ee29095e49b8b62
David ibarguen <kibarguenrivas@gmail.com>
2023-06-02 13:58:16 -0500

    Termine seccion de productos desde javascript y lo elimine del html

--- tienda.js
+++ tienda.js
@@ -12,7 +12,6 @@ correo.addEventListener('click',aparecer)
 function aparecer(){
     desktopMenu.classList.toggle('inactive');
 }
-
 // este es el menu mobil 
 
 btnburgue.addEventListener('click',menuMobil)
@@ -38,3 +37,57 @@ if(!mobilMenuOpen ){
  ventana.classList.toggle('inactive');
 
 }
+
+// empezamos con nuestra area de productos
+
+const containerCards = document.querySelector('.cards-container')
+const productCards = document.querySelector('.product-card')
+
+const product = []
+ product.push({
+    name: 'bike',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    price: 1000,
+ })
+ product.push({
+    name: 'pantalla',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    price: 1500,
+ })
+ product.push({
+    name: 'moto',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    price: 2500,
+ })
+  
+function bloqueCardsProductos(arr) {
+
+    for (const recorProduc of arr) {
+        const productCards = document.createElement("div");
+        productCards.classList.add("product-card");
+        const imgProduct = document.createElement("img");
+        imgProduct.src = recorProduc.image;      //tambien puedo usar la siguiente sintasis imgProduct.setAttribute("src", recorProduc.image)
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+        const blockPriceName = document.createElement("div");
+        const pPrice = document.createElement("p");
+        pPrice.textContent = recorProduc.price; 
+        const pName = document.createElement("p");
+        pName.textContent = recorProduc.name; 
+        const figure = document.createElement("figure");
+        const imgFigureIcon = document.createElement("img");
+        imgFigureIcon.src = "./icons/bt_add_to_cart.svg";
+        figure.appendChild(imgFigureIcon);
+        blockPriceName.appendChild(pPrice);
+        blockPriceName.appendChild(pName);
+        productInfo.appendChild(blockPriceName);
+        productInfo.appendChild(figure);
+        productCards.appendChild(imgProduct);
+        productCards.appendChild(productInfo);
+        containerCards.appendChild(productCards);
+        
+     }
+    
+}
+
+bloqueCardsProductos(product)
