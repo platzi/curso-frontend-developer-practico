42beafc59c6594b223ef32e95de85d3dd4bd4f81
willy23jaramillo <alexis20hj@hotmail.com>
2023-06-02 19:10:17 -0600

    se realizan cambios en menu mobile

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-let NavEmail=document.querySelector(`.navbar-email`)
-let DesktopMenu=document.querySelector(`.desktop-menu`)
+let NavEmail=document.querySelector(`.navbar-email`);
+let DesktopMenu=document.querySelector(`.desktop-menu`);
+let mobileMenu=document.querySelector(`.menu`);
+let menuMobile=document.querySelector(`.mobile-menu`);
 
 
-NavEmail.addEventListener(`click`,menuEmail)
+NavEmail.addEventListener(`click`,menuEmail);
+mobileMenu.addEventListener(`click`,touchMobileMenu);
+
 function menuEmail(){
-   DesktopMenu.classList.toggle("inactive");
+   DesktopMenu.classList.toggle(`inactive`);
+   
+   
    
 }
-
+function touchMobileMenu(){
+    menuMobile.classList.toggle(`inactive`);
+}
