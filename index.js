e3001f5032d68957cdc3b669faffb1bd63dd249f
willy23jaramillo <alexis20hj@hotmail.com>
2023-06-03 13:26:38 -0600

    se agrego producto y se modifico color y efecto de shopin car

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 let NavEmail=document.querySelector(`.navbar-email`);
 let DesktopMenu=document.querySelector(`.desktop-menu`);
+
 let mobileMenu=document.querySelector(`.menu`);
 let menuMobile=document.querySelector(`.mobile-menu`);
 
+let shopincar=document.querySelector(`.navbar-shopping-cart`)
+let menuShopinCar=document.querySelector(`.product-detail`)
 
-NavEmail.addEventListener(`click`,menuEmail);
-mobileMenu.addEventListener(`click`,touchMobileMenu);
+NavEmail.addEventListener(`click`,toggleMenus);
+mobileMenu.addEventListener(`click`,toggleMobile);
+shopincar.addEventListener(`click`,toggleShopincar)
 
-function menuEmail(){
-   DesktopMenu.classList.toggle(`inactive`);
-   
-   
-   
+function toggleMenus(){
+    let carshopinopen=menuShopinCar.classList.contains(`inactive`)
+    if(!carshopinopen){
+        menuShopinCar.classList.add(`inactive`)
+    }
+    DesktopMenu.classList.toggle(`inactive`) 
 }
-function touchMobileMenu(){
-    menuMobile.classList.toggle(`inactive`);
+function toggleMobile(){
+    let carshopinopen=menuShopinCar.classList.contains(`inactive`)
+    if(!carshopinopen){
+        menuShopinCar.classList.add(`inactive`)
+    }
+    menuMobile.classList.toggle(`inactive`);  
+
+}
+function toggleShopincar (){
+ let desktopopen=DesktopMenu.classList.contains(`inactive`)
+ let menuopen= menuMobile.classList.contains(`inactive`)
+ if(!menuopen){
+    menuMobile.classList.add(`inactive`)
+ }else if(!desktopopen){
+    DesktopMenu.classList.add(`inactive`)
+
+ }
+ menuShopinCar.classList.toggle(`inactive`)
+
 }
