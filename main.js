97da0c4acb6c972f8e0945f3752db670d76871ee
h marin <hmarin01@gmail.com>
2023-06-04 15:58:59 -0600

    Actualización de menus

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+const menuImg = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+menuEmail.addEventListener('click', toggleDesktopMenu)
+menuImg.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
