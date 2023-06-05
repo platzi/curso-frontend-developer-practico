1159dd9bd88c2b691108f649a41f89eb27b45c73
fabian-co <fabian-co@hotmail.com>
2023-06-05 18:52:32 -0500

    agregamos funciones al aside para que se cierre cuando abrimos otro panel esta todo ok

--- main.js
+++ main.js
@@ -2,50 +2,49 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const closeProductDetailIcon = document.querySelector(".product-detail-close")
 const mobileMenu = document.querySelector(".mobile-menu")
 const cardsContainer = document.querySelector(".cards-container")
+const asideproductDetail = document.querySelector(".product-detail")
 const asidecartproductdetail = document.querySelector(".cart-product-detail")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
 
 function toggleDesktopMenu() {
-    const siAsideColsed = asidecartproductdetail.classList.contains("inactive")
-
-    if(!siAsideColsed){
-        asidecartproductdetail.classList.add("inactive")
-    }
-
+    asidecartproductdetail.classList.add("inactive")
+    asideproductDetail.classList.add("inactive")
     desktopMenu.classList.toggle("inactive");
 }
 
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
-    const isMenuCarritoIcon = menuCarritoIcon.classList.contains("inactive")
-    
-    if (!isMenuCarritoIcon){
-        asidecartproductdetail.classList.add("inactive")
-    }
-    
+    asidecartproductdetail.classList.add("inactive")   
+    asideproductDetail.classList.add("inactive") 
     mobileMenu.classList.toggle("inactive");
 }
 
 menuCarritoIcon.addEventListener("click", toggleCarritoAside)
 
 function toggleCarritoAside() {
-    const isMobileMenuClosed = menuHamIcon.classList.contains("inactive")
-    const isDesctopMenuClosed = desktopMenu.classList.contains("inactive")
-
-    if (!isMobileMenuClosed) {
-        mobileMenu.classList.add("inactive");
-    }
-
-    if (!isDesctopMenuClosed){
-        desktopMenu.classList.add("inactive")
-    }
-
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
+    asideproductDetail.classList.add("inactive")
     asidecartproductdetail.classList.toggle("inactive")
+}
+
+function openProductDetail() {
+    asidecartproductdetail.classList.add("inactive")
+    asideproductDetail.classList.remove("inactive")
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+}
+
+closeProductDetailIcon.addEventListener("click", closeProductDetail)
+
+function closeProductDetail(){
+    asideproductDetail.classList.add("inactive")
 }
 
 const productList =  []
@@ -76,6 +75,7 @@ function renderProducts (array){
     
         const productImg = document.createElement("img")
         productImg.setAttribute("src", product.image)
+        productImg.addEventListener("click", openProductDetail)
     
         const productInfo = document.createElement("div")
         productInfo.classList.add("product-info")
