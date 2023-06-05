256faaf3ea461cc9cd9bf8d3cf6cf56e42c3580c
Juan Sintora <juansintora@gmail.com>
2023-06-04 22:39:14 -0300

    unimos la parte mobile del menu hamburguesa y @media para pc

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const  menuEmail = document.querySelector(".navbar-email");
 const  desktopMenu = document.querySelector(".desktop-menu");
+const  hamburger = document.querySelector(".hamburger");
+const  menuMobile = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+hamburger.addEventListener('click', toggleMovileMenu)
+
+function toggleMovileMenu() {
+    menuMobile.classList.toggle('inactive');
 }
