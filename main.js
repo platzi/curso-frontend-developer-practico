ebc95f55a45384e7cd05b02ff1bba0e0bf3fe702
Juan Sintora <juansintora@gmail.com>
2023-06-06 15:56:18 -0300

    carrito

--- main.js
+++ main.js
@@ -2,15 +2,27 @@ const  menuEmail = document.querySelector(".navbar-email");
 const  desktopMenu = document.querySelector(".desktop-menu");
 const  hamburger = document.querySelector(".hamburger");
 const  menuMobile = document.querySelector(".mobile-menu");
+const  menuCarrito = document.querySelector(".navbar-shopping-cart");
+const  aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 hamburger.addEventListener('click', toggleMovileMenu)
 
 function toggleMovileMenu() {
+    aside.classList.add('inactive');
     menuMobile.classList.toggle('inactive');
+}
+
+menuCarrito.addEventListener('click', toggleMovileCarrito)
+
+function toggleMovileCarrito() {
+    menuMobile.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
