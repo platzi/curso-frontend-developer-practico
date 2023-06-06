64441b32babed6412dac66999dc3d0540078e41f
David ibarguen <kibarguenrivas@gmail.com>
2023-06-05 23:27:04 -0500

    agregue la descripcion del producto y otras modificasiones con javascript

--- tienda.js
+++ tienda.js
@@ -4,6 +4,10 @@ const btnburgue = document.querySelector('.menu')
 const mobilMenu = document.querySelector('.mobile-menu ')
 const ventana = document.querySelector('.product-detail')
 const carrito = document.querySelector('.carrito')
+const blockdeTalles =document.querySelector('.product-detail-segundary')
+const btnclosedetalles = document.querySelector('.product-detail-segundary-close')
+ 
+
 
 // configuracion del correo
 
@@ -18,23 +22,31 @@ btnburgue.addEventListener('click',menuMobil)
 
 function menuMobil(){
  const ventanaOpen= ventana.classList.contains('inactive')
-  
+ const closTalle=blockdeTalles.classList.contains('inactive')
 
    if(!ventanaOpen ){
     ventana.classList.add('inactive')
     }
+    if(!closTalle){
+        blockdeTalles.classList.add('inactive')
+        }
     mobilMenu.classList.toggle('inactive');
 }
+
 // esta es la ventana del carrito 
 carrito.addEventListener('click',ventanaCarrito)
 
 function ventanaCarrito(){
  const mobilMenuOpen= mobilMenu.classList.contains('inactive')
- 
+ const talles = blockdeTalles.classList.contains('inactive')
 if(!mobilMenuOpen ){
  mobilMenu.classList.add('inactive')
  }
  ventana.classList.toggle('inactive');
+ if(!talles){
+   blockdeTalles.classList.add('inactive')
+   }
+
 
 }
 
@@ -66,6 +78,7 @@ function bloqueCardsProductos(arr) {
         const productCards = document.createElement("div");
         productCards.classList.add("product-card");
         const imgProduct = document.createElement("img");
+        imgProduct.addEventListener('click',open)
         imgProduct.src = recorProduc.image;      //tambien puedo usar la siguiente sintasis imgProduct.setAttribute("src", recorProduc.image)
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
@@ -89,5 +102,24 @@ function bloqueCardsProductos(arr) {
      }
     
 }
+function open() {
+   const close= ventana.classList.contains('inactive')
+   const closeMenu= mobilMenu.classList.contains('inactive')
+   if (!close) {
+      ventana.classList.add('inactive')
+   }
+   blockdeTalles.classList.remove('inactive')
+   if (!closeMenu) {
+         mobilMenu.classList.add('inactive')
+      }
+}
 
-bloqueCardsProductos(product)
+bloqueCardsProductos(product) 
+
+btnclosedetalles.addEventListener('click',btnclose)
+
+function btnclose() {
+   blockdeTalles.classList.add('inactive')
+   
+}
+
