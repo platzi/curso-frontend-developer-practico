7f4090a07a823c4938f6f049dcec4e74aef843ab
Ckevn <cknicolas@icloud.com>
2023-06-06 10:02:17 -0300

    Fusionamos componentes y agregamos detalles de un producto a el proyecto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuHamburgerMobile = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container') 
 
 
@@ -16,20 +16,20 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 // Funciones
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     
     if(!isAsideClosed){ 
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -47,7 +47,7 @@ function toggleCarritoAside(){
     if(!isDesktopMenuClosed){
         desktopMenu.classList.add('inactive');
     }
-     aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 /* <div class="product-card">
