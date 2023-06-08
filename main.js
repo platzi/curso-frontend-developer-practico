cb15709156b5fb41826cf4d8030fd8083032eb1b
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-06-07 21:34:37 -0500

    Aside de carrito fusionado

--- main.js
+++ main.js
@@ -12,16 +12,28 @@ const navbarEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const hamMenuIcon = document.querySelector('.ham-menu-icon')
 const mobileMenu = document.querySelector('.mobile-menu')
+const cartIcon = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
 
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
 
 function toggleDesktopMenu() {
-     desktopMenu.classList.toggle('inactive')
+    productDetail.classList.add('inactive')
+    desktopMenu.classList.toggle('inactive')
 }
 
-hamMenuIcon.addEventListener('click', toogleMobileMenu)
+hamMenuIcon.addEventListener('click', toggleMobileMenu)
 
-function toogleMobileMenu() {
+function toggleMobileMenu() {
+    productDetail.classList.add('inactive')
     mobileMenu.classList.toggle('inactive')
+}
+
+cartIcon.addEventListener('click', toggleCartIcon)
+
+function toggleCartIcon() {
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    productDetail.classList.toggle('inactive')
 }
