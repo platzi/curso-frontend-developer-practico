9bcfa4742f6659937f840e1af0e8efbbbd83bb3e
Yutamo <alca.amb.96@gmail.com>
2023-06-09 17:48:46 -0500

    se agrego el menu mobile y se añadieron media queries

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const navEmail = document.querySelector (".navbar-email");
 const menuEmail= document.querySelector (".desktop-menu");
+const botonMenuMovil = document.querySelector (".menu");
+const menuMovil = document.querySelector(".mobile-menu");
 
 console.log(menuEmail);
 
 navEmail.addEventListener("click", () => {
     menuEmail.classList.toggle("inactive");
+});
+
+botonMenuMovil.addEventListener("click", () => {
+    menuMovil.classList.toggle("inactive");
 });
 
