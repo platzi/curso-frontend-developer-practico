13b3d7066a1191af1c68eee39a210e9e464811b9
sanriago <santhy200243@gmail.com>
2023-06-09 16:15:10 -0500

    fusion aside

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const menuEnable = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEnable.addEventListener('click',()=>{
   desktopMenu.classList.toggle('inactive');
+})
+
+burgerMenu.addEventListener('click',()=>{
+  mobileMenu.classList.toggle('inactive');
 })
