26d712378d16630013b29080aa8cb5d514bd774f
sanriago <santhy200243@gmail.com>
2023-06-10 17:23:34 -0500

    pull request

--- js/index.js
+++ js/index.js
@@ -6,11 +6,14 @@ const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
 const cardContainer = document.querySelector('.cards-container');
+const openAsideO = document.querySelector('#product-detail');
+const openAsideC = document.querySelector('#product-detail-close');
 
 
 menuEnable.addEventListener('click',()=>{
   aside.classList.add('inactive')
   desktopMenu.classList.toggle('inactive');
+  openAsideO.classList.add('inactive')
   
 })
 
@@ -20,6 +23,8 @@ burgerMenu.addEventListener('click',()=>{
    aside.classList.add('inactive');
  }
  mobileMenu.classList.toggle('inactive');
+ openAsideO.classList.add('inactive');
+
 })
 
 menuCarrito.addEventListener('click',()=>{
@@ -31,8 +36,19 @@ menuCarrito.addEventListener('click',()=>{
   }
    desktopMenu.classList.add('inactive')
   aside.classList.toggle('inactive')
+  openAsideO.classList.add('inactive')
 
 })
+openAsideC.addEventListener('click',()=>{
+  openAsideO.classList.toggle('inactive')
+})
+
+function openAside(){
+  openAsideO.classList.toggle('inactive')
+  mobileMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive')
+}
+
 
 const products = [];
 products.push({
@@ -40,7 +56,6 @@ products.push({
   precio: '120',
   image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
-
 
 function renderProducts(arr){
   for(product of arr){
@@ -50,6 +65,7 @@ function renderProducts(arr){
     // product= {name, price, image} -> product.image
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click',openAside)
   
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
@@ -78,3 +94,10 @@ function renderProducts(arr){
 }
 
 renderProducts(products);
+renderProducts(products);
+renderProducts(products);
+renderProducts(products);
+renderProducts(products);
+
+
+
