90d958cb730af7745829434d4dab8d53008c3833
Yutamo <alca.amb.96@gmail.com>
2023-06-11 12:06:14 -0500

    Se agrego reposive y dinamica a los menus desplegables de la tienda

--- main.js
+++ main.js
@@ -6,9 +6,24 @@ const menuMovil = document.querySelector(".mobile-menu");
 const carroCompras = document.querySelector(".navbar-shopping-cart");
 const asaidCompras = document.querySelector(".product-detail");
 const contedorTarjetas = document.querySelector(".cards-container");
+const asaid2 = document.querySelector(".product-detail-as2");
+const closeAsaid2 = document.querySelector(".product-detail-close");
+const cartasProductos = document.querySelector(".cards-container");
+const cartasProductos2 = document.querySelector(".main-container");
+
+const precio2 = document.getElementById("valorAsaid2");
+const nombre2 = document.getElementById("productoAsaid2");
+
 
 //VARIABLES JS
 const listaProductos = [];
+let anchoVentana = null;
+
+//VARIABLES MOSTRAR/OCULTAR ELEMENTOS
+let asaidComprasView = asaidCompras.classList.contains("inactive");
+let menuEmailView = menuEmail.classList.contains("inactive")
+let menuMovilView = menuMovil.classList.contains("inactive")
+
 
 
 //NO esta en uso-----FUNCION PARA MOSTRAR Y OCULTAR UN ELEMENTO 
@@ -19,35 +34,85 @@ function mostrarOcultarElemento(activador, elementoMostrar) {
 
 }
 
-navEmail.addEventListener("click", () => {
-    if (asaidCompras.classList.contains("inactive") === false) {
-        asaidCompras.classList.add("inactive");
+//ESCUCHO LA PANTALLA Y LA GUARDO EL ANCHO EN UNA VARIABLE
+window.addEventListener("resize", () => {
+    anchoVentana = window.innerWidth;
+    //console.log(typeof(anchoVentana))
+});
+
+//se deben tener en cuenta:
+        //menuEmail
+        //asaidCompras
+        //menuMovil
+        //asaid2
+
+navEmail.addEventListener("click", () => {    
+
+    if (asaidCompras.classList.contains("inactive") === false || asaid2.classList.contains("inactive") === false) {
+        asaidCompras.classList.add("inactive");        
         menuEmail.classList.toggle("inactive");
+        menuEmail.style.zIndex = "1";
     } else {
         menuEmail.classList.toggle("inactive")
     }
 })
 
 botonMenuMovil.addEventListener("click", () => {
-    if (asaidCompras.classList.contains("inactive") === false) {
+   
+    if (asaidCompras.classList.contains("inactive") === false || asaid2.classList.contains("inactive") === false) {
         console.log("menu movil escuchando")
         asaidCompras.classList.add("inactive");
+        asaid2.classList.add("inactive");
         menuMovil.classList.toggle("inactive");
     } else {
         menuMovil.classList.toggle("inactive")
     }
 })
 
-carroCompras.addEventListener("click", () => {
-    if (menuEmail.classList.contains("inactive") === false) {
-        menuEmail.classList.add("inactive");
-        asaidCompras.classList.toggle("inactive");
-    } else if (menuMovil.classList.contains("inactive") === false) {
-        menuMovil.classList.add("inactive");
-        asaidCompras.classList.toggle("inactive");
-    } else {
-        asaidCompras.classList.toggle("inactive")
+carroCompras.addEventListener("click", () => {    
+
+    if (window.innerWidth > 641){
+
+        if (menuEmail.classList.contains("inactive") === false || asaid2.classList.contains("inactive") === false) {
+            menuEmail.classList.add("inactive");
+            asaidCompras.classList.toggle("inactive");
+            asaidCompras.style.zIndex = "1";
+        } else if (menuMovil.classList.contains("inactive") === false) {
+            menuMovil.classList.add("inactive");
+            asaidCompras.classList.toggle("inactive");
+        } else {
+            asaidCompras.classList.toggle("inactive")
+        }
     }
+    
+    if (window.innerWidth < 641){
+
+        if (menuEmail.classList.contains("inactive") === false || asaid2.classList.contains("inactive") === false) {
+            menuEmail.classList.add("inactive");
+            asaidCompras.classList.toggle("inactive");
+            asaid2.classList.toggle("inactive");
+            
+        } else if (menuMovil.classList.contains("inactive") === false) {
+            menuMovil.classList.add("inactive");
+            asaidCompras.classList.toggle("inactive");
+        } else {
+            asaidCompras.classList.toggle("inactive")
+        }
+    }    
+})
+
+//Funcionalidad del boton de la X del assaid2
+closeAsaid2.addEventListener("click",() => {
+    if (window.innerWidth > 641){
+        //console.log("asaid2")
+        asaid2.classList.toggle("inactive")
+        cartasProductos2.classList.remove("inactive")        
+    }
+    
+    if (window.innerWidth < 641){
+        asaid2.classList.toggle("inactive")        
+    }      
+
 })
 
 //insertar productos------------------------------------------------------------
@@ -71,8 +136,31 @@ listaProductos.push({
     precio: 20,
     imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 })
-
-
+listaProductos.push({
+    nombre: "Herramienta",
+    precio: 80,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+listaProductos.push({
+    nombre: "Lica Deportiva",
+    precio: 150,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+listaProductos.push({
+    nombre: "Silla acolchada",
+    precio: 45,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+listaProductos.push({
+    nombre: "Gorra",
+    precio: 17,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+listaProductos.push({
+    nombre: "Luces de transito",
+    precio: 71,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
 
 /*< div class="product-card" >
     <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"alt="">
@@ -92,12 +180,13 @@ listaProductos.push({
 
 //Convertir este ciclo en una funcion
 for (productos of listaProductos) {
-    console.log(productos)
-    const productCard = document.createElement("div");
+    //console.log(productos)
+    const productCard = document.createElement("div"); 
     productCard.classList.add("product-card");
 
     const imgCard = document.createElement("img");
-    imgCard.setAttribute("src",productos.imagen);    
+    imgCard.setAttribute("src",productos.imagen);
+    imgCard.classList.add("imgCardProducto");    
 
     const productInfo = document.createElement("div");
     productInfo.classList.add("product-info");    
@@ -128,3 +217,49 @@ for (productos of listaProductos) {
     
     contedorTarjetas.appendChild(productCard);
 };
+
+//se deben tener en cuenta:
+        //menuEmail
+        //asaidCompras
+        //menuMovil
+
+const imgCP = document.querySelectorAll(".imgCardProducto");
+
+imgCP.forEach((imagenProducto) => {
+
+    imagenProducto.addEventListener("click",() =>{
+        
+        const asaid2View = asaid2.classList.contains("inactive")
+
+        if (window.innerWidth > 641){
+
+            if (asaid2View === true){
+                asaid2.classList.toggle("inactive")
+                menuEmail.classList.add("inactive")
+                asaidCompras.classList.add("inactive")
+                menuMovil.classList.add("inactive")
+                cartasProductos2.classList.add("inactive")
+            } else {
+                asaid2.classList.toggle("inactive")
+            }
+        }
+        
+        if (window.innerWidth < 641){
+
+            if (asaid2View === true){
+                asaid2.classList.toggle("inactive")
+                menuEmail.classList.add("inactive")
+                asaidCompras.classList.add("inactive")
+                menuMovil.classList.add("inactive")                
+            } else {
+                asaid2.classList.toggle("inactive")
+            }                   
+        }
+    })
+})
+
+
+
+
+
+
