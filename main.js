f8ac9600729862da0d10ea71c7272a7cf6ef6466
Felipe Cabana <cabanita2390@hotmail.com>
2023-06-11 20:46:28 -0500

    Terminamos el menu mobile y la clase 20

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+console.log(hamburguerMenu);
 console.log(menuEmail);
 console.log(desktopMenu);
-
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     console.log('Clic');
     desktopMenu.classList.toggle('inactive');
+}
+
+hamburguerMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    console.log('Clic hamburger menu');
+    mobileMenu.classList.toggle('inactive');
 }
