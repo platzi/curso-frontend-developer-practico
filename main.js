b115478e20eeea5b967bb972594440fb24d3736c
Joaquin Ibañez <joaquinpulga2004@gmail.com>
2023-06-16 13:20:19 -0300

    Proyecto en buen camino arreglando errores que tenia en el  anterior commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const DesktopMenu = document.querySelector(".desktop-menu")
 const MenuCarritoIcon = document.querySelector(".navbar-shopping-cart")
 const MenuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
-const aside = document.querySelector(".product-detail")
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer")
 const cardsContainer = document.querySelector(".cards-container")
 
 menuemail.addEventListener("click" , toggleDesktopMenu)
@@ -11,19 +11,19 @@ MenuHamIcon.addEventListener("click" , toggleMobileMenu)
 MenuCarritoIcon.addEventListener("click" , toggleCarritoAside)
 
 function toggleDesktopMenu () {
-    const isAsideClosed = aside.classList.contains("inactive")
+    const isAsideClosed = shoppingCartContainer.classList.contains("inactive")
 
     if(!isAsideClosed) {
-        aside.classList.add("inactive")
+        shoppingCartContainer.classList.add("inactive")
     }
     DesktopMenu.classList.toggle("inactive")
 }
 
 function toggleMobileMenu () {
-    const isAsideClosed = aside.classList.contains("inactive")
+    const isAsideClosed = shoppingCartContainer.classList.contains("inactive")
 
     if(!isAsideClosed) {
-        aside.classList.add("inactive")
+        shoppingCartContainer.classList.add("inactive")
     }
 
     mobileMenu.classList.toggle("inactive")
@@ -36,7 +36,7 @@ function toggleCarritoAside () {
         mobileMenu.classList.add("inactive")
     }
 
-    aside.classList.toggle("inactive")
+    shoppingCartContainer.classList.toggle("inactive")
 }
 
 const productList = []
@@ -59,40 +59,40 @@ productList.push({
 function renderProducts (arr) {
     for (product of arr) {
 
-        const productCard = document.createElement("div")
-        productCard.classList.add("product-card")
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
     
-        const productImg = document.createElement("img")
-        productImg.setAttribute("src", product.image)
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.image);
     
-        const productInfo = document.createElement("div")
-        productInfo.classList.add("product-info")
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
         
-        const productInfoDiv = document.createElement("div")
+        const productInfoDiv = document.createElement("div");
     
-        const productPrice = document.createElement("p")
-        productPrice.innerText = "$" + product.Price
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.Price;
     
-        const productName = document.createElement("p")
-        productName.innerText = product.Name
+        const productName = document.createElement("p");
+        productName.innerText = product.Name;
     
-        productInfoDiv.appendChild(productPrice)
-        productInfoDiv.appendChild(productName)
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
     
-        const productInfoFigure = document.createElement("figure")
-        const productImgCart = document.createElement("Img")
-        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
+        const productInfoFigure = document.createElement("figure");
+        const productImgCart = document.createElement("Img");
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
     
-        productInfoFigure.appendChild(productImgCart)
+        productInfoFigure.appendChild(productImgCart);
     
-        productInfo.appendChild(productInfoDiv)
-        productInfo.appendChild(productInfoFigure)
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
     
-        productCard.appendChild(productImg)
-        productCard.appendChild(productInfo)
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
     
-        cardsContainer.appendChild(productCard)
+        cardsContainer.appendChild(productCard);
     }
 }
 
-
+renderProducts (productList);
