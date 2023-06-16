fe9bde74d6e1ae4ce072b318e1718b4287d2c814
pipecove <pipecove@gmail.com>
2023-06-16 02:36:21 -0500

    Fusión: Lista de productos HTML a partir de arrays

--- main.js
+++ main.js
@@ -4,37 +4,112 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
 
-    if(!isAsideClosed){
+    if (!isAsideClosed) {
         aside.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu(){
+function toggleMobileMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
 
-    if(!isAsideClosed){
+    if (!isAsideClosed) {
         aside.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCarritoAside(){
+function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
-    if(!isMobileMenuClosed){
-    mobileMenu.classList.add('inactive');
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
     }
 
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Yamato',
+    price: 120,
+    image: 'https://i.pinimg.com/564x/ce/9c/68/ce9c68dc189a912ce3705da7b2350c28.jpg',
+})
+productList.push({
+    name: 'Luffy',
+    price: 220,
+    image: 'https://i.pinimg.com/564x/1b/aa/51/1baa516470fc278145718dd2048bdf6d.jpg',
+})
+productList.push({
+    name: 'Nami',
+    price: 520,
+    image: 'https://i.pinimg.com/564x/3d/88/62/3d8862a4a06c5e0c7272a00d0b84dff4.jpg',
+})
+
+/*
+    < div class="product-card" >
+        <img src="https://i.pinimg.com/564x/ce/9c/68/ce9c68dc189a912ce3705da7b2350c28.jpg"
+            alt="Producto">
+            <div class="product-info">
+                <div>
+                    <p>$120,00</p>
+                    <p>Yamato</p>
+                </div>
+                <figure>
+                    <img src="./icons/bt_add_to_cart.svg" alt="">
+                </figure>
+            </div>
+        </div>
+*/
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        // product= {name, price, image} -> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+
+renderProducts (productList);
