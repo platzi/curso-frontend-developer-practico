c511255356a050540eaa386244ba1a8e41f46b7d
Pabu <pabu@MacBook-Pro-de-Pabu.local>
2023-06-16 09:55:01 -0400

    Integracion navBar y menuMail

---
+++ main.js
@@ -0,0 +1,11 @@
+alert('hola')
+
+const navbarEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+navbarEmail.addEventListener('click', toggleDesktopMenu)
+
+function toggleDesktopMenu (){
+    desktopMenu.classList.toggle('inactive')
+}
+
