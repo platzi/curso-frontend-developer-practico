9e7a67c67de63e47f9eea3a522fbbb6f66f13730
Pabu <pabu@MacBook-Pro-de-Pabu.local>
2023-06-16 10:34:58 -0400

    Integracion Menu-mobile

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-alert('hola')
+//alert('hola')
 
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-navbarEmail.addEventListener('click', toggleDesktopMenu)
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+navbarEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu (){
+    mobileMenu.classList.toggle('inactive')
 }
 
