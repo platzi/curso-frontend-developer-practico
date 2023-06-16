a478ec90336ab31d0b657cbfe03f1d6c326377b8
Oscar980817 <oscar980817@hotmail.com>
2023-06-15 19:17:00 -0500

    Vista principal temrinada

--- main.js
+++ main.js
@@ -3,33 +3,39 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');   // para cuando se le de click
 const mobileMenu = document.querySelector('.mobile-menu'); //para que aparezca y no
 const menuCarIcon = document.querySelector('.navbar-shopping-cart'); //para que aparezca y no
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarIcon.addEventListener('click', toggleCarritoAside);
-
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside)
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
+
     mobileMenu.classList.toggle('inactive');
+
+    closeProductDetailAside();
 }
 
 function toggleCarritoAside(){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
 
     if(!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
@@ -39,8 +45,34 @@ function toggleCarritoAside(){
         desktopMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
+    }
+
+    shoppingCartContainer.classList.toggle('inactive');
 }
+
+function openProductDetailAside(){
+    // const isShoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
+
+    // if(!isShoppingCartContainerClosed){
+    //     shoppingCartContainer.classList.add('inactive');
+    // }  podriamos hacer esto o ...
+    // lo de abajo ya que sabemos que queremos abrir si fueran toogle es abrir y cerrar y si tocaria el condicional pero como no es asi se puede agregar el inactive d euna vez
+
+    shoppingCartContainer.classList.add('inactive');
+
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
+}
+
+ 
+
+
+closeProductDetailAside
 
 const productList = []; 
 productList.push({
@@ -52,7 +84,7 @@ productList.push({
     name : "Pantalla",
     price : 220,
     image : "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-});
+});-
 productList.push({
     name : "Computador",
     price : 620,
@@ -66,6 +98,7 @@ function renderProducts(arr){
     
         const img = document.createElement('img');
         img.setAttribute('src',product.image);
+        img.addEventListener('click',openProductDetailAside)
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -92,7 +125,7 @@ function renderProducts(arr){
         cardsContainer.appendChild(productCard);
     }
 }
-
+ 
 renderProducts(productList);
 
 
