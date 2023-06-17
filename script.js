41d45a2c6e9eafc44bf0cbf3b0c11d2d1f5c5e90
AndresRey <reyandres015@gmail.com>
2023-06-17 10:00:19 -0500

    Productos de Carro de compras

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ const email = document.querySelector(".navbar-email a");
 const mobile_menu = document.querySelector(".mobile-menu");
 const icon_menu = document.querySelector(".menu");
 
+const shopping_car = document.querySelector(".navbar-shopping-cart");
+const products_car = document.querySelector(".product-detail");
+const return_products_car = document.querySelector(".title-container img");
+
 email.addEventListener("click", toggleDesktopUser);
 icon_menu.addEventListener("click", toggleMobileMenu);
+
+shopping_car.addEventListener("click", toggleProductsCar);
+return_products_car.addEventListener("click", toggleProductsCar);
 
 function toggleDesktopUser(event) {
   event.preventDefault();
@@ -14,4 +21,8 @@ function toggleDesktopUser(event) {
 
 function toggleMobileMenu() {
   mobile_menu.classList.toggle("inactive");
+}
+
+function toggleProductsCar() {
+  products_car.classList.toggle("inactive");
 }
