cf2175ece30bb51c4a9b3d2ec8460d1aa263ad96
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2023-06-17 13:00:59 +0200

    Html a partir de arrays

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamBtn = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartBtn = document.querySelector('.navbar-shopping-cart');
 const cartMenu = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click' , toggleDesktopMenu);
@@ -38,3 +39,60 @@ function toggleCartMenu(){
     }
     cartMenu.classList.toggle('inactive');
  }
+
+ const productList = [];
+ productList.push({
+    nombre: 'Bike',
+    precio: 120,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+ });
+
+ productList.push({
+    nombre: 'Pantalla',
+    precio: 100,
+    imagen: 'https://acf.geeknetic.es/imgri/imagenes/auto/22/02/17/rqw-windowscolor.jpg?f=webp'
+ });
+ 
+ productList.push({
+    nombre: 'Laptop',
+    precio: 80,
+    imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGTLMQw6pkEeQ6GrI67s46ztTfQ76RIPoaCQ&usqp=CAU'
+ });
+
+ renderproducts(productList);
+ 
+
+ function renderproducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const img = document.createElement('img');
+        img.setAttribute('src' , product.imagen);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const precio = document.createElement('p');
+        precio.innerText = '$' + product.precio;
+    
+        const nombre= document.createElement('p');
+        nombre.innerText = product.nombre;
+    
+        const figure = document.createElement('figure');
+        
+        const icono = document.createElement('img');
+        icono.setAttribute('src' , './icons/bt_add_to_cart.svg');
+    
+        figure.append(icono);
+        productInfoDiv.append(precio,nombre);
+        productInfo.append(productInfoDiv , figure);
+        productCard.append(img , productInfo);
+    
+        cardsContainer.append(productCard);
+    
+     }
+ }
+
