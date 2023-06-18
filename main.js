9785b76ee518399013ac826cf004f9f2ff409a0b
Johan Sanabria <jjsanabria.s04@gmail.com>
2023-06-18 13:38:29 -0500

    Agregue menu mobile con mouseenter y mouseleave

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const navbarEmail =document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu );
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+menu.addEventListener('mouseenter', removeMobileMenu );
+mobileMenu.addEventListener('mouseleave', addMobileMenu );
+
+function removeMobileMenu(){
+    console.log('entro el mouse')
+    mobileMenu.classList.remove('inactive');
+}
+function addMobileMenu(){
+    console.log('salio del mouse')
+    mobileMenu.classList.add('inactive');
 }
