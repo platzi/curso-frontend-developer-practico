0c7bfbe13d2741858bfcd091d4d6e0f090040192
danielV1213 <daniel_andres.velez@uao.edu.co>
2023-06-19 12:53:31 -0500

    [ADD] Mobile menu functionality

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', () => toggleMenu(desktopMenu))
+menuHamIcon.addEventListener('click', () => toggleMenu(mobileMenu))
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+function toggleMenu(menu) {
+    menu.classList.toggle('inactive')
 }
+
+// Código usado en el curso.
+// function toggleDesktopMenu() {
+//     desktopMenu.classList.toggle('inactive')
+// }
+
+// function toggleMobileMenu() {
+//     mobileMenu.classList.toggle('inactive')
+// }
