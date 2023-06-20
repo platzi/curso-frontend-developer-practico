cbfe1d3a6a640aa1459454fcc138b5d257935c87
arielChacon-arg <arielchacon73@gmail.com>
2023-06-19 21:53:49 -0300

    Fusión de navbar con mobileMenu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector (".navbar-email");
 const deskTopMenu = document.querySelector (".desktop-menu");
+const menuHamIcon = document.querySelector (".menu");
+const mobileMenu = document.querySelector (".mobile-menu");
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+menuHamIcon.addEventListener('click', toogleMobileMenu);
 
 function toogleDesktopMenu () {
     deskTopMenu.classList.toggle('inactive')
+}
+
+function toogleMobileMenu (){
+    mobileMenu.classList.toggle('inactive')
 }
