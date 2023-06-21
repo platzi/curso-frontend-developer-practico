1dbdec2cfc1d6e2786de9ccb8bd7408f7790884e
Maximiliano <maximilianogalindo7@gmail.com>
2023-06-20 22:59:17 -0600

    Menu-Mobile

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
-
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 navbarEmail.addEventListener('click', toggDesktopMenu)
+menuHamIcon.addEventListener('click', toggMobileMenu)
 
 function toggDesktopMenu() { 
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
