198c5242a31beb8d858d90a7d1c7e91700a8d3df
Guadalupe Lima <lupelima67@gmail.com>
2023-06-21 18:04:08 -0300

    fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopMenu= document.querySelector('.desktop-menu');
-
+const menuHamIcon=document.querySelector('.menu');
+const movileMenu=document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu (){
 desktopMenu.classList.toggle ("inactive");
  
-
 }
+
+function toggleMobileMenu (){
+    movileMenu.classList.toggle ("inactive");
+     
+    }
