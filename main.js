7a54ee03e2a11a80403f3e42abd4690044ff4841
tortudev <cesosa8@gmail.com>
2023-06-22 16:44:52 -0400

    add_aside_Event

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('oculto-men-desktop');
@@ -13,7 +15,14 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('oculto-mobile');
+    aside.classList.add('oculto-aside');
+
 }
 
+function toggleCarritoAside() {
+    aside.classList.toggle('oculto-aside');
+    mobileMenu.classList.add('oculto-mobile');
+    
 
+};
 
