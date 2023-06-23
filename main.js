eddc588eebae04d335c86230814d88b0bab818e0
Anuhar Eid <anuhar_eid07@hotmail.com>
2023-06-23 18:51:03 +0900

    agregue index y la primera parte del usuario

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
