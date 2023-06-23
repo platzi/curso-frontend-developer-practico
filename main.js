df9af333fa7cd1315d86e2794d0039dafee80251
Daniel Andres Velez Vargas <daniel.velez@JETOURS.LOCAL>
2023-06-23 17:12:09 -0500

    [ADD] Products List

--- main.js
+++ main.js
@@ -43,3 +43,63 @@ function toggleCarritoAside() {
 
   asideProductDetail.classList.toggle("inactive");
 }
+
+const productList = [];
+
+// La idea de hacerlo así es que en el futuro se pueda llenar con una API.
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Computador Gamer",
+  price: 1000,
+  image:
+    "https://media.cnn.com/api/v1/images/stellar/prod/210910130214-gaming-pcs.jpg?q=x_0,y_0,h_900,w_1599,c_fill/h_720,w_1280",
+});
+
+productList.push({
+  name: "Monitor",
+  price: 250,
+  image: "https://i.blogs.es/19315c/mejores-monitores-gaming/840_560.jpeg",
+});
+
+// Construir el elemento general a partir de JS para que itere sobre todos los productos
+function renderProducts(array) {
+  for (const product of array) {
+    // Constantes y agregar clases
+    const cardsContainer = document.querySelector(".cards-container");
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+    const productInfoInnerDiv = document.createElement("div");
+    const priceParagraph = document.createElement("p");
+    const nameParagraph = document.createElement("p");
+    const productInfoFigure = document.createElement("figure");
+    const figureImg = document.createElement("img");
+    const productImg = document.createElement("img");
+
+    // Modificación de atributos
+    productImg.src = product.image;
+    priceParagraph.innerText = `$${product.price}`;
+    nameParagraph.innerText = product.name;
+    figureImg.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    // Unión de elementos HTML
+    cardsContainer.appendChild(productCard);
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+    productInfo.appendChild(productInfoInnerDiv);
+    productInfo.appendChild(productInfoFigure);
+    productInfoInnerDiv.appendChild(priceParagraph);
+    productInfoInnerDiv.appendChild(nameParagraph);
+    productInfoFigure.appendChild(figureImg);
+  }
+}
+
+renderProducts(productList);
