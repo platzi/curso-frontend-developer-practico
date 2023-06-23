c804b83fa51d94832f4d6c1508ac2511fd0236e1
Daniella Quijada <Daneq152@gmail.com>
2023-06-23 11:23:32 -0400

    Logramos que los menu no chocaran entre si, y agregamos el menu del carrito

--- main.js
+++ main.js
@@ -2,15 +2,37 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const iconCart = document.querySelector('.navbar-shopping-cart');
+const cartMenu = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click', toggleMobileMenu);
+iconCart.addEventListener('click', toggleCartMenu);
+
 
 function toggleDesktopMenu() {
+    const isCartMenuClosed = cartMenu.classList.contains('inactive');
+
+    if (!isCartMenuClosed) {
+        cartMenu.classList.add('inactive');}
+
     desktopMenu.classList.toggle('inactive');
 }
 
-menuHamburguesa.addEventListener('click', toggleMobileMenu);
-
 function toggleMobileMenu() {
+    const isCartMenuClosed = cartMenu.classList.contains('inactive');
+
+    if (!isCartMenuClosed) {
+        cartMenu.classList.add('inactive');}
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartMenu() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');}
+    
+    cartMenu.classList.toggle('inactive');    
 }
