aacf7f921a9bd10da46153d11c53de7bbcb34b07
elvisdavid21 <79238237+elvisdavid21@users.noreply.github.com>
2023-06-27 12:38:42 -0500

    Fusion del menu mobile

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 /*Para seleccionar la clase con document.querySelector se utiliza el punto(.)*/
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const iconHamMenu =document.querySelector('.menu')
 
-menuEmail.addEventListener('click', toggleDesktopmenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+iconHamMenu.addEventListener('click', toggleMobileMenu);
 
-function toggleDesktopmenu() {
+function toggleDesktopMenu() {
     // si inactive está presente la elimina, de lo contrario la añade
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
+}
+
