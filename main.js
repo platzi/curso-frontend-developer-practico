a10aa61b59024d93058d267c5e2902321ebe9c0e
Andres Alarcon <andres.alarcon.fonseca@gmail.com>
2023-06-27 21:49:29 -0500

    Implementación de carrito de compras, con condicionales de acople a componentes de navbar

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const navMenuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 navMenuEmail.addEventListener('click',toggleDesktopMenu);
 menuBurgerIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClose = aside.classList.contains('inactive');
+    if(!isAsideClose){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu(){
+    const isAsideClose = aside.classList.contains('inactive');
+    if(!isAsideClose){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside(){
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
+    const isAsideMenuClose = aside.classList.contains('inactive');
+    if(!isMobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+    if(isAsideMenuClose && !isDesktopMenuClose){
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
