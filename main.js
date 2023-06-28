67d210d2145e9d9a8a0b06bd5a6dc2c37c7e683f
Banda <miguel_bx96@hotmail.com>
2023-06-28 11:02:02 -0500

    Se agrego el carrtio y se corrigio CSS

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuHamIcon = document.querySelector('.menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-const aside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
@@ -13,17 +13,17 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 // Funciones de menus displays
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains('inactive')
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
   if (!isAsideClosed) {
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
   }
   desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains('inactive')
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
   if (!isAsideClosed) {
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
   }
   mobileMenu.classList.toggle('inactive')
 }
@@ -35,7 +35,7 @@ function toggleCarritoAside() {
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive')
   } 
-  aside.classList.toggle('inactive')
+  shoppingCartContainer.classList.toggle('inactive')
 
   if (!isDesktopmenuClose){
     desktopMenu.classList.toggle('inactive')
