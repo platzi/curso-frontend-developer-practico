77f2fbceada03a7b40ee3c3a339c7c7354a62688
Grace Villas <elizabeth.villasmil@urbe.edu>
2023-06-28 09:11:37 -0400

    integrando y resolviendo conflicto de product detail en el html y css

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const navEmail = document.querySelector('.navbar-email'); 
+const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hamMenu = document.querySelector('.hamMenu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCart = document.querySelector('.navbar-shopping-cart');
-const productDetail = document.querySelector('.product-detail')
+const shoppingCartContainer = document.getElementById('shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,42 +11,46 @@ hamMenu.addEventListener('click', toggleMobileMenu);
 shoppingCart.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu() {
-    const isAsideClosed = productDetail.classList.contains('inactive');
-    
-    if (!isAsideClosed) {
-        productDetail.classList.add('inactive');}
+    const isshoppingCartContainerClosed = productDetail.classList.contains('inactive');
+
+    if (!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive');
+    }
 
     desktopMenu.classList.toggle('inactive');
 
-//ASI RESOLVÍ YO EL EJERCICIO
-/*if (desktopMenu.style.display === 'none') {
-    desktopMenu.style.display = 'block';
-}
-else {
-        desktopMenu.style.display = 'none';
-    }*/
+    //ASI RESOLVÍ YO EL EJERCICIO
+    /*if (desktopMenu.style.display === 'none') {
+        desktopMenu.style.display = 'block';
+    }
+    else {
+            desktopMenu.style.display = 'none';
+        }*/
 
 }
 
-function toggleMobileMenu(){
-    const isAsideClosed = productDetail.classList.contains('inactive');
-    
-    if (!isAsideClosed) {
-        productDetail.classList.add('inactive');}
+function toggleMobileMenu() {
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
+
+    if (!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive');
+    }
 
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleShoppingCart (){
+function toggleShoppingCart() {
     const isMenuDesktop = desktopMenu.classList.contains('inactive');
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    
+
     if (!isMenuDesktop) {
-        desktopMenu.classList.add('inactive');}
+        desktopMenu.classList.add('inactive');
+    }
     if (!isMobileMenuClosed) {
-        mobileMenu.classList.add('inactive');}
-    
-    productDetail.classList.toggle('inactive');
+        mobileMenu.classList.add('inactive');
+    }
+
+    shoppingCartContainer.classList.toggle('inactive');
 
 }
 
@@ -86,38 +90,38 @@ productList.push({
 </div>
 */
 
-function renderProducts(arr){
+function renderProducts(arr) {
     for (product of arr) {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
-    
-        const productImg = document.createElement('img'); 
+
+        const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
         //product = {name, price, image} -> product.image
-    
+
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
-        
-        const productInfoDiv = document.createElement('div');    
-    
+
+        const productInfoDiv = document.createElement('div');
+
         const productPrice = document.createElement('p');
         productPrice.innerText = '$' + product.price;
-    
+
         const productName = document.createElement('p');
         productName.innerHTML = product.name
-    
+
         productInfoDiv.append(productPrice, productName);
-    
-        const productInfoFifure = document.createElement('figure'); 
+
+        const productInfoFifure = document.createElement('figure');
         const productImgCart = document.createElement('img');
         productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-    
+
         productInfoFifure.appendChild(productImgCart);
         productInfo.append(productInfoDiv, productInfoFifure);
         productCard.append(productImg, productInfo);
         cardsContainer.appendChild(productCard);
-    
-    
+
+
     }
 }
 
