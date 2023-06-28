190d23be9d1bd9e0c69c7999a2a1927fd23e977d
Jonathan Salcedo <jonatandavidsal@gmail.com>
2023-06-28 16:34:21 -0500

    Add cart menu to index

--- main.js
+++ main.js
@@ -2,14 +2,39 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideOpen = !aside.classList.contains('inactive');
+
+    if (isAsideOpen) {
+        aside.classList.toggle('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideOpen = !aside.classList.contains('inactive');
+
+    if (isAsideOpen) {
+        aside.classList.toggle('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+
+    if (isMobileMenuOpen) {
+        mobileMenu.classList.toggle('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
