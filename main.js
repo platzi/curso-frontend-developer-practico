62d23b0883704423371553dc79385d1e98b842d9
Danluis <danluis2a25@gmail.com>
2023-06-29 18:24:00 -0400

    condicional clase inactive

--- main.js
+++ main.js
@@ -2,16 +2,40 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoMenu);
 
 function toggleDesktopMenu(){
+    const isAsideMenuClosed = aside.classList.contains('inactive')
+
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu(){
-    console.log("hola mundo")
+
+    const isAsideMenuClosed = aside.classList.contains('inactive')
+
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoMenu(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
+
 }
