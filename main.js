62439655a1c06027c1fcc10e4e98d1dd8b49d986
Miguel Negron Garcia <josuefrontend@gmail.com>
2023-06-29 18:04:07 -0400

    Se corrigieron errores de tabulacion en los archivos, se agregaron los menus y aside junto con sus funciones necesarias para que no se puedan tener cada uno de ellos abiertos al mismo tiempo.

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburguer = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+// Experimento
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
-navbarEmail.addEventListener('click', desktopMenuToggle)
+
+shoppingCart.addEventListener('click', productDetailAside);
+
+function productDetailAside() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive-mobile');
+    productDetail.classList.toggle('inactive-aside')
+}
+// Experimento
+//Arriba 
+
+
+navbarEmail.addEventListener('click', desktopMenuToggle);
+menuHamburguer.addEventListener('click', mobileMenuToggle);
 
 
 function desktopMenuToggle() {
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive-aside')
+
 }
+function mobileMenuToggle() {
+    mobileMenu.classList.toggle('inactive-mobile');
+    productDetail.classList.add('inactive-aside')
+
+}
+
