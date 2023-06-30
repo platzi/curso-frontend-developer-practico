52d6c9ba9724ba3370bb3dc73ab860a53d4b2bbf
alcantaraagil <galcantaraa1600@alumno.ipn.mx>
2023-06-30 11:37:03 -0600

    interact elements, open the product detail and changing about the product clicked

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon= document.querySelector('.navbar-shopping-cart');
 const productDetailCloseIcon = document.querySelector('.product-detail-close');
+/**/
+const imagen = document.querySelector("#img_product");
+const priceProduct = document.querySelector("#prudct_price");
+
 
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
@@ -63,13 +67,19 @@ function toggleCarritoAside(){
     shoppingCartContainer.classList.toggle('inactive');
 }
 
-function openProductDetailAside(){
-    shoppingCartContainer.classList.add('inactive');
-    productDetailContainer.classList.remove('inactive');
+function openProductDetailAside(event){
+    shoppingCartContainer.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+    productDetailContainer.classList.remove("inactive");
+    desktopMenu.classList.add("inactive");
+    console.log(event)
+    imagen.setAttribute("src", event.target.src);
+    priceProduct.innerText = event.target.nextElementSibling.innerText; 
 }
 function closeProductDetailAside(){
     productDetailContainer.classList.add('inactive');
 }
+
 
 const productList = [];
 productList.push({
@@ -80,29 +90,27 @@ productList.push({
 productList.push({
     name: 'Ball',
     price: 25,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image:'https://images.unsplash.com/photo-1516567727245-ad8c68f3ec93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=449&q=80'
 });
 productList.push({
     name: 'TV',
     price: 220,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image:'https://m.media-amazon.com/images/I/51puug1pU3L._AC_UF700,800_QL80_.jpg'
 });
 productList.push({
-    name: 'Jeans',
-    price: 40,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    name: 'Iphone',
+    price: 790,
+    image:'https://i5.walmartimages.com.mx/mg/gm/3pp/asr/22a682ad-7638-4e7c-bfc9-b9e605ac0e29.9088ecff021979252b5ef1b151315500.jpeg?odnHeight=2000&odnWidth=2000&odnBg=ffffff'
 });
 productList.push({
     name: 'Laptop',
-    price: 240,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    price: 40,
+    image:'https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=868&q=80'
 });
-
-
 productList.push({
     name: 'Mouse',
     price: 130,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image:'https://images.unsplash.com/photo-1605773527852-c546a8584ea3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80'
 });
 
 /* <div class="product-card">
@@ -125,8 +133,8 @@ function renderProducts(arr){
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src',product.image);
-        productImg.addEventListener('click',openProductDetailAside);
-
+        productImg.setAttribute("id", "imagen");
+        productImg.addEventListener("click", openProductDetailAside);
 
     
         const productInfo = document.createElement('div');
