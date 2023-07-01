04e7f63e626c3c88f935123fcb3996c0e243db65
sara segura <sarasegura224@gmail.com>
2023-07-01 16:18:38 -0500

    [FEAT[ Se agrego detalles del producto

--- main.js
+++ main.js
@@ -3,41 +3,43 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const burguerMenu = document.querySelector(".menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burguerMenu.addEventListener("click", toggleMobileMenu);
-menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+menuCarritoIcon.addEventListener("click", toggleCarritoshoppingCartContainer);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isshoppingCartContainerClosed =
+    shoppingCartContainer.classList.contains("inactive");
 
-  if (!isAsideClosed) {
-    aside.classList.add("inactive");
+  if (!isshoppingCartContainerClosed) {
+    shoppingCartContainer.classList.add("inactive");
   }
 
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isshoppingCartContainerClosed =
+    shoppingCartContainer.classList.contains("inactive");
 
-  if (!isAsideClosed) {
-    aside.classList.add("inactive");
+  if (!isshoppingCartContainerClosed) {
+    shoppingCartContainer.classList.add("inactive");
   }
 
   mobileMenu.classList.toggle("inactive");
 }
 
-function toggleCarritoAside() {
+function toggleCarritoshoppingCartContainer() {
   const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
 
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add("inactive");
   }
 
-  aside.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
