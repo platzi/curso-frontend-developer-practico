c0ce17770026c698279e09de995a3ad2e193f8b8
Danluis <danluis2a25@gmail.com>
2023-07-03 14:05:33 -0400

    interaccion entre componentes

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
+const productDetailContainer = document.querySelector('#productDetail')
 const cardsContainer = document.querySelector('.cards-container')
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
 menuCarritoIcon.addEventListener('click',toggleCarritoMenu);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
+
 
 function toggleDesktopMenu(){
     const isshoppingCartContainerMenuClosed = shoppingCartContainer.classList.contains('inactive')
@@ -28,19 +32,35 @@ function toggleMobileMenu(){
         shoppingCartContainer.classList.add('inactive')
     }
     mobileMenu.classList.toggle('inactive')
+    closeProductDetailAside();
 }
 
 function toggleCarritoMenu(){
+
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
     
     if(!isMobileMenuClosed){
         mobileMenu.classList.add('inactive')
     }
 
-    shoppingCartContainer.classList.toggle('inactive')
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
+    
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive')
+    }
 
+    shoppingCartContainer.classList.toggle('inactive')
 }
 
+function openProductDetailAside(){
+
+    shoppingCartContainer.classList.add("inactive")
+
+    productDetailContainer.classList.remove("inactive")
+}
+function closeProductDetailAside(){
+    productDetailContainer.classList.add("inactive")
+}
 const productList = [];
 productList.push ({
     name:'Bike',
@@ -96,6 +116,7 @@ function renderProducts(arr){
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src',product.image);
+        productImg.addEventListener('click', openProductDetailAside)
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
