6adcff59612b15f4b305a89d58326076b8d333a3
nico <ngarcia_85@hotmail.com.ar>
2023-07-04 19:42:36 -0300

    se agrega menu mobile

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+const menu = document.querySelector('.menu')
+const mobileMenu =document.querySelector('.mobile-menu')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+
+menu.addEventListener('click', toggleMenu)
+
+function toggleMenu(){
+
+    mobileMenu.classList.toggle("inactiv")
+
+}
 
 function toggleDesktopMenu(){
 
