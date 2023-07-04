d906ee6fd1894c2d11560f2028dbe032bb857178
Steveen-u <s.restrepo3@utp.edu.co>
2023-07-04 15:25:14 -0500

    fixed media query for max and min, add mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menu_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
+const menu = document.querySelector('.menu');
+const mobile = document.querySelector('.mobile-menu');
 
 menu_email.addEventListener('click', toggleDesktopMenu);
+menu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktop_menu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobile.classList.toggle('inactive');
 }
