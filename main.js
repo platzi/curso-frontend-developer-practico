ba2c5112152f4e5f391c41e82187d38739755607
Joel014 <joelrs.14@hotmail.com>
2023-07-04 20:03:31 -0400

    productDetail agregado y funcionando

--- main.js
+++ main.js
@@ -2,28 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const productDetailContainer = document.querySelector('.product-detail-secondary');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu(){
   desktopMenu.classList.toggle('inactive');
   aside.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
 }
 
 function toggleMobileMenu(){
   mobileMenu.classList.toggle('inactive');
+  productDetailContainer.classList.add('inactive');
   aside.classList.add('inactive');
 }
 
 function toggleCarritoAside(){
-  mobileMenu.classList.add('inactive')
-  desktopMenu.classList.add('inactive')
+  mobileMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
   aside.classList.toggle('inactive');
+}
+
+function openProductDetailAside() {
+  productDetailContainer.classList.remove('inactive');
+  aside.classList.add('inactive');
+  desktopMenu.classList.toggle('inactive');
+}
+
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -45,6 +61,7 @@ productList.push({
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
+
 function renderPrducts(arr) {
   for (product of arr) {
     const productCard = document.createElement('div');
@@ -53,6 +70,7 @@ function renderPrducts(arr) {
     //product = {name, price, image} -> product.image
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside)
   
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
