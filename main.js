af4fa013fb9afa3135da8b177ef6302cc4f3f767
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-07-06 16:07:45 -0400

    add car shopping functions

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
 const menuEmailMobile = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuEmailMobile.addEventListener("click", toggleMobileMenu);
+menuCarIcon.addEventListener("click", toggleCarAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle("inactive");
 }
+
+function toggleCarAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }
+    
+    aside.classList.toggle("inactive");
+    
+    const isDesktopClosed = desktopMenu.classList.contains("inactive");
+
+    if (!isDesktopClosed) {
+        desktopMenu.classList.add("inactive");
+    }
+}
+
+
