97469f49a605eb96c66f85d2a360a5473f893cbf
Felipe <felipecortes0604@gmail.com>
2023-07-07 10:26:21 -0500

    clase 19

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const navEmail= document.querySelector(".navbar-email");
-const desktopMenu= document.querySelector(".desktop-menu")
+const desktopMenu= document.querySelector(".desktop-menu");
+const menuHamIcon= document.querySelector(".menu")
+const mobileMenu= document.querySelector(".mobile-menu")
 
 navEmail.addEventListener('click', toggleDescktopMenu)
+menuHamIcon.addEventListener('click', toggleMovileMenu)
 
 function toggleDescktopMenu(){
     desktopMenu.classList.toggle("inactive")
+}
+function toggleMovileMenu(){
+    mobileMenu.classList.toggle("inactive")
 }
