195097dafbabd5ed916c1f75868219f59046dd23
sara segura <sarasegura224@gmail.com>
2023-07-07 12:00:10 -0500

    [FEAT]Se modificaros los estilos del diseño.

--- main.js
+++ main.js
@@ -64,55 +64,55 @@ function closeProductDetailAside() {
 
 const productList = [];
 productList.push({
-  name: "Bike",
-  price: 12700,
+  name: "Tan skin swimsuit",
+  price: 100,
   image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://mipielcanela.com/cdn/shop/files/colombian-manufacturer-one-piece-medellin-bogota.jpg?v=1673476864",
 });
 productList.push({
-  name: "Bicycle helmet",
-  price: 1200,
-  image:
-    "https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto",
+  name: "Ivory white swimsuit",
+  price: 200,
+  image: "https://tendencybook.com/wp-content/uploads/2020/05/mitos-2.jpg",
 });
 productList.push({
-  name: "Bicycle helmet",
+  name: "Two pieces black",
   price: 1600,
-  image: "https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg",
-});
-productList.push({
-  name: "Bicycle helmet",
-  price: 1500,
   image:
-    "https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto",
+    "https://img104.urbanic.com/goods-pic/0b70685db1ef402cadb118c002e29327_w1000_q90",
 });
 productList.push({
-  name: "Seat",
+  name: "Two pieces white",
+  price: 1500,
+  image: "https://tendencybook.com/wp-content/uploads/2020/05/mitos-1.jpg",
+});
+productList.push({
+  name: "two pieces blue waves",
   price: 300,
-  image: "https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg",
+  image: "https://m.media-amazon.com/images/I/71je88S3ryL._AC_SL1500_.jpg",
 });
 productList.push({
-  name: "Tennis Montain Bike",
+  name: "strawberries",
   price: 2200,
   image:
-    "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg",
+    "https://http2.mlstatic.com/D_NQ_NP_780908-MLA52848717715_122022-O.webp",
 });
 productList.push({
-  name: "Sunglasses",
+  name: "two piece print",
   price: 800,
   image:
-    "https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602",
+    "https://www.glamesence.com/cdn/shop/products/29_1_1024x.png?v=1643741718",
 });
 productList.push({
-  name: "Sunglasses",
+  name: "bicolor",
   price: 600,
   image:
-    "https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603",
+    "https://www.likeme.com.co/media/catalog/product/r/h/rh596086_1_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=600&width=400&canvas=400:600",
 });
 productList.push({
-  name: "Bicycle seat bag",
+  name: "two pieces yellow",
   price: 876,
-  image: "https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg",
+  image:
+    "https://www.likeme.com.co/media/catalog/product/p/a/pacifika598573_1_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=600&width=400&canvas=400:600",
 });
 
 /*<div class="product-card">
