56defd3e393ee4a58405eccc03386f3039a5783f
Erin Carmona <carmona.erin.u@gmail.com>
2023-07-07 20:32:21 -0600

    Se agrego la clase 12

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const burgerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -12,20 +12,20 @@ menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains("inactive");
+    const isShoppingCartContainerClosed = shoppingCartContainer.classList.contains("inactive");
 
-    if (!isAsideClosed) {
-        aside.classList.add("inactive");
+    if (!isShoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add("inactive");
     } 
 
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains("inactive");
+    const isShoppingCartContainerClosed = shoppingCartContainer.classList.contains("inactive");
 
-    if (!isAsideClosed) {
-        aside.classList.add("inactive");
+    if (!isShoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add("inactive");
     } 
 
     mobileMenu.classList.toggle("inactive");
@@ -42,7 +42,7 @@ function toggleCarritoAside() {
         desktopMenu.classList.add("inactive");
     
 
-    aside.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 
 }
 
