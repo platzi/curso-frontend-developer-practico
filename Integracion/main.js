0beb2abb9e71bf95aeb45dda87e5ef9deb62b61e
Santiago Ferrer <santiago1ferrer@gmail.com>
2023-07-10 18:33:56 -0500

    Integración del menu mobile y corrección de bug del menu desktop al reducir

--- Integracion/main.js
+++ Integracion/main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuBars = document.querySelector(".menu");
+const menuMobile = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBars.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
-    console.log('Click en Menu en el Email');
+    // console.log('Click en Menu en el Email');
     desktopMenu.classList.toggle('inactive');
+};
+
+function toggleMobileMenu() {
+    // console.log('Click en Menu Mobile');
+    menuMobile.classList.toggle('inactive');
 };
