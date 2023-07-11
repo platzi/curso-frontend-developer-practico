5908c16f47a4b5ce2f18429734db235269573e62
Santiago Ferrer <santiago1ferrer@gmail.com>
2023-07-11 11:27:51 -0500

    Creación de lista de prodcutos con JS dinamica

--- Integracion/main.js
+++ Integracion/main.js
@@ -4,6 +4,8 @@ const menuBars = document.querySelector(".menu");
 const menuMobile = document.querySelector(".mobile-menu");
 const cartIcon = document.querySelector(".navbar-shopping-cart");
 const menuCart = document.querySelector(".product-detail");
+const cardContainer = document.querySelector(".card-container");
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBars.addEventListener('click', toggleMobileMenu);
@@ -41,4 +43,81 @@ function toggleCartMenu() {
     }
 
     menuCart.classList.toggle('inactive');
+};
+
+
+const productList = []; //Esto es lo que traeria al hacer una consulta API Rest a la BD
+
+//Llenamos el array manual mientras
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/13738406/pexels-photo-13738406.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: 625,
+    image: 'https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+productList.push({
+    name: 'Camara',
+    price: 1200,
+    image: 'https://images.pexels.com/photos/17436320/pexels-photo-17436320/free-photo-of-camara-fotografia-tecnologia-dispositivo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+productList.push({
+    name: 'Iphone',
+    price: 1900,
+    image: 'https://images.pexels.com/photos/1647976/pexels-photo-1647976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+
+function renderProducts(arr){
+//Este ciclo recorre el array de productos y crea el HTML para cada uno
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        //Cada iteración: product = {name, price, image}
+        productImg.setAttribute('src',product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', '/icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardContainer.appendChild(productCard);
+
+    };
 }
+
+//de esta forma no se carga automatico sino cuando yo quiera llamar la función: un click, hover...
+renderProducts(productList);
+
+
+
