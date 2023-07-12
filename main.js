5915c4882ce4907498f33e5f88dd901b26d92e79
Erin Carmona <carmona.erin.u@gmail.com>
2023-07-11 21:28:27 -0600

    Se resuelven conflictos con los menus

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const burgerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const productDetailIcon = document.querySelector(".product-detail-close")
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
-const cardsContainer = document.querySelector(".cards-container")
+const productDetailContainer = document.querySelector("#productDetail");
+
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burgerIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+productDetailIcon.addEventListener("click", closeProductDetailAside);
+
 
 
 function toggleDesktopMenu() {
@@ -28,6 +33,8 @@ function toggleMobileMenu() {
         shoppingCartContainer.classList.add("inactive");
     } 
 
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle("inactive");
 };
 
@@ -41,10 +48,25 @@ function toggleCarritoAside() {
     } else (!isDesktopMenuClosed)
         desktopMenu.classList.add("inactive");
     
+    const isProductDetailClosed = productDetailContainer.classList.contains("inactive");
 
+    
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add("inactive"); 
+    }
     shoppingCartContainer.classList.toggle("inactive");
 
-}
+};
+
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add("inactive")
+    
+    productDetailContainer.classList.remove("inactive")
+};
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add("inactive")
+};
 
 const productList = [];
 productList.push({
@@ -70,6 +92,7 @@ function renderProducts(arr) {
     
         const productImg = document.createElement("img");
         productImg.setAttribute("src", product.image);
+        productImg.addEventListener("click", openProductDetailAside);
         
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
