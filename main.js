dd3ca39a621e5f085b9c67c251b204397b193e43
F. Michael Acosta <acostamichael80@gmail.com>
2023-07-13 17:36:58 -0500

    Creacion de un array con html y maquetacion de JS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');//
 const mobileMenu = document.querySelector('.mobile-menu');//para conectar con el menu mobile
 const desktopMenu = document.querySelector('.desktop-menu');//para conectar con el menu desktop
 const aside = document.querySelector('.product-detail');//
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -41,6 +42,100 @@ function toggleCarritoAside(){
     if(!isdesktopMenuClosed){
         desktopMenu.classList.add('inactive');
     }
-    
     aside.classList.toggle('inactive');//toggle -> permite intercambiar
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Computer',
+    price: 567,
+    image: 'https://img.global.news.samsung.com/pe/wp-content/uploads/2018/05/224-1024x683.jpg'
+});
+productList.push({
+    name: 'Monitor',
+    price: 230,
+    image: 'https://es.digitaltrends.com/wp-content/uploads/2018/07/que-monitor-comprar-feat.jpg'
+});
+productList.push({
+    name: 'Keys',
+    price: 89,
+    image: 'https://www.shutterstock.com/image-vector/vector-black-pc-keyboard-very-260nw-1026742981.jpg'
+});
+productList.push({
+    name: 'Camara',
+    price: 699,
+    image: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/110CC/production/_90463896_ccc.jpg'
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Computer',
+    price: 567,
+    image: 'https://img.global.news.samsung.com/pe/wp-content/uploads/2018/05/224-1024x683.jpg'
+});
+productList.push({
+    name: 'Monitor',
+    price: 230,
+    image: 'https://es.digitaltrends.com/wp-content/uploads/2018/07/que-monitor-comprar-feat.jpg'
+});
+productList.push({
+    name: 'Keys',
+    price: 89,
+    image: 'https://www.shutterstock.com/image-vector/vector-black-pc-keyboard-very-260nw-1026742981.jpg'
+});
+productList.push({
+    name: 'Camara',
+    price: 699,
+    image: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/110CC/production/_90463896_ccc.jpg'
+});
+
+function renderProducts(arreglo){
+    for (product of arreglo) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src',product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
+
+
+
+
