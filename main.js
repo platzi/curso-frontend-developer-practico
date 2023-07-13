6eb7e1d921619e1dd5f64643b3dd7dc9233a666e
F. Michael Acosta <acostamichael80@gmail.com>
2023-07-13 12:03:27 -0500

    fusion del elemento menu mobile al menu de la barra de navegacion

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector('.navbar-email');//para conectar con el elemento del email de barra de navegacion
+const desktopMenu = document.querySelector('.desktop-menu');//para conectar con el menu desktop
+const menuHamburguerIcon = document.querySelector('.menu');//para conectar con el elemento sanduche dde barra de navegacion
+const mobileMenu = document.querySelector('.mobile-menu');//para conectar con el menu mobile
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     console.log('clickeando');
     desktopMenu.classList.toggle('inactive');//toggle -> permite intercambiar
+}
+function toggleMobileMenu(){
+    console.log('clickeando');
+    mobileMenu.classList.toggle('inactive');//toggle -> permite intercambiar
 }
