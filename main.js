1283f1abae22a252aeb0380ec952b034bd7d7c36
F. Michael Acosta <acostamichael80@gmail.com>
2023-07-14 15:44:42 -0500

    codigo JS para abrir o cerrar el detalle del product

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ const mobileMenu = document.querySelector('.mobile-menu');//para conectar con el
 const desktopMenu = document.querySelector('.desktop-menu');//para conectar con el menu desktop
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');//
 const cardsContainer = document.querySelector('.cards-container');
-
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguerIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 
 function toggleDesktopMenu(){
@@ -28,6 +30,8 @@ function toggleMobileMenu(){
         shoppingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');//toggle -> permite intercambiar
+
+    closeProductDetailAside();
 }
 
 
@@ -38,11 +42,25 @@ function toggleCarritoAside(){
         mobileMenu.classList.add('inactive');
     }
 
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    if (!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
+    }
+
     const isdesktopMenuClosed = desktopMenu.classList.contains('inactive');
     if(!isdesktopMenuClosed){
         desktopMenu.classList.add('inactive');
     }
     shoppingCartContainer.classList.toggle('inactive');//toggle -> permite intercambiar
+}
+
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -105,6 +123,7 @@ function renderProducts(arreglo){
 
         const productImg = document.createElement('img');
         productImg.setAttribute('src',product.image);
+        productImg.addEventListener('click',openProductDetailAside);
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
