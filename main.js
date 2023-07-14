1267f483287fecd47bbd4d65941b4c9eed74c10b
Juan Zabala <zabala1juan@gmail.com>
2023-07-14 19:22:54 -0400

    002 - Integracion Menu principal con menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector( '.desktop-menu');
+const menuHamIcon = document.querySelector( '.menu');
+const mobileMenu = document.querySelector( '.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     console.log('click email');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    console.log('click menu Ham icon');
+    mobileMenu.classList.toggle('inactive');
 }
