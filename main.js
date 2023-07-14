0b165a0439979807a03e65ce9219c3490ec1a737
pruiz5514 <pablo_ruiza@hotmail.com>
2023-07-14 15:43:43 -0700

    Se agrego menu del carrito

--- main.js
+++ main.js
@@ -2,15 +2,46 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoMenu);
 
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle("inactive")
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive")
+    }
+
+    desktopMenu.classList.toggle("inactive");
+    
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle("inactive")
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive")
+    }
+    mobileMenu.classList.toggle("inactive");
 }
+
+function toggleCarritoMenu(){
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isdesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive");
+    }
+    
+    if(!isdesktopMenuClosed){
+        desktopMenu.classList.add("inactive");
+    }
+
+
+    aside.classList.toggle("inactive");
+ }
