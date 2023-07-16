acba37ba4d983fa71f3ad070fcfe25b737d482ff
Santiago Forero <toxicthrashd@gmail.com>
2023-07-16 17:28:42 -0500

    Maquetacion menus y product cards con array

--- main.js
+++ main.js
@@ -1,8 +1,122 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenuIcon = document.querySelector('.menu');
+const menucarritoIcon = document.querySelector('.navbar-shopping-cart');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenuIcon.addEventListener('click', toggleMobileMenu);
+menucarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }    
+
     desktopMenu.classList.toggle('inactive');
 };
+
+function toggleMobileMenu() {
+     
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }     
+    mobileMenu.classList.toggle('inactive');
+};
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopClosed = desktopMenu.classList.contains('inactive');
+     
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }   
+    if(!isDesktopClosed){
+        desktopMenu.classList.add('inactive');
+    }    
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Laptop',
+    price: 630,
+    image: 'https://hogarymoda.com.co/wp-content/uploads/2023/02/Computador-portatil-HP-14-14dq0500la.jpg',
+});
+productList.push({
+    name: 'cellphone',
+    price: 420,
+    image: 'https://panamericana.vtexassets.com/arquivos/ids/443070/celular-motorola-g22-pau50023co-ram-4-gb-128-gb-azul-3-840023229459.jpg?v=637908395678100000',
+});
+productList.push({
+    name: 'Table',
+    price: 299,
+    image: 'https://disegnimobili.com.ar/wp-content/uploads/2018/05/WhatsApp-Image-2022-10-01-at-9.15.29-AM.jpeg',
+});
+
+/* <div class="product-card">
+      <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+      <div class="product-info">
+        <div>
+          <p>$120,00</p>
+          <p>Bike</p>
+        </div>
+        <figure>
+          <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+      </div>
+    </div>
+*/
+
+function renderProducts(array) {
+    for (product of array) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        //product={name, price, image} ->product. image
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.append(productPrice, productName);
+        
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart); 
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
