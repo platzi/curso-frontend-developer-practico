5ddcf8a3ced13cb8595a838ad028742607097695
Miguel Ciavato <miguelciavato@gmail.com>
2023-07-18 16:05:08 -0400

    Añadido el menu en la version mobile

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let desktopMenu = document.querySelector(".desktop-menu");
 let navBarEmail = document.querySelector(".navbar-email");
+let mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+let mobileMenu = document.querySelector(".mobile-menu");
 
-navBarEmail.addEventListener('click',toggleDesktop);
+navBarEmail.addEventListener('click',toggleDesktopMenu);
+mobileMenuIcon.addEventListener('click', toggleMobileMenu)
 
-function toggleDesktop(){
+function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
