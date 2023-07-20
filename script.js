9b39f84d4f94e3877eeee2281a0eeec15e3c4485
Miguel Ciavato <miguelciavato@gmail.com>
2023-07-20 13:42:46 -0400

    Añadido Menú de detalles de producto

--- script.js
+++ script.js
@@ -3,38 +3,43 @@ let navBarEmail = document.querySelector(".navbar-email");
 let mobileMenuIcon = document.querySelector(".mobile-menu-icon");
 let mobileMenu = document.querySelector(".mobile-menu");
 let shoppingCart = document.querySelector(".navbar-shopping-cart");
+let cartProductsDetail = document.querySelector(".cart-products-detail");
 let productDetail = document.querySelector(".product-detail");
+let productDetailClose = document.querySelector(".product-detail-close");
 
 navBarEmail.addEventListener('click',toggleDesktopMenu);
 mobileMenuIcon.addEventListener('click', toggleMobileMenu);
 shoppingCart.addEventListener('click', toggleShoppingCart);
+productDetailClose.addEventListener('click',closeProductDetails);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
-
-    if(!productDetail.classList.contains('inactive')){
-        productDetail.classList.add('inactive');
-    }
+    cartProductsDetail.classList.add('inactive');
+    productDetail.classList.add('inactive');
 }
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
-
-    if(!productDetail.classList.contains('inactive')){
-        productDetail.classList.add('inactive');
-    }
+    cartProductsDetail.classList.add('inactive');
+    productDetail.classList.add('inactive');
 }
 
 function toggleShoppingCart(){
+    cartProductsDetail.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetail.classList.add('inactive');
+}
+
+function openProductDetails(){
     productDetail.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    cartProductsDetail.classList.add('inactive');
+}
 
-    if(!mobileMenu.classList.contains('inactive')){
-        mobileMenu.classList.add('inactive');
-    }
-
-    if(!desktopMenu.classList.contains('inactive')){
-        desktopMenu.classList.add('inactive');
-    }
+function closeProductDetails(){
+    productDetail.classList.add('inactive');
 }
 
 let products = [];
@@ -47,7 +52,7 @@ products.push({
 
 products.push({
     img : "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    price : 120,
+    price : 150,
     name : 'Bike'
 })
 
@@ -55,12 +60,13 @@ function generateProductCards(arr){
     
     let cardsContainer = document.querySelector('.cards-container');
 
-    for(product of arr){
+    for(let product of arr){
         let productCard = document.createElement('div');
         productCard.classList.add('product-card');
 
         let productImage = document.createElement('img');
         productImage.setAttribute('src', product.img);
+        productImage.addEventListener('click', openProductDetails);
 
         let productInfo = document.createElement('div');
         productInfo.classList.add("product-info");
@@ -85,3 +91,4 @@ function generateProductCards(arr){
 }
 
 generateProductCards(products);
+
