affda1c5b2ddd27d6fc2eadc6faabde2b7d4944f
Roy Rodriguez <rrodriguezmesia@gmail.com>
2023-07-29 22:28:17 -0500

    Se termino con el navbar y estamos en carrito de compras

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const menuBurger = document.querySelector('.menu')
+const menuMobile = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurger.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
-    menuDesktop.classList.toggle('inactive')
+    menuDesktop.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    menuMobile.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    aside.classList.toggle('inactive')
 }
