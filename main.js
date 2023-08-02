3309aacb18ddd5ec7f689a41fe1ff131f0a8c584
Alejandro Lopez Correa <allopezcorrea@gmail.com>
2023-08-02 12:33:36 -0500

    Add last changes for academic JS platzi course

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const shoppingCartContainer = document.querySelector("#shopping-cart-container")
 
 const cardsContainer = document.querySelector(".cards-container")
 
+const productDetailContainer = document.querySelector("#product-detail")
+
+const productCloseDetail = document.querySelector(".product-detail-close")
+
 navbarEmail.addEventListener("click", toggleMenu)
 
 function toggleMenu(){
@@ -19,9 +23,14 @@ menu.addEventListener("click", toggleMobileMenu)
 
 function toggleMobileMenu(){
   const shoppingCartContainerClass = shoppingCartContainer.classList
+  const productDetailClass = productDetailContainer.classList
 
   if (!shoppingCartContainerClass.contains('inactive')) {
-    shoppingCartContainerClass.toggle('inactive')
+    shoppingCartContainerClass.add('inactive')
+  }
+
+  if (!productDetailClass.contains('inactive')){
+    productDetailClass.add('inactive')
   }
 
   mobileMenu.classList.toggle('inactive')
@@ -31,15 +40,38 @@ shoppingCart.addEventListener("click", toggleShoppingCar)
 
 function toggleShoppingCar(){
   const mobileClass = mobileMenu.classList
+  const productDetailClass = productDetailContainer.classList
 
   if (!mobileClass.contains('inactive')) {
-    mobileClass.toggle('inactive')
+    mobileClass.add('inactive')
+  }
+
+  if (!productDetailClass.contains('inactive')){
+    productDetailClass.add('inactive')
   }
 
   shoppingCartContainer.classList.toggle('inactive')
 }
 
 const productList = [];
+
+productList.push({
+  name: 'Bike',
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  price: '120'
+})
+
+productList.push({
+  name: 'TV',
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  price: '520'
+})
+
+productList.push({
+  name: 'Other',
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  price: '420'
+})
 
 productList.push({
   name: 'Bike',
@@ -66,6 +98,9 @@ function renderProducts (arr){
    
     const img = document.createElement('img')
     img.setAttribute('src', product.img)
+
+    img.addEventListener("click", openProductDetailAside)
+    productCloseDetail  .addEventListener("click", closeProductDetailAside)
    
     const productInfo = document.createElement('div')
     productInfo.classList.add('product-info')
@@ -95,6 +130,25 @@ function renderProducts (arr){
    
     cardsContainer.appendChild(productCard)
    }   
+}
+
+function openProductDetailAside (){
+  const mobileClass = mobileMenu.classList
+  const menuClass = shoppingCartContainer.classList
+
+  if (!mobileClass.contains('inactive')) {
+    mobileClass.add('inactive')
+  }
+
+  if (!menuClass.contains('inactive')){
+    menuClass.add('inactive')
+  }
+
+  productDetailContainer.classList.remove('inactive')
+}
+
+function closeProductDetailAside (){
+  productDetailContainer.classList.add('inactive')
 }
 
 renderProducts(productList)
