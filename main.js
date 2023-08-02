f4893c46ba17e0a04cfbfc2ea2aa8c6db679e5ef
Ferney Reyes <bowl.en5minutos@gmail.com>
2023-08-01 21:59:33 -0500

    Condicionales para que aparezca y desaparezca menu desktop, carrito deompras y menu mobile según corresponda

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 let nav_email = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
+let MovileMenu = document.querySelector(".mobile-menu")
+let menuMovile = document.querySelector(".menu")
+let detalleProducto = document.querySelector(".product-detail")
+let carritoCompras = document.querySelector(".navbar-shopping-cart")
+
+let mostrarcarrito = detalleProducto.classList.contains("inactive")
+let mostrarMenuDesktop = desktopMenu.classList.contains("inactive")
+let mostrarMenuMobile = MovileMenu.classList.contains("inactive")
+
 
 nav_email.addEventListener("click" , aparecer);
 
@@ -7,5 +16,37 @@ nav_email.addEventListener("click" , aparecer);
 
 function aparecer()
 {
-   desktopMenu.classList.toggle("inactive");
+   if(mostrarcarrito)
+   {
+      detalleProducto.classList.add("inactive");
+   } 
+   
+   desktopMenu.classList.toggle("inactive")
 }
+
+menuMovile.addEventListener("click" , aparecerMenu );
+
+function aparecerMenu()
+{
+if(mostrarMenuMobile)
+{
+   detalleProducto.classList.add("inactive");
+}
+
+   MovileMenu.classList.toggle("inactive");
+}
+
+carritoCompras.addEventListener("click" , aparacerDetalleDeCompra)
+
+function aparacerDetalleDeCompra()
+{
+   if(mostrarMenuDesktop)
+   {
+       desktopMenu.classList.add("inactive");
+       MovileMenu.classList.add("inactive");      
+   }
+
+   detalleProducto.classList.toggle("inactive")  
+}
+
+
