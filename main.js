b67f837521e95766f56a1a3b09e623cc0949d63c
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-08-03 08:59:34 +0200

    Toggle desktop-menu, mobile-menu and media query

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ navbarEmail.addEventListener("click", toogleDesktopMenu)
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
 }
+
+const hamburguerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+hamburguerMenu.addEventListener("click", toggleMobileMenu)
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
