936a908580b80892c71a37823f8c975a655f2f8e
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-08-04 00:26:42 +0200

    toggle in menus

--- main.js
+++ main.js
@@ -4,14 +4,35 @@ const desktopMenu = document.querySelector('.desktop-menu')
 navbarEmail.addEventListener("click", toogleDesktopMenu)
 
 function toogleDesktopMenu() {
+    const isAsideMenuOpen = !aside.classList.contains('inactive')
+    if (isAsideMenuOpen) {
+        aside.classList.toggle('inactive')
+    }
     desktopMenu.classList.toggle('inactive')
 }
 
+
 const hamburguerMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-
 hamburguerMenu.addEventListener("click", toggleMobileMenu)
-
 function toggleMobileMenu() {
+    const isAsideMenuOpen = !aside.classList.contains('inactive')
+    if (isAsideMenuOpen) {
+        aside.classList.toggle('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
+}
+
+const navbarShoppingCart = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+navbarShoppingCart.addEventListener("click", toggleCartAside)
+function toggleCartAside() {
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive')
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive')
+    if (isMobileMenuOpen) {
+        toggleMobileMenu()
+    }else if(isDesktopMenuOpen){
+        toogleDesktopMenu()
+    }
+    aside.classList.toggle('inactive')
 }
