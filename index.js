929a3d5ffa0aae6d3828059f4b56320ebcaf3e7c
Jhon Rios <r.jhonf@gmail.com>
2023-08-02 19:19:20 -0500

    starting product details

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-const menuEmail = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
-const menuHamburguer = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
-const cartIcon = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
-const container = document.querySelector('.cards-container')
+const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const cardsContainer = document.querySelector('.cards-container');
 
-const toggleDesktopMenu = () => {
-  aside.classList.add('inactive')
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleDesktopMenu() {
+  shoppingCartContainer.classList.add('inactive')
   desktopMenu.classList.toggle('inactive')
 }
 
-const toggleMobileMenu = () => {
-  aside.classList.add('inactive')
+function toggleMobileMenu() {
+  shoppingCartContainer.classList.add('inactive')
   mobileMenu.classList.toggle('inactive')
 }
 
-const toggleCartAside = () => {
+function toggleCarritoAside() {
   mobileMenu.classList.add('inactive')
   desktopMenu.classList.add('inactive')
-  aside.classList.toggle('inactive')
+  shoppingCartContainer.classList.toggle('inactive')
 }
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
-menuHamburguer.addEventListener('click', toggleMobileMenu)
-cartIcon.addEventListener('click', toggleCartAside)
-
-const productList = []
+const productList = [];
 productList.push({
   name: 'Bike',
   price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
 productList.push({
-  name: 'Smart TV',
-  price: 600,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+  name: 'Pantalla',
+  price: 220,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
 productList.push({
-  name: 'Laptop',
-  price: 1000,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+  name: 'Compu',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
 
-const homeProducts = (list) => {
-  for (product of list) {
+function renderProducts(arr) {
+  for (product of arr) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
   
@@ -77,8 +77,8 @@ const homeProducts = (list) => {
     productCard.appendChild(productImg);
     productCard.appendChild(productInfo);
   
-    container.appendChild(productCard);
+    cardsContainer.appendChild(productCard);
   }
 }
 
-homeProducts(productList)
+renderProducts(productList);
