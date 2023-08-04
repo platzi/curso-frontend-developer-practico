24f92a57cbccbe35244f7f1e2dd444b00409f87b
Angel Sulbaran <angeldv.sulbaran@gmail.com>
2023-08-04 18:25:28 +0200

    menu desktop y mobile listos

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamICon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuHamICon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive')
+}
+
+
+
+function toggleMobileMenu (){
+    mobileMenu.classList.toggle('inactive')
 }
