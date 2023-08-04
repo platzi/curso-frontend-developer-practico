c5eebd6702e3d63131dcde133095d75f971c4073
Alvaro Ojeda <oj.alv99@gmail.com>
2023-08-04 06:59:21 -0300

    fusion mobile menu

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burgerMenuIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
 }
-
-console.log("js funcionando");
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+}
