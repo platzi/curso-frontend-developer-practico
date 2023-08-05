1896d7d591372b0053aec7e77de3be9c42d0e6ba
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-08-04 22:48:40 -0300

    actualizacion viernes 04/08/23

--- main.js
+++ main.js
@@ -10,13 +10,32 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle("inactive");
 }
+
+
 function toggleCarritoAside() {
-    mobileMenu.classList.toggle("inactive");
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
 }
 
