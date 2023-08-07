10f23973fbeb99b20c1f3aeff07e43f80f826dab
Jenni <jennifer.arabel@gmail.com>
2023-08-07 11:00:19 -0300

    Tercera fusion con clase 13 en index.html styles.css main.js y Identación de todas los archivos.

--- main.js
+++ main.js
@@ -4,14 +4,40 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 burgerMenu.addEventListener('click', toggleMobileMenu);
 
+menuCarritoIcon.addEventListener('click', toggleCarrito);
+
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarrito() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
 }
