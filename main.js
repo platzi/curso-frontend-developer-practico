93987504ba9d0ab81f6493ee713b01e521ecd365
daniel mena <dmena044@gmail.com>
2023-08-08 17:28:03 -0600

    Se agrego carrito y funcion de esconder menu de carrio

--- main.js
+++ main.js
@@ -5,5 +5,12 @@
 document.addEventListener("click", e=>{
     if (e.target.matches(".navbar-email")) {
         document.querySelector(".desktop-menu").classList.toggle("active");
+        document.querySelector(".product-detail").classList.add("active")
+
+    }
+    if (e.target.matches(".navbar-shopping-cart *")) {
+        document.querySelector(".product-detail").classList.toggle("active")
+        document.querySelector(".desktop-menu").classList.add("active");
+
     }
 })
