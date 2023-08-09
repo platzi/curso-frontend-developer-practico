b9c216d2109beda00bf3a0fbd2aa0744ba3b2e58
Maria Pulgar <mariabpulgar@gmail.com>
2023-08-09 14:38:58 -0400

    Mobile menu added

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 toggleDesktopMenu()
 
@@ -9,4 +12,8 @@ function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
+toggleMobileMenu()
 
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
