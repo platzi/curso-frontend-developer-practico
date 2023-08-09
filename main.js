8a2b3a264e5676c710464101d48ecdd37b52a825
gonzalopozo <gonzalopozo2005@gmail.com>
2023-08-09 13:58:23 +0200

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuLine = document.querySelector('mobile-menu ul:nth-child(1)');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('active');
+    mobileMenuLine.classList.toggle('active');
 }
