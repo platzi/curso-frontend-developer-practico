ecdb60bdc0148a2e12b76be3e083b1d06593f107
davidArce <davidsteven654@gamil.com>
2023-08-10 18:27:38 -0500

    fusionando lista de productos

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
+const CarIconMenu = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+CarIconMenu.addEventListener('click', toggleCarAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+    
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+function toggleMobileMenu() { 
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+    
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarAside() {
+    const isMobileClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileClosed) {
+        mobileMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
 }
