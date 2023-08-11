58c6f72fbe1097151a443f9ed227de170c441bab
Draekk <rodriver1992@gmail.com>
2023-08-11 19:10:30 -0400

    update: creation of shoppingCart function

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,23 @@ const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burgerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const cartIcon = document.querySelector(".navbar-shopping-cart");
+const asideCartDetail = document.querySelector(".product-detail");
 
 const toggleElement = (element) => {
+    let elements = [desktopMenu, mobileMenu, asideCartDetail];
     element.classList.toggle('inactive');
+
+    ifActiveElement(elements.filter((_element) => _element !== element));
 };
+
+function ifActiveElement(elements) {
+    elements.forEach(element => {
+        if (!element.classList.contains('inactive')) {
+            element.classList.add('inactive');
+        }
+    });
+}
 
 navEmail.addEventListener('click', () => {
     toggleElement(desktopMenu);
@@ -13,4 +26,8 @@ navEmail.addEventListener('click', () => {
 
 burgerIcon.addEventListener('click', () => {
     toggleElement(mobileMenu);
+});
+
+cartIcon.addEventListener('click', () => {
+    toggleElement(asideCartDetail);
 })
