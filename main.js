d0746eb12fe0446aee8db78587dcc30343e3ace2
Luis Uriel Robles Estrada <luis.robles7678@alumnos.udg.mx>
2023-08-11 15:57:16 -0600

    Cambios V1.1

---
+++ main.js
@@ -0,0 +1,29 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = !mobileMenu.classList.contains('inactive');
+    const isAsideClosed = !mobileMenu.classList.contains('inactive');
+
+    aside.classList.toggle('inactive');
+
+    if (isAsideOpen)
+
+    if (mobileMenu.classList.contains('inactive'))
+}
