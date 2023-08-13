4d14042e2ce532c4e8dab753fc5f86066144f784
Daniel Cabana <danielcabana0727@gmail.com>
2023-08-13 17:11:47 -0500

    clase 22

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const asaid = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleasaid)
 
 function toggleDesktopMenu() {
+
+    const isAsaidClose = asaid.classList.contains('inactive');
+
+    if (!isAsaidClose) {
+        asaid.classList.add('inactive');
+    
+    }
+    
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsaidClose = asaid.classList.contains('inactive');
+
+    if (!isAsaidClose) {
+        asaid.classList.add('inactive');
+    
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleasaid() {
+    const ismobileMenuclose = mobileMenu.classList.contains('inactive');
+
+    if (!ismobileMenuclose) {
+        mobileMenu.classList.add('inactive');
+    
+    }
+    
+    asaid.classList.toggle('inactive');
+    
 }
