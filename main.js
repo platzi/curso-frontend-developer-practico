fd8442de564dad4a4f526067c38098708b5440ea
jesus0012 <ontiverosjesus15@gmail.com>
2023-08-14 12:18:20 -0400

    Estoy actualizando mis archivos por ejemplo mi css, html y main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const navEmail =document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
@@ -12,4 +15,21 @@ function toggleDesktopMenu(){
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isAsideClosed = aside.classlist.contains('inactive')
+    // adise.classList.toggle('inactive')
+
+    if(isAsideClosed){
+        //abrir el aside
+        if(!isMobileMenuClosed) {
+
+        }
+    } else {
+        aside.class
+    }
+    aside.classList.toggle('inactive');
+
+    
 }
