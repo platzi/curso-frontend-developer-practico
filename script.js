67396726cb01d7884e5ce843b642ab88011b82b8
Gabriela Meza <gabrielamezam25@gmail.com>
2023-08-15 13:39:24 -0500

    Se añadio el menu del icon para mobile, se  modifico css, se ajusto mediaquery y se creo funcion en js para visualizar el menu

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuSelection = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
+menuSelection.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
+}
