c4972e656ec119a4dda64494828e0fa459ffbaed
Gabriela Meza <gabrielamezam25@gmail.com>
2023-08-15 17:58:27 -0500

    Se adiciona el carrito, se configura para que no se supergongan los demas menu cuando alguno este en uso

--- script.js
+++ script.js
@@ -1,15 +1,64 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuSelection = document.querySelector('.menu');
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+//const desktopMenu = document.querySelector('.desktop-menu');
+
+
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const asideCart = document.querySelector('.product-detail');
+//const desktopMenu = document.querySelector('.desktop-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuSelection.addEventListener('click', toggleMobileMenu);
+menuShoppingCart.addEventListener('click', toggleAsideCart);
+//menuShoppingCar.addEventListener('click', toggleAsideCar);
 
 function toggleDesktopMenu() {
+  asideCart.classList.add('inactive');
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+  asideCart.classList.add('inactive');
   mobileMenu.classList.toggle('inactive');
 }
+
+function toggleAsideCart() {
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  asideCart.classList.toggle('inactive');
+}
+
+  
+  //version profesor
+/*function toggleDesktopMenu() {
+  const isAsideCartClosed = asideCart.classList.contains('inactive');
+
+  if (!isAsideCartClosed) {
+    asideCart.classList.add('inactive');    
+  }
+  desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  const isAsideCartClosed = asideCart.classList.contains('inactive');
+
+  if (!isAsideCartClosed) {
+    asideCart.classList.add('inactive');    
+  }
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAsideCart() {
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+  if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add('inactive');    
+  } else if(!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+  asideCart.classList.toggle("inactive");l
+}*/
