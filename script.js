5692d5f80792dca0ebb344e62abb294922cb9308
Gabriela Meza <gabrielamezam25@gmail.com>
2023-08-16 14:12:32 -0500

    Se ajustó la interacción entre componentes para evitar que se superpongan uno sobre otro.

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuSelection = document.querySelector('.menu');
 const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
-//const desktopMenu = document.querySelector('.product-detail');
-
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const asideCart = document.querySelector('.product-list');
+const productDetailContainer = document.querySelector('.product-detail');
 const cardsContainer = document.querySelector('.cards-container');
-
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuSelection.addEventListener('click', toggleMobileMenu);
 menuShoppingCart.addEventListener('click', toggleAsideCart);
-//menuShoppingCar.addEventListener('click', toggleAsideCar);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
   asideCart.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
   asideCart.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
   mobileMenu.classList.toggle('inactive');
 }
 
 function toggleAsideCart() {
   desktopMenu.classList.add('inactive');
   mobileMenu.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
   asideCart.classList.toggle('inactive');
+}
+
+function openProductDetailAside() {
+  productDetailContainer.classList.remove('inactive');
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  asideCart.classList.add('inactive');
+}
+
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive');
 }
 
 const producList = [];
@@ -40,12 +53,12 @@ producList.push({
 producList.push({
   name: 'Skate', 
   price: 130, 
-  image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  image:'https://diamondsport.com.co/wp-content/uploads/2022/07/WhatsApp-Image-2022-05-06-at-12.16.12-PM-1-370x370_magic.jpeg'
 });
 producList.push({
   name: 'Skateboard', 
   price: 100, 
-  image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  image:'https://m.media-amazon.com/images/I/71fvZGRIJJL.jpg'
 });
 
 function renderProducts(arr) {
@@ -54,8 +67,10 @@ function renderProducts(arr) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
   
+    // product= {name, price, image} -> product.image
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside)
   
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
@@ -83,9 +98,7 @@ function renderProducts(arr) {
     productCard.appendChild(productInfo);
   
     cardsContainer.appendChild(productCard);
-  }
-  
-  
+  }  
 }
 
 renderProducts(producList);
