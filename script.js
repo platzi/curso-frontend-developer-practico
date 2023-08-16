00b2714733b97188287bc170afde4fe7c2e0b0bf
Gabriela Meza <gabrielamezam25@gmail.com>
2023-08-16 08:51:36 -0500

    Se añade modulo de la lista de productos

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const asideCart = document.querySelector('.product-detail');
-//const desktopMenu = document.querySelector('.desktop-menu');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -31,34 +31,62 @@ function toggleAsideCart() {
   asideCart.classList.toggle('inactive');
 }
 
+const producList = [];
+producList.push({
+  name: 'Bike', 
+  price: 250, 
+  image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+}); 
+producList.push({
+  name: 'Skate', 
+  price: 130, 
+  image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+producList.push({
+  name: 'Skateboard', 
+  price: 100, 
+  image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProducts(arr) {
+
+  for(product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
   
-  //version profesor
-/*function toggleDesktopMenu() {
-  const isAsideCartClosed = asideCart.classList.contains('inactive');
-
-  if (!isAsideCartClosed) {
-    asideCart.classList.add('inactive');    
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+    
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
   }
-  desktopMenu.classList.toggle('inactive');
+  
+  
 }
 
-function toggleMobileMenu() {
-  const isAsideCartClosed = asideCart.classList.contains('inactive');
+renderProducts(producList);
 
-  if (!isAsideCartClosed) {
-    asideCart.classList.add('inactive');    
-  }
-  mobileMenu.classList.toggle('inactive');
-}
-
-function toggleAsideCart() {
-  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
-  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-
-  if (!isDesktopMenuClosed) {
-    desktopMenu.classList.add('inactive');    
-  } else if(!isMobileMenuClosed) {
-    mobileMenu.classList.add('inactive');
-  }
-  asideCart.classList.toggle("inactive");l
-}*/
