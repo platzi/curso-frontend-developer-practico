5129488ca71297adac603f425ed65909c4397037
luisvls <luisvls88@gmail.com>
2023-08-17 12:11:00 -0300

    Se agrega mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burgerMenuIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
-    // console.log("click");
+  // console.log("click");
   desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu() {
+  // console.log("click");
+  mobileMenu.classList.toggle("inactive");
 }
