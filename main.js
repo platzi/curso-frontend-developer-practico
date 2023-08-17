562f6897a2cfb7c581b761f02d83733d232ca433
Federico Molina <federico.molina.146@gmail.com>
2023-08-17 16:13:56 -0300

    se agrega, se estiliza y se le da funcionalidad al aside

--- main.js
+++ main.js
@@ -2,14 +2,28 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menu = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+/*   .navbar-shopping-cart img,
+  .navbar-shopping-cart div */
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleAsideMenu);
 
 function toggleDesktopMenu() {
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 };
 
 function toggleMobileMenu() {
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
 };
+
+function toggleAsideMenu() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
+}
