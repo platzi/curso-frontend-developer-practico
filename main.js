96bbae3d6f04901972e2c5ce4a846594d5a4c275
Roger Morales (Rojo) <74221578+r0j0Code@users.noreply.github.com>
2023-08-17 07:50:51 -0500

    includes the mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuMail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuMail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
