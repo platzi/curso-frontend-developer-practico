cf11aa26b5f8cd87dc39ada64665258da0d12be1
Alejandro Arroyo <alejandroar211@gmail.com>
2023-08-18 15:05:54 -0600

    Cambio de imagenes

--- main.js
+++ main.js
@@ -49,12 +49,12 @@ productList.push({
 productList.push({
     name: 'Pantalla',
     price: 220,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image: 'https://th.bing.com/th/id/R.c677a958d65e28dc188b23ada6d80676?rik=Nr%2bGKZ0MMdlfrA&pid=ImgRaw&r=0'
 });
 productList.push({
     name: 'Computadora',
     price: 620,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image: 'https://th.bing.com/th/id/R.799e3e5a4e5cb3ed9be75b919779fbdb?rik=POIiJQk%2fQxg4yA&pid=ImgRaw&r=0'
 });
 
 function renderProducts(arr) {
