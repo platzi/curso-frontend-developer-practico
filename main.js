ed7b56d02cb36e264a2ec6dfa591d1deaf6a439c
pacho <pacho2390@gmail.com>
2023-08-18 15:52:04 -0500

    integramos el detalle de producto al html y css, sustituimos las clases de product detail y agremamos ID, hicimos ajustes con estas variaciones en el css, js y html

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon= document.querySelector(".menu")
 const mobileMenu= document.querySelector(".mobile-menu")
 const menuCarritoIcon= document.querySelector(".navbar-shopping-cart")
-const aside = document.querySelector(".product-detail")
+const shoppingCardAside = document.querySelector("#shoppingCardAside")
 const cardsContainer = document.querySelector(".cards-container")
 
 
@@ -12,20 +12,20 @@ menuHamIcon.addEventListener('click', toggleMobileMenu)
 menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isShoppingCardAsideClosed =shoppingCardAside.classList.contains('inactive');
 
-  if (!isAsideClosed) {
-    aside.classList.add('inactive'); 
+  if (!isShoppingCardAsideClosed) {
+    shoppingCardAside.classList.add('inactive'); 
   }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isShoppingCardAsideClosed = shoppingCardAside.classList.contains('inactive');
 
-  if (!isAsideClosed) {
-    aside.classList.add('inactive'); 
+  if (!isShoppingCardAsideClosed) {
+    shoppingCardAside.classList.add('inactive'); 
   }
   
   mobileMenu.classList.toggle('inactive');
@@ -38,7 +38,7 @@ function toggleCarritoAside(){
         mobileMenu.classList.add('inactive')
     }
 
-    aside.classList.toggle('inactive')
+    shoppingCardAside.classList.toggle('inactive')
 
 }
    
