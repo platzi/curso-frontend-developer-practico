b9b57bf22b01ade61ffe04d66fd55295ce469db1
Osvaldo <osvalodjesus@gmail.com>
2023-08-18 06:48:19 -0500

    adding the mobile menu, shopping cart and removing one when we select another

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuCartIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click',  toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartMenu);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains('inactive')
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive')
+  }
+
   desktopMenu.classList.toggle('inactive');
 }
 
-console.log("js funcionando");
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
+  
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
 
-console.log(desktopMenu);
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartMenu() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+
+  if (!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive');
+  }
+
+  aside.classList.toggle('inactive');
+  }
