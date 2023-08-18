ed2d20c6b17c2f228a7e853c06ec7ee2339b14e9
Edwin Contreras <ed25gonz@gmail.com>
2023-08-18 17:58:32 -0400

    'fusion mobile menu with nav'

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const emailMenu = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const burgerIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 emailMenu.addEventListener('click', toggleDesktopMenu)
+burgerIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
