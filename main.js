35d44dc6c4a6b4d730ed798cae333a2dcabe3711
Roger Morales (Rojo) <74221578+r0j0Code@users.noreply.github.com>
2023-08-18 19:41:58 -0500

    feat(product-detail): add interactive hover menu for product images
    - This commit adds an ASIDE element with the class "product-detail" to the HTML structure.
    - Styles have been included to ensure proper display. Additionally, JavaScript behavior
    has been implemented to enable the ASIDE to display when an image in the Product Grid
    is selected. Any other open hover menus are now closed when a new one is opened
    by a click action.

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
 const menuMail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
+const productDetailCloseIcon = document.querySelector(".product-detail-close");
+
 const mobileMenu = document.querySelector(".mobile-menu");
-const shoppingCartAside = document.querySelector(".product-detail");
+const shoppingCartAside = document.querySelector("#shoppingCartContainer");
+const productDetailContainer = document.querySelector("#productDetail");
 
 menuMail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleShoppingCart);
 
-function toggleDesktopMenu() {
-  if (!shoppingCartAside.classList.contains("inactive")) {
-    shoppingCartAside.classList.add("inactive");
+function collisionManager(arrToClose) {
+  for (windowToClose of arrToClose) {
+    windowToClose.classList.contains("inactive") == true
+      ? ""
+      : windowToClose.classList.add("inactive");
   }
+}
 
+function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+
+  collisionManager([shoppingCartAside, productDetailContainer]);
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
 
-  if (!shoppingCartAside.classList.contains("inactive")) {
-    shoppingCartAside.classList.add("inactive");
-  }
+  collisionManager([shoppingCartAside, productDetailContainer]);
 }
 
 function toggleShoppingCart() {
-  const isMobileMenuOpen = !mobileMenu.classList.contains("inactive");
-  const isAsideCarritoOpen = !shoppingCartAside.classList.contains("inactive");
+  shoppingCartAside.classList.toggle("inactive");
+  collisionManager([desktopMenu, mobileMenu, productDetailContainer]);
+}
 
-  if (isAsideCarritoOpen) {
-    // its open
-    shoppingCartAside.classList.add("inactive");
-  } else {
-    // its close
-    shoppingCartAside.classList.remove("inactive");
-    isMobileMenuOpen == true ? mobileMenu.classList.add("inactive") : "";
-  }
+function openProductDetailAside() {
+  productDetailContainer.classList.remove("inactive");
 
-  if (!desktopMenu.classList.contains("inactive")) {
-    desktopMenu.classList.add("inactive");
-  }
+  // Close btn
+  productDetailCloseIcon.addEventListener("pointerdown", (e) => {
+    productDetailContainer.classList.add("inactive");
+  });
+
+  collisionManager([desktopMenu, mobileMenu, shoppingCartAside]);
 }
 
 const productsList = [];
@@ -50,42 +56,80 @@ productsList.push(
     precio: 120,
     image:
       "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image_alt: "Image of a Bicicle",
   },
   {
     nombre: "Computadora",
     precio: 2400,
     image:
       "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp14-spacegray-select-202301?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1671304673229",
+    image_alt: "Macbook Pro M1 Laptop",
   },
   {
     nombre: "Scooter",
     precio: 1200,
     image:
       "https://solarscooters.co.uk/cdn/shop/products/wb1Purple.jpg?v=1674056128",
+    image_alt: "Electric Scooter",
   }
 );
 
 function renderProducts(arr) {
+  // <!-- <div class="product-card">
+  //     <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  //         alt="">
+  //     <div class="product-info">
+  //         <div>
+  //             <p>$120,00</p>
+  //             <p>Bike</p>
+  //         </div>
+  //         <figure>
+  //             <img src="./icons/bt_add_to_cart.svg" alt="">
+  //         </figure>
+  //     </div>
+  // </div> -->
+
   const cardsContainer = document.querySelector(".cards-container");
   for (product of productsList) {
     const productCard = document.createElement("div");
     productCard.classList.add("product-card");
-    productCard.innerHTML = `
-                <img src="${product.image}" alt="">
-                <div class="product-info">
-                    <div>
-                        <p>${Number(product.precio).toLocaleString("en-US", {
-                          minimumFractionDigits: 2,
-                          maximumFractionDigits: 2,
-                          style: "currency",
-                          currency: "USD",
-                        })}</p>
-                        <p>${product.nombre}</p>
-                    </div>
-                    <figure>
-                        <img src="./icons/bt_add_to_cart.svg" alt="">
-                    </figure>
-                </div>`;
+
+    const productImage = document.createElement("img");
+    productImage.setAttribute("src", product.image);
+    productImage.setAttribute("alt", product.image_alt);
+    productImage.addEventListener("pointerdown", openProductDetailAside);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = Number(product.precio).toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+      style: "currency",
+      currency: "USD",
+    });
+
+    const productName = document.createElement("p");
+    productName.innerText = product.nombre;
+
+    productInfoDiv.append(productPrice, productName);
+
+    const figure = document.createElement("figure");
+    const addButtonImage = document.createElement("img");
+    addButtonImage.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    addButtonImage.setAttribute("alt", "Add to Cart");
+
+    figure.appendChild(addButtonImage);
+
+    productInfo.append(productInfoDiv, figure);
+
+    // Build the product card with image & info
+    productCard.append(productImage, productInfo);
+
+    // Add each product card into the cards Container
     cardsContainer.appendChild(productCard);
   }
 }
