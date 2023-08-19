f641bf48996dd8b13595d0bf96676a8c98db9e57
Edwin Contreras <ed25gonz@gmail.com>
2023-08-18 23:09:51 -0400

    'fusion product list'

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const shoppingCartIcon = document.querySelector('.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
 const mobileCloseMenu = document.querySelector('.mobile-close-menu')
 const logo = document.querySelector('.logo')
+const cardsContainer = document.querySelector('.cards-container')
 
 emailMenu.addEventListener('click', toggleDesktopMenu)
 burgerIcon.addEventListener('click', toggleMobileMenu)
@@ -32,3 +33,75 @@ function toggleAside() {
 function logoMobileOpen() {
     logo.classList.toggle('logo-menu-open')
 }
+
+const productList = []
+productList.push({
+    name: 'Bike',
+    price: 380,
+    image: 'https://m.media-amazon.com/images/I/91MqcxxSzFL.__AC_SY300_SX300_QL70_FMwebp_.jpg'
+})
+productList.push({
+    name: 'Gaming Desk',
+    price: 420,
+    image: 'https://m.media-amazon.com/images/I/71SMNFiD9bL._AC_SX569_.jpg'
+})
+productList.push({
+    name: 'Coffee Cup',
+    price: 11,
+    image: 'https://m.media-amazon.com/images/I/71ijLyRmNXL.__AC_SY300_SX300_QL70_FMwebp_.jpg'
+})
+productList.push({
+    name: 'Git and Github',
+    price: 14.99,
+    image: 'https://pbs.twimg.com/media/FubgrSEWYAATGTr.jpg'
+})
+productList.push({
+    name: 'Learning JavaScript',
+    price: 14.85,
+    image: 'https://d2sofvawe08yqg.cloudfront.net/aprendiendo-javascript/s_hero2x?1620475858'
+})
+productList.push({
+    name: 'Bike',
+    price: 79.99,
+    image: 'https://i5.walmartimages.com.mx/mg/gm/3pp/asr/b2b10812-dbea-4cdd-823c-22d67ed22215.caad0e2aa520aa4be27909fe53de6adf.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF'
+})
+
+function renderProducts(arr) {
+    for (product of productList) {
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+
+        const productImg = document.createElement('img')
+        productImg.setAttribute('src', product.image)
+        productImg.setAttribute('alt', product.name)
+
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+
+        const productInfoDiv = document.createElement('div')
+
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+
+        const productFigure = document.createElement('figure')
+        
+        const productFigureCart = document.createElement('img')
+        productFigureCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+        productFigureCart.setAttribute('alt', 'Add to Cart')
+
+        productInfoDiv.append(productPrice, productName)
+
+        productFigure.appendChild(productFigureCart)
+
+
+        productInfo.append(productInfoDiv, productFigure)
+
+        productCard.append(productImg, productInfo)
+
+        cardsContainer.append(productCard)
+    }
+}
+
+renderProducts(productList)
