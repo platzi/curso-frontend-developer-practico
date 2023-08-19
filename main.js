00e8c0243d6c791b328ae2cbe8310b2650bb8eee
mrgiron <mrgiron@gmail.com>
2023-08-19 14:32:11 -0600

    Agregados los menús (mobile y desktop) al index

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
-
+const bntMenuMobile = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+bntMenuMobile.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    menuMobile.classList.toggle('inactive');
 }
