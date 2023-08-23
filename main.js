42b7f9de042df9696d21127712041a1d84aab80f
Humberto Viveros <humberto.viverosrios@gmail.com>
2023-08-23 16:54:58 -0400

    Lista de productos: HTML a partir de arrays JS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuBurger = document.querySelector('.menu');
 const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurger.addEventListener('click', toggleMobileMenu);
@@ -42,3 +43,84 @@ function toggleCartAside(){
     
     aside.classList.toggle('inactive');
 }
+
+// Array que devolverá la consulta a la API Rest
+const productList = [];
+// push() -> inserta un nuevo elemento al final de un array
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Computadora',
+    price: 350,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+
+/* <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt="">
+    <div class="product-info">
+        <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+        </div>
+        <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div>  */
+
+
+function renderProducts(arr){
+    // Acceder a cada elemento de cada objeto con for of
+    for(product of arr){
+        // createElement() -> crea elementos HTML
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        // Con setAttribute establecemos atributos a elementos HTML
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productIconCart= document.createElement('img');
+        productIconCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        // appendChild() -> inserta elementos HTML dentro de otro
+        productInfoFigure.appendChild(productIconCart);
+
+        // Maquetación HTML desde JavaScript
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        // Contenedor principal, cree como hijo a productCard
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
