64c28fc0f66582079a531117d4ee10a22f9dca7a
Luis Herrera <luis1818.lh@gmail.com>
2023-08-23 16:44:52 -0500

    Se agrega lógica de JS, para hacer interactivo el product detail con las demas funciones de la página

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobile);
 carritoIcon.addEventListener('click', toggleCarritoAside)
+productDetailCloseIcon.addEventListener('click', closeProductoDetailAside);
 
 function toggleDesktopMenu(){
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
@@ -21,20 +24,37 @@ function toggleDesktopMenu(){
 function toggleMobile(){
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
     if(!isAsideClosed){
-        shoppingCartContainer.classList.add('inactive')
+      shoppingCartContainer.classList.add('inactive');
     }
-    mobileMenu.classList.toggle('inactive') 
+    closeProductoDetailAside();
+    mobileMenu.classList.toggle('inactive') ;
 }
 
 function toggleCarritoAside(){
-    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    
-    
-    if (!isMobileMenuClosed){
-        mobileMenu.classList.add('inactive')
-    }
-    shoppingCartContainer.classList.toggle('inactive') 
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
         
+  if (!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive')
+  }
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    
+  if (!isProductDetailClosed){
+    productDetailContainer.classList.add('inactive')
+  }
+
+  shoppingCartContainer.classList.toggle('inactive') 
+        
+}
+
+function openProductDetailAside (){
+  shoppingCartContainer.classList.add('inactive')
+  
+  productDetailContainer.classList.remove('inactive')
+  
+}
+
+function closeProductoDetailAside() {
+  productDetailContainer.classList.add('inactive')
 }
 
 const productList = [];
@@ -75,6 +95,7 @@ for (product of arr){
 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside);
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
