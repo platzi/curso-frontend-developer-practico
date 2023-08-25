ba75532c00253a415ca04ce69d6e8dc166116e39
Piero Lavy <lavypiero096@gmail.com>
2023-08-25 17:57:59 -0500

    Agregar productos v1.0

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const clickMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const clickCarrito = document.querySelector(".navbar-shopping-cart");
 const productDetail = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
+
 
 clickEmail.addEventListener("click", mostrarMenu);
 
@@ -31,3 +33,116 @@ function mostrarMenuCarrito(){
         mobileMenu.classList.add("inactive");
     }
 }
+
+const productList = [];
+
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Laptop",
+    price: 1120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Secadora",
+    price: 220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Laptop",
+    price: 1120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Secadora",
+    price: 220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Laptop",
+    price: 1120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Secadora",
+    price: 220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Laptop",
+    price: 1120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: "Secadora",
+    price: 220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+
+function renderProducts(arr){
+    arr.forEach((element) => {
+
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+        
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", element.image);
+        
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+        
+        const productInfoDiv = document.createElement("div");
+        const productPrice = document.createElement("p");
+        productPrice.innerHTML = "$" + element.price + ".00";
+        const productName = document.createElement("p");
+        productName.innerHTML = element.name;
+    
+        const figure = document.createElement("figure");
+        const imgFigure = document.createElement("img");
+        imgFigure.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    
+        figure.appendChild(imgFigure);
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(figure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+        
+    });
+}
+
+renderProducts(productList);
+
