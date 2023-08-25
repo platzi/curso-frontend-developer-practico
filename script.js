7ae512dae9bf9a72dec561a578ad3e4db775210e
Piero Lavy <lavypiero096@gmail.com>
2023-08-25 16:36:46 -0500

    Menus listos v1.1

--- script.js
+++ script.js
@@ -2,18 +2,32 @@ const clickEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const clickMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-
+const clickCarrito = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 
 clickEmail.addEventListener("click", mostrarMenu);
 
 function mostrarMenu(){
-    desktopMenu.classList.toggle("inactive");
+    if(!desktopMenu.classList.toggle("inactive")){
+        productDetail.classList.add("inactive");
+    }
 }
-
 
 
 clickMenu.addEventListener("click", mostrarMenuMobile);
 
 function mostrarMenuMobile(){
-    mobileMenu.classList.toggle("inactive");
+    if(!mobileMenu.classList.toggle("inactive")){
+        productDetail.classList.add("inactive");
+    }   
+}
+
+
+clickCarrito.addEventListener("click", mostrarMenuCarrito);
+
+function mostrarMenuCarrito(){
+    if(!productDetail.classList.toggle("inactive")){
+        desktopMenu.classList.add("inactive");
+        mobileMenu.classList.add("inactive");
+    }
 }
