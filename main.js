9b41448e11c202afacf759d0a227155b7e6de7ba
Randy Moya <stevenmoya02@gmail.com>
2023-08-24 22:22:38 -0600

    Se funciono en el archivo index el componente del menu en mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const menu = document.querySelector(".desktop-menu");
+const menuDesktop = document.querySelector(".desktop-menu");
+const menuMobile = document.querySelector(".mobile-menu");
 const menuEmail = document.querySelector(".navbar-email");
+const menuMobileIcon = document.querySelector(".menu")
 
 function toggleDesktopMenu(){
-    menu.classList.toggle('inactive');
+    menuDesktop.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    menuMobile.classList.toggle('inactive');
 }
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuMobileIcon.addEventListener('click', toggleMobileMenu);
