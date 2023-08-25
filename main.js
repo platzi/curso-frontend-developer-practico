41cb16c1953c9577ff951c335074d5408db2fdb0
Josue Baez <josuejbaeze4@gmail.com>
2023-08-25 19:23:12 -0400

    Anexado el Aside del carrito  y terminada parte de la barra de nav

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Opcion email nav*/
 const opc_email = document.querySelector('.navbar-email')
 const desktop_menu_email = document.querySelector('.desktop-menu')
 
@@ -14,6 +15,10 @@ opc_email.addEventListener('mouseout', () => {
 opc_email.addEventListener('mouseover', () => {
     clearTimeout(mouseoutTimeout);
     desktop_menu_email.classList.remove('inactive'); // Eliminar la clase 'inactive'
+
+    if(!product_detail.classList.contains('inactive')){
+        product_detail.classList.toggle('inactive')
+    }
 });
 
 // Evento mouseout en desktop_menu
@@ -27,4 +32,31 @@ desktop_menu_email.addEventListener('mouseout', () => {
 desktop_menu_email.addEventListener('mouseover', () => {
     clearTimeout(mouseoutTimeout);
     desktop_menu_email.classList.remove('inactive');
+});
+
+/* opcion general mobile */
+
+const mob_menu_img = document.querySelector('.menu')
+const mob_menu = document.querySelector('.mobile-menu')
+
+mob_menu_img.addEventListener('click', () => {
+    mob_menu.classList.toggle('inactive')
+    if(!product_detail.classList.contains('inactive')){
+        product_detail.classList.toggle('inactive')
+    }
+    
+});
+
+/* carrito de compra */
+
+const product_detail_img = document.querySelector('.navbar-shopping-cart')
+const product_detail = document.querySelector('.product-detail')
+
+product_detail_img.addEventListener('click', () => {
+    product_detail.classList.toggle('inactive')
+
+    if(!mob_menu.classList.contains('inactive')){
+        mob_menu.classList.toggle('inactive')
+    }
+    
 });
