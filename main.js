6468ba296f3836e1129fcbf9326644b1cfbd7950
Alexander <prodbydsceron@gmail.com>
2023-08-28 22:11:41 -0500

    2. Fusionamos el mobile menu e implementamos JS para aparecer y desaparecer con un click el menu hamburguesa

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const hamburMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu); //toggle es como intercambiar
+hamburMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive")
 }
