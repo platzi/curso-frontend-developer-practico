22d7d6a5cfa02696c1f97c3be10c137873bd7bea
Deathfox66 <84390515+Deathfox66@users.noreply.github.com>
2023-08-28 20:49:22 -0600

    Carrito de Compras [Clase 21]
    Se realizo la forma en la que se cierra y abren los diferentes menus de la aplicacion.

--- main.js
+++ main.js
@@ -2,15 +2,38 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carIcon = document.querySelector('.navbar-shopping-cart');
+const detailProduct = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+carIcon.addEventListener('click', toggleCarAside);
 
 function toggleDesktopMenu (){
-    desktopMenu.classList.toggle('inactive')
+    const isDetaiProductClosed = detailProduct.classList.contains('inactive');
+
+    if (!isDetaiProductClosed) {
+        detailProduct.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive')
+    const isDetaiProductClosed = detailProduct.classList.contains('inactive');
+
+    if (!isDetaiProductClosed) {
+        detailProduct.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
 }
 
+function toggleCarAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed || !isDesktopMenuClosed) {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+    detailProduct.classList.toggle('inactive');
+}
