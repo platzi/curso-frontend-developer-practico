21c116e1071e91c3b00e85a31f6602de3fbe0d63
Ricardo Delfin <richardo.delfin94@gmail.com>
2023-08-28 19:25:44 -0700

    hasta aqui me quede en el trabajo, continuo en mi casa. Se esta agregando el menu del carrito

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
+
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
@@ -12,4 +16,20 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside() {
+    const isMobileMenuClosed = !mobileMenu.classList.contains('inactive');
+    const isAsideClosed = !aside.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if(isAsideClosed) {
+        // abrir el aside
+        // Si mobileMenu esta open, hay que cerrarlo
+    } else {
+        aside.classList.add('inactive');
+    }
 }
