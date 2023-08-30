da6149831b49c1f51c53aa85d1aeda2beb8c0e2b
dav-dmdo <david.davila@correo.unimet.edu.ve>
2023-08-30 09:25:45 -0400

    Clase8.html file added. Mobile and Desktop Menus added with media query

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('img.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+/**
+ * Events listeners
+ */
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu)
+
+
+/**
+ * onClick events
+ */
+function toHideOrUnhideOnClick(element){
+    element.classList.toggle('inactive');
+}
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+    toHideOrUnhideOnClick(desktopMenu);
+}
+
+function toggleMobileMenu(){
+    toHideOrUnhideOnClick(mobileMenu);
 }
