a49f430c6c88229025525cd4f57a9260eb2699b3
dav-dmdo <david.davila@correo.unimet.edu.ve>
2023-08-30 10:58:04 -0400

    Adding shopping cart element

--- main.js
+++ main.js
@@ -1,27 +1,57 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 const burgerMenu = document.querySelector('img.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+
+const navbarShoppingCrat = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
 
 /**
  * Events listeners
  */
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-burgerMenu.addEventListener('click', toggleMobileMenu)
-
+burgerMenu.addEventListener('click', toggleMobileMenu);
+navbarShoppingCrat.addEventListener('click', toggleProductDetail);
 
 /**
  * onClick events
  */
-function toHideOrUnhideOnClick(element){
-    element.classList.toggle('inactive');
-}
 
 function toggleDesktopMenu(){
-    toHideOrUnhideOnClick(desktopMenu);
+    checkToSwitchState(productDetail);
+    toToggleOnClick(desktopMenu);
 }
 
 function toggleMobileMenu(){
-    toHideOrUnhideOnClick(mobileMenu);
+    checkToSwitchState(productDetail);
+    toToggleOnClick(mobileMenu);
+
 }
+
+function toggleProductDetail(){
+    checkToSwitchState(mobileMenu);
+    checkToSwitchState(desktopMenu);
+    toToggleOnClick(productDetail);
+}
+
+/**
+ * Helpers
+ */
+
+function toToggleOnClick(element){
+    element.classList.toggle('inactive');
+}
+
+function isInactive(element){
+    return element.classList.contains('inactive');
+}
+
+function checkToSwitchState(element){
+    if(!isInactive(element)){
+        toToggleOnClick(element)
+    }
+}
+
+
