7ace16a242729dd83c2c4a7b4b81c2c26eae77f1
sojoko <jonhathansojo@gmail.com>
2023-08-31 03:51:09 -0500

    Integracion de primeros modulos en la vista index.html

---
+++ main.js
@@ -0,0 +1,112 @@
+const desktopManu = document.querySelector('.navbar-email')
+const deskM = document.querySelector('.desktop-menu')
+const menuHamburgerIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const orderList = document.querySelector('#shoppingCartContainer')
+const cardsContainer = document.querySelector('.cards-container');
+
+
+
+
+desktopManu.addEventListener('click', toggleDesktopManu);
+menuHamburgerIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleOrderList);
+
+function toggleDesktopManu() {
+   const  isOrderListOpen = !orderList.classList.contains('inactive');
+
+
+   if (isOrderListOpen){
+        orderList.classList.add('inactive');
+    }
+
+    deskM.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const  isOrderListOpen = !orderList.classList.contains('inactive');
+
+    if (isOrderListOpen){
+        orderList.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleOrderList() {
+    const isNavMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isDeskMenuOpen = !deskM.classList.contains('inactive');
+
+    if (isNavMobileMenuOpen){
+        mobileMenu.classList.add('inactive');
+    }
+    if (isDeskMenuOpen){
+        deskM.classList.add('inactive')
+    }
+    orderList.classList.toggle('inactive');
+}
+
+const producList = [];
+producList.push({
+    name: 'bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }
+);
+producList.push({
+    name: 'mountainBike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }
+);
+producList.push({
+    name: 'bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }
+);
+
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        cardsContainer.appendChild(productCard)
+
+    }
+
+}
+
+renderProducts(producList);
