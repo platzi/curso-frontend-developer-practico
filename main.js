1c549a9994fa3eae53869d089b69d44098c4ab51
Jeremias Gunsett <jeremias.gunsett@itscordoba.edu.ar>
2023-08-30 23:34:49 -0300

    Se agrega funcion de renderizado para implementar la galeria del Ecommerce

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuBurger = document.querySelector('.menu-burger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const iconCarrito = document.querySelector('.navbar-shopping-cart');
 const productDetail = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);//Evento de click en el menu de usuario de desktop
 
@@ -54,3 +55,74 @@ function toggleProductDetail(){
 
     productDetail.classList.toggle('inactive');
 }
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Montain Bike',
+    price: 190,
+    image: 'https://biciurbana.com.ar/14268-large_default/mountain-bike-fire-bird-r29.jpg'
+});
+productList.push({
+    name: 'Racing Bike',
+    price: 220,
+    image: 'https://images.immediate.co.uk/production/volatile/sites/21/2021/03/20210317_SB_5DSR_MG_4042-4cbecec.jpg?webp=true&quality=90&resize=620%2C413'
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Montain Bike',
+    price: 190,
+    image: 'https://biciurbana.com.ar/14268-large_default/mountain-bike-fire-bird-r29.jpg'
+});
+productList.push({
+    name: 'Racing Bike',
+    price: 220,
+    image: 'https://images.immediate.co.uk/production/volatile/sites/21/2021/03/20210317_SB_5DSR_MG_4042-4cbecec.jpg?webp=true&quality=90&resize=620%2C413'
+});
+
+function RenderProduct (arr){
+    for (product of productList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const ProductImg = document.createElement('img');
+        ProductImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', product.image);
+    
+        productInfoFigure.appendChild(productImgCard);
+    
+        productInfo.append(productInfoDiv, productInfoFigure);
+    
+        productCard.append(ProductImg, productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+RenderProduct(productList);
