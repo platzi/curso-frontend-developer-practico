527d18834791441da5db3b175fc87bba3eeb5b65
Jesus Montealegre <jesusmonaq@gmail.com>
2023-08-30 19:15:19 -0500

    Se agrega modal articulos, solución de modales abiertos

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailClose = document.querySelector('.product-detail-close')
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailClose.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -26,20 +29,37 @@ function toggleMobileMenu() {
 
     if (!isAsideClosed) {
         shoppingCartContainer.classList.add('inactive');
-    
     }
+
+    closeProductDetailAside();
+    
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside() {
-
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     
     }
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive');
+    
+    }
     shoppingCartContainer.classList.toggle('inactive');
+}
+
+function openProductDetailAside() {
+
+    shoppingCartContainer.classList.add('inactive');
+
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -69,6 +89,7 @@ function renderProducts(arr) {
      
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside)
      
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
