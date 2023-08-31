fa770e23231f1bc77079c92c9c3b0b8e112cf9d4
Gabriel Huaman <gabrielhuaman683@gmail.com>
2023-08-30 22:20:06 -0500

    agregue el aside, luego programe para que se viera con el click, adicional a eso agregue que cuando hace click el resto desaparece todo esto en el js

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 const menuEmail=document.querySelector('.navbar-email');
-const desktopMenu=document.querySelector('.desktop-menu')
+const desktopMenu=document.querySelector('.desktop-menu');
+const menuCarrito=document.querySelector('.navbar-shopping-cart');
+const menuhamIcon=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+const aside=document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuhamIcon.addEventListener('click',toggleMovileMenu);
+menuCarrito.addEventListener('click',toggleCarritoAside);
+
 
 function toggleDesktopMenu(){
+
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive')
+}
+
+
+
+
+function toggleMovileMenu(){
+
+    const isAsideClosed= aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.toggle('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+
+
+function toggleCarritoAside(){
+
+    const ismobileMenuClosed= mobileMenu.classList.contains('inactive');
+
+    if(!ismobileMenuClosed){
+        mobileMenu.classList.toggle('inactive');
+    }
+    aside.classList.toggle('inactive');
+
 }
