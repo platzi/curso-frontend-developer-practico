a44194e971bb90e84b390d6d51ab9adf2380ac28
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-08-31 09:47:24 -0500

    fusion menu mobile y desktop.

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burguerIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burguerIcon.addEventListener("click", toggleDesktopMenu);
+
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleDesktopMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
