be273bbdb87c700599865f714b1637b7f5f2ce9e
edime09 <edison09mr@gmail.com>
2023-09-01 14:37:58 -0500

    fusion menu desktop

---
+++ main.js
@@ -0,0 +1,9 @@
+// Funcion para darle interactividad al menu desktop dando click al correo.
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
