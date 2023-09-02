1a8010e47db196ac8837c9958e597ee7dc337ca1
Jeremias Gunsett <jeremias.gunsett@itscordoba.edu.ar>
2023-09-02 16:17:00 -0300

    Se agrega el detalle del producto individual en html y css

--- main.js
+++ main.js
@@ -3,40 +3,40 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurger = document.querySelector('.menu-burger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const iconCarrito = document.querySelector('.navbar-shopping-cart');
-const productDetail = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);//Evento de click en el menu de usuario de desktop
 
 menuBurger.addEventListener('click', toggleMobileMenu);//Evento de click en el menu hamburgesa de mobile
 
-iconCarrito.addEventListener('click', toggleProductDetail);//Evento de click en el detalle del producto
+iconCarrito.addEventListener('click', toggleshoppingCartContainer);//Evento de click en el detalle del producto
 
 function toggleMobileMenu() {
-    //Constante que contiene Booleano de si esta inactivo el ProductDetail
-    const isProductDetailClosed = productDetail.classList.contains('inactive');
+    //Constante que contiene Booleano de si esta inactivo el shoppingCartContainer
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
     
     // Si el Detalle del carrito esta abierto lo cerramos
-    if (!isProductDetailClosed) {
-        productDetail.classList.add('inactive');
+    if (!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleDesktopMenu(){
-    //Constante que contiene Booleano de si esta inactivo el ProductDetail
-    const isProductDetailClosed = productDetail.classList.contains('inactive');
+    //Constante que contiene Booleano de si esta inactivo el shoppingCartContainer
+    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
     
     // Si el Detalle del carrito esta abierto lo cerramos
-    if (!isProductDetailClosed) {
-        productDetail.classList.add('inactive');
+    if (!isshoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleProductDetail(){
+function toggleshoppingCartContainer(){
     //Constante que contiene Booleano de si esta inactivo el menu del Celular
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
 
@@ -53,7 +53,7 @@ function toggleProductDetail(){
         mobileMenu.classList.add('inactive');
     }
 
-    productDetail.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
