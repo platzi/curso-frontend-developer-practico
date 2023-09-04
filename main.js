ec01ac64133dc8b74c9b101efa2d10854bdf9b6b
Marcemuca <marcela.muca70@gmail.com>
2023-09-04 15:40:55 -0500

    Guardando por primera vez

---
+++ main.js
@@ -0,0 +1,9 @@
+const navMail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+navMail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+
+}
