36338ae0ea09adfd3219f8ad456c03320c34c0da
edime09 <edison09mr@gmail.com>
2023-09-04 16:11:43 -0500

    finalizacion carrito de compras

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Funcion para darle interactividad al menu desktop dando click al correo.
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBarIcon = document.querySelector('.menu');
@@ -11,14 +10,32 @@ menuBarIcon.addEventListener('click', toggleMobileMenu);
 carIcon.addEventListener('click', togglesaide);
 
 function toggleDesktopMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        aside.classList.add('inactive');     
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 }
 
 
 function togglesaide() {
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClose) {
+        mobileMenu.classList.add('inactive');
+    }else if (!isDesktopMenuClose) {
+        desktopMenu.classList.add('inactive');
+    }
     aside.classList.toggle('inactive');
 }
