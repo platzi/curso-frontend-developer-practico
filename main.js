7805c16495943a6805f632a71c6284a44bb19ea8
edime09 <edison09mr@gmail.com>
2023-09-04 12:03:33 -0500

    Punto intermedio carrito de compras

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBarIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-
+const carIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBarIcon.addEventListener('click', toggleMobileMenu);
+carIcon.addEventListener('click', togglesaide);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
@@ -14,4 +16,9 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+}
+
+
+function togglesaide() {
+    aside.classList.toggle('inactive');
 }
