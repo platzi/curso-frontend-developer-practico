b7906cb17eeb2711eebbd8c8ddf004af9358f03c
edime09 <edison09mr@gmail.com>
2023-09-04 11:22:25 -0500

    Menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 // Funcion para darle interactividad al menu desktop dando click al correo.
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBarIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBarIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
