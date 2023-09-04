51983feee199c7f1f229ad79631a586ef3eacf8e
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-09-04 09:21:12 -0500

    finished class 21

--- main.js
+++ main.js
@@ -6,17 +6,38 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
-burgerIcon.addEventListener("click", toggleDesktopMenu);
+burgerIcon.addEventListener("click", toggleMobileMenu);
 cartIcon.addEventListener("click", toggleCartAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.toggle("inactive");
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
-function toggleDesktopMenu() {
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.toggle("inactive");
+    }
     mobileMenu.classList.toggle("inactive");
 }
 
 function toggleCartAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }else if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
     aside.classList.toggle("inactive");
-}
+    
+    
+} 
