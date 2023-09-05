8993bf40fd293b88ebf6746a47cb54a5bb4f593c
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-09-05 08:53:28 -0500

    mejora en js, interacción menus.

--- main.js
+++ main.js
@@ -10,34 +10,41 @@ burgerIcon.addEventListener("click", toggleMobileMenu);
 cartIcon.addEventListener("click", toggleCartAside);
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains("inactive");
-
-    if (!isAsideClosed) {
-        aside.classList.toggle("inactive");
-    }
     desktopMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
+    // const isAsideClosed = aside.classList.contains("inactive");
+
+    // if (!isAsideClosed) {
+    //     aside.classList.toggle("inactive");
+    // }
+    // desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains("inactive");
-
-    if (!isAsideClosed) {
-        aside.classList.toggle("inactive");
-    }
+    aside.classList.add("inactive");
     mobileMenu.classList.toggle("inactive");
+    // const isAsideClosed = aside.classList.contains("inactive");
+
+    // if (!isAsideClosed) {
+    //     aside.classList.toggle("inactive");
+    // }
+    // mobileMenu.classList.toggle("inactive");
 }
 
 function toggleCartAside(){
-    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
-
-    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
-
-    if (!isMobileMenuClosed) {
-        mobileMenu.classList.add("inactive");
-    }else if (!isDesktopMenuClosed){
-        desktopMenu.classList.add('inactive');
-    }
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
     aside.classList.toggle("inactive");
+    // const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    // const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    // if (!isMobileMenuClosed) {
+    //     mobileMenu.classList.add("inactive");
+    // }else if (!isDesktopMenuClosed){
+    //     desktopMenu.classList.add('inactive');
+    // }
+    // aside.classList.toggle("inactive");
     
     
 } 
