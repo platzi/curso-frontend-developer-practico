31c3deb9eb041a216b27c7ee0146421841a88f33
Felipe Orduz <orduzfelipe05@gmail.com>
2023-09-04 21:18:16 -0500

    Añadi el menu del carrito y solucione ciertos conflictos que generaba

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 const menuEmail = document.querySelector(".navbar-email");
+const cartIcon = document.querySelector(".navbar-shopping-cart")
+const asideMenu = document.querySelector(".product-detail")
 const desktopMenu = document.querySelector(".desktop-menu");
 const burguerMenu = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burguerMenu.addEventListener("click", toggleMobileMenu);
+cartIcon.addEventListener("click", toggleAsideMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
+    asideMenu.classList.add("inactive")
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive")
+    asideMenu.classList.add("inactive") 
+}
+
+function toggleAsideMenu() {
+    asideMenu.classList.toggle("inactive")
+    desktopMenu.classList.add("inactive")
+    mobileMenu.classList.add("inactive")
 }
 
 console.log("FUNCIONOOOO!!!")
