22ef2d595769b49454ad5a95a9ef0ad1ee9964b5
Felipe Orduz <orduzfelipe05@gmail.com>
2023-09-06 21:41:24 -0500

    Añadi el aside de la descripcion del producto y realice algunas modificaciones a los selectores

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const menuEmail = document.querySelector(".navbar-email");
 const cartIcon = document.querySelector(".navbar-shopping-cart")
-const asideMenu = document.querySelector(".product-detail")
+const shopingCartCont = document.querySelector("#shopingCartCont")
 const desktopMenu = document.querySelector(".desktop-menu");
 const burguerMenu = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
@@ -12,16 +12,16 @@ cartIcon.addEventListener("click", toggleAsideMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
-    asideMenu.classList.add("inactive")
+    shopingCartCont.classList.add("inactive")
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive")
-    asideMenu.classList.add("inactive") 
+    shopingCartCont.classList.add("inactive") 
 }
 
 function toggleAsideMenu() {
-    asideMenu.classList.toggle("inactive")
+    shopingCartCont.classList.toggle("inactive")
     desktopMenu.classList.add("inactive")
     mobileMenu.classList.add("inactive")
 }
