78a8176eff3050206cfd954002253951e7eeb315
Nicolas Baak <nicolas123alejandro@gmail.com>
2023-09-06 21:46:25 -0600

    second commit: merge movile menu

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const mobileMenu = document.querySelector('.mobile-menu');
+const HamIconMenu = document.querySelector('.menu');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+HamIconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu()
 {
@@ -16,4 +20,9 @@ function toggleDesktopMenu()
     }
     */
    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu()
+{
+    mobileMenu.classList.toggle('inactive');
 }
