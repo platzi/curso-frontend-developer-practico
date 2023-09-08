b138fe56a348ae28506184c70fcc42be936ccfea
Jose Peralta <joseperalta2910@gmail.com>
2023-09-07 22:47:10 -0400

    Mobile menu integrated

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const burgerIcon = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerIcon.addEventListener('click', toggleMobileMenu);
+closeMenu.addEventListener('click', toggleMobileMenu);
