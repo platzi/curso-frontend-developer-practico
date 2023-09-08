bbbe94e3ae69f11cd4c784006664ac5a880f7825
Jose Peralta <joseperalta2910@gmail.com>
2023-09-07 22:25:00 -0400

    Menu desktop and navbar integrated

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
+
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
