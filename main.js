4b89893195fe5497e6f7bd9c126baa3402a70366
Nicolas Baak <nicolas123alejandro@gmail.com>
2023-09-07 20:40:56 -0600

    third commit: merge cart menu

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const HamIconMenu = document.querySelector('.menu');
 
+const cartMenuIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 HamIconMenu.addEventListener('click', toggleMobileMenu);
+cartMenuIcon.addEventListener('click', toggleCartMenuIcon);
 
 function toggleDesktopMenu()
 {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive'); 
+    }
     /*
     if(desktopMenu.classList.contains("inactive"))
     {
@@ -24,5 +32,28 @@ function toggleDesktopMenu()
 
 function toggleMobileMenu()
 {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive'); 
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartMenuIcon()
+{
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed )
+    {
+        mobileMenu.classList.add('inactive'); 
+    }
+
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isDesktopMenuClosed )
+    {
+        desktopMenu.classList.add('inactive'); 
+    }
+
+    aside.classList.toggle('inactive');
 }
