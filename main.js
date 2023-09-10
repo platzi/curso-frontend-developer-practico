2b0c24cb4ae82b78ddedd7364ad82546e66bdbe4
ricardocollado <ricardocolladorothschild@gmail.com>
2023-09-10 02:10:54 -0400

    add Mobile Menu, and add position absolute and a top: 60px to move it to the right

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const desktop_menu = document.querySelector('.desktop-menu');
 const navbar_email = document.querySelector('.navbar-email');
 
 
-navbar_email.addEventListener('mouseenter',function(){
-    desktop_menu.classList.remove('inactive');
-});
-desktop_menu.addEventListener('mouseleave', function(){
-    desktop_menu.classList.add('inactive');
-});
+navbar_email.addEventListener('mouseenter',toggleDesktopMenu);
+desktop_menu.addEventListener('mouseleave',toggleDesktopMenu);
+
+/**Para que funciona toggle? Esta propiedad de classList, lo que hace es revisar, si el elemento actual, tiene esta clase, si la tiene
+ * procede a removersela, sino la tiene, procede agregar esa cLASE. Esto ahorra codigo, permitiendo ahorrar un add or remove de un classList
+ */
+function toggleDesktopMenu(){
+    desktop_menu.classList.toggle('inactive');
+}
