5e2293f76176ea7b046076e51441fa57bc6aee19
Josue Baez <josuejbaeze4@gmail.com>
2023-09-11 22:19:18 -0400

    Arreglado codigo mas limpio y mejorado el carrito, faltan detalles de calculos

--- main.js
+++ main.js
@@ -5,15 +5,20 @@ const desktop_menu_email = document.querySelector('.desktop-menu')
 const mob_menu_img = document.querySelector('.menu');
 const mob_menu = document.querySelector('.mobile-menu');
 /* carrito de compra */
+const number_cart = document.querySelector('.number-cart');
 const shopping_cart_container_img = document.querySelector('.navbar-shopping-cart');
 const shopping_cart_container = document.querySelector('#shopping-cart-container');
 const my_order_content = document.querySelector('.my-order-content');
 const shopping_cart_button = document.querySelector('.add-to-cart-button');
 /*contenedor de productos*/
 const cards_container = document.querySelector('.cards-container');
-/*open/closed Detalles del producto*/
+let img_card = cards_container.querySelectorAll('img');
+let btn_add = cards_container.querySelectorAll('.btn_add_cart');
+
+/* Detalles del producto*/
 const product_detail = document.querySelector('#product-detail');
-const product_detail_close = document.querySelector('.product-detail-close')
+const product_detail_close = product_detail.querySelector('.product-detail-close')
+let btn_add_product_detail = product_detail.querySelector('.add-to-cart-button');
 /*variable de lista de productos del carrito de compra*/
 let all_products_cart = [];
 let total = 0;
@@ -78,7 +83,7 @@ function close_product_detail(){
 }
 
 
-/* cargar los productos de la lista en el HTML */
+/* cargar los productos*/
 
 function render_products(products_list){
     for(let product of products_list){
@@ -107,6 +112,7 @@ function render_products(products_list){
         const img_logo_cart = document.createElement('img');
         img_logo_cart.classList.add('btn_add_cart');
         img_logo_cart.setAttribute('src','./icons/bt_add_to_cart.svg');
+        img_logo_cart.setAttribute('id', product.id);
      
         figure.appendChild(img_logo_cart);
         product_info_div.append(product_price, product_name,product_description);
@@ -114,113 +120,82 @@ function render_products(products_list){
         product_card.append(img, product_info);
 
         cards_container.appendChild(product_card);   
-     }
-     open_detail_product();
-     upload_cart();
-     delete_product_cart(); 
-}
-//evento mostrar detalle de producto - obtengo los datos a traves de html - aqui solo modifico HTML ya existente.
-function open_detail_product(){
-    cards_container.addEventListener('click', e=> {
-        if(e.target.classList.contains('img-card')){
-            const product = e.target.parentElement;
-            //obtengo los datos de cards(cartas contenedoras del main)
-            const price_element = product.querySelector('.product-info:nth-child(2) p:nth-child(1)');
-            const name_element  = product.querySelector('.product-info:nth-child(2) p:nth-child(2)');
-            const description_element = product.querySelector('.product-info:nth-child(2) p:nth-child(3)');
-            const imgElement = product.querySelector('.img-card').src;
-            //obtengo la ubicacion de los elementos en detalles del producto
-            const price  = document.querySelector('#product-detail .product-info p:nth-child(1)');
-            const name  = document.querySelector('#product-detail .product-info p:nth-child(2)');
-            const description  = document.querySelector('#product-detail .product-info p:nth-child(3)');
-            const img = document.querySelector('#product-detail > img:nth-child(2)');
-            
-            price.textContent = price_element.textContent ;
-            name.textContent  = name_element.textContent ;
-            description.textContent  = description_element.textContent ;
-            img.src = imgElement; // Cambia esto por la nueva URL que deseas usar
-            img.alt = 'imagen del producto'; // Cambia el texto alternativo de la imagen
-    
-            
-        }
-    });
-}
-// convertir el texto en numeros y quitar el simbolo $
-function conver_price(k){
-    return Number(k.slice(1));
-}
-function calculate_total(price , quantity, op){
-
-    if(op==='+'){
-        total = total + (conver_price(price) * +quantity);
-        const total_contain = document.querySelector('#shopping-cart-container .order .total');
-        total_contain.textContent = '$' + total;
-    }else if(op==='-'){
-        total = total - (conver_price(price) * +quantity);
-        const total_contain = document.querySelector('#shopping-cart-container .order .total');
-        total_contain.textContent = '$' + total;
     }
+    update_btn_card();
+    select_img_card();
+    select_btn_detail();
+
 }
-/*eventos para cargar el carrito con los productos*/
-function upload_cart(){
-    //desde las cards del menu principal
-    cards_container.addEventListener('click', e=> {
-        if(e.target.classList.contains('btn_add_cart')){
-            const product = e.target.parentElement.parentElement;
-    
-            const info_product = {
-                quantity: 1,
-                name: product.querySelector('div p:nth-child(2)').textContent,
-                price: product.querySelector('div p:nth-child(1)').textContent,
-                image: product.parentElement.querySelector('img').src,
-            }
-            
-            all_products_cart = [...all_products_cart, info_product];
 
-            render_shopping_cart();
-            calculate_total(info_product.price , info_product.quantity, '+');
-            shopping_cart_container.classList.remove('inactive');
-        }
-    });
-    //desde la vista de detalles del producto
-    product_detail.addEventListener('click', e=> {
-        if(e.target.classList.contains('add-to-cart-button')){
-            const product = e.target.parentElement;
-    
-            const info_product = {
-                quantity: 1,
-                name: product.querySelector('div p:nth-child(2)').textContent,
-                price: product.querySelector('div p:nth-child(1)').textContent,
-                image: product.parentElement.querySelector('img:nth-child(2)').src,
-            }
-            
-            all_products_cart = [...all_products_cart, info_product];
-            
-            render_shopping_cart();
-            calculate_total(info_product.price , info_product.quantity, '+');
-            close_product_detail();
-            shopping_cart_container.classList.remove('inactive');
-        }
-    });
-    
-}
-/*elminar productos del carrito*/
-function delete_product_cart(){
-    my_order_content.addEventListener('click', e=> {
-        if(e.target.classList.contains('delete-product-cart')){
-            const product = e.target.parentElement;
-            const name_delete = product.querySelector('div p:nth-child(2)').textContent
-            const removed_product = all_products_cart.find((producto) => producto.name == name_delete);
-
-            calculate_total(removed_product.price , removed_product.quantity, '-');
-            all_products_cart = all_products_cart.filter((producto) => producto.name !== name_delete);
-
-            render_shopping_cart();
-        }
+function select_img_card(){
+    img_card = cards_container.querySelectorAll('img');
+    img_card.forEach(button => {
+        button.addEventListener('click',render_detail_card);
+        
     });
 }
+
+function render_detail_card(e){
+    const product = e.target.parentElement;
+    const id_btn = product.querySelector('figure:nth-child(1) img').id;
+    const product_view = product_list.find(product => product.id === id_btn);
+  
+    //obtengo la ubicacion de los elementos en detalles del producto
+    const price  = product_detail.querySelector('.product-info p:nth-child(1)');
+    const name  = product_detail.querySelector('.product-info p:nth-child(2)');
+    const description  = product_detail.querySelector('.product-info p:nth-child(3)');
+    const img = product_detail.querySelector('img:nth-child(2)');
+    const selector = product_detail.querySelector('.add-to-cart-button');
+
+    price.textContent = '$' + product_view.price;
+    name.textContent  = product_view.name ;
+    description.textContent  = product_view.description ;
+    img.src = product_view.image; // Cambia esto por la nueva URL que deseas usar
+    img.alt = 'imagen del producto'; // Cambia el texto alternativo de la imagen
+    selector.setAttribute('id', product_view.id);
+}
+
+// selecionar boton para ver detalles del producto
+function select_btn_detail(){
+    btn_add_product_detail.addEventListener('click',upload_cart);
+    close_product_detail();
+}
+
+function update_btn_card(){
+    btn_add = cards_container.querySelectorAll('.btn_add_cart');
+
+    btn_add.forEach(button => {
+        button.addEventListener('click',upload_cart);
+        
+    });
+}
+
+function upload_cart(e){
+    
+    const id_btn = e.target.id;
+    console.log(id_btn);
+    const add_product = product_list.find(product => product.id === id_btn);
+    // nos devuleve un bool si existe o no el producto
+    if(all_products_cart.some(product => product.id === id_btn)){
+        const index = all_products_cart.findIndex(product => product.id === id_btn);
+        all_products_cart[index].quantity++;
+    }else{
+        add_product.quantity = 1;
+        all_products_cart.push(add_product);
+    }
+    //console.log(all_products_cart);
+    update_number_cart();
+    render_shopping_cart();
+}
+
+function update_number_cart(){
+    let  number = all_products_cart.reduce((acc, product) => acc + product.quantity,0);
+    number_cart.textContent = number;
+}
+
 /*cargar los productos al carrito en el html */ 
 function render_shopping_cart() {
+    shopping_cart_container.classList.remove('inactive');
     //limpiar el contenedor del carrito
     const element = document.querySelector('.my-order-content');
     while(element.firstChild){
@@ -238,40 +213,61 @@ function render_shopping_cart() {
         img__shopping_cart.setAttribute('src', product.image );
 
         const product_price = document.createElement('p');
-        product_price.innerText =  product.price;
+        product_price.innerText =  '$'+ product.price;
+        product_price.classList.add('sub-total-shopping-cart');
+
+        const figure_quantity = document.createElement('figure');
+        figure_quantity.classList.add('figure-quantity');
+
+        const i_more = document.createElement('i');
+        i_more.classList.add('fa', 'fa-plus', 'sumar-cantidad');
+        i_more.setAttribute('style', 'color: #d1d1d1');
+        const product_quantity = document.createElement('input');
+        product_quantity.classList.add('quantity-container');
+        product_quantity.setAttribute('type', 'text');   
+        product_quantity.value = product.quantity;
+        const i_less = document.createElement('i');
+        i_less.classList.add('fa', 'fa-minus', 'restar-cantidad');
+        i_less.setAttribute('style', 'color: #d1d1d1');
         const product_name = document.createElement('p');
         product_name.innerText = product.name;
 
-        const img__shopping_close = document.createElement('img');
-        img__shopping_close.setAttribute('src', './icons/icon_close.png');
-        img__shopping_close.classList.add('delete-product-cart');
+        const button_close = document.createElement('button');
+        button_close.classList.add('btn-delete')
+        const i_button = document.createElement('i');
+        i_button.classList.add('fas', 'fa-trash-alt');
+        i_button.setAttribute('style', 'color: #d1d1d1');
 
         figure.appendChild(img__shopping_cart);
-        shopping_cart_item.append(figure,product_name, product_price,img__shopping_close);
+        figure_quantity.append(i_more,product_quantity ,i_less);
+        button_close.appendChild(i_button);
+        shopping_cart_item.append(figure,product_name,figure_quantity, product_price,button_close);
 
         my_order_content.appendChild(shopping_cart_item);
 
     });  
 }
 
-
 // se maneja for ** of *** para valores o for ** in *** para indices
 // representa lo que viene de la BDD basicamente
 const product_list = [];
 
 product_list.push({
+    id: '001',
     name: 'Bicicleta Montañera',
     price: '180',
     image: 'https://http2.mlstatic.com/D_NQ_NP_2X_622456-MLV47573234374_092021-F.webp',
     description: 'CUADRO DE ALUMINIO SUPER LIVIANO CON CABLEADO INTERNO HORQUILLA SUSPENSION CON BLOQUEO FRENOS DE DISCOS MECANICOS ',
 });
 product_list.push({
+    id: '002',
     name: 'Powerhouse Ups 1500w',
     price: '504',
     image: 'https://http2.mlstatic.com/D_NQ_NP_2X_717980-MLV47977090096_102021-F.webp',
     description: 'sistema de alimentación ininterrumpida (UPS), en un cerramiento, montado y probado en fábrica, diseñado para todo tipo de entornos.'
 });
 product_list.push({
+    id: '003',
     name: 'Camara Digital Sony Ilce',
     price: '390',
     image: 'https://http2.mlstatic.com/D_NQ_NP_2X_745276-MLV52320264720_112022-F.webp',
@@ -279,5 +275,8 @@ product_list.push({
 });
 
 render_products(product_list);
+
+
+
 
 
