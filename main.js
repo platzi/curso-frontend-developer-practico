c557cafea36ff88488882fc3b020a147d4f16392
unknown <jagr511@gmail.com>
2023-09-13 09:52:40 +0200

    666666666666666

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ productList.push({
 });
 productList.push({
   name: 'Compu',
-  price: 620,
+  price: 666666620,
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
