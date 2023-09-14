1eebece8ef2d50e8f418f522fe807ced356c2b52
Carlos Lopez <carloslopezr29@gmail.com>
2023-09-14 14:19:04 -0400

    Se agregaró HTML Product detail & style css carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const navEmail = document.querySelector('.navbar-email');
 const signOutMenu = document.querySelector('.desktop-menu');
 
+// Creación del selector para el menú en versión mobile
+const menuHamIcon =  document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 // Toggle: cambiar entre dos estados (altenar, ya se que sea visible o no el menu sign out)
 navEmail.addEventListener('click', togglesignOutMenu );
+
+
 
 function togglesignOutMenu()
 {
 
     signOutMenu.classList.toggle('inactive')
-
+ 
 }
 
-console.log('hola, mundo');
+//Togle: ahora para hacer visible o no el menú de la versión mobile
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu()
+{
+    mobileMenu.classList.toggle('inactive')
+}
