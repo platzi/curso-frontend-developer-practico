611d22011b696083c7c677f7f3c1a6302f011a35
Nikolas <nikolas2004rodriguez@gmail.com>
2023-09-14 11:38:51 -0500

    Unir menu del carrito de compras

--- main.js
+++ main.js
@@ -4,14 +4,23 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
+}
 
+function toggleCarritoAside(){
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
