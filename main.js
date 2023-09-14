85a56b81ffd4a50bc36f9f5c229c942d971d97ba
Nikolas <nikolas2004rodriguez@gmail.com>
2023-09-14 12:43:10 -0500

    Unir lista de productos

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container');
+
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
@@ -24,3 +26,58 @@ function toggleCarritoAside(){
     mobileMenu.classList.add('inactive');
     aside.classList.toggle('inactive');
 }
+
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'TV',
+    price: 910,
+    image: 'https://www.lg.com/co/images/televisores/md07550828/gallery/medium02.jpg'
+});
+productList.push({
+    name: 'Macbook',
+    price: 999,
+    image: 'https://i0.wp.com/regionalista.cl/web/wp-content/uploads/2022/01/FACH0144-FOTO-1.jpg?resize=696%2C557&ssl=1'
+});
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' +product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productDiv.append(productPrice, productName);
+    
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.append(productDiv, productInfoFigure  );
+    
+        productCard.append(productImg, productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+};
+
+renderProducts(productList);
