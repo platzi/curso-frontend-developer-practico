dbb829c6f0162818ef400221ef680a4d84fbe2d8
julian-uribe47 <juribedio@hotmail.com>
2023-09-14 16:45:42 -0500

    Organización de funcionalidad en menus movil y desktop

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 
 const toggleDesktopMenu = () => {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
 }
 
+const toggleMobileMenu = () => {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+const toggleCarritoAside = () => {
+    const isMObileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMObileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive')
+}
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
