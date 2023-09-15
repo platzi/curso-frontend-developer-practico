b6e73aa45f3a3cf2c02b5860fa49a24e53f346b3
JohanBM <jebm096@gmail.com>
2023-09-15 12:07:17 -0600

    mobile menu fusion

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
+hamMenu.addEventListener('click', toggleMobileMenu)
 navEmail.addEventListener('click', toggleDestopMenu);
 
 //Esta funcion alternará la clase inactive
 function toggleDestopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
 
