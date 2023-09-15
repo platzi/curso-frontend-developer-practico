cc823b2eeeec8d44cc7176373b1230de08e6085f
Alexis Calixto <alexiscfx1@gmail.com>
2023-09-15 12:16:28 -0300

    Html y Css mobile-menu (menu hamburguesa), su mediaQuerie e interactividad con JavaScript

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const menuDesktop = document.querySelector('.desktop-menu')
+const burgerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 //let inactive = document.querySelector('.inactive')    // Cambiar valor de la propiedad del selector inactive
 
 menuEmail.addEventListener('click', toogleDesktopMenu)
+burgerMenu.addEventListener('click', toogleMobileMenu)
 
 function toogleDesktopMenu() {
 //    inactive.style.display = ('block')                // Change the property of 'none' to block
     menuDesktop.classList.toggle('inactive')
+}
+function toogleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
