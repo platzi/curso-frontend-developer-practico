c56b16969bd753c8e9efb52aed5c297e27f6b01f
Alexis Calixto <alexiscfx1@gmail.com>
2023-09-18 22:38:26 -0300

    Agregando Lista de productos en HTML a partir de Arrays

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const mobileMenu = document.querySelector('.mobile-menu')
 const shoppingMenuIcon = document.querySelector('.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
 
+const cardsContainer = document.querySelector('.cards-container')
+
 //let inactive = document.querySelector('.inactive')    // Cambiar valor de la propiedad del selector inactive
 
 menuEmail.addEventListener('click', toogleDesktopMenu)
@@ -40,3 +42,61 @@ function toogleShoppingMenu() {
     }
     aside.classList.toggle('inactive')
 }
+
+const productList = [];
+
+productList.push ({
+    name : 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push ({
+    name : 'Pantalla',
+    price: 200,
+    image: 'https://images.pexels.com/photos/17136622/pexels-photo-17136622/free-photo-of-escritorio-tecnologia-ordenador-monitor.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+})
+productList.push ({
+    name : 'Laptop',
+    price: 1500,
+    image: 'https://images.pexels.com/photos/4061525/pexels-photo-4061525.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+})
+
+function renderProducts(arr) {
+    for (product of arr) {
+        
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+
+        const productImg = document.createElement('img')
+        productImg.setAttribute('src', product.image)
+        
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+
+        const productInfoDiv = document.createElement('div')
+        
+        const productPrice = document.createElement('p')
+        productPrice.innerText =  '$' + (product.price)
+        const productName = document.createElement('p')
+        productName.innerText =(product.name)
+
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+        
+        const productInfoFigure = document.createElement('figure')
+        const productInfoFigureImg = document.createElement('img')
+        productInfoFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg')
+        
+        productInfoFigure.appendChild(productInfoFigureImg)
+
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList);
