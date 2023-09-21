4477939bc9e97379186f7c2e79c95b4350b98ae8
Carlos Lopez <carloslopezr29@gmail.com>
2023-09-20 21:15:49 -0400

    Se agregaron funciones para cerrar el product detail cuando se abran otras ventanas

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ const signOutMenu = document.querySelector('.desktop-menu');
 // Creación del selector para el menú en versión mobile
 const menuHamIcon =  document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+// Selector Icon close product detail
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 // Creación de la constante product detail (aside detail product)
 const ShoppingCartContainer = document.querySelector('#ShoppingCartContainer');
 // Selector del ícono 'carrito de compras'
 const IconShoppingCart = document.querySelector('.navbar-shopping-cart');
 // Selector del contenedor 'cardsContainer' referente a la img e info de los productos
 const cardsContainer = document.querySelector('.cards-container');
+
+// Selector que contiene los detalles de un producto
+const productDetailContainer = document.querySelector('#productDetail');
+
+
 
 
 // Toggle: cambiar entre dos estados (altenar, ya se que sea visible o no el menu sign out)
@@ -17,7 +24,17 @@ navEmail.addEventListener('click', togglesignOutMenu );
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 //Toggle: carrito de compras (mostrar la lista de productos al hacer click al carrito)
 IconShoppingCart.addEventListener('click', toggleShowProducts)
+// Cerrar los detalles del producto al hacer click en el ícono de la X BY MY OWN
+productDetailCloseIcon.addEventListener('click', closeProductDetails);
 
+// Tip: Se puede hacer lo siguiente con cada función toggle:
+// function toggleProductDetail(){
+//     productDetailContainer.classList.toggle("inactive");
+//     signOutMenu.classList.toggle("inactive");
+//     mobileMenu.classList.toggle("inactive");
+//     // ETC..
+
+// }
 
 function togglesignOutMenu()
 {
@@ -43,14 +60,17 @@ function toggleMobileMenu()
         ShoppingCartContainer.classList.add('inactive');
     }
 
+    closeProductDetails ();
+ 
     mobileMenu.classList.toggle('inactive');
 }
 
 // Para mostrar productos agregados al carrito:
 function toggleShowProducts()
 {
-    // Si el menú desktop esta abierto:
+    // Si el menú desktop esta abierto: es porque no contiene la clase inactive
     const isDesktopMenuOpen = !signOutMenu.classList.contains('inactive');
+
     if (isDesktopMenuOpen) 
     {
         signOutMenu.classList.add('inactive');
@@ -64,11 +84,40 @@ function toggleShowProducts()
         mobileMenu.classList.add('inactive');
     }
 
+    // Cerrar el contenedor product detail si se le da click al ícono de shopping cart
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+     
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive');
+    }
+
+    
+    
+    // Para que toggle, cambie a inactive si es que no lo tiene al volver a darle click
     ShoppingCartContainer.classList.toggle('inactive');
+ 
+}
 
 
-    
-    
+
+// función para desplegar los detalles de un producto al hacer click en img
+function openProductDetatilAside()
+{
+    productDetailContainer.classList.remove('inactive');
+
+    // Siempre que se ABRAN los detalles de un producto, pondremos la clase 'inactive' al contenedor de Shopping CArt
+    ShoppingCartContainer.classList.add('inactive');
+
+
+}
+
+// BY MY OWN
+function closeProductDetails ()
+{
+    productDetailContainer.classList.add('inactive');
+
+
 }
 
 // Bloque de código para creación de productos (imgs, description & AddToCart)
@@ -111,6 +160,7 @@ productList.push({
     
 });
 
+
 /*{ <div class="product-card">
 <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
 <div class="product-info">
@@ -140,6 +190,11 @@ function renderProducts(arr){
     // se toma como referencia la lista de productos creada en el array productLIst
 
 
+    // Desplegar la info de un producto al momento de hacer click en su img
+    productImg.addEventListener('click', openProductDetatilAside);
+
+
+    
     // Creación del contenedor <div> para la información del producto
     // Creación del contendor div
     const productInfo = document.createElement('div');
@@ -181,3 +236,5 @@ function renderProducts(arr){
 
 // Inicialización de la función, parametro: producList en este caso
 renderProducts(productList);
+
+
