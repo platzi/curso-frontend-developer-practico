c67ff29f66bd6c68dd769c6589fb3302d62b5a80
luisrichaud <luisrichaud21@gmail.com>
2023-09-21 14:20:25 -0600

    Se integro el carrito de compras al index y al styles y se corrigieron algunas interacciones entre los menpus en JS

--- main.js
+++ main.js
@@ -2,18 +2,48 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoButton = document.querySelector('.navbar-shopping-cart');
+const asideProductDetail = document.querySelector('.product-detail')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-
+menuCarritoButton.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    console.log('Click en navbar-email')
+    const isAsideMenuClose = asideProductDetail.classList.contains('inactive');
+
+    if(!isAsideMenuClose){
+        asideProductDetail.classList.add('inactive');
+    }
+
+    console.log('Click en navbar-email');
     desktopMenu.classList.toggle('inactive'); 
 }
 
 function toggleMobileMenu(){
+    const isAsideMenuClose = asideProductDetail.classList.contains('inactive');
+ 
+    if(!isAsideMenuClose){
+        asideProductDetail.classList.add('inactive');
+    }
+
+    console.log('click en Carrito de compras');
     mobileMenu.classList.toggle('inactive');
 }
 
-console.log('Prueba de JS')
+function toggleCarritoAside(){
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
+    if(!isMobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+
+    if(!isDesktopMenuClose){
+        desktopMenu.classList.add('inactive');
+    }
+
+    console.log('click en Carrito de compras');
+    asideProductDetail.classList.toggle('inactive');
+
+}
