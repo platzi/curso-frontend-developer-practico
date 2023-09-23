ee0bec7878fbceba6f322649da746b70d778c253
= <=>
2023-09-23 13:34:33 -0300

    merging menu on desktop and applying js in emaimenu

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle("inactive");
+}
