ba2c189aebe1918a92904551ab08d2b759480ee2
= <=>
2023-09-24 13:55:05 -0300

    merging the shopping cart and adding events so that the menus do not overlap

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuHamIcon = document.querySelector(".menu");
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCartIcon.addEventListener("click", toggleProductDetail);
 
 function toggleDesktopMenu() {
+  const asideClosed = aside.classList.contains("inactive");
+  if (!asideClosed) {
+    aside.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  const asideClosed = aside.classList.contains("inactive");
+
+  if (!asideClosed) {
+    aside.classList.add("inactive");
+  }
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleProductDetail() {
+  const mobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!mobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
 }
