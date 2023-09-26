f79148cce1e0cf784d923cdc42e6f2700fe2566e
AndreaCampo <andreacampog@gmail.com>
2023-09-26 09:59:27 -0500

    Agrego al proyecto MobileMenu, agrego mediaquery min-641px, corrijo mediaquery max-640px

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ console.log('JS Conectado');
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const burguerMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click',toggleDesktopMenu);
 
+burguerMenuIcon.addEventListener('click',toggleMobileMenu); //Al hacer click en el icono de hamburguesa, ejecuta la función que 'inactiva' el menu Mobile
 
 function toggleDesktopMenu(){
 
     desktopMenu.classList.toggle('inactive');    
 }
 
-//Comentario para evidenciar que realice cambios al original
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');  //Inactivar menu mobile
+    
+}
