666c1bd15c9d16445669f78ec477e267f950f791
juancamilogarcue <juankgarcue@gmail.com>
2023-09-26 14:21:20 -0500

    avance parcial de la lista de productos

--- mian.js
+++ mian.js
@@ -1,13 +1,38 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const hamburguesaMenu = document.querySelector(".menu");
+const MobilMenu = document.querySelector(".mobile-menu")
+const carritoIcon = document.querySelector(".navbar-shopping-cart");
+const asideCarrito = document.querySelector(".product-detail");
+
 
 menuEmail.addEventListener("click", aparecerMenu);
+hamburguesaMenu.addEventListener("click", aparecerMenuHamburguesa);
+carritoIcon.addEventListener("click", aparecerCarritoDeCompras);
 
 function aparecerMenu () {
-    console.log("click")
+    if (!asideCarrito.classList.contains("inactive")) {
+        asideCarrito.classList.add("inactive")
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
-console.log("JS FUNCIONANDO");
+function aparecerMenuHamburguesa () {
+    if (!asideCarrito.classList.contains("inactive")) {
+        asideCarrito.classList.add("inactive")
+    }
+    MobilMenu.classList.toggle("inactive");
+}
+
+function aparecerCarritoDeCompras(){
+    if (!MobilMenu.classList.contains("inactive")) {
+        MobilMenu.classList.add("inactive")
+    } 
+    if (!desktopMenu.classList.contains("inactive")) {
+        desktopMenu.classList.add("inactive")
+    }
+    asideCarrito.classList.toggle("inactive");
+}
+
 
 
