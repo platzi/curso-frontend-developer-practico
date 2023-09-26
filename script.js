5bd31e5776bc82485ec024d5f19d80803ecc9a88
pipeob <javierfelipeortiz17@gmail.com>
2023-09-26 03:22:24 -0500

    Fusion de los menu para desktop  y mobile

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHam = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHam.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
