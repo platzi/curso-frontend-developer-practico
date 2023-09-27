ea9c54eb94e75bf04963c653701fa48548be1685
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2023-09-27 16:05:16 -0600

    Mobile-menu fusion

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 function deployDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
+function deployMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
 
 const navBarEmail = document.querySelector('.navbar-email');
- const desktopMenu = document.querySelector('.desktop-menu');
+const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.burguer-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
- navBarEmail.addEventListener('click', deployDesktopMenu);
+navBarEmail.addEventListener('click', deployDesktopMenu);
+burguerMenu.addEventListener('click', deployMobileMenu); 
