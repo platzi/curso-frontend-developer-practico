f26973258fb47640ae7a8c9a833d16c10c8196e4
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-09-26 21:50:39 -0300

    add mobileMenu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuBurguerIcon = document.querySelector(".menu");
+const burguerMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuBurguerIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  burguerMenu.classList.toggle("inactive");
 }
