426f629d820839d80795f976f17cc11b5b41434d
wvalverder <wvalverder@gmail.com>
2023-09-27 11:10:01 -0500

    Configuracion media Query y Menu Hamburguesa

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamDesktop = document.querySelector('.menu');
+const desktopMobile = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamDesktop.addEventListener('click', tooggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function tooggleMobileMenu() {
+    desktopMobile.classList.toggle('inactive');
 }
 
