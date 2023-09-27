0d7d8b22680bc31e8e38051fa9299d9cb3802cc7
alexscespedes <alexandersenc99@gmail.com>
2023-09-27 18:31:22 -0400

    Merge menu on mobile

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
+//Desktop
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+//Mobile
+const menuHamburgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     console.log('Click')
     desktopMenu.classList.toggle('inactive');
 
+}
+
+function toggleMobileMenu() {
+    console.log('Click')
+    mobileMenu.classList.toggle('inactive');
 }
 
 // console.log('JS funcionando')
