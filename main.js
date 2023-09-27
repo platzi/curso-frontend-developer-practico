42e7b3a8aac170eabb8bdf666695012fcf31e30d
wvalverder <wvalverder@gmail.com>
2023-09-27 10:16:09 -0500

    cambios en el archivo HTML-nav-bar,desktop-menu **Cambio en CSS** y primer cambio en el archivo main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
+
