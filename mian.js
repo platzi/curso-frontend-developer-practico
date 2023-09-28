f5d7a1f5e195fef2a34621329fe216c7e8c42601
juancamilogarcue <juankgarcue@gmail.com>
2023-09-28 18:09:41 -0500

    detalle del producto funcionando sin la informacion correcta

--- mian.js
+++ mian.js
@@ -6,6 +6,10 @@ const carritoIcon = document.querySelector(".navbar-shopping-cart");
 const asideCarrito = document.querySelector(".car-detail");
 const cardsContainer = document.querySelector(".cards-container");
 
+const productDetail = document.querySelector(".product-detail");
+const closeProductDetail = document.querySelector(".product-detail-close")
+
+
 menuEmail.addEventListener("click", aparecerMenu);
 hamburguesaMenu.addEventListener("click", aparecerMenuHamburguesa);
 carritoIcon.addEventListener("click", aparecerCarritoDeCompras);
@@ -42,19 +46,20 @@ productList.push({
     Image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    Nombre: "electric scooter",
+    Nombre: "Camera",
     Price: 1250,
-    Image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    Image: "https://images.pexels.com/photos/1983037/pexels-photo-1983037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 });
 productList.push({
     Nombre: "scooter",
     Price: 6480,
-    Image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    Image: "https://images.pexels.com/photos/18127120/pexels-photo-18127120/free-photo-of-red-vespa-scooter-parked-in-a-cozy-yard-of-a-house.jpeg?auto=compress&cs=tinysrgb&w=600"
 });
 
+/*
 productList.forEach((product) => {
       imprimirProductos = `<div class="product-card">
-          <img src = ${product.Image} alt="">
+          <img src = ${product.Image} alt="" id="p">
           <div class="product-info">
           <div>
             <p>${product.Price}</p>
@@ -66,15 +71,22 @@ productList.forEach((product) => {
         </div>
       </div>`
     cardsContainer.innerHTML += imprimirProductos;
-    })
-/*
+
+    imagenProducto = document.getElementById("p");
+    });
+
+    imagenProducto.addEventListener("click",console.log);
+*/
+
+
 function showProducts (array) {
     for (product of productList){
         const productCard = document.createElement("div");
         productCard.classList.add("product-card");
         
         const img = document.createElement("img");
-        img.setAttribute("src",product.Image);
+        img.setAttribute("src", product.Image);
+        img.addEventListener("click", aparecerProductDetail);
 
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
@@ -104,8 +116,20 @@ function showProducts (array) {
         productCard.appendChild(productInfo);
 
         cardsContainer.appendChild(productCard);
+
     }
 }
 
 showProducts (productList);
-*/
+
+function aparecerProductDetail () {
+    productDetail.classList.remove("inactive");
+}
+
+closeProductDetail.addEventListener("click", OcultarProductDetail)
+
+function OcultarProductDetail () {
+    productDetail.classList.add("inactive");
+}
+
+
