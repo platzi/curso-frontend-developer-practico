d0b30dc4cddc0e3a4631cc649a0d71953a7555a2
juancamilogarcue <juankgarcue@gmail.com>
2023-09-30 17:07:02 -0500

    se trata de hacer una sola funcion para aparecer y desaparecer los diferentes menus

--- mian.js
+++ mian.js
@@ -5,14 +5,29 @@ const MobilMenu = document.querySelector(".mobile-menu")
 const carritoIcon = document.querySelector(".navbar-shopping-cart");
 const asideCarrito = document.querySelector(".car-detail");
 const cardsContainer = document.querySelector(".cards-container");
-
 const productDetail = document.querySelector(".product-detail");
-const closeProductDetail = document.querySelector(".product-detail-close")
 
 
-menuEmail.addEventListener("click", aparecerMenu);
-hamburguesaMenu.addEventListener("click", aparecerMenuHamburguesa);
+
+menuEmail.addEventListener("click", ghost);
+//hamburguesaMenu.addEventListener("click", aparecerMenuHamburguesa);
 carritoIcon.addEventListener("click", aparecerCarritoDeCompras);
+
+
+function ghost (){
+    if (!asideCarrito.classList.contains("inactive") && desktopMenu.classList.contains("inactive") ) {
+        asideCarrito.classList.add("inactive")
+    } 
+    desktopMenu.classList.toggle("inactive");
+} 
+/*
+function ghost1 (){
+    if (!asideCarrito.classList.contains("inactive") && !desktopMenu.classList.contains("inactive") ) {
+        asideCarrito.classList.add("inactive")
+    } 
+    desktopMenu.classList.toggle("inactive");
+}     
+    
 
 function aparecerMenu () {
     if (!asideCarrito.classList.contains("inactive")) {
@@ -27,12 +42,10 @@ function aparecerMenuHamburguesa () {
     }
     MobilMenu.classList.toggle("inactive");
 }
-
+*/
 function aparecerCarritoDeCompras(){
-    if (!MobilMenu.classList.contains("inactive")) {
+    if (!MobilMenu.classList.contains("inactive") && !desktopMenu.classList.contains("inactive")) {
         MobilMenu.classList.add("inactive")
-    } 
-    if (!desktopMenu.classList.contains("inactive")) {
         desktopMenu.classList.add("inactive")
     }
     asideCarrito.classList.toggle("inactive");
@@ -43,40 +56,21 @@ const productList = [];
 productList.push({
     Nombre: "bike",
     Price: 120,
-    Image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    Image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    Description: "With its practical position, this bike also fulfills a decorative function, add your hall or workspace"  
 });
 productList.push({
     Nombre: "Camera",
     Price: 1250,
-    Image: "https://images.pexels.com/photos/1983037/pexels-photo-1983037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    Image: "https://images.pexels.com/photos/1983037/pexels-photo-1983037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Description: "This is a camera"
 });
 productList.push({
     Nombre: "scooter",
     Price: 6480,
-    Image: "https://images.pexels.com/photos/18127120/pexels-photo-18127120/free-photo-of-red-vespa-scooter-parked-in-a-cozy-yard-of-a-house.jpeg?auto=compress&cs=tinysrgb&w=600"
+    Image: "https://images.pexels.com/photos/18127120/pexels-photo-18127120/free-photo-of-red-vespa-scooter-parked-in-a-cozy-yard-of-a-house.jpeg?auto=compress&cs=tinysrgb&w=600",
+    Description: "This is a scooter"
 });
-
-/*
-productList.forEach((product) => {
-      imprimirProductos = `<div class="product-card">
-          <img src = ${product.Image} alt="" id="p">
-          <div class="product-info">
-          <div>
-            <p>${product.Price}</p>
-            <p>${product.Nombre}</p>
-          </div>
-          <figure>
-            <img src="./icons/bt_add_to_cart.svg" alt="">
-          </figure>
-        </div>
-      </div>`
-    cardsContainer.innerHTML += imprimirProductos;
-
-    imagenProducto = document.getElementById("p");
-    });
-
-    imagenProducto.addEventListener("click",console.log);
-*/
 
 
 function showProducts (array) {
@@ -118,18 +112,81 @@ function showProducts (array) {
         cardsContainer.appendChild(productCard);
 
     }
+    
 }
 
 showProducts (productList);
 
 function aparecerProductDetail () {
+
+    const productDetailClose = document.createElement("div");
+    productDetailClose.classList.add("product-detail-close");
+
+    const imgClose = document.createElement("img");
+    imgClose.setAttribute("src", "./icons/icon_close.png");
+
+    const imgProducto = document.createElement("img");
+    imgProducto.setAttribute("src", product.Image);
+    
+    const productInfoFromProductDetail = document.createElement("div");
+    productInfoFromProductDetail.classList.add("product-info-from-product-detail");
+
+    const textoPrice = document.createElement("p");
+    textoPrice.innerText = "$" + product.Price;
+
+    const textoNombre = document.createElement("p");
+    textoNombre.innerText = product.Nombre;
+
+    const textoDescription = document.createElement("p");
+    textoDescription.innerText = product.Description;
+
+    const buttonAddToCart = document.createElement("button");
+    buttonAddToCart.classList.add("primary-button-add-to-cart-button");
+
+    const imgAddToCart = document.createElement("img");
+    imgAddToCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    const textoAddToCart = document.createElement("p");
+    textoAddToCart.innerText = "Add to Cart";
+
+    buttonAddToCart.appendChild(textoAddToCart);
+    buttonAddToCart.appendChild(imgAddToCart);
+
+    productInfoFromProductDetail.appendChild(textoPrice);
+    productInfoFromProductDetail.appendChild(textoNombre);
+    productInfoFromProductDetail.appendChild(textoDescription);
+    productInfoFromProductDetail.appendChild(buttonAddToCart);
+
+    productDetailClose.appendChild(imgClose);
+
+    productDetail.appendChild(productDetailClose);
+    productDetail.appendChild(imgProducto);
+    productDetail.appendChild(productInfoFromProductDetail);
+
     productDetail.classList.remove("inactive");
-}
 
-closeProductDetail.addEventListener("click", OcultarProductDetail)
+    const ProductDetailCloseClose = document.querySelector(".product-detail-close");
+    ProductDetailCloseClose.addEventListener("click", OcultarProductDetail)
 
-function OcultarProductDetail () {
+    function OcultarProductDetail () {
     productDetail.classList.add("inactive");
+    }
 }
+
+/*
+      <div class="product-detail-close">
+        <img src="./icons/icon_close.png" alt="close">
+      </div>
+      <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike">
+      <div class=".product-info-from-product-detail">
+        <p>$35,00</p>
+        <p>Bike</p>
+        <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+        <button class="primary-button add-to-cart-button">
+          <img src="./icons/bt_add_to_cart.svg" alt="add to cart">
+         <p>Add to cart</p>
+        </button>
+      </div>
+*/
 
 
