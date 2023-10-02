18a3bde7a78fd0cf91875f3fec671b89d94ad67d
fernando.salazar <fernando.salazar@rappi.com>
2023-10-02 18:05:16 -0500

    Funcion_Menu_Mobile

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuMobileLogo = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuMobileLogo.addEventListener('click', toggleMobilepMenu);
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobilepMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
