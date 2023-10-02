b7472dcd4bd74606e4c48c2429c06fdadce038ba
LuisCarlos <carlosrm0994@gmail.com>
2023-10-02 12:58:06 -0500

    Se fusiono el aside del carrito de compras al menu principal, se aplico js para que aparezcan y desaparezcan los diferentes menus

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobilepMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside)
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+  if(!aside.classList.contains("inactive")){
+    aside.classList.add("inactive");
+  }
 }
 
 function toggleMobilepMenu(){
     mobileMenu.classList.toggle("inactive");
-    console.log("Menu")
+    console.log("Menu");
+    if(!aside.classList.contains("inactive")){
+        aside.classList.add("inactive");
+    }
+}
+
+function toggleCarritoAside (){
+    aside.classList.toggle("inactive");
+    if(!mobileMenu.classList.contains("inactive")){
+        mobileMenu.classList.add("inactive");
+    }
+    if(!desktopMenu.classList.contains("inactive")){
+        desktopMenu.classList.add("inactive");
+    }
 }
