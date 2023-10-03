bb4ff358c30e8ab7086e7d880f7dd6afe7486427
fernando.salazar <fernando.salazar@rappi.com>
2023-10-03 11:50:16 -0500

    HTML_to_Array_IN_JS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuMobileLogo = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarrito = document.querySelector('.navbar-shopping-cart');
 const asideCarrito = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -42,3 +43,60 @@ function toggleAsideCarrito(){
     }
     asideCarrito.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'PC Gammer',
+    price: '500',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'iPhone 15 Pro Max',
+    price: '1199',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+// Atajo para recorrer toda la lista
+// Aqui se ingresan los productos desde el JS, es decir se esta ***CONSTRUYENDO*** el HTML
+// Revisar HTML comentado en el index.html clase product-card
+function renderProducts (arr){
+    for (product of arr) {
+        const productCar = document.createElement('div');
+        productCar.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image); // Agrego al src el link que se encuentra dentro del elemento producto en Image
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const producInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+        producInfoDiv.appendChild(productPrice); // Agrega dentro del div, el elemento p llamado productPrice
+        producInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(producInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCar.appendChild(productImg);
+        productCar.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCar);
+    
+    }
+}
+
+renderProducts(productList);
