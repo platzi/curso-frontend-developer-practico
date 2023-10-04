64ab7f6b7624acf58cd9c94ce8bb1daf6e8148da
Mariano Fuentes <hardhopeid@gmail.com>
2023-10-03 19:05:10 -0500

    Lista de productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuBurger = document.querySelector ('.menu');
 const mobileMenu = document.querySelector ('.mobile-menu');
 const shoppingCart = document.querySelector ('.navbar-shopping-cart');
 const productDetail = document.querySelector ('.product-detail');
+const cardsContainer = document.querySelector ('.cards-container');
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -47,3 +49,64 @@ function toggleProductDetail() {
 
     productDetail.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'PC GAMER',
+    price: 620,
+    image: 'https://images.pexels.com/photos/7238759/pexels-photo-7238759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'TV PLUS',
+    price: 220,
+    image: 'https://images.pexels.com/photos/17233395/pexels-photo-17233395/free-photo-of-flores-vintage-retro-television.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement ('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
