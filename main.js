80311725471214fca149bdf004bbece3bf6b5eda
unknown <midnight.qva@gmail.com>
2023-10-04 22:02:13 +1345

    Menu desktop y mobile, falta el del carrito con JS

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const carritoMenu = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burguerMenu.addEventListener("click", toggleMobileMenu);
+carritoMenu.addEventListener("click", toggleAsideMenu);
+
 
 function toggleDesktopMenu(){
-    console.log("Click")
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive")
+}
+
+function toggleAsideMenu(){
+    aside.classList.toggle("inactive")
 }
