510192764cb35cf497261b81bbf374fc92ebcd80
luis-ran1 <luisgabrielrangel212@gmail.com>
2023-10-04 20:33:46 -0500

    mobile function done

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.icon-menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
    menuDesktop.classList.toggle('inactive')
+}
+
+/* Function for mobile Menu */
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+function toggleMobileMenu(){
+   menuMobile.classList.toggle('inactive');
 }
