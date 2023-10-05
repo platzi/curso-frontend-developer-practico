64ddcae451f827b9e6c8e0ba8874f152da392a87
Dave Dicelis <dave.dicelis@gmail.com>
2023-10-05 15:52:26 -0500

    Agregamos el aside y carrito de compras

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 
@@ -10,13 +12,41 @@ const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click' , toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click' , toggleCarritoAside);
 
 function toggleDesktopMenu(){
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 
 
 function toggleMobileMenu(){
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+
+    const ismobileMenuClosed = mobileMenu.classList.contains('inactive');
+   
+
+
+    if(!ismobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+
 }
