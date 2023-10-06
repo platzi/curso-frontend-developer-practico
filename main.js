eca001ff6548f39f8d3c5835f725a033084aeab7
Victor Marquina <vmz131200@outlook.com>
2023-10-06 15:36:57 -0600

    Clase 13 - Responsive menus entre mobile y desktop

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
-function toggleDesktopMenu(){
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
