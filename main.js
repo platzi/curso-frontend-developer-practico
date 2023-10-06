ab073b55ebb10922b9d113e05134db815f495d03
shou <shou.developer@gmail.com>
2023-10-06 18:46:53 -0500

    Se agrega la lista-actual-de-pedido y su interacción con los demás menúes del proyecto a través de JS

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const cartOrderList = document.querySelector('.product-detail');
 
 function toggleDesktopMenu() {
+    const cartListIsClosed = cartOrderList.classList.contains('inactive');
+    
+    if (!cartListIsClosed) {
+        cartOrderList.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
-
 function toggleMobileMenu() {
+    const cartListIsClosed = cartOrderList.classList.contains('inactive');
+    
+    if (!cartListIsClosed) {
+        cartOrderList.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCartOrderList() {
+    const mobileMenuIsClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!mobileMenuIsClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    cartOrderList.classList.toggle('inactive');
 }
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenuIcon.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toggleCartOrderList);
