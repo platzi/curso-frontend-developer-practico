61b85026b29c8d7b48f64eb32ab6d68188071b45
Samuel Rivas <robertbbenn@gmail.com>
2023-10-10 03:34:55 -0500

    we have suffered a lot with this class

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const menuHamIcon = document.querySelector('.menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-const aside = document.querySelector('.product-detail')
+const shoppingCardContainer = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 // EVENT LISTENERS PARA MENUS Y LISTA DE COMPRAS
@@ -14,13 +14,13 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 // MOSTRAR Y OCULTAR MENU DESKTOP
 function toggleDesktopMenu() {
-  aside.classList.add('inactive')
+  shoppingCardContainer.classList.add('inactive')
   desktopMenu.classList.toggle('inactive');
 }
 
 //MOSTRAR Y OCULTAR MENU MOBILE
 function toggleMobileMenu() {
-  aside.classList.add('inactive')
+  shoppingCardContainer.classList.add('inactive')
   mobileMenu.classList.toggle('inactive')
 }
 
@@ -28,7 +28,7 @@ function toggleMobileMenu() {
 function toggleCarritoAside() {
   desktopMenu.classList.add('inactive')
   mobileMenu.classList.add('inactive')
-  aside.classList.toggle('inactive')
+  shoppingCardContainer.classList.toggle('inactive')
 }
 
 const productList = [];
