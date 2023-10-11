22ae1897f5ff537f635638b4837b3595d55302a9
Melisa500 <melisabarrera24@gmail.com>
2023-10-11 11:25:28 -0300

    show and hide dropdown menus

--- main.js
+++ main.js
@@ -1,15 +1,52 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileIconMenu = document.querySelector('.menu');
+const menuShoppingCartIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 mobileIconMenu.addEventListener('click', toggleMobileMenu);
+menuShoppingCartIcon.addEventListener('click', toggleShoppingCartAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    const isShoppingCartOpen = !aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
+
+        if(isShoppingCartOpen){
+            aside.classList.add('inactive');
+        }
 }
 
+function toggleShoppingCartAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive'); 
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+}
+
+
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
 }
+
