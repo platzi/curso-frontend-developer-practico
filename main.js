45bd5edc945645b64645bf8d2fcbc8ff525c0be3
Nestor Ardila <nestor.ardila.it@gmail.com>
2023-10-15 10:14:43 -0500

    Se realiza la inclusion de diferentes productos, se crea un titulo para la pagina y se cambia el color de los elementos del menu de productos y se adiciona uno

--- main.js
+++ main.js
@@ -85,6 +85,31 @@ productList.push({
     price: 20000000,
     image: 'https://www.las2orillas.co/wp-content/uploads/2023/09/ford-expedition-portada.jpg',
 });
+productList.push({
+  name: 'Yacht',
+  price: 5000000000,
+  image: 'https://media.revistagq.com/photos/6364cef59b80b43a7d25bc57/16:9/w_2560%2Cc_limit/el-yate-de-cristiano-ronaldo.jpg',
+});
+productList.push({
+  name: 'Airplane',
+  price: 3000000000,
+  image: 'https://i.ebayimg.com/images/g/XvQAAMXQQJBQ73Ru/s-l500.jpg',
+});
+productList.push({
+  name: 'Parachute',
+  price: 200000,
+  image: 'https://www.coats.com/-/media/Coats/End-Use/Household-and-Recreation/Parachutes/Household-and-Rec---Outdoor-Goods-parachutes.jpg?rev=-1&width=600&height=460&op=crop',
+});
+productList.push({
+  name: 'Electric Motorcycle',
+  price: 7000000,
+  image: 'https://biologica.com.co/wp-content/uploads/2023/07/IMG_1284a.jpeg',
+});
+productList.push({
+  name: 'Hot Air Balloon',
+  price: 500000,
+  image: 'https://i.ebayimg.com/images/g/3MUAAOSwgB5cVNXt/s-l1600.jpg',
+});
 /*
 <div class="product-card">
         <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
