5a5996202604f4b6b39099f0f5b7e272b683f007
Franco Mezzi <mezzifrancodev@gmail.com>
2023-10-16 16:04:57 -0300

    Agregamos el menu de mobile y se abre y se cierra. Desaparece en desktop y viceversa

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const navEmail = document.querySelector(".navbar-email");
 const inactive = document.querySelector(".inactive");
+const menuHamIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 
 function toggleDesktopMenu(){
     inactive.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
+
 
 
