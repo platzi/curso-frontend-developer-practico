cb2cc79d818bf2e795befaf003f1663054436258
Balam <poblanoeslavaa@gmail.com>
2023-10-16 13:10:18 -0600

    ultimos cambios

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const productDetailCloseIcon = document.querySelector(".product-detail-close")
 const mobileMenu = document.querySelector(".mobile-menu");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
+const productDetailContainer = document.querySelector("#productDetail");
 const cardsContainer = document.querySelector(".cards-container");
 
 
@@ -12,7 +14,7 @@ const cardsContainer = document.querySelector(".cards-container");
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
-
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside);
 
 
 function toggleDesktopMenu() {
@@ -26,6 +28,9 @@ function toggleMobileMenu() {
     if (!isAsideClosed) {
         shoppingCartContainer.classList.add("inactive");
     } 
+
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle("inactive");
 
 }
@@ -37,7 +42,23 @@ function toggleCarritoAside() {
         mobileMenu.classList.add("inactive");
     } 
 
+    const isProductDetailClosed = productDetailContainer.classList.contains("inactive");
+
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add("inactive");
+    } 
+
     shoppingCartContainer.classList.toggle("inactive");
+
+}
+
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add("inactive");
+    productDetailContainer.classList.remove("inactive")
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add("inactive")
 
 }
 
@@ -93,6 +114,7 @@ function renderProducts(arr) {
     
         const productImg = document.createElement("img");
         productImg.setAttribute("src", product.image);
+        productImg.addEventListener("click", openProductDetailAside);
     
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
