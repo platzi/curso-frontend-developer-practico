2fa5122da1f6d7f2f1c7cbedf1fc4027ae05b453
fer8614 <fernando.inversionmf@gmail.com>
2023-10-16 14:09:51 -0700

    desktop mobile

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 
 menuEmail.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive');
+})
+
+menuHamIcon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
 })
