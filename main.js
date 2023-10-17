372c822663d0c9e1cc474976f94508a5b8889691
marco perez <mp4667596@gmail.com>
2023-10-17 11:32:37 -0300

    menu mobile javascript

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
-const destopMenu = document.querySelector(".desktop-menu")
+const destopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-menuEmail.addEventListener("click", toggleDestopMenu)
+menuEmail.addEventListener("click", toggleDestopMenu);
+menuHamIcon.addEventListener("click", togglemobileMenu);
 
 function toggleDestopMenu () {
     destopMenu.classList.toggle("inactive")
+
+}
+function togglemobileMenu () {
+    mobileMenu.classList.toggle("inactive")
 
 }
