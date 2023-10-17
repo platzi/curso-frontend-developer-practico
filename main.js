58398463de3a9f9c852563014aa5b2bd1497b0c7
raquelgm88 <raquel.rgm7@gmail.com>
2023-10-17 12:25:46 +0200

    Merge menu on mobile

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 // VARIABLES
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 // FUNCIONES
@@ -10,7 +12,12 @@ function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
 
 
 // EVENTOS
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
