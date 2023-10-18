87b9afe1f819724526b1c70f85babcb7b3ec81d1
fer8614 <fernando.inversionmf@gmail.com>
2023-10-18 14:42:48 -0700

    shopping cart

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 
@@ -11,4 +13,10 @@ menuEmail.addEventListener('click', () => {
 
 menuHamIcon.addEventListener('click', () => {
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+})
+
+menuCarIcon.addEventListener('click', () => {
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
 })
