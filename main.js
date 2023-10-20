cbf4efefa67a0a0550f3c285f4dce3523501a03b
edwinmg27 <edwinmg27@gmail.com>
2023-10-19 21:11:50 -0400

    cambios en menu mobile

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu'); // constante para s
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 menuEmail.addEventListener('click', toggleDeskptopMenu); // 
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -11,18 +11,18 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDeskptopMenu(){
-	const isAsideClosed = aside.classList.contains('inactive');
+	const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 	if(!isAsideClosed) {
-		aside.classList.add('inactive');
+		shoppingCartContainer.classList.add('inactive');
 	}	
 	//funcion para agregar la clase inactive de modo toggle(que cambie al hacer click)
 	desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-	const isAsideClosed = aside.classList.contains('inactive');
+	const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 	if(!isAsideClosed) {
-		aside.classList.add('inactive');
+		shoppingCartContainer.classList.add('inactive');
 	}
 	mobileMenu.classList.toggle('inactive')
 }
@@ -34,7 +34,7 @@ function toggleCarritoAside() {
 		mobileMenu.classList.add('inactive')
 	}
 
-	aside.classList.toggle('inactive')
+	shoppingCartContainer.classList.toggle('inactive')
 }
 
 // Creacion de array para lista de productos
