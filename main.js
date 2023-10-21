67a902b784aa1622cdbf18eb71372c48832c4067
gaorte <gaorte@live.com.mx>
2023-10-21 02:08:09 -0500

    Se maqueta html desde javascript y se crea un arreglo para dejar listo el front para consumir una api de base de datos

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
+
 
 menuEmail.addEventListener('click', showDesktopMenu);
 menuHamIcon.addEventListener('click', showMobileMenu);
@@ -44,7 +46,8 @@ function showCarritoAside() {
 
     aside.classList.toggle('inactive');
 }
-
+/* Aqui crea un arreglo por medio de una variable de nombre productList y le inserta 3 objetos al arreglo por medio de 
+ el metodo .push*/
 const productList = [];
 productList.push({
 
@@ -81,23 +84,65 @@ productList.push({
   </div>
 </div> */
  
+ /*Aqui se crea un ciclo for of el cual recorre desde el primer elemento del arreglo hasta el ultimo como variable contenedora
+ se le pone de nombre "product" y el segundo elemento de ciclo for es el nombre del arreglo productList*/
  
-  for(product of productList){
-    const productCart = document.createElement('div'); 
-    productCart.classList.add('product-card');
+ function mostrarProductos(arreglo){
 
-    // product= {name, price, image} ->product.image
-    const img = document.createElement('img');
-    img.setAttribute('src', product.image);
+    for(product of arreglo){
 
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
+        /*Dentro de este ciclo for el primer paso es ir creando los elementos html desde javascript de entrada se crea una variable
+        con el nombre similar al de una clase contenedora productCard y lo hace manipulando el DOM con el metodo .createElement,
+        en este caso crea un div y a la variable le asigna por medio de la propiedad .classlist y el metodo .add el nombre de esa clase */ 
+    
+        const productCard = document.createElement('div'); 
+        productCard.classList.add('product-card');
+    
+        /* Aqui se crea el elemento img del html por medio del manipulacion del DOM y el metodo .createElement y la asigna a una variable
+         productImg y se le agrega el source de ubicacion del archivo mediante el atributo .setAttribute y se le agrega la bandera del for
+         a la propiedad image de los objetos que se crearon*/
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        /*Nuevamente se crea primero un div por medio de una variable y un metodo, esto se repite constantemente hasta lograr la estructura 
+        deseada, lo mismo en la segunda linea se asigna una clase al div por medio de un metodo add, y se repite y se repite con toda la 
+        estructura*/
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+    
+        /* En esta parte se observa que crea una variable arriba para insertar un parrafo y con la ayuda de la propiedad .innerText de 
+        manipulacion de DOM inserta texto y concatena a la bandera del for la propiedad price del objeto que contiene el precion */
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        /*La propiedad .appendChild adjunta un eleento html a un contenedor padre*/ 
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        /*Aqui nuevamente previamente crean elementos html por medio de variables y metodos y despues asignan esos elementos hijos a
+        un contenedor padre*/
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+      } 
+ }
 
-    const productInfoDiv = document.createElement('div');
-
-    const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price;
-    const productName = document.createElement('p');
-    productName.innerText = '$' + product.name;
-
-  }
+    mostrarProductos(productList);
