4e04d48a90ca35fc0d11afa68e4c9ec62a6cf0f3
josemasster <joseh.jgvr55@gmail.com>
2023-10-20 22:42:51 -0500

    Actualizacion de fusion de componentes del curso de javascript de Platzi

---
+++ main.js
@@ -0,0 +1,113 @@
+const navEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const MobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+const cardContainer = document.querySelector('.cards-container')
+
+const productList = []; 
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'Screen',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'NoteBook',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'Screen',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'NoteBook',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+navEmail.addEventListener('click', toggleDesktopMenu)
+menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+
+function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+    MobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = MobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+
+    }
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+
+    }
+    aside.classList.toggle('inactive');
+}
+
+function renderProducts(arr){
+    for(product of productList){
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+    
+        const ProductImg = document.createElement('img');
+        ProductImg.setAttribute('src', product.image);
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+    
+        const productInfoDiv = document.createElement('div')
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price
+        const productName = document.createElement('p')
+        productName.innerText = '$' + product.name
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+        const productInfoFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+        productInfoFigure.appendChild(productImgCart)
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+        productCard.appendChild(ProductImg)
+        productCard.appendChild(productInfo)
+        cardContainer.appendChild(productCard)
+    
+    }
+
+}
+
+renderProducts(productList)
