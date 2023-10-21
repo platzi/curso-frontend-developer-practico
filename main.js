30793efc7ec8e2c7b3445ace194a3d34730a6ef4
Jeison <jei_son1516@hotmail.com>
2023-10-21 02:57:30 -0400

    Merge cart menu into main project
    - Consolidated class 13 html and styles into index.html and styles.css
        files
    - Added an event listener to toggle the cart menu
    - Prevented superposition of cart menu with desktop and mobile menus

--- main.js
+++ main.js
@@ -2,12 +2,23 @@ const navbarEmailElement = document.querySelector('.navbar-email');
 const desktopMenuElement =document.querySelector('.desktop-menu');
 
 const hamburgerElement = document.querySelector('.menu');
-const mobileMenuElement = document.querySelector('.mobile-menu')
+const mobileMenuElement = document.querySelector('.mobile-menu');
+
+const navbarCartElement = document.querySelector('.navbar-shopping-cart');
+const cartMenuElement = document.querySelector('.product-detail');
 
 navbarEmailElement.addEventListener('click', () => {
     desktopMenuElement.classList.toggle('inactive')
+    cartMenuElement.classList.add('inactive')
 })
 
 hamburgerElement.addEventListener('click', () => {
     mobileMenuElement.classList.toggle('inactive')
+    cartMenuElement.classList.add('inactive')
+})
+
+navbarCartElement.addEventListener('click', () => {
+    cartMenuElement.classList.toggle('inactive')
+    mobileMenuElement.classList.add('inactive')
+    desktopMenuElement.classList.add('inactive')
 })
