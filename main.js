c0e901851d2f26a280a6b0aa6977bafada83fdee
Jeison <jei_son1516@hotmail.com>
2023-10-22 04:01:41 -0400

    Refactor Product class

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const mobileMenuElement = document.querySelector('.mobile-menu');
 
 const navbarCartElement = document.querySelector('.navbar-shopping-cart');
 const cartMenuElement = document.querySelector('.shopping-cart-detail');
+const cardContainerElement = document.querySelector('.cards-container');
+
+const productInfoElement = document.querySelector('.product-detail');
 
 // 
 class Product {
@@ -13,15 +16,24 @@ class Product {
         this.name = name;
         this.price = price;
         this.img = img;
-        this.cardContainerElement = document.querySelector('.cards-container');
-    }
+    } 
+}
 
-    // Method to create and add a product card to the card container
-    addProduct(product) {
+const productList = [
+    new Product('Bike', 120, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2'),
+    new Product('TV', 399, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2'),
+    new Product('Laptop', 650, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2'),
+    new Product('Desktop Computer', 1700, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2'),
+];
+
+// Function to create and add a product card to the card container
+function renderProducts (arr) { 
+    for (const product of arr) {   
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
         
         const productImg = document.createElement('img');
+        productImg.classList.add('product-img')
         productImg.setAttribute('src', product.img);
         
         const productInfo = document.createElement('div');
@@ -42,14 +54,8 @@ class Product {
         productInfoFigure.appendChild(productImgCart);
         productInfo.append(productInfoDiv, productInfoFigure);
         productCard.append(productImg, productInfo);
-        this.cardContainerElement.appendChild(productCard);
-    }
-
-    // Method to display a list of products
-    displayProducts(products) {
-        for (const product of products) {
-          this.addProduct(product);
-        }
+        
+        cardContainerElement.appendChild(productCard);
     }
 }
 
@@ -70,13 +76,4 @@ navbarCartElement.addEventListener('click', () => {
     desktopMenuElement.classList.add('inactive');
 })
 
-// Sample product data
-const productList = [
-    new Product('Bike', 120, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2'),
-    new Product('TV', 399, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2'),
-    new Product('Laptop', 650, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2'),
-    new Product('Desktop Computer', 1700, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2'),
-];
-
-const productDisplay = new Product();
-productDisplay.displayProducts(productList);
+renderProducts(productList)
