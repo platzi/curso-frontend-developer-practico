f56792a755b56c8960e14968198139dc009b003a
Andres Diago <andresdiag@unicauca.edu.co>
2023-10-22 11:39:18 -0500

    :construction: Shopping cart added
    Using JS to close one and open the other, avoid opening both

--- main.js
+++ main.js
@@ -10,4 +10,13 @@ const mobileMenuButton = document.querySelector('.mobile-menu-button');
 const mobileMenu = document.querySelector('.mobile-menu');
 mobileMenuButton.addEventListener('click', function () {
     mobileMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive'); //close product detail
+});
+
+//product detail
+const productDetail = document.querySelector('.product-detail');
+const productDetailButton = document.querySelector('.navbar-shopping-cart');
+productDetailButton.addEventListener('click', function () {
+    productDetail.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive'); //close mobile menu
 });
