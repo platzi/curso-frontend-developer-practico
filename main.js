b324f019e65ba750ec3b9feaf4db8510c384c30a
MaraXGB <32316385+MaraXGB@users.noreply.github.com>
2023-10-24 08:48:02 -0500

    finalizar muestra de product detail mostrando informacion por item

--- main.js
+++ main.js
@@ -4,44 +4,76 @@ const menuBurgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
+const productDetailContainer = document.querySelector('#productDetail');
 const productCardContainer = document.querySelector('.cards-container');
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuBurgerIcon.addEventListener('click',toggleMobileMenu);
 menuCarritoIcon.addEventListener('click',toggleCarritoAside);
-
+productDetailCloseIcon.addEventListener('click', CloseProductDetailAside);
 function toggleDesktopMenu(){
     shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu(){
     shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
 function toggleCarritoAside(){
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
     shoppingCartContainer.classList.toggle('inactive');
 }
 
+
 const productList = [];
 productList.push({
+    id: 'uno',
     name: 'Bike',
     price:'120',
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     alt: 'Imagen de una bicicleta en plano horizontal'
 });
 productList.push({
-    name: 'Bike',
-    price:'120',
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    alt: 'Imagen de una bicicleta en plano horizontal'
+    id: 'dos',
+    name: 'Computer',
+    price:'300',
+    image: 'https://i.linio.com/p/a67d3dd22a047cd654d48d877d7ec559-catalog.webp',
+    alt: 'Laptop ultimo modelo'
 });
 productList.push({
-    name: 'Bike',
-    price:'120',
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    alt: 'Imagen de una bicicleta en plano horizontal'
+    id: 'tres',
+    name: 'Smartphone',
+    price:'200',
+    image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSf6gkIWitiIXPIDod68wLEEdeV5A84Z0__rXWYC_9fXUt-kpcQuXuM_nLTpzm7YBmjiYNHTMWaZihEGyXC5YHzK4vRsyzr0zbM3G09-PHq&usqp=CAE',
+    alt: 'Smartphone ultmo modelo'
 });
+function OpenProductDetailAside(event){
+    const priceP = document.querySelector('.price');
+    const nameP = document.querySelector('.name');
+    const infoP = document.querySelector('.info');
+    const imgProductDetail = document.querySelector('img[alt="imagenProductDetail"]');
+    console.log(event);
+    const productItem = productList.find(product => product.image == event.target.currentSrc);
+    console.log(productItem);
+    imgProductDetail.setAttribute('src',productItem.image);
+    priceP.innerText = '$'+productItem.price;
+    nameP.innerText = productItem.name;
+    infoP.innerText = productItem.alt;
+    shoppingCartContainer.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+
+}
+function CloseProductDetailAside(){
+    
+    
+    productDetailContainer.classList.add('inactive');
+}
+
 //for(of) obtiene el objeto completo del array
 //for(in) obtiene el indice del objeto en el array
 function RenderProductList(arrList)
@@ -49,11 +81,11 @@ function RenderProductList(arrList)
     for(product of arrList){
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
-
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
         productImg.setAttribute('alt', product.alt);
-
+        productImg.setAttribute('id', product.id);
+        productImg.addEventListener('click', OpenProductDetailAside);
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
         
