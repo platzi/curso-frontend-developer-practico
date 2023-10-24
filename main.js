36ad5f02dc875b496890a8419009cf8b90618282
diegocamachod <dcamachod05@hotmail.com>
2023-10-24 09:50:39 -0600

    Avance

---
+++ main.js
@@ -0,0 +1,100 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+     
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+     
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://raudor.com/cdn/shop/products/Marlin5_22_34587_B_Portrait_1024x1024.jpg?v=1657560256',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://fotografias.lasexta.com/clipping/cmsimages01/2021/08/04/F80CD976-6299-407D-AF0D-14F40A563A19/98.jpg?crop=1300,731,x0,y7&width=1900&height=1069&optimize=high&format=webply',
+});
+productList.push({
+    name: 'Compu',
+    price: 620,
+    image: 'https://www.laik.com.ar/wp-content/uploads/2020/07/maxresdefault-5-1080x675.jpg',
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        // product= {name, price, image} -> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+};
+
+renderProducts(productList);
