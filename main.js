e5f4abd1402746b78580974693f061dc408650d6
Ruben Martinez <rub3n888@gmail.com>
2023-10-24 11:51:38 +0200

    Integracion del menu en movil y resolucion de conflictos entre menus

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Toggle Desktop Menu
+
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
@@ -5,4 +7,16 @@ navEmail.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+
+// Toggle Mobile Menu
+
+const desplegableMovil = document.querySelector('.menu');
+const mobilMenu = document.querySelector('.mobile-menu');
+
+desplegableMovil.addEventListener('click', toggleMobile);
+
+function toggleMobile() {
+    mobilMenu.classList.toggle('inactive');
 }
