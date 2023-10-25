6faf1e7993ef51c960378b28d17f2e72d2a12342
Ruben Martinez <rub3n888@gmail.com>
2023-10-25 10:29:32 +0200

    Añadido carrito desplegable y resolucion de conflictos entre menus.

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const desktopMenu = document.querySelector('.desktop-menu');
 navEmail.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
+    const isAsideOpen = !aside.classList.contains('inactive');
+
+    if (isAsideOpen) {
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 
@@ -18,5 +24,31 @@ const mobilMenu = document.querySelector('.mobile-menu');
 desplegableMovil.addEventListener('click', toggleMobile);
 
 function toggleMobile() {
+    const isAsideOpen = !aside.classList.contains('inactive');
+
+    if (isAsideOpen) {
+        aside.classList.add('inactive')
+    }
+
     mobilMenu.classList.toggle('inactive');
+}
+
+// Toggle carrito
+
+const iconoCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+iconoCarrito.addEventListener('click', toggleCarrito);
+
+function toggleCarrito() {
+    const isMobileMenuOpen = !mobilMenu.classList.contains('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+
+    if (isMobileMenuOpen) {
+        mobilMenu.classList.add('inactive')
+    }
+    if (isDesktopMenuOpen) {
+        desktopMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')
 }
