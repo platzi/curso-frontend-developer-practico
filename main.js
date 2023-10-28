0a65c78675126eaa9b4687c94b91de28980fa8a3
samsep10l <mario.maycotteg@gmail.com>
2023-10-28 07:20:41 -0600

    Added functionalities to Shopping Cart Aside menu, Desktop Menu, Mobile Menu, Product Detail Aside Menu in main.js. Added modified style of Product Detail Icon close in styles.css

--- main.js
+++ main.js
@@ -2,38 +2,42 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const mobileMenu = document.querySelector('.mobile-menu');
-const shoppingCardContainer = document.querySelector('#shopping-cart-container')
-const cardContainer = document.querySelector('.cards-container')
+const shoppingCardContainer = document.querySelector('#shopping-cart-container');
+const productDetailContainer = document.querySelector('#product-detail');
+const cardContainer = document.querySelector('.cards-container');
 const productList = [];
 
 productList.push({
     name: 'Bike',
     price: 120,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+});
 productList.push({
     name: 'Pantalla',
     price: 220,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+});
 productList.push({
     name: 'Computador',
     price: 620,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+});
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
-    const isShoppingCardContainerClosed = shoppingCardContainer.classList.contains('inactive')
+    const isShoppingCardContainerClosed = shoppingCardContainer.classList.contains('inactive');
 
     if (!isShoppingCardContainerClosed) {
         shoppingCardContainer.classList.add('inactive')
     }
 
+    closeProductDetailAside()
     desktopMenu.classList.toggle('inactive')
 }
 
@@ -44,20 +48,33 @@ function toggleMobileMenu() {
         shoppingCardContainer.classList.add('inactive')
     }
 
+    closeProductDetailAside()
     mobileMenu.classList.toggle('inactive')
 }
 
 function toggleCarritoAside() {
     const mobileMenuClosed = mobileMenu.classList.contains('inactive');
-    const desktopMenuClosed = desktopMenu.classList.contains('inactive')
+    const desktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
 
     if (!mobileMenuClosed) {
         mobileMenu.classList.add('inactive')
     } else if (!desktopMenuClosed) {
         desktopMenu.classList.add('inactive')
+    } else if (!isProductDetailClosed) {
+        closeProductDetailAside()
     }
 
     shoppingCardContainer.classList.toggle('inactive')
+}
+
+function openProductDetailAside() {
+    shoppingCardContainer.classList.add('inactive')
+    productDetailContainer.classList.remove('inactive')
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive')
 }
 
 function renderProducts(arr) {
@@ -72,18 +89,19 @@ function renderProducts(arr) {
         const iconAddCart = document.createElement('img') // child: productInfoFigure --> child: productInfo --> child: productCard
         const routeIconAddCart = './icons/bt_add_to_cart.svg'
     
-        productCard.classList.add('product-card')
+        productCard.classList.add('product-card') //add class 'product-card'
         
-        img.setAttribute('src', product.image)
+        img.setAttribute('src', product.image) //source of the image
     
-        productInfo.classList.add('product-info')
+        productInfo.classList.add('product-info') //add class 'product-info'
     
-        productInfoDivName.innerText = product.name
+        productInfoDivName.innerText = product.name //add text of the value in key 'name' in object contained in array productList
     
-        productInfoDivPrice.innerText = '$' + product.price
+        productInfoDivPrice.innerText = '$' + product.price //add text of the value in key 'price' in object contained in array productList
     
-        iconAddCart.setAttribute('src', routeIconAddCart)
-    
+        iconAddCart.setAttribute('src', routeIconAddCart) //set atribute source to get image contained in variable routeIconAddCart
+
+        // Appending children of different components (from inside to outside)
         productInfoFigure.appendChild(iconAddCart)
         productInfoDiv.appendChild(productInfoDivName)
         productInfoDiv.appendChild(productInfoDivPrice)
@@ -92,6 +110,9 @@ function renderProducts(arr) {
         productCard.appendChild(img)
         productCard.appendChild(productInfo)
         cardContainer.appendChild(productCard)
+
+        // Adding event listeners of components
+        img.addEventListener('click', openProductDetailAside)
     }    
 }
 
