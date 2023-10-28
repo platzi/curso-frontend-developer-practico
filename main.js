8fc159cabb030d6b514f7ba4d7f0ec5db83e9681
samsep10l <mario.maycotteg@gmail.com>
2023-10-28 02:53:46 -0600

    Fusion of mobile menu from clase8.html and aside menu from clase13 in index.html and functionalities in main.js

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 const navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside() {
+    const mobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const desktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if (!mobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    } else if (!desktopMenuClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
 }
