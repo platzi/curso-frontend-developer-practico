a4d842ddcdf7a9244494d851f1940e5ba2825054
Diego Montenegro <montenegroa1001isb@gmail.com>
2023-10-31 09:08:17 -0500

    Carrito de Compras

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 const menuEmail=document.querySelector('.navbar-email')
-const desktopMenu=document.querySelector(".desktop-menu")
 const menuHamIco=document.querySelector(".menu")
+const menuCarritoIcon=document.querySelector(".navbar-shopping-cart")
+const desktopMenu=document.querySelector(".desktop-menu")
 const mobileMenu=document.querySelector(".mobile-menu")
+const aside= document.querySelector(".product-detail")
 
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu)
 menuHamIco.addEventListener('click',toggleMobileMenu)
+menuCarritoIcon.addEventListener('click',toggleCarritoAside)
+
 
 
 function toggleDesktopMenu(){
+    const isAsideClosed=aside.classList.contains('inactive')
+    if(!isAsideClosed){
+        aside.classList.toggle('inactive')
+    }
+    
     desktopMenu.classList.toggle('inactive')
 }
 function toggleMobileMenu(){
+    const isAsideClosed=aside.classList.contains('inactive')
+    if(!isAsideClosed){
+        aside.classList.toggle('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')
+}
+function toggleCarritoAside(){
+    const isMobileMenuClosed=mobileMenu.classList.contains('inactive')
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.toggle('inactive')
+    }
+    aside.classList.toggle('inactive')
 }
