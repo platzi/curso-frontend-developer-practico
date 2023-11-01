23ab9e64b8ff25668569a954ae5cd1761d6cc6f8
Erick Del Piero <Erick.gozales.r@uni.pe>
2023-11-01 15:31:00 -0500

    Carrito de Compras

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-const EmailMenu= document.querySelector('.navbar-email');   //Email donde haces click
-const MenuMobil= document.querySelector('.menu');                //Logo donde haces click
+const iconEmailMenu= document.querySelector('.navbar-email');   //Email donde haces click
+const iconMenuMobil= document.querySelector('.menu');           //Logo donde haces click
+const iconCarCompra= document.querySelector('.navbar-shopping-cart');
 
 const MenuBoxd= document.querySelector('.desktop-menu');     //Box que aparece al hacer click a email
-const MenuMobile= document.querySelector('.mobile-menu');
+const MenuMobil= document.querySelector('.mobile-menu');
+const CarBox= document.querySelector('.product-detail');
 
-EmailMenu.addEventListener('click', AparecerMD);
-MenuMobil.addEventListener('click', AparecerM)
+iconEmailMenu.addEventListener('click', AparecerMD);
+iconMenuMobil.addEventListener('click', AparecerMM);
+iconCarCompra.addEventListener('click', AparecerC);
 
 function AparecerMD(){
+    const isCarBox= CarBox.classList.contains('inactive');
+    if(isCarBox==false){
+        CarBox.classList.toggle('inactive');
+    }
     MenuBoxd.classList.toggle('inactive'); 
 }
 
-function AparecerM(){
-    MenuMobile.classList.toggle('inactive');
+function AparecerMM(){
+    const isCarBox= CarBox.classList.contains('inactive');
+    if(isCarBox==false){
+        CarBox.classList.toggle('inactive');
+    }
+    MenuMobil.classList.toggle('inactive');
+}
+
+function AparecerC() {
+    const isMenumobil= MenuMobil.classList.contains('inactive');
+    const isMenuBoxd= MenuBoxd.classList.contains('inactive');
+
+    if(isMenumobil==false){
+        MenuMobil.classList.toggle('inactive');
+    }
+
+    if(isMenuBoxd==false){
+        MenuBoxd.classList.toggle('inactive');
+    }
+    
+    CarBox.classList.toggle('inactive');
 }
