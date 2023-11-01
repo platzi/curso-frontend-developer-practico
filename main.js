be9fe1f652a7ac88876f34b51f2c3f9e8eebedc4
Joswui <castrojoswui@gmail.com>
2023-11-01 13:40:19 -0400

    listo el menu de carrito para mobile y deskstop

--- main.js
+++ main.js
@@ -10,14 +10,34 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+
+    const isAsideClosed = aside.classList.contains
+    ('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains
+    ('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains
+    ('inactive');
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
     aside.classList.toggle('inactive');
-    // toggleMobileMenu()
 }
