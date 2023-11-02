879a74d648567f4b3b7a3283e7535ee2216a1aeb
Kevin Guerrero <Kevinn8guerrero@gmail.com>
2023-11-01 21:09:10 -0500

    Segundo commit con modificaciones para funcionalidades de menu en el navbar para desktop and movile

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 
-// Menu correo electronico
-
+// Menu correo electronico desktop
 const menuEmail = document.querySelector('.navbar-email'); // creo la variable que va a tener el click
-const desktopMenu = document.querySelector('.desktop-menu'); // creo la variable que se va a ejecutar despues del clicl
+const desktopMenu = document.querySelector('.desktop-menu'); // creo la variable que se va a ejecutar despues del click
+// Menu movile
+const burguerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
-menuEmail.addEventListener('click', toggledesktopMenu); // Escucho el evento
+menuEmail.addEventListener('click', toggledesktopMenu); // Escucho el evento desktop
+burguerMenu.addEventListener('click', toggleMovileMenu); // Escucho el evento movile
 
 function toggledesktopMenu(){ // creo mi funcion "toggle" es intercambiar ene español
     desktopMenu.classList.toggle('inctive'); // con classList.toggle lo que hacemso es quitar o poner la clase dependiendo si el elemento ya la tiene o no
 }
+
+function toggleMovileMenu(){ // creo mi funcion "toggle" es intercambiar ene español
+    mobileMenu.classList.toggle('inctive'); // con classList.toggle lo que hacemso es quitar o poner la clase dependiendo si el elemento ya la tiene o no
+}
+
+
+
