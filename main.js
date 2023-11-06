193d7859630e9b064130e5b85986f3bbd7027d0d
Kevin Guerrero <Kevinn8guerrero@gmail.com>
2023-11-06 15:29:37 -0500

    Se agregaron mas productos

--- main.js
+++ main.js
@@ -123,7 +123,54 @@ const productList = [];
         image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141321.jpg?w=360&t=st=1699231071~exp=1699231671~hmac=364ad3b63279ae7d1e91eff09c0a80c6793ae13ab47c2387ea968b99ebaefe4c',
         description: "Un dispositivo de comunicación móvil que permite realizar llamadas, enviar mensajes de texto, acceder a Internet y ejecutar una amplia variedad de aplicaciones, convirtiéndolo en una herramienta esencial para la comunicación y la productividad en movimiento"
 });
-
+productList.push({
+    name: 'Licuadora',
+    price: 330.000,
+    image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141324.jpg?w=360&t=st=1699231095~exp=1699231695~hmac=751c41d9eebd2a3711421e5743bf800d0abd4f0e13e4985783701f208ccbfa3a',
+    description: "Un electrodoméstico de cocina versátil que combina y mezcla ingredientes para preparar deliciosos batidos, jugos y sopas en cuestión de segundos"
+    });
+    productList.push({
+        name: 'Combo tecaldo mause',
+        price: 230.000,
+        image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141330.jpg?w=360&t=st=1699231104~exp=1699231704~hmac=0a7262b0826482d0a7435e58297a373ae9eb821e798ff37bd3d586b662dc599c',
+        description: "Un conjunto de dispositivos de entrada diseñados para facilitar la interacción con una computadora. El teclado permite la entrada de texto, mientras que el mouse ofrece control preciso del cursor en la pantalla"
+    });
+    productList.push({
+    name: 'Licuadora',
+    price: 330.000,
+    image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141324.jpg?w=360&t=st=1699231095~exp=1699231695~hmac=751c41d9eebd2a3711421e5743bf800d0abd4f0e13e4985783701f208ccbfa3a',
+    description:"Un electrodoméstico de cocina versátil que combina y mezcla ingredientes para preparar deliciosos batidos, jugos y sopas en cuestión de segundos"
+    });
+    productList.push({
+        name: 'Ipad',
+        price: 330,
+        image: 'https://img.freepik.com/fotos-premium/vista-balanceo-productos-electronicos-podio_23-2150141342.jpg?w=360',
+        description: "Una tablet de alta calidad fabricada por Apple que ofrece un diseño elegante, una pantalla nítida y aplicaciones diversas para tareas de productividad, entretenimiento y más"
+    });
+    productList.push({
+    name: 'Placha',
+    price: 510.000,
+    image: 'https://img.freepik.com/fotos-premium/vista-balanceo-productos-electronicos-podio_23-2150141341.jpg?w=360',
+    description: "Un aparato eléctrico utilizado para alisar y eliminar arrugas de la ropa mediante calor y presión, manteniendo tus prendas con un aspecto impecable"
+    });
+    productList.push({
+        name: 'Impresora',
+        price: 890.000,
+        image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141334.jpg?w=360&t=st=1699231132~exp=1699231732~hmac=458fe5339784b7f78de300399b3c96ff15a12c149192eb0b1aefeeb4ad243c99',
+        description: "Un dispositivo que permite convertir documentos y fotografías digitales en copias impresas en papel, facilitando la difusión y el almacenamiento de información física"
+    });
+    productList.push({
+    name: 'Radio',
+    price: 640.000,
+    image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141339.jpg?w=360&t=st=1699231136~exp=1699231736~hmac=b50aed4b7f872619c8995c53160bf85fce608808e0f2c2e7ea36bce84d9cf916',
+    description: "Un dispositivo de comunicación y entretenimiento que sintoniza emisoras de radio AM o FM para escuchar música, noticias, programas de entrevistas y otros contenidos de audio en tiempo real. La radio es una fuente tradicional de información y entretenimiento que ofrece una amplia variedad de estaciones y contenido para satisfacer gustos diversos"
+    });
+    productList.push({
+        name: 'Telefono',
+        price: 551,
+        image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141321.jpg?w=360&t=st=1699231071~exp=1699231671~hmac=364ad3b63279ae7d1e91eff09c0a80c6793ae13ab47c2387ea968b99ebaefe4c',
+        description: "Un dispositivo de comunicación móvil que permite realizar llamadas, enviar mensajes de texto, acceder a Internet y ejecutar una amplia variedad de aplicaciones, convirtiéndolo en una herramienta esencial para la comunicación y la productividad en movimiento"
+});
 
 function productosHome(productList) {
     cardsContainer.innerHTML = ''; // Borra los productos existentes
