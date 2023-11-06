86ed818426411efb237ac6dbfc5b9bc4190b9eeb
Kevin Guerrero <Kevinn8guerrero@gmail.com>
2023-11-05 19:54:19 -0500

    ajustes varios

--- main.js
+++ main.js
@@ -23,18 +23,6 @@ function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu() {
-  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
-
-  if (!isAsideClosed) {
-    shoppingCartContainer.classList.add('inactive'); 
-  }
-
-  closeProductDetailAside();
-  
-  mobileMenu.classList.toggle('inactive');
-}
-
 function toggleCarritoAside() {
   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
   
@@ -60,21 +48,65 @@ function closeProductDetailAside() {
   productDetailContainer.classList.add('inactive');
 }
 
+function toggleMobileMenu() {
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    if (!isAsideClosed) {
+        shoppingCartContainer.classList.add('inactive'); 
+    }
+    closeProductDetailAside();
+    mobileMenu.classList.toggle('inactive');
+}
+
 const productList = [];
 productList.push({
-  name: 'Bike',
-  price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  name: 'Computadora',
+  price: 200.000,
+  image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141319.jpg?w=360&t=st=1699230988~exp=1699231588~hmac=dfd23728e98f15a58a4672ee0e02d6608764f43643b22ea112d5be3fc5e71cff',
 });
 productList.push({
-  name: 'Pantalla',
-  price: 220,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  name: 'Toca discos',
+  price: 300.000,
+  image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141320.jpg?w=360&t=st=1699231063~exp=1699231663~hmac=29db89f2522628628119f5a77443d0fbcd7ba2ad69e5ebf17d750defce66f2ee',
 });
 productList.push({
-  name: 'Compu',
-  price: 620,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  name: 'Licuadora',
+  price: 330.000,
+  image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141324.jpg?w=360&t=st=1699231095~exp=1699231695~hmac=751c41d9eebd2a3711421e5743bf800d0abd4f0e13e4985783701f208ccbfa3a',
+});
+productList.push({
+    name: 'Combo tecaldo mause',
+    price: 230.000,
+    image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141330.jpg?w=360&t=st=1699231104~exp=1699231704~hmac=0a7262b0826482d0a7435e58297a373ae9eb821e798ff37bd3d586b662dc599c',
+});
+productList.push({
+  name: 'Licuadora',
+  price: 330.000,
+  image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141324.jpg?w=360&t=st=1699231095~exp=1699231695~hmac=751c41d9eebd2a3711421e5743bf800d0abd4f0e13e4985783701f208ccbfa3a',
+});
+productList.push({
+    name: 'Ipad',
+    price: 330,
+    image: 'https://img.freepik.com/fotos-premium/vista-balanceo-productos-electronicos-podio_23-2150141342.jpg?w=360',
+});
+productList.push({
+  name: 'Placha',
+  price: 510.000,
+  image: 'https://img.freepik.com/fotos-premium/vista-balanceo-productos-electronicos-podio_23-2150141341.jpg?w=360',
+});
+productList.push({
+    name: 'Impresora',
+    price: 890.000,
+    image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141334.jpg?w=360&t=st=1699231132~exp=1699231732~hmac=458fe5339784b7f78de300399b3c96ff15a12c149192eb0b1aefeeb4ad243c99',
+});
+productList.push({
+  name: 'Radio',
+  price: 640.000,
+  image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141339.jpg?w=360&t=st=1699231136~exp=1699231736~hmac=b50aed4b7f872619c8995c53160bf85fce608808e0f2c2e7ea36bce84d9cf916',
+});
+productList.push({
+    name: 'Telefono',
+    price: 551,
+    image: 'https://img.freepik.com/foto-gratis/vista-balanceo-productos-electronicos-podio_23-2150141321.jpg?w=360&t=st=1699231071~exp=1699231671~hmac=364ad3b63279ae7d1e91eff09c0a80c6793ae13ab47c2387ea968b99ebaefe4c',
 });
 
 function renderProducts(arr) {
