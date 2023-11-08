a3be85e1077a9ece9048da5180b1fc0e219175c0
iES <esocias@gmail.com>
2023-11-08 15:18:45 -0300

    en rama Develop para aside de producto individual

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
 const cardsContainer = document.querySelector('.cards-container');
+const productAsideContainer = document.querySelector('.product-individual-detail');
+const productDetailClose = document.querySelector('.product-detail-close');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailClose.addEventListener('click', closeProductDetail)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
@@ -24,37 +27,14 @@ function toggleCarritoAside() {
     aside.classList.toggle('inactive');
 }
 
+function openProductDetailAside() {
+    productAsideContainer.classList.remove('inactive');
+} 
+
+function closeProductDetail() {
+    productAsideContainer.classList.add('inactive');
+}
 const productList = [];
-// productList.push({
-//     name:'Bike',
-//     price: 120.00, 
-//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-// });
-// productList.push({
-//     name:'Pantalla',
-//     price: 220.00, 
-//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-// });
-// productList.push({
-//     name:'Computadora',
-//     price: 620.00, 
-//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-// });
-// productList.push({
-//     name:'Bike',
-//     price: 120.00, 
-//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-// });
-// productList.push({
-//     name:'Pantalla',
-//     price: 220.00, 
-//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-// });
-// productList.push({
-//     name:'Computadora',
-//     price: 620.00, 
-//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-// });
 
 productList.push ({
     name:'Bike',
@@ -101,18 +81,6 @@ productList.push ({
     price: 876,
     image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
 }); 
-/* <div class="product-card">
-    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-    <div class="product-info">
-      <div>
-        <p>$120,00</p>
-        <p>Bike</p>
-      </div>
-      <figure>
-        <img src="./icons/bt_add_to_cart.svg" alt="">
-      </figure>
-    </div>
-</div> */
 
 function renderProducts(arr) {
     for (product of arr){
@@ -123,6 +91,7 @@ function renderProducts(arr) {
         // product = {name, price, image}
         const img = document.createElement('img');
         img.setAttribute('src', product.image);
+        img.addEventListener('click', openProductDetailAside);
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
