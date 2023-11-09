f574fcb5bde45e14b17d5fc617d5b51c5fae31d1
MarkoTeixido <teixido_marko@outlook.es>
2023-11-09 04:15:44 -0300

    [FEAT-CursoJavascriptPráctico] - asideCartMenu toggle terminado

--- js/main.js
+++ js/main.js
@@ -2,14 +2,25 @@ const menuEmail = document.querySelector(".email-nav");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuBurger = document.querySelector(".menu-burger");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCart = document.querySelector(".navbar-shopping-cart");
+const asideCartMenu = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuBurger.addEventListener("click", toggleMobileMenu);
+menuCart.addEventListener("click", toggleAsideCartMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    asideCartMenu.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    asideCartMenu.classList.add('inactive');
+}
+
+function toggleAsideCartMenu() {
+    asideCartMenu.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
 }
