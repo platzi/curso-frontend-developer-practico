f7b3a98df0aa653d681c4b1a37f8cd56219f8d4e
Ronald Zafra <zafra_x@hotmail.com>
2023-11-08 19:00:30 -0600

    Chart menu upload

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 //Codigo de JS para unir Proyecto de HTML y CSS - Curso Practico Frontend
 const navEmail= document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleAsideMenu);
 
 function toggleDesktopMenu(){
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAsideMenu() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
