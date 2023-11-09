aa0427a568546c0e129cbc4e78ac4b3ac50769a8
MarkoTeixido <teixido_marko@outlook.es>
2023-11-09 03:39:19 -0300

    [FEAT-CursoJavascriptPráctico] - mobileMenu toggle terminado

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".email-nav");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuBurger = document.querySelector(".menu-burger");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuBurger.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
