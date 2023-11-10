b490cc060c86e9c1f09bb7eeac8e27f51382a01b
Cristian Agudelo <cristianmira39@hotmail.com>
2023-11-10 11:37:27 -0500

    Desktop menu and mobile menu integrados

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 navMenu.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    menuMobile.classList.toggle('inactive');
 }
