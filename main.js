13dc971076849e6cf3f89f0f07f469000288142d
cmrobledo <robledozapata@gmail.com>
2023-11-11 17:25:18 -0500

    Scripts en JS para hacer aparecer y desaparecer todos los menús

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
+
+// --- MENÚ EMAIL --- //
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu);  
-menuHamIcon.addEventListener('click', toggleMobileMenu); 
+menuEmail.addEventListener('click', toggleDesktopMenu); 
 
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive');
   mobileMenu.classList.add('inactive');
+  aside.classList.add('inactive');
+  desktopMenu.classList.toggle('inactive');
+  
 }
+// --------------------------------// 
+
+// --- MENÚ VERSION MOVIL --- //
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+ 
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
-  mobileMenu.classList.toggle('inactive');
-  desktopMenu.classList.add('inactive')
+  desktopMenu.classList.add('inactive');
+  aside.classList.add('inactive');
+  mobileMenu.classList.toggle('inactive');    
 }
+// --------------------------------// 
 
+// --- CARRITO DE COMPRAS --- //
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+menuCarritoIcon.addEventListener('click', toggleCarritoAside); 
+
+function toggleCarritoAside() {
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  aside.classList.toggle('inactive');  
+}
+// --------------------------------// 
 
 
 
