93e06cc84312959f840756c58633e4a53edbe9f0
mario <mario.florez249@gmail.com>
2023-11-10 21:46:15 -0500

    Fusion del menu en mobile, integracion del carrito de compras y comportamiento dinámico de los diferentes menus

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHamb = document.querySelector('.menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamb.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    } 
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    } 
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    } 
+    aside.classList.toggle('inactive')
 }
