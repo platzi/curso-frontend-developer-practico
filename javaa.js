16599c8a43ff4a686e554dbec97a9702e8663db7
carlos martinez <kartelosmm@gmail.com>
2023-11-13 18:13:10 -0600

    termine interaccion entre todos los componentes 24

--- javaa.js
+++ javaa.js
@@ -1,14 +1,17 @@
-const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
-const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const menuHam = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
-const cardsContainer = document.querySelector(".cards-container");
+const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const cardsContainer = document.querySelector('.cards-container');
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
-menuHam.addEventListener("click", toggleMobileMenu);
-menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -28,18 +31,28 @@ function toggleMobileMenu() {
 
     mobileMenu.classList.toggle("inactive");
 }
-
-
 function toggleCarritoAside() {
-    const isMobileMenuClosed = mobileMenu.classList.contains
-    ("inactive");
-
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
     if (!isMobileMenuClosed) {
-        mobileMenu.classList.add("inactive");
-    }
-    shoppingCartContainer.classList.toggle("inactive");
+        mobileMenu.classList.add('inactive');
+      }
+    
+      const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+      
+      if (!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive'); 
+      }
+      
+      shoppingCartContainer.classList.toggle('inactive');
 }
 
+function openProductDetailAside( ) {
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove("inactive");
+}
+function closeProductDetailAside() {
+    productDetailContainer.classList.add("inactive");
+}
 const productList = [];
 productList.push ({
     name: "bike",
@@ -94,6 +107,7 @@ productList.push ({
           // product={name,price,image} -> product.image
           const productImg = document.createElement("img");
           productImg.setAttribute("src", product.image);
+          productImg.addEventListener("click", openProductDetailAside);
     
           const productInfo = document.createElement("div");
           productInfo.classList.add("product-info");
