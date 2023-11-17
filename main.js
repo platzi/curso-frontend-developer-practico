beb4093116822dd77d23c33429187d9f5db93fdf
camilo <zcamilozcastillo@gmail.com>
2023-11-17 10:30:47 -0500

    se hizo la funcion para mostrar menu responsive en mobile

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+// menu pequeño con click
 const navEmail = document.querySelector(".navbar-email")
-const menu = document.querySelector(".desktop-menu")
-// funcion para mostrar o ocultar el menupequño
+const menupequeño = document.querySelector(".desktop-menu")
+/* menu  en responsivo */
+const menuMostrar = document.querySelector(".mobile-menu")
+const menuResponsive = document.querySelector(".menu")
+
+// funcion para mostrar o ocultar el menupequño  menu pequeño con click
 navEmail.addEventListener("click",mostrarOcultarMenu)
  function mostrarOcultarMenu(){
-    menu.classList.toggle("inactive") // se coloca toggle para agrgar si tiene o no la la clase inactive
+    menupequeño.classList.toggle("inactive") // se coloca toggle para agrgar si tiene o no la la clase inactive
     
  }
+
+/* menu  en responsivo funcion */
+menuResponsive.addEventListener("click",mostrarMenu)
+function mostrarMenu(){
+    menuMostrar.classList.toggle("inactive")
+}
