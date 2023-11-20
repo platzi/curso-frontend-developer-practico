4b4b95f71697295674d32174326006ed03c032a0
Danna Paola <dannacapera16@gmail.com>
2023-11-20 11:54:54 -0500

    Fusion de lista de productos utilizando HTML a partir de arrays

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuIconMobile = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuIconCarrito = document.querySelector(".navbar-shopping-cart");
 const carritoMenu = document.querySelector(".product-detail");
+const cardContainer = document.querySelector(".cards-container");
 
 //Menu desktop
 menuIconEmail.addEventListener("click", toggleDesktopMenu);
@@ -47,3 +48,66 @@ function toggleCarritoMenu() {
 
   carritoMenu.classList.toggle("inactive");
 }
+
+const productList = [];
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+});
+
+productList.push({
+  name: "Pantalla",
+  price: 220,
+  image:
+    "https://www.noticias3d.com/articulos/202201/la_era_de_las_pantallas/imagenes/la-era-de-las-pantallas-mig.jpg",
+});
+
+productList.push({
+  name: "Computador",
+  price: 620,
+  image:
+    "https://www.tiendatoshiba.es/327-525-large/portatil-toshiba-satellite-l850-150.jpg",
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImgCard = document.createElement("img");
+    productImgCard.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCard);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productList);
