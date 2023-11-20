8ca0aa1f4be9e48f6f4af1b3085f327217f7aa20
Fernando Montoya <programadorjunior97@gmail.com>
2023-11-20 16:36:58 -0600

    Agrege el evento que me guarda los menus tanto en mobil como en deskto la forma en la que funciona es que cuando se sale de sus respectivos contenedores se les agrege la clase inactive guardandolos por defecto

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+mobileMenu.addEventListener("mouseout", outMobil);
+desktopMenu.addEventListener("mouseout", outDesktop);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
+function outMobil() { // Guarda el menu mobile guando se sale del contenedor
+    mobileMenu.classList.toggle('inactive');
+}
+function outDesktop() { // Guarda el menu desktop cuando se sale del contendor
+    desktopMenu.classList.toggle('inactive');
+}
