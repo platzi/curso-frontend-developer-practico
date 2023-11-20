1c59b0b44c938a4b2d0dc6d7f16aac602dad6897
Danna Paola <dannacapera16@gmail.com>
2023-11-19 22:24:54 -0500

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuIconMobile = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
+//Menu desktop
 menuEmail.addEventListener("click", toggleDesktopMenu);
+//Menu mobile
+menuIconMobile.addEventListener("click", toggleMobileMenu);
 
+//Menu desktop
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive"); //para que lo muestre o no dependiendo si esta activo
+}
+
+//Menu mobile
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive"); //para que lo muestre o no dependiendo si esta activo
 }
