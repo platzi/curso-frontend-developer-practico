2aeafb6eef1251d94be752aecdc9659b3eb69d3c
Danna Paola <dannacapera16@gmail.com>
2023-11-19 23:47:47 -0500

    Fusion del carrito de compras y fix de conflictos de visibilidad con menus mobile y desktop

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
-const menuEmail = document.querySelector(".navbar-email");
+const menuIconEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuIconMobile = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuIconCarrito = document.querySelector(".navbar-shopping-cart");
+const carritoMenu = document.querySelector(".product-detail");
 
 //Menu desktop
-menuEmail.addEventListener("click", toggleDesktopMenu);
+menuIconEmail.addEventListener("click", toggleDesktopMenu);
 //Menu mobile
 menuIconMobile.addEventListener("click", toggleMobileMenu);
+//menu carrito
+menuIconCarrito.addEventListener("click", toggleCarritoMenu);
 
 //Menu desktop
 function toggleDesktopMenu() {
+  const isCarritoMenuClosed = carritoMenu.classList.contains("inactive");
+
+  if (!isCarritoMenuClosed) {
+    carritoMenu.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive"); //para que lo muestre o no dependiendo si esta activo
 }
 
 //Menu mobile
 function toggleMobileMenu() {
+  const isCarritoMenuClosed = carritoMenu.classList.contains("inactive");
+
+  if (!isCarritoMenuClosed) {
+    carritoMenu.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive"); //para que lo muestre o no dependiendo si esta activo
+}
+
+//Menu carrito
+function toggleCarritoMenu() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  } else if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add("inactive");
+  }
+
+  carritoMenu.classList.toggle("inactive");
 }
