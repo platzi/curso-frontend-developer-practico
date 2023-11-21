f4c1ba4b419af5775ae19f78c12ea767886d6f38
Fernando Montoya <programadorjunior97@gmail.com>
2023-11-20 20:42:19 -0600

    Agregamos los 2 menus tanto el mobil como el desktop mas aparte la seccion de carrito de compras, aparte con JS le pusimos eventos para que cada uno se esconda cuando este abierto el otro

--- main.js
+++ main.js
@@ -1,24 +1,49 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 mobileMenu.addEventListener("mouseout", outMobil);
 desktopMenu.addEventListener("mouseout", outDesktop);
+aside.addEventListener("mouseout", outAside);
 
 function toggleDesktopMenu(){
+    //desktopMenu.classList.toggle('inactive');
+    if(mobileMenu.classList.contains('inactive') || aside.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive');
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    // mobileMenu.classList.toggle('inactive');
+    if(desktopMenu.classList.contains('inactive') || aside.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive');
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 }
 
-function outMobil() { // Guarda el menu mobile guando se sale del contenedor
+function toggleCarritoAside() {
+    if(desktopMenu.classList.contains('inactive') || mobileMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive');
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+}
+
+function outMobil() {
     mobileMenu.classList.toggle('inactive');
 }
-function outDesktop() { // Guarda el menu desktop cuando se sale del contendor
+function outDesktop() {
     desktopMenu.classList.toggle('inactive');
+}
+function outAside() {
+    aside.classList.toggle('inactive');
 }
