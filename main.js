6fe0b788c04ae455c66977afaa619a7148af1e07
Fernando Montoya <programadorjunior97@gmail.com>
2023-11-21 00:48:26 -0600

    Agregamos la lista de productos disponibles use innerHTML para mas practico pero creo que eso fue una mala idea me voy enterando que es una mala practica

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -12,9 +13,9 @@ mobileMenu.addEventListener("mouseout", outMobil);
 desktopMenu.addEventListener("mouseout", outDesktop);
 aside.addEventListener("mouseout", outAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
     //desktopMenu.classList.toggle('inactive');
-    if(mobileMenu.classList.contains('inactive') || aside.classList.contains('inactive')) {
+    if (mobileMenu.classList.contains('inactive') || aside.classList.contains('inactive')) {
         mobileMenu.classList.add('inactive');
         aside.classList.add('inactive');
     }
@@ -23,7 +24,7 @@ function toggleDesktopMenu(){
 
 function toggleMobileMenu() {
     // mobileMenu.classList.toggle('inactive');
-    if(desktopMenu.classList.contains('inactive') || aside.classList.contains('inactive')) {
+    if (desktopMenu.classList.contains('inactive') || aside.classList.contains('inactive')) {
         desktopMenu.classList.add('inactive');
         aside.classList.add('inactive');
     }
@@ -31,7 +32,7 @@ function toggleMobileMenu() {
 }
 
 function toggleCarritoAside() {
-    if(desktopMenu.classList.contains('inactive') || mobileMenu.classList.contains('inactive')) {
+    if (desktopMenu.classList.contains('inactive') || mobileMenu.classList.contains('inactive')) {
         desktopMenu.classList.add('inactive');
         mobileMenu.classList.add('inactive');
     }
@@ -47,3 +48,63 @@ function outDesktop() {
 function outAside() {
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+{/* <div class="product-card">
+                <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    alt="">
+                <div class="product-info">
+                    <div>
+                        <p>$120,00</p>
+                        <p>Bike</p>
+                    </div>
+                    <figure>
+                        <img src="./icons/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div> */}
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+
+        productCard.innerHTML = `
+        <img src="${product.image}" alt="Product Image">
+        <div class="product-info">
+            <div>
+                <p>$${product.price}</p>
+                <p>${product.name}</p>
+            </div>
+            <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="Add to Cart">
+            </figure>
+        </div>
+    `;
+    cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
