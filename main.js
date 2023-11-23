0f856fc6c29b0a4013617682e0ca297217265abb
José Luis Orihuela Conde <orihuelaconde@gmail.com>
2023-11-23 16:55:23 -0300

    * Carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenuIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuCarrito = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 mobileMenuIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleMenuCarrito);
 
 function toggleDesktopMenu(){
+    menuCarrito.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    menuCarrito.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMenuCarrito(){
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    menuCarrito.classList.toggle('inactive');
 }
