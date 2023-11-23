c73d522260123c705f48c89c4c764448f6ccb4e1
ATH3R15 <josedmuros@gmail.com>
2023-11-23 14:40:03 -0500

    Fusionado menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
  const profileEmail = document.querySelector(".navbar-email");
  const desktopMenu = document.querySelector(".desktop-menu");
+ const menuButton = document.querySelector(".menu")
+ const mobileMenu = document.querySelector(".mobile-menu")
 
 profileEmail.addEventListener("click", toggleDesktopMenu);
 
  function toggleDesktopMenu () {
     desktopMenu.classList.toggle("inactive")
+
+ }
+
+ menuButton.addEventListener("click", toggleMobileMenu);
+
+ function toggleMobileMenu () {
+   mobileMenu.classList.toggle("inactive")
+    
  }
