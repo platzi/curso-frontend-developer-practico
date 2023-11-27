aa98f27ccdbaf9e4a075358e0589ccd994e1c7d5
manXDuel <manXDuelyt@gmail.com>
2023-11-27 09:31:03 -0600

    Integracion del aside y creacion de las funciones para cambiar la visibilidad o apagarla, adapte los event listener a estas funciones ademas de que ahora se cierran los demas menus cuando se abre otro

--- main.js
+++ main.js
@@ -3,18 +3,31 @@ const navBarRight= document.querySelector('.navbar-right');
 const navBarDesktopMenu= document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const hamburgerButton = document.querySelector('.menu');
+const asideProductDetail = document.querySelector('.product-detail')
 
-// Asegurándonos de que existen los elementos antes de intentar agregar el manejador de eventos
-if(navBarRight != undefined && navBarDesktopMenu != undefined ){
-    console.log('hola');
-    const navBarEmail = navBarRight.getElementsByClassName('navbar-email')[0];
+const navBarEmail = navBarRight.querySelector('.navbar-email');
+const navBarShoppingCart = navBarRight.querySelector('.navbar-shopping-cart');
+//const shoppingCartMenu = document.querySelector('.product-detail');
 
-    navBarEmail.addEventListener('click', function(){
-        // Alternar la clase 'inactive' en desktopMenu
-        navBarDesktopMenu.classList.toggle('inactive');
-    });
-}
-
-hamburgerButton.addEventListener('click', function(){
-    mobileMenu.classList.toggle('inactive')
+navBarEmail.addEventListener('click',() => {
+    toggleVisibilityOf(navBarDesktopMenu);
+    turnOffVisibilityOf(mobileMenu);
+    turnOffVisibilityOf(asideProductDetail);
 });
+hamburgerButton.addEventListener('click',() => {
+    toggleVisibilityOf(mobileMenu);
+    turnOffVisibilityOf(navBarDesktopMenu);
+    turnOffVisibilityOf(asideProductDetail);
+});
+navBarShoppingCart.addEventListener('click',() => {
+    toggleVisibilityOf(asideProductDetail);
+    turnOffVisibilityOf(navBarDesktopMenu);
+    turnOffVisibilityOf(mobileMenu);
+});
+
+function toggleVisibilityOf(element){
+    element.classList.toggle('inactive');
+}
+function turnOffVisibilityOf(element){
+    element.classList.add('inactive');
+}
