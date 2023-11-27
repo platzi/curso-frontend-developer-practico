79d79321cc05238c0a6156a5fb4ee117acb58004
Leonard Reyes <leonardarzuaga@gmail.com>
2023-11-27 11:27:27 -0500

    En esta ocasion se fusiono el menu mobile a nuestro archivo index.html, se agrego responsive para los menu tanto en mobile como en desktop

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+// Variables
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const imagenMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-
+// Eventos
 menuEmail.addEventListener('click', toggleDesktopMenu);
+imagenMenu.addEventListener('click', toggleMobileMenu);
 
+// Funciones
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('INACTIVE')
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('INACTIVE')
 }
