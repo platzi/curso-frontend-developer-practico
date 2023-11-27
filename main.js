4af9e7fb77d1958b623be495e691e029f8a28406
Leonard Reyes <leonardarzuaga@gmail.com>
2023-11-27 12:13:58 -0500

    Se agrego el menu de las ordenes que tenemos en el carrito, tambien se corrigio la sobre posicion de cuando abrimos el menu del carrito con el menu mobile

--- main.js
+++ main.js
@@ -2,17 +2,44 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const imagenMenu = document.querySelector('.menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside= document.querySelector('.product-detail');
 
 // Eventos
 menuEmail.addEventListener('click', toggleDesktopMenu);
 imagenMenu.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleMenuCarrito);
 
 // Funciones
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('INACTIVE');
+    if (!isAsideClosed) {
+        aside.classList.add('INACTIVE')
+    }
     desktopMenu.classList.toggle('INACTIVE')
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('INACTIVE');
+    if (!isAsideClosed) {
+        aside.classList.add('INACTIVE')
+    }
     mobileMenu.classList.toggle('INACTIVE')
 }
+
+function toggleMenuCarrito() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('INACTIVE');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('INACTIVE');
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('INACTIVE')
+    }if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('INACTIVE')
+    }
+     aside.classList.toggle('INACTIVE')
+}
+    
+
+    
+
+    
