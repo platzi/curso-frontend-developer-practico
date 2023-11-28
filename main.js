17dde942d87990c0cbc37fc1635d54096dd76c02
ErikVestrel <erik.candle0700@hotmail.com>
2023-11-28 04:11:19 -0600

    agregado el menu mobile y el toggle

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuMobileIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+menuMobileIcon.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu(){
+    menuMobile.classList.toggle('inactive');
 }
