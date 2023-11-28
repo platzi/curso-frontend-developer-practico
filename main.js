8423f736257174a146e98476e173a6a8ace99540
David Dominguez Valerio <david.dominguez@beeckerco.com>
2023-11-27 19:04:07 -0600

    Implementacion del aside del carrito

--- main.js
+++ main.js
@@ -2,14 +2,38 @@
     const desktopMenu = document.querySelector('.desktop-menu');
     const menuIcon = document.querySelector('.menu');
     const mobileMenu = document.querySelector('.mobile-menu');
+    const carritoIcon = document.querySelector('.navbar-shopping-cart');
+    const aside = document.querySelector('.product-detail');
 
     menuEmail.addEventListener('click', ToggleDesktopMenu);
     menuIcon.addEventListener('click', ToggleMobileMenu);
+    carritoIcon.addEventListener('click', ToggleAside);
 
     function ToggleDesktopMenu() {
+        const isAsideClosed = aside.classList.contains('inactive');
+
+        if(!isAsideClosed){
+            aside.classList.add('inactive');
+        }
         desktopMenu.classList.toggle('inactive');
     }
 
     function ToggleMobileMenu(){
+        const isAsideClosed = aside.classList.contains('inactive');
+
+        if(!isAsideClosed){
+            aside.classList.add('inactive');
+        }
         mobileMenu.classList.toggle('inactive');
+    }
+
+    function ToggleAside(){
+        const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+        if(!isMobileMenuClosed){
+            mobileMenu.classList.add('inactive');
+        }
+        
+        aside.classList.toggle('inactive');
+
     }
