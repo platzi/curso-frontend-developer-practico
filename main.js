f8645b80f12d2b54a6cc60c5bd2f36a937cd7289
David Dominguez Valerio <david.dominguez@beeckerco.com>
2023-11-27 18:29:41 -0600

    implementacion del menu de hamburguesa para mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
     const menuEmail = document.querySelector('.navbar-email');
     const desktopMenu = document.querySelector('.desktop-menu');
+    const menuIcon = document.querySelector('.menu');
+    const mobileMenu = document.querySelector('.mobile-menu');
 
     menuEmail.addEventListener('click', ToggleDesktopMenu);
+    menuIcon.addEventListener('click', ToggleMobileMenu);
 
     function ToggleDesktopMenu() {
         desktopMenu.classList.toggle('inactive');
+    }
+
+    function ToggleMobileMenu(){
+        mobileMenu.classList.toggle('inactive');
     }
