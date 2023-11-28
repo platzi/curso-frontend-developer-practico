8d6d0bed19559779583ab8d9105316afb3ce6586
bruno <-soy.brunosilva@gmail.com>
2023-11-28 13:50:26 -0300

    Fusionando el codigo del menu-desktop y menu-mobile

--- code.js
+++ code.js
@@ -1,7 +1,16 @@
+//This is the code that I used to show/hide the user-menu
 const menuEmail = document.querySelector(".desktop-menu");
 const navEmail = document.querySelector(".navbar-email");
 navEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
   menuEmail.classList.toggle('inactive');
+}
+//This is the code that I used to show/hide the mobile-menu
+const mobileMenuImg = document.querySelector('.menu-img');
+const mobileMenuContainer = document.querySelector('.mobile-menu');
+mobileMenuImg.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+  mobileMenuContainer.classList.toggle('inactive');
 }
