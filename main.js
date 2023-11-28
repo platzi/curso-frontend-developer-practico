085801e20bf234b7d37420a81ce8baa9ac65223c
AldoVital <aldo.vitalg@gmail.com>
2023-11-27 21:31:07 -0600

    Integracion de lista de productos, maquetacion HTML con JavaScript, creación y modificacion de eventos.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const mobileMenu = document.querySelector(".mobile-menu");
 
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
@@ -24,7 +25,11 @@ function toggleMobileMenu() {
 }
 
 function toggleCarritoAside() {
-  closeMenus(mobileMenu);
+  if (!desktopMenu.classList.contains("inactive")) {
+    closeMenus(desktopMenu);
+  } else if (!mobileMenu.classList.contains("inactive")) {
+    closeMenus(mobileMenu);
+  }
 
   aside.classList.toggle("inactive");
 }
@@ -32,3 +37,66 @@ function toggleCarritoAside() {
 function closeMenus(close) {
   close.classList.add("inactive");
 }
+
+const productList = [];
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Pantalla",
+  price: 220,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Computadora",
+  price: 620,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    document.createElement;
+
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = `$${product.price}`;
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    productInfoDiv.append(productPrice, productName);
+
+    const productInfoFigure = document.createElement("figure");
+
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImg, productInfo);
+
+    cardsContainer.append(productCard);
+    console.log(cardsContainer);
+  }
+}
+
+renderProducts(productList);
