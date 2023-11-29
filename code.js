2f4a39137b3e96469de27fb9859a70d045e9519d
bruno <-soy.brunosilva@gmail.com>
2023-11-29 12:08:05 -0300

    Adding a new features, ie: the new mobile-menu and now I'm working on the shopping cart list feature'

--- code.js
+++ code.js
@@ -4,13 +4,23 @@ const navEmail = document.querySelector(".navbar-email");
 navEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
-  menuEmail.classList.toggle('inactive');
+  menuEmail.classList.toggle("inactive");
 }
 //This is the code that I used to show/hide the mobile-menu
-const mobileMenuImg = document.querySelector('.menu-img');
-const mobileMenuContainer = document.querySelector('.mobile-menu');
-mobileMenuImg.addEventListener('click', toggleMobileMenu);
+const mobileMenuImg = document.querySelector(".menu-img");
+const mobileMenuContainer = document.querySelector(".mobile-menu");
+mobileMenuImg.addEventListener("click", toggleMobileMenu);
 
-function toggleMobileMenu(){
-  mobileMenuContainer.classList.toggle('inactive');
+function toggleMobileMenu() {
+  mobileMenuContainer.classList.toggle("hidden-mobile-menu");
+}
+
+//This part is your the shopping cart system
+
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailContainer = document.querySelector('.product-detail');
+shoppingCartIcon.addEventListener('click', toggleShoppingCart);
+
+function toggleShoppingCart(){
+  productDetailContainer.classList.toggle("inactive");
 }
