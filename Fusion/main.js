342060f18e74f633ba3e3a8eefb43588cdf4e429
Lina Rodriguez <linarodriguezrincon03@gmail.com>
2023-11-30 10:31:19 -0500

    menu de escritorio y de celular

--- Fusion/main.js
+++ Fusion/main.js
@@ -1,8 +1,15 @@
 const emailNav = document.querySelector('.navbar-email');
-const deskNav = document.querySelector('.desktop-menu')
+const deskNav = document.querySelector('.desktop-menu');
+const menuIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 emailNav.addEventListener('click', togglesDesktopMenu);
+menuIcon.addEventListener('click', togglesMobileMenu);
 
 function togglesDesktopMenu() {
     deskNav.classList.toggle('inactive');
+}
+
+function togglesMobileMenu() {
+    menuMobile.classList.toggle('inactive');
 }
