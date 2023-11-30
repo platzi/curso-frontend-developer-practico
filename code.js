73c7fe26fc8b53e5d35e6f719c6fd35aad9573ba
bruno <-soy.brunosilva@gmail.com>
2023-11-30 14:21:02 -0300

    added the shopping cart, and the feature that allows users to open it and at the same time, collapse others menus

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ const navEmail = document.querySelector(".navbar-email");
 navEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
+  productDetailContainer.classList.add('inactive');
   menuEmail.classList.toggle("inactive");
 }
 //This is the code that I used to show/hide the mobile-menu
@@ -12,15 +13,20 @@ const mobileMenuContainer = document.querySelector(".mobile-menu");
 mobileMenuImg.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
-  mobileMenuContainer.classList.toggle("hidden-mobile-menu");
+  productDetailContainer.classList.add('inactive');
+  mobileMenuContainer.classList.remove('inactive');
+  mobileMenuContainer.classList.toggle('hidden-mobile-menu');
 }
 
 //This part is your the shopping cart system
 
-const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCartIcon = document.querySelector('#shopping-cart-icon');
 const productDetailContainer = document.querySelector('.product-detail');
 shoppingCartIcon.addEventListener('click', toggleShoppingCart);
 
 function toggleShoppingCart(){
-  productDetailContainer.classList.toggle("inactive");
+  mobileMenuContainer.classList.add('inactive');
+  menuEmail.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+  productDetailContainer.classList.toggle('hidden-product-detail');
 }
