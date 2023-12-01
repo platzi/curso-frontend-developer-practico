2571e6a3765cd631e339f3e2c092df55102ca47a
juan-pablo-marin <jupamarin@gmail.com>
2023-11-30 22:36:47 -0500

    primeros cambios en el forke del proyecto que clonamos

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopmenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click',toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+    desktopmenu.classList.toggle('inactive');
+    console.log("entro");
+}
