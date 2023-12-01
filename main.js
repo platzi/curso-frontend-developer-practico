d3694f4ce7d39db176843f638efdd2ddc6883ae4
juan-pablo-marin <jupamarin@gmail.com>
2023-12-01 07:05:06 -0500

    cambios segunda clase

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
+const mobileHam= document.querySelector('.menu');
+const mobileMenu= document.querySelector('.mobile-menu');
+const productDetail = document.querySelector('.product-detail');
+const shoppinCart = document.querySelector('.navbar-shopping-cart');
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+mobileHam.addEventListener('click',toggleMobilepMenu);
+shoppinCart.addEventListener('click',toggleCarrito);
 
-function toggleDesktopMenu(){
-    desktopmenu.classList.toggle('inactive');
-    console.log("entro");
+function toggleDesktopMenu(){    
+ var isMenuCarritoOpen= !productDetail.classList.contains('inactive');
+
+ if(isMenuCarritoOpen){
+    productDetail.classList.add('inactive');
+}
+
+    desktopmenu.classList.toggle('inactive');   
+}
+
+function toggleMobilepMenu(){
+var isMenuCarritoOpen = !productDetail.classList.contains('inactive');
+
+if (isMenuCarritoOpen){
+    productDetail.classList.add('inactive');
+}
+    mobileMenu.classList.toggle('inactive');   
+}
+
+function toggleCarrito() {
+    var isMenuMobileOpen=!mobileMenu.classList.contains('inactive');
+
+    if(isMenuMobileOpen){
+        mobileMenu.classList.add('inactive');
+    }
+    productDetail.classList.toggle('inactive')
 }
