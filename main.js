a80a5635118c17ca5e87dcc7387674755e955796
manXDuel <manXDuelyt@gmail.com>
2023-12-03 17:20:34 -0600

    Arreglo un problema de precisión de punto flotante de los precios y mejoré la legibilidad del código

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
-const navBarLeft= document.querySelector('.navbar-left');
-const navBarRight= document.querySelector('.navbar-right');
-const navBarDesktopMenu= document.querySelector('.desktop-menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-const hamburgerButton = document.querySelector('.menu');
-const asideShoppingCart = document.querySelector('.shopping-cart-detail');
-const asideProductDetail = document.querySelector('.product-detail');
-const productCardsContainer = document.querySelector('.cards-container');
-
-const navBarEmail = navBarRight.querySelector('.navbar-email');
-const navBarShoppingCart = navBarRight.querySelector('.navbar-shopping-cart');
-
-navBarEmail.addEventListener('click',() => {
-    toggleVisibilityOf(navBarDesktopMenu);
-    turnOffVisibilityOf(mobileMenu);
-    turnOffVisibilityOf(asideShoppingCart);
-    turnOffVisibilityOf(asideProductDetail);
-});
-hamburgerButton.addEventListener('click',() => {
-    toggleVisibilityOf(mobileMenu);
-    turnOffVisibilityOf(navBarDesktopMenu);
-    turnOffVisibilityOf(asideShoppingCart);
-    turnOffVisibilityOf(asideProductDetail);
-});
-navBarShoppingCart.addEventListener('click', renderShoppingCartMenu);
-
-let productList = [
+let productList = [ //Productos placeholder
     {
     "name": "Classic Watch",
     "category": "Others",
@@ -122,10 +96,43 @@ let productList = [
     "description": "Eco-friendly yoga mat for all levels of yoga practitioners."
     }
 ];
+//Manejadores de la aplicación 
+//Navbar escritorio/tablets
+const navBarLeft= document.querySelector('.navbar-left');
+const navBarRight= document.querySelector('.navbar-right');
+const navBarDesktopMenu= document.querySelector('.desktop-menu');
+const navBarEmail = navBarRight.querySelector('.navbar-email');
+const navBarShoppingCart = navBarRight.querySelector('.navbar-shopping-cart');
+//Mobile
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamburgerButton = document.querySelector('.menu');
+//Shopping cart menu/Product details
+const asideShoppingCart = document.querySelector('.shopping-cart-detail');
+const asideProductDetail = document.querySelector('.product-detail');
+//Productos 
+const productCardsContainer = document.querySelector('.cards-container');
 
+
+navBarEmail.addEventListener('click',() => {
+    toggleVisibilityOf(navBarDesktopMenu);
+    turnOffVisibilityOf(mobileMenu);
+    turnOffVisibilityOf(asideShoppingCart);
+    turnOffVisibilityOf(asideProductDetail);
+});
+hamburgerButton.addEventListener('click',() => {
+    toggleVisibilityOf(mobileMenu);
+    turnOffVisibilityOf(navBarDesktopMenu);
+    turnOffVisibilityOf(asideShoppingCart);
+    turnOffVisibilityOf(asideProductDetail);
+});
+navBarShoppingCart.addEventListener('click', renderShoppingCartMenu);
 renderProducts(productList);
 
 const isFloat = value => Number(value) === value && value % 1 !== 0;
+function formatPrice(price) { // Esta función convierte el número a un formato de precio
+    // Asume que si el número es un entero no necesita decimales, y si es flotante, los mantiene
+    return isFloat(price) ? `$${price.toFixed(2)}` : `$${price}.00`;
+}
 
 function toggleVisibilityOf(element){
     element.classList.toggle('inactive');
@@ -209,41 +216,49 @@ function renderProductDetails(productDetails){
     //imgOfProduct.setAttribute('src', productDetails.imgSrc);
     imgOfProduct.setAttribute('src', "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
     imgOfProduct.setAttribute('alt', productDetails.imgAlt);
-    priceOfProduct.innerText = productDetails.price;
+    priceOfProduct.innerText = formatPrice(productDetails.price);
     nameOfProduct.innerText = productDetails.name;
     descriptionOFProduct.innerText = productDetails.description;
 }
 
 function addProductToCart(productDetails){
-    const cartOrderContainer = asideShoppingCart.querySelector('.my-order-content');
-    const productCartContainer = document.createElement('div');
-    productCartContainer.classList.add('shopping-cart');
-    const orderDiv = asideShoppingCart.querySelector('.order');
-    const numberOfSameProduct = document.createElement('div');
+    // Asegúrate de que el precio es un número para evitar errores
+    if (typeof productDetails.price !== 'number' || isNaN(productDetails.price)) {
+        console.error('El precio del producto no es un número válido:', productDetails.price);
+        return; // Sal de la función si el precio no es válido
+    }
 
+    //Div que contiene el numero de veces que un producto esta repetido en el carrito
+    const numberOfSameProduct = document.createElement('div');
     if(isThisProductInTheCart(cartOrderContainer, productDetails)){
-        return;
+        //En caso de que el producto este en el carrito la función isThisProductInTheCart aumenta la cantidad en 1
+        return; //Sal de la función
     } else {
         numberOfSameProduct.innerText = 1;
     }
 
-    //Genero la imagen del producto y la agrego un un figure 
+    //Creo los manejadores del carrito
+    const cartOrderContainer = asideShoppingCart.querySelector('.my-order-content');
+    const orderDiv = asideShoppingCart.querySelector('.order');
+
+    //Genero el contenedor del producto
+    const productCartContainer = document.createElement('div');
+    productCartContainer.classList.add('shopping-cart');
+
+    //Genero la imagen del producto y la agrego un figure 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
     productImg.setAttribute('alt', productDetails.imgAlt);
     const productFigure = document.createElement('figure');
     productFigure.classList.add('cart-product-figure');
-    
     productFigure.append(productImg, numberOfSameProduct);
 
     //Agrego el precio y nombre del producto
     const productPrice = document.createElement('p');
     const productName = document.createElement('p');
-    if(isFloat(productDetails.price)){ //Verifico si tengo que agregar los decimales a mano o es un flotante
-        productPrice.innerText = '$' + (productDetails.price*numberOfSameProduct.innerText);
-    } else {
-        productPrice.innerText = '$' + productDetails.price + '.00';
-    }
+    const productPriceCents = Math.round(productDetails.price * 100); // Convertir a centavos
+    const totalPriceCents = productPriceCents * parseInt(numberOfSameProduct.innerText);
+    productPrice.innerText = formatPrice(totalPriceCents / 100);
     productName.innerText = productDetails.name;
     
     //Agrego la imagen para quitar el producto del carrito junto con el evento para eliminarlo y hacer un update de precios
@@ -274,7 +289,7 @@ function updateCart(){
     shoppingCartProductsQty[0].innerText = 0;
 
     for(productPos = 0; productPos < products.length; productPos++){
-        // Obtiene el precio del producto (asumiendo que el precio está en el segundo elemento <p>)
+        // Obtiene el precio del producto (asumiendo que el precio está en el segundo elemento <p>) y la cantidad 
         const priceElement = products[productPos].querySelectorAll('p')[1];
         const amountOfSameProduct = products[productPos].querySelector('.cart-product-figure').querySelector('div');
         const price = parseFloat(priceElement.innerText.replace('$', '')) * parseInt(amountOfSameProduct.innerText);
@@ -282,19 +297,14 @@ function updateCart(){
         totalPrice = parseFloat(totalPrice) + parseFloat(price);
     }
 
-    if(isFloat(totalPrice)){ //Verifico si tengo que agregar los decimales a mano o es un flotante
-        totalPriceContainer.innerText = '$' + totalPrice;
-    } else {
-
-        totalPriceContainer.innerText = '$' + totalPrice + '.00';
-    }
+    totalPriceContainer.innerText = formatPrice(totalPrice);
 }
 
 function isThisProductInTheCart(containerOfProducts, product){
     const products = containerOfProducts.getElementsByClassName('shopping-cart');
     for(productNumber = 0; productNumber < products.length; productNumber++){
         const infoOfProduct = products[productNumber].querySelectorAll('p');
-        //Verifico que tanto el nombre como el precio sean el mismo
+        //Verifico que tanto el nombre como el precio sean el mismo, próximamente puede ser buena idea añadir un ID de cada producto
         if(product.name == infoOfProduct[0].innerText && product.price == parseFloat(infoOfProduct[1].innerText.replace('$', ''))){
             const numberOfSameProduct = products[productNumber].querySelector('.cart-product-figure').querySelector('div');
             numberOfSameProduct.innerText++;
