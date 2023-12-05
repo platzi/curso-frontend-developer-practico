8b5c67233e2f2d1929bf42df9704267226d94372
bruno <-soy.brunosilva@gmail.com>
2023-12-05 14:11:40 -0300

    Adding a new feature, ie: A script that creates new products cards instead of writing it on the html file

--- code.js
+++ code.js
@@ -28,3 +28,28 @@ function toggleShoppingCart(){
   menuEmail.classList.add('inactive');
   productDetailContainer.classList.toggle('inactive');
 }
+
+//this part is for the products cards
+
+const productList = [];
+const productsContainer = document.querySelector('.cards-container');
+
+productList.push({
+  name: 'Product 1',
+  price: 100,
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+  name: 'Product 2',
+  price: 200,
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+  name: 'Product 3',
+  price: 300,
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+for (product of productList){
+ //here you will develop the code that creates new product cards
+}
