1030bc56f5d8f57fad59137df044f8f6b365b044
bruno <-soy.brunosilva@gmail.com>
2023-12-06 12:32:12 -0300

    Adding the script that generates new products on the index

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ const navEmail = document.querySelector(".navbar-email");
 navEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
-  productDetailContainer.classList.add('inactive');
+  productDetailContainer.classList.add("inactive");
   menuEmail.classList.toggle("inactive");
 }
 //This is the code that I used to show/hide the mobile-menu
@@ -13,43 +13,82 @@ const mobileMenuContainer = document.querySelector(".mobile-menu");
 mobileMenuImg.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
-  productDetailContainer.classList.add('inactive');
-  mobileMenuContainer.classList.toggle('inactive');
+  productDetailContainer.classList.add("inactive");
+  mobileMenuContainer.classList.toggle("inactive");
 }
 
 //This part is your the shopping cart system
 
-const shoppingCartIcon = document.querySelector('#shopping-cart-icon');
-const productDetailContainer = document.querySelector('.product-detail');
-shoppingCartIcon.addEventListener('click', toggleShoppingCart);
+const shoppingCartIcon = document.querySelector("#shopping-cart-icon");
+const productDetailContainer = document.querySelector(".product-detail");
+shoppingCartIcon.addEventListener("click", toggleShoppingCart);
 
-function toggleShoppingCart(){
-  mobileMenuContainer.classList.add('inactive');
-  menuEmail.classList.add('inactive');
-  productDetailContainer.classList.toggle('inactive');
+function toggleShoppingCart() {
+  mobileMenuContainer.classList.add("inactive");
+  menuEmail.classList.add("inactive");
+  productDetailContainer.classList.toggle("inactive");
 }
 
 //this part is for the products cards
 
 const productList = [];
-const productsContainer = document.querySelector('.cards-container');
+const productsContainer = document.querySelector(".cards-container");
 
 productList.push({
-  name: 'Product 1',
-  price: 100,
-  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+  name: "Samsung galaxy S7",
+  price: "$300",
+  imgSrc: "https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg",
+});
 productList.push({
-  name: 'Product 2',
-  price: 200,
-  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+  name: "Camera",
+  price: "$500",
+  imgSrc:
+    "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
 productList.push({
-  name: 'Product 3',
-  price: 300,
-  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+  name: "PC Gamer",
+  price: "$1000",
+  imgSrc: "https://images.pexels.com/photos/2399840/pexels-photo-2399840.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
 
-for (product of productList){
- //here you will develop the code that creates new product cards
+//This is all the code that is necessary to create new products
+function productCreator() {
+  for (product of productList) {
+    let mainDiv = document.createElement("div");
+    mainDiv.className = "product-card";
+
+    let mainImg = document.createElement("img");
+    let img = product.imgSrc;
+    mainImg.setAttribute("src", img);
+    mainImg.setAttribute("alt", "A product");
+    mainDiv.appendChild(mainImg);
+
+    let secondDiv = document.createElement("div");
+    secondDiv.className = "product-info";
+    mainDiv.appendChild(secondDiv);
+
+    let thirdDiv = document.createElement("div");
+    secondDiv.appendChild(thirdDiv);
+
+    let priceP = document.createElement("p");
+    priceP.innerText = product.price;
+    let nameProductP = document.createElement("p");
+    nameProductP.innerText = product.name;
+
+    thirdDiv.appendChild(priceP);
+    thirdDiv.appendChild(nameProductP);
+
+    let figure = document.createElement('figure');
+    let shoppingCartImg = document.createElement('img');
+    shoppingCartImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+    figure.appendChild(shoppingCartImg);
+
+    secondDiv.appendChild(figure);
+
+    //After all this code, I'mg appending this code to the main div where the products will be
+    const cardContainer = document.querySelector(".cards-container");
+    cardContainer.append(mainDiv);
+  }
 }
+
+productCreator();
