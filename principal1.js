9f022d24dcf45c494d490fcd53b595a2bf9d4f1b
ReyesJuan27 <juanreyvir@gmail.com>
2023-12-07 17:20:16 -0500

    adlkadjajdlaj

--- principal1.js
+++ principal1.js
@@ -9,7 +9,7 @@ const botonCarrito = document.querySelector('.navbar-shopping-cart');
 
 const opcionesCorreoPc = document.querySelector('.desktop-menu');
 const opcionesCorreoMobile = document.querySelector('.mobile-menu');
-const opcionesCarrito = document.querySelector('.product-detail');
+const opcionesCarrito = document.querySelector('.product-detail-menu');
 
 //Desplegar menu con opciones del correo - Desktop/Mobile
 
@@ -18,10 +18,12 @@ function menuDestoktop_Mobile() {
     if(resolucion < 640) {
         opcionesCorreoMobile.classList.toggle('inactivo'); 
         opcionesCarrito.classList.add('inactivo');
+        detailsMenu.classList.add('inactivo');
     }
     else {
         opcionesCorreoPc.classList.toggle('inactivo');
         opcionesCarrito.classList.add('inactivo');
+        detailsMenu.classList.add('inactivo');
     }
 }
 botonCorreoDesktop.addEventListener('click', menuDestoktop_Mobile);
@@ -33,29 +35,13 @@ function menuCarroCompras () {
     opcionesCarrito.classList.toggle('inactivo');
     opcionesCorreoMobile.classList.add('inactivo');
     opcionesCorreoPc.classList.add('inactivo');
+    detailsMenu.classList.add('inactivo');
 }
 botonCarrito.addEventListener('click', menuCarroCompras);
 
+
 // Productos de la pag. principal
 
-const productCardHtml = document.querySelector('.cards-container')
-const listaDeProductos = [];
-
-listaDeProductos.push({
-    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    price: 120.0,
-    name: 'Bike'
-})
-listaDeProductos.push({
-    img: 'https://skate-home.com/6629-thickbox_default/skateboard-arte-de-pared-california.jpg' ,
-    price: 80.5,
-    name: 'Skate'
-})
-listaDeProductos.push({
-    img: 'https://cdnx.jumpseller.com/klikmuebles/image/32667328/thumb/610/610?1677673449' ,
-    price: 200.0,
-    name: 'Electric Scooper'
-})
 
 function newProduct () {
     for (product of listaDeProductos){
@@ -63,6 +49,8 @@ function newProduct () {
         productCard.classList.add('product-card');
         const imgHtml = document.createElement('img');
         imgHtml.setAttribute('src', product.img);
+        imgHtml.classList.add('cartImage');
+        
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -84,4 +72,70 @@ function newProduct () {
         
     }
 }
+const productCardHtml = document.querySelector('.cards-container');
+
+const listaDeProductos = [];
+
+listaDeProductos.push({
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    price: 120.0,
+    name: 'Bike',
+    description: "Cicla en buen estado, con tematica de los 2000', con un diseño estrategico para un uso comodo"
+})
+listaDeProductos.push({
+    img: 'https://skate-home.com/6629-thickbox_default/skateboard-arte-de-pared-california.jpg' ,
+    price: 80.5,
+    name: 'Skate',
+    description: "Skate practicamente nuevo, no presenta daños, y la pintura de la parte posterior esta bien compuesta"
+})
+listaDeProductos.push({
+    img: 'https://cdnx.jumpseller.com/klikmuebles/image/32667328/thumb/610/610?1677673449' ,
+    price: 200.0,
+    name: 'Electric Scooper',
+    description: "Scooper original, viene con cargador, y puede llegar hasta los 15 km/h"
+})
 newProduct();
+
+
+// Detalles de Productos de la pag. principal
+// .1 Botones para abrir y cerrar el menu de detalles.
+
+const productsBttn = document.querySelectorAll('.cartImage');
+const detailsMenu = document.querySelector('.product-detail-principal');
+
+    productsBttn.forEach(function(element) {
+    element.addEventListener('click', function(event){
+        productDetailMenu();
+        const indice = Array.from(productsBttn).indexOf(event.currentTarget);
+        productDetailFunction(indice);        
+    });
+    
+});
+
+function productDetailMenu() {
+    detailsMenu.classList.toggle('inactivo');
+    opcionesCorreoMobile.classList.add('inactivo');
+    opcionesCorreoPc.classList.add('inactivo');
+    opcionesCarrito.classList.add('inactivo');
+}
+
+const bttnCloseMenu = document.querySelector('.product-detail-principal-close');
+bttnCloseMenu.addEventListener('click', productDetailMenu);
+
+// .2 Detalles especificos de cada producto.
+
+function productDetailFunction(indice) {  
+    const imagenPrincipalMenu = document.querySelector('#image');
+    imagenPrincipalMenu.setAttribute('src', listaDeProductos[indice].img);
+    imagenPrincipalMenu.setAttribute('alt', listaDeProductos[indice].name);
+
+    const divProductInfo = document.querySelector('.product-info-principal');
+    const p1 = document.querySelector('#p1');
+    p1.innerText = ('$' + listaDeProductos[indice].price);
+    const p2 = document.querySelector('#p2');
+    p2.innerText = (listaDeProductos[indice].name);
+    const p3 = document.querySelector('#p3');
+    p3.innerText = (listaDeProductos[indice].description);
+}  
+
+const productDetailHtml= document.querySelector('.product-detail-principal');
