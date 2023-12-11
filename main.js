a711f5bf6f93555e025dabf0fde7cc8d609a8874
Oscar Ortiz <inge.oscarortiz@gmail.com>
2023-12-11 15:47:26 -0500

    Se añadieron con JS los menús de desktop y mobile

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const carritoMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+carritoMenu.addEventListener('click', toogleCarritoAside);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
+
+burguerMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');    
+});
+
+function toogleCarritoAside() {
+    // const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    // const isAsideClosed = aside.classList.contains('inactive');
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive'); 
+    desktopMenu.classList.add('inactive');
+}
+
