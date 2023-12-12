fcfdf6a8505a6eff045ff08aea351f6317838c23
Oscar Ortiz <inge.oscarortiz@gmail.com>
2023-12-12 16:49:17 -0500

    lista de productos HTML a partir de arrays con JS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carritoMenu = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 carritoMenu.addEventListener('click', toogleCarritoAside);
@@ -26,3 +27,78 @@ function toogleCarritoAside() {
     desktopMenu.classList.add('inactive');
 }
 
+const productList = [];
+productList.push({
+    name: 'bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/10963241/pexels-photo-10963241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'computador',
+    price: 620,
+    image: 'https://images.pexels.com/photos/4067506/pexels-photo-4067506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+  name: 'bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+  name: 'pantalla',
+  price: 220,
+  image: 'https://images.pexels.com/photos/10963241/pexels-photo-10963241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+  name: 'computador',
+  price: 620,
+  image: 'https://images.pexels.com/photos/4067506/pexels-photo-4067506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    // Creamos el elemento div (como estaba en el HTML)
+    const productCard = document.createElement('div');
+    // Le añadimos la clase (al igual como en el HTML)
+    productCard.classList.add('product-card');
+  
+    // Vamos a crear una imagen
+    const productImg = document.createElement('img');
+    // A esta imagen no tenemos que agregarle una clase sino un atributo src
+    // Pero esa imagen no tiene que ser la url sino la propiedad image de cada uno de los objetos dentro del array
+    productImg.setAttribute('src', product.image);
+    // product = {name, price, image} -> product.image  
+  
+    const productoInfo = document.createElement('div');
+    productoInfo.classList.add('product-info');
+  
+    const productoInfoDiv = document.createElement('div');
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+    productoInfoDiv.append(productPrice, productName);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    productInfoFigure.appendChild(productImgCart);
+  
+    productoInfo.append(productoInfoDiv, productInfoFigure);
+  
+    productCard.append(productImg, productoInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productList);
