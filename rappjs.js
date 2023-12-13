d4b7006c0ce632ee9a752d94252690b3b818da7a
Roberto Poveda <rapprj4500@gmail.com>
2023-12-12 21:18:21 -0500

    fusionando elementos de una tienda digital

--- rappjs.js
+++ rappjs.js
@@ -1,14 +1,95 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
-
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
 function toggleDesktopMenu(){
-
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
-
-    //menuEmail.innerHTML(desktopMenu);
-   
-
 }
 
+
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed){
+            mobileMenu.classList.add('inactive');
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+    
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Laptop',
+    price: 980,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProduct(arr){
+for(product of arr){
+    //product = {name, price e image} ->product.image
+    const productCard=document.createElement('div');
+    productCard.classList.add('product-card');
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src',product.image);
+
+    const productInfo=document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv=document.createElement('div');
+        const productPrice=document.createElement('p');    
+        productPrice.innerText='$'+product.price;
+        const productName=document.createElement('p');
+        productName.innerText= product.name;
+
+        const productInfoFigure=document.createElement('figure');
+        const figureImg=document.createElement('img');
+        figureImg.setAttribute('src','./icons/bt_add_to_cart.svg');        
+
+    productInfoFigure.appendChild(figureImg);
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+        
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    const cardsContainer = document.querySelector('.cards-container');
+    cardsContainer.appendChild(productCard);
+
+    }
+}
+
+renderProduct(productList);
