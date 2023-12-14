cbdef712dc96f20afe7fb14e08e50b04071cadf5
Frtavonatti <frtavonatti@gmail.com>
2023-12-14 20:27:57 -0300

    mobile menu functioning

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const burgerIcon = document.querySelector("img.menu")
+const burgerMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toogleDesktopMenu)
+burgerIcon.addEventListener("click", toogleBurgerMenu)
+
 
 function toogleDesktopMenu () {
     desktopMenu.classList.toggle("inactive")
+}
+
+function toogleBurgerMenu () {
+    console.log("Click")
+    burgerMenu.classList.toggle("inactive")
 }
