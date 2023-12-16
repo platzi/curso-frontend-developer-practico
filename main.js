d120cb5013aef114edace8b1f7c911ce414723c3
Manuel Fernando Rendon Orrego <rmanuelorrego@gmail.com>
2023-12-16 12:33:33 -0500

    se agregaron los detalles del producto y se corrigió el menu en mobile

---
+++ main.js
@@ -0,0 +1,115 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
+
+const cardsContainer = document.querySelector('.cards-container')
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+menuHamIcon.addEventListener('click', togglemobileMenu);
+
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleDesktopMenu(){
+    const isAsideClose= shoppingCartContainer.classList.contains('inactive');
+
+    if(!isAsideClose){
+        shoppingCartContainer.classList.add('inactive')
+    }
+
+    desktopMenu.classList.toggle('inactive')
+};
+
+function togglemobileMenu(){
+    const isAsideClose= mobileMenu.classList.contains('inactive');
+
+    if(!isAsideClose){
+        shoppingCartContainer.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+};
+
+function toggleCarritoAside(){
+    const isMobileMenuClose= mobileMenu.classList.contains('inactive');
+    const isDesktClose = desktopMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isDesktClose){
+        desktopMenu.classList.add('inactive');
+    }
+
+    shoppingCartContainer.classList.toggle('inactive');
+};
+
+const productList =[];
+productList.push({
+    name: 'bike',
+    price:'120',
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name :'phone',
+    price: '500',
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "laptop",
+    price:"800",
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "watch",
+    price: "300",
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+})
+
+
+function renderProducts(arr){
+
+    for (product  of arr){
+
+        const productCard=document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src',product.img);
+        //product = {name, price,image}
+    
+        const productInfo=document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv=document.createElement('div');
+    
+        const productPrice=document.createElement('p');
+        productPrice.innerText='$'+product.price;
+        const productName=document.createElement('p');
+        productName.innerText=product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure=document.createElement('figure');
+        const productImgCard=document.createElement('img');
+        productImgCard.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCard);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    };
+}
+
+renderProducts(productList);
