c9b96cc5de6a68c4ec36a1849a96442cf07bb298
Roberto Poveda <rapprj4500@gmail.com>
2023-12-18 00:28:46 -0500

    Agregando product detail

--- rappjs.js
+++ rappjs.js
@@ -18,6 +18,8 @@ function toggleMobileMenu(){
     if(!isAsideClosed){
         shoppingCartContainer.classList.add('inactive');
     }
+
+    closeProductDetailAside();
     mobileMenu.classList.toggle('inactive');
 }
 
@@ -33,6 +35,15 @@ function toggleCarritoAside(){
     if(!isDesktopMenuClosed){
         desktopMenu.classList.add('inactive');
     }
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    if(!isProductDetailClosed){
+            productDetailContainer.classList.add('inactive');
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
 
     shoppingCartContainer.classList.toggle('inactive');
     
@@ -55,6 +66,20 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 
+const productDetailContainer=document.querySelector('#productDetail');
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+
+}
+
+const productDetailCloseIcon=document.querySelector('.product-detail-close');
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
+}
+
+
 function renderProduct(arr){
 for(product of arr){
     //product = {name, price e image} ->product.image
@@ -62,6 +87,9 @@ for(product of arr){
     productCard.classList.add('product-card');
     const productImg = document.createElement('img');
     productImg.setAttribute('src',product.image);
+    productImg.addEventListener('click',openProductDetailAside);
+
+
 
     const productInfo=document.createElement('div');
     productInfo.classList.add('product-info');
