c3eda2628638fbd3895a000e66c96e16c07725fc
Oscar Ortiz <inge.oscarortiz@gmail.com>
2023-12-18 17:08:22 -0500

    Se abre el productDetail  dependiendo de a que imagen se le de click, también oscurecer mientras este abierto un elemento

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const cardsContainer = document.querySelector('.cards-container');
 const productDetailContainer = document.querySelector('#productDetail');
 const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const dark = document.querySelector('.dark');
+const imageDetail = document.querySelector('.imageDetail');
+const productInfoPrice = document.querySelector('.product-info_price');
+const productInfoName = document.querySelector('.product-info_name');
+const productInfoMsg = document.querySelector('.product-info_msg');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 carritoMenu.addEventListener('click', toogleCarritoAside);
@@ -25,6 +29,10 @@ dark.addEventListener('click', () => {
 
 function toggleDesktopMenu() {
     closeDark();
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    if(!isDesktopMenuClosed){
+      dark.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
     shoppingCartContainer.classList.add('inactive');
     productDetailContainer.classList.add('inactive');    
@@ -36,20 +44,37 @@ burguerMenu.addEventListener('click', () => {
     productDetailContainer.classList.add('inactive');     
 });
 
-function toogleCarritoAside() {
-    // const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    // const isAsideClosed = aside.classList.contains('inactive');
+function toogleCarritoAside() {    
     closeDark();
+    const isCarritoAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    if(!isCarritoAsideClosed){
+      dark.classList.add('inactive');
+    }
     shoppingCartContainer.classList.toggle('inactive');
     mobileMenu.classList.add('inactive'); 
     desktopMenu.classList.add('inactive');
     productDetailContainer.classList.add('inactive');       
 }
 
-function openProductDetailAside() {
-  closeDark();
-  productDetailContainer.classList.remove('inactive');
-  shoppingCartContainer.classList.add('inactive');  
+/* Detalles de los productos */
+
+function openProductDetailAside(e) {
+  closeDark(); 
+  productDetailContainer.classList.remove('inactive'); 
+  shoppingCartContainer.classList.add('inactive');      
+
+  //Objengo la imagen sobre la cual se hace clic
+  imageDetail.src = e.target.src;
+  
+  // Obtengo el contendor padre
+  var contenedor = e.target.parentElement;  
+  //Buscamos el elemento dentro del contenedor padre
+  var precioElement = contenedor.querySelector('.price');
+  var messageElement = contenedor.querySelector('.message');
+  var nameElement = contenedor.querySelector('.name');
+  productInfoPrice.innerText = precioElement.innerText;
+  productInfoName.innerText = nameElement.innerText;
+  productInfoMsg.innerText = messageElement.innerText;  
 }
 
 productDetailCloseIcon.addEventListener('click', () => {
@@ -62,36 +87,48 @@ productList.push({
     name: 'bike',
     price: 120,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    msg: 'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.',
+    class: 'imageBike_1',
 });
 
 productList.push({
-    name: 'pantalla',
+    name: 'iMac',
     price: 220,
     image: 'https://images.pexels.com/photos/10963241/pexels-photo-10963241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    msg: 'iMac A1419 2017 8gb 1tb Video 4gb',
+    class: 'imageImac_1',
 });
 
 productList.push({
-    name: 'computador',
+    name: 'portátil',
     price: 620,
     image: 'https://images.pexels.com/photos/4067506/pexels-photo-4067506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    msg: 'Portátil Dell Inspiron 3511 negra 15.6", Intel Core i5 1135G7 8GB de RAM 256GB SSD',
+    class: 'imagePortatil_1',
 });
 
 productList.push({
   name: 'bike',
   price: 120,
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  msg: 'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.',
+  class: 'imageBike_2',
 });
 
 productList.push({
-  name: 'pantalla',
+  name: 'iMac',
   price: 220,
   image: 'https://images.pexels.com/photos/10963241/pexels-photo-10963241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  msg: 'iMac A1419 2017 8gb 1tb Video 4gb',
+  class: 'imageImac_2',
 });
 
 productList.push({
-  name: 'computador',
+  name: 'portátil',
   price: 620,
   image: 'https://images.pexels.com/photos/4067506/pexels-photo-4067506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  msg: 'Portátil Dell Inspiron 3511 negra 15.6", Intel Core i5 1135G7 8GB de RAM 256GB SSD',
+  class: 'imagePortatil_2',
 });
 
 function renderProducts(arr) {
@@ -106,18 +143,24 @@ function renderProducts(arr) {
     // A esta imagen no tenemos que agregarle una clase sino un atributo src
     // Pero esa imagen no tiene que ser la url sino la propiedad image de cada uno de los objetos dentro del array
     productImg.setAttribute('src', product.image);
+    productImg.classList.add(product.class);
     // product = {name, price, image} -> product.image
-    productImg.addEventListener('click', openProductDetailAside);  
-  
+        
     const productoInfo = document.createElement('div');
     productoInfo.classList.add('product-info');
   
     const productoInfoDiv = document.createElement('div');
     const productPrice = document.createElement('p');
+    productPrice.classList.add('price');
     productPrice.innerText = '$' + product.price;
     const productName = document.createElement('p');
+    productName.classList.add('name');
     productName.innerText = product.name;
-    productoInfoDiv.append(productPrice, productName);
+    const message = document.createElement('p');
+    message.classList.add('message');    
+    message.classList.add('inactive');  
+    message.innerText = product.msg; 
+    productoInfoDiv.append(productPrice, productName, message);
   
     const productInfoFigure = document.createElement('figure');
     const productImgCart = document.createElement('img');
@@ -131,5 +174,6 @@ function renderProducts(arr) {
     cardsContainer.appendChild(productCard);
   }
 }
-
 renderProducts(productList);
+
+cardsContainer.addEventListener('click', openProductDetailAside);
