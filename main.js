88da0b17aef030774544c994e21a689953bb02b3
Jhon Camacho <jhonfcp@hotmail.com>
2023-12-22 12:12:11 -0500

    Se han fusionado archivos clase 11,8,13,6 y se ha maqueteado los productos dinamicamente en JS

--- main.js
+++ main.js
@@ -1,9 +1,106 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+
+}
+
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://cdn-3.expansion.mx/dims4/default/24d810e/2147483647/strip/true/crop/4500x3000+0+0/resize/1800x1200!/format/webp/quality/80/?url=https%3A%2F%2Fcdn-3.expansion.mx%2F13%2F9a%2Fb8761e8a445384a8f737bf384452%2Fc34g55t-001-front-black.jpg'
+})
+productList.push({
+    name: 'Computador',
+    price: 620,
+    image: 'https://www.alkosto.com/medias/750Wx750H-master-hotfolder-transfer-incoming-deposit-hybris-interfaces-IN-media-product-196548729449-001.jpg?context=bWFzdGVyfGltYWdlc3wyMjc4Nzd8aW1hZ2UvanBlZ3xhRFk1TDJoalpDOHhNelV5TVRBNU5qQXhOVGt3TWk4M05UQlhlRGMxTUVoZmJXRnpkR1Z5TDJodmRHWnZiR1JsY2k5MGNtRnVjMlpsY2k5cGJtTnZiV2x1Wnk5a1pYQnZjMmwwTDJoNVluSnBjeTFwYm5SbGNtWmhZMlZ6TDBsT0wyMWxaR2xoTDNCeWIyUjFZM1F2TVRrMk5UUTROekk1TkRRNVh6QXdNUzVxY0djfDI2YjhmNDE5ZWI2NDQ5YTFmYmFkZTNjOGFiODhlOTA3YTg4ODNmNzVjZjY3MzY3ZDkxYTlkMTJkMjAzNTc2OTg'
+})
+
+
+function renderProducts(arr){
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        // product = {name,price,image} ->product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProducts(productList);
