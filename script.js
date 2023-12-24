2d648c20eede78d526b513472315da50dd9ca32e
Sergio Palacios <sergid26pal@gmail.com>
2023-12-24 17:25:48 -0500

    Fusión de la visualización de productos. Estos se mostraran según el array obtenido

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
 const navbarR = document.querySelector(".navbar-email");
 const carrito = document.querySelector(".navbar-shopping-cart");
 const btn_movilMenu = document.querySelector(".menu");
+const containerProductos = document.querySelector(".cards-container");
 
 const menuPerfil = document.querySelector(".desktop-menu");
 const movilMenu =  document.querySelector(".mobile-menu");
 const asideCarrito = document.querySelector(".product-detail");
 
+//Array de Productos 
+const productos = [];
 
+productos.push({
+    name:"Cicla",
+    price:"120",
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productos.push({
+    name:"TV",
+    price:"110",
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productos.push({
+    name:"Carro",
+    price:"220",
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productos.push({
+    name:"Paleta",
+    price:"10",
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productos.push({
+    name:"Arroz",
+    price:"320",
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+//Control de aparición de interfaces
 navbarR.addEventListener("click",()=>{
     menuPerfil.classList.toggle("inactive");
     asideCarrito.classList.add("inactive");
@@ -32,3 +66,28 @@ carrito.addEventListener("click",()=>{
         movilMenu.classList.add("inactive");
     }
 });
+
+//Integración de  los productos usando el DOM
+function renderProductos(x){
+    for(let i=0; i<x.length; i++){
+        let tarjeta = document.createElement("div");
+        
+        tarjeta.setAttribute("class","product-card");
+        tarjeta.innerHTML = `
+            <img src="${x[i].img}" alt="${x[i].name}">
+            <div class="product-info">
+                <div>
+                    <p>${x[i].price}</p>
+                    <p>${x[i].name}</p>
+                </div>
+                <figure>
+                    <img src="./icons/bt_add_to_cart.svg" alt="">
+                </figure>
+            </div>
+        `;
+    
+        containerProductos.append(tarjeta);
+    }
+}
+
+renderProductos(productos);
