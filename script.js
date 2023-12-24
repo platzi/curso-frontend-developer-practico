a84d0c1b375636cc7c732cbd8931a24f97e5410c
Sergio Palacios <sergid26pal@gmail.com>
2023-12-24 16:20:09 -0500

    adaptación del nav a dispositivos moviles

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const navbarR = document.querySelector(".navbar-email");
 const menuPerfil = document.querySelector(".desktop-menu");
+const movilMenu =  document.querySelector(".mobile-menu");
+const btn_movilMenu = document.querySelector(".menu");
 
 navbarR.addEventListener("click",()=>{
     menuPerfil.classList.toggle("inactive");
+});
+
+btn_movilMenu.addEventListener("click",()=>{
+    movilMenu.classList.toggle("inactive");
 });
