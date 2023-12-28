6c8125d1e2fe10ba886bc8b0d80587ab975dda39
Juan David Santamaria <juanda404@gmail.com>
2023-12-28 17:09:55 -0500

    Se implemente el menu en vista mobile and is modifica el mediaquery para los tipos de tamaño se oculten el submenu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamDesktop = document.querySelector('.menu');
+const menuMobil = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click',toggleDesktopMenu);
+menuHamDesktop.addEventListener('click',toggleMobilMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobilMenu(){
+    menuMobil.classList.toggle('inactive');
 }
