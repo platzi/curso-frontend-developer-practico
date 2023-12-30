13cd3f5a8b63d4db203408095bbee7997877a610
crdCalma <cdguttierrez11@gmail.com>
2023-12-30 18:06:01 -0500

    Se integran los menus mobile y desktop

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+//Selectores
 const menuEmail = document. querySelector ('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerIconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+//Llamadas
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerIconMenu.addEventListener('click', togglemobilMenu)
+
 
 function toggleDesktopMenu() {
 console. log('Click');
 desktopMenu.classList.toggle('inactive');
 }
+
+function togglemobilMenu() {
+    console. log('Click');
+    mobileMenu.classList.toggle('inactive');
+    }
