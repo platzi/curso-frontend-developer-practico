dc756091dd148db18acc20366b380043c0b748f3
Gabriel Luna <130194539+leirbagvan@users.noreply.github.com>
2023-12-30 12:04:54 -0500

    Fusionando todos los menus

--- main.js
+++ main.js
@@ -1,17 +1,53 @@
 const menuEmail     = document.querySelector('.navbar-email')
-const desktopMenu   = document.querySelector('.desktop-menu')
 const menuHamIcon   = document.querySelector('.menu')
+const menuCarrito   = document.querySelector('.navbar-shopping-cart')
+
+
+const desktopMenu   = document.querySelector('.desktop-menu')
 const mobileMenu    = document.querySelector('.mobile-menu')
+const cartDetail    = document.querySelector('.product-detail')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarrito.addEventListener('click', toggleMenuCart)
+
 
 function toggleDesktopMenu(){
+   const ismobileMenuClosed = mobileMenu.classList.contains('inactive')
+   const isCartDetailClosed = cartDetail.classList.contains('inactive')
+   if (!ismobileMenuClosed){
+      mobileMenu.classList.add('inactive')
+   }
+   if (!isCartDetailClosed){
+      cartDetail.classList.add('inactive')
+   }
    desktopMenu.classList.toggle('inactive') 
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive') 
+   const isCartDetailClosed = cartDetail.classList.contains('inactive')
+   const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+   if (!isCartDetailClosed){
+      cartDetail.classList.add('inactive')
+   }
+   if (!isDesktopMenuClosed){
+      desktopMenu.classList.add('inactive')
+   }
+   
+   mobileMenu.classList.toggle('inactive')
  }
+
+ function toggleMenuCart(){
+   const ismobileMenuClosed = mobileMenu.classList.contains('inactive')
+   const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+   if (!ismobileMenuClosed){
+      mobileMenu.classList.add('inactive')
+   }
+   if (!isDesktopMenuClosed){
+      desktopMenu.classList.add('inactive')
+   }
+   cartDetail.classList.toggle('inactive') 
+}
+
 
