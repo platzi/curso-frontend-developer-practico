69a4e5bd2b3ffdad1bcd172a5a878840311e6989
crdCalma <cdguttierrez11@gmail.com>
2023-12-30 19:01:55 -0500

    solucion menus solapados

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 //Selectores
 const menuEmail = document. querySelector ('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const burgerIconMenu = document.querySelector('.menu');
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart')
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 //Llamadas
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerIconMenu.addEventListener('click', togglemobilMenu)
+menuShoppingCart.addEventListener('click', toggleShoppingCart)
 
 
 function toggleDesktopMenu() {
-console. log('Click');
-desktopMenu.classList.toggle('inactive');
+
+    aside.classList.add('inactive')
+    desktopMenu.classList.toggle('inactive')
 }
 
 function togglemobilMenu() {
-    console. log('Click');
-    mobileMenu.classList.toggle('inactive');
-    }
+
+    aside.classList.add('inactive')
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleShoppingCart() {
+    
+    desktopMenu.classList.add('inactive')
+    mobileMenu.classList.add('inactive')
+    aside.classList.toggle('inactive')
+}
