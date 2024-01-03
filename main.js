d503ee5f7f2c49c514a75fa9138e148f56f6d42a
DeivyRivas <deyruiz555@gmail.com>
2024-01-03 16:28:53 -0500

    SE INTEGRA EL MENU MOBILE

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 //DECLARACION E INICIALIZACION DE VARIABLE
-const menuEmail = document.querySelector('.navbar-email') // selecionamos el elemento que daremos clickdesktop-menu
-const desktopMenu = document.querySelector('.desktop-menu') // selecionamos el elemento mostraremos al dar click desktop-menu
+const menuEmail = document.querySelector('.navbar-email') // selecionamos el elemento que daremos click 
+const desktopMenu = document.querySelector('.desktop-menu') // selecionamos el elemento mostraremos al dar click 
+const menuHamIcon = document.querySelector('.menu') // selecionamos el elemento que daremos clickd
+const mobileMenu = document.querySelector('.mobile-menu') // selecionamos el elemento mostraremos al dar click 
+
+
 
 
 //EVENTOS
 menuEmail.addEventListener('click', toggleDesktopMenu) // metodo para ejecutar algo cuando le demos click al elemento selecionado
+menuHamIcon.addEventListener('click', toggleMobileMenu) // metodo para ejecutar algo cuando le demos click al elemento selecionado
+
 
 
 //FUNCIONES
 // funcion para que el menu aparezca y desaparezca 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive')// le pone o le quita la clase inactive si la tiene o no 
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')// le pone o le quita la clase inactive si la tiene o no 
 }
