75224518a5304c00773a8489efd532bc4d1d9ea8
Torres2000 <pedro21t25@gmail.com>
2024-01-09 16:05:12 -0500

    Fusionamos el menu para mobile y desktop

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-menuEmail.addEventListener("click", toogle);
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamMenu.addEventListener("click", toggleMobileMenu);
 
-function toogle() {
+function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
