6fcb5b60e2f6685b62d6ea29ab000e6d52b70f35
Alvaro Mosqueda <almos41294@gmail.com>
2024-01-14 20:32:59 -0500

    Clase #21 Carrito de compras... Se agregó el menú del carritos de compras y se le pusieron las funciones .add a nuestras funciones que usan el .toogle

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const nav_bar = document.querySelector(".navbar-email")
 const nav_email = document.querySelector(".desktop-menu")
 const menu = document.querySelector(".menu")
 const mobileMenu= document.querySelector(".mobile-menu")
+const productDetail = document.querySelector(".product-detail")
+const shoppingCart = document.querySelector(".shopping-cart-image")
 
 function quitarInactive(){
 nav_email.classList.toggle ("inactive")
+productDetail.classList.add("inactive")
 }
 
 function quitarInactive2(){
     mobileMenu.classList.toggle ("inactive")
-    }
+    productDetail.classList.add ("inactive")
+}
+
+function quitarInactive3(){
+    productDetail.classList.toggle("inactive")
+    nav_email.classList.add("inactive")
+    mobileMenu.classList.add("inactive")
+}
 
 nav_bar.addEventListener("click", quitarInactive)
 menu.addEventListener("click", quitarInactive2)
+shoppingCart.addEventListener ("click", quitarInactive3)
