9d421799da547bc1a0e7553c3dc83423a5a387c7
Alvaro Mosqueda <almos41294@gmail.com>
2024-01-14 19:47:42 -0500

    Clase #20 Fusión del menú en mobile... Clase en la que volvimos a utilizar la función toogle.

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const nav_bar = document.querySelector(".navbar-email")
 const nav_email = document.querySelector(".desktop-menu")
+const menu = document.querySelector(".menu")
+const mobileMenu= document.querySelector(".mobile-menu")
 
 function quitarInactive(){
 nav_email.classList.toggle ("inactive")
 }
 
+function quitarInactive2(){
+    mobileMenu.classList.toggle ("inactive")
+    }
+
 nav_bar.addEventListener("click", quitarInactive)
+menu.addEventListener("click", quitarInactive2)
