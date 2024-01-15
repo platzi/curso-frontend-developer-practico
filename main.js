358d839444accf211765c8c7c7a65525118b7c24
Alvaro Mosqueda <almos41294@gmail.com>
2024-01-14 21:44:14 -0500

    Clase #22 Lista de productos: HTML a partir de arrays... Clase de utilidad para entender mejor el manejo del DOM y el uso de una manera diferente para imrpimir de manera dinámica mis arrays en un HTML.

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const menu = document.querySelector(".menu")
 const mobileMenu= document.querySelector(".mobile-menu")
 const productDetail = document.querySelector(".product-detail")
 const shoppingCart = document.querySelector(".shopping-cart-image")
+const cardContainer = document.querySelector(".cards-container")
+
+nav_bar.addEventListener("click", quitarInactive)
+menu.addEventListener("click", quitarInactive2)
+shoppingCart.addEventListener ("click", quitarInactive3)
+
+const productList = []
 
 function quitarInactive(){
 nav_email.classList.toggle ("inactive")
@@ -21,6 +28,70 @@ function quitarInactive3(){
     mobileMenu.classList.add("inactive")
 }
 
-nav_bar.addEventListener("click", quitarInactive)
-menu.addEventListener("click", quitarInactive2)
-shoppingCart.addEventListener ("click", quitarInactive3)
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image2: "./icons/bt_add_to_cart.svg"
+})
+
+productList.push({
+    name: "MacBook",
+    price: 2220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image2: "./icons/bt_add_to_cart.svg"
+})
+
+productList.push({
+    name: "Screen",
+    price: 300,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image2: "./icons/bt_add_to_cart.svg"
+})
+
+
+
+
+
+for (product of productList){
+
+    const productCard= document.createElement("div")
+    productCard.classList.add("product-card")
+
+   
+   
+
+    const img= document.createElement("img")
+    img.setAttribute("src", product.image)
+
+    const productInfo = document.createElement("div")
+    productInfo.classList.add("product-info")
+
+  
+
+    const productInfoDiv = document.createElement("div")
+
+
+    const parrafPrice= document.createElement("p")
+    parrafPrice.innerText= "$" + product.price
+
+    const parrafProduct= document.createElement("p")
+    parrafProduct.innerText= product.name
+
+    const figure = document.createElement("figure")
+
+    const img2= document.createElement("img")
+    img2.setAttribute("src", product.image2)
+
+  
+
+    productCard.appendChild(img)
+    productCard.appendChild(productInfo)
+    productInfo.appendChild(productInfoDiv)
+    productInfo.appendChild(figure)
+    productInfoDiv.appendChild(parrafPrice)
+    productInfoDiv.appendChild(parrafProduct)
+    figure.appendChild(img2)
+    cardContainer.appendChild(productCard)
+
+}
