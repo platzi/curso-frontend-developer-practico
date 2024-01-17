29b02c8b0c7b7a57de3c15b2d912ad51e78a5c4f
Maximiliano <mc349821@gmail.com>
2024-01-17 13:02:14 -0500

    se agrego la funcion del menu mobile en la pagina principal

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const MenuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu'); 
+const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu'); 
+
+
 
 MenuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobilrMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobilrMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
