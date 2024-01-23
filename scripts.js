aa2f996cebe9d30da4c6c3fce0a5702415bb967f
Sebastian Cazon <sebastiancazon301@gmail.com>
2024-01-23 15:32:51 -0400

    agregando los menues desplegables y que solo se muestre uno a la vez

--- scripts.js
+++ scripts.js
@@ -2,13 +2,32 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBurger = document.querySelector('.menu');
+const carrito = document.querySelector('.navbar-shopping-cart');
+const menuCarrito = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', abrirMenu);
 function abrirMenu() {
     desktopMenu.classList.toggle('inactive');
+    if (!menuCarrito.classList.contains('inactive')) {
+        menuCarrito.classList.toggle('inactive');
+    }
 }
 
 menuBurger.addEventListener('click', abrirMenuMobile);
 function abrirMenuMobile() {
     mobileMenu.classList.toggle('inactive');
+    if (!menuCarrito.classList.contains('inactive')) {
+        menuCarrito.classList.toggle('inactive');
+    }
+}
+
+carrito.addEventListener('click', abrirDetalle);
+function abrirDetalle() {
+    menuCarrito.classList.toggle('inactive');
+    if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.toggle('inactive');
+    }
+    if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.toggle('inactive');
+    }
 }
