59cafa4a9e4f495bb20bb5da474c084939e7567d
Sebastian Cazon <sebastiancazon301@gmail.com>
2024-01-23 11:08:32 -0400

    agregando el menu mobile al index y sus estilos a styles, de la clase 8

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBurger = document.querySelector('.menu');
 
 menuEmail.addEventListener('click', abrirMenu);
-
 function abrirMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+menuBurger.addEventListener('click', abrirMenuMobile);
+function abrirMenuMobile() {
+    mobileMenu.classList.toggle('inactive');
 }
