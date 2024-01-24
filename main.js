dd30c18b77680980a0372d1999cd79a5600b57f8
Bruno Velcheff <bvelcheff@gmail.com>
2024-01-24 02:53:17 -0300

    fusion menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenuIcon = document.querySelector('.mobileMenuIcon');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
