45996d58c6ae80868d09a9d2e3d73c08687036d4
Bruno Velcheff <bvelcheff@gmail.com>
2024-01-24 03:34:40 -0300

    fusionamos el carrito de compras-aside

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const carritoDetail = document.querySelector('.product-detail');
 const burgerMenuIcon = document.querySelector('.mobileMenuIcon');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenuIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 
 function toggleDesktopMenu() {
+    const isCarritoDetailClosed = carritoDetail.classList.contains('inactive');
+
+    if (!isCarritoDetailClosed){
+        carritoDetail.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isCarritoDetailClosed = carritoDetail.classList.contains('inactive');
+
+    if (!isCarritoDetailClosed){
+        carritoDetail.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    carritoDetail.classList.toggle('inactive')
 }
