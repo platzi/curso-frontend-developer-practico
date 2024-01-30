c9d1f803f7892bc77187da1b4d64cb49cbb2ba2d
rully74 <julian_hernando.ruiz@uao.edu.co>
2024-01-30 14:15:28 -0500

    Se fusiona el codigo principal con el carrito de compras, se le añade el funcionamiento y logica, y se arreglan bugs por medio de condicionales para que los diferentes menus no se solapen.

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const MenuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 const burguerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+MenuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideCarritoClosed = aside.classList.contains("inactive");
+
+    if(!isAsideCarritoClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isAsideCarritoClosed = aside.classList.contains("inactive");
+
+    if(!isAsideCarritoClosed){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }else if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
