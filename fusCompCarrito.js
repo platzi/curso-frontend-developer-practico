dea930942920789d9a401e6f0f4e981416be51d0
Camilo Alarcón <camiloalarcon11155@gmail.com>
2024-02-05 11:34:45 -0500

    use archivos fuscompCarrito. html, js y css para ajustar menus de correo, icono hamburguesa e icono carrito con ssus respectivos menus, para que se cerraran y abrieran sin interpponerse entre ellos

--- fusCompCarrito.js
+++ fusCompCarrito.js
@@ -3,27 +3,54 @@
 
 
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const DesktopMenuEmail = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuIHam = document.querySelector('.mobile-menu');
+const menuCarritoIcon  = document.querySelector('.navbar-shopping-cart');
+const menuCarritoCompras = document.querySelector('.product-detail');
 
 
 
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenuEmail);
 
 menuHamIcon.addEventListener('click', ToggleMobilMenu);
 
-function toggleDesktopMenu() {
+menuCarritoIcon.addEventListener('click', ToggleCarritoCompras);
+
+
+function toggleDesktopMenuEmail() {
+
+     const ismMenuCarritoComprasClosed = menuCarritoCompras.classList.contains('inactive');//Si contiene esa clase, entonces esta el menu cerrado
+    if (!ismMenuCarritoComprasClosed) {
+        menuCarritoCompras.classList.add('inactive');
+    }
    
-    desktopMenu.classList.toggle('inactive');//quiere decir que el toggle lo activa e inactiva
+    DesktopMenuEmail.classList.toggle('inactive');//quiere decir que el toggle lo activa e inactiva
 }
+
 
 function ToggleMobilMenu() {
-   
-    mobileMenu.classList.toggle('inactive');//quiere decir que el toggle lo activa e inactiva
+    //en una variable almacenamos 
+    const ismMenuCarritoComprasClosed = menuCarritoCompras.classList.contains('inactive');//Si contiene esa clase, entonces esta el menu cerrado
+    if (!ismMenuCarritoComprasClosed) {
+        menuCarritoCompras.classList.add('inactive');
+    }
+        mobileMenuIHam.classList.toggle('inactive');//quiere decir que el toggle lo activa e inactiva
+    
 }
 
 
+function ToggleCarritoCompras() {
+    //menuCarritoCompras.classList.toggle('inactive');
+    //para que cuando est en modo movil con el menu del movil desplegado y le demos en el menu de tus ordenes y no se superpongan, entonces tenemos que preguntar si estan activos o inactivos, para que aparezca uno o el otro
+    const ismobileMenuIHamClosed = mobileMenuIHam.classList.contains('inactive');
+   // const ismMenuCarritoComprasClosed = menuCarritoCompras.classList.contains('inactive');
+    if (!ismobileMenuIHamClosed) {
+        mobileMenuIHam.classList.add('inactive');
+    }
+    menuCarritoCompras.classList.toggle('inactive');
+
+}
 
 
