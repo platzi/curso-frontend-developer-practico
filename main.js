61a9cb125d42d58c5e779da7d4c496ca7d861c25
Victor Castillo <vc3986792@gmail.com>
2024-02-08 21:43:57 -0500

    creado la tienda de la pagina

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const burguerMenu = document.querySelector('.menu')
 const menuCarrito = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu')
 const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
@@ -42,3 +43,58 @@ function toggleCarritoAside () {
 
     aside.classList.toggle('inactive')
 }
+
+const productList = [];
+productList.push({
+    name: 'bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+productList.push({
+    name: 'Play 5',
+    price: 175,
+    image: "https://pladani.com/wp-content/uploads/2022/04/Play5-Digital.jpg"
+})
+productList.push({
+    name: 'computador',
+    price: 175,
+    image: "https://images.pexels.com/photos/17136620/pexels-photo-17136620/free-photo-of-escritorio-tecnologia-ordenador-monitor.jpeg"
+})
+
+function rendersProduct (arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+    
+        const productImg = document.createElement('img')
+        productImg.setAttribute('src', product.image)
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+    
+        const productInfoDiv = document.createElement('div')
+    
+        const productPrice = document.createElement('p') 
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p') 
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productName);
+    
+    
+        const productInfoFigure = document.createElement('Figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+    
+        cardsContainer.appendChild(productCard);    
+    }
+}
+
+rendersProduct(productList);
