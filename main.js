0912c53ed1869d7b0aff56b402ce0e34f906b036
Carlos Colorado <caancoqu@gmail.com>
2024-02-21 18:09:06 -0500

    Integrando el menu para mobiles con el menu de desktop

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const menuEmail = document.querySelector('.navbar-email') //al darle click al email aparece el menu desktop
-const desktopMenu = document.querySelector('.desktop-menu')
+const menuEmail = document.querySelector('.navbar-email'); //al darle click al email aparece el menu desktop
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
+menuHamIcon.addEventListener('click',toggleMobileMenu)
 
 menuEmail.addEventListener('click',toggleDesktopMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
     /*e agrega o quita la clase inactive de desktop-menu para mostrar o cerrar el menu desktop al dar click en el correo */
+}
+
+function toggleMobileMenu(){
+   mobileMenu.classList.toggle('inactive');
 }
