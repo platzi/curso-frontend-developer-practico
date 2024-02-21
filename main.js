f45f60bd5da308e3de3cab2c54a2d53f399a38a4
Divar Parra <arc.eseisnos@gmail.com>
2024-02-21 16:39:49 -0500

    Carrito de compras y funcion inactive para aparecer o desaparecer los menus

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const menuHamIcon = document.querySelector('.menu');
+const mobilemenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 
 function toggleDesktopMenu() {
+    const isasideClosed = aside.classList.contains('inactive');
+    
+    if (!isasideClosed) {
+        aside.classList.add('inactive');
+    }
+    mobilemenu.classList.toggle('inactive');
+
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isasideClosed = aside.classList.contains('inactive');
+
+    if (!isasideClosed) {
+        aside.classList.add('inactive');
+    }
+    mobilemenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const ismobilMenuClosed = mobilemenu.classList.contains('inactive');
+
+    if (!ismobilMenuClosed) {
+        mobilemenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
