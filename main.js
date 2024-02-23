e7b08fc40dd3f623aa9dc0321f86c8cf1908b125
Carlos Colorado <caancoqu@gmail.com>
2024-02-23 17:03:03 -0500

    Fusion del resto de componentes en el archivo principal

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const carsContainer = document.querySelector('.cards-container')
 
 menuHamIcon.addEventListener('click',toggleMobileMenu)
 
@@ -43,3 +44,68 @@ function toggleCarritoAside(){
     aside.classList.toggle('inactive');
 
 }
+
+//para agregar los prodcutos de manera dinamica 
+const productList = [];
+productList.push({
+  name:'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+  name:'Pc',
+  price: 500,
+  image: 'https://megaobzor.com/uploads/stories/169455/inf1.jpg'
+})
+
+productList.push({
+  name:'Print',
+  price: 250,
+  image: 'https://www.provesersa.com/wp-content/uploads/2021/12/precio-regular-PIXMAG2160-BK_xl-6-min.jpg'
+})
+
+
+/*este codigo agrega automaticamente todos los productos que esten en una lista no importa la cantidad sean 1 o 10*/
+
+
+function renderProducts(arr){
+  for (product of arr ){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src',product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+  
+    const productInfoDiv = document.createElement('div');
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$'+ product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+  
+    const productInfoFigure  = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+    
+    carsContainer.appendChild(productCard);
+  }
+}
+
+
+renderProducts(productList);
