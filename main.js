7176b87d84cf2d79c288298eb17aabe13fdc2a7d
VictorEstevez02 <Estevezvictor17@gmail.com>
2024-03-03 19:15:29 -0300

    Agregando detalle de producto

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ const mobileMenu = document.querySelector('.mobile-menu')
 const carsDetail = document.querySelector('.navbar-shopping-cart')
 const asideCartShopping = document.querySelector('#detailsCart')
 const cardsContainer = document.querySelector('.cards-container')
+const detalleProducto = document.querySelector('#productDetails')
+const detalleProductoClosed = document.querySelector('.product-detail-close')
 
 naveMenu.addEventListener('click', toggleDesktopMenu)
 menuIcon.addEventListener('click', toggleMobileMenu)
 carsDetail.addEventListener('click',toggleCarsDetails)
+detalleProductoClosed.addEventListener('click', closedProductsDetails)
 
 function toggleDesktopMenu() {
     const asideCartShoppingClosed = asideCartShopping.classList.contains('inactive')
@@ -26,20 +29,31 @@ function toggleMobileMenu() {
         asideCartShopping.classList.add('inactive')
     }
     mobileMenu.classList.toggle('inactive')
+    
+    closedProductsDetails()
 }
 
 function toggleCarsDetails() {
     const mobileMenuClosed = mobileMenu.classList.contains('inactive')
-    const desktopMenuClosed = desktopMenu.classList.contains('inactive')
+    const productDetail = detalleProducto.classList.contains('inactive')
 
     if(!mobileMenuClosed) {
         mobileMenu.classList.add('inactive')
     }
 
-    if(!desktopMenuClosed) {
-        desktopMenu.classList.add('inactive')
+    if(!productDetail) {
+        detalleProducto.classList.add('inactive')
     }
     asideCartShopping.classList.toggle('inactive')
+}
+
+function openProductsDetails() {
+    asideCartShopping.classList.add('inactive')
+    detalleProducto.classList.remove('inactive')
+}
+
+function closedProductsDetails() {
+    detalleProducto.classList.add('inactive')
 }
 
  const productList = [
@@ -48,12 +62,15 @@ function toggleCarsDetails() {
     {name : "Computadora", price: "620", image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
  ]
 
-      for(product of productList) {
+ function rederProductos(productos) {
+
+    for(product of productos) {
         const productCard = document.createElement('div')
         productCard.classList.add('product-card')
 
         const productImg = document.createElement('img')
         productImg.setAttribute('src', product.image)
+        productImg.addEventListener('click', openProductsDetails)
 
         const productIfo = document.createElement('div')
         productIfo.classList.add('product-info')
@@ -77,3 +94,6 @@ function toggleCarsDetails() {
         productFigure.appendChild(imageCart)
         cardsContainer.appendChild(productCard)
       }
+ }
+
+ rederProductos(productList)
