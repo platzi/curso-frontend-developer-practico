7793dc554c447b5606ed31e47947476d278636a7
KarenVane24 <karenvanesa140@gmail.com>
2024-03-11 11:39:49 -0500

    Fucionando menu de movile

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopmenu =document.querySelector('.desktop-menu');
+const menuHamburger= document.querySelector('.menuHamburger');
+const mobileMenu= document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu); //Crear funcion con ese nombre
 
 function toggleDesktopMenu(){
     desktopmenu.classList.toggle('inactive');
+}
+
+menuHamburger.addEventListener('click',toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
