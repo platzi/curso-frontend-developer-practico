4a71d7b5593979c758603e922a88c745aa491012
KarenVane24 <karenvanesa140@gmail.com>
2024-03-11 12:10:41 -0500

    Carrito de comptras

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopmenu =document.querySelector('.desktop-menu');
+
 const menuHamburger= document.querySelector('.menuHamburger');
 const mobileMenu= document.querySelector('.mobile-menu');
 
+const carritoCompras=document.querySelector('.carritoCompras');
+const detallecarrito=document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu); //Crear funcion con ese nombre
 
 function toggleDesktopMenu(){
+    detallecarrito.classList.add('inactive');
     desktopmenu.classList.toggle('inactive');
 }
 
 menuHamburger.addEventListener('click',toggleMobileMenu);
 
 function toggleMobileMenu(){
+    
+    detallecarrito.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+carritoCompras.addEventListener('click',toggleproduct)
+
+function toggleproduct(){
+    mobileMenu.classList.add('inactive');
+    detallecarrito.classList.toggle('inactive');
+    
 }
