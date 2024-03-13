40ec6f4b717b82bc3c070978ae98afdbe7b74d3b
Thom037 <thomasmorales037@gmail.com>
2024-03-13 08:50:57 -0500

    fusión de menú con desplegable en index

---
+++ main.js
@@ -0,0 +1,12 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
+
+//ClassList.toggle quita o pone una clase
+function toggleDesktopMenu(){
+    console.log('hizo click')
+    desktopMenu.classList.toggle('inactive')
+}
+
+
